d39e30c57920cede7f612e975f5d2ba6274598a8
Bertrand Dunogier <bd@ez.no>
2011-05-27 18:03:37 +0200

    Added properties list to ezp\Content\Content

--- public/content/content.php
+++ public/content/content.php
@@ -13,6 +13,21 @@
  * This class represents a Content item
  *
  * It is used for both input and output manipulation.
+ *
+ * @property-read integer $id
+ *                The Content's ID, automaticaly assigned by the persistence layer
+ * @property eZDateTime $creationDate
+ *           The date the object was created
+ * @property-read integer status
+ *                The Content's status, as one of the ezp\Content::STATUS_* constants
+ * @property string $remoteId
+ *           A custom ID for the object. It receives a default value, but can be changed to anything
+ * @property-read ezp\Content\VersionCollection versions
+ * @property-read ezp\Content\TranslationCollection locations
+ * @property ezp\User $owner
+ * @property ezp\Content\RelationCollection $relations
+ * @property ezp\Content\RelationCollection $reverseRelations
+ *
  * @package API
  * @subpackage Content
  */
@@ -20,6 +35,21 @@ namespace ezp\Content;
 
 class Content
 {
+    public function __get( $property )
+    {
+        throw new ezcBasePropertyNotFoundException( $property );
+    }
+
+    public function __isset( $property )
+    {
+        throw new ezcBasePropertyNotFoundException( $property );
+    }
+
+    public function __set( $property, $value )
+    {
+        throw new ezcBasePropertyNotFoundException( $property );
+    }
+
     public function __set_state( $array )
     {
 
