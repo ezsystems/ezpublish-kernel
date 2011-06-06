61e7401050e1586540b8b5afeffd45c8d8ecb45f
Jérôme Vieilledent <lolautruche@gmail.com>
2011-06-06 13:02:58 +0200

    Added content load & update stub

--- ezp/stubs/content_load.php
+++ ezp/stubs/content_load.php
@@ -1,7 +1,32 @@
 <?php
 /**
- * Load a Content based on its Id (1)
+ * Load a Content based on its Id (60)
+ * Assume that this content is a folder
  */
-$repository = ezp\Content\Repository::get();
-$content = $repository->loadContent( 1 );
+use ezp\Content\Repository as ContentRepository;
+
+$repository = ContentRepository::get();
+try
+{
+    $content = $repository->loadContent( 60 );
+}
+catch ( ezp\Content\ContentNotFoundException $e )
+{
+    echo "Content could not be found in the repository !";
+    exit;
+}
+
+// Loop against fields.
+// $identifier is the attribute identifier
+// $value is the corresponding value object
+echo "Content '{$content}' has following fields:\n";
+foreach ( $content->fields as $identifier => $value )
+{
+    echo "Field '{$identifier}': {$value}\n"; // Using $value __toString() method
+}
+
+// Now updating content
+$content->fields["name"] = new ezp\Content\Fields\String( "New content name" );
+$repository->getContentService()->update( $content );
+
 ?>
