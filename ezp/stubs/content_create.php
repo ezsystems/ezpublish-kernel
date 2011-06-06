54a407415560085617d1af64c30bc46222ea1eff
Jérôme Vieilledent <lolautruche@gmail.com>
2011-06-06 17:56:21 +0200

    Simplified field value objects manipulation and added some comments

--- ezp/stubs/content_create.php
+++ ezp/stubs/content_create.php
@@ -1,24 +1,29 @@
 <?php
 /*
  * Assume that :
- * - $contentType is a valid content type object (formerly "content class") => \ezp\Content\ContentType
  * - $parentLocation is a valid Location (formerly "parent node") => \ezp\Content\Location
+ * - Current default locale is eng-GB.
+ * - Locale fre-FR has also been set in the system
  */
 
 use ezp\Content\Content;
 use ezp\Content\ContentType;
 use ezp\Content\Repository as ContentRepository;
+use ezp\Content\Fields;
 
-// Here $contentType represent a "Folder"
-// Current default locale is eng-GB and fre-FR has been set in the system
-$content = new Content( ContentType::ByIdentifier( "folder" ) );
-$content->fields['name'] = new ezp\Content\Fields\String( "My folder name" );
-$content->fields['description'] = new ezp\Content\Fields\XMLText( "<p>This is the <strong>HTML description</strong></p>" );
+$content = new Content( ContentType::byIdentifier( "folder" ) );
+/*
+ * $content->fields have been set default value objects with default values set for content type
+ * Setting a new value object to a field will unset the previous one (and avoid potential memory leaks)
+ * Value object manipulation is totally up to the field type
+ */
+$content->fields['name'] = new Fields\String( "My folder name" );
+$content->fields['description'] = new Fields\XMLText( "<p>This is the <strong>HTML description</strong></p>" );
 
 // Now set an fre-FR translation
 $content->addTranslation( "fre-FR" );
-$content->translations["fre-FR"]->fields["name"] = new ezp\Content\Fields\String( "Nom du dossier" );
-$content->translations["fre-FR"]->fields["description"] = new ezp\Content\Fields\XMLText( "<p>Ceci est la <strong>description HTML</strong></p>" );
+$content->translations["fre-FR"]->fields["name"] = new Fields\String( "Nom du dossier" );
+$content->translations["fre-FR"]->fields["description"] = new Fields\XMLText( "<p>Ceci est la <strong>description HTML</strong></p>" );
 
 // Get the content service from the repository and insert the new content
 try
