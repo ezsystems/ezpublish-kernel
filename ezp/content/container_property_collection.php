23d122433d9a7c3da3c4896a8e5b8b7721f51bf1
Damien Pobel <dp@ez.no>
2011-06-13 10:39:30 +0200

    Added ContainerProperty class to handle sorting

---
+++ ezp/content/container_property_collection.php
@@ -0,0 +1,24 @@
+<?php;
+/**
+ * File containing the ezp\Content\ContainerPropertyCollection class.
+ *
+ * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ * @package API
+ * @subpackage Content
+ */
+
+/**
+ * This class represents a Container Property collection
+ *
+ * @package API
+ * @subpackage Content
+ */
+namespace ezp\Content;
+
+class ContainerPropertyCollection extends BaseCollection
+{
+
+}
+?>
