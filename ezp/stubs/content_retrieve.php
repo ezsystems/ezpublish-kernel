ccc21dd5ea45019c38c79a6895892b8b9219a1fd
Jérôme Vieilledent <lolautruche@gmail.com>
2011-06-22 14:50:04 +0200

    Update content_retrieve stub

--- ezp/stubs/content_retrieve.php
+++ ezp/stubs/content_retrieve.php
@@ -1,13 +1,20 @@
 <?php
 use ezp\Content\Repository as ContentRepository;
 
-$contentService = ContentRepository::get()->getContentService();
 $c = $contentService->createCriteria();
-$c->subtree( $parentLocation )
-     ->where( $c->field["folder/name"]->eq( "My folder name" ) )
-     ->limit( 5 )
-     ->offset( 0 );
+$c->where( // andCondition() is implicit
+    $c->location->isChildOf( $parentLocation ), // Direct children
+    // $c->location->subTree( $parentLocation ), // Recursive
+    $c->type( "folder" ),
+    $c->field->eq( "show_children", true ),
+    $c->meta->gte( "published", new DateTime( "yesterday" ) ),
+    $c->logic->orCondition(
+        $c->field->eq( "name", "My folder name" ),
+        $c->field->eq( "name", "Another name" )
+    )
+)
+->limit( 5 )
+->offset( 0 );
 
 $collection = $contentService->find( $c );
-
 ?>
