239e85cb929b138ac8d93d0ef0d78638354e55f7
Ole Marius Smestad <oms@ez.no>
2011-06-28 11:07:58 +0200

    Added file level license header to the io package.

--- ezp/persistence/io/FileSystemStorage.php
+++ ezp/persistence/io/FileSystemStorage.php
@@ -1,5 +1,14 @@
 <?php
+/**
+ * File containing the FileSystemStorage class
+ *
+ * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ *
+ */
+
 namespace ezp\persistence\io;
+
 /**
  * @package ezp.persistence.io
  */
