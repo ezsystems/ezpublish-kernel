123b7a3d16eb897933b37544e215fcd264ae49b2
Ole Marius Smestad <oms@ez.no>
2011-06-28 10:46:31 +0200

    Added file level license headers for the content package.

--- ezp/persistence/content/SectionHandler.php
+++ ezp/persistence/content/SectionHandler.php
@@ -1,5 +1,14 @@
 <?php
+/**
+ * File containing the SectionHandler interface
+ *
+ * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ *
+ */
+
 namespace ezp\persistence\content;
+
 /**
  * @package ezp.persistence.content
  */
--- ezp/persistence/content/values/Content.php
+++ ezp/persistence/content/values/Content.php
@@ -1,5 +1,14 @@
 <?php
+/**
+ * File containing the Content class
+ *
+ * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ *
+ */
+
 namespace ezp\persistence\content\values;
+
 /**
  * @package ezp.persistence.content.values
  */
