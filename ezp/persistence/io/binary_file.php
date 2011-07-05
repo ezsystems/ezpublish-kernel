1a71dbeecdcc1b687d9617f7767fc75f1f9dd9fd
André R <ar@ez.no>
2011-07-05 13:53:48 +0200

    PHPDoc & Autoload convention fixes: io

--- ezp/persistence/io/binary_file.php
+++ ezp/persistence/io/binary_file.php
@@ -4,15 +4,17 @@
  *
  * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
- *
+ * @package ezp
+ * @subpackage persistence_io
  */
 
 namespace ezp\persistence\io;
 
 /**
- * @package ezp.persistence.io
+ * @package ezp
+ * @subpackage persistence_io
  */
-class BinaryFile 
+class BinaryFile
 {
 	/**
 	 */
--- ezp/persistence/io/file_system_storage.php
+++ ezp/persistence/io/file_system_storage.php
@@ -4,55 +4,62 @@
  *
  * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
- *
+ * @package ezp
+ * @subpackage persistence_io
  */
 
 namespace ezp\persistence\io;
 
 /**
- * @package ezp.persistence.io
+ * @package ezp
+ * @subpackage persistence_io
  */
-class FileSystemStorage implements BinaryFileStorageInterface 
+class FileSystemStorage implements BinaryFileStorageInterface
 {
 	/**
 	 */
 	public $contains;
 
 	/**
-	 * @param string fileIdentifier
+	 * @param string $fileIdentifier
 	 * @return boolean
 	 */
-	public function storeFile($fileIdentifier) {
+	public function storeFile( $fileIdentifier )
+    {
 		// Not yet implemented
 	}
 
 	/**
-	 * @param string fileIdentifier
+	 * @param string $fileIdentifier
 	 * @return FileReference
 	 */
-	public function getFile($fileIdentifier) {
+	public function getFile( $fileIdentifier )
+    {
 		// Not yet implemented
 	}
 
 	/**
-	 * @param string fileIdentifier
+	 * @param string $fileIdentifier
 	 * @return FileChunk
 	 */
-	public function streamFile($fileIdentifier) {
+	public function streamFile( $fileIdentifier )
+    {
 		// Not yet implemented
 	}
 
 	/**
-	 * @param string fileIdentifier
+	 * @param string $fileIdentifier
 	 * @return boolean
 	 */
-	public function exists($fileIdentifier) {
+	public function exists( $fileIdentifier )
+    {
 		// Not yet implemented
 	}
 
 	/**
 	 */
-	public function authenticate() {
+	public function authenticate()
+    {
 		// Not yet implemented
 	}
 }
