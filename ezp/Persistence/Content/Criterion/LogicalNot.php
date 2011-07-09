1d36770ca140a1dce9b81b2f627099593ac3e5d6
Bertrand Dunogier <bd@ez.no>
2011-07-09 19:17:17 +0200

    Added Criterion/LogicalNot

---
+++ ezp/Persistence/Content/Criterion/LogicalNot.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * File containing the ezp\Persistence\Content\Criterion\LogicalNot class.
+ *
+ * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPL v2
+ * @version //autogentag//
+ */
+
+namespace ezp\Persistence\Content\Criterion;
+use ezp\Persistence\Content\Criterion;
+
+/**
+ * A NOT logical criterion
+ *
+ * @package ezp.persistence.content.criterion
+ */
+class LogicalNot extends LogicalOperator
+{
+    /**
+     * Creates a new NOT logic criterion.
+     *
+     * Will match of the given criterion doesn't match
+     *
+     * @param array(Criterion) $criteria One criterion, as a an array
+     *
+     * @throws InvalidArgumentException if more than one criterion is given in the array parameter
+     */
+    public function __construct( array $criterion )
+    {
+        if ( count( $criterion ) > 1 )
+        {
+            throw new \InvalidArgumentException( "This operator only accepts one Criterion" );
+        }
+
+        parent::__construct( $criterion );
+    }
+}
+?>
