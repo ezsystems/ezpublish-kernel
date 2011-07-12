f7befeeef9b45e2d2cb1997f1d0822ab5a467a40
Kore Nordmann <mail@kore-nordmann.de>
2011-07-12 17:33:55 +0200

    Fixed wrong string replace
    Thanks to Andre for pointing that out

--- ezp/Persistence/User/Interfaces/UserHandler.php
+++ ezp/Persistence/User/Interfaces/UserHandler.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * File containing the UserHandler mixederface
+ * File containing the UserHandler interface
  *
  * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
@@ -18,7 +18,7 @@ namespace ezp\Persistence\User\Interfaces;
  * @subpackage persistence_user
  */
 use \ezp\Persistence\User;
-mixederface UserHandler
+interface UserHandler
 {
 
     /**
