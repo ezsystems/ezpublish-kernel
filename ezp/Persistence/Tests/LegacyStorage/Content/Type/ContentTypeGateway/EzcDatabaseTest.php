3e8a2f89cb98a29bb6e7de95ea1b57ef8b2dfbac
Tobias Schlitt <toby@php.net>
2011-07-18 21:28:05 +0200

    Implemented: Loading of database fixtures.
    This methods loads a database fixture in form of a row array per table
    into the database. Seems to work out fine, so we might take this over to
    the base TestCase.

--- ezp/Persistence/Tests/LegacyStorage/Content/Type/ContentTypeGateway/EzcDatabaseTest.php
+++ ezp/Persistence/Tests/LegacyStorage/Content/Type/ContentTypeGateway/EzcDatabaseTest.php
@@ -38,6 +38,36 @@ class EzcDatabaseTest extends TestCase
     }
 
     /**
+     * Inserts database fixture from $fileName.
+     *
+     * @param string $fileName 
+     * @return void
+     */
+    protected function insertFixture( $fileName )
+    {
+        $data = require( __DIR__ . '/_fixtures/' . $fileName );
+        $db   = $this->getDatabaseHandler();
+
+        foreach ( $data as $table => $rows )
+        {
+            foreach ( $rows as $row )
+            {
+                $q = $db->createInsertQuery();
+                $q->insertInto( $db->quoteIdentifier( $table ) );
+                foreach ( $row as $col => $val )
+                {
+                    $q->set(
+                        $db->quoteIdentifier( $col ),
+                        $q->bindValue( $val )
+                    );
+                }
+                $stmt = $q->prepare();
+                $stmt->execute();
+            }
+        }
+    }
+
+    /**
      * Returns the test suite with all tests declared in this class.
      *
      * @return \PHPUnit_Framework_TestSuite
