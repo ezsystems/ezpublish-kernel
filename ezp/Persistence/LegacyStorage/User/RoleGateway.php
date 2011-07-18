6d8760e4bffc9d707e2486e84665dba0fe6d3146
Kore Nordmann <mail@kore-nordmann.de>
2011-07-18 12:57:07 +0200

    Added base class and stub for role gateway

---
+++ ezp/Persistence/LegacyStorage/User/RoleGateway.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * File containing the ContentTypeGateway class
+ *
+ * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace ezp\Persistence\LegacyStorage\User;
+
+/**
+ * Base class for content type gateways.
+ */
+abstract class RoleGateway
+{
+    /**
+     * Create new role
+     *
+     * @param Role $role
+     * @return Role
+     */
+    abstract public function createRole( Role $role );
+
+    /**
+     * Update role
+     *
+     * @param Role $role
+     */
+    abstract public function updateRole( Role $role );
+
+    /**
+     * Delete the specified role
+     *
+     * @param mixed $roleId
+     */
+    abstract public function deleteRole( $roleId );
+}
