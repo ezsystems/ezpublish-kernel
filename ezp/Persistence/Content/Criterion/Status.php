43c6de4004e63265081627abc6ece193d8526369
Bertrand Dunogier <bd@ez.no>
2011-07-19 12:33:51 +0200

    Added a QueryBuilder stub

---
+++ ezp/Persistence/Content/Criterion/Status.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * File containing the ezp\Persistence\Content\Criterion\Status class
+ *
+ * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ *
+ */
+
+namespace ezp\Persistence\Content\Criterion;
+use ezp\Persistence\Content\Criterion,
+    ezp\Persistence\Content\Interfaces\Criterion as CriterionInterface;
+
+/**
+ * A criterion that matches content based on its status
+ *
+ * Supported operators:
+ * - IN: will match from a list of statuses
+ * - EQ: will match against one status
+ */
+class Status extends Criterion implements CriterionInterface
+{
+    /**
+     * Creates a new Status criterion
+     * @param null $target Not used
+     * @param string $operator eq / in
+     * @param string $value Status: self::STATUS_ARCHIVED, self::STATUS_DRAFT, self::STATUS_PUBLISHED
+     */
+    public function __construct( $target, $operator, $value )
+    {
+        if ( !is_array( $value ) )
+        {
+            $testValue = array( $value );
+        }
+        else
+        {
+            $testValue = $value;
+        }
+        foreach ( $testValue as $statusValue )
+        {
+            if ( !in_array( $statusValue, array( self::STATUS_ARCHIVED, self::STATUS_DRAFT, self::STATUS_PUBLISHED ) ) )
+            {
+                throw new InvalidArgumentException( "Invalid status $statusValue" );
+            }
+        }
+        parent::__construct( $target, $operator, $value );
+    }
+
+    public function getSpecifications()
+    {
+        return array(
+            new OperatorSpecifications(
+                Operator::IN,
+                OperatorSpecifications::FORMAT_ARRAY,
+                OperatorSpecifications::TYPE_STRING
+            ),
+            new OperatorSpecifications(
+                Operator::EQ,
+                OperatorSpecifications::FORMAT_SINGLE,
+                OperatorSpecifications::TYPE_STRING
+            ),
+        );
+    }
+
+    /**
+     * Status constant: draft
+     */
+    const STATUS_DRAFT = "draft";
+
+    /**
+     * Status constant: published
+     */
+    const STATUS_PUBLISHED = "published";
+
+    /**
+     * Status constant: archived
+     */
+    const STATUS_ARCHIVED = "archived";
+}
+?>
---
+++ ezp/Stubs/ContentQuery.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * File containing the content_query API stub
+ *
+ * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2.0
+ * @package ezp
+ * @subpackage stub
+ */
+
+use ezp\base\Repository;
+use ezp\content;
+
+$contentService = Repository::get()->getContentService();
+$qb = $contentService->getQueryBuilder();
+
+// a full criteria
+$qb->addCriteria(
+    $qb->fulltext->like( 'eZ Publish' ),
+    $qb->urlAlias->like( '/cms/amazing/*' ),
+    $qb->contentType->eq( 'blog_post' ),
+    $qb->field->eq( 'author', 'community@ez.no' )
+);
+
+// individual criteria objects
+
+// creation date
+$qb->dateMetadata->between( 'created', strtotime( 'last month' ), strtotime( 'last week' ) );
+
+// section
+$qb->section->in( array( 2, 6 ) );
+?>
