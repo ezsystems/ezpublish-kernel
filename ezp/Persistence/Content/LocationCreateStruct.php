7dc7e7c2d07a1a917e7deb6e9f4ef5614b67c410
Ole Marius Smestad <oms@ez.no>
2011-07-19 10:47:09 +0200

    Added LocationCreateStruct

---
+++ ezp/Persistence/Content/LocationCreateStruct.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * File containing the LocationCreateStruct class
+ *
+ * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+class LocationCreateStruct extends \ezp\Persistence\AbstractValueObject
+{
+    /**
+     * Location position aka priority
+     *
+     * Position of the Location among its siblings when sorted using sort order.
+     *
+     * @var int
+     */
+    public $position;
+
+    /**
+     * @var bool
+     */
+    public $hidden;
+
+    /**
+     * @var bool
+     */
+    public $invisible;
+
+    /**
+     * Remote ID.
+     *
+     * @var mixed
+     */
+    public $remoteId;
+
+    /**
+     * ID of the corresponding {@link Content}.
+     *
+     * @var mixed Content ID.
+     */
+    public $contentId;
+
+    /**
+     * Legacy format of the url alias.
+     *
+     * This field might be removed in a later version.
+     *
+     * @var string
+     */
+    public $pathIdentificationString;
+
+    /**
+     * The materialized path of the location entry.
+     *
+     * @var string
+     */
+    public $pathString;
+
+    /**
+     * Timestamp of the latest update of a content object in a sub location.
+     *
+     * @var int
+     */
+    public $modifiedSubLocation;
+
+    /**
+     * Identifier of the main location.
+     *
+     * If the content object in this location has multiple locations,
+     * $mainLocationId will point to the main one.
+     *
+     * @var mixed
+     */
+    public $mainLocationId;
+
+    /**
+     * Depth location has in the location tree.
+     *
+     * @var int
+     */
+    public $depth;
+}
