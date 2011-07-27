767254fe6438bd920e069fcaf6190bd218b5c2de
Tobias Schlitt <toby@php.net>
2011-07-27 10:34:57 +0200

    Implemented: Initial stub for content Mapper.

---
+++ ezp/Persistence/LegacyStorage/Content/Mapper.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * File containing the Mapper class
+ *
+ * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ *
+ */
+
+namespace ezp\Persistence\LegacyStorage\Content;
+
+/**
+ *
+ */
+abstract class Mapper
+{
+    // TODO: Implement
+}
