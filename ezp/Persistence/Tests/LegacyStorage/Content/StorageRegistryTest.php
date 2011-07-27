7246cdb6f8c79addb64f45c889d436916a730f6f
Tobias Schlitt <toby@php.net>
2011-07-27 11:54:15 +0200

    Implemented: Tests for storage registry.

---
+++ ezp/Persistence/Tests/LegacyStorage/Content/StorageRegistryTest.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * File contains: ezp\Persistence\Tests\LegacyStorage\Content\StorageRegistryTest class
+ *
+ * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace ezp\Persistence\Tests\LegacyStorage\Content;
+use ezp\Persistence\Tests\LegacyStorage\TestCase,
+    ezp\Persistence\LegacyStorage\Content\StorageRegistry,
+    ezp\Persistence\Fields\StorageInterface;
+
+/**
+ * Test case for StorageRegistry
+ */
+class StorageRegistryTest extends TestCase
+{
+    /**
+     * @return void
+     * @covers ezp\Persistence\LegacyStorage\Content\StorageRegistry::register
+     */
+    public function testRegister()
+    {
+        $registry = new StorageRegistry();
+
+        $storage = $this->getStorageMock();
+
+        $registry->register( 'some-type', $storage );
+
+        $this->assertPropertySame(
+            array(
+                'some-type' => $storage,
+            ),
+            'storageMap',
+            $registry
+        );
+    }
+
+    /**
+     * @return void
+     * @covers ezp\Persistence\LegacyStorage\Content\StorageRegistry::getStorage
+     */
+    public function testGetStorage()
+    {
+        $registry = new StorageRegistry();
+
+        $storage = $this->getStorageMock();
+        $registry->register( 'some-type', $storage );
+
+        $res = $storage->getStorage( 'some-type' );
+
+        $this->assertSame(
+            $storage,
+            $res
+        );
+    }
+
+    /**
+     * Returns a mock for StorageInterface
+     *
+     * @return StorageInterface
+     */
+    protected function getStorageMock()
+    {
+        return $this->getMock(
+            'ezp\Persistence\Fields\StorageInterface'
+        );
+    }
+
+    /**
+     * Returns the test suite with all tests declared in this class.
+     *
+     * @return \PHPUnit_Framework_TestSuite
+     */
+    public static function suite()
+    {
+        return new \PHPUnit_Framework_TestSuite( __CLASS__ );
+    }
+}
