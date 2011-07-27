dd2ff3aa9757a4c991dbe502bfc900746ae19a92
Tobias Schlitt <toby@php.net>
2011-07-27 16:36:08 +0200

    Implemented: Mapper stub.

--- ezp/Persistence/LegacyStorage/Content/Mapper.php
+++ ezp/Persistence/LegacyStorage/Content/Mapper.php
@@ -9,11 +9,61 @@
  */
 
 namespace ezp\Persistence\LegacyStorage\Content;
+use ezp\Persistence\Content,
+    ezp\Persistence\Content\ContentCreateStruct,
+    ezp\Persistence\Content\Field,
+    ezp\Persistence\Content\Version;
 
 /**
  *
  */
-abstract class Mapper
+class Mapper
 {
-    // TODO: Implement
+    /**
+     * Creates a Content from the given $struct
+     *
+     * @param ContentCreateStruct $struct
+     * @return Content
+     */
+    public function createContentFromCreateStruct( ContentCreateStruct $struct )
+    {
+        throw new \RuntimeException( 'Not implemented, yet.' );
+    }
+
+    /**
+     * Creates a new version for the given $content
+     *
+     * @param Content $content
+     * @param int $versionNo
+     * @return Version
+     * @todo: created, modified, initial_language_id, status, user_id?
+     */
+    public function createVersionForContent( Content $content, $versionNo )
+    {
+        throw new \RuntimeException( 'Not implemented, yet.' );
+        /*
+        $version = new Version();
+
+        $version->versionNo = $versionNo;
+        $version->created   = time();
+        $version->modified  = $version->created;
+        $version->creatorId = $content->ownerId;
+        // @todo: Define
+        $version->state     = 1;
+        $version->contentId = $content->id;
+
+        return $version;
+        */
+    }
+
+    /**
+     * Converts value of $field to storage value
+     *
+     * @param Field $field
+     * @return StorageFieldValue
+     */
+    public function convertToStorageValue( Field $field )
+    {
+        throw new \RuntimeException( 'Not implemented, yet.' );
+    }
 }
