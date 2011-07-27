5a4609f437d4231805ac71b89b346f98ad8c5108
Tobias Schlitt <toby@php.net>
2011-07-27 10:34:33 +0200

    Implemented: Initial stub of ContentGateway.

---
+++ ezp/Persistence/LegacyStorage/Content/ContentGateway.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * File containing the ContentGateway base class
+ *
+ * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ *
+ */
+
+namespace ezp\Persistence\LegacyStorage\Content;
+
+/**
+ * Base class for contentg gateways
+ */
+abstract class ContentGateway
+{
+    // TODO: Implement
+}
