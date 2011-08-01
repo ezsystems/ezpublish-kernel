0740500d9acd75d730c25881072b1ae4c3dbac36
Kore Nordmann <mail@kore-nordmann.de>
2011-08-01 11:43:24 +0200

    Stubbed content locator test

---
+++ ezp/Persistence/Tests/LegacyStorage/Content/ContentLocatorTest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * File contains: ezp\Persistence\Tests\LegacyStorage\Content\ContentLocatorTest class
+ *
+ * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace ezp\Persistence\Tests\LegacyStorage\Content;
+use ezp\Persistence\Tests\LegacyStorage\TestCase,
+    ezp\Persistence\LegacyStorage\Content,
+    ezp\Persistence;
+
+/**
+ * Test case for ContentLocator
+ */
+class ContentLocatorTest extends TestCase
+{
+    /**
+     * Returns the test suite with all tests declared in this class.
+     *
+     * @return \PHPUnit_Framework_TestSuite
+     */
+    public static function suite()
+    {
+        return new \PHPUnit_Framework_TestSuite( __CLASS__ );
+    }
+
+    public function testStub()
+    {
+        $this->markTestIncomplete( "@TODO: Add tests." );
+    }
+}
