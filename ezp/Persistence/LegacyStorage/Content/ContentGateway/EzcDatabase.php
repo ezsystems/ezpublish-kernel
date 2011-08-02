2efc4d7033a2e7e23deba049a7991e2f7cbb533a
Tobias Schlitt <toby@php.net>
2011-08-02 15:26:04 +0200

    Implemented: Methods in ContentGateway.

--- ezp/Persistence/LegacyStorage/Content/ContentGateway/EzcDatabase.php
+++ ezp/Persistence/LegacyStorage/Content/ContentGateway/EzcDatabase.php
@@ -21,14 +21,57 @@ use ezp\Persistence\LegacyStorage\Content\ContentGateway,
 class EzcDatabase extends ContentGateway
 {
     /**
+     * Zeta Components database handler.
+     *
+     * @var ezcDbHandler
+     */
+    protected $dbHandler;
+
+    /**
+     * Creates a new gateway based on $db
+     *
+     * @param ezcDbHandler $db
+     */
+    public function __construct( \ezcDbHandler $db )
+    {
+        $this->dbHandler = $db;
+    }
+
+    /**
      * Inserts a new content object.
      *
      * @param Content $content
      * @return int ID
+     * @todo Race condition by lastInsertId()?
+     * @todo Oracle sequences?
      */
     public function insertContentObject( Content $content )
     {
-        throw new \RuntimeException( 'Not implemented, yet.' );
+        $q = $this->dbHandler->createInsertQuery();
+        $q->insertInto(
+            $this->dbHandler->quoteIdentifier( 'ezcontentobject' )
+        )->set(
+            // @FIXME: Determine version?
+            $this->dbHandler->quoteIdentifier( 'current_version' ),
+            $q->bindValue( 1, null, \PDO::PARAM_INT )
+        )->set(
+            $this->dbHandler->quoteIdentifier( 'name' ),
+            $q->bindValue( $content->name )
+        )->set(
+            $this->dbHandler->quoteIdentifier( 'contentclass_id' ),
+            $q->bindValue( $content->typeId, null, \PDO::PARAM_INT )
+        )->set(
+            $this->dbHandler->quoteIdentifier( 'section_id' ),
+            $q->bindValue( $content->sectionId, null, \PDO::PARAM_INT )
+        )->set(
+            $this->dbHandler->quoteIdentifier( 'owner_id' ),
+            $q->bindValue( $content->ownerId, null, \PDO::PARAM_INT )
+        );
+
+        $stmt = $q->prepare();
+        $stmt->execute();
+
+        return $this->dbHandler->lastInsertId();
     }
 
     /**
@@ -39,7 +82,33 @@ class EzcDatabase extends ContentGateway
      */
     public function insertVersion( Version $version )
     {
-        throw new \RuntimeException( 'Not implemented, yet.' );
+        $q = $this->dbHandler->createInsertQuery();
+        $q->insertInto(
+            $this->dbHandler->quoteIdentifier( 'ezcontentobject_version' )
+        )->set(
+            $this->dbHandler->quoteIdentifier( 'version' ),
+            $q->bindValue( $version->versionNo )
+        )->set(
+            $this->dbHandler->quoteIdentifier( 'modified' ),
+            $q->bindValue( $version->modified, null, \PDO::PARAM_INT )
+        )->set(
+            $this->dbHandler->quoteIdentifier( 'creator_id' ),
+            $q->bindValue( $version->creatorId, null, \PDO::PARAM_INT )
+        )->set(
+            $this->dbHandler->quoteIdentifier( 'created' ),
+            $q->bindValue( $version->created, null, \PDO::PARAM_INT )
+        )->set(
+            $this->dbHandler->quoteIdentifier( 'status' ),
+            $q->bindValue( $version->state, null, \PDO::PARAM_INT )
+        )->set(
+            $this->dbHandler->quoteIdentifier( 'contentobject_id' ),
+            $q->bindValue( $version->contentId, null, \PDO::PARAM_INT )
+        );
+
+        $stmt = $q->prepare();
+        $stmt->execute();
+
+        return $this->dbHandler->lastInsertId();
     }
 
     /**
@@ -55,6 +124,45 @@ class EzcDatabase extends ContentGateway
      */
     public function insertNewField( Content $content, Field $field, StorageFieldValue $value )
     {
-        throw new \RuntimeException( 'Not implemented, yet.' );
+        $q = $this->dbHandler->createInsertQuery();
+        $q->insertInto(
+            $this->dbHandler->quoteIdentifier( 'ezcontentobject_attribute' )
+        )->set(
+            $this->dbHandler->quoteIdentifier( 'contentobject_id' ),
+            $q->bindValue( $content->id, null, \PDO::PARAM_INT )
+        )->set(
+            $this->dbHandler->quoteIdentifier( 'contentclassattribute_id' ),
+            $q->bindValue( $field->fieldDefinitionId, null, \PDO::PARAM_INT )
+        )->set(
+            $this->dbHandler->quoteIdentifier( 'data_type_string' ),
+            $q->bindValue( $field->type )
+        )->set(
+            // @FIXME: Is language code correct?
+            $this->dbHandler->quoteIdentifier( 'language_code' ),
+            $q->bindValue( $field->language )
+        )->set(
+            $this->dbHandler->quoteIdentifier( 'version' ),
+            $q->bindValue( $field->versionNo )
+        )->set(
+            $this->dbHandler->quoteIdentifier( 'data_float' ),
+            $q->bindValue( $value->dataFloat )
+        )->set(
+            $this->dbHandler->quoteIdentifier( 'data_int' ),
+            $q->bindValue( $value->dataInt )
+        )->set(
+            $this->dbHandler->quoteIdentifier( 'data_text' ),
+            $q->bindValue( $value->dataText )
+        )->set(
+            $this->dbHandler->quoteIdentifier( 'sort_key_int' ),
+            $q->bindValue( $value->sortKeyInt )
+        )->set(
+            $this->dbHandler->quoteIdentifier( 'sort_key_string' ),
+            $q->bindValue( $value->sortKeyString )
+        );
+
+        $stmt = $q->prepare();
+        $stmt->execute();
+
+        return $this->dbHandler->lastInsertId();
     }
 }
