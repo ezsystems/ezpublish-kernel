92fdef16e3781ed97d0c1b1b90e69622d87b087b
Tobias Schlitt <toby@php.net>
2011-08-03 16:43:44 +0200

    Implemented: Mapping of database rows to Content objects.
    Note: This does not respect Location objects, yet.

--- ezp/Persistence/LegacyStorage/Content/Mapper.php
+++ ezp/Persistence/LegacyStorage/Content/Mapper.php
@@ -103,9 +103,124 @@ class Mapper
      *
      * @param array $rows
      * @return array(Content)
+     * @todo Take care for locations
      */
     public function extractContentFromRows( array $rows )
     {
-        return array();
+        $contentObjs = array();
+        $versions    = array();
+
+        foreach ( $rows as $row )
+        {
+            $contentId = (int) $row['ezcontentobject_id'];
+            if ( !isset( $contentObjs[$contentId] ) )
+            {
+                $contentObjs[$contentId]  = $this->extractContentFromRow( $row );
+                $versions[$contentId] = array();
+            }
+
+            $versionNo = (int) $row['ezcontentobject_version_version'];
+            if ( !isset( $versions[$contentId][$versionNo] ) )
+            {
+                $versions[$contentId][$versionNo] =
+                    $this->extractVersionFromRow( $row );
+            }
+
+            $versions[$contentId][$versionNo]->fields[] =
+                $this->extractFieldFromRow( $row );
+        }
+
+        foreach ( $contentObjs as $content )
+        {
+            $content->versionInfos = array_values( $versions[$content->id] );
+        }
+        return array_values( $contentObjs );
+    }
+
+    /**
+     * Extracts a Content object from $row
+     *
+     * @param array $row
+     * @return Content
+     * @todo Take care for locations
+     */
+    protected function extractContentFromRow( array $row )
+    {
+        $content = new Content();
+
+        $content->id           = (int) $row['ezcontentobject_id'];
+        $content->name         = $row['ezcontentobject_name'];
+        $content->typeId       = (int) $row['ezcontentobject_contentclass_id'];
+        $content->sectionId    = (int) $row['ezcontentobject_section_id'];
+        $content->ownerId      = (int) $row['ezcontentobject_owner_id'];
+        $content->versionInfos = array();
+        $content->locations    = array();
+
+        return $content;
+    }
+
+    /**
+     * Extracts a Version from the given $row
+     *
+     * @param array $row
+     * @return Version
+     */
+    protected function extractVersionFromRow( array $row )
+    {
+        $version = new Version();
+
+        $version->id        = (int) $row['ezcontentobject_version_id'];
+        $version->versionNo = (int) $row['ezcontentobject_version_version'];
+        $version->modified  = (int) $row['ezcontentobject_version_modified'];
+        $version->creatorId = (int) $row['ezcontentobject_version_creator_id'];
+        $version->created   = (int) $row['ezcontentobject_version_created'];
+        $version->state     = (int) $row['ezcontentobject_version_status'];
+        $version->contentId = (int) $row['ezcontentobject_version_contentobject_id'];
+        $version->fields    = array();
+
+        return $version;
+    }
+
+    /**
+     * Extracts a Field from $row
+     *
+     * @param array $row
+     * @return Field
+     */
+    protected function extractFieldFromRow( array $row )
+    {
+        $field = new Field();
+
+        $field->id                = (int) $row['ezcontentobject_attribute_id'];
+        $field->fieldDefinitionId = (int) $row['ezcontentobject_attribute_contentclassattribute_id'];
+        $field->type              = $row['ezcontentobject_attribute_data_type_string'];
+        $field->value             = $this->extractFieldValueFromRow( $row, $field->type );
+        $field->language          = $row['ezcontentobject_attribute_language_code'];
+        $field->versionNo         = (int) $row['ezcontentobject_attribute_version'];
+
+        return $field;
+    }
+
+    /**
+     * Extracts a FieldValue of $type from $row
+     *
+     * @param array $row
+     * @param string $type
+     * @return FieldValue
+     * @throws \ezp\Persistence\LegacyStorage\Exception\FieldValueConverterNotFoundException
+     *         if the necessary converter for $type could not be found.
+     */
+    protected function extractFieldValueFromRow( array $row, $type )
+    {
+        $storageValue = new StorageFieldValue();
+
+        $storageValue->dataFloat     = (float) $row['ezcontentobject_attribute_data_float'];
+        $storageValue->dataInt       = (int) $row['ezcontentobject_attribute_data_int'];
+        $storageValue->dataText      = $row['ezcontentobject_attribute_data_text'];
+        $storageValue->sortKeyInt    = (int) $row['ezcontentobject_attribute_sort_key_int'];
+        $storageValue->sortKeyString = $row['ezcontentobject_attribute_sort_key_string'];
+
+        $converter = $this->converterRegistry->getConverter( $type );
+        return $converter->toFieldValue( $storageValue );
     }
 }
