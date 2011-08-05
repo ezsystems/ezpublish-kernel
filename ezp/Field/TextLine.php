069091bf3c21a075f42cecf932e8cfcab79ce9a9
Ole Marius Smestad <oms@ez.no>
2011-08-05 16:30:19 +0200

    Added initialy prototype of text line field type.

---
+++ ezp/Field/TextLine.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * File containing the TextLine class
+ *
+ * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace ezp\Field;
+use ezp\Field\FieldType;
+
+class TextLine extends FieldType
+{
+    public $fieldTypeString = 'ezstring';
+    protected $defaultValue = '';
+    protected $isSearchable = true;
+    protected $isTranslateable = true;
+}
