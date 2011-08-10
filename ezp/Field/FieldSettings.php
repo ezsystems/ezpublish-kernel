92bdc64e53160dadedac7260d19607515663c00f
Ole Marius Smestad <oms@ez.no>
2011-08-10 14:42:45 +0200

    Updated FieldSettings to only allow pre-defined keys to be set.

--- ezp/Field/FieldSettings.php
+++ ezp/Field/FieldSettings.php
@@ -8,11 +8,31 @@
  */
 
 namespace ezp\Field;
-use \ArrayObject;
+use \ArrayObject,
+    ezp\Base\Exception\PropertyPermission;
 
 /**
  * Container for field type specific properties.
  */
-class FieldSettings extends \ArrayObject
+class FieldSettings extends ArrayObject
 {
+    /**
+     * Only allows existing indexes to be updated.
+     *
+     * This is so that only settings specified by a field type can be set.
+     *
+     * @internal
+     * @throws ezp\Base\Exception\PropertyPermission
+     * @param string|int $index
+     * @param mixed $value
+     * @return void
+     */
+    public function offsetSet( $index, $value )
+    {
+        if ( !parent::offsetExists( $index ) )
+        {
+            throw new PropertyPermission( $index, PropertyPermission::WRITE, __CLASS__ );
+        }
+           parent::offsetSet( $index, $value );
+    }
 }
