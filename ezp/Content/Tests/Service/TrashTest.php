464f1062556fcb16959abe7e4afdd45aa6f91380
Patrick Allaert <pa@ez.no>
2011-08-31 17:51:50 +0200

    Fixed: doc

--- ezp/Content/Tests/Service/TrashTest.php
+++ ezp/Content/Tests/Service/TrashTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * File contains: ezp\Content\Tests\Location\ServiceTest class
+ * File contains: ezp\Content\Tests\Service\TrashTest class
  *
  * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
