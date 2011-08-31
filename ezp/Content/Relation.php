a1301a2295e1a7ddec81b2740b91abad0a781aa2
Patrick Allaert <pa@ez.no>
2011-08-31 19:09:49 +0200

    Added: Relation DO implementation

--- ezp/Content/Relation.php
+++ ezp/Content/Relation.php
@@ -8,13 +8,79 @@
  */
 
 namespace ezp\Content;
-use ezp\Base\Model;
+use ezp\Content,
+    ezp\Base\Model,
+    ezp\Base\Exception\InvalidArgumentValue,
+    ezp\Persistence\Content\Relation as RelationValue;
 
 /**
- * This class represents a Relationship between Contents
+ * This class represents a Content Relation
  *
+ * @property-read int $id
+ * @property-read int $sourceContentId
+ * @property-read int $sourceContentVersion
+ * @property-read int $destinationContentId
+ * @property-read int $type Bitmask
+ * @property-read \ezp\Content $content Associated Content object
  */
 class Relation extends Model
 {
+    // Those constants relates to eZContentObject::RELATION_*
+    const COMMON = 1;
+    const EMBED = 2;
+    const LINK = 4;
+    const ATTRIBUTE = 8;
+
+    /**
+     * @var array Readable of properties on this object
+     */
+    protected $readWriteProperties = array(
+        "id" => false,
+        "sourceContentId" => true,
+        "sourceContentVersion" => true,
+        "destinationContentId" => false,
+        "type" => false,
+        "content" => false,
+    );
+
+    /**
+     * Associated Content object
+     *
+     * @var \ezp\Content
+     */
+    protected $content;
+
+    /**
+     * Type bitmask
+     *
+     * @var int
+     */
+    protected $type;
+
+    /**
+     * Setups a Relation object
+     *
+     * @param int $type
+     * @param \ezp\Content $content
+     */
+    public function __construct( $type, Content $content )
+    {
+        if ( !is_int( $type ) || $type & ~( self::COMMON | self::EMBED | self::LINK | self::ATTRIBUTE ) )
+            throw new InvalidArgumentValue( "type", $type );
+
+        $this->properties = new RelationValue;
+        $this->properties->destinationContentId = $content->id;
+        $this->type = $type;
+        $this->content = $content;
+    }
+
+    /**
+     * Returns the associated Content
+     *
+     * @return \ezp\Content
+     */
+    protected function getContent()
+    {
+        return $this->content;
+    }
 }
-?>
---
+++ ezp/Content/Tests/RelationTest.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * File contains: ezp\Content\Tests\RelationTest class
+ *
+ * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace ezp\Content\Tests;
+use ezp\Content,
+    ezp\Content\Type,
+    ezp\Content\Relation,
+    ezp\Persistence\Content\Relation as RelationValue,
+    PHPUnit_Framework_TestCase;
+
+/**
+ * Test case for Relation class
+ */
+class RelationTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var \ezp\Content
+     */
+    protected $content;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        // setup a content type & content object of use by tests, fields are not needed for relation
+        $contentType = new Type();
+        $contentType->identifier = "article";
+
+        $this->content = new Content( $contentType );
+        $this->content->setState(
+            array(
+                "properties" => new RelationValue(
+                    array(
+                        "id" => 42,
+                    )
+                )
+            )
+        );
+    }
+
+    /**
+     * @covers \ezp\Content\Relation::__construct
+     */
+    public function testConstruct()
+    {
+        $relation = new Relation( Relation::COMMON, $this->content );
+        $this->assertEquals( 42, $relation->destinationContentId );
+        $this->assertEquals( Relation::COMMON, $relation->type );
+    }
+
+    /**
+     * @expectedException \ezp\Base\Exception\InvalidArgumentValue
+     * @covers \ezp\Content\Relation::__construct
+     */
+    public function testConstructWrongType1()
+    {
+        $relation = new Relation( "common", $this->content );
+    }
+
+    /**
+     * @expectedException \ezp\Base\Exception\InvalidArgumentValue
+     * @covers \ezp\Content\Relation::__construct
+     */
+    public function testConstructWrongType2()
+    {
+        $relation = new Relation( ~0, $this->content );
+    }
+}
