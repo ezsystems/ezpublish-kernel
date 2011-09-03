95167b35f72537e66b0e552820c4c55e814b8ff3
Patrick Allaert <pa@ez.no>
2011-09-03 09:31:53 +0200

    Changed: Added and refactored relation tests (handler level)

---
+++ ezp/Persistence/Tests/ContentHandlerRelationTest.php
@@ -0,0 +1,219 @@
+<?php
+/**
+ * File contains: ezp\Persistence\Tests\ContentHandlerRelationTest class
+ *
+ * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace ezp\Persistence\Tests;
+use ezp\Persistence\Content,
+    ezp\Persistence\Content\CreateStruct,
+    ezp\Persistence\Content\UpdateStruct,
+    ezp\Persistence\Content\Field,
+    ezp\Persistence\Content\Relation as RelationValue,
+    ezp\Persistence\Content\Criterion\ContentId,
+    ezp\Base\Exception\NotFound,
+    ezp\Content\Version,
+    ezp\Content\Relation;
+
+/**
+ * Test case for relations operation in ContentHandler using in memory storage.
+ *
+ */
+class ContentHandlerRelationTest extends HandlerTest
+{
+    /**
+     * @var \ezp\Content
+     */
+    protected $content;
+
+    /**
+     *
+     * @var int
+     */
+    protected $contentId;
+
+    /**
+     * @var \ezp\Content[]
+     */
+    protected $contentToDelete = array();
+
+    /**
+     * Setup the HandlerTest.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $struct = new CreateStruct();
+        $struct->name = "test";
+        $struct->ownerId = 14;
+        $struct->sectionId = 1;
+        $struct->typeId = 2;
+        $struct->fields[] = new Field(
+            array(
+                'type' => 'ezstring',
+                // @todo Use FieldValue object
+                'value' => 'Welcome',
+                'language' => 'eng-GB',
+            )
+        );
+
+        $this->content = $this->repositoryHandler->contentHandler()->create( $struct );
+        $this->contentToDelete[] = $this->content;
+        $this->contentId = $this->content->id;
+
+        $this->repositoryHandler->contentHandler()->addRelation( 1, null, $this->contentId, Relation::COMMON | Relation::EMBED );
+    }
+
+    /**
+     * Removes stuff created in setUp().
+     */
+    protected function tearDown()
+    {
+        $contentHandler = $this->repositoryHandler->contentHandler();
+
+        try
+        {
+            // Removing default objects as well as those created by tests
+            foreach ( $this->contentToDelete as $content )
+            {
+                $contentHandler->delete( $content->id );
+            }
+        }
+        catch ( NotFound $e )
+        {
+        }
+        unset( $this->contentId );
+        parent::tearDown();
+    }
+
+    /**
+     * Test addRelation function
+     *
+     * @covers ezp\Persistence\Storage\InMemory\ContentHandler::addRelation
+     */
+    public function testAddRelation1()
+    {
+        $relation = $this->repositoryHandler->contentHandler()->addRelation( 14, null, 10, Relation::COMMON );
+        $this->assertEquals( 1, $relation->id );
+        $this->assertEquals( 14, $relation->sourceContentId );
+        $this->assertNull( $relation->sourceContentVersion );
+        $this->assertEquals( 10, $relation->destinationContentId );
+    }
+
+    /**
+     * Test addRelation function with a version
+     *
+     * @covers ezp\Persistence\Storage\InMemory\ContentHandler::addRelation
+     */
+    public function testAddRelation2()
+    {
+        $relation = $this->repositoryHandler->contentHandler()->addRelation( 14, 1, 10, Relation::COMMON );
+        $this->assertEquals( 1, $relation->id );
+        $this->assertEquals( 14, $relation->sourceContentId );
+        $this->assertEquals( 1, $relation->sourceContentVersion );
+        $this->assertEquals( 10, $relation->destinationContentId );
+    }
+
+    /**
+     * Test addRelation function with unexisting source content ID
+     *
+     * @expectedException ezp\Base\Exception\NotFound
+     * @covers ezp\Persistence\Storage\InMemory\ContentHandler::addRelation
+     */
+    public function testAddRelationSourceDoesNotExist1()
+    {
+        $this->repositoryHandler->contentHandler()->addRelation( 123456, null, 10, Relation::COMMON );
+    }
+
+    /**
+     * Test addRelation function with unexisting source content version
+     *
+     * @expectedException ezp\Base\Exception\NotFound
+     * @covers ezp\Persistence\Storage\InMemory\ContentHandler::addRelation
+     */
+    public function testAddRelationSourceDoesNotExist2()
+    {
+        $this->repositoryHandler->contentHandler()->addRelation( 14, 123456, 10, Relation::COMMON );
+    }
+
+    /**
+     * Test loadRelations function
+     *
+     * @covers ezp\Persistence\Storage\InMemory\ContentHandler::loadRelations
+     */
+    public function testLoadRelations()
+    {
+        $relations = $this->repositoryHandler->contentHandler()->loadRelations( 1 );
+        $this->assertEquals( 1, count( $relations ) );
+        $this->assertEquals( 1, $relations[0]->sourceContentId );
+        $this->assertNull( $relations[0]->sourceContentVersion );
+        $this->assertEquals( $this->contentId, $relations[0]->destinationContentId );
+        $this->assertEquals( Relation::COMMON | Relation::EMBED, $relations[0]->type );
+    }
+
+    /**
+     * Test loadRelations function with a type
+     *
+     * @covers ezp\Persistence\Storage\InMemory\ContentHandler::loadRelations
+     */
+    public function testLoadRelationsWithType1()
+    {
+        $relations = $this->repositoryHandler->contentHandler()->loadRelations( 1, null, Relation::EMBED );
+        $this->assertEquals( 1, count( $relations ) );
+        $this->assertEquals( 1, $relations[0]->sourceContentId );
+        $this->assertNull( $relations[0]->sourceContentVersion );
+        $this->assertEquals( $this->contentId, $relations[0]->destinationContentId );
+        $this->assertEquals( Relation::COMMON | Relation::EMBED, $relations[0]->type );
+    }
+
+    /**
+     * Test loadRelations function with combined types
+     *
+     * @covers ezp\Persistence\Storage\InMemory\ContentHandler::loadRelations
+     */
+    public function testLoadRelationsWithType2()
+    {
+        $relations = $this->repositoryHandler->contentHandler()->loadRelations( 1, null, Relation::COMMON | Relation::EMBED );
+        $this->assertEquals( 1, count( $relations ) );
+        $this->assertEquals( 1, $relations[0]->sourceContentId );
+        $this->assertNull( $relations[0]->sourceContentVersion );
+        $this->assertEquals( $this->contentId, $relations[0]->destinationContentId );
+        $this->assertEquals( Relation::COMMON | Relation::EMBED, $relations[0]->type );
+    }
+
+    /**
+     * Test loadRelations function with no associated results
+     *
+     * @covers ezp\Persistence\Storage\InMemory\ContentHandler::loadRelations
+     */
+    public function testLoadRelationsWithTypeNoResult1()
+    {
+        $this->assertEmpty(
+            $this->repositoryHandler->contentHandler()->loadRelations(
+                1,
+                null,
+                Relation::COMMON | Relation::EMBED | Relation::LINK
+            )
+        );
+    }
+
+    /**
+     * Test loadRelations function with no associated results
+     *
+     * @covers ezp\Persistence\Storage\InMemory\ContentHandler::loadRelations
+     */
+    public function testLoadRelationsWithTypeNoResult2()
+    {
+        $this->assertEmpty(
+            $this->repositoryHandler->contentHandler()->loadRelations(
+                1,
+                null,
+                Relation::LINK
+            )
+        );
+    }
+}
