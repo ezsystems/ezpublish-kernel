14d775e9046a38855117486e92c9c67928cb4df6
Tobias Schlitt <toby@php.net>
2011-09-06 20:34:00 +0200

    Implemented: Basic stub for Language gateway.

---
+++ ezp/Persistence/Storage/Legacy/Content/Language/Gateway.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * File containing the Language Gateway class
+ *
+ * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace ezp\Persistence\Storage\Legacy\Content\Language;
+
+/**
+ * Language Handler
+ */
+abstract class Gateway
+{
+}
