d687fca94131d178bcd133e8c1181bdb8eeb25f2
Tobias Schlitt <toby@php.net>
2011-09-07 16:32:04 +0200

    Added: Section database fixture.

---
+++ ezp/Persistence/Storage/Legacy/Tests/Content/_fixtures/sections.php
@@ -0,0 +1,53 @@
+<?php
+
+return array(
+    'ezsection' => array(
+        array(
+            'id' => 1,
+            'identifier' => 'standard',
+            'locale' => '',
+            'name' => 'Standard',
+            'navigation_part_identifier' => 'ezcontentnavigationpart'
+        ),
+
+        array(
+            'id' => 2,
+            'identifier' => 'users',
+            'locale' => '',
+            'name' => 'Users',
+            'navigation_part_identifier' => 'ezusernavigationpart'
+        ),
+
+        array(
+            'id' => 3,
+            'identifier' => 'media',
+            'locale' => '',
+            'name' => 'Media',
+            'navigation_part_identifier' => 'ezmedianavigationpart'
+        ),
+
+        array(
+            'id' => 4,
+            'identifier' => 'setup',
+            'locale' => '',
+            'name' => 'Setup',
+            'navigation_part_identifier' => 'ezsetupnavigationpart'
+        ),
+
+        array(
+            'id' => 5,
+            'identifier' => 'design',
+            'locale' => '',
+            'name' => 'Design',
+            'navigation_part_identifier' => 'ezvisualnavigationpart'
+        ),
+
+        array(
+            'id' => 6,
+            'identifier' => '',
+            'locale' => '',
+            'name' => 'Restricted',
+            'navigation_part_identifier' => 'ezcontentnavigationpart'
+        )
+    )
+);
