111ad1e8987edd2d50155cd8167186d3d83e600a
Kore Nordmann <mail@kore-nordmann.de>
2011-09-08 13:26:34 +0200

    Coding style

--- ezp/Persistence/Storage/Legacy/Content/Search/Utf8Converter.php
+++ ezp/Persistence/Storage/Legacy/Content/Search/Utf8Converter.php
@@ -2,7 +2,7 @@
 /**
  * File containing the Utf8Converter class
  *
- * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
+ * @copyright Copyright ( C ) 1999-2011 eZ Systems AS. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
  * @version //autogentag//
  *
@@ -31,38 +31,51 @@ class Utf8Converter
         {
             case 0:
                 $char = chr( 0 );
-            case !($charCode & 0xffffff80): // 7 bit
+                break;
+            case !( $charCode & 0xffffff80 ): // 7 bit
                 $char = chr( $charCode );
                 break;
-            case !($charCode & 0xfffff800): // 11 bit
-                $char = ( chr(0xc0 | (($charCode >> 6) & 0x1f)) .
-                          chr(0x80 | ($charCode & 0x3f)) );
+            case !( $charCode & 0xfffff800 ): // 11 bit
+                $char = (
+                    chr( 0xc0 | ( ( $charCode >> 6 ) & 0x1f ) ) .
+                    chr( 0x80 | ( $charCode & 0x3f ) )
+                );
                 break;
-            case !($charCode & 0xffff0000): // 16 bit
-                $char = ( chr(0xe0 | (($charCode >> 12) & 0x0f)) .
-                          chr(0x80 | (($charCode >> 6) & 0x3f)) .
-                          chr(0x80 | ($charCode & 0x3f)) );
+            case !( $charCode & 0xffff0000 ): // 16 bit
+                $char = (
+                    chr( 0xe0 | ( ( $charCode >> 12 ) & 0x0f ) ) .
+                    chr( 0x80 | ( ( $charCode >> 6 ) & 0x3f ) ) .
+                    chr( 0x80 | ( $charCode & 0x3f ) )
+                );
                 break;
-            case !($charCode & 0xffe00000): // 21 bit
-                $char = ( chr(0xf0 | (($charCode >> 18) & 0x07)) .
-                          chr(0x80 | (($charCode >> 12) & 0x3f)) .
-                          chr(0x80 | (($charCode >> 6) & 0x3f)) .
-                          chr(0x80 | ($charCode & 0x3f)) );
+            case !( $charCode & 0xffe00000 ): // 21 bit
+                $char = (
+                    chr( 0xf0 | ( ( $charCode >> 18 ) & 0x07 ) ) .
+                    chr( 0x80 | ( ( $charCode >> 12 ) & 0x3f ) ) .
+                    chr( 0x80 | ( ( $charCode >> 6 ) & 0x3f ) ) .
+                    chr( 0x80 | ( $charCode & 0x3f ) )
+                );
                 break;
-            case !($charCode & 0xfc000000): // 26 bit
-                $char = ( chr(0xf8 | (($charCode >> 24) & 0x03)) .
-                          chr(0x80 | (($charCode >> 18) & 0x3f)) .
-                          chr(0x80 | (($charCode >> 12) & 0x3f)) .
-                          chr(0x80 | (($charCode >> 6) & 0x3f)) .
-                          chr(0x80 | ($charCode & 0x3f)) );
+            case !( $charCode & 0xfc000000 ): // 26 bit
+                $char = (
+                    chr( 0xf8 | ( ( $charCode >> 24 ) & 0x03 ) ) .
+                    chr( 0x80 | ( ( $charCode >> 18 ) & 0x3f ) ) .
+                    chr( 0x80 | ( ( $charCode >> 12 ) & 0x3f ) ) .
+                    chr( 0x80 | ( ( $charCode >> 6 ) & 0x3f ) ) .
+                    chr( 0x80 | ( $charCode & 0x3f ) )
+                );
+                break;
             default: // 31 bit
-                $char = ( chr(0xfc | (($charCode >> 30) & 0x01)) .
-                          chr(0x80 | (($charCode >> 24) & 0x3f)) .
-                          chr(0x80 | (($charCode >> 18) & 0x3f)) .
-                          chr(0x80 | (($charCode >> 12) & 0x3f)) .
-                          chr(0x80 | (($charCode >> 6) & 0x3f)) .
-                          chr(0x80 | ($charCode & 0x3f)) );
+                $char = (
+                    chr( 0xfc | ( ( $charCode >> 30 ) & 0x01 ) ) .
+                    chr( 0x80 | ( ( $charCode >> 24 ) & 0x3f ) ) .
+                    chr( 0x80 | ( ( $charCode >> 18 ) & 0x3f ) ) .
+                    chr( 0x80 | ( ( $charCode >> 12 ) & 0x3f ) ) .
+                    chr( 0x80 | ( ( $charCode >> 6 ) & 0x3f ) ) .
+                    chr( 0x80 | ( $charCode & 0x3f ) )
+                );
         }
+
         return $char;
     }
 
@@ -79,79 +92,108 @@ class Utf8Converter
         {
             $charCode = ord( $char[0] );
         }
-        else if ( ( ord( $char[0] ) & 0xe0 ) == 0xc0 ) // 11 bit, 2 chars
+        elseif ( ( ord( $char[0] ) & 0xe0 ) == 0xc0 ) // 11 bit, 2 chars
         {
             if ( ( ord( $char[1] ) & 0xc0 ) != 0x80 )
-                return $charCode;
-            $charCode = ( (( ord( $char[0] ) & 0x1f ) << 6) +
-                           (( ord( $char[1] ) & 0x3f )) );
-            if ( $charCode < 128 ) // Illegal multibyte, should use less than 2 chars
             {
-                $charCode == false;
+                return $charCode;
+            }
+
+            $charCode = (
+                ( ( ord( $char[0] ) & 0x1f ) << 6 ) +
+                ( ( ord( $char[1] ) & 0x3f ) )
+            );
+
+            if ( $charCode < 128 )
+            {
+                throw new \RuntimeException( 'Illegal UTF-8 input character: ' . $char );
             }
         }
-        else if ( ( ord( $char[0] ) & 0xf0 ) == 0xe0 ) // 16 bit, 3 chars
+        elseif ( ( ord( $char[0] ) & 0xf0 ) == 0xe0 ) // 16 bit, 3 chars
         {
-            if ( ( ord( $char[1] ) & 0xc0 ) != 0x80 or
+            if ( ( ord( $char[1] ) & 0xc0 ) != 0x80 ||
                  ( ord( $char[2] ) & 0xc0 ) != 0x80 )
-                return $charCode;
-            $charCode = ( (( ord( $char[0] ) & 0x0f ) << 12) +
-                           (( ord( $char[1] ) & 0x3f ) << 6) +
-                           (( ord( $char[2] ) & 0x3f )) );
-            if ( $charCode < 2048 ) // Illegal multibyte, should use less than 3 chars
             {
-                $charCode == false;
+                return $charCode;
+            }
+            $charCode = (
+                ( ( ord( $char[0] ) & 0x0f ) << 12 ) +
+                ( ( ord( $char[1] ) & 0x3f ) << 6 ) +
+                ( ( ord( $char[2] ) & 0x3f ) )
+            );
+
+            if ( $charCode < 2048 )
+            {
+                throw new \RuntimeException( 'Illegal UTF-8 input character: ' . $char );
             }
         }
-        else if ( ( ord( $char[0] ) & 0xf8 ) == 0xf0 ) // 21 bit, 4 chars
+        elseif ( ( ord( $char[0] ) & 0xf8 ) == 0xf0 ) // 21 bit, 4 chars
         {
-            if ( ( ord( $char[1] ) & 0xc0 ) != 0x80 or
-                 ( ord( $char[2] ) & 0xc0 ) != 0x80 or
+            if ( ( ord( $char[1] ) & 0xc0 ) != 0x80 ||
+                 ( ord( $char[2] ) & 0xc0 ) != 0x80 ||
                  ( ord( $char[3] ) & 0xc0 ) != 0x80 )
-                return $charCode;
-            $charCode = ( (( ord( $char[0] ) & 0x07 ) << 18) +
-                           (( ord( $char[1] ) & 0x3f ) << 12) +
-                           (( ord( $char[2] ) & 0x3f ) << 6) +
-                           (( ord( $char[3] ) & 0x3f )) );
-            if ( $charCode < 65536 ) // Illegal multibyte, should use less than 4 chars
             {
-                $charCode == false;
+                return $charCode;
+            }
+
+            $charCode = (
+                ( ( ord( $char[0] ) & 0x07 ) << 18 ) +
+                ( ( ord( $char[1] ) & 0x3f ) << 12 ) +
+                ( ( ord( $char[2] ) & 0x3f ) << 6 ) +
+                ( ( ord( $char[3] ) & 0x3f ) )
+            );
+
+            if ( $charCode < 65536 )
+            {
+                throw new \RuntimeException( 'Illegal UTF-8 input character: ' . $char );
             }
         }
-        else if ( ( ord( $char[0] ) & 0xfc ) == 0xf8 ) // 26 bit, 5 chars
+        elseif ( ( ord( $char[0] ) & 0xfc ) == 0xf8 ) // 26 bit, 5 chars
         {
-            if ( ( ord( $char[1] ) & 0xc0 ) != 0x80 or
-                 ( ord( $char[2] ) & 0xc0 ) != 0x80 or
-                 ( ord( $char[3] ) & 0xc0 ) != 0x80 or
+            if ( ( ord( $char[1] ) & 0xc0 ) != 0x80 ||
+                 ( ord( $char[2] ) & 0xc0 ) != 0x80 ||
+                 ( ord( $char[3] ) & 0xc0 ) != 0x80 ||
                  ( ord( $char[4] ) & 0xc0 ) != 0x80 )
-                return $charCode;
-            $charCode = ( (( ord( $char[0] ) & 0x03 ) << 24) +
-                           (( ord( $char[1] ) & 0x3f ) << 18) +
-                           (( ord( $char[2] ) & 0x3f ) << 12) +
-                           (( ord( $char[3] ) & 0x3f ) << 6) +
-                           (( ord( $char[4] ) & 0x3f )) );
-            if ( $charCode < 2097152 ) // Illegal multibyte, should use less than 5 chars
             {
-                $charCode == false;
+                return $charCode;
+            }
+
+            $charCode = (
+                ( ( ord( $char[0] ) & 0x03 ) << 24 ) +
+                ( ( ord( $char[1] ) & 0x3f ) << 18 ) +
+                ( ( ord( $char[2] ) & 0x3f ) << 12 ) +
+                ( ( ord( $char[3] ) & 0x3f ) << 6 ) +
+                ( ( ord( $char[4] ) & 0x3f ) )
+            );
+
+            if ( $charCode < 2097152 )
+            {
+                throw new \RuntimeException( 'Illegal UTF-8 input character: ' . $char );
             }
         }
-        else if ( ( ord( $char[0] ) & 0xfe ) == 0xfc ) // 31 bit, 6 chars
+        elseif ( ( ord( $char[0] ) & 0xfe ) == 0xfc ) // 31 bit, 6 chars
         {
-            if ( ( ord( $char[1] ) & 0xc0 ) != 0x80 or
-                 ( ord( $char[2] ) & 0xc0 ) != 0x80 or
-                 ( ord( $char[3] ) & 0xc0 ) != 0x80 or
-                 ( ord( $char[4] ) & 0xc0 ) != 0x80 or
+            if ( ( ord( $char[1] ) & 0xc0 ) != 0x80 ||
+                 ( ord( $char[2] ) & 0xc0 ) != 0x80 ||
+                 ( ord( $char[3] ) & 0xc0 ) != 0x80 ||
+                 ( ord( $char[4] ) & 0xc0 ) != 0x80 ||
                  ( ord( $char[5] ) & 0xc0 ) != 0x80 )
-                return $charCode;
-            $charCode = ( (( ord( $char[0] ) & 0x01 ) << 30) +
-                           (( ord( $char[1] ) & 0x3f ) << 24) +
-                           (( ord( $char[2] ) & 0x3f ) << 18) +
-                           (( ord( $char[3] ) & 0x3f ) << 12) +
-                           (( ord( $char[4] ) & 0x3f ) << 6) +
-                           (( ord( $char[5] ) & 0x3f )) );
-            if ( $charCode < 67108864 ) // Illegal multibyte, should use less than 6 chars
             {
-                $charCode == false;
+                return $charCode;
+            }
+
+            $charCode = (
+                ( ( ord( $char[0] ) & 0x01 ) << 30 ) +
+                ( ( ord( $char[1] ) & 0x3f ) << 24 ) +
+                ( ( ord( $char[2] ) & 0x3f ) << 18 ) +
+                ( ( ord( $char[3] ) & 0x3f ) << 12 ) +
+                ( ( ord( $char[4] ) & 0x3f ) << 6 ) +
+                ( ( ord( $char[5] ) & 0x3f ) )
+            );
+
+            if ( $charCode < 67108864 )
+            {
+                throw new \RuntimeException( 'Illegal UTF-8 input character: ' . $char );
             }
         }
 
