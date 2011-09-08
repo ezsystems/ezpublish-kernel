bb39b469b2f27eb9ed6647a7f908a3f981e85ff2
Jérôme Vieilledent <jv@ez.no>
2011-09-08 17:26:10 +0200

    Added missing LocationCollection class

---
+++ ezp/Content/Location/Collection.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * File containing Location collection class
+ *
+ * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+namespace ezp\Content\Location;
+use ezp\Base\Collection\Type as TypeCollection;
+
+/**
+ * Location collection class
+ */
+class Collection extends TypeCollection
+{
+    /**
+     * Constructor
+     * @param \ezp\Content\Location[] $elements
+     */
+    public function __construct( array $elements )
+    {
+        parent::__construct( 'ezp\\Content\\Location', $elements );
+    }
+}
