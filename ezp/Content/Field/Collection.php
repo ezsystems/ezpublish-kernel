610ccd33a79d7608c8a239f2f9554feb5248eed6
Jérôme Vieilledent <jv@ez.no>
2011-09-15 11:09:40 +0200

    FieldCollection: FieldValue objects can be now directly assigned to fields via the collection

--- ezp/Content/Field/Collection.php
+++ ezp/Content/Field/Collection.php
@@ -10,7 +10,10 @@
 namespace ezp\Content\Field;
 use ezp\Base\Collection\Lazy,
     ezp\Content\Service as ContentService,
-    ezp\Content\Version;
+    ezp\Content\Version,
+    ezp\Base\Exception\Logic as LogicException,
+    ezp\Base\Exception\InvalidArgumentType,
+    ezp\Content\FieldType\Value as FieldValue;
 
 /**
  * Field Collection class. Fields are indexed by field identifier
@@ -27,5 +30,25 @@ class Collection extends Lazy
     public function __construct( ContentService $contentService, Version $version )
     {
         parent::__construct( 'ezp\\Content\\Field', $contentService, $version, 'loadFields' );
+    }
+
+    /**
+     * Tries to assign $value as field value to a Field object identified by $identifier
+     *
+     * @param string $identifier Field identifier
+     * @param \ezp\Content\FieldType\Value $value Field value object
+     * @throws \ezp\Base\Exception\Logic If any field identified by $identifier doesn't exist in the collection
+     * @throws \ezp\Base\Exception\InvalidArgumentType If $value is not a field value object
+     * @todo Direct string assignation ? If we decide to implement this, magic should be done here
+     */
+    public function offsetSet( $identifier, $value )
+    {
+        $this->load();
+        if ( !$this->offsetExists( $identifier ) )
+            throw new LogicException( 'FieldCollection', "Field with identifier '$identifier' doesn't exist in this collection" );
+        else if ( !$value instanceof FieldValue )
+            throw new InvalidArgumentType( 'value', 'ezp\\Content\\FieldType\\Value', $value );
+
+        $this->offsetGet( $identifier )->value = $value;
     }
 }
