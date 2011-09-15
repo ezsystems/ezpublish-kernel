79b5bd52a6be9490d1875b1dd98c3f3a2d1a844a
Patrick Allaert <pa@ez.no>
2011-09-15 17:01:56 +0200

    Added: keyword FieldType

---
+++ ezp/Content/FieldType/Keyword/Type.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * File containing the Keyword field type
+ *
+ * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace ezp\Content\FieldType\Keyword;
+use ezp\Content\FieldType,
+    ezp\Content\FieldType\Value as BaseValue,
+    ezp\Base\Exception\BadFieldTypeInput,
+    ezp\Persistence\Content\FieldValue;
+
+/**
+ * Keyword field types
+ *
+ * Represents keywords.
+ */
+class Type extends FieldType
+{
+    protected $fieldTypeString = "ezkeyword";
+    protected $defaultValue = "";
+    protected $isSearchable = true;
+
+    protected $allowedValidators = array();
+
+    /**
+     * Checks if value can be parsed.
+     *
+     * If the value actually can be parsed, the value is returned.
+     *
+     * @todo Implement it
+     * @throws ezp\Base\Exception\BadFieldTypeInput Thrown when $inputValue is not understood.
+     * @param mixed $inputValue
+     * @return mixed
+     */
+    protected function canParseValue( BaseValue $inputValue )
+    {
+        return $inputValue;
+    }
+
+    /**
+     * Sets the value of a field type.
+     *
+     * @param $inputValue
+     * @return void
+     */
+    public function setValue( BaseValue $inputValue )
+    {
+        $this->value = $this->canParseValue( $inputValue );
+    }
+
+    /**
+     * Method to populate the FieldValue struct for field types.
+     *
+     * This method is used by the business layer to populate the value object
+     * for field type data.
+     *
+     * @internal
+     * @param \ezp\Persistence\Content\FieldValue $valueStruct The value struct which the field type data is packaged in for consumption by the storage engine.
+     * @return void
+     */
+    public function setFieldValue( FieldValue $valueStruct )
+    {
+        $valueStruct->data = $this->getFieldTypeSettings() + $this->getValueData();
+        $valueStruct->sortKey = $this->getSortInfo();
+    }
+
+    /**
+     * Returns information for FieldValue->$sortKey relevant to the field type.
+     *
+     * @todo Review this, created from copy/paste to unblock failing tests!
+     * @return array
+     */
+    protected function getSortInfo()
+    {
+        return array( "sort_key_int" => $this->value );
+    }
+
+    /**
+     * Returns the value of the field type in a format suitable for packing it
+     * in a FieldValue.
+     *
+     * @todo Review this, created from copy/paste to unblock failing tests!
+     * @return array
+     */
+    protected function getValueData()
+    {
+        return array( "value" => $this->value );
+    }
+}
---
+++ ezp/Content/FieldType/Keyword/Value.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * File containing the Keyword Value class
+ *
+ * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace ezp\Content\FieldType\Keyword;
+use ezp\Content\FieldType\Value as ValueInterface;
+
+/**
+ * Value for Keyword field type
+ */
+class Value implements ValueInterface
+{
+    /**
+     * Content of the value
+     *
+     * @var string[]
+     */
+    public $values = array();
+
+    /**
+     * Construct a new Value object and initialize with $values
+     *
+     * @param string[] $values
+     */
+    public function __construct( array $values = null )
+    {
+        if ( $values !== null )
+            $this->values = $values;
+    }
+
+    /**
+     * @see \ezp\Content\FieldType\Value
+     */
+    public static function fromString( $stringValue )
+    {
+        throw new RuntimeException( "@TODO: Implement" );
+        return new static( $stringValue );
+    }
+
+    /**
+     * @see \ezp\Content\FieldType\Value
+     */
+    public function __toString()
+    {
+        throw new RuntimeException( "@TODO: Implement" );
+        return (string)$this->values;
+    }
+}
