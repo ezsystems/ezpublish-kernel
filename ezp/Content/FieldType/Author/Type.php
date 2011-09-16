d12b4ca165ad8f40b445bae49297e33c45ee877e
Patrick Allaert <pa@ez.no>
2011-09-16 14:26:04 +0200

    Fixed: missing import of Complex FieldType

--- ezp/Content/FieldType/Author/Type.php
+++ ezp/Content/FieldType/Author/Type.php
@@ -9,10 +9,9 @@
 
 
 namespace ezp\Content\FieldType\Author;
-use ezp\Content\FieldType,
+use ezp\Content\FieldType\Complex,
     ezp\Content\FieldType\Value as BaseValue,
-    \ezp\Base\Exception\BadFieldTypeInput,
-    \ezp\Persistence\Content\FieldValue,
+    ezp\Base\Exception\BadFieldTypeInput,
     DOMDocument;
 
 /**
