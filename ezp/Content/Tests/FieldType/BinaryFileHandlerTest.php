558a402082d98b72c1a1d990ce2cf5a873f5c336
Jérôme Vieilledent <jv@ez.no>
2011-09-22 21:53:38 +0200

    Added Tests for BinaryFile field type

---
+++ ezp/Content/Tests/FieldType/BinaryFileHandlerTest.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * File containing the BinaryFileHandlerTest class
+ *
+ * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace ezp\Content\Tests\FieldType;
+use ezp\Content\FieldType\BinaryFile\Handler as BinaryFileHandler,
+    ezp\Io\BinaryFile,
+    ezp\Io\SysInfo,
+    ezp\Io\FileInfo,
+    ezp\Base\BinaryRepository;
+
+/**
+ * Test case for {@link \ezp\Content\FieldType\BinaryFile\Handler}
+ */
+class BinaryFileHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Path to test image
+     * @var string
+     */
+    protected $imagePath;
+
+    /**
+     * FileInfo object for test image
+     * @var \ezp\Io\FileInfo
+     */
+    protected $imageFileInfo;
+
+    /**
+     * Binary file handler object
+     * @var \ezp\Content\FieldType\BinaryFile\Handler
+     */
+    protected $handler;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        BinaryRepository::setOverrideOptions( 'inmemory' );
+        $this->imagePath = __DIR__ . '/squirrel-developers.jpg';
+        $this->imageFileInfo = new FileInfo( $this->imagePath );
+        $this->handler = new BinaryFileHandler;
+    }
+
+    /**
+     * @group fieldType
+     * @group binaryFile
+     * @covers \ezp\Content\FieldType\BinaryFile\Handler::createFromLocalFile
+     */
+    public function testCreateFromLocalFile()
+    {
+        $file = $this->handler->createFromLocalPath( $this->imagePath );
+        self::assertInstanceOf( 'ezp\\Io\\BinaryFile', $file );
+
+        $pathPattern = '#^' . SysInfo::storageDirectory() . '/original/' .
+                       $this->imageFileInfo->getContentType()->type .
+                       '/[a-z0-9]{32}.' . $this->imageFileInfo->getExtension() . '$#';
+        self::assertRegExp( $pathPattern, $file->path );
+    }
+
+    /**
+     * @group fieldType
+     * @group binaryFile
+     * @covers \ezp\Content\FieldType\BinaryFile\Handler::getBinaryRepository
+     */
+    public function testGetBinaryRepository()
+    {
+        self::assertInstanceOf( 'ezp\\Base\\BinaryRepository', $this->handler->getBinaryRepository() );
+    }
+}
--- ezp/Content/Tests/FieldType/BinaryFileTest.php
+++ ezp/Content/Tests/FieldType/BinaryFileTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * File containing the TextLineTest class
+ * File containing the BinaryFileTest class
  *
  * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
@@ -9,147 +9,139 @@
 
 namespace ezp\Content\Tests\FieldType;
 use ezp\Content\FieldType\Factory,
-    ezp\Content\FieldType\TextLine\Type as TextLine,
-    ezp\Content\FieldType\TextLine\Value as TextLineValue,
+    ezp\Content\FieldType\BinaryFile\Type as BinaryFileType,
+    ezp\Content\FieldType\BinaryFile\Value as BinaryFileValue,
+    ezp\Content\FieldType\BinaryFile\Handler as BinaryFileHandler,
+    ezp\Io\BinaryFile,
+    ezp\Io\FileInfo,
     ezp\Base\Exception\BadFieldTypeInput,
+    ezp\Base\BinaryRepository,
     ezp\Persistence\Content\FieldValue,
     PHPUnit_Framework_TestCase,
     ReflectionObject;
 
-class TextLineTest extends PHPUnit_Framework_TestCase
+class BinaryFileTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * Path to test image
+     * @var string
+     */
+    protected $imagePath;
+
+    /**
+     * FileInfo object for test image
+     * @var \ezp\Io\FileInfo
+     */
+    protected $imageFileInfo;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        BinaryRepository::setOverrideOptions( 'inmemory' );
+        $this->imagePath = __DIR__ . '/squirrel-developers.jpg';
+        $this->imageFileInfo = new FileInfo( $this->imagePath );
+    }
+
+
     /**
      * This test will make sure a correct mapping for the field type string has
      * been made.
      *
      * @group fieldType
-     * @group textLine
+     * @group binaryFile
      * @covers \ezp\Content\FieldType\Factory::build
      */
     public function testBuildFactory()
     {
         self::assertInstanceOf(
-            "ezp\\Content\\FieldType\\TextLine\\Type",
-            Factory::build( "ezstring" ),
-            "TextLine object not returned for 'ezstring', incorrect mapping? "
+            "ezp\\Content\\FieldType\\BinaryFile\\Type",
+            Factory::build( "ezbinaryfile" ),
+            "BinaryFile object not returned for 'ezbinaryfile', incorrect mapping? "
         );
     }
 
     /**
      * @group fieldType
-     * @group textLine
+     * @group binaryFile
      * @covers \ezp\Content\FieldType::allowedValidators
      */
-    public function testTextLineSupportedValidators()
+    public function testBinaryFileSupportedValidators()
     {
-        $ft = new TextLine();
+        $ft = new BinaryFileType;
         self::assertSame(
-            array( 'ezp\\Content\\FieldType\\TextLine\\StringLengthValidator' ),
+            array( 'ezp\\Content\\FieldType\\BinaryFile\\FileSizeValidator' ),
             $ft->allowedValidators(),
             "The set of allowed validators does not match what is expected."
         );
     }
 
     /**
-     * @covers \ezp\Content\FieldType\TextLine\Type::canParseValue
+     * @covers \ezp\Content\FieldType\BinaryFile\Type::canParseValue
      * @expectedException ezp\Base\Exception\BadFieldTypeInput
      * @group fieldType
-     * @group textLine
+     * @group binaryFile
      */
     public function testCanParseValueInvalidFormat()
     {
-        $ft = new TextLine();
+        $ft = new BinaryFileType;
+        $invalidValue = new BinaryFileValue;
+        $invalidValue->file = 'This is definitely not a binary file !';
         $ref = new ReflectionObject( $ft );
         $refMethod = $ref->getMethod( 'canParseValue' );
         $refMethod->setAccessible( true );
-        $refMethod->invoke( $ft, new TextLineValue( 42 ) );
+        $refMethod->invoke( $ft, $invalidValue );
     }
 
     /**
-     * @covers ezp\Content\FieldType::setValue
      * @group fieldType
-     * @group textLine
-     * @covers \ezp\Content\FieldType\TextLine\Type::canParseValue
+     * @group binaryFile
+     * @covers \ezp\Content\FieldType\BinaryFile\Type::canParseValue
      */
     public function testCanParseValueValidFormat()
     {
-        $ft = new TextLine();
+        $ft = new BinaryFileType;
         $ref = new ReflectionObject( $ft );
         $refMethod = $ref->getMethod( 'canParseValue' );
         $refMethod->setAccessible( true );
 
-        $value = new TextLineValue( 'Strings works just fine.' );
+        $handler = new BinaryFileHandler;
+        $value = new BinaryFileValue;
+        $value->file = $handler->createFromLocalPath( $this->imagePath );
         self::assertSame( $value, $refMethod->invoke( $ft, $value ) );
     }
 
     /**
      * @group fieldType
-     * @group textLine
-     * @covers \ezp\Content\FieldType\TextLine\Type::toFieldValue
+     * @group binaryFile
+     * @covers \ezp\Content\FieldType\BinaryFile\Value::getHandler
      */
-    public function testToFieldValue()
+    public function testValueGetHandler()
     {
-        $string = 'Test of FieldValue';
-        $ft = new TextLine();
-        $ft->setValue( $fv = new TextLineValue( $string ) );
-
-        $fieldValue = $ft->toFieldValue();
-
-        self::assertSame( $fv, $fieldValue->data );
-        self::assertNull( $fieldValue->externalData );
-        self::assertSame( array( 'sort_key_string' => $string ), $fieldValue->sortKey );
+        $value = new BinaryFileValue;
+        self::assertInstanceOf( 'ezp\\Content\\FieldType\\BinaryFile\\Handler', $value->getHandler() );
     }
 
     /**
      * @group fieldType
-     * @group textLine
-     * @covers \ezp\Content\FieldType\TextLine\Value::__construct
-     */
-    public function testBuildFieldValueWithParam()
-    {
-        $text = 'According to developers, strings are good for women health.';
-        $value = new TextLineValue( $text );
-        self::assertSame( $text, $value->text );
-    }
-
-    /**
-     * @group fieldType
-     * @covers \ezp\Content\FieldType\TextLine\Value::__construct
-     */
-    public function testBuildFieldValueWithoutParam()
-    {
-        $value = new TextLineValue;
-        self::assertSame( '', $value->text );
-    }
-
-    /**
-     * @group fieldType
-     * @group textLine
-     * @covers \ezp\Content\FieldType\TextLine\Value::fromString
+     * @group binaryFile
+     * @covers \ezp\Content\FieldType\BinaryFile\Value::fromString
      */
     public function testBuildFieldValueFromString()
     {
-        $string = "Most programmers don't wear strings. Most...";
-        $value = TextLineValue::fromString( $string );
-        self::assertInstanceOf( 'ezp\\Content\\FieldType\\TextLine\\Value', $value );
-        self::assertSame( $string, $value->text );
+        $value = BinaryFileValue::fromString( $this->imagePath );
+        self::assertInstanceOf( 'ezp\\Content\\FieldType\\BinaryFile\\Value', $value );
+        self::assertInstanceOf( 'ezp\\Io\\BinaryFile', $value->file );
+        self::assertSame( $this->imageFileInfo->getBasename(), $value->originalFilename );
     }
 
     /**
      * @group fieldType
-     * @group textLine
-     * @covers \ezp\Content\FieldType\TextLine\Value::__toString
+     * @group binaryFile
+     * @covers \ezp\Content\FieldType\BinaryFile\Value::__toString
      */
     public function testFieldValueToString()
     {
-        $string = "Believe it or not, but most geeks find strings very comfortable to work with";
-        $value = TextLineValue::fromString( $string );
-        self::assertSame( $string, (string)$value );
-
-        self::assertSame(
-            $string,
-            TextLineValue::fromString( (string)$value )->text,
-            'fromString() and __toString() must be compatible'
-        );
+        $value = BinaryFileValue::fromString( $this->imagePath );
+        self::assertSame( $value->file->path, (string)$value );
     }
 }
