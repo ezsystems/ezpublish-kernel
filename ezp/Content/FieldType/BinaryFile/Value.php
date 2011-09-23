d5a30e19c48c189a4a8b486a833edabd26a471a9
Jérôme Vieilledent <jv@ez.no>
2011-09-23 18:01:46 +0200

    Fixed incorrect phpdoc

--- ezp/Content/FieldType/BinaryFile/Value.php
+++ ezp/Content/FieldType/BinaryFile/Value.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * File containing the TextLine Value class
+ * File containing the BinaryFile Value class
  *
  * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
