2fe8f36944a51e90593c06cf0d14cd5136a77943
Patrick Allaert <pa@ez.no>
2011-09-29 16:09:37 +0200

    Fixed: doc typo

--- ezp/Persistence/Storage/Legacy/Content/FieldValue/Converter/UrlStorage.php
+++ ezp/Persistence/Storage/Legacy/Content/FieldValue/Converter/UrlStorage.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * File containing the Url class
+ * File containing the UrlStorage Converter class
  *
  * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
