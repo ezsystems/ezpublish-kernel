b26a4b0ebc5d1533f4a125eb5b4f87174591347b
Ole Marius Smestad <oms@ez.no>
2011-10-07 16:35:41 +0200

    Fixed EZPNEXT-40:
    Implemented internal addRelation method.

--- ezp/Content/FieldType/Service.php
+++ ezp/Content/FieldType/Service.php
@@ -8,8 +8,67 @@
  */
 
 namespace ezp\Content\FieldType;
-use ezp\Base\Service as BaseService;
+use ezp\Base\Service as BaseService,
+    ezp\Content\Relation,
+    ezp\Content\Proxy,
+    ezp\Persistence\Content\Relation\CreateStruct as RelationCreateStruct,
+    ezp\Base\Exception\Logic;
 
+/**
+ * Service providing necessary functionality for field types to perform internal content operations.
+ */
 class Service extends BaseService
 {
+
+    /**
+     * Creates a new relation entry.
+     *
+     * This method will only be used by field types.
+     *
+     * @access private
+     * @internal
+     * @param int $relationType The type of relation to create.
+     * @param mixed $contentFromId The content id you want where you want to add
+     *                             the relation.
+     * @param mixed $versionFrom The version number where we are adding this relation.
+     * @param mixed $contentToId The destination of the relation, which content
+     *                           id we are relating to.
+     * @param null|mixed $fieldDefinitionId The id of the field definition that
+     *                                      holds the attribute level relation.
+     * @throws \ezp\Base\Exception\InvalidArgumentValue If $relationType has an unsupported value
+     * @throws \ezp\Base\Exception\Logic If there is a mismatch between $relationType and provided values.
+     * @return \ezp\Content\Relation
+     */
+    public function addRelation( $relationType, $contentFromId, $versionFrom, $contentToId, $fieldDefinitionId = null )
+    {
+        // Creating the Relation object to be returned here, in order insert
+        // proxied destination Content
+        // @todo If ezp\Content\Relation is refactored wrt. the internal
+        //       destination Content then this method can likely be refactored to, to fully create the Relation from creation struct.
+        $relation = new Relation( $relationType, new Proxy( $contentToId, $this->repository->getContentService() ) );
+
+        if ( $relationType === Relation::ATTRIBUTE && $fieldDefinitionId === null )
+        {
+            throw new Logic( 'addRelation', 'Attribute level relation requested, but no field definition id given.');
+        }
+
+        // We grab the remaining values for the creation struct from the created object.
+        $creationStruct = $this->fillStruct(
+            new RelationCreateStruct(
+                array(
+                     'sourceContentId' => $contentFromId,
+                     'sourceContentVersion' => $versionFrom,
+                     'sourceFieldDefinitionId' => $fieldDefinitionId ?: 0
+                )
+            ),
+            $relation
+        );
+
+        // Finally we refresh the object with state from the stored entity.
+        return $relation->setState(
+            array(
+                'properties' => $this->handler->contentHandler()->addRelation( $creationStruct )
+            )
+        );
+    }
 }
