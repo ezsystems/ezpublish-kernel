7386c2d03906c860e3374f84390ebcbbd3cda738
Patrick Allaert <pa@ez.no>
2011-10-07 14:56:56 +0200

    Changed: typo about class name

--- ezp/Content/Tests/FieldType/CheckboxConverterLegacyTest.php
+++ ezp/Content/Tests/FieldType/CheckboxConverterLegacyTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * File containing the CheckboxConverterLegacy class
+ * File containing the CheckboxConverterLegacyTest class
  *
  * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
