b3530b419f973db63b07c1ea3dc4a812b9305055
André R <ar@ez.no>
2012-01-26 18:49:03 +0100

    Add some more missing exceptions

---
+++ ezp/Base/Exception/PropertyNull.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Contains PropertyNull Exception implementation
+ *
+ * @copyright Copyright (C) 2012 andrerom & eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace ezp\Base\Exception;
+use ezp\Base\Exception,
+    Exception as PHPException,
+    InvalidArgumentException;
+
+/**
+ * PropertyNull Exception implementation
+ *
+ * Use:
+ *   throw new PropertyNull( 'nodeId', __CLASS__ );
+ *
+ */
+class PropertyNull extends InvalidArgumentException implements Exception
+{
+    /**
+     * Generates: Property '{$propertyName}' did not have a value[ on class '{$className}']
+     *
+     * @param string $propertyName
+     * @param string|null $className Optionally to specify class in abstract/parent classes
+     * @param PHPException|null $previous
+     */
+    public function __construct( $propertyName, $className = null, PHPException $previous = null )
+    {
+        if ( $className === null )
+            parent::__construct( "Property '{$propertyName}' did not have a value", 0, $previous );
+        else
+            parent::__construct( "Property '{$propertyName}' did not have a value on class '{$className}'", 0, $previous );
+    }
+}
