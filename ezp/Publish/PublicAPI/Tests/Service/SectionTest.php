507a4d21f5205f4b8e8f182d01e88125f1102210
Petar Spanja <petar.spanja@gmail.com>
2012-01-27 14:18:35 +0100

    added Section new class, default values and missing property tests, remove unused test file

--- ezp/Publish/PublicAPI/Tests/Service/SectionTest.php
+++ ezp/Publish/PublicAPI/Tests/Service/SectionTest.php
@@ -21,6 +21,39 @@ use ezp\Publish\PublicAPI\Tests\Service\Base as BaseServiceTest,
 class SectionTest extends BaseServiceTest
 {
     /**
+     * Test a new class and default values on properties
+     * @covers \ezp\PublicAPI\Values\Content\Section::__construct
+     */
+    public function testNewClass()
+    {
+        $section = new Section();
+        self::assertEquals( $section->id, null );
+        self::assertEquals( $section->identifier, null );
+        self::assertEquals( $section->name, null );
+    }
+
+    /**
+     * @expectedException ezp\Base\Exception\PropertyNotFound
+     * @covers \ezp\PublicAPI\Values\Content\Section::__get
+     */
+    public function testMissingProperty()
+    {
+        $section = new Section();
+        $value = $section->notDefined;
+    }
+
+    /**
+     * @expectedException ezp\Base\Exception\PropertyPermission
+     * @covers \ezp\PublicAPI\Values\Content\Section::__set
+     */
+    public function testReadOnlyProperty()
+    {
+    	self::markTestSkipped( 'ID is a public property, will not fail' );
+        $section = new Section();
+        $section->id = 22;
+    }
+
+    /**
      * Test service function for creating sections
      * @covers \ezp\Publish\PublicAPI\Content\SectionService::create
      */
