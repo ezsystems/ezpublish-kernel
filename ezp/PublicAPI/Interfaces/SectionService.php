cfb8fe7624c3014ee3ab680c4d032a63610a7f69
André R <ar@ez.no>
2012-01-27 18:04:03 +0100

    Change namespaces in @throw to start with '\' (needed for absolute namespace in all cases but 'use' statments)

--- ezp/PublicAPI/Interfaces/SectionService.php
+++ ezp/PublicAPI/Interfaces/SectionService.php
@@ -26,8 +26,8 @@ interface SectionService
      *
      * @return Section The newly create section
      *
-     * @throws ezp\PublicAPI\Interfaces\UnauthorizedException If the current user user is not allowed to create a section
-     * @throws ezp\PublicAPI\Interfaces\IllegalArgumentException If the new identifier in $sectionCreateStruct already exists
+     * @throws \ezp\PublicAPI\Interfaces\UnauthorizedException If the current user user is not allowed to create a section
+     * @throws \ezp\PublicAPI\Interfaces\IllegalArgumentException If the new identifier in $sectionCreateStruct already exists
      */
     public function createSection(SectionCreateStruct $sectionCreateStruct );
 
@@ -39,9 +39,9 @@ interface SectionService
      *
      * @return Section
      *
-     * @throws ezp\PublicAPI\Interfaces\NotFoundException if section could not be found
-     * @throws ezp\PublicAPI\Interfaces\UnauthorizedException If the current user user is not allowed to create a section
-     * @throws ezp\PublicAPI\Interfaces\IllegalArgumentException If the new identifier already exists (if set in the update struct)
+     * @throws \ezp\PublicAPI\Interfaces\NotFoundException if section could not be found
+     * @throws \ezp\PublicAPI\Interfaces\UnauthorizedException If the current user user is not allowed to create a section
+     * @throws \ezp\PublicAPI\Interfaces\IllegalArgumentException If the new identifier already exists (if set in the update struct)
      */
     public function updateSection( Section $section, SectionUpdateStruct $sectionUpdateStruct );
 
@@ -52,8 +52,8 @@ interface SectionService
      *
      * @return Section
      *
-     * @throws ezp\PublicAPI\Interfaces\NotFoundException if section could not be found
-     * @throws ezp\PublicAPI\Interfaces\UnauthorizedException If the current user user is not allowed to read a section
+     * @throws \ezp\PublicAPI\Interfaces\NotFoundException if section could not be found
+     * @throws \ezp\PublicAPI\Interfaces\UnauthorizedException If the current user user is not allowed to read a section
      */
     public function loadSection( $sectionId );
 
@@ -62,7 +62,7 @@ interface SectionService
      *
      * @return array of {@link Section}
      *
-     * @throws ezp\PublicAPI\Interfaces\UnauthorizedException If the current user user is not allowed to read a section
+     * @throws \ezp\PublicAPI\Interfaces\UnauthorizedException If the current user user is not allowed to read a section
      */
     public function loadSections();
 
@@ -73,8 +73,8 @@ interface SectionService
      *
      * @return Section
      *
-     * @throws ezp\PublicAPI\Interfaces\NotFoundException if section could not be found
-     * @throws ezp\PublicAPI\Interfaces\UnauthorizedException If the current user user is not allowed to read a section
+     * @throws \ezp\PublicAPI\Interfaces\NotFoundException if section could not be found
+     * @throws \ezp\PublicAPI\Interfaces\UnauthorizedException If the current user user is not allowed to read a section
      */
     public function loadSectionByIdentifier( $sectionIdentifier );
 
@@ -94,7 +94,7 @@ interface SectionService
      * @param ContentInfo $contentInfo
      * @param Section $section
      *
-     * @throws ezp\PublicAPI\Interfaces\UnauthorizedException If user does not have access to view provided object
+     * @throws \ezp\PublicAPI\Interfaces\UnauthorizedException If user does not have access to view provided object
      */
     public function assignSection( ContentInfo $contentInfo, Section $section );
 
@@ -108,7 +108,7 @@ interface SectionService
      *
      * @return array  a list (string) of descendants which are not changed due to permissions
      *
-     * @throws ezp\PublicAPI\Interfaces\UnauthorizedException If the current user is not allowed to assign a section to the starting point
+     * @throws \ezp\PublicAPI\Interfaces\UnauthorizedException If the current user is not allowed to assign a section to the starting point
      *
      */
     public function assignSectionToSubTree( Location $startingPoint, Section $section );
@@ -118,9 +118,9 @@ interface SectionService
      *
      * @param Section $section
      *
-     * @throws ezp\PublicAPI\Interfaces\NotFoundException If the specified section is not found
-     * @throws ezp\PublicAPI\Interfaces\UnauthorizedException If the current user user is not allowed to delete a section
-     * @throws ezp\PublicAPI\Interfaces\BadStateException  if section can not be deleted
+     * @throws \ezp\PublicAPI\Interfaces\NotFoundException If the specified section is not found
+     * @throws \ezp\PublicAPI\Interfaces\UnauthorizedException If the current user user is not allowed to delete a section
+     * @throws \ezp\PublicAPI\Interfaces\BadStateException  if section can not be deleted
      *         because it is still assigned to some contents.
      */
     public function deleteSection( Section $section );
