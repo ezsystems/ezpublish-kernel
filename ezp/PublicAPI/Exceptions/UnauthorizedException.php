0d9d0e0b3076a369eb427f1dcb565cfe3d69039b
Christian Bacher <cba@ez.no>
2012-01-31 14:11:56 +0100

    fixed extends "ForbiddenException"

--- ezp/PublicAPI/Exceptions/UnauthorizedException.php
+++ ezp/PublicAPI/Exceptions/UnauthorizedException.php
@@ -3,7 +3,7 @@ namespace ezp\PublicAPI\Exceptions;
 /**
  * This Exception is thrown if theuser has is not allowed to perform a service operation
  */
-abstract class UnauthorizedException extends ForbiddenException
+abstract class UnauthorizedException extends RuntimeException
 {
 }
 
