0b4980efac4d506e64531b4d28cf2b8f0181aa84
Manuel Pichler <mapi@manuel-pichler.de>
2012-02-03 16:43:21 +0100

    Typo in class doc comment fixed.

--- eZ/Publish/API/Repository/Exceptions/UnauthorizedException.php
+++ eZ/Publish/API/Repository/Exceptions/UnauthorizedException.php
@@ -1,7 +1,7 @@
 <?php
 namespace eZ\Publish\API\Repository\Exceptions;
 /**
- * This Exception is thrown if theuser has is not allowed to perform a service operation
+ * This Exception is thrown if the user has is not allowed to perform a service operation
  */
 abstract class UnauthorizedException extends \RuntimeException
 {
