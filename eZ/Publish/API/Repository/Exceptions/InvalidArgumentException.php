cc9796e3aa7cacdaee1851ea3db36a7cd5a96a31
André R <ar@ez.no>
2012-02-04 13:03:10 +0100

    Add missing InvalidArgumentException

---
+++ eZ/Publish/API/Repository/Exceptions/InvalidArgumentException.php
@@ -0,0 +1,13 @@
+<?php
+namespace eZ\Publish\API\Repository\Exceptions;
+
+use InvalidArgumentException;
+
+/**
+ *
+ * This exception is throen if a service method is called with an illegal or non appriprite value
+ *
+ */
+abstract class InvalidArgumentException extends InvalidArgumentException
+{
+}
