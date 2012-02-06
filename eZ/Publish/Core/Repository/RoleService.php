bb0ab35266495094112ac04ffcd78be18011ed15
Edi Modric <edi.modric@gmail.com>
2012-02-06 17:33:18 +0100

    Implement addPolicy & removePolicy methods in role service

--- eZ/Publish/Core/Repository/RoleService.php
+++ eZ/Publish/Core/Repository/RoleService.php
@@ -1,20 +1,29 @@
 <?php
 namespace  eZ\Publish\Core\Repository;
 
-use eZ\Publish\API\Repository\Values\User\PolicyUpdateStruct;
-use eZ\Publish\API\Repository\Values\User\Policy;
+use eZ\Publish\Core\Repository\Values\User\PolicyUpdateStruct;
+use eZ\Publish\API\Repository\Values\User\PolicyUpdateStruct as APIPolicyUpdateStruct;
+use eZ\Publish\Core\Repository\Values\User\Policy;
+use eZ\Publish\API\Repository\Values\User\Policy as APIPolicy;
 use eZ\Publish\API\Repository\Values\User\RoleUpdateStruct;
-use eZ\Publish\API\Repository\Values\User\PolicyCreateStruct;
-use eZ\Publish\API\Repository\Values\User\Role;
-use eZ\Publish\API\Repository\Values\User\RoleCreateStruct;
+use eZ\Publish\Core\Repository\Values\User\PolicyCreateStruct;
+use eZ\Publish\API\Repository\Values\User\PolicyCreateStruct as APIPolicyCreateStruct;
+use eZ\Publish\Core\Repository\Values\User\Role;
+use eZ\Publish\API\Repository\Values\User\Role as APIRole;
+use eZ\Publish\API\Repository\Values\User\RoleCreateStruct as APIRoleCreateStruct;
 use eZ\Publish\API\Repository\Values\User\RoleAssignment;
 use eZ\Publish\API\Repository\Values\User\Limitation\RoleLimitation;
 use eZ\Publish\API\Repository\Values\User\User;
 use eZ\Publish\API\Repository\Values\User\UserGroup;
 
+use eZ\Publish\SPI\Persistence\User\Policy as SPIPolicy;
+use eZ\Publish\SPI\Persistence\User\Role as SPIRole;
+
 use eZ\Publish\API\Repository\RoleService as RoleServiceInterface;
 use eZ\Publish\API\Repository\Repository;
 use eZ\Publish\SPI\Persistence\Handler;
+
+use eZ\Publish\Core\Base\Exceptions\InvalidArgumentValue;
 
 /**
  * This service provides methods for managing Roles and Policies
@@ -57,7 +66,7 @@ class RoleService implements RoleServiceInterface
      *
      * @return \eZ\Publish\API\Repository\Values\User\Role
      */
-    public function createRole( RoleCreateStruct $roleCreateStruct ){}
+    public function createRole( APIRoleCreateStruct $roleCreateStruct ){}
 
     /**
      * Updates the name and (5.x) description of the role
@@ -70,7 +79,7 @@ class RoleService implements RoleServiceInterface
      *
      * @return \eZ\Publish\API\Repository\Values\User\Role
      */
-    public function updateRole( Role $role, RoleUpdateStruct $roleUpdateStruct ){}
+    public function updateRole( APIRole $role, RoleUpdateStruct $roleUpdateStruct ){}
 
     /**
      * adds a new policy to the role
@@ -82,7 +91,42 @@ class RoleService implements RoleServiceInterface
      *
      * @return \eZ\Publish\API\Repository\Values\User\Role
      */
-    public function addPolicy( Role $role, PolicyCreateStruct $policyCreateStruct ){}
+    public function addPolicy( APIRole $role, APIPolicyCreateStruct $policyCreateStruct )
+    {
+        if ( empty( $role->id ) )
+            throw new InvalidArgumentValue( "id", $role->id, "Role" );
+
+        if ( empty( $policyCreateStruct->module ) )
+            throw new InvalidArgumentValue( "module", $policyCreateStruct->module, "PolicyCreateStruct" );
+
+        if ( empty( $policyCreateStruct->function ) )
+            throw new InvalidArgumentValue( "function", $policyCreateStruct->function, "PolicyCreateStruct" );
+
+        if ( $policyCreateStruct->module === '*' && $policyCreateStruct->function !== '*' )
+            throw new InvalidArgumentValue( "module", $policyCreateStruct->module, "PolicyCreateStruct" );
+
+        $limitationsToCreate = '*';
+        if ( $policyCreateStruct->module !== '*' && $policyCreateStruct->function !== '*'
+             && is_array( $policyCreateStruct->limitations ) && !empty( $policyCreateStruct->limitations ) )
+        {
+            $limitationsToCreate = array();
+            foreach ( $policyCreateStruct->limitations as $limitation )
+            {
+                $limitationsToCreate[$limitation->getIdentifier()] = $limitation->limitationValues;
+            }
+        }
+
+        $policy = new SPIPolicy( array(
+            'module'      => $policyCreateStruct->module,
+            'function'    => $policyCreateStruct->function,
+            'limitations' => $limitationsToCreate
+        ) );
+
+        $this->persistenceHandler->userHandler()->addPolicy( $role->id, $policy );
+
+        $spiRole = $this->persistenceHandler->userHandler()->loadRole( $role->id );
+        return $this->buildDomainRoleObject( $spiRole );
+    }
 
     /**
      * removes a policy from the role
@@ -94,20 +138,32 @@ class RoleService implements RoleServiceInterface
      *
      * @return \eZ\Publish\API\Repository\Values\User\Role the updated role
      */
-    public function removePolicy( Role $role, Policy $policy ){}
+    public function removePolicy( APIRole $role, APIPolicy $policy )
+    {
+        if ( empty( $role->id ) )
+            throw new InvalidArgumentValue( "id", $role->id, "Role" );
+
+        if ( empty( $policy->id ) )
+            throw new InvalidArgumentValue( "id", $policy->id, "Policy" );
+
+        $this->persistenceHandler->userHandler()->removePolicy( $role->id, $policy->id );
+
+        $spiRole = $this->persistenceHandler->userHandler()->loadRole( $role->id );
+        return $this->buildDomainRoleObject( $spiRole );
+    }
 
     /**
      * Updates the limitations of a policy. The module and function cannot be changed and
      * the limitaions are replaced by the ones in $roleUpdateStruct
      *
-     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException if the authenticated user is not allowed to u�date a policy
+     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException if the authenticated user is not allowed to update a policy
      *
      * @param \eZ\Publish\API\Repository\Values\User\PolicyUpdateStruct $policyUpdateStruct
      * @param \eZ\Publish\API\Repository\Values\User\Policy $policy
      *
      * @return \eZ\Publish\API\Repository\Values\User\Policy
      */
-    public function updatePolicy( Policy $policy, PolicyUpdateStruct $policyUpdateStruct ){}
+    public function updatePolicy( APIPolicy $policy, APIPolicyUpdateStruct $policyUpdateStruct ){}
 
     /**
      * loads a role for the given name
@@ -137,7 +193,7 @@ class RoleService implements RoleServiceInterface
      *
      * @param \eZ\Publish\API\Repository\Values\User\Role $role
      */
-    public function deleteRole( Role $role ){}
+    public function deleteRole( APIRole $role ){}
 
     /**
      * loads all policies from roles which are assigned to a user or to user groups to which the user belongs
@@ -159,7 +215,7 @@ class RoleService implements RoleServiceInterface
      * @param \eZ\Publish\API\Repository\Values\User\UserGroup $userGroup
      * @param \eZ\Publish\API\Repository\Values\User\Limitation\RoleLimitation $roleLimitation an optional role limitation (which is either a subtree limitation or section limitation)
      */
-    public function assignRoleToUserGroup( Role $role, UserGroup $userGroup, RoleLimitation $roleLimitation = null ){}
+    public function assignRoleToUserGroup( APIRole $role, UserGroup $userGroup, RoleLimitation $roleLimitation = null ){}
 
     /**
      * removes a role from the given user group.
@@ -170,7 +226,7 @@ class RoleService implements RoleServiceInterface
      * @param \eZ\Publish\API\Repository\Values\User\Role $role
      * @param \eZ\Publish\API\Repository\Values\User\UserGroup $userGroup
      */
-    public function unassignRoleFromUserGroup( Role $role, UserGroup $userGroup ){}
+    public function unassignRoleFromUserGroup( APIRole $role, UserGroup $userGroup ){}
 
     /**
      * assigns a role to the given user
@@ -183,7 +239,7 @@ class RoleService implements RoleServiceInterface
      * @param \eZ\Publish\API\Repository\Values\User\User $user
      * @param \eZ\Publish\API\Repository\Values\User\Limitation\RoleLimitation $roleLimitation an optional role limitation (which is either a subtree limitation or section limitation)
      */
-    public function assignRoleToUser( Role $role, User $user, RoleLimitation $roleLimitation = null ){}
+    public function assignRoleToUser( APIRole $role, User $user, RoleLimitation $roleLimitation = null ){}
 
     /**
      * removes a role from the given user.
@@ -194,7 +250,7 @@ class RoleService implements RoleServiceInterface
      * @param \eZ\Publish\API\Repository\Values\User\Role $role
      * @param \eZ\Publish\API\Repository\Values\User\User $user
      */
-    public function unassignRoleFromUser( Role $role, User $user ){}
+    public function unassignRoleFromUser( APIRole $role, User $user ){}
 
     /**
      * returns the assigned user and user groups to this role
@@ -205,7 +261,7 @@ class RoleService implements RoleServiceInterface
      *
      * @return array an array of {@link RoleAssignment}
      */
-    public function getRoleAssignments( Role $role ){}
+    public function getRoleAssignments( APIRole $role ){}
 
     /**
      * returns the roles assigned to the given user
@@ -236,7 +292,12 @@ class RoleService implements RoleServiceInterface
      *
      * @return \eZ\Publish\API\Repository\Values\User\RoleCreateStruct
      */
-    public function newRoleCreateStruct( $name ){}
+    public function newRoleCreateStruct( $name )
+    {
+        return new PolicyCreateStruct( array(
+            'name' => $name
+        ) );
+    }
 
     /**
      * instanciates a policy create class
@@ -246,19 +307,69 @@ class RoleService implements RoleServiceInterface
      *
      * @return \eZ\Publish\API\Repository\Values\User\PolicyCreateStruct
      */
-    public function newPolicyCreateStruct( $module, $function ){}
+    public function newPolicyCreateStruct( $module, $function )
+    {
+        return new PolicyCreateStruct( array(
+            'module'   => $module,
+            'function' => $function
+        ) );
+    }
 
     /**
      * instanciates a policy update class
      *
      * @return \eZ\Publish\API\Repository\Values\User\PolicyUpdateStruct
      */
-    public function newPolicyUpdateStruct(){}
+    public function newPolicyUpdateStruct()
+    {
+        return new PolicyUpdateStruct();
+    }
 
     /**
      * instanciates a policy update class
      *
      * @return \eZ\Publish\API\Repository\Values\User\RoleUpdateStruct
      */
-    public function newRoleUpdateStruct(){}
+    public function newRoleUpdateStruct()
+    {
+        return new RoleUpdateStruct();
+    }
+
+    protected function buildDomainRoleObject( SPIRole $role )
+    {
+        $rolePolicies = array();
+        foreach ( $role->policies as $spiPolicy )
+        {
+            $policyLimitations = '*';
+            if ( $spiPolicy->module !== '*' && $spiPolicy->function !== '*'
+                 && is_array( $spiPolicy->limitations ) && !empty( $spiPolicy->limitations ) )
+            {
+                $policyLimitations = array();
+                foreach ( $spiPolicy->limitations as $limitationIdentifier => $limitationValues )
+                {
+                    $policyLimitations[] = new Limitation( array(
+                        'identifier'       => $limitationIdentifier,
+                        'limitationValues' => $limitationValues
+                    ) );
+                }
+            }
+
+            $tempPolicy = new Policy( array(
+                'id'          => $spiPolicy->id,
+                'roleId'      => $spiPolicy->roleId,
+                'module'      => $spiPolicy->module,
+                'function'    => $spiPolicy->function,
+                'limitations' => $policyLimitations
+            ) );
+
+            $rolePolicies[] = $tempPolicy;
+        }
+
+        return new Role( array(
+            'id'          => $role->id,
+            'name'        => $role->name,
+            'description' => $role->description,
+            'policies'    => $rolePolicies
+        ) );
+    }
 }
