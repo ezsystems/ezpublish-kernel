e7645fc90a66a348ecf4f86e4f3ddd17684f5d93
Manuel Pichler <mapi@manuel-pichler.de>
2012-02-06 15:03:39 +0100

    Fixed: TrashItem must be abstract, because Location is abstract.

--- eZ/Publish/API/Repository/Values/Content/TrashItem.php
+++ eZ/Publish/API/Repository/Values/Content/TrashItem.php
@@ -8,6 +8,6 @@ use eZ\Publish\API\Repository\Values\Content\Location;
  * this class represents a trash item, which is actually a trashed location
  *
  */
-class TrashItem extends Location
+abstract class TrashItem extends Location
 {
 }
