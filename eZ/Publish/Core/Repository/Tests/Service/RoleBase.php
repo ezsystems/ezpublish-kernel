3705002443db98d49af9e5d7ad3bcc419acd9365
Edi Modric <edi.modric@gmail.com>
2012-02-07 16:04:25 +0100

    Add placeholder for role service test

---
+++ eZ/Publish/Core/Repository/Tests/Service/RoleBase.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * File contains: eZ\Publish\Core\Repository\Tests\Service\RoleBase class
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\Core\Repository\Tests\Service;
+use eZ\Publish\Core\Repository\Tests\Service\Base as BaseServiceTest;
+use eZ\Publish\Core\Repository\Values\User\Role;
+use eZ\Publish\API\Repository\Values\User\RoleUpdateStruct;
+use eZ\Publish\SPI\Persistence\User\RoleUpdateStruct as SPIRoleUpdateStruct;
+
+/**
+ * Test case for Role Service
+ *
+ */
+abstract class RoleBase extends BaseServiceTest
+{
+}
