badc817218d2b8dfd76f087ea08bbc255cefbba2
Edi Modric <edi.modric@gmail.com>
2012-02-07 16:08:39 +0100

    Implement some role service methods

--- eZ/Publish/Core/Repository/RoleService.php
+++ eZ/Publish/Core/Repository/RoleService.php
@@ -10,20 +10,25 @@ use eZ\Publish\Core\Repository\Values\User\PolicyCreateStruct;
 use eZ\Publish\API\Repository\Values\User\PolicyCreateStruct as APIPolicyCreateStruct;
 use eZ\Publish\Core\Repository\Values\User\Role;
 use eZ\Publish\API\Repository\Values\User\Role as APIRole;
+use eZ\Publish\Core\Repository\Values\User\RoleCreateStruct;
 use eZ\Publish\API\Repository\Values\User\RoleCreateStruct as APIRoleCreateStruct;
 use eZ\Publish\API\Repository\Values\User\RoleAssignment;
 use eZ\Publish\API\Repository\Values\User\Limitation\RoleLimitation;
 use eZ\Publish\API\Repository\Values\User\User;
 use eZ\Publish\API\Repository\Values\User\UserGroup;
+use eZ\Publish\API\Repository\Values\User\Limitation;
 
 use eZ\Publish\SPI\Persistence\User\Policy as SPIPolicy;
 use eZ\Publish\SPI\Persistence\User\Role as SPIRole;
+use eZ\Publish\SPI\Persistence\User\RoleUpdateStruct as SPIRoleUpdateStruct;
 
 use eZ\Publish\API\Repository\RoleService as RoleServiceInterface;
-use eZ\Publish\API\Repository\Repository;
+use eZ\Publish\API\Repository\Repository as RepositoryInterface;
 use eZ\Publish\SPI\Persistence\Handler;
 
 use eZ\Publish\Core\Base\Exceptions\InvalidArgumentValue;
+use eZ\Publish\Core\Base\Exceptions\IllegalArgumentException;
+use eZ\Publish\Core\Base\Exceptions\NotFoundException;
 
 /**
  * This service provides methods for managing Roles and Policies
@@ -42,7 +47,7 @@ class RoleService implements RoleServiceInterface
     /**
      * @var \eZ\Publish\SPI\Persistence\Handler
      */
-    protected $persistenceHandler;
+    public $persistenceHandler;
 
     /**
      * Setups service with reference to repository object that created it & corresponding handler
@@ -50,7 +55,7 @@ class RoleService implements RoleServiceInterface
      * @param \eZ\Publish\API\Repository\Repository  $repository
      * @param \eZ\Publish\SPI\Persistence\Handler $handler
      */
-    public function __construct( Repository $repository, Handler $handler )
+    public function __construct( RepositoryInterface $repository, Handler $handler )
     {
         $this->repository = $repository;
         $this->persistenceHandler = $handler;
@@ -66,7 +71,24 @@ class RoleService implements RoleServiceInterface
      *
      * @return \eZ\Publish\API\Repository\Values\User\Role
      */
-    public function createRole( APIRoleCreateStruct $roleCreateStruct ){}
+    public function createRole( APIRoleCreateStruct $roleCreateStruct )
+    {
+        if ( empty( $roleCreateStruct->name ) )
+            throw new InvalidArgumentValue( "name", $roleCreateStruct->name, "RoleCreateStruct" );
+
+        try
+        {
+            $existingRole = $this->loadRole( $roleCreateStruct->name );
+            if ( $existingRole !== null )
+                throw new IllegalArgumentException( "name", $roleCreateStruct->name );
+        }
+        catch ( NotFoundException $e ) {}
+
+        $spiRole = $this->buildPersistanceRoleObject( $roleCreateStruct );
+        $createdRole = $this->persistenceHandler->userHandler()->createRole( $spiRole );
+
+        return $this->buildDomainRoleObject( $createdRole );
+    }
 
     /**
      * Updates the name and (5.x) description of the role
@@ -79,7 +101,33 @@ class RoleService implements RoleServiceInterface
      *
      * @return \eZ\Publish\API\Repository\Values\User\Role
      */
-    public function updateRole( APIRole $role, RoleUpdateStruct $roleUpdateStruct ){}
+    public function updateRole( APIRole $role, RoleUpdateStruct $roleUpdateStruct )
+    {
+        if ( empty( $role->id ) )
+            throw new InvalidArgumentValue( "id", $role->id, "Role" );
+
+        if ( !empty( $roleUpdateStruct->name ) )
+        {
+            try
+            {
+                $existingRole = $this->loadRole( $roleUpdateStruct->name );
+                if ( $existingRole !== null )
+                    throw new IllegalArgumentException( "name", $roleUpdateStruct->name );
+            }
+            catch ( NotFoundException $e ) {}
+        }
+
+        $spiRole = $this->persistenceHandler->userHandler()->loadRole( $role->id );
+
+        $this->persistenceHandler->userHandler()->updateRole( new SPIRoleUpdateStruct( array(
+            'id'          => $role->id,
+            'name'        => !empty( $roleUpdateStruct->name ) ? $roleUpdateStruct->name : $spiRole->name,
+            //'description' => !empty( $roleUpdateStruct->description ) ? $roleUpdateStruct->description : $spiRole->description,
+        ) ) );
+
+        $spiRole = $this->persistenceHandler->userHandler()->loadRole( $role->id );
+        return $this->buildDomainRoleObject( $spiRole );
+    }
 
     /**
      * adds a new policy to the role
@@ -105,24 +153,14 @@ class RoleService implements RoleServiceInterface
         if ( $policyCreateStruct->module === '*' && $policyCreateStruct->function !== '*' )
             throw new InvalidArgumentValue( "module", $policyCreateStruct->module, "PolicyCreateStruct" );
 
-        $limitationsToCreate = '*';
-        if ( $policyCreateStruct->module !== '*' && $policyCreateStruct->function !== '*'
-             && is_array( $policyCreateStruct->limitations ) && !empty( $policyCreateStruct->limitations ) )
-        {
-            $limitationsToCreate = array();
-            foreach ( $policyCreateStruct->limitations as $limitation )
-            {
-                $limitationsToCreate[$limitation->getIdentifier()] = $limitation->limitationValues;
-            }
-        }
+        // load role to check existence
+        $this->persistenceHandler->userHandler()->loadRole( $role->id );
 
-        $policy = new SPIPolicy( array(
-            'module'      => $policyCreateStruct->module,
-            'function'    => $policyCreateStruct->function,
-            'limitations' => $limitationsToCreate
-        ) );
+        $spiPolicy = $this->buildPersistancePolicyObject( $policyCreateStruct->module,
+                                                          $policyCreateStruct->function,
+                                                          $policyCreateStruct->limitations );
 
-        $this->persistenceHandler->userHandler()->addPolicy( $role->id, $policy );
+        $this->persistenceHandler->userHandler()->addPolicy( $role->id, $spiPolicy );
 
         $spiRole = $this->persistenceHandler->userHandler()->loadRole( $role->id );
         return $this->buildDomainRoleObject( $spiRole );
@@ -145,6 +183,9 @@ class RoleService implements RoleServiceInterface
 
         if ( empty( $policy->id ) )
             throw new InvalidArgumentValue( "id", $policy->id, "Policy" );
+
+        // load role to check existence
+        $this->persistenceHandler->userHandler()->loadRole( $role->id );
 
         $this->persistenceHandler->userHandler()->removePolicy( $role->id, $policy->id );
 
@@ -193,7 +234,16 @@ class RoleService implements RoleServiceInterface
      *
      * @param \eZ\Publish\API\Repository\Values\User\Role $role
      */
-    public function deleteRole( APIRole $role ){}
+    public function deleteRole( APIRole $role )
+    {
+        if ( empty( $role->id ) )
+            throw new InvalidArgumentValue( "id", $role->id, "Role" );
+
+        // load role to check existence
+        $this->persistenceHandler->userHandler()->loadRole( $role->id );
+
+        $this->persistenceHandler->userHandler()->deleteRole( $role->id );
+    }
 
     /**
      * loads all policies from roles which are assigned to a user or to user groups to which the user belongs
@@ -204,7 +254,24 @@ class RoleService implements RoleServiceInterface
      *
      * @return array an array of {@link Policy}
      */
-    public function loadPoliciesByUserId( $userId ){}
+    public function loadPoliciesByUserId( $userId )
+    {
+        if ( empty( $userId ) )
+            throw new InvalidArgumentValue( "userId", $userId );
+
+        $spiPolicies = $this->persistenceHandler->userHandler()->loadPoliciesByUserId( $userId );
+
+        $policies = array();
+        if ( is_array( $spiPolicies ) && !empty( $spiPolicies ) )
+        {
+            foreach ( $spiPolicies as $spiPolicy )
+            {
+                $policies[] = $this->buildDomainPolicyObject( $spiPolicy );
+            }
+        }
+
+        return $policies;
+    }
 
     /**
      * assigns a role to the given user group
@@ -286,7 +353,7 @@ class RoleService implements RoleServiceInterface
     public function getRoleAssignmentsForUserGroup( UserGroup $userGroup ){}
 
     /**
-     * instanciates a role create class
+     * instantiates a role create class
      *
      * @param string $name
      *
@@ -294,13 +361,15 @@ class RoleService implements RoleServiceInterface
      */
     public function newRoleCreateStruct( $name )
     {
-        return new PolicyCreateStruct( array(
-            'name' => $name
+        return new RoleCreateStruct( array(
+            'name'        => $name,
+            'description' => '',
+            'policies'    => array()
         ) );
     }
 
     /**
-     * instanciates a policy create class
+     * instantiates a policy create class
      *
      * @param string $module
      * @param string $function
@@ -310,23 +379,26 @@ class RoleService implements RoleServiceInterface
     public function newPolicyCreateStruct( $module, $function )
     {
         return new PolicyCreateStruct( array(
-            'module'   => $module,
-            'function' => $function
+            'module'      => $module,
+            'function'    => $function,
+            'limitations' => array()
         ) );
     }
 
     /**
-     * instanciates a policy update class
+     * instantiates a policy update class
      *
      * @return \eZ\Publish\API\Repository\Values\User\PolicyUpdateStruct
      */
     public function newPolicyUpdateStruct()
     {
-        return new PolicyUpdateStruct();
+        return new PolicyUpdateStruct( array(
+            'limitations' => array()
+        ) );
     }
 
     /**
-     * instanciates a policy update class
+     * instantiates a policy update class
      *
      * @return \eZ\Publish\API\Repository\Values\User\RoleUpdateStruct
      */
@@ -335,41 +407,182 @@ class RoleService implements RoleServiceInterface
         return new RoleUpdateStruct();
     }
 
+    /**
+     * Maps provided SPI Role value object to API Role value object
+     *
+     * @param \eZ\Publish\SPI\Persistence\User\Role $role
+     *
+     * @return \eZ\Publish\API\Repository\Values\User\Role
+     */
     protected function buildDomainRoleObject( SPIRole $role )
     {
         $rolePolicies = array();
         foreach ( $role->policies as $spiPolicy )
         {
-            $policyLimitations = '*';
-            if ( $spiPolicy->module !== '*' && $spiPolicy->function !== '*'
-                 && is_array( $spiPolicy->limitations ) && !empty( $spiPolicy->limitations ) )
-            {
-                $policyLimitations = array();
-                foreach ( $spiPolicy->limitations as $limitationIdentifier => $limitationValues )
-                {
-                    $policyLimitations[] = new Limitation( array(
-                        'identifier'       => $limitationIdentifier,
-                        'limitationValues' => $limitationValues
-                    ) );
-                }
-            }
-
-            $tempPolicy = new Policy( array(
-                'id'          => $spiPolicy->id,
-                'roleId'      => $spiPolicy->roleId,
-                'module'      => $spiPolicy->module,
-                'function'    => $spiPolicy->function,
-                'limitations' => $policyLimitations
-            ) );
-
-            $rolePolicies[] = $tempPolicy;
+            $rolePolicies[] = $this->buildDomainPolicyObject( $spiPolicy );
         }
 
         return new Role( array(
             'id'          => $role->id,
             'name'        => $role->name,
-            'description' => $role->description,
+            // 'description' => $role->description,
             'policies'    => $rolePolicies
+        ) );
+    }
+
+    /**
+     * Maps provided SPI Policy value object to API Policy value object
+     *
+     * @param \eZ\Publish\SPI\Persistence\User\Policy $policy
+     *
+     * @return \eZ\Publish\API\Repository\Values\User\Policy
+     */
+    protected function buildDomainPolicyObject( SPIPolicy $policy )
+    {
+        $policyLimitations = '*';
+        if ( $policy->module !== '*' && $policy->function !== '*'
+             && is_array( $policy->limitations ) && !empty( $policy->limitations ) )
+        {
+            $policyLimitations = array();
+            foreach ( $policy->limitations as $limitationIdentifier => $limitationValues )
+            {
+                $limitation = $this->getLimitationFromIdentifier( $limitationIdentifier );
+                $limitation->limitationValues = $limitationValues;
+                $policyLimitations[] = $limitation;
+            }
+        }
+
+        return new Policy( array(
+            'id'          => $policy->id,
+            'roleId'      => $policy->roleId,
+            'module'      => $policy->module,
+            'function'    => $policy->function,
+            'limitations' => $policyLimitations
+        ) );
+    }
+
+    /**
+     * Returns the correct implementation of API Limitation value object
+     * based on provided identifier
+     *
+     * @param string $identifier
+     *
+     * @return \eZ\Publish\API\Repository\Values\User\Limitation
+     */
+    protected function getLimitationFromIdentifier( $identifier )
+    {
+        switch( $identifier )
+        {
+            case Limitation::CONTENTTYPE :
+                return new \eZ\Publish\API\Repository\Values\User\Limitation\ContentTypeLimitation();
+                break;
+
+            case Limitation::LANGUAGE :
+                return new \eZ\Publish\API\Repository\Values\User\Limitation\LanguageLimitation();
+                break;
+
+            case Limitation::LOCATION :
+                return new \eZ\Publish\API\Repository\Values\User\Limitation\LocationLimitation();
+                break;
+
+            case Limitation::OWNER :
+                return new \eZ\Publish\API\Repository\Values\User\Limitation\OwnerLimitation();
+                break;
+
+            case Limitation::PARENTOWNER :
+                return new \eZ\Publish\API\Repository\Values\User\Limitation\ParentOwnerLimitation();
+                break;
+
+            case Limitation::PARENTCONTENTTYPE :
+                return new \eZ\Publish\API\Repository\Values\User\Limitation\ParentContentTypeLimitation();
+                break;
+
+            case Limitation::PARENTDEPTH :
+                return new \eZ\Publish\API\Repository\Values\User\Limitation\ParentDepthLimitation();
+                break;
+
+            case Limitation::SECTION :
+                return new \eZ\Publish\API\Repository\Values\User\Limitation\SectionLimitation();
+                break;
+
+            case Limitation::SITEACCESS :
+                return new \eZ\Publish\API\Repository\Values\User\Limitation\SiteaccessLimitation();
+                break;
+
+            case Limitation::STATE :
+                return new \eZ\Publish\API\Repository\Values\User\Limitation\StateLimitation();
+                break;
+
+            case Limitation::SUBTREE :
+                return new \eZ\Publish\API\Repository\Values\User\Limitation\SubtreeLimitation();
+                break;
+
+            case Limitation::USERGROUP :
+                return new \eZ\Publish\API\Repository\Values\User\Limitation\UserGroupLimitation();
+                break;
+
+            case Limitation::PARENTUSERGROUP :
+                return new \eZ\Publish\API\Repository\Values\User\Limitation\ParentUserGroupLimitation();
+                break;
+
+            default:
+                return new \eZ\Publish\API\Repository\Values\User\Limitation\CustomLimitation( $identifier );
+        }
+    }
+
+    /**
+     * Creates SPI Role value object from provided API role create struct
+     *
+     * @param \eZ\Publish\API\Repository\Values\User\RoleCreateStruct $roleCreateStruct
+     *
+     * @return \eZ\Publish\SPI\Persistence\User\Role
+     */
+    protected function buildPersistanceRoleObject( APIRoleCreateStruct $roleCreateStruct )
+    {
+        $policiesToCreate = array();
+        if ( !empty( $roleCreateStruct->policies ) )
+        {
+            foreach ( $roleCreateStruct->policies as $policy )
+            {
+                /* @var $policy APIPolicy */
+                $policiesToCreate[] = $this->buildPersistancePolicyObject( $policy->module,
+                                                                           $policy->function,
+                                                                           $policy->getLimitations() );
+            }
+        }
+
+        return new SPIRole( array(
+            'name'     => $roleCreateStruct->name,
+            'policies' => $policiesToCreate
+        ) );
+    }
+
+    /**
+     * Creates SPI Policy value object from provided module, function and limitations
+     *
+     * @param string $module
+     * @param string $function
+     * @param array $limitations
+     *
+     * @return \eZ\Publish\SPI\Persistence\User\Policy
+     */
+    protected function buildPersistancePolicyObject( $module, $function, array $limitations )
+    {
+        $limitationsToCreate = '*';
+        if ( $module !== '*' && $function !== '*' && is_array( $limitations ) && !empty( $limitations ) )
+        {
+            $limitationsToCreate = array();
+            foreach ( $limitations as $limitation )
+            {
+                /* @var $limitation Limitation */
+                $limitationsToCreate[$limitation->getIdentifier()] = $limitation->limitationValues;
+            }
+        }
+
+        return new SPIPolicy( array(
+            'module'      => $module,
+            'function'    => $function,
+            'limitations' => $limitationsToCreate
         ) );
     }
 }
