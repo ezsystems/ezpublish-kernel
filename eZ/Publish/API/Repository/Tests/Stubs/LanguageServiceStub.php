a9c3652da0304bf04385bbcb3e60446bb955a649
Manuel Pichler <mapi@manuel-pichler.de>
2012-02-07 14:25:33 +0100

    Added: Stub version of the LanguageService

---
+++ eZ/Publish/API/Repository/Tests/Stubs/LanguageServiceStub.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * File containing the LanguageServiceStub class
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\API\Repository\Tests\Stubs;
+
+use \eZ\Publish\API\Repository\LanguageService;
+
+use \eZ\Publish\API\Repository\Values\Content\Language;
+use \eZ\Publish\API\Repository\Values\Content\LanguageCreateStruct;
+
+/**
+ * Stubbed implementation of the {@link \eZ\Publish\API\Repository\LanguageService}
+ * interface.
+ *
+ * @see \eZ\Publish\API\Repository\LanguageService
+ */
+class LanguageServiceStub implements LanguageService
+{
+    /**
+     * Creates the a new Language in the content repository
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException If user does not have access to content translations
+     * @throws \eZ\Publish\API\Repository\Exceptions\IllegalArgumentException if the languageCode already exists
+     *
+     * @param \eZ\Publish\API\Repository\Values\Content\LanguageCreateStruct $languageCreateStruct
+     *
+     * @return \eZ\Publish\API\Repository\Values\Content\Language
+     */
+    public function createLanguage( LanguageCreateStruct $languageCreateStruct )
+    {
+        // TODO: Implement createLanguage() method.
+    }
+
+    /**
+     * Changes the name of the language in the content repository
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException If user does not have access to content translations
+     *
+     * @param \eZ\Publish\API\Repository\Values\Content\Language $language
+     * @param string $newName
+     *
+     * @return \eZ\Publish\API\Repository\Values\Content\Language
+     */
+    public function updateLanguageName( Language $language, $newName )
+    {
+        // TODO: Implement updateLanguageName() method.
+    }
+
+    /**
+     * enables a language
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException If user does not have access to content translations
+     *
+     * @param \eZ\Publish\API\Repository\Values\Content\Language $language
+     */
+    public function enableLanguage( Language $language )
+    {
+        // TODO: Implement enableLanguage() method.
+    }
+
+    /**
+     * disables a language
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException If user does not have access to content translations
+     *
+     * @param \eZ\Publish\API\Repository\Values\Content\Language $language
+     */
+    public function disableLanguage( Language $language )
+    {
+        // TODO: Implement disableLanguage() method.
+    }
+
+    /**
+     * Loads a Language from its language code ($languageCode)
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException if language could not be found
+     *
+     * @param string $languageCode
+     *
+     * @return \eZ\Publish\API\Repository\Values\Content\Language
+     */
+    public function loadLanguage( $languageCode )
+    {
+        // TODO: Implement loadLanguage() method.
+    }
+
+    /**
+     * Loads all Languages
+     *
+     * @return array an aray of {@link  \eZ\Publish\API\Repository\Values\Content\Language}
+     */
+    public function loadLanguages()
+    {
+        // TODO: Implement loadLanguages() method.
+    }
+
+    /**
+     * Loads a Language by its id ($languageId)
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException if language could not be found
+     *
+     * @param int $languageId
+     *
+     * @return \eZ\Publish\API\Repository\Values\Content\Language
+     */
+    public function loadLanguageById( $languageId )
+    {
+        // TODO: Implement loadLanguageById() method.
+    }
+
+    /**
+     * Deletes  a language from content repository
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\IllegalArgumentException
+     *         if language can not be deleted
+     *         because it is still assigned to some content / type / (...).
+     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException If user does not have access to content translations
+     *
+     * @param \eZ\Publish\API\Repository\Values\Content\Language $language
+     */
+    public function deleteLanguage( Language $language )
+    {
+        // TODO: Implement deleteLanguage() method.
+    }
+
+    /**
+     * returns a configured default language code
+     *
+     * @return string
+     */
+    public function getDefaultLanguageCode()
+    {
+        // TODO: Implement getDefaultLanguageCode() method.
+    }
+
+}
