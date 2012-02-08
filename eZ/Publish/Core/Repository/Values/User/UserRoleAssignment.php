9c2d95ca06046b354c9e38f161139efb696af652
Edi Modric <edi.modric@gmail.com>
2012-02-08 11:02:38 +0100

    Correct implementation of RoleAssignment value objects

--- eZ/Publish/Core/Repository/Values/User/UserRoleAssignment.php
+++ eZ/Publish/Core/Repository/Values/User/UserRoleAssignment.php
@@ -1,22 +1,58 @@
 <?php
 namespace eZ\Publish\Core\Repository\Values\User;
 
-use eZ\Publish\Core\Repository\Values\User\RoleAssignment;
+use eZ\Publish\API\Repository\Values\User\UserRoleAssignment as APIUserRoleAssignment;
 
 /**
  *
  * This class represents a user to role assignment
  *
- * @property-read \eZ\Publish\API\Repository\Values\User\User $user user group to which the role is assigned to
+ * @property-read \eZ\Publish\API\Repository\Values\User\Limitation\RoleLimitation $limitation the limitation of this role assignment
+ * @property-read \eZ\Publish\API\Repository\Values\User\Role $role the role which is assigned to the user
+ * @property-read \eZ\Publish\API\Repository\Values\User\User $user user to which the role is assigned to
  */
-class UserRoleAssignment extends RoleAssignment
+class UserRoleAssignment extends APIUserRoleAssignment
 {
     /**
-     * user group to which the role is assigned to
+     * the limitation of this role assignment
+     *
+     * @var \eZ\Publish\API\Repository\Values\User\Limitation\RoleLimitation
+     */
+    protected $limitation;
+
+    /**
+     * the role which is assigned to the user
+     *
+     * @var \eZ\Publish\API\Repository\Values\User\Role
+     */
+    protected $role;
+
+    /**
+     * user to which the role is assigned to
      *
      * @var \eZ\Publish\API\Repository\Values\User\User
      */
     protected $user;
+
+    /**
+     * returns the limitation of the user role assignment
+     *
+     * @return \eZ\Publish\API\Repository\Values\User\Limitation\RoleLimitation
+     */
+    public function getLimitation()
+    {
+        return $this->limitation;
+    }
+
+    /**
+     * returns the role to which the user is assigned to
+     *
+     * @return \eZ\Publish\API\Repository\Values\User\Role
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
 
     /**
      * returns the user to which the role is assigned to
