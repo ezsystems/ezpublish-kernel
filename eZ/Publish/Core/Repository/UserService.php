eeb715438d4d23035e86b727322e1ef2c1d962b9
Edi Modric <edi.modric@gmail.com>
2012-02-13 18:27:49 +0100

    Implement methods for loading/creating/deleting users/user groups in user service

--- eZ/Publish/Core/Repository/UserService.php
+++ eZ/Publish/Core/Repository/UserService.php
@@ -7,8 +7,10 @@ namespace eZ\Publish\Core\Repository;
 use eZ\Publish\Core\Repository\Values\User\UserCreateStruct,
     eZ\Publish\API\Repository\Values\User\UserCreateStruct as APIUserCreateStruct,
     eZ\Publish\API\Repository\Values\User\UserUpdateStruct,
-    eZ\Publish\API\Repository\Values\User\User,
-    eZ\Publish\API\Repository\Values\User\UserGroup,
+    eZ\Publish\Core\Repository\Values\User\User,
+    eZ\Publish\API\Repository\Values\User\User as APIUser,
+    eZ\Publish\Core\Repository\Values\User\UserGroup,
+    eZ\Publish\API\Repository\Values\User\UserGroup as APIUserGroup,
     eZ\Publish\Core\Repository\Values\User\UserGroupCreateStruct,
     eZ\Publish\API\Repository\Values\User\UserGroupCreateStruct as APIUserGroupCreateStruct,
     eZ\Publish\API\Repository\Values\User\UserGroupUpdateStruct,
@@ -17,8 +19,13 @@ use eZ\Publish\Core\Repository\Values\User\UserCreateStruct,
     eZ\Publish\API\Repository\Repository as RepositoryInterface,
     eZ\Publish\API\Repository\UserService as UserServiceInterface,
 
+    eZ\Publish\SPI\Persistence\User as SPIUser,
+
+    ezp\Base\Exception\NotFound,
+    eZ\Publish\Core\Base\Exceptions\NotFoundException,
     eZ\Publish\Core\Base\Exceptions\InvalidArgumentValue,
-    eZ\Publish\Core\Base\Exceptions\IllegalArgumentException;
+    eZ\Publish\Core\Base\Exceptions\IllegalArgumentException,
+    eZ\Publish\Core\Base\Exceptions\BadStateException;
 
 /**
  * This service provides methods for managing users and user groups
@@ -37,7 +44,7 @@ class UserService implements UserServiceInterface
     /**
      * @var \eZ\Publish\SPI\Persistence\Handler
      */
-    public $persistenceHandler;
+    protected $persistenceHandler;
 
     /**
      * Setups service with reference to repository object that created it & corresponding handler
@@ -68,7 +75,36 @@ class UserService implements UserServiceInterface
      * @throws \eZ\Publish\API\Repository\Exceptions\ContentFieldValidationException if a field in the $userGroupCreateStruct is not valid
      * @throws \eZ\Publish\API\Repository\Exceptions\ContentValidationException if a required field is missing
      */
-    public function createUserGroup( APIUserGroupCreateStruct $userGroupCreateStruct, UserGroup $parentGroup ){}
+    public function createUserGroup( APIUserGroupCreateStruct $userGroupCreateStruct, APIUserGroup $parentGroup )
+    {
+        if ( empty( $parentGroup->id ) )
+            throw new InvalidArgumentValue( "id", $parentGroup->id, "UserGroup" );
+
+        $contentService = $this->repository->getContentService();
+        $locationService = $this->repository->getLocationService();
+
+        $loadedParentGroup = $this->loadUserGroup( $parentGroup->id );
+        $mainParentGroupLocation = $locationService->loadMainLocation( $loadedParentGroup->contentInfo );
+
+        $locationCreateStructs = array();
+        if ( $mainParentGroupLocation !== null )
+        {
+            $locationCreateStruct = $locationService->newLocationCreateStruct( $mainParentGroupLocation->id );
+            $locationCreateStruct->isMainLocation = true;
+            $locationCreateStructs[] = $locationCreateStruct;
+        }
+
+        $contentDraft = $contentService->createContent( $userGroupCreateStruct->contentCreateStruct, $locationCreateStructs );
+        $publishedContent = $contentService->publishVersion( $contentDraft->getVersionInfo() );
+
+        return new UserGroup( array(
+            'id'            => $publishedContent->contentId,
+            'parentId'      => $mainParentGroupLocation !== null ? $mainParentGroupLocation->id : null,
+            // @todo: calculate sub group count
+            'subGroupCount' => 0,
+            'content'       => $publishedContent
+        ) );
+    }
 
     /**
      * Loads a user group for the given id
@@ -80,7 +116,24 @@ class UserService implements UserServiceInterface
      * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException if the authenticated user is not allowed to create a user group
      * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException if the user group with the given id was not found
      */
-    public function loadUserGroup( $id ){}
+    public function loadUserGroup( $id )
+    {
+        if ( empty( $id ) )
+            throw new InvalidArgumentValue( "id", $id );
+
+        $locationService = $this->repository->getLocationService();
+
+        $content = $this->repository->getContentService()->loadContent( $id );
+        $mainLocation = $locationService->loadMainLocation( $content->contentInfo );
+
+        return new UserGroup( array(
+            'id'            => $content->contentId,
+            'parentId'      => $mainLocation !== null ? $mainLocation->parentId : null,
+            //@todo: calculate sub group count
+            'subGroupCount' => 0,
+            'content'       => $content
+        ) );
+    }
 
     /**
      * Loads the sub groups of a user group
@@ -92,7 +145,7 @@ class UserService implements UserServiceInterface
      * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException if the authenticated user is not allowed to read the user group
      * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException if the user group with the given id was not found
      */
-    public function loadSubUserGroups( UserGroup $userGroup ){}
+    public function loadSubUserGroups( APIUserGroup $userGroup ){}
 
     /**
      * Removes a user group
@@ -104,7 +157,15 @@ class UserService implements UserServiceInterface
      * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException if the authenticated user is not allowed to create a user group
      * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException if the user group with the given id was not found
      */
-    public function deleteUserGroup( UserGroup $userGroup ){}
+    public function deleteUserGroup( APIUserGroup $userGroup )
+    {
+        if ( empty( $userGroup->id ) )
+            throw new InvalidArgumentValue( "id", $userGroup->id, "UserGroup" );
+
+        $userGroup = $this->loadUserGroup( $userGroup->id );
+
+        $this->repository->getContentService()->deleteContent( $userGroup->contentInfo );
+    }
 
     /**
      * Moves the user group to another parent
@@ -115,7 +176,7 @@ class UserService implements UserServiceInterface
      * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException if the authenticated user is not allowed to move the user group
      * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException if the user group with the given id was not found
      */
-    public function moveUserGroup( UserGroup $userGroup, UserGroup $newParent ){}
+    public function moveUserGroup( APIUserGroup $userGroup, APIUserGroup $newParent ){}
 
     /**
      * Updates the group profile with fields and meta data
@@ -132,13 +193,13 @@ class UserService implements UserServiceInterface
      * @throws \eZ\Publish\API\Repository\Exceptions\ContentFieldValidationException if a field in the $userGroupUpdateStruct is not valid
      * @throws \eZ\Publish\API\Repository\Exceptions\ContentValidationException if a required field is set empty
      */
-    public function updateUserGroup( UserGroup $userGroup, UserGroupUpdateStruct $userGroupUpdateStruct ){}
+    public function updateUserGroup( APIUserGroup $userGroup, UserGroupUpdateStruct $userGroupUpdateStruct ){}
 
     /**
      * Create a new user. The created user is published by this method
      *
      * @param \eZ\Publish\API\Repository\Values\User\UserCreateStruct $userCreateStruct the data used for creating the user
-     * @param array $parentGroups the groups of type {@link \eZ\Publish\API\Repository\Values\User\UserGroup} which are assigned to the user after creation
+     * @param \eZ\Publish\API\Repository\Values\User\UserGroup[] $parentGroups the groups which are assigned to the user after creation
      *
      * @return \eZ\Publish\API\Repository\Values\User\User
      *
@@ -147,7 +208,43 @@ class UserService implements UserServiceInterface
      * @throws \eZ\Publish\API\Repository\Exceptions\ContentFieldValidationException if a field in the $userCreateStruct is not valid
      * @throws \eZ\Publish\API\Repository\Exceptions\ContentValidationException if a required field is missing
      */
-    public function createUser( APIUserCreateStruct $userCreateStruct, array $parentGroups ){}
+    public function createUser( APIUserCreateStruct $userCreateStruct, array $parentGroups )
+    {
+        if ( empty( $parentGroups ) )
+            throw new InvalidArgumentValue( "parentGroups", $parentGroups );
+
+        if ( empty( $userCreateStruct->login ) )
+            throw new InvalidArgumentValue( "login", $userCreateStruct->login, "UserCreateStruct" );
+
+        if ( empty( $userCreateStruct->email ) )
+            throw new InvalidArgumentValue( "email", $userCreateStruct->email, "UserCreateStruct" );
+
+        if ( empty( $userCreateStruct->password ) )
+            throw new InvalidArgumentValue( "password", $userCreateStruct->password, "UserCreateStruct" );
+
+        $contentService = $this->repository->getContentService();
+        $locationService = $this->repository->getLocationService();
+
+        $locationCreateStructs = array();
+        foreach ( $parentGroups as $parentGroup )
+        {
+            $parentGroup = $this->loadUserGroup( $parentGroup->id );
+            $mainLocation = $locationService->loadMainLocation( $parentGroup->contentInfo );
+            if ( $mainLocation !== null )
+            {
+                $locationCreateStruct = $locationService->newLocationCreateStruct( $mainLocation->id );
+                $locationCreateStruct->isMainLocation = true;
+                $locationCreateStructs[] = $locationCreateStruct;
+            }
+        }
+
+        $contentDraft = $contentService->createContent( $userCreateStruct->contentCreateStruct, $locationCreateStructs );
+        $contentService->publishVersion( $contentDraft->getVersionInfo() );
+
+        $spiUser = $this->buildPersistenceUserObject( $userCreateStruct );
+        $spiUser = $this->persistenceHandler->userHandler()->create( $spiUser );
+        return $this->buildDomainUserObject( $spiUser );
+    }
 
     /**
      * Loads a user
@@ -158,7 +255,22 @@ class UserService implements UserServiceInterface
      *
      * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException if a user with the given id was not found
      */
-    public function loadUser( $userId ){}
+    public function loadUser( $userId )
+    {
+        if ( empty( $userId ) )
+            throw new InvalidArgumentValue( "userId", $userId );
+
+        try
+        {
+            $spiUser = $this->persistenceHandler->userHandler()->load( $userId );
+        }
+        catch ( NotFound $e )
+        {
+            throw new NotFoundException( "user", $userId, $e );
+        }
+
+        return $this->buildDomainUserObject( $spiUser );
+    }
 
     /**
      * Loads a user for the given login and password
@@ -170,7 +282,29 @@ class UserService implements UserServiceInterface
      *
      * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException if a user with the given credentials was not found
      */
-    public function loadUserByCredentials( $login, $password ){}
+    public function loadUserByCredentials( $login, $password )
+    {
+        if ( empty( $login ) )
+            throw new InvalidArgumentValue( "login", $login );
+
+        if ( empty( $password ) )
+            throw new InvalidArgumentValue( "password", $password );
+
+        try
+        {
+            $spiUser = $this->persistenceHandler->userHandler()->loadByLogin( $login );
+        }
+        catch ( NotFound $e )
+        {
+            throw new NotFoundException( "user", $login, $e );
+        }
+
+        // @todo: read site name from settings
+        if ( $spiUser->passwordHash !== $this->createPasswordHash( $login, $password, null, $spiUser->hashAlgorithm ) )
+            throw new InvalidArgumentValue( "password", $password );
+
+        return $this->buildDomainUserObject( $spiUser );
+    }
 
     /**
      * This method deletes a user
@@ -179,7 +313,15 @@ class UserService implements UserServiceInterface
      *
      * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException if the authenticated user is not allowed to delete the user
      */
-    public function deleteUser( User $user ){}
+    public function deleteUser( APIUser $user )
+    {
+        if ( empty( $user->id ) )
+            throw new InvalidArgumentValue( "id", $user->id, "User" );
+
+        $user = $this->loadUser( $user->id );
+
+        $this->repository->getContentService()->deleteContent( $user->contentInfo );
+    }
 
     /**
      * Updates a user
@@ -194,7 +336,7 @@ class UserService implements UserServiceInterface
      * @throws \eZ\Publish\API\Repository\Exceptions\ContentFieldValidationException if a field in the $userUpdateStruct is not valid
      * @throws \eZ\Publish\API\Repository\Exceptions\ContentValidationException if a required field is set empty
      */
-    public function updateUser( User $user, UserUpdateStruct $userUpdateStruct ){}
+    public function updateUser( APIUser $user, UserUpdateStruct $userUpdateStruct ){}
 
     /**
      * Assigns a new user group to the user
@@ -206,7 +348,7 @@ class UserService implements UserServiceInterface
      *
      * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException if the authenticated user is not allowed to assign the user group to the user
      */
-    public function assignUserToUserGroup( User $user, UserGroup $userGroup )
+    public function assignUserToUserGroup( APIUser $user, APIUserGroup $userGroup )
     {
         if ( empty( $user->id ) )
             throw new InvalidArgumentValue( "id", $user->id, "User" );
@@ -263,7 +405,7 @@ class UserService implements UserServiceInterface
      * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException if the authenticated user is not allowed to remove the user group from the user
      * @throws \eZ\Publish\API\Repository\Exceptions\IllegalArgumentException if the user is not in the given user group
      */
-    public function unAssignUssrFromUserGroup( User $user, UserGroup $userGroup )
+    public function unAssignUssrFromUserGroup( APIUser $user, APIUserGroup $userGroup )
     {
         if ( empty( $user->id ) )
             throw new InvalidArgumentValue( "id", $user->id, "User" );
@@ -368,5 +510,69 @@ class UserService implements UserServiceInterface
         return new UserGroupUpdateStruct( array(
             'contentUpdateStruct' => $contentUpdateStruct
         ) );
+    }
+
+    protected function buildDomainUserObject( SPIUser $spiUser )
+    {
+        $content = $this->repository->getContentService()->loadContent( $spiUser->id );
+
+        return new User( array(
+            'id'            => $spiUser->id,
+            'login'         => $spiUser->login,
+            'email'         => $spiUser->email,
+            'passwordHash'  => $spiUser->passwordHash,
+            'hashAlgorithm' => $spiUser->hashAlgorithm,
+            'isEnabled'     => $spiUser->isEnabled,
+            'maxLogin'      => $spiUser->maxLogin,
+            'content'       => $content
+        ) );
+    }
+
+    protected function buildPersistenceUserObject( APIUserCreateStruct $userCreateStruct )
+    {
+        return new SPIUser( array(
+            'login'         => $userCreateStruct->login,
+            'email'         => $userCreateStruct->email,
+            'password'      => $userCreateStruct->password,
+            // @todo: read password hash algorithm and site from INI settings
+            'passwordHash'  => $this->createPasswordHash( $userCreateStruct->login, $userCreateStruct->password, null, null ),
+            'hashAlgorithm' => null,
+            'isEnabled'     => $userCreateStruct->enabled
+        ) );
+    }
+
+    protected function createPasswordHash( $login, $password, $site, $type )
+    {
+        $passwordHash = "";
+
+        switch ( $type )
+        {
+            case User::PASSWORD_HASH_MD5_PASSWORD :
+            {
+                $passwordHash = md5( $password );
+            }
+            break;
+            case User::PASSWORD_HASH_MD5_USER :
+            {
+                $passwordHash = md5( "$login\n$password" );
+            }
+            break;
+            case User::PASSWORD_HASH_MD5_SITE :
+            {
+                $passwordHash = md5( "$login\n$password\n$site" );
+            }
+            break;
+            case User::PASSWORD_HASH_PLAINTEXT :
+            {
+                $passwordHash = $password;
+            }
+            break;
+            default :
+            {
+                $passwordHash = md5( $password );
+            }
+        }
+
+        return $passwordHash;
     }
 }
