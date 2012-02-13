5a6e1aba73d3e6c4d3e187175d327bde47332969
Tobias Schlitt <toby@php.net>
2012-02-13 19:28:08 +0100

    Implemented: ContentTypeGroupStub.

---
+++ eZ/Publish/API/Repository/Tests/Stubs/ContentTypeGroupStub.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * File containing the ContentTypeGroup class
+ *
+ * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\API\Repository\Tests\Stubs;
+use eZ\Publish\API\Repository\Values\ContentType\ContentTypeGroup;
+
+class ContentTypeGroupStub extends ContentTypeGroup
+{
+    public function __construct( array $values )
+    {
+        foreach ( $values as $propertyName => $propertyValue )
+        {
+            $this->$propertyName = $propertyValue;
+        }
+    }
+
+    /**
+     * 5.x only
+     * This method returns the human readable name in all provided languages
+     * of the content type
+     *
+     * The structure of the return value is:
+     * <code>
+     * array( 'eng' => '<name_eng>', 'de' => '<name_de>' );
+     * </code>
+     *
+     * @return string[]
+     */
+    public function getNames()
+    {
+        // TODO: Implement
+    }
+
+    /**
+     * 5.x only
+     * this method returns the name of the content type in the given language
+     * @param string $languageCode
+     * @return string the name for the given language or null if none existis.
+     */
+    public function getName( $languageCode )
+    {
+        // TODO: Implement
+    }
+
+    /**
+     *  5.x only
+     * This method returns the human readable description of the content type
+     * The structure of this field is:
+     * <code>
+     * array( 'eng' => '<description_eng>', 'de' => '<description_de>' );
+     * </code>
+     *
+     * @return string[]
+     */
+    public function getDescriptions()
+    {
+        // TODO: Implement
+    }
+
+    /**
+     * 5.x only
+     * this method returns the name of the content type in the given language
+     * @param string $languageCode
+     * @return string the description for the given language or null if none existis.
+     */
+    public function getDescription( $languageCode )
+    {
+        // TODO: Implement
+    }
+}
