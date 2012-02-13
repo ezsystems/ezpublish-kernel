be95d58be48b42dbe3dfc5930280cecb9603f1ab
Manuel Pichler <mapi@manuel-pichler.de>
2012-02-13 15:59:11 +0100

    Added: Stub version of the RoleCreateStruct

--- eZ/Publish/API/Repository/Tests/Stubs/RoleServiceStub.php
+++ eZ/Publish/API/Repository/Tests/Stubs/RoleServiceStub.php
@@ -20,6 +20,8 @@ use \eZ\Publish\API\Repository\Values\User\RoleUpdateStruct;
 use \eZ\Publish\API\Repository\Values\User\User;
 use \eZ\Publish\API\Repository\Values\User\UserGroup;
 
+use \eZ\Publish\API\Repository\Tests\Stubs\Values\User\RoleCreateStructStub;
+
 /**
  * Stubbed implementation of the {@link \eZ\Publish\API\Repository\RoleService}
  * interface.
@@ -267,7 +269,7 @@ class RoleServiceStub implements RoleService
      */
     public function newRoleCreateStruct( $name )
     {
-        // TODO: Implement newRoleCreateStruct() method.
+        return new RoleCreateStructStub( $name );
     }
 
     /**
@@ -302,75 +304,4 @@ class RoleServiceStub implements RoleService
     {
         // TODO: Implement newRoleUpdateStruct() method.
     }
-
-    function __construct()
-    {
-        // TODO: Implement __construct() method.
-    }
-
-    function __destruct()
-    {
-        // TODO: Implement __destruct() method.
-    }
-
-    function __call( $name, $arguments )
-    {
-        // TODO: Implement __call() method.
-    }
-
-    public static function __callStatic( $name, $arguments )
-    {
-        // TODO: Implement __callStatic() method.
-    }
-
-    function __get( $name )
-    {
-        // TODO: Implement __get() method.
-    }
-
-    function __set( $name, $value )
-    {
-        // TODO: Implement __set() method.
-    }
-
-    function __isset( $name )
-    {
-        // TODO: Implement __isset() method.
-    }
-
-    function __unset( $name )
-    {
-        // TODO: Implement __unset() method.
-    }
-
-    function __sleep()
-    {
-        // TODO: Implement __sleep() method.
-    }
-
-    function __wakeup()
-    {
-        // TODO: Implement __wakeup() method.
-    }
-
-    function __toString()
-    {
-        // TODO: Implement __toString() method.
-    }
-
-    function __invoke()
-    {
-        // TODO: Implement __invoke() method.
-    }
-
-    static function __set_state( $an_array )
-    {
-        // TODO: Implement __set_state() method.
-    }
-
-    function __clone()
-    {
-        // TODO: Implement __clone() method.
-    }
-
 }
---
+++ eZ/Publish/API/Repository/Tests/Stubs/Values/User/RoleCreateStructStub.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * File containing the RoleCreateStructStub class
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\API\Repository\Tests\Stubs\Values\User;
+
+use \eZ\Publish\API\Repository\Values\User\PolicyCreateStruct;
+use \eZ\Publish\API\Repository\Values\User\RoleCreateStruct;
+
+/**
+ * Stubbed implementation of the {@link \eZ\Publish\API\Repository\RoleCreateStruct}
+ * interface.
+ *
+ * @see \eZ\Publish\API\Repository\RoleCreateStruct
+ */
+class RoleCreateStructStub extends RoleCreateStruct
+{
+    /**
+     * instanciates a role create class
+     *
+     * @param string $name
+     */
+    public function __construct( $name )
+    {
+        parent::__construct( array( 'name' => $name ) );
+    }
+
+    /**
+     * Adds a policy to this role
+     *
+     * @param \eZ\Publish\API\Repository\Values\User\PolicyCreateStruct $policyCreateStruct
+     */
+    public function addPolicy( PolicyCreateStruct $policyCreateStruct )
+    {
+        // TODO: Implement addPolicy() method.
+    }
+}
