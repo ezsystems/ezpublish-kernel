81e5ecbb584cdfd694339712c9996b23cf1b7bee
Tobias Schlitt <toby@php.net>
2012-02-14 17:30:38 +0100

    Tested ContentTypeGroupUpdateStruct.

--- eZ/Publish/API/Repository/Tests/Values/ContentType/ContentTypeGroupUpdateStructTest.php
+++ eZ/Publish/API/Repository/Tests/Values/ContentType/ContentTypeGroupUpdateStructTest.php
@@ -22,36 +22,99 @@ class ContentTypeGroupUpdateStructTest extends BaseTest
      * Test for the setName() method.
      *
      * @return void
-     * @see \eZ\Publish\API\Repository\Values\ContentType\ContentTypeGroupUpdateStruct::setName()
-     * 
+     * @see \eZ\Publish\API\Repository\Values\ContentType\ContentTypeGroupUpdateStruct::setName($name, $language)
+     * @dep_ends eZ\Publish\API\Repository\Tests\ContentTypeServiceTest::testNewContentTypeGroupUpdateStruct
      */
-    public function testSetName()
+    public function testSetNameWithLanguage()
     {
-        $this->markTestIncomplete( "Test for ContentTypeGroupUpdateStruct::setName() is not implemented." );
+        $repository =  $this->getRepository();
+
+        /* BEGIN: Use Case */
+        $contentTypeService = $repository->getContentTypeService();
+
+        $groupUpdate = $contentTypeService->newContentTypeGroupUpdateStruct(
+            'new-group'
+        );
+
+        $groupUpdate->setName( 'John Michael Dorian', 'eng_US' );
+        /* END: Use Case */
+
+        $this->assertEquals(
+            'John Michael Dorian',
+            $groupUpdate->names['eng_US']
+        );
     }
 
     /**
      * Test for the setName() method.
      *
      * @return void
-     * @see \eZ\Publish\API\Repository\Values\ContentType\ContentTypeGroupUpdateStruct::setName($name, $language)
-     * 
+     * @see \eZ\Publish\API\Repository\Values\ContentType\ContentTypeGroupUpdateStruct::setName()
+     * @dep_ends eZ\Publish\API\Repository\Tests\Values\ContentType\ContentTypeGroupUpdateStructTest::testSetNameWithLanguage
      */
-    public function testSetNameWithSecondParameter()
+    public function testSetName()
     {
-        $this->markTestIncomplete( "Test for ContentTypeGroupUpdateStruct::setName() is not implemented." );
+        $repository =  $this->getRepository();
+
+        /* BEGIN: Use Case */
+        $contentTypeService = $repository->getContentTypeService();
+
+        $groupUpdate = $contentTypeService->newContentTypeGroupUpdateStruct(
+            'new-group'
+        );
+        $groupUpdate->mainLanguageCode = 'de_DE';
+
+        $groupUpdate->setName( 'John Michael Dorian' );
+        /* END: Use Case */
+
+        $this->assertEquals(
+            'John Michael Dorian',
+            $groupUpdate->names['de_DE']
+        );
     }
 
     /**
-     * Test for the setDescription() method.
+     * Test for the setName() method via magic property access.
      *
      * @return void
-     * @see \eZ\Publish\API\Repository\Values\ContentType\ContentTypeGroupUpdateStruct::setDescription()
-     * 
+     * @see \eZ\Publish\API\Repository\Values\ContentType\ContentTypeGroupUpdateStruct::setName()
+     * @dep_ends eZ\Publish\API\Repository\Tests\Values\ContentType\ContentTypeGroupUpdateStructTest::testSetName
      */
-    public function testSetDescription()
+    public function testSetNameMagicAccess()
     {
-        $this->markTestIncomplete( "Test for ContentTypeGroupUpdateStruct::setDescription() is not implemented." );
+        $repository =  $this->getRepository();
+
+        /* BEGIN: Use Case */
+        $contentTypeService = $repository->getContentTypeService();
+
+        $groupUpdate = $contentTypeService->newContentTypeGroupUpdateStruct(
+            'new-group'
+        );
+        $groupUpdate->mainLanguageCode = 'de_DE';
+
+        $groupUpdate->name = 'John Michael Dorian';
+        /* END: Use Case */
+
+        $this->assertEquals(
+            'John Michael Dorian',
+            $groupUpdate->names['de_DE']
+        );
+        return $groupUpdate;
+    }
+
+    /**
+     * Test for magic property access to $name
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\Values\ContentType\ContentTypeGroupUpdateStruct::setName()
+     * @depends eZ\Publish\API\Repository\Tests\Values\ContentType\ContentTypeGroupUpdateStructTest::testSetNameMagicAccess
+     */
+    public function testGetNameMagicAccess( $groupUpdate )
+    {
+        $this->assertEquals(
+            'John Michael Dorian',
+            $groupUpdate->name
+        );
     }
 
     /**
@@ -61,9 +124,95 @@ class ContentTypeGroupUpdateStructTest extends BaseTest
      * @see \eZ\Publish\API\Repository\Values\ContentType\ContentTypeGroupUpdateStruct::setDescription($description, $language)
      * 
      */
-    public function testSetDescriptionWithSecondParameter()
+    public function testSetDescriptionWithLanguage()
     {
-        $this->markTestIncomplete( "Test for ContentTypeGroupUpdateStruct::setDescription() is not implemented." );
+        $repository =  $this->getRepository();
+
+        /* BEGIN: Use Case */
+        $contentTypeService = $repository->getContentTypeService();
+
+        $groupUpdate = $contentTypeService->newContentTypeGroupUpdateStruct(
+            'new-group'
+        );
+
+        $groupUpdate->setDescription( 'Elliot Reid', 'eng_US' );
+        /* END: Use Case */
+
+        $this->assertEquals(
+            'Elliot Reid',
+            $groupUpdate->descriptions['eng_US']
+        );
     }
 
+    /**
+     * Test for the setDescription() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\Values\ContentType\ContentTypeGroupUpdateStruct::setDescription()
+     * @dep_ends eZ\Publish\API\Repository\Tests\Values\ContentType\ContentTypeGroupUpdateStructTest::testSetDescriptionWithLanguage
+     */
+    public function testSetDescription()
+    {
+        $repository =  $this->getRepository();
+
+        /* BEGIN: Use Case */
+        $contentTypeService = $repository->getContentTypeService();
+
+        $groupUpdate = $contentTypeService->newContentTypeGroupUpdateStruct(
+            'new-group'
+        );
+        $groupUpdate->mainLanguageCode = 'de_DE';
+
+        $groupUpdate->setDescription( 'Elliot Reid' );
+        /* END: Use Case */
+
+        $this->assertEquals(
+            'Elliot Reid',
+            $groupUpdate->descriptions['de_DE']
+        );
+    }
+
+    /**
+     * Test for the setDescription() method via magic property access.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\Values\ContentType\ContentTypeGroupUpdateStruct::setDescription()
+     * @dep_ends eZ\Publish\API\Repository\Tests\Values\ContentType\ContentTypeGroupUpdateStructTest::testSetDescription
+     */
+    public function testSetDescriptionMagicAccess()
+    {
+        $repository =  $this->getRepository();
+
+        /* BEGIN: Use Case */
+        $contentTypeService = $repository->getContentTypeService();
+
+        $groupUpdate = $contentTypeService->newContentTypeGroupUpdateStruct(
+            'new-group'
+        );
+        $groupUpdate->mainLanguageCode = 'de_DE';
+
+        $groupUpdate->description = 'Elliot Reid';
+        /* END: Use Case */
+
+        $this->assertEquals(
+            'Elliot Reid',
+            $groupUpdate->descriptions['de_DE']
+        );
+        return $groupUpdate;
+    }
+
+    /**
+     * Test for magic property access to $description
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\Values\ContentType\ContentTypeGroupUpdateStruct::setDescription()
+     * @depends eZ\Publish\API\Repository\Tests\Values\ContentType\ContentTypeGroupUpdateStructTest::testSetDescriptionMagicAccess
+     */
+    public function testGetDescriptionMagicAccess( $groupUpdate )
+    {
+        $this->assertEquals(
+            'Elliot Reid',
+            $groupUpdate->description
+        );
+    }
 }
