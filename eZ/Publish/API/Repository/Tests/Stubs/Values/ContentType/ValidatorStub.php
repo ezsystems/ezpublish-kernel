dcf53d0e328007dbcbf6dbf3cbfb6f22acac1330
Tobias Schlitt <toby@php.net>
2012-02-15 11:44:09 +0100

    Added ValidatorStub.

---
+++ eZ/Publish/API/Repository/Tests/Stubs/Values/ContentType/ValidatorStub.php
@@ -0,0 +1,12 @@
+<?php
+namespace eZ\Publish\API\Repository\Values\ContentType;
+use eZ\Publish\API\Repository\Values\ContentType\Validator;
+
+/**
+ * This class represents a validatoor provided by a field type.
+ * It consists of a name and a set of paraameters. This field type implementations
+ * are providing a set of concrete validators.
+ */
+class ValidatorStub extends Validator
+{
+}
