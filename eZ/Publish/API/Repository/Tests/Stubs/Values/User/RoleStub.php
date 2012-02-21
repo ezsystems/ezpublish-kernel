ab0a5a8ea8d67f2035efb6bea121e3ea5ca52fb3
Manuel Pichler <mapi@manuel-pichler.de>
2012-02-21 12:43:04 +0100

    Fixed: Latest changes to Role adapted.

--- eZ/Publish/API/Repository/Tests/Stubs/Values/User/RoleStub.php
+++ eZ/Publish/API/Repository/Tests/Stubs/Values/User/RoleStub.php
@@ -39,6 +39,70 @@ class RoleStub extends Role
     }
 
     /**
+     *
+     * This method returns the human readable name in all provided languages
+     * of the role
+     *
+     * The structure of the return value is:
+     * <code>
+     * array( 'eng' => '<name_eng>', 'de' => '<name_de>' );
+     * </code>
+     *
+     * @since 5.0
+     *
+     * @return string[]
+     */
+    public function getNames()
+    {
+        // TODO: Implement getNames() method.
+    }
+
+    /**
+     * this method returns the name of the role in the given language
+     *
+     * @since 5.0
+     *
+     * @param string $languageCode
+     *
+     * @return string the name for the given language or null if none exists.
+     */
+    public function getName($languageCode)
+    {
+        // TODO: Implement getName() method.
+    }
+
+    /**
+     * This method returns the human readable description of the role
+     *
+     * The structure of this field is:
+     * <code>
+     * array( 'eng' => '<description_eng>', 'de' => '<description_de>' );
+     * </code>
+     *
+     * @since 5.0
+     *
+     * @return string[]
+     */
+    public function getDescriptions()
+    {
+        // TODO: Implement getDescriptions() method.
+    }
+
+    /**
+     * this method returns the name of the role in the given language
+     *
+     * @since 5.0
+     *
+     * @param string $languageCode
+     *
+     * @return string the description for the given language or null if none existis.
+     */
+    public function getDescription($languageCode)
+    {
+        // TODO: Implement getDescription() method.
+    }
+
+    /**
      * returns the list of policies of this role
      * @return \eZ\Publish\API\Repository\Values\User\Policy[]
      */
