709a6ac93d18adb009efb1cfd18662c69f981ab7
Edi Modric <edi.modric@gmail.com>
2012-02-22 11:51:37 +0100

    Add TrashItem value object implementation

---
+++ eZ/Publish/Core/Repository/Values/Content/TrashItem.php
@@ -0,0 +1,36 @@
+<?php
+namespace eZ\Publish\Core\Repository\Values\Content;
+
+use eZ\Publish\API\Repository\Values\Content\TrashItem as APITrashItem;
+
+/**
+ *
+ * this class represents a trash item, which is actually a trashed location
+ *
+ */
+class TrashItem extends APITrashItem
+{
+    /**
+     * content info of the content object of this trash item
+     *
+     * @var \eZ\Publish\API\Repository\Values\Content\ContentInfo
+     */
+    protected $contentInfo;
+
+    /**
+     * content ID of the content object of this trash item
+     *
+     * @var int
+     */
+    protected $contentId;
+
+    /**
+     * returns the content info of the content object of this trash item
+     *
+     * @return \eZ\Publish\API\Repository\Values\Content\ContentInfo
+     */
+    public function getContentInfo()
+    {
+        return $this->contentInfo;
+    }
+}
