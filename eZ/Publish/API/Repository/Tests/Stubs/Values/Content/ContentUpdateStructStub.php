82c13d7f0ddcadb93c243bf90329625266edc42b
Manuel Pichler <mapi@manuel-pichler.de>
2012-02-27 14:37:25 +0100

    Fixed: Missing ContentUpdateStruct stub added.

---
+++ eZ/Publish/API/Repository/Tests/Stubs/Values/Content/ContentUpdateStructStub.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * File containing the ContentUpdateStructStub class
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\API\Repository\Tests\Stubs\Values\Content;
+
+use \eZ\Publish\API\Repository\Values\Content\ContentUpdateStruct;
+
+/**
+ * Stubbed implementation of the {@link \eZ\Publish\API\Repository\Values\Content\ContentUpdateStruct}
+ * class.
+ *
+ * @see \eZ\Publish\API\Repository\Values\Content\ContentUpdateStruct
+ */
+class ContentUpdateStructStub extends ContentUpdateStruct
+{
+    /**
+     * Adds a field to the field collection.
+     * This method could also be implemented by ArrayAccess so that
+     * $fields[$fieldDefIdentifier][$language] = $value or without language $fields[$fieldDefIdentifier] = $value
+     * is an equivalent call.
+     *
+     * @param string $fieldDefIdentifier the identifier of the field definition
+     * @param mixed $value Either a plain value which is understandable by the field type or an instance of a Value class provided by the field type
+     * @param bool|string $languageCode If not given on a translatable field the initial language is used,
+     */
+    public function setField( $fieldDefIdentifier, $value, $languageCode = false )
+    {
+
+    }
+}
