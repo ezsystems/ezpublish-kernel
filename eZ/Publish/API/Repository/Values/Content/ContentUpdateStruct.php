791fafd9dcaabd92a0c212d6f2bf30c3c0651a09
Christian Bacher <cba@ez.no>
2012-02-28 09:28:12 +0100

    removed user id and added exception in set Fields

--- eZ/Publish/API/Repository/Values/Content/ContentUpdateStruct.php
+++ eZ/Publish/API/Repository/Values/Content/ContentUpdateStruct.php
@@ -8,12 +8,7 @@ use eZ\Publish\API\Repository\Values\ValueObject;
  */
 abstract class ContentUpdateStruct extends ValueObject
 {
-    /**
-     * @var mixed modifier of the new version. If not set the current authenticated user is used.
-     */
-    public $userId;
-
-
+ 
     /**
      * The language code of the version. In 4.x this code will be used as the language code of the translation
      * (which is shown in the admin interface).
@@ -29,9 +24,11 @@ abstract class ContentUpdateStruct extends ValueObject
      * $fields[$fieldDefIdentifier][$language] = $value or without language $fields[$fieldDefIdentifier] = $value
      * is an equivalent call.
      *
+     * @throws \eZ\Publish\API\Repository\Exceptions\BasStateException if the field is translatable and $initialLanguageCode is not set and $languageCode is NULL
+     *
      * @param string $fieldDefIdentifier the identifier of the field definition
      * @param mixed $value Either a plain value which is understandable by the field type or an instance of a Value class provided by the field type
      * @param bool|string $languageCode If not given on a translatable field the initial language is used,
      */
-    abstract public function setField( $fieldDefIdentifier, $value, $languageCode = false );
+    abstract public function setField( $fieldDefIdentifier, $value, $languageCode = NULL );
 }
