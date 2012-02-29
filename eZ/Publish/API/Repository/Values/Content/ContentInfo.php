b34cbcbb7d2a67e99fdca854958250395aa6634a
Petar Spanja <petar.spanja@gmail.com>
2012-02-29 21:07:18 +0100

    Fix namespaces

--- eZ/Publish/API/Repository/Values/Content/ContentInfo.php
+++ eZ/Publish/API/Repository/Values/Content/ContentInfo.php
@@ -4,11 +4,10 @@ namespace eZ\Publish\API\Repository\Values\Content;
 use eZ\Publish\API\Repository\Values\ContentType\ContentType;
 use eZ\Publish\API\Repository\Values\Content\Location;
 use eZ\Publish\API\Repository\Values\ValueObject;
-use DateTime;
 
 /**
  * This class provides all version independent information of the content object.
- * 
+ *
  * @property-read eZ\Publish\API\Repository\Values\ContentType\ContentType $contentType calls {@link getContentType()}
  * @property-read mixed $contentId The unique id of the content object
  * @property-read string $name the computed name (via name schema) in the main language of the content object
@@ -16,8 +15,8 @@ use DateTime;
  * @property-read int $currentVersionNo Current Version number is the version number of the published version or the version number of a newly created draft (which is 1).
  * @property-read boolean $published true if there exists a published version false otherwise
  * @property-read mixed $ownerId the user id of the owner of the content
- * @property-read DateTime $modificationDate Content modification date
- * @property-read DateTime $publishedDate date of the last publish operation
+ * @property-read \DateTime $modificationDate Content modification date
+ * @property-read \DateTime $publishedDate date of the last publish operation
  * @property-read boolean $alwaysAvailable Indicates if the content object is shown in the mainlanguage if its not present in an other requested language
  * @property-read string $remoteId a global unique id of the content object
  * @property-read string $mainLanguageCode The main language code of the content. If the available flag is set to true the content is shown in this language if the requested language does not exist.
@@ -63,7 +62,7 @@ abstract class ContentInfo extends ValueObject
      * @var boolean Constant.
      */
     protected $published;
-    
+
     /**
      * the owner of this content object
      *
@@ -73,13 +72,13 @@ abstract class ContentInfo extends ValueObject
 
     /**
      * Content modification date
-     * @var DateTime
+     * @var \DateTime
      */
     protected $modificationDate;
 
     /**
      * Content protectedation date
-     * @var DateTime
+     * @var \DateTime
      */
     protected $publishedDate;
 
@@ -100,7 +99,7 @@ abstract class ContentInfo extends ValueObject
      * @var string
      */
     protected $mainLanguageCode;
-    
+
      /**
      * Identifier of the main location.
      *
@@ -110,5 +109,5 @@ abstract class ContentInfo extends ValueObject
      * @var mixed
      */
     protected $mainLocationId;
-    
+
 }
