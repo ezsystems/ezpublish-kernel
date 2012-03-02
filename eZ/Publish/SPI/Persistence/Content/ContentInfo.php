37e6fc1b04e1c4d9b8156514ff42998dea53ae6b
Jérôme Vieilledent <jv@ez.no>
2012-03-02 18:27:08 +0100

    Added ContentInfo for persistence layer

---
+++ eZ/Publish/SPI/Persistence/Content/ContentInfo.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * File containing the ContentInfo class
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\SPI\Persistence\Content;
+use eZ\Publish\SPI\Persistence\ValueObject;
+
+/**
+ * This class provides all version independent information of the content object.
+ * It is similar to {@link \eZ\Publish\Core\Repository\Values\Content\ContentInfo}, but for the persistence layer.
+ * Thus it only contains raw data.
+ */
+class ContentInfo extends ValueObject
+{
+    /**
+   	 * Content's unique ID
+     *
+ 	 * @var mixed
+ 	 */
+	public $contentId;
+
+	/**
+ 	 * Computed name (via name schema) in the main language
+     *
+ 	 * @var string
+ 	 */
+	public $name;
+
+	/**
+ 	 * Content type Id
+     *
+ 	 * @var int
+ 	 */
+	public $contentTypeId;
+
+	/**
+ 	 * Section id the content is assigned to
+     *
+ 	 * @var int
+ 	 */
+	public $sectionId;
+
+	/**
+ 	 * Version number of the current published version.
+     * If the content is not published yet (newly created draft), will be 1
+ 	 *
+ 	 * @var int
+ 	 */
+	public $currentVersionNo;
+
+
+	/**
+ 	 * Flag indicating if content is currently published
+ 	 *
+ 	 * @var boolean
+ 	 */
+	public $isPublished;
+
+	/**
+ 	 * Content owner's id
+ 	 *
+ 	 * @var int
+ 	 */
+	public $ownerId;
+
+	/**
+ 	 * Content modification date, as a UNIX timestamp
+     *
+ 	 * @var int
+ 	 */
+	public $modificationDate;
+
+	/**
+ 	 * Content publication date, as a UNIX timestamp
+     *
+ 	 * @var int
+ 	 */
+	public $publicationDate;
+
+	/**
+ 	 * Indicates if the content is shown in the main language if its not present in an other requested language
+     *
+ 	 * @var boolean
+ 	 */
+	public $isAlwaysAvailable;
+
+	/**
+ 	 * Remote identifier used as a custom identifier for the object
+     *
+ 	 * @var string
+ 	 */
+	public $remoteId;
+
+	/**
+ 	 * The main language code of the content.
+     *
+ 	 * @var string
+ 	 */
+	public $mainLanguageCode;
+}
