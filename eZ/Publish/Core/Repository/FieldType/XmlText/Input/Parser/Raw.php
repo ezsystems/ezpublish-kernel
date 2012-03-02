a4cd566ad3c96642682f910b428973fe94ecb84d
Patrick Allaert <pa@ez.no>
2012-03-02 13:47:05 +0100

    Fixed: incorrect doc

--- eZ/Publish/Core/Repository/FieldType/XmlText/Input/Parser/Raw.php
+++ eZ/Publish/Core/Repository/FieldType/XmlText/Input/Parser/Raw.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * File containing the eZ\Publish\Core\Repository\FieldType\XmlText\Input\Parser\Simplified class.
+ * File containing the eZ\Publish\Core\Repository\FieldType\XmlText\Input\Parser\Raw class.
  *
  * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
