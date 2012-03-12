5749df0858e702e55c3881d0544eb2aba2a64de7
André R <ar@ez.no>
2012-03-12 16:49:30 +0100

    Fixed failing mediaTests path to mp4 file

--- eZ/Publish/Core/Persistence/Legacy/Tests/Content/FieldValue/Converter/MediaTest.php
+++ eZ/Publish/Core/Persistence/Legacy/Tests/Content/FieldValue/Converter/MediaTest.php
@@ -47,7 +47,7 @@ class MediaTest extends \PHPUnit_Framework_TestCase
     {
         parent::setUp();
         $this->converter = new MediaTypeConverter;
-        $this->mediaPath = 'ezp/Content/Tests/FieldType/developer-got-hurt.m4v';
+        $this->mediaPath = 'eZ/Publish/Core/Repository/Tests/FieldType/developer-got-hurt.m4v';
 
         $repository = new Repository( new InMemoryPersistenceHandler(), new InMemoryIOHandler() );
         $mediaValue = new MediaTypeValue( $repository->getIOService(), $this->mediaPath );
