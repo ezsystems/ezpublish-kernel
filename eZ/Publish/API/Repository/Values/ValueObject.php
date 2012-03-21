cda603d0cbe52ba3d1705a17b27a9c24afcdf254
André R <ar@ez.no>
2012-03-21 09:22:57 +0100

    Added __set_state() n ValueObject
    As All values objects are aimed at being "dumb", and contains no circular reference.
    Use of this function should be feasable unless implimitation injects any heavy objects like for instance Repository in value objects, which is currently the case in Core implementation.

--- eZ/Publish/API/Repository/Values/ValueObject.php
+++ eZ/Publish/API/Repository/Values/ValueObject.php
@@ -103,4 +103,22 @@ abstract class ValueObject
     {
         $this->__set( $property, null );
     }
+
+    /**
+     * Returns a new instance of this class with the data specified by $array.
+     *
+     * $array contains all the data members of this class in the form:
+     * array('member_name'=>value).
+     *
+     * __set_state makes this class exportable with var_export.
+     * var_export() generates code, that calls this method when it
+     * is parsed with PHP.
+     *
+     * @param mixed[] $array
+     * @return ValueObject
+     */
+    static public function __set_state( array $array )
+    {
+        return new static( $array );
+    }
 }
