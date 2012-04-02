24fa1d67f1617ff7df0dbca29dde21d895c4c8f7
Kore Nordmann <mail@kore-nordmann.de>
2012-04-02 13:01:26 +0200

    Added: Exceptions

---
+++ eZ/Publish/API/REST/Client/Exceptions/UnauthorizedException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * File containing the UnauthorizedException class
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\API\REST\Client\Exceptions;
+
+
+/**
+ * Implementation of the {@link \eZ\Publish\API\Repository\Exceptions\UnauthorizedException}
+ * interface.
+ *
+ * @see \eZ\Publish\API\Repository\Exceptions\UnauthorizedException
+ */
+class UnauthorizedException extends \eZ\Publish\API\Repository\UnauthorizedException
+{
+}
