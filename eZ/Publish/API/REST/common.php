02a59f28aa9fb7331f06e75bfd63e319a8769259
Kore Nordmann <mail@kore-nordmann.de>
2012-04-11 15:42:54 +0200

    Added: Inline docs on client configuration

--- eZ/Publish/API/REST/common.php
+++ eZ/Publish/API/REST/common.php
@@ -9,19 +9,38 @@
 
 namespace eZ\Publish\API\REST;
 
+/**
+ * This file includes the configuration of the REST SDK client.
+ *
+ * This is a client configuration for testing purposes only.
+ */
+
 // Set communication encoding depending on environment defined in the
-// phpuni.xml files.
+// phpuni.xml files. This defines what encoding will be generated and thus send
+// to the server.
 $generator = getenv( 'backendEncoding' ) === 'xml' ?
     new Common\Output\Generator\Xml() :
     new Common\Output\Generator\Json();
 
 $repository = new Client\Repository(
+    // The HTTP Client. Needs to implement the Client\HttpClient interface.
+    //
+    // We are using a test session client here, so that we maintain a
+    // consistent session during each test case.
     new Client\HttpClient\TestSession(
         new Client\HttpClient\Stream(
+            // Server address to communicate with. You might want to make this
+            // configurable using environment variables, or something alike.
             'http://localhost:8042/'
         )
     ),
     new Common\Input\Dispatcher(
+        // The parsing dispatcher configures which parsers are used for which
+        // mime type. The mime types (content types) are provided *WITHOUT* an
+        // encoding type (+json / +xml).
+        //
+        // For each mime type you specify an instance of the parser which
+        // should be used to process the given mime type.
         new Common\Input\ParsingDispatcher(
             array(
                 'application/vnd.ez.api.ContentList'              => new Client\Input\Parser\ContentList(),
@@ -35,12 +54,27 @@ $repository = new Client\Repository(
             )
         ),
         array(
+            // Defines the available data format encoding handlers. used to
+            // process the input data and convert it into an array structure
+            // usable by the parsers.
+            //
+            // More generators should not be necessary to configure, unless new transport
+            // encoding formats need to be supported.
             'json' => new Common\Input\Handler\Json(),
             'xml'  => new Common\Input\Handler\Xml(),
         )
     ),
     new Common\Output\Visitor(
+        // The generator defines what transport encoding format will be used.
+        // This should either be the XML or JSON generator. In this case we use
+        // a generator depending on an environment variable, as defined above.
         $generator,
+        // The defined output visitors for the available value objects.
+        //
+        // If there is new data available, which should be visited and send to
+        // the server extend this array. It always maps the class name of the
+        // value object (or its parent class(es)) to the respective visitor
+        // implementation instance.
         array(
             '\\eZ\\Publish\\API\\Repository\\Values\\Content\\SectionCreateStruct'                  => new Client\Output\ValueObjectVisitor\SectionCreateStruct(),
             '\\eZ\\Publish\\API\\Repository\\Values\\Content\\SectionUpdateStruct'                  => new Client\Output\ValueObjectVisitor\SectionUpdateStruct(),
