6e2582ffcecca9cbd213698616b490eac176fbd2
Kore Nordmann <mail@kore-nordmann.de>
2012-04-13 13:57:21 +0200

    Added: Url handler with URLs extracted from specification
    All those URLs have been extracted from the API docs. They just need
    names to be usable now.

---
+++ eZ/Publish/API/REST/Common/UrlHandler/eZPublish.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * File containing the eZPublish UrlHandler class
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\API\REST\Common\UrlHandler;
+
+use eZ\Publish\API\REST\Common\UrlHandler;
+use eZ\Publish\API\REST\Common\Exceptions;
+
+/**
+ * Pattern based URL Handler preconfigured for eZPusblish
+ */
+class eZPublish extends Pattern
+{
+    /**
+     * Map of URL types to their URL patterns
+     *
+     * @var array
+     * @TODO: Add sensible missing names
+     */
+    protected $map = array(
+        ''         => '/',
+        ''         => '/content/locations',
+        'location' => '/content/locations/{location}',
+        ''         => '/content/locations/{location}/children',
+        ''         => '/content/objects',
+        'object'   => '/content/objects/{object}',
+        ''         => '/content/objects/{object}/currentversion',
+        ''         => '/content/objects/{object}/{lang_code}',
+        ''         => '/content/objects/{object}/locations',
+        ''         => '/content/objects/{object}/versions',
+        ''         => '/content/objects/{object}/versions/{version}',
+        ''         => '/content/objects/{object}/versions/{version}/relations',
+        ''         => '/content/objects/{object}/versions/{version}/relations/{relation}',
+        ''         => '/content/sections',
+        'section'  => '/content/sections/{section}',
+        ''         => '/content/trash',
+        ''         => '/content/trash/{trash}',
+        ''         => '/content/typegroups',
+        ''         => '/content/typegroups/{typegroup}',
+        ''         => '/content/typegroups/{typegroup}/types',
+        ''         => '/content/types',
+        ''         => '/content/types/{type}',
+        ''         => '/content/types/{type}/draft',
+        ''         => '/content/types/{type}/draft/fieldDefinitions',
+        ''         => '/content/types/{type}/draft/fieldDefinitions/{fieldDefinition}',
+        ''         => '/content/types/{type}/groups',
+        ''         => '/content/types/{type}/groups/{group}',
+        ''         => '/content/views',
+        ''         => '/content/views/{view}',
+        ''         => '/content/views/{view}/results',
+        ''         => '/user/groups',
+        ''         => '/user/groups/{group}',
+        ''         => '/user/groups/{group}/roles',
+        ''         => '/user/groups/{group}/roles/{role}',
+        ''         => '/user/groups/{group}/subgroups',
+        ''         => '/user/groups/{group}/users',
+        ''         => '/user/groups/root',
+        ''         => '/user/roles',
+        ''         => '/user/roles/{role}',
+        ''         => '/user/roles/{role}/policies',
+        ''         => '/user/roles/{role}/policies/{policy}',
+        ''         => '/user/users',
+        ''         => '/user/users/{user}',
+        ''         => '/user/users/{user}/drafts',
+        ''         => '/user/users/{user}/groups',
+        ''         => '/user/users/{user}/roles',
+        ''         => '/user/users/{user}/roles/{role}',
+    );
+}
+
