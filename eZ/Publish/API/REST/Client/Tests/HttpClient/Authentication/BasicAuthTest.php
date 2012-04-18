5c99d70ecb6f0dc23c8c77751f1e629e19659ed4
Tobias Schlitt <toby@php.net>
2012-04-18 12:25:47 +0200

    Implemented: Basic Auth HTTP Client.

---
+++ eZ/Publish/API/REST/Client/Tests/HttpClient/Authentication/BasicAuthTest.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * File containing a test class.
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\API\REST\Client\Tests\HttpClient\Authentication;
+use \eZ\Publish\API\REST\Client\HttpClient\Authentication\BasicAuth;
+use \eZ\Publish\API\REST\Common\Message;
+
+/**
+ * Test case for Basic Auht HTTP Client.
+ */
+class BasicAuthTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Mock for the inner HTTP client
+     *
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $innerHttpClientMock;
+
+    /**
+     * @return void
+     */
+    public function testAuthWithoutMessage()
+    {
+        $innerClientMock = $this->getInnerHttpClientMock();
+
+        $client = new BasicAuth( $innerClientMock, 'sindelfingen', 's3cr3t' );
+
+        $innerClientMock->expects( $this->once() )
+            ->method( 'request' )
+            ->with(
+                'GET',
+                '/some/path',
+                new Message(
+                    array( 'Authorization' => 'Basic c2luZGVsZmluZ2VuOnMzY3IzdA==' )
+                )
+            )->will( $this->returnValue( new \stdClass() ) );
+
+        $result = $client->request( 'GET', '/some/path' );
+
+        $this->assertInstanceOf(
+            '\\stdClass',
+            $result
+        );
+    }
+
+    /**
+     * @return void
+     */
+    public function testAuthWithMessage()
+    {
+        $innerClientMock = $this->getInnerHttpClientMock();
+
+        $client = new BasicAuth( $innerClientMock, 'sindelfingen', 's3cr3t' );
+
+        $innerClientMock->expects( $this->once() )
+            ->method( 'request' )
+            ->with(
+                'PUT',
+                '/some/path',
+                new Message(
+                    array(
+                        'X-Some-Header' => 'foobar',
+                        'Authorization' => 'Basic c2luZGVsZmluZ2VuOnMzY3IzdA=='
+                    ),
+                    'body content'
+                )
+            )->will( $this->returnValue( new \stdClass() ) );
+
+        $result = $client->request(
+            'PUT',
+            '/some/path',
+            new Message(
+                array(
+                    'X-Some-Header' => 'foobar',
+                ),
+                'body content'
+            )
+        );
+
+        $this->assertInstanceOf(
+            '\\stdClass',
+            $result
+        );
+    }
+
+    /**
+     * @return \eZ\Publish\API\REST\Client\HttpClient
+     */
+    protected function getInnerHttpClientMock()
+    {
+        if ( !isset( $this->innerHttpClientMock ) )
+        {
+            $this->innerHttpClientMock = $this->getMock(
+                '\\eZ\\Publish\\API\\REST\\Client\\HttpClient',
+                array(),
+                array(),
+                '',
+                false
+            );
+        }
+        return $this->innerHttpClientMock;
+    }
+}
