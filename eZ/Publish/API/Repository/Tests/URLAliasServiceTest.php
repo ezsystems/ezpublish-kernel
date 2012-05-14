1b45f592112d61450dfcd39f2e5e44064a9b077c
Tobias Schlitt <toby@php.net>
2012-05-14 14:51:34 +0200

    Added: Generated tests for URLAliasService.

---
+++ eZ/Publish/API/Repository/Tests/URLAliasServiceTest.php
@@ -0,0 +1,355 @@
+<?php
+/**
+ * File containing the URLAliasServiceTest class
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\API\Repository\Tests;
+
+/**
+ * Test case for operations in the URLAliasService using in memory storage.
+ *
+ * @see eZ\Publish\API\Repository\URLAliasService
+ */
+class URLAliasServiceTest extends \eZ\Publish\API\Repository\Tests\BaseTest
+{
+    /**
+     * Test for the createUrlAlias() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\URLAliasService::createUrlAlias()
+     *
+     */
+    public function testCreateUrlAlias()
+    {
+        $this->markTestIncomplete( "Test for URLAliasService::createUrlAlias() is not implemented." );
+    }
+
+    /**
+     * Test for the createUrlAlias() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\URLAliasService::createUrlAlias($location, $path, $languageCode, $forwarding)
+     *
+     */
+    public function testCreateUrlAliasWithFourthParameter()
+    {
+        $this->markTestIncomplete( "Test for URLAliasService::createUrlAlias() is not implemented." );
+    }
+
+    /**
+     * Test for the createUrlAlias() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\URLAliasService::createUrlAlias($location, $path, $languageCode, $forwarding, $alwaysAvailable)
+     *
+     */
+    public function testCreateUrlAliasWithFifthParameter()
+    {
+        $this->markTestIncomplete( "Test for URLAliasService::createUrlAlias() is not implemented." );
+    }
+
+    /**
+     * Test for the createUrlAlias() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\URLAliasService::createUrlAlias()
+     * @expectedException \eZ\Publish\API\Repository\Exceptions\ForbiddenException
+     */
+    public function testCreateUrlAliasThrowsForbiddenException()
+    {
+        $this->markTestIncomplete( "Test for URLAliasService::createUrlAlias() is not implemented." );
+    }
+
+    /**
+     * Test for the createUrlAlias() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\URLAliasService::createUrlAlias($location, $path, $languageCode, $forwarding)
+     * @expectedException \eZ\Publish\API\Repository\Exceptions\ForbiddenException
+     */
+    public function testCreateUrlAliasThrowsForbiddenExceptionWithFourthParameter()
+    {
+        $this->markTestIncomplete( "Test for URLAliasService::createUrlAlias() is not implemented." );
+    }
+
+    /**
+     * Test for the createUrlAlias() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\URLAliasService::createUrlAlias($location, $path, $languageCode, $forwarding, $alwaysAvailable)
+     * @expectedException \eZ\Publish\API\Repository\Exceptions\ForbiddenException
+     */
+    public function testCreateUrlAliasThrowsForbiddenExceptionWithFifthParameter()
+    {
+        $this->markTestIncomplete( "Test for URLAliasService::createUrlAlias() is not implemented." );
+    }
+
+    /**
+     * Test for the createGlobalUrlAlias() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\URLAliasService::createGlobalUrlAlias()
+     *
+     */
+    public function testCreateGlobalUrlAlias()
+    {
+        $this->markTestIncomplete( "Test for URLAliasService::createGlobalUrlAlias() is not implemented." );
+    }
+
+    /**
+     * Test for the createGlobalUrlAlias() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\URLAliasService::createGlobalUrlAlias($resource, $path, $languageCode, $forward)
+     *
+     */
+    public function testCreateGlobalUrlAliasWithFourthParameter()
+    {
+        $this->markTestIncomplete( "Test for URLAliasService::createGlobalUrlAlias() is not implemented." );
+    }
+
+    /**
+     * Test for the createGlobalUrlAlias() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\URLAliasService::createGlobalUrlAlias($resource, $path, $languageCode, $forward, $alwaysAvailable)
+     *
+     */
+    public function testCreateGlobalUrlAliasWithFifthParameter()
+    {
+        $this->markTestIncomplete( "Test for URLAliasService::createGlobalUrlAlias() is not implemented." );
+    }
+
+    /**
+     * Test for the createGlobalUrlAlias() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\URLAliasService::createGlobalUrlAlias()
+     * @expectedException \eZ\Publish\API\Repository\Exceptions\ForbiddenException
+     */
+    public function testCreateGlobalUrlAliasThrowsForbiddenException()
+    {
+        $this->markTestIncomplete( "Test for URLAliasService::createGlobalUrlAlias() is not implemented." );
+    }
+
+    /**
+     * Test for the createGlobalUrlAlias() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\URLAliasService::createGlobalUrlAlias($resource, $path, $languageCode, $forward)
+     * @expectedException \eZ\Publish\API\Repository\Exceptions\ForbiddenException
+     */
+    public function testCreateGlobalUrlAliasThrowsForbiddenExceptionWithFourthParameter()
+    {
+        $this->markTestIncomplete( "Test for URLAliasService::createGlobalUrlAlias() is not implemented." );
+    }
+
+    /**
+     * Test for the createGlobalUrlAlias() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\URLAliasService::createGlobalUrlAlias($resource, $path, $languageCode, $forward, $alwaysAvailable)
+     * @expectedException \eZ\Publish\API\Repository\Exceptions\ForbiddenException
+     */
+    public function testCreateGlobalUrlAliasThrowsForbiddenExceptionWithFifthParameter()
+    {
+        $this->markTestIncomplete( "Test for URLAliasService::createGlobalUrlAlias() is not implemented." );
+    }
+
+    /**
+     * Test for the listLocationAliases() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\URLAliasService::listLocationAliases()
+     *
+     */
+    public function testListLocationAliases()
+    {
+        $this->markTestIncomplete( "Test for URLAliasService::listLocationAliases() is not implemented." );
+    }
+
+    /**
+     * Test for the listLocationAliases() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\URLAliasService::listLocationAliases($location, $custom)
+     *
+     */
+    public function testListLocationAliasesWithSecondParameter()
+    {
+        $this->markTestIncomplete( "Test for URLAliasService::listLocationAliases() is not implemented." );
+    }
+
+    /**
+     * Test for the listLocationAliases() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\URLAliasService::listLocationAliases($location, $custom, $languageCode)
+     *
+     */
+    public function testListLocationAliasesWithThirdParameter()
+    {
+        $this->markTestIncomplete( "Test for URLAliasService::listLocationAliases() is not implemented." );
+    }
+
+    /**
+     * Test for the listGlobalAliases() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\URLAliasService::listGlobalAliases()
+     *
+     */
+    public function testListGlobalAliases()
+    {
+        $this->markTestIncomplete( "Test for URLAliasService::listGlobalAliases() is not implemented." );
+    }
+
+    /**
+     * Test for the listGlobalAliases() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\URLAliasService::listGlobalAliases($languageCode)
+     *
+     */
+    public function testListGlobalAliasesWithFirstParameter()
+    {
+        $this->markTestIncomplete( "Test for URLAliasService::listGlobalAliases() is not implemented." );
+    }
+
+    /**
+     * Test for the listGlobalAliases() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\URLAliasService::listGlobalAliases($languageCode, $offset)
+     *
+     */
+    public function testListGlobalAliasesWithSecondParameter()
+    {
+        $this->markTestIncomplete( "Test for URLAliasService::listGlobalAliases() is not implemented." );
+    }
+
+    /**
+     * Test for the listGlobalAliases() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\URLAliasService::listGlobalAliases($languageCode, $offset, $limit)
+     *
+     */
+    public function testListGlobalAliasesWithThirdParameter()
+    {
+        $this->markTestIncomplete( "Test for URLAliasService::listGlobalAliases() is not implemented." );
+    }
+
+    /**
+     * Test for the removeAliases() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\URLAliasService::removeAliases()
+     *
+     */
+    public function testRemoveAliases()
+    {
+        $this->markTestIncomplete( "Test for URLAliasService::removeAliases() is not implemented." );
+    }
+
+    /**
+     * Test for the lookUp() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\URLAliasService::lookUp()
+     *
+     */
+    public function testLookUp()
+    {
+        $this->markTestIncomplete( "Test for URLAliasService::lookUp() is not implemented." );
+    }
+
+    /**
+     * Test for the lookUp() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\URLAliasService::lookUp($url, $languageCode)
+     *
+     */
+    public function testLookUpWithSecondParameter()
+    {
+        $this->markTestIncomplete( "Test for URLAliasService::lookUp() is not implemented." );
+    }
+
+    /**
+     * Test for the lookUp() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\URLAliasService::lookUp()
+     * @expectedException \eZ\Publish\API\Repository\Exceptions\NotFoundException
+     */
+    public function testLookUpThrowsNotFoundException()
+    {
+        $this->markTestIncomplete( "Test for URLAliasService::lookUp() is not implemented." );
+    }
+
+    /**
+     * Test for the lookUp() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\URLAliasService::lookUp($url, $languageCode)
+     * @expectedException \eZ\Publish\API\Repository\Exceptions\NotFoundException
+     */
+    public function testLookUpThrowsNotFoundExceptionWithSecondParameter()
+    {
+        $this->markTestIncomplete( "Test for URLAliasService::lookUp() is not implemented." );
+    }
+
+    /**
+     * Test for the reverseLookup() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\URLAliasService::reverseLookup()
+     *
+     */
+    public function testReverseLookup()
+    {
+        $this->markTestIncomplete( "Test for URLAliasService::reverseLookup() is not implemented." );
+    }
+
+    /**
+     * Test for the reverseLookup() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\URLAliasService::reverseLookup($location, $languageCode)
+     *
+     */
+    public function testReverseLookupWithSecondParameter()
+    {
+        $this->markTestIncomplete( "Test for URLAliasService::reverseLookup() is not implemented." );
+    }
+
+    /**
+     * Test for the reverseLookup() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\URLAliasService::reverseLookup()
+     * @expectedException \eZ\Publish\API\Repository\Exceptions\NotFoundException
+     */
+    public function testReverseLookupThrowsNotFoundException()
+    {
+        $this->markTestIncomplete( "Test for URLAliasService::reverseLookup() is not implemented." );
+    }
+
+    /**
+     * Test for the reverseLookup() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\URLAliasService::reverseLookup($location, $languageCode)
+     * @expectedException \eZ\Publish\API\Repository\Exceptions\NotFoundException
+     */
+    public function testReverseLookupThrowsNotFoundExceptionWithSecondParameter()
+    {
+        $this->markTestIncomplete( "Test for URLAliasService::reverseLookup() is not implemented." );
+    }
+
+}
