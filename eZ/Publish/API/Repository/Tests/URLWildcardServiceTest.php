17794312b57a5c75a9bc6505ae238744f05f8e5e
Manuel Pichler <mapi@manuel-pichler.de>
2012-05-23 13:33:37 +0200

    Added: Empty test class for the URLWildcardService

---
+++ eZ/Publish/API/Repository/Tests/URLWildcardServiceTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * File containing the URLWildcardServiceTest class
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\API\Repository\Tests;
+
+use eZ\Publish\API\Repository\Values\Content\URLAlias;
+
+/**
+ * Test case for operations in the URLWildcardService.
+ *
+ * @see eZ\Publish\API\Repository\URLWildcardService
+ */
+class URLWildcardServiceTest extends \eZ\Publish\API\Repository\Tests\BaseTest
+{
+
+}
