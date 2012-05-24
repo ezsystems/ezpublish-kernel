04f99fdc0fc3aab1c2b6bda43e50e5ae0a7e8268
Kore Nordmann <mail@kore-nordmann.de>
2012-05-24 12:47:58 +0200

    Completed list of status codes

--- eZ/Publish/API/REST/Common/Output/Visitor.php
+++ eZ/Publish/API/REST/Common/Output/Visitor.php
@@ -47,13 +47,63 @@ class Visitor
      * Mapping of status codes.
      *
      * @var array(int=>string)
-     * @todo Complete as needed.
      */
     public static $statusMap = array(
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        102 => 'Processing',
+        118 => 'Connection timed out',
         200 => 'OK',
         201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
         204 => 'No Content',
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+        207 => 'Multi-Status',
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',
+        302 => 'Found',
+        303 => 'See Other',
+        304 => 'Not Modified',
+        305 => 'Use Proxy',
+        306 => '(reserviert)',
         307 => 'Temporary Redirect',
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Time-out',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Request Entity Too Large',
+        414 => 'Request-URL Too Long',
+        415 => 'Unsupported Media Type',
+        416 => 'Requested range not satisfiable',
+        417 => 'Expectation Failed',
+        418 => 'I\'m a teapot',
+        421 => 'There are too many connections from your internet address',
+        422 => 'Unprocessable Entity',
+        423 => 'Locked',
+        424 => 'Failed Dependency',
+        425 => 'Unordered Collection',
+        426 => 'Upgrade Required',
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Time-out',
+        505 => 'HTTP Version not supported',
+        506 => 'Variant Also Negotiates',
+        507 => 'Insufficient Storage',
+        509 => 'Bandwidth Limit Exceeded',
+        510 => 'Not Extended',
     );
 
     /**
@@ -117,16 +167,15 @@ class Visitor
      */
     public function setStatus( $statusCode )
     {
-        $this->setHeader(
-            'Status',
-            sprintf(
-                '%s %s',
-                $statusCode,
-                ( isset( self::$statusMap[$statusCode] )
-                    ? self::$statusMap[$statusCode]
-                    : 'Unknown' )
-            )
+        $status = sprintf(
+            '%s %s',
+            $statusCode,
+            ( isset( self::$statusMap[$statusCode] )
+                ? self::$statusMap[$statusCode]
+                : 'Unknown' )
         );
+
+        $this->setHeader( 'Status', $status );
     }
 
     /**
