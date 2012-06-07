53103a5f9753cd9aedf4e546a48208ff6cbff827
André R <ar@ez.no>
2012-06-07 14:41:57 +0200

    Add missing methods on Repository (with not implemented exception)

--- eZ/Publish/Core/Repository/Repository.php
+++ eZ/Publish/Core/Repository/Repository.php
@@ -380,6 +380,36 @@ class Repository implements RepositoryInterface
     }
 
     /**
+     * Get URLAliasService
+     *
+     * @return \eZ\Publish\API\Repository\URLAliasService
+     */
+    public function getURLAliasService()
+    {
+        throw new \Exception("@todo URLAliasService Not Implemented");
+    }
+
+    /**
+     * Get URLWildcardService
+     *
+     * @return \eZ\Publish\API\Repository\URLWildcardService
+     */
+    public function getURLWildcardService()
+    {
+        throw new \Exception("@todo URLWildcardService Not Implemented");
+    }
+
+    /**
+     * Get ObjectStateService
+     *
+     * @return \eZ\Publish\API\Repository\ObjectStateService
+     */
+    public function getObjectStateService()
+    {
+        throw new \Exception("@todo ObjectStateService Not Implemented");
+    }
+
+    /**
      * Get IO Service
      *
      * Get service object to perform operations on binary files
