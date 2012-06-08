2eb90391e9a9f86accf88ecbbf6fd4682325308d
Tobias Schlitt <toby@php.net>
2012-06-08 17:15:32 +0200

    Added: ObjectStateServiceStub.

---
+++ eZ/Publish/API/Repository/Tests/Stubs/ObjectStateServiceStub.php
@@ -0,0 +1,267 @@
+<?php
+/**
+ * File containing the eZ\Publish\API\Repository\Tests\Stubs\ObjectStateServiceStub class.
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ * @package eZ\Publish\API\Repository
+ */
+
+
+namespace eZ\Publish\API\Repository\Tests\Stubs;
+
+use eZ\Publish\API\Repository\ObjectStateService;
+
+use eZ\Publish\API\Repository\Values\ObjectState\ObjectStateUpdateStruct;
+use eZ\Publish\API\Repository\Values\ObjectState\ObjectStateCreateStruct;
+use eZ\Publish\API\Repository\Values\ObjectState\ObjectStateGroupUpdateStruct;
+use eZ\Publish\API\Repository\Values\ObjectState\ObjectStateGroup;
+use eZ\Publish\API\Repository\Values\Content\ContentInfo;
+use eZ\Publish\API\Repository\Values\ObjectState\ObjectState;
+use eZ\Publish\API\Repository\Values\ObjectState\ObjectStateGroupCreateStruct;
+
+/**
+ * ObjectStateServiceStub
+ */
+class ObjectStateServiceStub implements ObjectStateService
+{
+     /**
+     * creates a new object state group
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException if the user is not allowed to create an object state group
+     *
+     * @param \ez\Publish\API\Repository\Values\ObjectState\ObjectStateGroupCreateStruct $objectStateGroupCreateStruct
+     *
+     * @return \ez\Publish\API\Repository\Values\ObjectState\ObjectStateGroup
+     */
+    public function createObjectStateGroup( ObjectStateGroupCreateStruct $objectStateGroupCreateStruct )
+    {
+        throw new \RuntimeException( "Not implemented, yet." );
+    }
+
+    /**
+     * Loads a object state group
+     *
+     * @param mixed $objectStateGroupId
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException if the group was not found
+     *
+     * @return \ez\Publish\API\Repository\Values\ObjectState\ObjectStateGroup
+     */
+    public function loadObjectStateGroup( $objectStateGroupId )
+    {
+        throw new \RuntimeException( "Not implemented, yet." );
+    }
+
+
+    /**
+     * Loads all object state groups
+     *
+     * @param int $offset
+     * @param int $limit
+     *
+     * @return \ez\Publish\API\Repository\Values\ObjectState\ObjectStateGroup[]
+     */
+    public function loadObjectStateGroups( $offset = 0, $limit = -1 )
+    {
+        throw new \RuntimeException( "Not implemented, yet." );
+    }
+
+    /**
+     * This method returns the ordered list of object states of a group
+     *
+     * @param \ez\Publish\API\Repository\Values\ObjectState\ObjectStateGroup $objectStateGroup
+     *
+     * @return \ez\Publish\API\Repository\Values\ObjectState\ObjectState[]
+     */
+    public function loadObjectStates( ObjectStateGroup $objectStateGroup )
+    {
+        throw new \RuntimeException( "Not implemented, yet." );
+    }
+
+    /**
+     * updates an object state group
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException if the user is not allowed to update an object state group
+     *
+     * @param \ez\Publish\API\Repository\Values\ObjectState\ObjectStateGroup $objectStateGroup
+     * @param \ez\Publish\API\Repository\Values\ObjectState\ObjectStateGroupUpdateStruct $objectStateGroupUpdateStruct
+     *
+     * @return \ez\Publish\API\Repository\Values\ObjectState\ObjectStateGroup
+     */
+    public function updateObjectStateGroup( ObjectStateGroup $objectStateGroup, ObjectStateGroupUpdateStruct $objectStateGroupUpdateStruct )
+    {
+        throw new \RuntimeException( "Not implemented, yet." );
+    }
+
+    /**
+     * Deletes a object state group including all states and links to content
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException if the user is not allowed to delete an object state group
+     *
+     * @param \ez\Publish\API\Repository\Values\ObjectState\ObjectStateGroup $objectStateGroup
+     */
+    public function deleteObjectStateGroup( ObjectStateGroup $objectStateGroup )
+    {
+        throw new \RuntimeException( "Not implemented, yet." );
+    }
+
+    /**
+     * creates a new object state in the given group.
+     *
+     * Note: in current kernel: If it is the first state all content objects will
+     * set to this state.
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException if the user is not allowed to create an object state
+     *
+     * @param \ez\Publish\API\Repository\Values\ObjectState\ObjectStateGroup $objectStateGroup
+     * @param \ez\Publish\API\Repository\Values\ObjectState\ObjectStateCreateStruct $objectStateCreateStruct
+     *
+     * @return \ez\Publish\API\Repository\Values\ObjectState\ObjectState
+     */
+    public function createObjectState( ObjectStateGroup $objectStateGroup, ObjectStateCreateStruct $objectStateCreateStruct )
+    {
+        throw new \RuntimeException( "Not implemented, yet." );
+    }
+
+    /**
+     * Loads an object state
+     *
+     * @param $stateId
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException if the state was not found
+     *
+     * @return \ez\Publish\API\Repository\Values\ObjectState\ObjectState
+     */
+    public function loadObjectState( $stateId )
+    {
+        throw new \RuntimeException( "Not implemented, yet." );
+    }
+
+    /**
+     * updates an object state
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException if the user is not allowed to update an object state
+     *
+     * @param \ez\Publish\API\Repository\Values\ObjectState\ObjectStateUpdateStruct $objectStateUpdateStruct
+     *
+     * @return \ez\Publish\API\Repository\Values\ObjectState\ObjectState
+     */
+    public function updateObjectState( ObjectState $objectState, ObjectStateUpdateStruct $objectStateUpdateStruct )
+    {
+        throw new \RuntimeException( "Not implemented, yet." );
+    }
+
+    /**
+     * changes the priority of the state
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException if the user is not allowed to change priority on an object state
+     *
+     * @param \ez\Publish\API\Repository\Values\ObjectState\ObjectState $objectState
+     * @param int $priority
+     */
+    public function setPriorityOfObjectState( ObjectState $objectState, $priority )
+    {
+        throw new \RuntimeException( "Not implemented, yet." );
+    }
+
+    /**
+     * Deletes a object state. The state of the content objects is reset to the
+     * first object state in the group.
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException if the user is not allowed to delete an object state
+     *
+     * @param \ez\Publish\API\Repository\Values\ObjectState\ObjectState $objectState
+     */
+    public function deleteObjectState( ObjectState $objectState )
+    {
+        throw new \RuntimeException( "Not implemented, yet." );
+    }
+
+
+    /**
+     * Sets the object-state of a state group to $state for the given content.
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentExceptioon if the object state does not belong to the given group
+     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException if the user is not allowed to change the object state
+     *
+     * @param \eZ\Publish\API\Repository\Values\Content\ContentInfo $contentInfo
+     * @param \ez\Publish\API\Repository\Values\ObjectState\ObjectStateGroup $objectStateGroup
+     * @param \ez\Publish\API\Repository\Values\ObjectState\ObjectState $objectState
+     *
+     */
+    public function setObjectState( ContentInfo $contentInfo, ObjectStateGroup $objectStateGroup, ObjectState $objectState )
+    {
+        throw new \RuntimeException( "Not implemented, yet." );
+    }
+
+    /**
+     * Gets the object-state of object identified by $contentId.
+     *
+     * The $state is the id of the state within one group.
+     *
+     * @param \eZ\Publish\API\Repository\Values\Content\ContentInfo $contentInfo
+     * @param \ez\Publish\API\Repository\Values\ObjectState\ObjectStateGroup $objectStateGroup
+     *
+     * @return \ez\Publish\API\Repository\Values\ObjectState\ObjectState
+     */
+    public function getObjectState( ContentInfo $contentInfo, ObjectStateGroup $objectStateGroup )
+    {
+        throw new \RuntimeException( "Not implemented, yet." );
+    }
+
+    /**
+     * returns the number of objects which are in this state
+     *
+     * @param \ez\Publish\API\Repository\Values\ObjectState\ObjectState $objectState
+     *
+     * @return int
+     */
+    public function getContentCount( ObjectState $objectState )
+    {
+        throw new \RuntimeException( "Not implemented, yet." );
+    }
+
+    /**
+     * Instantiates a new Object State Group Create Struct and sets $identified in it.
+     *
+     * @param string $identifier
+     * @return \eZ\Publish\API\Repository\Values\ObjectState\ObjectStateGroupCreateStruct
+     */
+    public function newObjectStateGroupCreateStruct( $identifier )
+    {
+        throw new \RuntimeException( "Not implemented, yet." );
+    }
+
+    /**
+     * Instantiates a new Object State Group Update Struct.
+     *
+     * @return \eZ\Publish\API\Repository\Values\ObjectState\ObjectStateGroupUpdateStruct
+     */
+    public function newObjectStateGroupUpdateStruct()
+    {
+        throw new \RuntimeException( "Not implemented, yet." );
+    }
+
+    /**
+     * Instantiates a new Object State Create Struct and sets $identifier in it.
+     *
+     * @param string $identifier
+     * @return \eZ\Publish\API\Repository\Values\ObjectState\ObjectStateCreateStruct
+     */
+    public function newObjectStateCreateStruct( $identifier )
+    {
+        throw new \RuntimeException( "Not implemented, yet." );
+    }
+
+    /**
+     * Instantiates a new Object State Update Struct and sets $identifier in it.
+     *
+     * @return \eZ\Publish\API\Repository\Values\ObjectState\ObjectStateUpdateStruct
+     */
+    public function newObjectStateUpdateStruct()
+    {
+        throw new \RuntimeException( "Not implemented, yet." );
+    }
+}
