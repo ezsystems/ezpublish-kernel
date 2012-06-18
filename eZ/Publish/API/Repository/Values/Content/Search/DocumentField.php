0efc1fcc54da95894d7c93ac97ad5ac1b6c249bd
Kore Nordmann <mail@kore-nordmann.de>
2012-06-18 17:59:47 +0200

    Fixed docs

--- eZ/Publish/API/Repository/Values/Content/Search/DocumentField.php
+++ eZ/Publish/API/Repository/Values/Content/Search/DocumentField.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * File containing the eZ\Publish\API\Repository\Values\Content\Search\Facet class.
+ * File containing the eZ\Publish\API\Repository\Values\Content\Search\DocumentField class.
  *
  * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
@@ -11,8 +11,7 @@ namespace eZ\Publish\API\Repository\Values\Content\Search;
 use eZ\Publish\API\Repository\Values\ValueObject;
 
 /**
- * Base class for facets
- *
+ * Base class for document fields
  */
 abstract class DocumentField extends ValueObject
 {
