cc77689841e605ad02a50a4f0e3d3ab2a52ecac6
Kore Nordmann <mail@kore-nordmann.de>
2012-06-18 17:54:58 +0200

    Added: Abstract base class for document field

---
+++ eZ/Publish/API/Repository/Values/Content/Search/DocumentField.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * File containing the eZ\Publish\API\Repository\Values\Content\Search\Facet class.
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+namespace eZ\Publish\API\Repository\Values\Content\Search;
+
+use eZ\Publish\API\Repository\Values\ValueObject;
+
+/**
+ * Base class for facets
+ *
+ */
+abstract class DocumentField extends ValueObject
+{
+    /**
+     * Name of the document field. Will be used to query this field.
+     *
+     * @var string
+     */
+    public $name;
+
+    /**
+     * The type name of the facet. Has to be handled by the solr schema.
+     *
+     * @var string
+     */
+    protected $type;
+
+    /**
+     * Value of the document field.
+     *
+     * Might be about anything depending on the type of the document field.
+     *
+     * @var mixed
+     */
+    public $value;
+
+    /**
+     * Whether highlighting should be performed for this field on result documents
+     *
+     * @var bool
+     */
+    public $highlight = false;
+
+    /**
+     * The importance of that field ( boost factor) )
+     *
+     * @var int
+     */
+    public $boost = 1;
+
+    /**
+     * Whether the field supports multiple values
+     *
+     * @var bool
+     */
+    public $multiValue = false;
+
+    /**
+     * Whether the field should be a part of the resulting document
+     *
+     * @var bool
+     */
+    public $inResult = true;
+}
+
