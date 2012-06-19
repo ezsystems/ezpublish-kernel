55ce39dc97db25e20eac30b8ad2a3858f2f00e83
Kore Nordmann <mail@kore-nordmann.de>
2012-06-19 16:23:07 +0200

    Updated fixtures

--- eZ/Publish/API/Repository/Tests/_fixtures/Legacy/DateMetadataGt.php
+++ eZ/Publish/API/Repository/Tests/_fixtures/Legacy/DateMetadataGt.php
@@ -1,283 +1,37 @@
 <?php
 
-return eZ\Publish\SPI\Persistence\Content\Search\Result::__set_state(array(
-   'count' => 2,
-   'content' => 
+return eZ\Publish\API\Repository\Values\Content\Search\SearchResult::__set_state(array(
+   'facets' => 
+  array (
+  ),
+   'searchHits' => 
   array (
     0 => 
-    eZ\Publish\SPI\Persistence\Content::__set_state(array(
-       'versionInfo' => 
-      eZ\Publish\SPI\Persistence\Content\VersionInfo::__set_state(array(
-         'id' => 674,
-         'versionNo' => 2,
-         'contentId' => 11,
-         'names' => 
-        array (
-          'eng-US' => 'Members',
-        ),
-         'creationDate' => 1311154215,
-         'modificationDate' => 1311154215,
-         'creatorId' => 14,
-         'status' => 1,
-         'initialLanguageCode' => 'eng-US',
-         'languageIds' => 
-        array (
-          0 => 2,
-        ),
-      )),
-       'contentInfo' => 
-      eZ\Publish\SPI\Persistence\Content\ContentInfo::__set_state(array(
-         'id' => 11,
-         'name' => 'Members',
-         'contentTypeId' => 3,
-         'sectionId' => 2,
-         'currentVersionNo' => 2,
-         'isPublished' => true,
-         'ownerId' => 14,
-         'modificationDate' => 1311154215,
-         'publicationDate' => 1033920746,
-         'isAlwaysAvailable' => true,
-         'remoteId' => '5f7f0bdb3381d6a461d8c29ff53d908f',
-         'mainLanguageCode' => 'eng-US',
-      )),
-       'relations' => NULL,
-       'fields' => 
-      array (
-        0 => 
-        eZ\Publish\SPI\Persistence\Content\Field::__set_state(array(
-           'id' => 23,
-           'fieldDefinitionId' => 7,
-           'type' => 'ezstring',
-           'value' => 
-          eZ\Publish\SPI\Persistence\Content\FieldValue::__set_state(array(
-             'data' => '',
-             'fieldSettings' => NULL,
-             'externalData' => NULL,
-             'sortKey' => 
-            array (
-              'sort_key_string' => '',
-            ),
-          )),
-           'languageCode' => 'eng-US',
-           'versionNo' => 2,
-        )),
-        1 => 
-        eZ\Publish\SPI\Persistence\Content\Field::__set_state(array(
-           'id' => 22,
-           'fieldDefinitionId' => 6,
-           'type' => 'ezstring',
-           'value' => 
-          eZ\Publish\SPI\Persistence\Content\FieldValue::__set_state(array(
-             'data' => 'Members',
-             'fieldSettings' => NULL,
-             'externalData' => NULL,
-             'sortKey' => 
-            array (
-              'sort_key_string' => 'members',
-            ),
-          )),
-           'languageCode' => 'eng-US',
-           'versionNo' => 2,
-        )),
-      ),
-       'locations' => 
-      array (
-        0 => 
-        eZ\Publish\SPI\Persistence\Content\Location::__set_state(array(
-           'id' => '12',
-           'priority' => '0',
-           'hidden' => '0',
-           'invisible' => '0',
-           'remoteId' => '602dcf84765e56b7f999eaafd3821dd3',
-           'contentId' => '11',
-           'parentId' => '5',
-           'pathIdentificationString' => 'users/members',
-           'pathString' => '/1/5/12/',
-           'modifiedSubLocation' => '1311154216',
-           'mainLocationId' => '12',
-           'depth' => '2',
-           'sortField' => '1',
-           'sortOrder' => '1',
-        )),
-      ),
+    eZ\Publish\API\Repository\Values\Content\Search\SearchHit::__set_state(array(
+       'valueObject' => 11,
+       'score' => NULL,
+       'index' => NULL,
+       'highlight' => NULL,
     )),
     1 => 
-    eZ\Publish\SPI\Persistence\Content::__set_state(array(
-       'versionInfo' => 
-      eZ\Publish\SPI\Persistence\Content\VersionInfo::__set_state(array(
-         'id' => 439,
-         'versionNo' => 1,
-         'contentId' => 11,
-         'names' => 
-        array (
-          'eng-US' => 'Guest accounts',
-        ),
-         'creationDate' => 1033920737,
-         'modificationDate' => 1033920746,
-         'creatorId' => 14,
-         'status' => 3,
-         'initialLanguageCode' => 'eng-US',
-         'languageIds' => 
-        array (
-          0 => 2,
-        ),
-      )),
-       'contentInfo' => 
-      eZ\Publish\SPI\Persistence\Content\ContentInfo::__set_state(array(
-         'id' => 11,
-         'name' => 'Members',
-         'contentTypeId' => 3,
-         'sectionId' => 2,
-         'currentVersionNo' => 2,
-         'isPublished' => true,
-         'ownerId' => 14,
-         'modificationDate' => 1311154215,
-         'publicationDate' => 1033920746,
-         'isAlwaysAvailable' => true,
-         'remoteId' => '5f7f0bdb3381d6a461d8c29ff53d908f',
-         'mainLanguageCode' => 'eng-US',
-      )),
-       'relations' => NULL,
-       'fields' => 
-      array (
-        0 => 
-        eZ\Publish\SPI\Persistence\Content\Field::__set_state(array(
-           'id' => 22,
-           'fieldDefinitionId' => 6,
-           'type' => 'ezstring',
-           'value' => 
-          eZ\Publish\SPI\Persistence\Content\FieldValue::__set_state(array(
-             'data' => 'Guest accounts',
-             'fieldSettings' => NULL,
-             'externalData' => NULL,
-             'sortKey' => 
-            array (
-              'sort_key_string' => '',
-            ),
-          )),
-           'languageCode' => 'eng-US',
-           'versionNo' => 1,
-        )),
-        1 => 
-        eZ\Publish\SPI\Persistence\Content\Field::__set_state(array(
-           'id' => 23,
-           'fieldDefinitionId' => 7,
-           'type' => 'ezstring',
-           'value' => 
-          eZ\Publish\SPI\Persistence\Content\FieldValue::__set_state(array(
-             'data' => '',
-             'fieldSettings' => NULL,
-             'externalData' => NULL,
-             'sortKey' => 
-            array (
-              'sort_key_string' => '',
-            ),
-          )),
-           'languageCode' => 'eng-US',
-           'versionNo' => 1,
-        )),
-      ),
-       'locations' => 
-      array (
-      ),
+    eZ\Publish\API\Repository\Values\Content\Search\SearchHit::__set_state(array(
+       'valueObject' => 11,
+       'score' => NULL,
+       'index' => NULL,
+       'highlight' => NULL,
     )),
     2 => 
-    eZ\Publish\SPI\Persistence\Content::__set_state(array(
-       'versionInfo' => 
-      eZ\Publish\SPI\Persistence\Content\VersionInfo::__set_state(array(
-         'id' => 673,
-         'versionNo' => 1,
-         'contentId' => 225,
-         'names' => 
-        array (
-          'eng-US' => 'Partners',
-        ),
-         'creationDate' => 1311154215,
-         'modificationDate' => 1311154215,
-         'creatorId' => 14,
-         'status' => 1,
-         'initialLanguageCode' => 'eng-US',
-         'languageIds' => 
-        array (
-          0 => 2,
-        ),
-      )),
-       'contentInfo' => 
-      eZ\Publish\SPI\Persistence\Content\ContentInfo::__set_state(array(
-         'id' => 225,
-         'name' => 'Partners',
-         'contentTypeId' => 3,
-         'sectionId' => 1,
-         'currentVersionNo' => 1,
-         'isPublished' => true,
-         'ownerId' => 14,
-         'modificationDate' => 1311154215,
-         'publicationDate' => 1311154215,
-         'isAlwaysAvailable' => true,
-         'remoteId' => '713e1709ba03bf1fde1396d2a0451e28',
-         'mainLanguageCode' => 'eng-US',
-      )),
-       'relations' => NULL,
-       'fields' => 
-      array (
-        0 => 
-        eZ\Publish\SPI\Persistence\Content\Field::__set_state(array(
-           'id' => 1331,
-           'fieldDefinitionId' => 7,
-           'type' => 'ezstring',
-           'value' => 
-          eZ\Publish\SPI\Persistence\Content\FieldValue::__set_state(array(
-             'data' => '',
-             'fieldSettings' => NULL,
-             'externalData' => NULL,
-             'sortKey' => 
-            array (
-              'sort_key_string' => '',
-            ),
-          )),
-           'languageCode' => 'eng-US',
-           'versionNo' => 1,
-        )),
-        1 => 
-        eZ\Publish\SPI\Persistence\Content\Field::__set_state(array(
-           'id' => 1330,
-           'fieldDefinitionId' => 6,
-           'type' => 'ezstring',
-           'value' => 
-          eZ\Publish\SPI\Persistence\Content\FieldValue::__set_state(array(
-             'data' => 'Partners',
-             'fieldSettings' => NULL,
-             'externalData' => NULL,
-             'sortKey' => 
-            array (
-              'sort_key_string' => 'partners',
-            ),
-          )),
-           'languageCode' => 'eng-US',
-           'versionNo' => 1,
-        )),
-      ),
-       'locations' => 
-      array (
-        0 => 
-        eZ\Publish\SPI\Persistence\Content\Location::__set_state(array(
-           'id' => '227',
-           'priority' => '0',
-           'hidden' => '0',
-           'invisible' => '0',
-           'remoteId' => '1ee27657f5f41fe6458fcb90e81d5d82',
-           'contentId' => '225',
-           'parentId' => '5',
-           'pathIdentificationString' => 'users/partners',
-           'pathString' => '/1/5/227/',
-           'modifiedSubLocation' => '1311154215',
-           'mainLocationId' => '227',
-           'depth' => '2',
-           'sortField' => '1',
-           'sortOrder' => '1',
-        )),
-      ),
+    eZ\Publish\API\Repository\Values\Content\Search\SearchHit::__set_state(array(
+       'valueObject' => 225,
+       'score' => NULL,
+       'index' => NULL,
+       'highlight' => NULL,
     )),
   ),
+   'spellSuggestion' => NULL,
+   'time' => 1,
+   'timedOut' => NULL,
+   'maxScore' => NULL,
+   'totalCount' => 2,
 ));
 
--- eZ/Publish/API/Repository/Tests/_fixtures/Legacy/LogicalOr.php
+++ eZ/Publish/API/Repository/Tests/_fixtures/Legacy/LogicalOr.php
@@ -1,354 +1,37 @@
 <?php
 
-return eZ\Publish\SPI\Persistence\Content\Search\Result::__set_state(array(
-   'count' => 3,
-   'content' => 
+return eZ\Publish\API\Repository\Values\Content\Search\SearchResult::__set_state(array(
+   'facets' => 
+  array (
+  ),
+   'searchHits' => 
   array (
     0 => 
-    eZ\Publish\SPI\Persistence\Content::__set_state(array(
-       'versionInfo' => 
-      eZ\Publish\SPI\Persistence\Content\VersionInfo::__set_state(array(
-         'id' => 4,
-         'versionNo' => 1,
-         'contentId' => 4,
-         'names' => 
-        array (
-          'eng-US' => 'Users',
-        ),
-         'creationDate' => 0,
-         'modificationDate' => 0,
-         'creatorId' => 14,
-         'status' => 1,
-         'initialLanguageCode' => 'eng-US',
-         'languageIds' => 
-        array (
-          0 => 2,
-        ),
-      )),
-       'contentInfo' => 
-      eZ\Publish\SPI\Persistence\Content\ContentInfo::__set_state(array(
-         'id' => 4,
-         'name' => 'Users',
-         'contentTypeId' => 3,
-         'sectionId' => 2,
-         'currentVersionNo' => 1,
-         'isPublished' => true,
-         'ownerId' => 14,
-         'modificationDate' => 1033917596,
-         'publicationDate' => 1033917596,
-         'isAlwaysAvailable' => true,
-         'remoteId' => 'f5c88a2209584891056f987fd965b0ba',
-         'mainLanguageCode' => 'eng-US',
-      )),
-       'relations' => NULL,
-       'fields' => 
-      array (
-        0 => 
-        eZ\Publish\SPI\Persistence\Content\Field::__set_state(array(
-           'id' => 7,
-           'fieldDefinitionId' => 7,
-           'type' => 'ezstring',
-           'value' => 
-          eZ\Publish\SPI\Persistence\Content\FieldValue::__set_state(array(
-             'data' => 'Main group',
-             'fieldSettings' => NULL,
-             'externalData' => NULL,
-             'sortKey' => 
-            array (
-              'sort_key_string' => '',
-            ),
-          )),
-           'languageCode' => 'eng-US',
-           'versionNo' => 1,
-        )),
-        1 => 
-        eZ\Publish\SPI\Persistence\Content\Field::__set_state(array(
-           'id' => 8,
-           'fieldDefinitionId' => 6,
-           'type' => 'ezstring',
-           'value' => 
-          eZ\Publish\SPI\Persistence\Content\FieldValue::__set_state(array(
-             'data' => 'Users',
-             'fieldSettings' => NULL,
-             'externalData' => NULL,
-             'sortKey' => 
-            array (
-              'sort_key_string' => '',
-            ),
-          )),
-           'languageCode' => 'eng-US',
-           'versionNo' => 1,
-        )),
-      ),
-       'locations' => 
-      array (
-        0 => 
-        eZ\Publish\SPI\Persistence\Content\Location::__set_state(array(
-           'id' => '5',
-           'priority' => '0',
-           'hidden' => '0',
-           'invisible' => '0',
-           'remoteId' => '3f6d92f8044aed134f32153517850f5a',
-           'contentId' => '4',
-           'parentId' => '1',
-           'pathIdentificationString' => 'users',
-           'pathString' => '/1/5/',
-           'modifiedSubLocation' => '1311154216',
-           'mainLocationId' => '5',
-           'depth' => '1',
-           'sortField' => '1',
-           'sortOrder' => '1',
-        )),
-      ),
+    eZ\Publish\API\Repository\Values\Content\Search\SearchHit::__set_state(array(
+       'valueObject' => 4,
+       'score' => NULL,
+       'index' => NULL,
+       'highlight' => NULL,
     )),
     1 => 
-    eZ\Publish\SPI\Persistence\Content::__set_state(array(
-       'versionInfo' => 
-      eZ\Publish\SPI\Persistence\Content\VersionInfo::__set_state(array(
-         'id' => 474,
-         'versionNo' => 2,
-         'contentId' => 10,
-         'names' => 
-        array (
-          'eng-US' => 'Anonymous User',
-        ),
-         'creationDate' => 1072180337,
-         'modificationDate' => 1072180405,
-         'creatorId' => 14,
-         'status' => 1,
-         'initialLanguageCode' => 'eng-US',
-         'languageIds' => 
-        array (
-          0 => 2,
-        ),
-      )),
-       'contentInfo' => 
-      eZ\Publish\SPI\Persistence\Content\ContentInfo::__set_state(array(
-         'id' => 10,
-         'name' => 'Anonymous User',
-         'contentTypeId' => 4,
-         'sectionId' => 2,
-         'currentVersionNo' => 2,
-         'isPublished' => true,
-         'ownerId' => 14,
-         'modificationDate' => 1072180405,
-         'publicationDate' => 1033920665,
-         'isAlwaysAvailable' => true,
-         'remoteId' => 'faaeb9be3bd98ed09f606fc16d144eca',
-         'mainLanguageCode' => 'eng-US',
-      )),
-       'relations' => NULL,
-       'fields' => 
-      array (
-        0 => 
-        eZ\Publish\SPI\Persistence\Content\Field::__set_state(array(
-           'id' => 177,
-           'fieldDefinitionId' => 179,
-           'type' => 'eztext',
-           'value' => 
-          eZ\Publish\SPI\Persistence\Content\FieldValue::__set_state(array(
-             'data' => '',
-             'fieldSettings' => NULL,
-             'externalData' => NULL,
-             'sortKey' => 
-            array (
-              'sort_key_string' => '',
-            ),
-          )),
-           'languageCode' => 'eng-US',
-           'versionNo' => 2,
-        )),
-        1 => 
-        eZ\Publish\SPI\Persistence\Content\Field::__set_state(array(
-           'id' => 179,
-           'fieldDefinitionId' => 180,
-           'type' => 'ezimage',
-           'value' => 
-          eZ\Publish\SPI\Persistence\Content\FieldValue::__set_state(array(
-             'data' => 0,
-             'fieldSettings' => NULL,
-             'externalData' => NULL,
-             'sortKey' => 
-            array (
-              'sort_key_int' => 0,
-            ),
-          )),
-           'languageCode' => 'eng-US',
-           'versionNo' => 2,
-        )),
-        2 => 
-        eZ\Publish\SPI\Persistence\Content\Field::__set_state(array(
-           'id' => 21,
-           'fieldDefinitionId' => 12,
-           'type' => 'ezuser',
-           'value' => 
-          eZ\Publish\SPI\Persistence\Content\FieldValue::__set_state(array(
-             'data' => 0,
-             'fieldSettings' => NULL,
-             'externalData' => NULL,
-             'sortKey' => 
-            array (
-              'sort_key_int' => 0,
-            ),
-          )),
-           'languageCode' => 'eng-US',
-           'versionNo' => 2,
-        )),
-        3 => 
-        eZ\Publish\SPI\Persistence\Content\Field::__set_state(array(
-           'id' => 20,
-           'fieldDefinitionId' => 9,
-           'type' => 'ezstring',
-           'value' => 
-          eZ\Publish\SPI\Persistence\Content\FieldValue::__set_state(array(
-             'data' => 'User',
-             'fieldSettings' => NULL,
-             'externalData' => NULL,
-             'sortKey' => 
-            array (
-              'sort_key_string' => 'user',
-            ),
-          )),
-           'languageCode' => 'eng-US',
-           'versionNo' => 2,
-        )),
-        4 => 
-        eZ\Publish\SPI\Persistence\Content\Field::__set_state(array(
-           'id' => 19,
-           'fieldDefinitionId' => 8,
-           'type' => 'ezstring',
-           'value' => 
-          eZ\Publish\SPI\Persistence\Content\FieldValue::__set_state(array(
-             'data' => 'Anonymous',
-             'fieldSettings' => NULL,
-             'externalData' => NULL,
-             'sortKey' => 
-            array (
-              'sort_key_string' => 'anonymous',
-            ),
-          )),
-           'languageCode' => 'eng-US',
-           'versionNo' => 2,
-        )),
-      ),
-       'locations' => 
-      array (
-        0 => 
-        eZ\Publish\SPI\Persistence\Content\Location::__set_state(array(
-           'id' => '45',
-           'priority' => '0',
-           'hidden' => '0',
-           'invisible' => '0',
-           'remoteId' => '2cf8343bee7b482bab82b269d8fecd76',
-           'contentId' => '10',
-           'parentId' => '44',
-           'pathIdentificationString' => 'users/anonymous_users/anonymous_user',
-           'pathString' => '/1/5/44/45/',
-           'modifiedSubLocation' => '1081860719',
-           'mainLocationId' => '45',
-           'depth' => '3',
-           'sortField' => '9',
-           'sortOrder' => '1',
-        )),
-      ),
+    eZ\Publish\API\Repository\Values\Content\Search\SearchHit::__set_state(array(
+       'valueObject' => 10,
+       'score' => NULL,
+       'index' => NULL,
+       'highlight' => NULL,
     )),
     2 => 
-    eZ\Publish\SPI\Persistence\Content::__set_state(array(
-       'versionInfo' => 
-      eZ\Publish\SPI\Persistence\Content\VersionInfo::__set_state(array(
-         'id' => 440,
-         'versionNo' => 1,
-         'contentId' => 12,
-         'names' => 
-        array (
-          'eng-US' => 'Administrator users',
-        ),
-         'creationDate' => 1033920760,
-         'modificationDate' => 1033920775,
-         'creatorId' => 14,
-         'status' => 1,
-         'initialLanguageCode' => 'eng-US',
-         'languageIds' => 
-        array (
-          0 => 2,
-        ),
-      )),
-       'contentInfo' => 
-      eZ\Publish\SPI\Persistence\Content\ContentInfo::__set_state(array(
-         'id' => 12,
-         'name' => 'Administrator users',
-         'contentTypeId' => 3,
-         'sectionId' => 2,
-         'currentVersionNo' => 1,
-         'isPublished' => true,
-         'ownerId' => 14,
-         'modificationDate' => 1033920775,
-         'publicationDate' => 1033920775,
-         'isAlwaysAvailable' => true,
-         'remoteId' => '9b47a45624b023b1a76c73b74d704acf',
-         'mainLanguageCode' => 'eng-US',
-      )),
-       'relations' => NULL,
-       'fields' => 
-      array (
-        0 => 
-        eZ\Publish\SPI\Persistence\Content\Field::__set_state(array(
-           'id' => 25,
-           'fieldDefinitionId' => 7,
-           'type' => 'ezstring',
-           'value' => 
-          eZ\Publish\SPI\Persistence\Content\FieldValue::__set_state(array(
-             'data' => '',
-             'fieldSettings' => NULL,
-             'externalData' => NULL,
-             'sortKey' => 
-            array (
-              'sort_key_string' => '',
-            ),
-          )),
-           'languageCode' => 'eng-US',
-           'versionNo' => 1,
-        )),
-        1 => 
-        eZ\Publish\SPI\Persistence\Content\Field::__set_state(array(
-           'id' => 24,
-           'fieldDefinitionId' => 6,
-           'type' => 'ezstring',
-           'value' => 
-          eZ\Publish\SPI\Persistence\Content\FieldValue::__set_state(array(
-             'data' => 'Administrator users',
-             'fieldSettings' => NULL,
-             'externalData' => NULL,
-             'sortKey' => 
-            array (
-              'sort_key_string' => '',
-            ),
-          )),
-           'languageCode' => 'eng-US',
-           'versionNo' => 1,
-        )),
-      ),
-       'locations' => 
-      array (
-        0 => 
-        eZ\Publish\SPI\Persistence\Content\Location::__set_state(array(
-           'id' => '13',
-           'priority' => '0',
-           'hidden' => '0',
-           'invisible' => '0',
-           'remoteId' => '769380b7aa94541679167eab817ca893',
-           'contentId' => '12',
-           'parentId' => '5',
-           'pathIdentificationString' => 'users/administrator_users',
-           'pathString' => '/1/5/13/',
-           'modifiedSubLocation' => '1311154214',
-           'mainLocationId' => '13',
-           'depth' => '2',
-           'sortField' => '1',
-           'sortOrder' => '1',
-        )),
-      ),
+    eZ\Publish\API\Repository\Values\Content\Search\SearchHit::__set_state(array(
+       'valueObject' => 12,
+       'score' => NULL,
+       'index' => NULL,
+       'highlight' => NULL,
     )),
   ),
+   'spellSuggestion' => NULL,
+   'time' => 1,
+   'timedOut' => NULL,
+   'maxScore' => NULL,
+   'totalCount' => 3,
 ));
 
