16e4202cfdcbd59fcd639b4e19301be9791b4f9a
Kore Nordmann <mail@kore-nordmann.de>
2012-06-19 13:36:13 +0200

    Stubbed search service in repository implementation

---
+++ eZ/Publish/Core/Repository/SearchService.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * File containing the eZ\Publish\Core\Repository\SearchService class.
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\Core\Repository;
+
+use eZ\Publish\API\Repository\SearchService as SearchServiceInterface;
+use \eZ\Publish\API\Repository\Values\Content\Query\Criterion;
+use \eZ\Publish\API\Repository\Values\Content\Query;
+use eZ\Publish\API\Repository\Repository as RepositoryInterface;
+use eZ\Publish\SPI\Persistence\Handler;
+
+/**
+ * Search service
+ *
+ * @package eZ\Publish\Core\Repository
+ */
+class SearchService implements SearchServiceInterface
+{
+    /**
+     * @var \eZ\Publish\API\Repository\Repository
+     */
+    protected $repository;
+
+    /**
+     * @var \eZ\Publish\SPI\Persistence\Handler
+     */
+    protected $persistenceHandler;
+
+    /**
+     * @var array
+     */
+    protected $settings;
+
+    /**
+     * Setups service with reference to repository object that created it & corresponding handler
+     *
+     * @param \eZ\Publish\API\Repository\Repository  $repository
+     * @param \eZ\Publish\SPI\Persistence\Handler $handler
+     * @param array $settings
+     */
+    public function __construct( RepositoryInterface $repository, Handler $handler, array $settings = array() )
+    {
+        $this->repository = $repository;
+        $this->persistenceHandler = $handler;
+        $this->settings = $settings;
+    }
+
+     /**
+     * finds content objects for the given query.
+     *
+     * @TODO define structs for the field filters
+     *
+     * @param \eZ\Publish\API\Repository\Values\Content\Query $query
+     * @param array  $fieldFilters - a map of filters for the returned fields.
+     *        Currently supported: <code>array("languages" => array(<language1>,..))</code>.
+     * @param boolean $filterOnUserPermissions if true only the objects which is the user allowed to read are returned.
+     *
+     * @return \eZ\Publish\API\Repository\Values\Content\Search\SearchResult
+     */
+    public function findContent( Query $query, array $fieldFilters = array(), $filterOnUserPermissions = true )
+    {
+        return $this->persistenceHandler->searchHandler()->find(
+            $query->criterion,
+            $query->offset,
+            $query->limit
+        );
+    }
+
+    /**
+     * Performs a query for a single content object
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException if the object was not found by the query or due to permissions
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException if there is more than than one result matching the criterions
+     *
+     * @TODO define structs for the field filters
+     * @param \eZ\Publish\API\Repository\Values\Content\Query\Criterion $criterion
+     * @param array  $fieldFilters - a map of filters for the returned fields.
+     *        Currently supported: <code>array("languages" => array(<language1>,..))</code>.
+     * @param boolean $filterOnUserPermissions if true only the objects which is the user allowed to read are returned.
+     *
+     * @return \eZ\Publish\API\Repository\Values\Content\Content
+     */
+    public function findSingle( Criterion $criterion, array $fieldFilters = array(), $filterOnUserPermissions = true )
+    {
+
+    }
+
+    /**
+     * Suggests a list of values for the given prefix
+     *
+     * @param string $prefix
+     * @param string[] $fieldpath
+     * @param int $limit
+     * @param \eZ\Publish\API\Repository\Values\Content\Query\Criterion $filter
+     */
+    public function suggest( $prefix, $fieldPaths = array(), $limit = 10, Criterion $filter = null )
+    {
+
+    }
+}
