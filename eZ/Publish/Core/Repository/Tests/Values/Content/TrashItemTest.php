a7417ea829496a8f829fd9d18ca539f5d88329c1
André R <ar@ez.no>
2012-06-26 00:43:23 +0200

    Added unit tests for ContentType and User + changed to use attribute()

--- eZ/Publish/Core/Repository/Tests/Values/Content/TrashItemTest.php
+++ eZ/Publish/Core/Repository/Tests/Values/Content/TrashItemTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * File containing the ContentTest class
+ * File containing the TrashItemTest class
  *
  * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
@@ -9,8 +9,7 @@
 
 namespace eZ\Publish\Core\Repository\Tests\Values\Content;
 use eZ\Publish\Core\Repository\Values\Content\TrashItem,
-    PHPUnit_Framework_TestCase,
-    ReflectionObject;
+    PHPUnit_Framework_TestCase;
 
 /**
  *
@@ -24,26 +23,37 @@ class TrashItemTest extends PHPUnit_Framework_TestCase
     public function testObjectProperties()
     {
         $object = new TrashItem;
-        $properties = array();
-        foreach( $object as $property => $propValue )
+        $properties = $object->attributes();
+        //self::assertNotContains( 'internalFields', $properties, 'Internal property found ' );
+        self::assertContains( 'contentInfo', $properties, 'Property not found' );
+        self::assertContains( 'contentId', $properties, 'Property not found' );
+        self::assertContains( 'id', $properties, 'Property not found' );
+        self::assertContains( 'priority', $properties, 'Property not found' );
+        self::assertContains( 'hidden', $properties, 'Property not found' );
+        self::assertContains( 'invisible', $properties, 'Property not found' );
+        self::assertContains( 'remoteId', $properties, 'Property not found' );
+        self::assertContains( 'parentLocationId', $properties, 'Property not found' );
+        self::assertContains( 'pathString', $properties, 'Property not found' );
+        self::assertContains( 'path', $properties, 'Property not found' );
+        self::assertContains( 'modifiedSubLocationDate', $properties, 'Property not found' );
+        self::assertContains( 'depth', $properties, 'Property not found' );
+        self::assertContains( 'sortField', $properties, 'Property not found' );
+        self::assertContains( 'sortOrder', $properties, 'Property not found' );
+        self::assertContains( 'childCount', $properties, 'Property not found' );
+
+        // check for duplicates and double check existence of property
+        $propertiesHash = array();
+        foreach ( $properties as $property )
         {
-            //self::assertNotEquals( 'internalProperty', $property );
-            $properties[] = $property;
+            if ( isset( $propertiesHash[$property] ) )
+            {
+                self::fail( "Property '{$property}' exists several times in properties list" );
+            }
+            else if ( !isset( $object->$property ) )
+            {
+                self::fail( "Property '{$property}' does not exist on object, even though it was hinted to be there" );
+            }
+            $propertiesHash[$property] = 1;
         }
-        self::assertContains( 'contentInfo', $properties, 'Property not found on TrashItem' );
-        self::assertContains( 'contentId', $properties, 'Property not found on TrashItem' );
-        self::assertContains( 'id', $properties, 'Property not found on TrashItem' );
-        self::assertContains( 'priority', $properties, 'Property not found on TrashItem' );
-        self::assertContains( 'hidden', $properties, 'Property not found on TrashItem' );
-        self::assertContains( 'invisible', $properties, 'Property not found on TrashItem' );
-        self::assertContains( 'remoteId', $properties, 'Property not found on TrashItem' );
-        self::assertContains( 'parentLocationId', $properties, 'Property not found on TrashItem' );
-        self::assertContains( 'pathString', $properties, 'Property not found on TrashItem' );
-        self::assertContains( 'path', $properties, 'Property not found on TrashItem' );
-        self::assertContains( 'modifiedSubLocationDate', $properties, 'Property not found on TrashItem' );
-        self::assertContains( 'depth', $properties, 'Property not found on TrashItem' );
-        self::assertContains( 'sortField', $properties, 'Property not found on TrashItem' );
-        self::assertContains( 'sortOrder', $properties, 'Property not found on TrashItem' );
-        self::assertContains( 'childCount', $properties, 'Property not found on TrashItem' );
     }
 }
--- eZ/Publish/Core/Repository/Tests/Values/User/UserGroupTest.php
+++ eZ/Publish/Core/Repository/Tests/Values/User/UserGroupTest.php
@@ -1,36 +1,38 @@
 <?php
 /**
- * File containing the ContentTest class
+ * File containing the UserGroupTest class
  *
  * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
  * @version //autogentag//
  */
 
-namespace eZ\Publish\Core\Repository\Tests\Values\Content;
-use eZ\Publish\Core\Repository\Values\Content\Content,
+namespace eZ\Publish\Core\Repository\Tests\Values\User;
+use eZ\Publish\Core\Repository\Values\User\UserGroup,
     PHPUnit_Framework_TestCase;
 
 /**
  *
  */
-class ContentTest extends PHPUnit_Framework_TestCase
+class UserGroupTest extends PHPUnit_Framework_TestCase
 {
     /**
-     * @covers \eZ\Publish\Core\Repository\Values\Content\Content::getIterator
-     * @covers \eZ\Publish\Core\Repository\Values\Content\Content::getProperties
+     * @covers \eZ\Publish\Core\Repository\Values\User\User::getIterator
+     * @covers \eZ\Publish\Core\Repository\Values\User\User::getProperties
      */
     public function testObjectProperties()
     {
-        $object = new Content( array( 'internalFields' => array() ) );
+        $object = new UserGroup;
         $properties = $object->attributes();
         self::assertNotContains( 'internalFields', $properties, 'Internal property found ' );
-        self::assertContains( 'id', $properties, 'Property not found ' );
-        self::assertContains( 'fields', $properties, 'Property not found ' );
-        self::assertContains( 'relations', $properties, 'Property not found ' );
-        self::assertContains( 'versionInfo', $properties, 'Property not found ' );
-        self::assertContains( 'contentInfo', $properties, 'Property not found ' );
-        self::assertContains( 'contentType', $properties, 'Property not found ' );
+        self::assertContains( 'id', $properties, 'Property not found' );
+        self::assertContains( 'fields', $properties, 'Property not found' );
+        self::assertContains( 'relations', $properties, 'Property not found' );
+        self::assertContains( 'versionInfo', $properties, 'Property not found' );
+        self::assertContains( 'contentInfo', $properties, 'Property not found' );
+        self::assertContains( 'contentType', $properties, 'Property not found' );
+        self::assertContains( 'parentId', $properties, 'Property not found' );
+        self::assertContains( 'subGroupCount', $properties, 'Property not found' );
 
         // check for duplicates and double check existence of property
         $propertiesHash = array();
