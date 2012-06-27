fa0880b0ac9ad6f22a44b43a49f63e184365c135
Edi Modric <edi.modric@gmail.com>
2012-06-27 17:56:57 +0200

    Add object state service tests

---
+++ eZ/Publish/Core/Repository/Tests/Service/InMemory/ObjectStateTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * File contains: eZ\Publish\Core\Repository\Tests\Service\InMemory\ObjectStateTest class
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\Core\Repository\Tests\Service\InMemory;
+use eZ\Publish\Core\Repository\Tests\Service\ObjectStateBase as BaseObjectStateServiceTest,
+    eZ\Publish\Core\Repository\Repository,
+    eZ\Publish\Core\IO\InMemoryHandler as InMemoryIOHandler,
+    eZ\Publish\Core\Persistence\InMemory\Handler as InMemoryPersistenceHandler;
+
+/**
+ * Test case for object state Service using InMemory storage class
+ */
+class ObjectStateTest extends BaseObjectStateServiceTest
+{
+    protected function getRepository( array $serviceSettings )
+    {
+        // Temporary hack for different language ids between in memory and legacy fixtures
+        $this->defaultLanguageCode = 'eng-GB';
+
+        return new Repository( new InMemoryPersistenceHandler(), new InMemoryIOHandler(), $serviceSettings );
+    }
+}
