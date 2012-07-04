ba06e3afa81c40490eb6fc8a6b3f4c85d9a737d1
Kore Nordmann <mail@kore-nordmann.de>
2012-07-04 15:07:53 +0200

    Implemented: Section criterion visitor

--- eZ/Publish/API/Repository/Tests/SetupFactory/LegacySolr.php
+++ eZ/Publish/API/Repository/Tests/SetupFactory/LegacySolr.php
@@ -62,6 +62,7 @@ class LegacySolr extends Legacy
                     new Solr\Content\Search\CriterionVisitor\LogicalNot(),
                     new Solr\Content\Search\CriterionVisitor\SubtreeIn(),
                     new Solr\Content\Search\CriterionVisitor\ContentTypeIdIn(),
+                    new Solr\Content\Search\CriterionVisitor\SectionIn(),
                 ) ),
                 new Solr\Content\Search\FieldValueMapper\Aggregate( array(
                     new Solr\Content\Search\FieldValueMapper\StringMapper(),
