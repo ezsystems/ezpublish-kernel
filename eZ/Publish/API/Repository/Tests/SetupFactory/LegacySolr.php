ca1f370d9c26d4a707037357b0079d9087337988
Kore Nordmann <mail@kore-nordmann.de>
2012-07-04 12:25:02 +0200

    Implemented: Subtree criterion visitor

--- eZ/Publish/API/Repository/Tests/SetupFactory/LegacySolr.php
+++ eZ/Publish/API/Repository/Tests/SetupFactory/LegacySolr.php
@@ -60,6 +60,7 @@ class LegacySolr extends Legacy
                     new Solr\Content\Search\CriterionVisitor\LogicalAnd(),
                     new Solr\Content\Search\CriterionVisitor\LogicalOr(),
                     new Solr\Content\Search\CriterionVisitor\LogicalNot(),
+                    new Solr\Content\Search\CriterionVisitor\SubtreeIn(),
                 ) ),
                 new Solr\Content\Search\FieldValueMapper\Aggregate( array(
                     new Solr\Content\Search\FieldValueMapper\StringMapper(),
---
+++ eZ/Publish/Core/Persistence/Solr/Content/Search/CriterionVisitor/SubtreeIn.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * File containing the Content Search handler class
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\Core\Persistence\Solr\Content\Search\CriterionVisitor;
+
+use eZ\Publish\Core\Persistence\Solr\Content\Search\CriterionVisitor,
+    eZ\Publish\API\Repository\Values\Content\Query\Criterion,
+    eZ\Publish\API\Repository\Values\Content\Query\Criterion\Operator;
+
+/**
+ * Visits the Subtree criterion
+ */
+class SubtreeIn extends CriterionVisitor
+{
+    /**
+     * CHeck if visitor is applicable to current criterion
+     *
+     * @param Criterion $criterion
+     * @return bool
+     */
+    public function canVisit( Criterion $criterion )
+    {
+        return
+            $criterion instanceof Criterion\Subtree &&
+            ( ( $criterion->operator ?: Operator::IN ) === Operator::IN ||
+              $criterion->operator === Operator::EQ );
+    }
+
+    /**
+     * Map field value to a proper Solr representation
+     *
+     * @param DocumentField $field
+     * @return void
+     */
+    public function visit( Criterion $criterion, CriterionVisitor $subVisitor = null )
+    {
+        return '(' .
+            implode(
+                ' OR ',
+                array_map(
+                    function( $value )
+                    {
+                        return 'location_s:' . $value . '*';
+                    },
+                    $criterion->value
+                )
+            ) .
+            ')';
+    }
+}
+
