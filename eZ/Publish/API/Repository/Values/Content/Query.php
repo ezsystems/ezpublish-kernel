e82417582bd3540deeda92d4de76af1b5fba4b19
Kore Nordmann <mail@kore-nordmann.de>
2012-07-04 16:08:13 +0200

    Default to empty array

--- eZ/Publish/API/Repository/Values/Content/Query.php
+++ eZ/Publish/API/Repository/Values/Content/Query.php
@@ -35,14 +35,14 @@ class Query extends ValueObject
      *
      * @var \eZ\Publish\API\Repository\Values\Content\Query\SortClause[]
      */
-    public $sortClauses;
+    public $sortClauses = array();
 
     /**
      * An array of facet builders
      *
      * @var \eZ\Publish\API\Repository\Values\Content\Query\FacetBuilder[]
      */
-    public $facetBuilders;
+    public $facetBuilders = array();
 
     /**
      * Query offset
