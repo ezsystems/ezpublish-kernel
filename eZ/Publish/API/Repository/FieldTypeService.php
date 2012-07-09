a593ec88083b2782c1019c90a1747fd2a2702f77
Tobias Schlitt <toby@php.net>
2012-07-09 16:23:43 +0200

    Implemented: FieldType & FieldTypeService interface.
    These interfaces will be used to expose SPI\FieldType\FieldType
    partially to the Public API.

---
+++ eZ/Publish/API/Repository/FieldTypeService.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * File containing the eZ\Publish\API\Repository\FieldTypeService class.
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ * @package eZ\Publish\API\Repository
+ */
+
+namespace eZ\Publish\API\Repository;
+use eZ\Publish\API\Repository\FieldType;
+
+/**
+ * An implementation of this class provides access to FieldTypes
+ *
+ * @package eZ\Publish\API\Repository
+ * @see eZ\Publish\API\Repository\FieldType
+ */
+interface FieldTypeService
+{
+    /**
+     * Returns a list of all field types.
+     *
+     * @return \eZ\Publish\API\Repository\FieldType[]
+     */
+    public function getFieldTypes();
+
+    /**
+     * Returns the FieldType registered with the given identifier
+     *
+     * @param string $identifier
+     * @return \eZ\Publish\API\Repository\FieldType
+     * @throws \eZ\Publish\API\Exceptions\NotFoundException
+     *         if there is no FieldType registered with $identifier
+     */
+    public function getFieldType( $identifier );
+
+    /**
+     * Returns if there is a FieldType registered under $identifier
+     *
+     * @param string $identifier
+     * @return bool
+     */
+    public function hasFieldType( $identifier );
+}
