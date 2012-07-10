f705501b18ebaa6157a322de54bb7949c2b79bc7
Patrick Allaert <pa@ez.no>
2012-07-10 16:00:04 +0200

    Fixed: newline removed from generated code

--- eZ/Publish/Core/Persistence/Legacy/Tests/Content/Gateway/EzcDatabaseTest.php
+++ eZ/Publish/Core/Persistence/Legacy/Tests/Content/Gateway/EzcDatabaseTest.php
@@ -1768,7 +1768,7 @@ class EzcDatabaseTest extends LanguageAwareTestCase
     {
         file_put_contents(
             $file,
-            "<?php\n\nreturn " . str_replace( " \n", "", var_export( $fixture, true ) ) . ";\n"
+            "<?php\n\nreturn " . str_replace( " \n", "\n", var_export( $fixture, true ) ) . ";\n"
         );
     }
 
