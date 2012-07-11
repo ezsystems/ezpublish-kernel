807f7e6437e18a7cf73aa354e1ee48963276ddd0
Jérôme Vieilledent <lolautruche@gmail.com>
2012-07-11 15:57:25 +0200

    Added IOServiceLegacyTest in integration tests, overriding IOServiceTest

---
+++ eZ/Publish/API/Repository/Tests/IOServiceLegacyTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * File containing the IOServiceTest class
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\API\Repository\Tests;
+
+/**
+ * Test case for operations in the IOService using legacy storage.
+ *
+ * @see eZ\Publish\API\Repository\IOService
+ * @group io
+ */
+class IOServiceLegacyTest extends IOServiceTest
+{
+    protected function getRepository()
+    {
+        if ( !isset( $_ENV['legacyKernel'] ) )
+            self::markTestSkipped( 'Legacy kernel is needed to run this test.' );
+
+        $repository = parent::getRepository();
+        $repository->setLegacyKernel( $_ENV['legacyKernel'] );
+        return $repository;
+    }
+}
