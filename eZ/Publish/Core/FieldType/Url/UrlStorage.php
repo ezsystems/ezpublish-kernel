43c187be9083ddae7d099e675bbac3165e043815
Tobias Schlitt <toby@php.net>
2012-07-11 17:14:49 +0200

    Updated: Refactored Url Storage to be Gateway based.

--- eZ/Publish/Core/FieldType/Url/UrlStorage.php
+++ eZ/Publish/Core/FieldType/Url/UrlStorage.php
@@ -9,6 +9,7 @@
 
 namespace eZ\Publish\Core\FieldType\Url;
 use eZ\Publish\SPI\FieldType\FieldStorage,
+    eZ\Publish\Core\FieldType\GatewayBasedStorage,
     eZ\Publish\SPI\Persistence\Content\VersionInfo,
     eZ\Publish\SPI\Persistence\Content\Field,
     eZ\Publish\Core\Persistence\Legacy\EzcDbHandler,
@@ -19,25 +20,15 @@ use eZ\Publish\SPI\FieldType\FieldStorage,
  * @TODO indroduce persistence layer (gateways)
  *
  */
-class UrlStorage implements FieldStorage
+class UrlStorage extends GatewayBasedStorage
 {
-    const URL_TABLE = "ezurl";
-
     /**
      * @see \eZ\Publish\SPI\FieldType\FieldStorage
      */
     public function storeFieldData( VersionInfo $versionInfo, Field $field, array $context )
     {
-        $dbHandler = $context["connection"];
-        if ( ( $row = $this->fetchByLink( $field->value->externalData, $dbHandler ) ) !== false )
-            $urlId = $row["id"];
-        else
-            $urlId = $this->insert( $versionInfo, $field, $dbHandler );
-
-        $field->value->data["urlId"] = $urlId;
-
-        // Signals that the Value has been modified and that an update is to be performed
-        return true;
+        $gateway = $this->getGateway( $context );
+        return $gateway->storeFieldData( $versionInfo, $field );
     }
 
     /**
@@ -52,9 +43,8 @@ class UrlStorage implements FieldStorage
      */
     public function getFieldData( VersionInfo $versionInfo, Field $field, array $context )
     {
-        $url = $this->fetchById( $field->value->data["urlId"], $context["connection"] );
-
-        $field->value->externalData = $url["url"];
+        $gateway = $this->getGateway( $context );
+        return $gateway->getFieldData( $field );
     }
 
     /**
@@ -82,94 +72,5 @@ class UrlStorage implements FieldStorage
      */
     public function getIndexData( VersionInfo $versionInfo, Field $field, array $context )
     {
-    }
-
-    /**
-     * Fetches a row in ezurl table referenced by its $id
-     *
-     * @param mixed $id
-     * @param \eZ\Publish\Core\Persistence\Legacy\EzcDbHandler $dbHandler
-     * @return null|array Hash with columns as keys or null if no entry can be found
-     */
-    private function fetchById( $id, EzcDbHandler $dbHandler )
-    {
-        $q = $dbHandler->createSelectQuery();
-        $e = $q->expr;
-        $q->select( "*" )
-            ->from( $dbHandler->quoteTable( self::URL_TABLE ) )
-            ->where(
-                $e->eq( "id", $q->bindValue( $id, null, \PDO::PARAM_INT ) )
-            );
-        $statement = $q->prepare();
-        $statement->execute();
-        $rows = $statement->fetchAll( \PDO::FETCH_ASSOC );
-        if ( count( $rows ) )
-        {
-            return $rows[0];
-        }
-
-        return false;
-    }
-
-    /**
-     * Fetches a row in ezurl table referenced by $link
-     *
-     * @param string $link
-     * @param \eZ\Publish\Core\Persistence\Legacy\EzcDbHandler $dbHandler
-     * @return null|array Hash with columns as keys or null if no entry can be found
-     */
-    private function fetchByLink( $link, EzcDbHandler $dbHandler )
-    {
-        $q = $dbHandler->createSelectQuery();
-        $e = $q->expr;
-        $q->select( "*" )
-            ->from( $dbHandler->quoteTable( self::URL_TABLE ) )
-            ->where(
-                $e->eq( "url", $q->bindValue( $link ) )
-            );
-        $statement = $q->prepare();
-        $statement->execute();
-        $rows = $statement->fetchAll( \PDO::FETCH_ASSOC );
-        if ( count( $rows ) )
-        {
-            return $rows[0];
-        }
-
-        return false;
-    }
-
-    /**
-     * Inserts a new entry in ezurl table with $field value data
-     *
-     * @param \eZ\Publish\SPI\Persistence\Content\Field $field
-     * @param \eZ\Publish\Core\Persistence\Legacy\EzcDbHandler $dbHandler
-     * @return mixed
-     */
-    private function insert( VersionInfo $versionInfo, Field $field, EzcDbHandler $dbHandler )
-    {
-        $time = time();
-        $q = $dbHandler->createInsertQuery();
-
-        $q->insertInto(
-            $dbHandler->quoteTable( self::URL_TABLE )
-        )->set(
-            $dbHandler->quoteColumn( "created" ),
-            $q->bindValue( $time, null, \PDO::PARAM_INT )
-        )->set(
-            $dbHandler->quoteColumn( "modified" ),
-            $q->bindValue( $time, null, \PDO::PARAM_INT )
-        )->set(
-            $dbHandler->quoteColumn( "original_url_md5" ),
-            $q->bindValue( md5( $field->value->externalData ) )
-        )->set(
-            $dbHandler->quoteColumn( "url" ),
-            $q->bindValue( $field->value->externalData )
-        );
-
-        $q->prepare()->execute();
-
-        return $dbHandler->lastInsertId(
-            $dbHandler->getSequenceName( self::URL_TABLE, "id" )
-        );
     }
 }
