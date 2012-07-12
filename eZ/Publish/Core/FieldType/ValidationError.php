a6c2f2a692f3922c60b81a6c5c8db17fce2c1ed3
Petar Spanja <petar.spanja@gmail.com>
2012-07-12 11:56:27 +0200

    Implemented: ValidationError

---
+++ eZ/Publish/Core/FieldType/ValidationError.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * File containing the ValidationError class
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\Core\FieldType;
+use eZ\Publish\SPI\FieldType\ValidationError as ValidationErrorInterface,
+    eZ\Publish\API\Repository\Values\Translation\Message,
+    eZ\Publish\API\Repository\Values\Translation\Plural;
+
+/**
+ * Class for validation errors.
+ */
+class ValidationError implements ValidationErrorInterface
+{
+    /**
+     * @var string
+     */
+    protected $singular;
+
+    /**
+     * @var string
+     */
+    protected $plural;
+
+    /**
+     * @var string
+     */
+    protected $values;
+
+    /**
+     * @param string $singular
+     * @param string $plural
+     * @param array $values
+     */
+    public function __construct( $singular, $plural = null, array $values = array() )
+    {
+        $this->singular = $singular;
+        $this->plural = $plural;
+        $this->values = $values;
+    }
+
+    /**
+     * Returns a translatable Message
+     *
+     * @return \eZ\Publish\API\Repository\Values\Translation
+     */
+    public function getTranslatableMessage()
+    {
+        if ( isset( $this->plural ) )
+        {
+            return new Plural(
+                $this->singular,
+                $this->plural,
+                $this->values
+            );
+        }
+        else
+        {
+            return new Message(
+                $this->singular,
+                $this->values
+            );
+        }
+    }
+}
