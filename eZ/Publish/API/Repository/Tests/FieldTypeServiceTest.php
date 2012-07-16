829188396ddbd74d5302e90a23e278820fb4468f
Tobias Schlitt <toby@php.net>
2012-07-16 19:04:02 +0200

    Added: Test stub for FieldTypeService.

---
+++ eZ/Publish/API/Repository/Tests/FieldTypeServiceTest.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * File containing the FieldTypeServiceTest class
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\API\Repository\Tests;
+
+/**
+ * Test case for operations in the FieldTypeService using in memory storage.
+ *
+ * @see eZ\Publish\API\Repository\FieldTypeService
+ * @group field-type
+ */
+class FieldTypeServiceTest extends \eZ\Publish\API\Repository\Tests\BaseTest
+{
+    /**
+     * Test for the getFieldTypes() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\FieldTypeService::getFieldTypes()
+     *
+     */
+    public function testGetFieldTypes()
+    {
+        throw new \RuntimeException( '@TODO: Implement.' );
+    }
+
+    /**
+     * Test for the getFieldType() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\FieldTypeService::getFieldType()
+     *
+     */
+    public function testGetFieldType()
+    {
+        throw new \RuntimeException( '@TODO: Implement.' );
+    }
+
+    /**
+     * Test for the getFieldType() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\FieldTypeService::getFieldType()
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException
+     */
+    public function testGetFieldTypeThrowsNotFoundException()
+    {
+        throw new \RuntimeException( '@TODO: Implement.' );
+    }
+
+    /**
+     * Test for the hasFieldType() method.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\FieldTypeService::hasFieldType()
+     *
+     */
+    public function testHasFieldType()
+    {
+        throw new \RuntimeException( '@TODO: Implement.' );
+    }
+}
