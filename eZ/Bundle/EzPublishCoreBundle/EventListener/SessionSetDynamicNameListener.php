e850801c87366a33f8e909b47d0c35bdc6488a3c
Damien Pobel <dp@ez.no>
2012-07-23 14:55:49 +0200

    Implemented dynamic name of the session based on the siteaccess name

---
+++ eZ/Bundle/EzPublishCoreBundle/EventListener/SessionSetDynamicNameListener.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * File containing the SessionSetDynamicNameListener class.
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Bundle\EzPublishCoreBundle\EventListener;
+
+use eZ\Publish\MVC\MVCEvents,
+    eZ\Publish\MVC\Event\PostSiteAccessMatchEvent,
+    Symfony\Component\EventDispatcher\EventSubscriberInterface,
+    Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * SiteAccess match listener.
+ *
+ * Allows to set a dynamic session name based on the siteaccess name.
+ */
+class SessionSetDynamicNameListener implements EventSubscriberInterface
+{
+    const MARKER = "{siteaccess_hash}";
+
+    /**
+     * @var \Symfony\Component\DependencyInjection\ContainerInterface
+     */
+    private $container;
+
+    public function __construct( ContainerInterface $container )
+    {
+        $this->container = $container;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            MVCEvents::SITEACCESS => array( 'onSiteAccessMatch', 250 )
+        );
+    }
+
+    public function onSiteAccessMatch( PostSiteAccessMatchEvent $event )
+    {
+        $siteAccess = $event->getSiteAccess();
+        $session = $this->container->get( 'session' );
+        $sessionName = $session->getName();
+        if ( strpos( $sessionName, self::MARKER ) !== false )
+        {
+            $session->setName(
+                str_replace(
+                    self::MARKER, md5( $siteAccess->name ), $sessionName
+                )
+            );
+        }
+    }
+}
