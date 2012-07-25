564015727d7f4d717baba271c8a1ebea5ea6d467
Kore Nordmann <mail@kore-nordmann.de>
2012-07-25 11:05:55 +0200

    Tested: Integer field type

---
+++ eZ/Publish/SPI/Tests/FieldType/IntegerIntegrationTest.php
@@ -0,0 +1,166 @@
+<?php
+/**
+ * File contains: eZ\Publish\Core\Persistence\Legacy\Tests\HandlerTest class
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\SPI\Tests\FieldType;
+use eZ\Publish\Core\Persistence\Legacy,
+    eZ\Publish\Core\FieldType,
+    eZ\Publish\SPI\Persistence\Content,
+    eZ\Publish\SPI\Persistence\Content\Field,
+    eZ\Publish\SPI\Persistence\Content\FieldTypeConstraints;
+
+/**
+ * Integration test for legacy storage field types
+ *
+ * This abstract base test case is supposed to be the base for field type
+ * integration tests. It basically calls all involved methods in the field type 
+ * ``Converter`` and ``Storage`` implementations. Fo get it working implement
+ * the abstract methods in a sensible way.
+ *
+ * The following actions are performed by this test using the custom field
+ * type:
+ *
+ * - Create a new content type with the given field type
+ * - Load create content type
+ * - Create content object of new content type
+ * - Load created content
+ * - Copy created content
+ * - Remove copied content
+ *
+ * @group integration
+ */
+class IntegerIntergrationTest extends BaseIntegrationTest
+{
+    /**
+     * Get name of tested field tyoe
+     *
+     * @return string
+     */
+    public function getTypeName()
+    {
+        return 'ezint';
+    }
+
+    /**
+     * Get handler with required custom field types registered
+     *
+     * @return Handler
+     */
+    public function getCustomHandler()
+    {
+        $handler = $this->getHandler();
+
+        $handler->getStorageRegistry()->register(
+            'ezint',
+            new FieldType\NullStorage()
+        );
+        $handler->getFieldValueConverterRegistry()->register(
+            'ezint',
+            new Legacy\Content\FieldValue\Converter\Integer()
+        );
+
+        return $handler;
+    }
+
+    /**
+     * Returns the FieldTypeConstraints to be used to create a field definition
+     * of the FieldType under test.
+     *
+     * @return \eZ\Publish\SPI\Persistence\Content\FieldTypeConstraints
+     */
+    public function getTypeConstraints()
+    {
+        return new Content\FieldTypeConstraints();
+    }
+
+    /**
+     * Get field definition data values
+     *
+     * This is a PHPUnit data provider
+     *
+     * @return array
+     */
+    public function getFieldDefinitionData()
+    {
+        return array(
+            // The ezint field type does not have any special field definition
+            // properties
+            array( 'fieldType', 'ezint' ),
+            array( 'fieldTypeConstraints', new Content\FieldTypeConstraints( array(
+            ) ) ),
+        );
+    }
+
+    /**
+     * Get initial field value
+     *
+     * @return \eZ\Publish\SPI\Persistence\Content\FieldValue
+     */
+    public function getInitialValue()
+    {
+        return new Content\FieldValue( array(
+            'data'         => 42,
+            'externalData' => null,
+            'sortKey'      => 42,
+        ) );
+    }
+
+    /**
+     * Asserts that the loaded field data is correct
+     *
+     * Performs assertions on the loaded field, mainly checking that the
+     * $field->value->externalData is loaded correctly. If the loading of
+     * external data manipulates other aspects of $field, their correctness
+     * also needs to be asserted. Make sure you implement this method agnostic
+     * to the used SPI\Persistence implementation!
+     */
+    public function assertLoadedFieldDataCorrect( Field $field )
+    {
+        $this->assertEquals(
+            $this->getInitialValue(),
+            $field->value
+        );
+    }
+
+    /**
+     * Get update field value.
+     *
+     * Use to update the field
+     *
+     * @return \eZ\Publish\SPI\Persistence\Content\FieldValue
+     */
+    public function getUpdatedValue()
+    {
+        return new Content\FieldValue( array(
+            'data'         => 23,
+            'externalData' => null,
+            'sortKey'      => 23,
+        ) );
+    }
+
+    /**
+     * Asserts that the updated field data is loaded correct
+     *
+     * Performs assertions on the loaded field after it has been updated,
+     * mainly checking that the $field->value->externalData is loaded
+     * correctly. If the loading of external data manipulates other aspects of
+     * $field, their correctness also needs to be asserted. Make sure you
+     * implement this method agnostic to the used SPI\Persistence
+     * implementation!
+     *
+     * @return void
+     */
+    public function assertUpdatedFieldDataCorrect( Field $field )
+    {
+        $this->assertEquals(
+            $this->getUpdatedValue(),
+            $field->value
+        );
+    }
+}
+
