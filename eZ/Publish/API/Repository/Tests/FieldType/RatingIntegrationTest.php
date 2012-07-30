35a771224e514b0141f6ea264af155d6141daa65
Kore Nordmann <mail@kore-nordmann.de>
2012-07-30 09:12:47 +0200

    Tested: Rating field type (API)

---
+++ eZ/Publish/API/Repository/Tests/FieldType/RatingIntegrationTest.php
@@ -0,0 +1,296 @@
+<?php
+/**
+ * File contains: eZ\Publish\Core\Persistence\Legacy\Tests\RepositoryTest class
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\API\Repository\Tests\FieldType;
+use eZ\Publish\API\Repository,
+    eZ\Publish\Core\FieldType\Rating\Value as RatingValue,
+    eZ\Publish\API\Repository\Values\Content\Field;
+
+/**
+ * Integration test for use field type
+ *
+ * @group integration
+ * @group field-type
+ */
+class RatingFieldTypeIntergrationTest extends BaseIntegrationTest
+{
+    /**
+     * Get name of tested field tyoe
+     *
+     * @return string
+     */
+    public function getTypeName()
+    {
+        return 'ezsrrating';
+    }
+
+    /**
+     * Get expected settings schema
+     *
+     * @return array
+     */
+    public function getSettingsSchema()
+    {
+        return array();
+    }
+
+    /**
+     * Get a valid $fieldSettings value
+     *
+     * @return mixed
+     */
+    public function getValidFieldSettings()
+    {
+        return array();
+    }
+
+    /**
+     * Get $fieldSettings value not accepted by the field type
+     *
+     * @return mixed
+     */
+    public function getInvalidFieldSettings()
+    {
+        return array(
+            'somethingUnknown' => 0,
+        );
+    }
+
+    /**
+     * Get expected validator schema
+     *
+     * @return array
+     */
+    public function getValidatorSchema()
+    {
+        return array();
+    }
+
+    /**
+     * Get a valid $validatorConfiguration
+     *
+     * @return mixed
+     */
+    public function getValidValidatorConfiguration()
+    {
+        return array();
+    }
+
+    /**
+     * Get $validatorConfiguration not accepted by the field type
+     *
+     * @return mixed
+     */
+    public function getInvalidValidatorConfiguration()
+    {
+        return array(
+            'somethingUnknown' => 0,
+        );
+    }
+
+    /**
+     * Get initial field data for valid object creation
+     *
+     * @return mixed
+     */
+    public function getValidCreationFieldData()
+    {
+        return new RatingValue( false );
+    }
+
+    /**
+     * Asserts that the field data was loaded correctly.
+     *
+     * Asserts that the data provided by {@link getValidCreationFieldData()}
+     * was stored and loaded correctly.
+     *
+     * @param Field $field
+     * @return void
+     */
+    public function assertFieldDataLoadedCorrect( Field $field)
+    {
+        $this->assertInstanceOf(
+            'eZ\\Publish\\Core\\FieldType\\Rating\\Value',
+            $field->value
+        );
+
+        $expectedData = array(
+            'isDisabled' => false,
+        );
+        $this->assertPropertiesCorrect(
+            $expectedData,
+            $field->value
+        );
+    }
+
+    /**
+     * Get field data which will result in errors during creation
+     *
+     * This is a PHPUnit data provider.
+     *
+     * The returned records must contain of an error producing data value and
+     * the expected exception class (from the API or SPI, not implementation
+     * specific!) as the second element. For example:
+     *
+     * <code>
+     * array(
+     *      array(
+     *          new DoomedValue( true ),
+     *          'eZ\\Publish\\API\\Repository\\Exceptions\\ContentValidationException'
+     *      ),
+     *      // ...
+     * );
+     * </code>
+     *
+     * @return array[]
+     */
+    public function provideInvalidCreationFieldData()
+    {
+        $value = new RatingValue( true );
+        $value->isDisabled = 'foo';
+
+        return array(
+            array(
+                $value,
+                'eZ\\Publish\\Core\\Base\\Exceptions\\InvalidArgumentType',
+            ),
+        );
+    }
+
+    /**
+     * Get update field externals data
+     *
+     * @return array
+     */
+    public function getValidUpdateFieldData()
+    {
+        return new RatingValue( true );
+    }
+
+    /**
+     * Get externals updated field data values
+     *
+     * This is a PHPUnit data provider
+     *
+     * @return array
+     */
+    public function assertUpdatedFieldDataLoadedCorrect( Field $field )
+    {
+        $this->assertInstanceOf(
+            'eZ\\Publish\\Core\\FieldType\\Rating\\Value',
+            $field->value
+        );
+
+        $expectedData = array(
+            'isDisabled' => true,
+        );
+        $this->assertPropertiesCorrect(
+            $expectedData,
+            $field->value
+        );
+    }
+
+    /**
+     * Get field data which will result in errors during update
+     *
+     * This is a PHPUnit data provider.
+     *
+     * The returned records must contain of an error producing data value and
+     * the expected exception class (from the API or SPI, not implementation
+     * specific!) as the second element. For example:
+     *
+     * <code>
+     * array(
+     *      array(
+     *          new DoomedValue( true ),
+     *          'eZ\\Publish\\API\\Repository\\Exceptions\\ContentValidationException'
+     *      ),
+     *      // ...
+     * );
+     * </code>
+     *
+     * @return array[]
+     */
+    public function provideInvalidUpdateFieldData()
+    {
+        return $this->provideInvalidCreationFieldData();
+    }
+
+    /**
+     * Asserts the the field data was loaded correctly.
+     *
+     * Asserts that the data provided by {@link getValidCreationFieldData()}
+     * was copied and loaded correctly.
+     *
+     * @param Field $field
+     */
+    public function assertCopiedFieldDataLoadedCorrectly( Field $field )
+    {
+        $this->assertInstanceOf(
+            'eZ\\Publish\\Core\\FieldType\\Rating\\Value',
+            $field->value
+        );
+
+        $expectedData = array(
+            'isDisabled' => false,
+        );
+        $this->assertPropertiesCorrect(
+            $expectedData,
+            $field->value
+        );
+    }
+
+    /**
+     * Get data to test to hash method
+     *
+     * This is a PHPUnit data provider
+     *
+     * The returned records must have the the original value assigned to the
+     * first index and the expected hash result to the second. For example:
+     *
+     * <code>
+     * array(
+     *      array(
+     *          new MyValue( true ),
+     *          array( 'myValue' => true ),
+     *      ),
+     *      // ...
+     * );
+     * </code>
+     *
+     * @return array
+     */
+    public function provideToHashData()
+    {
+        return array(
+            array(
+                new RatingValue( false ),
+                0,
+            ),
+        );
+    }
+
+    /**
+     * Get expectations for the fromHash call on our field value
+     *
+     * This is a PHPUnit data provider
+     *
+     * @return array
+     */
+    public function provideFromHashData()
+    {
+        return array(
+            array(
+                1,
+                new RatingValue( true )
+            ),
+        );
+    }
+}
+
