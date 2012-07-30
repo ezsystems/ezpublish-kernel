a6149579cd2049911f793164c9ff8be4f534e8a6
Kore Nordmann <mail@kore-nordmann.de>
2012-07-30 09:27:54 +0200

    Tested: Integer field type (API)

---
+++ eZ/Publish/API/Repository/Tests/FieldType/IntegerIntegrationTest.php
@@ -0,0 +1,319 @@
+<?php
+/**
+ * File contains: eZ\Publish\Core\Persistence\Legacy\Tests\RepositoryTest class
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\API\Repository\Tests\FieldType;
+use eZ\Publish\API\Repository,
+    eZ\Publish\Core\FieldType\Integer\Value as IntegerValue,
+    eZ\Publish\API\Repository\Values\Content\Field;
+
+/**
+ * Integration test for use field type
+ *
+ * @group integration
+ * @group field-type
+ */
+class IntegerFieldTypeIntergrationTest extends BaseIntegrationTest
+{
+    /**
+     * Get name of tested field tyoe
+     *
+     * @return string
+     */
+    public function getTypeName()
+    {
+        return 'ezinteger';
+    }
+
+    /**
+     * Get expected settings schema
+     *
+     * @return array
+     */
+    public function getSettingsSchema()
+    {
+        return array();
+    }
+
+    /**
+     * Get a valid $fieldSettings value
+     *
+     * @return mixed
+     */
+    public function getValidFieldSettings()
+    {
+        return array();
+    }
+
+    /**
+     * Get $fieldSettings value not accepted by the field type
+     *
+     * @return mixed
+     */
+    public function getInvalidFieldSettings()
+    {
+        return array(
+            'somethingUnknown' => 0,
+        );
+    }
+
+    /**
+     * Get expected validator schema
+     *
+     * @return array
+     */
+    public function getValidatorSchema()
+    {
+        return array(
+            'IntegerValueValidator' => array(
+                'minIntegerValue' => array(
+                    'type'    => 'int',
+                    'default' => null,
+                ),
+                'maxIntegerValue' => array(
+                    'type'    => 'int',
+                    'default' => null,
+                ),
+            )
+        );
+    }
+
+    /**
+     * Get a valid $validatorConfiguration
+     *
+     * @return mixed
+     */
+    public function getValidValidatorConfiguration()
+    {
+        return array(
+            'IntegerValueValidator' => array(
+                'minIntegerValue' => 23,
+                'maxIntegerValue' => 42,
+            )
+        );
+    }
+
+    /**
+     * Get $validatorConfiguration not accepted by the field type
+     *
+     * @return mixed
+     */
+    public function getInvalidValidatorConfiguration()
+    {
+        return array(
+            'IntegerValueValidator' => array(
+                'minStringLength' => new \stdClass(),
+            )
+        );
+    }
+
+    /**
+     * Get initial field data for valid object creation
+     *
+     * @return mixed
+     */
+    public function getValidCreationFieldData()
+    {
+        return new IntegerValue( 23 );
+    }
+
+    /**
+     * Asserts that the field data was loaded correctly.
+     *
+     * Asserts that the data provided by {@link getValidCreationFieldData()}
+     * was stored and loaded correctly.
+     *
+     * @param Field $field
+     * @return void
+     */
+    public function assertFieldDataLoadedCorrect( Field $field)
+    {
+        $this->assertInstanceOf(
+            'eZ\\Publish\\Core\\FieldType\\Integer\\Value',
+            $field->value
+        );
+
+        $expectedData = array(
+            'value' => 23,
+        );
+        $this->assertPropertiesCorrect(
+            $expectedData,
+            $field->value
+        );
+    }
+
+    /**
+     * Get field data which will result in errors during creation
+     *
+     * This is a PHPUnit data provider.
+     *
+     * The returned records must contain of an error producing data value and
+     * the expected exception class (from the API or SPI, not implementation
+     * specific!) as the second element. For example:
+     *
+     * <code>
+     * array(
+     *      array(
+     *          new DoomedValue( true ),
+     *          'eZ\\Publish\\API\\Repository\\Exceptions\\ContentValidationException'
+     *      ),
+     *      // ...
+     * );
+     * </code>
+     *
+     * @return array[]
+     */
+    public function provideInvalidCreationFieldData()
+    {
+        return array(
+            array(
+                new IntegerValue( new \stdClass() ),
+                'eZ\\Publish\\Core\\Base\\Exceptions\\InvalidArgumentType',
+            ),
+            array(
+                new IntegerValue( 5 ),
+                'eZ\\Publish\\Core\\Base\\Exceptions\\ContentFieldValidationException',
+            ),
+            array(
+                new IntegerValue( 127 ),
+                'eZ\\Publish\\Core\\Base\\Exceptions\\ContentFieldValidationException',
+            ),
+        );
+    }
+
+    /**
+     * Get update field externals data
+     *
+     * @return array
+     */
+    public function getValidUpdateFieldData()
+    {
+        return new IntegerValue( 42 );
+    }
+
+    /**
+     * Get externals updated field data values
+     *
+     * This is a PHPUnit data provider
+     *
+     * @return array
+     */
+    public function assertUpdatedFieldDataLoadedCorrect( Field $field )
+    {
+        $this->assertInstanceOf(
+            'eZ\\Publish\\Core\\FieldType\\Integer\\Value',
+            $field->value
+        );
+
+        $expectedData = array(
+            'value' => 42,
+        );
+        $this->assertPropertiesCorrect(
+            $expectedData,
+            $field->value
+        );
+    }
+
+    /**
+     * Get field data which will result in errors during update
+     *
+     * This is a PHPUnit data provider.
+     *
+     * The returned records must contain of an error producing data value and
+     * the expected exception class (from the API or SPI, not implementation
+     * specific!) as the second element. For example:
+     *
+     * <code>
+     * array(
+     *      array(
+     *          new DoomedValue( true ),
+     *          'eZ\\Publish\\API\\Repository\\Exceptions\\ContentValidationException'
+     *      ),
+     *      // ...
+     * );
+     * </code>
+     *
+     * @return array[]
+     */
+    public function provideInvalidUpdateFieldData()
+    {
+        return $this->provideInvalidCreationFieldData();
+    }
+
+    /**
+     * Asserts the the field data was loaded correctly.
+     *
+     * Asserts that the data provided by {@link getValidCreationFieldData()}
+     * was copied and loaded correctly.
+     *
+     * @param Field $field
+     */
+    public function assertCopiedFieldDataLoadedCorrectly( Field $field )
+    {
+        $this->assertInstanceOf(
+            'eZ\\Publish\\Core\\FieldType\\Integer\\Value',
+            $field->value
+        );
+
+        $expectedData = array(
+            'value' => 23,
+        );
+        $this->assertPropertiesCorrect(
+            $expectedData,
+            $field->value
+        );
+    }
+
+    /**
+     * Get data to test to hash method
+     *
+     * This is a PHPUnit data provider
+     *
+     * The returned records must have the the original value assigned to the
+     * first index and the expected hash result to the second. For example:
+     *
+     * <code>
+     * array(
+     *      array(
+     *          new MyValue( true ),
+     *          array( 'myValue' => true ),
+     *      ),
+     *      // ...
+     * );
+     * </code>
+     *
+     * @return array
+     */
+    public function provideToHashData()
+    {
+        return array(
+            array(
+                new IntegerValue( 23 ),
+                23,
+            ),
+        );
+    }
+
+    /**
+     * Get expectations for the fromHash call on our field value
+     *
+     * This is a PHPUnit data provider
+     *
+     * @return array
+     */
+    public function provideFromHashData()
+    {
+        return array(
+            array(
+                42,
+                new IntegerValue( 42 )
+            ),
+        );
+    }
+}
+
