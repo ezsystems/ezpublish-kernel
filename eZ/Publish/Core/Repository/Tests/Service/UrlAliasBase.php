02e9b7c236464463b4e6230122d03376cf45fdda
Petar Spanja <petar.spanja@gmail.com>
2012-07-30 11:12:17 +0200

    Tested service lookup method

--- eZ/Publish/Core/Repository/Tests/Service/UrlAliasBase.php
+++ eZ/Publish/Core/Repository/Tests/Service/UrlAliasBase.php
@@ -9,12 +9,49 @@
 
 namespace eZ\Publish\Core\Repository\Tests\Service;
 
-use eZ\Publish\Core\Repository\Tests\Service\Base as BaseServiceTest;
+use eZ\Publish\Core\Repository\Tests\Service\Base as BaseServiceTest,
+    eZ\Publish\API\Repository\Values\Content\UrlAlias,
+    eZ\Publish\SPI\Persistence\Content\UrlAlias as SPIUrlAlias;
 
 /**
  * Test case for UrlAlias Service
  */
 abstract class UrlAliasBase extends BaseServiceTest
 {
+    /**
+     * Test for the lookup() method.
+     *
+     * @covers \eZ\Publish\Core\Repository\ContentService::lookup
+     */
+    public function testLookup()
+    {
+        $urlAliasService = $this->repository->getURLAliasService();
+
+        $urlAlias = $urlAliasService->lookup( "eZ-Publish", "eng-US" );
+
+        self::assertEquals(
+            new UrlAlias(
+                array(
+                    "id" => "0-10e4c3cb527fb9963258469986c16240",
+                    "type" => UrlAlias::LOCATION,
+                    "destination" => "59",
+                    "path" => "eZ-Publish",
+                    "languageCodes" => array( "eng-US", "eng-GB" ),
+                    "alwaysAvailable" => true,
+                    "isHistory" => false,
+                    "isCustom" => false,
+                    "forward" => false
+                )
+            ),
+            $urlAlias
+        );
+    }
+
+
+
+
+
+
+
 
 }
