8a7b24ffd659e10118a02e1848df79a487370bea
André R <ar@ez.no>
2012-08-04 13:36:47 +0200

    Implement transaction support in InMemory\Backend

--- eZ/Publish/Core/Persistence/InMemory/Handler.php
+++ eZ/Publish/Core/Persistence/InMemory/Handler.php
@@ -133,24 +133,38 @@ class Handler implements HandlerInterface
     }
 
     /**
+     * Begin transaction
+     *
+     * Begins an transaction, make sure you'll call commit or rollback when done,
+     * otherwise work will be lost.
      */
     public function beginTransaction()
     {
-        //throw new \eZ\Publish\API\Repository\Exceptions\NotImplementedException( __METHOD__ );
+        $this->backend->beginTransaction();
     }
 
     /**
+     * Commit transaction
+     *
+     * Commit transaction, or throw exceptions if no transactions has been started.
+     *
+     * @throws \RuntimeException If no transaction has been started
      */
     public function commit()
     {
-        //throw new \eZ\Publish\API\Repository\Exceptions\NotImplementedException( __METHOD__ );
+        $this->backend->commit();
     }
 
     /**
+     * Rollback transaction
+     *
+     * Rollback transaction, or throw exceptions if no transactions has been started.
+     *
+     * @throws \RuntimeException If no transaction has been started
      */
     public function rollback()
     {
-        //throw new \eZ\Publish\API\Repository\Exceptions\NotImplementedException( __METHOD__ );
+        $this->backend->rollback();
     }
 
     /**
