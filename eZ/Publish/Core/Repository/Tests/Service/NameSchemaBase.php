1be5d2f65c1351b13bfb93efb3e9ea068bf44f87
Petar Spanja <petar.spanja@gmail.com>
2012-08-06 14:32:34 +0200

    Added: empty test suite for NameSchema service

---
+++ eZ/Publish/Core/Repository/Tests/Service/NameSchemaBase.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * File contains: eZ\Publish\Core\Repository\Tests\Service\NameSchemaBase class
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\Core\Repository\Tests\Service;
+use eZ\Publish\Core\Repository\Tests\Service\Base as BaseServiceTest;
+
+/**
+ * Test case for NameSchema service
+ */
+abstract class NameSchemaBase extends BaseServiceTest
+{
+    // @todo implement tests
+}
