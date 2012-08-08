fc27289f24daeca97de9fadeeb905f96a5c30c42
André R <ar@ez.no>
2012-08-08 13:49:38 +0200

    Add mention that LimitationType->valueSchema() might return Traversable instance instead of native php array in case of hash

--- eZ/Publish/SPI/Limitation/Type.php
+++ eZ/Publish/SPI/Limitation/Type.php
@@ -78,6 +78,7 @@ interface Type
      *
      * @return mixed[]|int In case of array, a hash with key as valid limitations value and value as human readable name
      *                     of that option, in case of int on of VALUE_SCHEMA_* constants.
+     *                     Note: The hash might be an instance of Traversable, and not a native php array.
      */
     public function valueSchema( Repository $repository );
 }
