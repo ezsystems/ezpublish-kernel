58c9d149dbf139f05dd5c4c3abefcc836c19f156
Petar Spanja <petar.spanja@gmail.com>
2012-08-09 20:31:57 +0200

    Added fixtures for testing cleanup methods

---
+++ eZ/Publish/Core/Persistence/Legacy/Tests/Content/UrlAlias/Gateway/_fixtures/urlaliases_relink.php
@@ -0,0 +1,59 @@
+<?php
+
+return array (
+    'ezurlalias_ml' => array (
+        0 => array (
+            'action' => 'eznode:2',
+            'action_type' => 'eznode',
+            'alias_redirects' => '1',
+            'id' => '1',
+            'is_alias' => '0',
+            'is_original' => '1',
+            'lang_mask' => '3',
+            'link' => '1',
+            'parent' => '0',
+            'text' => '',
+            'text_md5' => 'd41d8cd98f00b204e9800998ecf8427e',
+        ),
+        1 => array (
+            'action' => 'eznode:314',
+            'action_type' => 'eznode',
+            'alias_redirects' => '1',
+            'id' => '2',
+            'is_alias' => '0',
+            'is_original' => '0',
+            'lang_mask' => '2',
+            'link' => '2',
+            'parent' => '0',
+            'text' => 'history',
+            'text_md5' => '3cd15f8f2940aff879df34df4e5c2cd1',
+        ),
+        2 => array (
+            'action' => 'eznode:315',
+            'action_type' => 'eznode',
+            'alias_redirects' => '1',
+            'id' => '3',
+            'is_alias' => '0',
+            'is_original' => '0',
+            'lang_mask' => '2',
+            'link' => '3',
+            'parent' => '0',
+            'text' => 'reused-history',
+            'text_md5' => '51e775a611265b7b0cde62a413c91cdc',
+        ),
+    ),
+    'ezurlalias_ml_incr' => array (
+        0 => array (
+            'id' => '1',
+        ),
+        1 => array (
+            'id' => '2',
+        ),
+        2 => array (
+            'id' => '3',
+        ),
+        3 => array (
+            'id' => '4',
+        ),
+    ),
+);
