9dc6fe0ed03d76754ed3142201ec688beba2b6fc
Patrick Allaert <pa@ez.no>
2012-08-10 12:56:00 +0200

    Fixed: copy/paste doc issue

--- eZ/Publish/SPI/Tests/FieldType/FloatIntegrationTest.php
+++ eZ/Publish/SPI/Tests/FieldType/FloatIntegrationTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * File contains: eZ\Publish\Core\Persistence\Legacy\Tests\HandlerTest class
+ * File contains: eZ\Publish\SPI\Tests\FieldType\FloatIntegrationTest class
  *
  * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
