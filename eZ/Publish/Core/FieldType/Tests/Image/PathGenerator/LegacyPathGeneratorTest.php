da1db5748f363ff13f2ca7ca8073af350575f81d
Tobias Schlitt <toby@php.net>
2012-08-12 13:30:29 +0200

    Implemented: Unit tests for LegacyPathGenerator.

---
+++ eZ/Publish/Core/FieldType/Tests/Image/PathGenerator/LegacyPathGeneratorTest.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * File containing the LegacyPathGeneratorTest class.
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\Core\FieldType\Tests\Image;
+use eZ\Publish\Core\FieldType\Image\PathGenerator\LegacyPathGenerator;
+
+/**
+ * @group fieldType
+ * @group ezimage
+ */
+class LegacyPathGeneratorTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @param mixed $data
+     * @param mixed $expectedPath
+     * @return void
+     * @dataProvider provideStoragePathForFieldData
+     */
+    public function testGetStoragePathForField( $data, $expectedPath )
+    {
+        $pathGenerator = new LegacyPathGenerator();
+
+        $this->assertEquals(
+            $expectedPath,
+            $pathGenerator->getStoragePathForField(
+                $data['fieldId'],
+                $data['versionNo'],
+                $data['languageCode'],
+                $data['nodePathString']
+            )
+        );
+    }
+
+    public function provideStoragePathForFieldData()
+    {
+        return array(
+            array(
+                array(
+                    'fieldId' => 23,
+                    'versionNo' => 1,
+                    'languageCode' => 'eng-US',
+                    'nodePathString' => 'sindelfingen/bielefeld/',
+                ),
+                'sindelfingen/bielefeld/23-1-eng-US',
+            ),
+            array(
+                array(
+                    'fieldId' => 23,
+                    'versionNo' => 42,
+                    'languageCode' => 'ger-DE',
+                    'nodePathString' => 'sindelfingen/',
+                ),
+                'sindelfingen/23-42-ger-DE',
+            ),
+            array(
+                array(
+                    'fieldId' => 23,
+                    'versionNo' => 2,
+                    'languageCode' => 'eng-GB',
+                    'nodePathString' => null,
+                ),
+                '23-2-eng-GB',
+            ),
+        );
+    }
+}
