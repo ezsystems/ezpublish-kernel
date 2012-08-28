62241ba5d26cc47932ae43b0e8dd9f8791346ee2
Tobias Schlitt <toby@php.net>
2012-08-28 15:45:50 +0200

    Updated: Migrated FloatTest to standardized field type test.

--- eZ/Publish/Core/FieldType/Tests/FloatTest.php
+++ eZ/Publish/Core/FieldType/Tests/FloatTest.php
@@ -16,116 +16,256 @@ use eZ\Publish\Core\FieldType\Float\Type as Float,
  * @group fieldType
  * @group ezfloat
  */
-class FloatTest extends FieldTypeTest
+class FloatTest extends StandardizedFieldTypeTest
 {
     /**
-     * @covers \eZ\Publish\Core\FieldType\FieldType::getValidatorConfigurationSchema
+     * Returns the field type under test.
+     *
+     * This method is used by all test cases to retrieve the field type under
+     * test. Just create the FieldType instance using mocks from the provided
+     * get*Mock() methods and/or custom get*Mock() implementations. You MUST
+     * NOT take care for test case wide caching of the field type, just return
+     * a new instance from this method!
+     *
+     * @return FieldType
      */
-    public function testValidatorConfigurationSchema()
+    protected function createFieldTypeUnderTest()
     {
-        $ft = new Float( $this->validatorService, $this->fieldTypeTools );
-        self::assertSame(
-            array(
-                "FloatValueValidator" => array(
-                    "minFloatValue" => array(
-                        "type" => "float",
-                        "default" => false
-                    ),
-                    "maxFloatValue" => array(
-                        "type" => "float",
-                        "default" => false
-                    )
+        return new Float(
+            // TODO: Get rid of these
+            $this->validatorService,
+            $this->fieldTypeTools
+        );
+    }
+
+    /**
+     * Returns the validator configuration schema expected from the field type.
+     *
+     * @return array
+     */
+    protected function getValidatorConfigurationSchemaExpectation()
+    {
+        return array(
+            "FloatValueValidator" => array(
+                "minFloatValue" => array(
+                    "type" => "float",
+                    "default" => false
+                ),
+                "maxFloatValue" => array(
+                    "type" => "float",
+                    "default" => false
                 )
+            )
+        );
+    }
+
+    /**
+     * Returns the settings schema expected from the field type.
+     *
+     * @return array
+     */
+    protected function getSettingsSchemaExpectation()
+    {
+        return array();
+    }
+
+    /**
+     * Returns the empty value expected from the field type.
+     *
+     * @return void
+     */
+    protected function getEmptyValueExpectation()
+    {
+        return null;
+    }
+
+    /**
+     * Data provider for invalid input to acceptValue().
+     *
+     * Returns an array of data provider sets with 2 arguments: 1. The invalid
+     * input to acceptValue(), 2. The expected exception type as a string. For
+     * example:
+     *
+     * <code>
+     *  return array(
+     *      array(
+     *          new \stdClass(),
+     *          'eZ\\Publish\\Core\\Base\\Exceptions\\InvalidArgumentException',
+     *      ),
+     *      array(
+     *          array(),
+     *          'eZ\\Publish\\Core\\Base\\Exceptions\\InvalidArgumentException',
+     *      ),
+     *      // ...
+     *  );
+     * </code>
+     *
+     * @return array
+     */
+    public function provideInvalidInputForAcceptValue()
+    {
+        return array(
+            array(
+                'foo',
+                'eZ\\Publish\\Core\\Base\\Exceptions\\InvalidArgumentException',
             ),
-            $ft->getValidatorConfigurationSchema(),
-            "The set of allowed validators does not match what is expected."
+            array(
+                array(),
+                'eZ\\Publish\\Core\\Base\\Exceptions\\InvalidArgumentException',
+            ),
+            array(
+                new FloatValue( 'foo' ),
+                'eZ\\Publish\\Core\\Base\\Exceptions\\InvalidArgumentException',
+            ),
         );
     }
 
     /**
-     * @covers \eZ\Publish\Core\FieldType\FieldType::getSettingsSchema
+     * Data provider for valid input to acceptValue().
+     *
+     * Returns an array of data provider sets with 2 arguments: 1. The valid
+     * input to acceptValue(), 2. The expected return value from acceptValue().
+     * For example:
+     *
+     * <code>
+     *  return array(
+     *      array(
+     *          null,
+     *          null
+     *      ),
+     *      array(
+     *          __FILE__,
+     *          new BinaryFileValue( array(
+     *              'path' => __FILE__,
+     *              'fileName' => basename( __FILE__ ),
+     *              'fileSize' => filesize( __FILE__ ),
+     *              'downloadCount' => 0,
+     *              'mimeType' => 'text/plain',
+     *          ) )
+     *      ),
+     *      // ...
+     *  );
+     * </code>
+     *
+     * @return array
      */
-    public function testCheckboxSettingsSchema()
+    public function provideValidInputForAcceptValue()
     {
-        $ft = new Float( $this->validatorService, $this->fieldTypeTools );
-        self::assertSame(
-            array(),
-            $ft->getSettingsSchema(),
-            "The settings schema does not match what is expected."
+        return array(
+            array(
+                null,
+                null,
+            ),
+            array(
+                42.23,
+                new FloatValue( 42.23 ),
+            ),
+            array(
+                23,
+                new FloatValue( 23. ),
+            ),
+            array(
+                new FloatValue( 23.42 ),
+                new FloatValue( 23.42 ),
+            ),
         );
     }
 
     /**
-     * @covers \eZ\Publish\Core\FieldType\Float\Type::acceptValue
-     * @expectedException \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException
+     * Provide input for the toHash() method
+     *
+     * Returns an array of data provider sets with 2 arguments: 1. The valid
+     * input to toHash(), 2. The expected return value from toHash().
+     * For example:
+     *
+     * <code>
+     *  return array(
+     *      array(
+     *          null,
+     *          null
+     *      ),
+     *      array(
+     *          new BinaryFileValue( array(
+     *              'path' => 'some/file/here',
+     *              'fileName' => 'sindelfingen.jpg',
+     *              'fileSize' => 2342,
+     *              'downloadCount' => 0,
+     *              'mimeType' => 'image/jpeg',
+     *          ) ),
+     *          array(
+     *              'path' => 'some/file/here',
+     *              'fileName' => 'sindelfingen.jpg',
+     *              'fileSize' => 2342,
+     *              'downloadCount' => 0,
+     *              'mimeType' => 'image/jpeg',
+     *          )
+     *      ),
+     *      // ...
+     *  );
+     * </code>
+     *
+     * @return array
      */
-    public function testAcceptValueInvalidFormat()
+    public function provideInputForToHash()
     {
-        $ft = new Float( $this->validatorService, $this->fieldTypeTools );;
-        $ref = new ReflectionObject( $ft );
-        $refMethod = $ref->getMethod( "acceptValue" );
-        $refMethod->setAccessible( true );
-        $refMethod->invoke( $ft, new FloatValue( "Strings should not work." ) );
+        return array(
+            array(
+                null,
+                null,
+            ),
+            array(
+                new FloatValue( 23.42 ),
+                23.42,
+            ),
+        );
     }
 
     /**
-     * @covers \eZ\Publish\Core\FieldType\Float\Type::acceptValue
+     * Provide input to fromHash() method
+     *
+     * Returns an array of data provider sets with 2 arguments: 1. The valid
+     * input to fromHash(), 2. The expected return value from fromHash().
+     * For example:
+     *
+     * <code>
+     *  return array(
+     *      array(
+     *          null,
+     *          null
+     *      ),
+     *      array(
+     *          array(
+     *              'path' => 'some/file/here',
+     *              'fileName' => 'sindelfingen.jpg',
+     *              'fileSize' => 2342,
+     *              'downloadCount' => 0,
+     *              'mimeType' => 'image/jpeg',
+     *          ),
+     *          new BinaryFileValue( array(
+     *              'path' => 'some/file/here',
+     *              'fileName' => 'sindelfingen.jpg',
+     *              'fileSize' => 2342,
+     *              'downloadCount' => 0,
+     *              'mimeType' => 'image/jpeg',
+     *          ) )
+     *      ),
+     *      // ...
+     *  );
+     * </code>
+     *
+     * @return array
      */
-    public function testAcceptValueValidFormat()
+    public function provideInputForFromHash()
     {
-        $ft = new Float( $this->validatorService, $this->fieldTypeTools );
-        $ref = new ReflectionObject( $ft );
-        $refMethod = $ref->getMethod( "acceptValue" );
-        $refMethod->setAccessible( true );
-
-        $value = new FloatValue( 42.42 );
-        self::assertSame( $value, $refMethod->invoke( $ft, $value ) );
-    }
-
-    /**
-     * @covers \eZ\Publish\Core\FieldType\Float\Type::toPersistenceValue
-     */
-    public function testToPersistenceValue()
-    {
-        $ft = new Float( $this->validatorService, $this->fieldTypeTools );
-        $fieldValue = $ft->toPersistenceValue( new FloatValue( 42.42 ) );
-
-        self::assertSame( 42.42, $fieldValue->data );
-        self::assertSame( false, $fieldValue->sortKey );
-    }
-
-    /**
-     * @covers \eZ\Publish\Core\FieldType\Float\Value::__construct
-     */
-    public function testBuildFieldValueWithParam()
-    {
-        $value = new FloatValue( 420.42 );
-        self::assertSame( 420.42, $value->value );
-    }
-
-    /**
-     * @covers \eZ\Publish\Core\FieldType\Float\Value::__construct
-     */
-    public function testBuildFieldValueWithoutParam()
-    {
-        $value = new FloatValue;
-        self::assertSame( 0.0, $value->value );
-    }
-
-    /**
-     * @covers \eZ\Publish\Core\FieldType\Float\Value::__toString
-     */
-    public function testFieldValueToString()
-    {
-        $float = "4200.42";
-        $value = new FloatValue( $float );
-        self::assertSame( $float, (string)$value );
-
-        $value2 = new FloatValue( (string)$value );
-        self::assertSame(
-            $float,
-            $value2->value,
-            "fromString() and __toString() must be compatible"
+        return array(
+            array(
+                null,
+                null,
+            ),
+            array(
+                23.42,
+                new FloatValue( 23.42 ),
+            ),
         );
     }
 }
