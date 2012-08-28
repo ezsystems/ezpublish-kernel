eaaeb6891f0fed7bbd340719211f051349d64e5e
Jérôme Vieilledent <lolautruche@gmail.com>
2012-08-28 16:32:52 +0200

    Added override of Symfony default router in order to implement RequestMatcherInterface

---
+++ eZ/Bundle/EzPublishCoreBundle/Routing/DefaultRouter.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * File containing the DefaultRouter class.
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Bundle\EzPublishCoreBundle\Routing;
+
+use Symfony\Bundle\FrameworkBundle\Routing\Router,
+    Symfony\Component\Routing\Matcher\RequestMatcherInterface,
+    Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Extension of Symfony default router implementing RequestMatcherInterface
+ */
+class DefaultRouter extends Router implements RequestMatcherInterface
+{
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request The request to match
+     *
+     * @return array An array of parameters
+     *
+     * @throws ResourceNotFoundException If no matching resource could be found
+     * @throws MethodNotAllowedException If a matching resource was found but the request method is not allowed
+     */
+    public function matchRequest( Request $request )
+    {
+        if ( $request->attributes->has( 'semanticPathinfo' ) )
+        {
+            return $this->match( $request->attributes->get( 'semanticPathinfo' ) );
+        }
+
+        return $this->match( $request->getPathInfo() );
+    }
+}
