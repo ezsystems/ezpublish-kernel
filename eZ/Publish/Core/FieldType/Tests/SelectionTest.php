89047d9fa0c727c909b826efd69c7ae601ce9982
Tobias Schlitt <toby@php.net>
2012-08-29 11:08:10 +0200

    Updated: Migrated SelectionTest to use standardized field type test.

--- eZ/Publish/Core/FieldType/Tests/SelectionTest.php
+++ eZ/Publish/Core/FieldType/Tests/SelectionTest.php
@@ -16,109 +16,249 @@ use eZ\Publish\Core\FieldType\Selection\Type as Selection,
  * @group fieldType
  * @group ezselection
  */
-class SelectionTest extends FieldTypeTest
+class SelectionTest extends StandardizedFieldTypeTest
 {
     /**
-     * @covers \eZ\Publish\Core\FieldType\FieldType::getValidatorConfigurationSchema
+     * Returns the field type under test.
+     *
+     * This method is used by all test cases to retrieve the field type under
+     * test. Just create the FieldType instance using mocks from the provided
+     * get*Mock() methods and/or custom get*Mock() implementations. You MUST
+     * NOT take care for test case wide caching of the field type, just return
+     * a new instance from this method!
+     *
+     * @return FieldType
      */
-    public function testValidatorConfigurationSchema()
+    protected function createFieldTypeUnderTest()
     {
-        $ft = new Selection( $this->validatorService, $this->fieldTypeTools );
-        self::assertEmpty(
-            $ft->getValidatorConfigurationSchema(),
-            "The validator configuration schema does not match what is expected."
+        return new Selection(
+            $this->getValidatorServiceMock(),
+            $this->getFieldTypeToolsMock()
         );
     }
 
     /**
-     * @covers \eZ\Publish\Core\FieldType\Selection\Type::acceptValue
-     * @expectedException \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException
+     * Returns the validator configuration schema expected from the field type.
+     *
+     * @return array
      */
-    public function testAcceptValueInvalidFormat()
+    protected function getValidatorConfigurationSchemaExpectation()
     {
-        $ft = new Selection( $this->validatorService, $this->fieldTypeTools );
-        $invalidValue = new SelectionValue;
-        $invalidValue->selection = "This should be an array instead!";
-        $ref = new ReflectionObject( $ft );
-        $refMethod = $ref->getMethod( "acceptValue" );
-        $refMethod->setAccessible( true );
-        $refMethod->invoke( $ft, $invalidValue );
+        return array();
     }
 
     /**
-     * @covers \eZ\Publish\Core\FieldType\Selection\Type::acceptValue
+     * Returns the settings schema expected from the field type.
+     *
+     * @return array
      */
-    public function testAcceptValueValidStringFormat()
+    protected function getSettingsSchemaExpectation()
     {
-        $ft = new Selection( $this->validatorService, $this->fieldTypeTools );
-        $ref = new ReflectionObject( $ft );
-        $refMethod = $ref->getMethod( "acceptValue" );
-        $refMethod->setAccessible( true );
-
-        $value = new SelectionValue( array( "Choice1" ) );
-        self::assertSame( $value, $refMethod->invoke( $ft, $value ) );
+       return array(
+            'isMultiple' => array(
+                'type' => 'bool',
+                'default' => false,
+            ),
+            'options' => array(
+                'type' => 'hash',
+                'default' => array(),
+            ),
+        );
     }
 
     /**
-     * @covers \eZ\Publish\Core\FieldType\Selection\Type::acceptValue
+     * Returns the empty value expected from the field type.
+     *
+     * @return void
      */
-    public function testAcceptValueValidArrayFormat()
+    protected function getEmptyValueExpectation()
     {
-        $ft = new Selection( $this->validatorService, $this->fieldTypeTools );
-        $ref = new ReflectionObject( $ft );
-        $refMethod = $ref->getMethod( "acceptValue" );
-        $refMethod->setAccessible( true );
-
-        $value = new SelectionValue( array( "Choice1", "Choice2" ) );
-        self::assertSame( $value, $refMethod->invoke( $ft, $value ) );
+        return new SelectionValue();
     }
 
     /**
-     * @covers \eZ\Publish\Core\FieldType\Selection\Type::toPersistenceValue
+     * Data provider for invalid input to acceptValue().
+     *
+     * Returns an array of data provider sets with 2 arguments: 1. The invalid
+     * input to acceptValue(), 2. The expected exception type as a string. For
+     * example:
+     *
+     * <code>
+     *  return array(
+     *      array(
+     *          new \stdClass(),
+     *          'eZ\\Publish\\Core\\Base\\Exceptions\\InvalidArgumentException',
+     *      ),
+     *      array(
+     *          array(),
+     *          'eZ\\Publish\\Core\\Base\\Exceptions\\InvalidArgumentException',
+     *      ),
+     *      // ...
+     *  );
+     * </code>
+     *
+     * @return array
      */
-    public function testToPersistenceValue()
+    public function provideInvalidInputForAcceptValue()
     {
-        $string = "Choice1";
-        $ft = new Selection( $this->validatorService, $this->fieldTypeTools );
-        $fieldValue = $ft->toPersistenceValue( new SelectionValue( (array)$string ) );
-
-        self::assertSame( array( $string ), $fieldValue->data );
-        self::assertSame( $string, $fieldValue->sortKey );
+        return array(
+            array(
+                23,
+                'eZ\\Publish\\Core\\Base\\Exceptions\\InvalidArgumentException',
+            ),
+            array(
+                'sindelfingen',
+                'eZ\\Publish\\Core\\Base\\Exceptions\\InvalidArgumentException',
+            ),
+        );
     }
 
     /**
-     * @covers \eZ\Publish\Core\FieldType\Selection\Value::__construct
+     * Data provider for valid input to acceptValue().
+     *
+     * Returns an array of data provider sets with 2 arguments: 1. The valid
+     * input to acceptValue(), 2. The expected return value from acceptValue().
+     * For example:
+     *
+     * <code>
+     *  return array(
+     *      array(
+     *          null,
+     *          null
+     *      ),
+     *      array(
+     *          __FILE__,
+     *          new BinaryFileValue( array(
+     *              'path' => __FILE__,
+     *              'fileName' => basename( __FILE__ ),
+     *              'fileSize' => filesize( __FILE__ ),
+     *              'downloadCount' => 0,
+     *              'mimeType' => 'text/plain',
+     *          ) )
+     *      ),
+     *      // ...
+     *  );
+     * </code>
+     *
+     * @return array
      */
-    public function testBuildFieldValueWithParam()
+    public function provideValidInputForAcceptValue()
     {
-        $selection = array( "Choice1", "Choice2", "Choice3" );
-        $value = new SelectionValue( $selection );
-        self::assertSame( $selection, $value->selection );
+        return array(
+            array(
+                array(),
+                new SelectionValue(),
+            ),
+            array(
+                array( 23 ),
+                new SelectionValue( array( 23 )  ),
+            ),
+            array(
+                array( 23, 42 ),
+                new SelectionValue( array( 23, 42 ) ),
+            ),
+            array(
+                new SelectionValue( array( 23, 42 ) ),
+                new SelectionValue( array( 23, 42 ) ),
+            ),
+        );
     }
 
     /**
-     * @covers \eZ\Publish\Core\FieldType\Selection\Value::__construct
+     * Provide input for the toHash() method
+     *
+     * Returns an array of data provider sets with 2 arguments: 1. The valid
+     * input to toHash(), 2. The expected return value from toHash().
+     * For example:
+     *
+     * <code>
+     *  return array(
+     *      array(
+     *          null,
+     *          null
+     *      ),
+     *      array(
+     *          new BinaryFileValue( array(
+     *              'path' => 'some/file/here',
+     *              'fileName' => 'sindelfingen.jpg',
+     *              'fileSize' => 2342,
+     *              'downloadCount' => 0,
+     *              'mimeType' => 'image/jpeg',
+     *          ) ),
+     *          array(
+     *              'path' => 'some/file/here',
+     *              'fileName' => 'sindelfingen.jpg',
+     *              'fileSize' => 2342,
+     *              'downloadCount' => 0,
+     *              'mimeType' => 'image/jpeg',
+     *          )
+     *      ),
+     *      // ...
+     *  );
+     * </code>
+     *
+     * @return array
      */
-    public function testBuildFieldValueWithoutParam()
+    public function provideInputForToHash()
     {
-        $value = new SelectionValue;
-        self::assertSame( array(), $value->selection );
+        return array(
+            array(
+                new SelectionValue(),
+                array(),
+            ),
+            array(
+                new SelectionValue( array( 23, 42 ) ),
+                array( 23, 42 ),
+            ),
+        );
     }
 
     /**
-     * @covers \eZ\Publish\Core\FieldType\Selection\Value::__toString
+     * Provide input to fromHash() method
+     *
+     * Returns an array of data provider sets with 2 arguments: 1. The valid
+     * input to fromHash(), 2. The expected return value from fromHash().
+     * For example:
+     *
+     * <code>
+     *  return array(
+     *      array(
+     *          null,
+     *          null
+     *      ),
+     *      array(
+     *          array(
+     *              'path' => 'some/file/here',
+     *              'fileName' => 'sindelfingen.jpg',
+     *              'fileSize' => 2342,
+     *              'downloadCount' => 0,
+     *              'mimeType' => 'image/jpeg',
+     *          ),
+     *          new BinaryFileValue( array(
+     *              'path' => 'some/file/here',
+     *              'fileName' => 'sindelfingen.jpg',
+     *              'fileSize' => 2342,
+     *              'downloadCount' => 0,
+     *              'mimeType' => 'image/jpeg',
+     *          ) )
+     *      ),
+     *      // ...
+     *  );
+     * </code>
+     *
+     * @return array
      */
-    public function testFieldValueToString()
+    public function provideInputForFromHash()
     {
-        $string = "Choice3";
-        $value = new SelectionValue( array( $string ) );
-        self::assertSame( $string, (string)$value );
-
-        $value2 = new SelectionValue( explode( ',', (string)$value ) );
-        self::assertSame(
-            (array)$string,
-            $value2->selection,
-            "fromString() and __toString() must be compatible"
+        return array(
+            array(
+                array(),
+                new SelectionValue(),
+            ),
+            array(
+                array( 23, 42 ),
+                new SelectionValue( array( 23, 42 ) ),
+            ),
         );
     }
 }
