ezsystems/ezpublish-kernel

commit ca09586a5e82ac5f1fd667367b0b19f21379eceb

Updated: Make Search\TransformationParser use $install_dir to load rules.

eZ/Publish/Core/Persistence/Legacy/Content/Search/TransformationParser.php
@@ -54,12 +54,22 @@ class TransformationParser
     protected $tokenSpecifications = null;
 
     /**
+     * Directory to load rules relative from.
+     *
+     * @var string
+     */
+    protected $installDir;
+
+    /**
      * Construct
      *
+     * @param string $installDir Base dir for rule loading
      * @return void
      */
-    public function __construct()
+    public function __construct( $installDir )
     {
+        $this->installDir = $installDir;
+
         $character = '(?:U\\+[0-9a-fA-F]{4}|remove|keep|[0-9a-fA-F]+|"(?:[^\\\\"]+|\\\\\\\\|\\\\\'|\\\\")*?")';
 
         $this->tokenSpecifications = array(
@@ -90,7 +100,9 @@ class TransformationParser
      */
     public function parse( $file )
     {
-        return $this->parseString( file_get_contents( $file ) );
+        return $this->parseString( file_get_contents(
+            $this->installDir . '/' . $file
+        ) );
     }
 
     /**

eZ/Publish/Core/settings/service.ini
@@ -253,6 +253,7 @@ arguments[rules][]=eZ/Publish/Core/Persistence/Legacy/Tests/Content/SearchHandle
 
 [legacy_transformation_parser]
 class=eZ\Publish\Core\Persistence\Legacy\Content\Search\TransformationParser
+arguments[installDir]=$install_dir
 
 [legacy_transformation_pcre_compiler]
 class=eZ\Publish\Core\Persistence\Legacy\Content\Search\TransformationPcreCompiler

commit a4568a0835b77ba1d87e8dfbb5c661b3d0b1d518

Fix TrsfomationParser failures caused by ca09586a5e82ac5f1fd667367b0b19f21379eceb

eZ/Publish/Core/Persistence/Legacy/Tests/Content/SearchHandler/TransformationParserTest.php
@@ -32,12 +32,12 @@ class TransformationParserTest extends TestCase
      */
     public function testParse( $file )
     {
-        $parser = new Search\TransformationParser();
+        $parser = new Search\TransformationParser( self::getInstallationDir() );
 
         $fixture = include $file . '.result';
         $this->assertEquals(
             $fixture,
-            $parser->parse( $file )
+            $parser->parse( str_replace( self::getInstallationDir(), '', $file ) )
         );
     }
 }

eZ/Publish/Core/Persistence/Legacy/Tests/Content/SearchHandler/TransformationPcreCompilerTest.php
@@ -38,7 +38,7 @@ class TransformationPcreCompilerTest extends TestCase
 
     public function testCompileMap()
     {
-        $parser = new Search\TransformationParser();
+        $parser = new Search\TransformationParser( self::getInstallationDir() );
         $compiler = new Search\TransformationPcreCompiler( new Search\Utf8Converter() );
 
         $rules = $compiler->compile(
@@ -56,7 +56,7 @@ class TransformationPcreCompilerTest extends TestCase
 
     public function testCompileMapRemove()
     {
-        $parser = new Search\TransformationParser();
+        $parser = new Search\TransformationParser( self::getInstallationDir() );
         $compiler = new Search\TransformationPcreCompiler( new Search\Utf8Converter() );
 
         $rules = $compiler->compile(
@@ -74,7 +74,7 @@ class TransformationPcreCompilerTest extends TestCase
 
     public function testCompileMapKeep()
     {
-        $parser = new Search\TransformationParser();
+        $parser = new Search\TransformationParser( self::getInstallationDir() );
         $compiler = new Search\TransformationPcreCompiler( new Search\Utf8Converter() );
 
         $rules = $compiler->compile(
@@ -92,7 +92,7 @@ class TransformationPcreCompilerTest extends TestCase
 
     public function testCompileMapAscii()
     {
-        $parser = new Search\TransformationParser();
+        $parser = new Search\TransformationParser( self::getInstallationDir() );
         $compiler = new Search\TransformationPcreCompiler( new Search\Utf8Converter() );
 
         $rules = $compiler->compile(
@@ -110,7 +110,7 @@ class TransformationPcreCompilerTest extends TestCase
 
     public function testCompileMapUnicode()
     {
-        $parser = new Search\TransformationParser();
+        $parser = new Search\TransformationParser( self::getInstallationDir() );
         $compiler = new Search\TransformationPcreCompiler( new Search\Utf8Converter() );
 
         $rules = $compiler->compile(
@@ -128,7 +128,7 @@ class TransformationPcreCompilerTest extends TestCase
 
     public function testCompileReplace()
     {
-        $parser = new Search\TransformationParser();
+        $parser = new Search\TransformationParser( self::getInstallationDir() );
         $compiler = new Search\TransformationPcreCompiler( new Search\Utf8Converter() );
 
         $rules = $compiler->compile(
@@ -146,7 +146,7 @@ class TransformationPcreCompilerTest extends TestCase
 
     public function testCompileTranspose()
     {
-        $parser = new Search\TransformationParser();
+        $parser = new Search\TransformationParser( self::getInstallationDir() );
         $compiler = new Search\TransformationPcreCompiler( new Search\Utf8Converter() );
 
         $rules = $compiler->compile(
@@ -164,7 +164,7 @@ class TransformationPcreCompilerTest extends TestCase
 
     public function testCompileTransposeAsciiLowercase()
     {
-        $parser = new Search\TransformationParser();
+        $parser = new Search\TransformationParser( self::getInstallationDir() );
         $compiler = new Search\TransformationPcreCompiler( new Search\Utf8Converter() );
 
         $rules = $compiler->compile(
@@ -182,7 +182,7 @@ class TransformationPcreCompilerTest extends TestCase
 
     public function testCompileTransposePlus()
     {
-        $parser = new Search\TransformationParser();
+        $parser = new Search\TransformationParser( self::getInstallationDir() );
         $compiler = new Search\TransformationPcreCompiler( new Search\Utf8Converter() );
 
         $rules = $compiler->compile(
@@ -200,7 +200,7 @@ class TransformationPcreCompilerTest extends TestCase
 
     public function testCompileModuloTranspose()
     {
-        $parser = new Search\TransformationParser();
+        $parser = new Search\TransformationParser( self::getInstallationDir() );
         $compiler = new Search\TransformationPcreCompiler( new Search\Utf8Converter() );
 
         $rules = $compiler->compile(

eZ/Publish/Core/Persistence/Legacy/Tests/Content/SearchHandler/TransformationProcessorTest.php
@@ -19,13 +19,13 @@ class TransformationProcessorTest extends TestCase
     public function getProcessor()
     {
         $processor = new Search\TransformationProcessor(
-            new Search\TransformationParser(),
+            new Search\TransformationParser( self::getInstallationDir() ),
             new Search\TransformationPcreCompiler( new Search\Utf8Converter() )
         );
 
         foreach ( glob( __DIR__ . '/_fixtures/transformations/*.tr' ) as $file )
         {
-            $processor->loadRules( $file );
+            $processor->loadRules( str_replace( self::getInstallationDir(), '', $file ) );
         }
         return $processor;
     }

eZ/Publish/Core/Persistence/Legacy/Tests/Content/SearchHandlerSortTest.php
@@ -75,7 +75,7 @@ class SearchHandlerSortTest extends LanguageAwareTestCase
     protected function getContentSearchHandler( array $fullTextSearchConfiguration = array() )
     {
         $processor = new Content\Search\TransformationProcessor(
-            new Content\Search\TransformationParser(),
+            new Content\Search\TransformationParser( self::getInstallationDir() ),
             new Content\Search\TransformationPcreCompiler(
                 new Content\Search\Utf8Converter()
             )
@@ -83,7 +83,7 @@ class SearchHandlerSortTest extends LanguageAwareTestCase
 
         foreach ( glob( __DIR__ . '/SearchHandler/_fixtures/transformations/*.tr' ) as $file )
         {
-            $processor->loadRules( $file );
+            $processor->loadRules( str_replace( self::getInstallationDir(), '', $file ) );
         }
 
         $db = $this->getDatabaseHandler();

eZ/Publish/Core/Persistence/Legacy/Tests/Content/SearchHandlerTest.php
@@ -77,7 +77,7 @@ class SearchHandlerTest extends LanguageAwareTestCase
     protected function getContentSearchHandler( array $fullTextSearchConfiguration = array() )
     {
         $processor = new Content\Search\TransformationProcessor(
-            new Content\Search\TransformationParser(),
+            new Content\Search\TransformationParser( self::getInstallationDir() ),
             new Content\Search\TransformationPcreCompiler(
                 new Content\Search\Utf8Converter()
             )
@@ -85,7 +85,7 @@ class SearchHandlerTest extends LanguageAwareTestCase
 
         foreach ( glob( __DIR__ . '/SearchHandler/_fixtures/transformations/*.tr' ) as $file )
         {
-            $processor->loadRules( $file );
+            $processor->loadRules( str_replace( self::getInstallationDir(), '', $file ) );
         }
 
         return new Content\Search\Handler(

eZ/Publish/Core/Persistence/Legacy/Tests/TestCase.php
@@ -336,4 +336,19 @@ abstract class TestCase extends \PHPUnit_Framework_TestCase
             $refl->getProperties( ReflectionProperty::IS_PUBLIC )
         );
     }
+
+    /**
+     * @static
+     * @return string
+     */
+    static protected function getInstallationDir()
+    {
+        static $installDir = null;
+        if ($installDir === null)
+        {
+            $config = require 'config.php';
+            $installDir = $config['service']['parameters']['install_dir'];
+        }
+        return $installDir;
+    }
 }