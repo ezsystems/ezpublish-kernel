f42cb320e3000d1b83a88be00ba650120bfd59b9
Jérôme Vieilledent <lolautruche@gmail.com>
2012-09-03 18:08:33 +0200

    Fixed phpDoc

--- eZ/Publish/Core/MVC/Symfony/View/Tests/ContentViewProvider/Configured/Matcher/Id/ContentTest.php
+++ eZ/Publish/Core/MVC/Symfony/View/Tests/ContentViewProvider/Configured/Matcher/Id/ContentTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * File containing the LocationTest class.
+ * File containing the ContentTest class.
  *
  * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
