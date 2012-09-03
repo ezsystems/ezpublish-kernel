466399a63934bf452010182377c43a470da385d7
Tobias Schlitt <toby@php.net>
2012-09-03 21:19:53 +0200

    Implemented: FieldTypeService and FieldType for REST client.

---
+++ eZ/Publish/Core/REST/Client/FieldTypeService.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * File containing the FieldTypeService class
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\Core\REST\Client;
+use eZ\Publish\API\Repository;
+
+class FieldTypeService implements Repository\FieldTypeService
+{
+    /**
+     * FieldTypes by identifier
+     *
+     * @var eZ\Publish\Core\REST\Client\FieldType[] $fieldTypes
+     */
+    protected $fieldTypes = array();
+
+    /**
+     * @param eZ\Publish\Core\REST\Client\FieldType[] $fieldTypes
+     * @return void
+     */
+    public function __construct( array $fieldTypes = array() )
+    {
+        foreach ( $fieldTypes as $fieldType )
+        {
+            $this->addFieldType( $fieldType );
+        }
+    }
+
+    /**
+     * Adds the given $fieldType
+     *
+     * Note, this is not an API method and not meant to be used directly!
+     *
+     * @param FieldType $fieldType
+     * @return void
+     * @access protected
+     */
+    public function addFieldType( FieldType $fieldType )
+    {
+        $this->fieldTypes[$fieldType->getFieldTypeIdentifier()] = $fieldType;
+    }
+
+    /**
+     * Returns a list of all field types.
+     *
+     * @return \eZ\Publish\API\Repository\FieldType[]
+     */
+    public function getFieldTypes()
+    {
+        return array_values( $this->fieldTypes );
+    }
+
+    /**
+     * Returns the FieldType registered with the given identifier
+     *
+     * @param string $identifier
+     * @return \eZ\Publish\API\Repository\FieldType
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException
+     *         if there is no FieldType registered with $identifier
+     */
+    public function getFieldType( $identifier )
+    {
+        if ( $this->hasFieldType( $identifier ) )
+        {
+            return $this->fieldTypes[$identifier];
+        }
+
+        throw new Exceptions\NotFoundException(
+            sprintf(
+                'FieldType with identifier "%s" not found.',
+                $identifier
+            )
+        );
+    }
+
+    /**
+     * Returns if there is a FieldType registered under $identifier
+     *
+     * @param string $identifier
+     * @return bool
+     */
+    public function hasFieldType( $identifier )
+    {
+        return isset( $this->fieldTypes[$identifier] );
+    }
+}
---
+++ eZ/Publish/Core/REST/Client/Tests/FieldTypeServiceTest.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * File containing the FieldTypeServiceTest class
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\Core\REST\Client\Tests;
+
+use eZ\Publish\Core\REST\Client\FieldTypeService;
+
+class FieldTypeServiceTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHasFieldType()
+    {
+        $fieldTypeService = $this->getFieldTypeService();
+
+        $this->assertFalse(
+            $fieldTypeService->hasFieldType( 'my-type' )
+        );
+    }
+
+    public function testAddFieldType()
+    {
+        $fieldTypeMock = $this->getMock(
+            'eZ\\Publish\\Core\\REST\\Client\\FieldType',
+            array(),
+            array(),
+            '',
+            false
+        );
+        $fieldTypeMock->expects( $this->once() )
+            ->method( 'getFieldTypeIdentifier' )
+            ->will( $this->returnValue( 'my-type' ) );
+
+        $fieldTypeService = $this->getFieldTypeService();
+
+        $fieldTypeService->addFieldType( $fieldTypeMock );
+
+        $this->assertTrue(
+            $fieldTypeService->hasFieldType( 'my-type' )
+        );
+    }
+
+    public function testGetFieldType()
+    {
+        $fieldTypeMock = $this->getMock(
+            'eZ\\Publish\\Core\\REST\\Client\\FieldType',
+            array(),
+            array(),
+            '',
+            false
+        );
+        $fieldTypeMock->expects( $this->once() )
+            ->method( 'getFieldTypeIdentifier' )
+            ->will( $this->returnValue( 'my-type' ) );
+
+        $fieldTypeService = $this->getFieldTypeService();
+
+        $fieldTypeService->addFieldType( $fieldTypeMock );
+
+        $this->assertSame(
+            $fieldTypeMock,
+            $fieldTypeService->getFieldType( 'my-type' )
+        );
+    }
+
+    /**
+     * @expectedException eZ\Publish\API\Repository\Exceptions\NotFoundException
+     */
+    public function testGetFieldTypeThrowsNotFoundException()
+    {
+        $fieldTypeService = $this->getFieldTypeService();
+
+        $fieldTypeService->getFieldType( 'my-type' );
+    }
+
+    public function testGetFieldTypes()
+    {
+        $myFieldTypeMock = $this->getMock(
+            'eZ\\Publish\\Core\\REST\\Client\\FieldType',
+            array(),
+            array(),
+            '',
+            false
+        );
+        $myFieldTypeMock->expects( $this->once() )
+            ->method( 'getFieldTypeIdentifier' )
+            ->will( $this->returnValue( 'my-type' ) );
+
+        $yourFieldTypeMock = $this->getMock(
+            'eZ\\Publish\\Core\\REST\\Client\\FieldType',
+            array(),
+            array(),
+            '',
+            false
+        );
+        $yourFieldTypeMock->expects( $this->once() )
+            ->method( 'getFieldTypeIdentifier' )
+            ->will( $this->returnValue( 'your-type' ) );
+
+        $fieldTypeService = $this->getFieldTypeService();
+
+        $fieldTypeService->addFieldType( $myFieldTypeMock );
+        $fieldTypeService->addFieldType( $yourFieldTypeMock );
+
+        $this->assertEquals(
+            2,
+            count( $fieldTypeService->getFieldTypes() )
+        );
+    }
+
+    public function getFieldTypeService()
+    {
+        return new FieldTypeService();
+    }
+}
