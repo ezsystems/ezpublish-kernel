6f0dae74a56c113411d10cef5ec001f4ca6f5692
Tobias Schlitt <toby@php.net>
2012-09-04 11:20:05 +0200

    Implemented: Simple object cache to be used in REST SDK.

---
+++ eZ/Publish/Core/REST/Client/ObjectCache.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * File containing the SimpleObjectCache class
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\Core\REST\Client;
+use eZ\Publish\API\Repository\Values\ValueObject;
+
+/**
+ * Simple cache for value objects during a single request
+ */
+class ObjectCache
+{
+    /**
+     * Map of cached objects by their key
+     *
+     * @var array
+     */
+    protected $cachedObjects = array();
+
+    /**
+     * Stores $data under $key
+     *
+     * @param string $key
+     * @param ValueObject $data
+     * @return void
+     */
+    public function store( $key, ValueObject $data )
+    {
+        $this->cachedObjects[$key] = $data;
+    }
+
+    /**
+     * Restores data stored under $key, returns null if $key is not found
+     *
+     * @param string $key
+     * @return ValueObject|null
+     */
+    public function restore( $key )
+    {
+        if ( isset( $this->cachedObjects[$key] ) )
+        {
+            return $this->cachedObjects[$key];
+        }
+        return null;
+    }
+
+    /**
+     * Clears the data stored in $key
+     *
+     * @param string $key
+     * @return void
+     */
+    public function clear( $key )
+    {
+        unset( $this->cachedObjects[$key] );
+    }
+
+    /**
+     * Clears all cached items
+     *
+     * @return void
+     */
+    public function clearAll()
+    {
+        $this->cachedObjects = array();
+    }
+}
---
+++ eZ/Publish/Core/REST/Client/Tests/ObjectCacheTest.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * File containing the ObjectCacheTest class
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\Core\REST\Client\Tests;
+
+use eZ\Publish\Core\REST\Client\ObjectCache;
+use eZ\Publish\API\Repository\Values\ValueObject;
+
+class TestValueObject extends ValueObject {}
+
+class ObjectCacheTest extends \PHPUnit_Framework_TestCase
+{
+    public function testStoreRestore()
+    {
+        $cache = $this->getCache();
+
+        $object = new TestValueObject();
+
+        $cache->store( 'some-key', $object );
+
+        $this->assertSame(
+            $object,
+            $cache->restore( 'some-key' )
+        );
+    }
+
+    public function testStoreOverwrite()
+    {
+        $cache = $this->getCache();
+
+        $firstObject = new TestValueObject();
+        $secondObject = new TestValueObject();
+
+        $cache->store( 'some-key', $firstObject );
+        $cache->store( 'some-key', $secondObject );
+
+        $this->assertSame(
+            $secondObject,
+            $cache->restore( 'some-key' )
+        );
+        $this->assertNotSame(
+            $firstObject,
+            $cache->restore( 'some-key' )
+        );
+    }
+
+    public function testRestoreNotAvailable()
+    {
+        $cache = $this->getCache();
+
+        $this->assertNull( $cache->restore( 'non-existent' ) );
+    }
+
+    public function testClear()
+    {
+        $cache = $this->getCache();
+
+        $object = new TestValueObject();
+
+        $cache->store( 'some-key', $object );
+        $cache->clear( 'some-key' );
+
+        $this->assertNull( $cache->restore( 'some-key' ) );
+    }
+
+    public function testClearAll()
+    {
+        $cache = $this->getCache();
+
+        $object = new TestValueObject();
+
+        $cache->store( 'some-key', $object );
+        $cache->store( 'other-key', $object );
+
+        $this->assertNull( $cache->restore( 'some-key' ) );
+        $this->assertNull( $cache->restore( 'other-key' ) );
+    }
+
+    protected function getCache()
+    {
+        return new ObjectCache();
+    }
+}
