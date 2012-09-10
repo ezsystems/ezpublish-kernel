2bf41d7975ec17a18b5a6fc9f3e6612d661cb97b
Tobias Schlitt <toby@php.net>
2012-09-10 16:32:31 +0200

    Implemented: FieldDefinitionList struct in REST Client.

---
+++ eZ/Publish/Core/REST/Client/Values/FieldDefinitionList.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * File containing the FieldDefinitionList class
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\Core\REST\Client\Values;
+
+/**
+ * FieldDefinitonList
+ */
+class FieldDefinitionList
+{
+    /**
+     * References to contained field references
+     *
+     * @var string[]
+     */
+    protected $fieldDefinitionReferences;
+
+    /**
+     * Content type service
+     *
+     * @var ContentTypeService
+     */
+    protected $contentTypeService;
+
+    /**
+     * @param eZ\Publish\Core\REST\ContentTypeService $contentTypeService
+     * @param string[] $fieldDefinitionReferences
+     */
+    public function __construct( ContentTypeService $contentTypeService, array $fieldDefinitionReferences )
+    {
+        $this->contentTypeService = $contentTypeService;
+        $this->fieldDefinitionReferences;
+    }
+
+    /**
+     * Fetches and returnd the field definitions contained in the list
+     *
+     * @return FieldDefinition[]
+     */
+    public function getFieldDefinitions()
+    {
+        $fieldDefinitions = array();
+        foreach ( $this->fieldDefinitionReferences as $reference )
+        {
+            $fieldDefinitions[] = $this->contentTypeService->loadFieldDefinition( $reference );
+        }
+        return $fieldDefinitions;
+    }
+}
