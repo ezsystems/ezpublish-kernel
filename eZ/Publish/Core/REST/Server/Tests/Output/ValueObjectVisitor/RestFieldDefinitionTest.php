544f8e030ce482f9f7aac065326ef5411f88a7eb
Tobias Schlitt <toby@php.net>
2012-09-10 12:24:41 +0200

    Implemented: Rest server FieldDefinition visitor.

---
+++ eZ/Publish/Core/REST/Server/Tests/Output/ValueObjectVisitor/RestFieldDefinitionTest.php
@@ -0,0 +1,152 @@
+<?php
+/**
+ * File containing a test class
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\Core\REST\Server\Tests\Output\ValueObjectVisitor;
+use eZ\Publish\Core\REST\Common\Tests\Output\ValueObjectVisitorBaseTest;
+use eZ\Publish\Core\REST\Server\Values\RestFieldDefinition;
+
+use eZ\Publish\Core\REST\Server\Output\ValueObjectVisitor;
+use eZ\Publish\Core\REST\Server;
+use eZ\Publish\Core\REST\Common;
+
+use eZ\Publish\Core\Repository\Values;
+
+class RestFieldDefinitionTest extends ValueObjectVisitorBaseTest
+{
+    protected $fieldValueSerializerMock;
+
+    public function setUp()
+    {
+        $this->fieldValueSerializerMock = $this->getMock(
+            'eZ\\Publish\\Core\\REST\\Common\\Output\\FieldValueSerializer',
+            array(),
+            array(),
+            '',
+            false
+        );
+    }
+
+    /**
+     * @return DOMDocument
+     */
+    public function testVisitRestFieldDefinition()
+    {
+        $visitor   = $this->getContentVisitor();
+        $generator = $this->getGenerator();
+
+        $generator->startDocument( null );
+
+        $restFieldDefinition = $this->getBasicRestFieldDefinition();
+
+        $this->fieldValueSerializerMock->expects( $this->once() )
+            ->method( 'serializeFieldDefaultValue' )
+            ->with(
+                $this->isInstanceOf( 'eZ\\Publish\\Core\\REST\\Common\\Output\\Generator' ),
+                $this->isInstanceOf( 'eZ\\Publish\\API\\Repository\\Values\\ContentType\\FieldDefinition' ),
+                $this->equalTo(
+                    'my default value text'
+                )
+            );
+
+        $visitor->visit(
+            $this->getVisitorMock(),
+            $generator,
+            $restFieldDefinition
+        );
+
+        $result = $generator->endDocument( null );
+
+        $this->assertNotNull( $result );
+
+        $dom = new \DOMDocument();
+        $dom->loadXml( $result );
+
+        return $dom;
+    }
+
+    protected function getBasicRestFieldDefinition()
+    {
+        return new Server\Values\RestFieldDefinition(
+            new Values\ContentType\ContentType( array(
+                'id' => 'contentTypeId',
+                'status' => Values\ContentType\ContentType::STATUS_DEFINED,
+                'fieldDefinitions' => array(),
+            ) ),
+            new Values\ContentType\FieldDefinition( array(
+                'id' => 'fieldDefinitionId_23',
+                'fieldSettings' => array( 'setting' => 'foo' ),
+                'validatorConfiguration' => array( 'validator' => 'bar' ),
+                'identifier' => 'title',
+                'fieldGroup' => 'abstract-field-group',
+                'position' => 2,
+                'fieldTypeIdentifier' => 'my-field-type',
+                'isTranslatable' => true,
+                'isRequired' => false,
+                'isSearchable' => true,
+                'isInfoCollector' => false,
+                'defaultValue' => 'my default value text',
+                'names' => array( 'eng-US' => 'Sindelfingen' ),
+                'descriptions' => array( 'eng-GB' => 'Bielefeld' ),
+            ) )
+        );
+    }
+
+    public function provideXpathAssertions()
+    {
+        $xpathAssertions = array(
+            '/FieldDefinition[@href="/content/types/contentTypeId/fieldDefinitions/fieldDefinitionId_23"]',
+            '/FieldDefinition[@media-type="application/vnd.ez.api.FieldDefinition+xml"]',
+            '/FieldDefinition/id[text()="fieldDefinitionId_23"]',
+            '/FieldDefinition/identifier[text()="title"]',
+            '/FieldDefinition/fieldType[text()="my-field-type"]',
+            '/FieldDefinition/fieldGroup[text()="abstract-field-group"]',
+            '/FieldDefinition/position[text()="2"]',
+            '/FieldDefinition/isTranslatable[text()="true"]',
+            '/FieldDefinition/isRequired[text()="false"]',
+            '/FieldDefinition/isInfoCollector[text()="false"]',
+            '/FieldDefinition/isSearchable[text()="true"]',
+            '/FieldDefinition/names',
+            '/FieldDefinition/names/value[@languageCode="eng-US" and text()="Sindelfingen"]',
+            '/FieldDefinition/descriptions/value[@languageCode="eng-GB" and text()="Bielefeld"]',
+        );
+
+        return array_map(
+            function ( $xpath )
+            {
+                return array( $xpath );
+            },
+            $xpathAssertions
+        );
+    }
+
+    /**
+     * @param \DOMDocument $dom
+     * @return void
+     * @depends testVisitRestFieldDefinition
+     * @dataProvider provideXpathAssertions
+     */
+    public function testGeneratedXml( $xpath, \DOMDocument $dom )
+    {
+        $this->assertXPath( $dom, $xpath );
+    }
+
+    /**
+     * Get the Content visitor
+     *
+     * @return \eZ\Publish\Core\REST\Server\Output\ValueObjectVisitor\RestFieldDefinition
+     */
+    protected function getContentVisitor()
+    {
+        return new ValueObjectVisitor\RestFieldDefinition(
+            new Common\UrlHandler\eZPublish(),
+            $this->fieldValueSerializerMock
+
+        );
+    }
+}
