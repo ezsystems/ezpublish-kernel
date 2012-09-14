017773f39f54bfe3106508e9e87ef8296d8c459e
Tobias Schlitt <toby@php.net>
2012-09-14 08:47:18 +0200

    Implemented: Test for validateFieldSettings() in TextBlock FieldType.

--- eZ/Publish/Core/FieldType/Tests/TextBlockTest.php
+++ eZ/Publish/Core/FieldType/Tests/TextBlockTest.php
@@ -250,4 +250,80 @@ class TextBlockTest extends StandardizedFieldTypeTest
             ),
         );
     }
+
+    /**
+     * Provide data sets with field settings which are considered valid by the
+     * {@link validateFieldSettings()} method.
+     *
+     * Returns an array of data provider sets with a single argument: A valid
+     * set of field settings.
+     * For example:
+     *
+     * <code>
+     *  return array(
+     *      array(
+     *          array(),
+     *      ),
+     *      array(
+     *          array( 'rows' => 2 )
+     *      ),
+     *      // ...
+     *  );
+     * </code>
+     *
+     * @return array
+     */
+    public function provideValidFieldSettings()
+    {
+        return array(
+            array(
+                array()
+            ),
+            array(
+                array(
+                    'textRows' => 23,
+                )
+            ),
+        );
+    }
+
+    /**
+     * Provide data sets with field settings which are considered invalid by the
+     * {@link validateFieldSettings()} method. The method must return a
+     * non-empty array of validation error when receiving such field settings.
+     *
+     * Returns an array of data provider sets with a single argument: A valid
+     * set of field settings.
+     * For example:
+     *
+     * <code>
+     *  return array(
+     *      array(
+     *          true,
+     *      ),
+     *      array(
+     *          array( 'nonExistentKey' => 2 )
+     *      ),
+     *      // ...
+     *  );
+     * </code>
+     *
+     * @return array
+     */
+    public function provideInValidFieldSettings()
+    {
+        return array(
+            array(
+                array(
+                    'non-existent' => 'foo',
+                )
+            ),
+            array(
+                array(
+                    // textRows must be integer
+                    'textRows' => 'foo',
+                )
+            ),
+        );
+    }
 }
