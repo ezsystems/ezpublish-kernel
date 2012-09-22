40172187edf9117684ddac4814a5b89b22c7231c
André R <ar@ez.no>
2012-09-22 19:22:53 +0200

    Add --relative symlink and hardcopy options to ezpublish:legac:assets_install

--- eZ/Bundle/EzPublishLegacyBundle/Command/LegacyWrapperInstallCommand.php
+++ eZ/Bundle/EzPublishLegacyBundle/Command/LegacyWrapperInstallCommand.php
@@ -11,8 +11,10 @@ namespace eZ\Bundle\EzPublishLegacyBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
 use ezcPhpGenerator;
 use ezcPhpGeneratorParameter;
 
@@ -22,7 +24,11 @@ class LegacyWrapperInstallCommand extends ContainerAwareCommand
     {
         $this
             ->setName( 'ezpublish:legacy:assets_install' )
-            ->addArgument( 'webroot', InputArgument::OPTIONAL, 'The webroot directory (usually "web")', 'web' )
+            ->setDefinition(array(
+                new InputArgument('target', InputArgument::OPTIONAL, 'The target directory', 'web'),
+            ))
+            ->addOption('symlink', null, InputOption::VALUE_NONE, 'Symlinks the assets instead of copying it')
+            ->addOption('relative', null, InputOption::VALUE_NONE, 'Make relative symlinks')
             ->setDescription( 'Installs assets from eZ Publish legacy installation and wrapper scripts for front controllers (like index_cluster.php).' )
             ->setHelp( <<<EOT
 The command <info>%command.name%</info> installs <info>assets</info> from eZ Publish legacy installation
@@ -36,25 +42,48 @@ EOT
 
     protected function execute( InputInterface $input, OutputInterface $output )
     {
+        $targetArg = rtrim( $input->getArgument('target'), '/' );
+        if ( !is_dir($targetArg) )
+        {
+            throw new \InvalidArgumentException(sprintf('The target directory "%s" does not exist.', $input->getArgument('target')));
+        }
+        else if ( !function_exists('symlink') && $input->getOption('symlink') )
+        {
+            throw new \InvalidArgumentException('The symlink() function is not available on your system. You need to install the assets without the --symlink option.');
+        }
+
         /**
          * @var \Symfony\Component\Filesystem\Filesystem
          */
         $filesystem = $this->getContainer()->get( 'filesystem' );
-        $webroot = rtrim( $input->getArgument( 'webroot' ), '/' );
         $legacyRootDir = rtrim( $this->getContainer()->getParameter( 'ezpublish_legacy.root_dir' ), '/' );
 
-        $output->writeln( "Installing assets eZ Publish legacy, located at $legacyRootDir" );
+        $output->writeln( sprintf( "Installing eZ Publish legacy assets form $legacyRootDir using the <comment>%s</comment> option", $input->getOption('symlink') ? 'symlink' : 'hard copy' ) );
+
         foreach ( array( 'design', 'extension', 'share', 'var' ) as $folder )
         {
-            $webrootFolder = "$webroot/$folder";
-            $filesystem->remove( $webrootFolder );
-            $filesystem->symlink( "$legacyRootDir/$folder", $webrootFolder );
+            $targetDir = "$targetArg/$folder";
+            $originDir = "$legacyRootDir/$folder";
+            $filesystem->remove( $targetDir );
+            if ( $input->getOption('symlink') )
+            {
+                if ( $input->getOption('relative') )
+                    $originDir = $filesystem->makePathRelative( $originDir, realpath( $targetArg ) );
+
+                $filesystem->symlink( $originDir, $targetDir );
+            }
+            else
+            {
+                $filesystem->mkdir( $targetDir, 0777 );
+                // We use a custom iterator to ignore VCS files
+                $filesystem->mirror( $originDir, $targetDir, Finder::create()->in( $originDir ) );
+            }
         }
 
-        $output->writeln( "Installing wrappers for eZ Publish legacy front controllers" );
+        $output->writeln( "Installing wrappers for eZ Publish legacy front controllers (rest & cluster)" );
         foreach ( array( 'index_rest.php', 'index_cluster.php' ) as $frontController )
         {
-            $newFrontController = "$webroot/$frontController";
+            $newFrontController = "$targetArg/$frontController";
             $filesystem->remove( $newFrontController );
             $generator = new ezcPhpGenerator( $newFrontController, false );
             $generator->lineBreak = "\n";
