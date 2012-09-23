2df595854efad8d05b99f9471c8add13a8e29754
Tobias Schlitt <toby@php.net>
2012-09-23 10:56:35 +0200

    Updated: Removed requirement for DATABASE env var in start script.
    Database is taken from service.ini.

--- eZ/Publish/Core/REST/Server/startTestServer.sh
+++ eZ/Publish/Core/REST/Server/startTestServer.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-if [ -z "${DATABASE}" ];
-then
-	echo "Missing DATABASE environment variable. Please specify a DSN for a persistent database to make this server work.";
-	exit -1;
-fi
-
 /usr/bin/env php -S localhost:8042 index.php
