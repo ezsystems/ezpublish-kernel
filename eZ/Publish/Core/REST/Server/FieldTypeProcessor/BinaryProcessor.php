709c863238bc5efbb1d730f7d288388b0c24363f
Tobias Schlitt <toby@php.net>
2012-09-23 10:47:53 +0200

    Implemented: Field type processor for binary and media files.

---
+++ eZ/Publish/Core/REST/Server/FieldTypeProcessor/BinaryProcessor.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * File containing the ImageProcessor class.
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\Core\REST\Server\FieldTypeProcessor;
+
+use eZ\Publish\Core\REST\Common\FieldTypeProcessor;
+
+class BinaryProcessor extends BinaryInputProcessor
+{
+    /**
+     * Template for binary URLs
+     *
+     * The template may contain a "{path}" variable, which is replaced by the
+     * MD5 file name part of the binary path.
+     *
+     * @var string
+     */
+    protected $urlTemplate;
+
+    /**
+     * @param string $temporaryDirectory
+     * @param string $urlTemplate
+     */
+    public function __construct( $temporaryDirectory, $urlTemplate )
+    {
+        parent::__construct( $temporaryDirectory );
+        $this->urlTemplate = $urlTemplate;
+    }
+
+    /**
+     * Perform manipulations on an a generated $outgoingValueHash
+     *
+     * This method is called by the REST server to allow a field type to post
+     * process the given $outgoingValueHash, which was previously generated
+     * using {@link eZ\Publish\SPI\FieldType\FieldType::toHash()}, before it is
+     * sent to the client. The return value of this method replaces
+     * $outgoingValueHash and must obey to the same rules as the original
+     * $outgoingValueHash.
+     *
+     * @param mixed $outgoingValueHash
+     * @return mixed Post processed hash
+     */
+    public function postProcessHash( $outgoingValueHash )
+    {
+        $outgoingValueHash['url'] = $this->generateUrl(
+            $outgoingValueHash['path']
+        );
+        return $outgoingValueHash;
+    }
+
+    /**
+     * Generates a URL for $path
+     *
+     * @param string $path
+     * @return string
+     */
+    protected function generateUrl( $path )
+    {
+        if ( preg_match( '((?:^|/)([0-9a-f]+)(?:\.[a-z0-9]+)?)', $path, $matches ) )
+        {
+            $path = $matches[1];
+        }
+
+        return str_replace(
+            '{path}',
+            $path,
+            $this->urlTemplate
+        );
+    }
+}
---
+++ eZ/Publish/Core/REST/Server/Tests/FieldTypeProcessor/BinaryProcessorTest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * File containing the BinaryProcessorTest class
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\Core\REST\Server\Tests\FieldTypeProcessor;
+
+use eZ\Publish\Core\REST\Server\Tests\BaseTest;
+use eZ\Publish\Core\REST\Server\FieldTypeProcessor\BinaryProcessor;
+
+class BinaryProcessorTest extends BinaryInputProcessorTest
+{
+    public function testPostProcessHash()
+    {
+        $processor = $this->getProcessor();
+
+        $inputHash = array(
+            'path' => 'var/some_site/12ace8436c64ceb907536640b58788f0.pdf',
+        );
+
+        $outputHash = $processor->postProcessHash( $inputHash );
+
+        $this->assertEquals(
+            array(
+                'path' => 'var/some_site/12ace8436c64ceb907536640b58788f0.pdf',
+                'url' => 'http://example.com/binaries/12ace8436c64ceb907536640b58788f0',
+            ),
+            $outputHash
+        );
+    }
+
+    /**
+     * Returns the processor under test
+     *
+     * @return eZ\Publish\Core\REST\Server\FieldTypeProcessor\BinaryInputProcessor
+     */
+    protected function getProcessor()
+    {
+        return new BinaryProcessor(
+            $this->getTempDir(),
+            'http://example.com/binaries/{path}'
+        );
+    }
+}
