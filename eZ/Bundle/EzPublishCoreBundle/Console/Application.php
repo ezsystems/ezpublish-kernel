3058630c0ad33e0f16f8bcc7c623c74ff37966cb
Jérôme Vieilledent <lolautruche@gmail.com>
2012-09-27 23:27:39 +0200

    Added console application class for specific eZ Publish needs (such as siteaccess option). Fixes undefined 'ezpublish.siteaccess' service issue while running console

---
+++ eZ/Bundle/EzPublishCoreBundle/Console/Application.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * File containing the Application class.
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Bundle\EzPublishCoreBundle\Console;
+
+use eZ\Bundle\EzPublishCoreBundle\SiteAccess,
+    Symfony\Component\HttpKernel\KernelInterface,
+    Symfony\Bundle\FrameworkBundle\Console\Application as BaseApplication,
+    Symfony\Component\Console\Input\InputInterface,
+    Symfony\Component\Console\Output\OutputInterface,
+    Symfony\Component\Console\Input\InputOption;
+
+/**
+ * eZ Publish console application.
+ * Adds options specific to an eZ Publish environment, such as the siteaccess to use
+ */
+class Application extends BaseApplication
+{
+    /**
+     * @var string
+     */
+    private $siteAccessName;
+
+    public function __construct( KernelInterface $kernel )
+    {
+        parent::__construct( $kernel );
+        $this->getDefinition()->addOption(
+            new InputOption( '--siteaccess', null, InputOption::VALUE_OPTIONAL, 'SiteAccess to use for operations. If not provided, default siteaccess will be used' )
+        );
+    }
+
+    public function doRun( InputInterface $input, OutputInterface $output )
+    {
+        $this->siteAccessName = $input->getParameterOption( '--siteaccess', null );
+        return parent::doRun( $input, $output );
+    }
+
+    protected function registerCommands()
+    {
+        parent::registerCommands();
+
+        $container = $this->getKernel()->getContainer();
+        $this->siteAccessName = $this->siteAccessName ?: $container->getParameter( 'ezpublish.siteaccess.default' );
+        $siteAccess = new SiteAccess( $this->siteAccessName, 'cli' );
+        $container->set( 'ezpublish.siteaccess', $siteAccess );
+    }
+
+}
