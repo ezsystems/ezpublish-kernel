472756154276666708bb2a507db396aaefb9d846
Bertrand Dunogier <bd@ez.no>
2012-10-02 15:50:17 +0200

    EZP-155: enabled visiting of REST exceptions

--- eZ/Bundle/EzPublishRestBundle/EventListener/RestValueResponseListener.php
+++ eZ/Bundle/EzPublishRestBundle/EventListener/RestValueResponseListener.php
@@ -11,9 +11,11 @@ namespace eZ\Bundle\EzPublishRestBundle\EventListener;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * This class listens, as a service, for the kernel.view event, triggered when a controller method
@@ -28,34 +30,88 @@ class RestValueResponseListener implements EventSubscriberInterface
      */
     private $container;
 
+    /**
+     * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+     */
     public function __construct( ContainerInterface $container )
     {
         $this->container = $container;
     }
 
+    /**
+     * @return array
+     */
     public static function getSubscribedEvents()
     {
         return array(
-            KernelEvents::VIEW => 'onKernelView'
+            KernelEvents::VIEW => 'onKernelResultView',
+            KernelEvents::EXCEPTION => 'onKernelExceptionView'
         );
     }
 
-    public function onKernelView( GetResponseForControllerResultEvent $event )
+    /**
+     * @param \Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent $event
+     *
+     * @throws \Exception
+     */public function onKernelResultView( GetResponseForControllerResultEvent $event )
     {
         if ( $event->getRequestType() !== HttpKernelInterface::MASTER_REQUEST )
             return;
 
-        $result = $event->getControllerResult();
-        if ( !$result instanceof \eZ\Publish\Core\REST\Server\Value && !$result instanceof \eZ\Publish\API\Repository\Values\ValueObject )
-        {
-            throw new \Exception( print_r( $result, true ) );
+        if ( !$this->isRestRequest( $event->getRequest() ) )
             return;
-        }
+
+        $result = $event->getControllerResult();
+//        if ( !$result instanceof \eZ\Publish\Core\REST\Server\Value && !$result instanceof \eZ\Publish\API\Repository\Values\ValueObject )
+//        {
+//            throw new \Exception( print_r( $result, true ) );
+//            return;
+//        }
+
+        $event->setResponse( $this->visitResponse( $result ) );
+        $event->stopPropagation();
+    }
+
+    /**
+     * @param \Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent $event
+     *
+     * @throws \Exception
+     * @return void
+     */public function onKernelExceptionView( GetResponseForExceptionEvent $event )
+    {
+        if ( $event->getRequestType() !== HttpKernelInterface::MASTER_REQUEST )
+            return;
+
+
+        if ( !$this->isRestRequest( $event->getRequest() ) )
+            return;
+
+        $result = $event->getException();
+
+        $event->setResponse( $this->visitResult( $result ) );
+        $event->stopPropagation();
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return bool
+     */
+    protected function isRestRequest( Request $request )
+    {
+        return ( strpos( $request->getPathInfo(), '/api/ezp/v2/' ) === 0 );
+    }
+
+    /**
+     * @param $result
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function visitResult( $result )
+    {
         // visit response
         $viewDispatcher = $this->container->get( 'ezpublish_rest.response_visitor_dispatcher' );
-        $result = $viewDispatcher->dispatch( $this->container->get( 'ezpublish_rest.request' ), $result );
-        $response = new Response( $result->body, 200, $result->headers );
-        $event->setResponse( $response );
-        $event->stopPropagation();
+        $message = $viewDispatcher->dispatch( $this->container->get( 'ezpublish_rest.request' ), $result );
+
+        // @todo It would be even better if visitors would return a Symfony message directly
+        return new Response( $message->body, $message->statusCode, $message->headers );
     }
 }
