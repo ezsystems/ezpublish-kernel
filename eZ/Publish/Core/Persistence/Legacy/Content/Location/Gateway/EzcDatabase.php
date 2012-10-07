aa5393bf72c8ebb37aea2915f1382184b694f4b1
Petar Spanja <petar.spanja@gmail.com>
2012-10-07 21:22:36 +0200

    Fixed: c/p wrong variable name

--- eZ/Publish/Core/Persistence/Legacy/Content/Location/Gateway/EzcDatabase.php
+++ eZ/Publish/Core/Persistence/Legacy/Content/Location/Gateway/EzcDatabase.php
@@ -102,7 +102,7 @@ class EzcDatabase extends Gateway
             return $row;
         }
 
-        throw new NotFound( 'location', $nodeId );
+        throw new NotFound( 'location', $remoteId );
     }
 
     /**
