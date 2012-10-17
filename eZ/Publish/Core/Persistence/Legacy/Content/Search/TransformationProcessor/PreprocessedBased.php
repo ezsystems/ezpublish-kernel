2781cbbd354d5e85422ff98969e12fb774a618c5
Patrick Allaert <pa@ez.no>
2012-10-17 15:28:16 +0200

    Added: Preprocessed Transformation Processor

---
+++ eZ/Publish/Core/Persistence/Legacy/Content/Search/TransformationProcessor/PreprocessedBased.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * File containing the TransformationProcessor\PreprocessedBased class
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\Core\Persistence\Legacy\Content\Search\TransformationProcessor;
+
+use eZ\Publish\Core\Persistence\Legacy\Content\Search\TransformationProcessor;
+
+/**
+ * Class for processing a set of transformations, loaded from .tr files, on a string
+ */
+class PreprocessedBased extends TransformationProcessor
+{
+    /**
+     * Directory to load rules relative from.
+     *
+     * @var string
+     */
+    protected $installDir;
+
+    /**
+     * Constructor
+     *
+     * @param \eZ\Publish\Core\Persistence\Legacy\Content\Search\TransformationProcessor\PcreCompiler $compiler
+     * @param string $installDir Base dir for rule loading
+     * @param array $ruleFiles
+     */
+    public function __construct( PcreCompiler $compiler, $installDir, array $ruleFiles = array() )
+    {
+        parent::__construct( $compiler, $ruleFiles );
+        $this->installDir = $installDir;
+    }
+
+    /**
+     * Load rules
+     *
+     * @return array
+     */
+    protected function getRules()
+    {
+        if ( $this->compiledRules === null )
+        {
+            $rules = array();
+
+            foreach ( $this->ruleFiles as $file )
+            {
+                $rules += require $this->installDir . "/" . $file;
+            }
+
+            $this->compiledRules = $this->compiler->compile( $rules );
+        }
+
+        return $this->compiledRules;
+    }
+}
