8ad550e42fc7e362899a08ec0414b36798ead8fb
Jérôme Vieilledent <lolautruche@gmail.com>
2012-10-19 17:15:05 +0200

    Implemented image alias generator, using legacy kernel

---
+++ eZ/Publish/SPI/VariantService.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * File containing the VariantService interface.
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\SPI;
+
+use eZ\Publish\API\Repository\Values\Content\Field,
+    eZ\Publish\API\Repository\Values\Content\VersionInfo;
+
+/**
+ * Interface for Variant services.
+ * A variant service allows to generate variation from a given content field/version info
+ * (i.e. image aliases, variations of a document - doc, pdf...)
+ */
+interface VariantService
+{
+    /**
+     * Returns a Variant object for $field's $variantName.
+     * This method is responsible to create the variant if needed.
+     * Variants might be applicable for images (aliases), documents...
+     *
+     * @param \eZ\Publish\API\Repository\Values\Content\Field $field
+     * @param \eZ\Publish\API\Repository\Values\Content\VersionInfo $versionInfo
+     * @param string $variantName
+     * @return \eZ\Publish\API\Repository\Values\File\Variant
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidVariantException
+     */
+    public function getVariant( Field $field, VersionInfo $versionInfo, $variantName );
+}
