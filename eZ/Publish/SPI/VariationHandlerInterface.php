31f9c0e572eb99e620e827e2f6e259d0febcfeed
Jérôme Vieilledent <lolautruche@gmail.com>
2012-10-20 10:26:28 +0200

    Fixed some PHPDoc

--- eZ/Publish/SPI/VariationHandlerInterface.php
+++ eZ/Publish/SPI/VariationHandlerInterface.php
@@ -27,9 +27,9 @@ interface VariationHandlerInterface
      * @param \eZ\Publish\API\Repository\Values\Content\Field $field
      * @param \eZ\Publish\API\Repository\Values\Content\VersionInfo $versionInfo
      * @param string $variantName
-     * @return \eZ\Publish\API\Repository\Values\File\Variant
+     * @return \eZ\Publish\API\Repository\Values\File\Variation
      *
-     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidVariantException
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidVariationException
      */
     public function getVariation( Field $field, VersionInfo $versionInfo, $variantName );
 }
