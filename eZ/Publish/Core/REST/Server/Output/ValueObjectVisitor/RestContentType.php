bd1d3073b7c3a337543b43c1edb440626c9415eb
Edi Modrić <edi.modric@gmail.com>
2012-10-24 18:16:18 +0200

    REST: Fix ContentTypeInfo element name

--- eZ/Publish/Core/REST/Server/Output/ValueObjectVisitor/RestContentType.php
+++ eZ/Publish/Core/REST/Server/Output/ValueObjectVisitor/RestContentType.php
@@ -36,7 +36,7 @@ class RestContentType extends RestContentTypeBase
         $urlTypeSuffix = $this->getUrlTypeSuffix( $contentType->status );
         $mediaType = $data->fieldDefinitions !== null ? 'ContentType' : 'ContentTypeInfo';
 
-        $generator->startObjectElement( 'ContentType' );
+        $generator->startObjectElement( $mediaType );
 
         $visitor->setHeader( 'Content-Type', $generator->getMediaType( $mediaType ) );
 
@@ -135,7 +135,7 @@ class RestContentType extends RestContentTypeBase
             );
         }
 
-        $generator->endObjectElement( 'ContentType' );
+        $generator->endObjectElement( $mediaType );
     }
 
     /**
