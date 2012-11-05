799b1c94bf28796532b698d8b1dade0ebf4bee63
Patrick Allaert <pa@ez.no>
2012-11-05 18:37:14 +0100

    Fixed: missing copyright header

--- eZ/Publish/Core/REST/Server/Controller.php
+++ eZ/Publish/Core/REST/Server/Controller.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * File containing the REST Server Controller class
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
 namespace eZ\Publish\Core\REST\Server;
 
 use eZ\Publish\Core\REST\Common\UrlHandler\eZPublish as UrlHandler;
