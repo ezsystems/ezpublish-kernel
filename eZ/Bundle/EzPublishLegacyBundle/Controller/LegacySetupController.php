3c455b48bb5b576570ca42bdb841a82a860e2d1f
Bertrand Dunogier <bd@ez.no>
2012-11-05 17:39:17 +0100

    EZP-19968: Made sure settings conversion is made at the right time

--- eZ/Bundle/EzPublishLegacyBundle/Controller/LegacySetupController.php
+++ eZ/Bundle/EzPublishLegacyBundle/Controller/LegacySetupController.php
@@ -10,7 +10,8 @@ namespace eZ\Bundle\EzPublishLegacyBundle\Controller;
 
 use Symfony\Component\DependencyInjection\ContainerInterface as Container,
     Symfony\Component\HttpFoundation\Response,
-    Symfony\Component\Yaml\Dumper;
+    Symfony\Component\Yaml\Dumper,
+    eZ\Bundle\EzPublishLegacyBundle\DependencyInjection\Configuration\LegacyConfigResolver;
 
 class LegacySetupController
 {
@@ -22,6 +23,13 @@ class LegacySetupController
     private $legacyKernelClosure;
 
     /**
+     * The legacy config resolver
+     *
+     * @var \eZ\Bundle\EzPublishLegacyBundle\DependencyInjection\Configuration\LegacyConfigResolver
+     */
+    private $legacyConfigResolver;
+
+    /**
      * @var \Symfony\Component\DependencyInjection\ContainerInterface
      */
     protected $container;
@@ -29,11 +37,14 @@ class LegacySetupController
     /**
      * @todo Maybe following dependencies should be mutualized in an abstract controller
      *       Injection can be done through "parent service" feature for DIC : http://symfony.com/doc/master/components/dependency_injection/parentservices.html
+     *
      * @param \Closure $kernelClosure
+     * @param \eZ\Bundle\EzPublishLegacyBundle\DependencyInjection\Configuration\LegacyConfigResolver $legacyConfigResolver
      */
-    public function __construct( \Closure $kernelClosure )
+    public function __construct( \Closure $kernelClosure, LegacyConfigResolver $legacyConfigResolver )
     {
         $this->legacyKernelClosure = $kernelClosure;
+        $this->legacyConfigResolver = $legacyConfigResolver;
     }
 
     public function setContainer( Container $container )
@@ -79,26 +90,10 @@ class LegacySetupController
         $result->getContent();
         $response->setContent( $result->getContent() );
 
+        // After the registration step, we can re-use both POST data and written INI settings
+        // to generate a local ezpublish_<env>.yml
         if ( $currentStep == 'Registration' )
         {
-            $chosenSitePackage = $request->request->get( 'P_chosen_site_package-0' );
-
-            // match mode (host, url or port)
-            $accessType = $request->request->get( 'P_site_extra_data_access_type-' . $chosenSitePackage );
-            if ( $accessType == 'hostname' || $accessType == 'port' )
-            {
-                $adminSiteaccess = $chosenSitePackage . '_admin';
-            }
-            elseif ( $accessType === 'url' )
-            {
-                $adminSiteaccess = $request->request->get( 'P_site_extra_data_admin_access_type_value-' . $chosenSitePackage );
-            }
-
-            /** @var $configurationConverter \eZ\Bundle\EzPublishLegacyBundle\SetupWizard\ConfigurationConverter */
-            $configurationConverter = $this->container->get( 'ezpublish_legacy.setup_wizard.configuration_converter' );
-
-            $dumper = new Dumper();
-
             // Clear INI cache since setup has written new files
             $this->getLegacyKernel()->runCallback(
                 function()
@@ -109,24 +104,45 @@ class LegacySetupController
                 }
             );
 
-            $settingsArray = $configurationConverter->fromLegacy( $chosenSitePackage, $adminSiteaccess );
+            // Check that eZ Publish Legacy was actually installed, since one step can run several steps
+            if ( $this->legacyConfigResolver->getParameter( 'SiteAccessSettings.CheckValidity' ) == 'false' )
+            {
+                $chosenSitePackage = $request->request->get( 'P_chosen_site_package-0' );
 
-            // add the import statement for the root YAML file
-            $settingsArray['imports'] = array( array( 'resource' => 'ezpublish.yml' ) );
+                // match mode (host, url or port)
+                $accessType = $request->request->get( 'P_site_extra_data_access_type-' . $chosenSitePackage );
+                if ( $accessType == 'hostname' || $accessType == 'port' )
+                {
+                    $adminSiteaccess = $chosenSitePackage . '_admin';
+                }
+                elseif ( $accessType === 'url' )
+                {
+                    $adminSiteaccess = $request->request->get( 'P_site_extra_data_admin_access_type_value-' . $chosenSitePackage );
+                }
 
-            $kernel = $this->container->get( 'kernel' );
-            file_put_contents(
-                $kernel->getRootdir() . '/config/ezpublish_' . $kernel->getEnvironment(). '.yml',
-                $dumper->dump( $settingsArray, 7 )
-            );
+                /** @var $configurationConverter \eZ\Bundle\EzPublishLegacyBundle\SetupWizard\ConfigurationConverter */
+                $configurationConverter = $this->container->get( 'ezpublish_legacy.setup_wizard.configuration_converter' );
 
-            /** @var $filesystem \Symfony\Component\Filesystem\Filesystem */
-            $filesystem = $this->container->get( 'filesystem' );
-            $cacheDir = $this->container->getParameter( 'kernel.cache_dir' );
-            $oldCacheDirName = $cacheDir . '_old';
-            $filesystem->rename( $cacheDir, $oldCacheDirName );
-            $filesystem->remove( $oldCacheDirName );
+                $dumper = new Dumper();
 
+                $settingsArray = $configurationConverter->fromLegacy( $chosenSitePackage, $adminSiteaccess );
+
+                // add the import statement for the root YAML file
+                $settingsArray['imports'] = array( array( 'resource' => 'ezpublish.yml' ) );
+
+                $kernel = $this->container->get( 'kernel' );
+                file_put_contents(
+                    $kernel->getRootdir() . '/config/ezpublish_' . $kernel->getEnvironment(). '.yml',
+                    $dumper->dump( $settingsArray, 7 )
+                );
+
+                /** @var $filesystem \Symfony\Component\Filesystem\Filesystem */
+                $filesystem = $this->container->get( 'filesystem' );
+                $cacheDir = $this->container->getParameter( 'kernel.cache_dir' );
+                $oldCacheDirName = $cacheDir . '_old';
+                $filesystem->rename( $cacheDir, $oldCacheDirName );
+                $filesystem->remove( $oldCacheDirName );
+            }
         }
 
         return $response;
