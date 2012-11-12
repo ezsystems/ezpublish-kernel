d0ccdc549732ac9f845ab29bc435aa5a5434a097
Jérôme Vieilledent <lolautruche@gmail.com>
2012-11-12 18:31:32 +0100

    Added tests for Http PURGE client

---
+++ eZ/Publish/Core/MVC/Symfony/Cache/Tests/Http/HttpBasedPurgeClientTest.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * File containing the HttpBasedPurgeClientTest class.
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\Core\MVC\Symfony\Cache\Tests\Http;
+
+use PHPUnit_Framework_TestCase;
+
+abstract class HttpBasedPurgeClientTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $httpBrowser;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $httpClient;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $configResolver;
+
+    protected function setUp()
+    {
+        if ( !class_exists( 'Buzz\\Browser' ) )
+            self::markTestSkipped( 'Please install kriswallsmith/buzz library from Composer' );
+
+        parent::setUp();
+
+        $this->httpClient = $this->getMock( 'Buzz\\Client\\BatchClientInterface' );
+        $this->httpBrowser = $this
+            ->getMockBuilder( 'Buzz\\Browser' )
+            ->setConstructorArgs( array( $this->httpClient ) )
+            ->getMock()
+        ;
+        $this->httpBrowser
+            ->expects( $this->any() )
+            ->method( 'getClient' )
+            ->will( $this->returnValue( $this->httpClient ) )
+        ;
+
+        $this->configResolver = $this->getMock( 'eZ\\Publish\\Core\\MVC\\ConfigResolverInterface' );
+    }
+
+    abstract public function testPurge();
+}
