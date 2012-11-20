d1e219305dd3d76e650f691950430aecfb53f3aa
André R <ar@ez.no>
2012-11-20 16:52:59 +0100

    Add class PHPDoc on SPI\FieldType\EventListener

--- eZ/Publish/SPI/FieldType/EventListener.php
+++ eZ/Publish/SPI/FieldType/EventListener.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * File containing the FieldType interface
+ * File containing the EventListener interface
  *
  * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
@@ -9,26 +9,16 @@
 
 namespace eZ\Publish\SPI\FieldType;
 
-use eZ\Publish\API\Repository\Values\ContentType\FieldDefinition,
-    eZ\Publish\SPI\Persistence\Content\FieldValue,
-    eZ\Publish\SPI\FieldType\Event;
+use eZ\Publish\SPI\FieldType\Event;
 
 /**
- * The field type interface which all field types have to implement.
+ * A field type interface which field types can optionally implement.
  *
+ * Field types that implemented this interface will be notified about certain events, both before and after something
+ * has happened. This allows the field type to perform certain actions.
  *
- * Hashes:
- *
- * The {@link toHash()} method in this class is meant to generate a simple
- * representation of a value of this field type. Hash does here not refer to
- * MD5 or similar hashing algorithms, but rather to hash-map (associative array)
- * type representation. This representation must be
- * usable, to transfer the value over plain text encoding formats, like e.g.
- * XML. As a result, the returned "hash" must either be a scalar value, a hash
- * array (associative array) a pure numeric array or a nested combination of
- * these. It must by no means contain objects, resources or cyclic references.
- * The corresponding {@link fromHash()} method must convert such a
- * representation back into a value, which is understood by the FieldType.
+ * Note: This is a low level synchronous events, for more generic asynchronous events like sending out notifications,
+ * please use the signal slot system instead (create and register a slot to listen to events).
  */
 interface EventListener
 {
