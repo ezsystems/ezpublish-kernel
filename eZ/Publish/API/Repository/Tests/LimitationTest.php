44ac0e0ecc301726656a11ee5bb617acda12fb61
Manuel Pichler <mapi@manuel-pichler.de>
2012-11-26 15:38:16 +0100

    Added: Two integration tests for limitations
    This test checks the allow and forbid execution path for a limitation
    combination of ParentContentTypeLimitation and ContentTypeLimitation.

--- eZ/Publish/API/Repository/Tests/LimitationTest.php
+++ eZ/Publish/API/Repository/Tests/LimitationTest.php
@@ -24,7 +24,14 @@ use eZ\Publish\API\Repository\Values\User\Limitation\ParentContentTypeLimitation
  */
 class LimitationTest extends BaseTest
 {
-    public function testContentTypeAndParentContentTypeLimitation()
+    /**
+     * Test for ParentContentTypeLimitation and ContentTypeLimitation.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\Values\User\Limitation\ContentTypeLimitation
+     * @see \eZ\Publish\API\Repository\Values\User\Limitation\ParentContentTypeLimitation
+     */
+    public function testContentTypeAndParentContentTypeLimitationAllow()
     {
         $repository = $this->getRepository();
 
@@ -82,11 +89,82 @@ class LimitationTest extends BaseTest
         $wikiPageCreate->alwaysAvailable = true;
 
         // Create a draft
-        $contentService->publishVersion(
-            $contentService->createContent(
-                $wikiPageCreate,
-                array( $locationCreate )
-            )->versionInfo
+        $contentService->createContent(
+            $wikiPageCreate,
+            array( $locationCreate )
+        );
+        /* END: Use Case */
+    }
+
+    /**
+     * Test for ParentContentTypeLimitation and ContentTypeLimitation.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\Values\User\Limitation\ContentTypeLimitation
+     * @see \eZ\Publish\API\Repository\Values\User\Limitation\ParentContentTypeLimitation
+     * @expectedException \eZ\Publish\API\Repository\Exceptions\UnauthorizedException
+     */
+    public function testContentTypeAndParentContentTypeLimitationForbid()
+    {
+        $repository = $this->getRepository();
+
+        $parentLocationId = $this->generateId( 'location', 60 );
+        $sectionId = $this->generateId( 'section', 1 );
+        /* BEGIN: Use Case */
+        $user = $this->createUserVersion1();
+
+        $roleService = $repository->getRoleService();
+
+        $role = $roleService->loadRoleByIdentifier( 'Editor' );
+
+        $policyCreate = $roleService->newPolicyCreateStruct( 'content', 'create' );
+        $policyCreate->addLimitation(
+            new ParentContentTypeLimitation(
+                array( 'limitationValues' => array( 20 ) )
+            )
+        );
+        $policyCreate->addLimitation(
+            new ContentTypeLimitation(
+                array( 'limitationValues' => array( 33 ) )
+            )
+        );
+
+        $role = $roleService->addPolicy( $role, $policyCreate );
+
+        $roleService->assignRoleToUser( $roleService->loadRole( $role->id ), $user );
+
+        $repository->setCurrentUser( $user );
+
+        $contentTypeService = $repository->getContentTypeService();
+        $locationService = $repository->getLocationService();
+        $contentService = $repository->getContentService();
+
+        // Configure new location
+        // $parentLocationId is the id of the /Home/Contact-Us node
+        $locationCreate = $locationService->newLocationCreateStruct( $parentLocationId );
+
+        $locationCreate->priority = 23;
+        $locationCreate->hidden = true;
+        $locationCreate->remoteId = '0123456789abcdef0123456789abcdef';
+        $locationCreate->sortField = Location::SORT_FIELD_NODE_ID;
+        $locationCreate->sortOrder = Location::SORT_ORDER_DESC;
+
+        // Load content type
+        $contentType = $contentTypeService->loadContentTypeByIdentifier( 'wiki_page' );
+
+        // Configure new content object
+        $contentCreate = $contentService->newContentCreateStruct( $contentType, 'eng-US' );
+
+        $contentCreate->setField( 'title', 'An awesome wiki page' );
+        $contentCreate->remoteId = 'abcdef0123456789abcdef0123456789';
+        // $sectionId is the ID of section 1
+        $contentCreate->sectionId = $sectionId;
+        $contentCreate->alwaysAvailable = true;
+
+        // This call will fail with an UnauthorizedException
+        $contentService->createContent(
+            $contentCreate,
+            array( $locationCreate )
         );
         /* END: Use Case */
     }
