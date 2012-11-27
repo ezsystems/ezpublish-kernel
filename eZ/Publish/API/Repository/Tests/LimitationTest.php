9efcf9bc2ff3278111b31c0cf57a604d273f4fb6
Manuel Pichler <mapi@manuel-pichler.de>
2012-11-27 12:21:32 +0100

    Use inline delegate method as test fixture.

--- eZ/Publish/API/Repository/Tests/LimitationTest.php
+++ eZ/Publish/API/Repository/Tests/LimitationTest.php
@@ -62,6 +62,68 @@ class LimitationTest extends BaseTest
 
         $repository->setCurrentUser( $user );
 
+        $draft = $this->createWikiPageDraft();
+        /* END: Use Case */
+
+        $this->assertEquals(
+            'An awesome wiki page',
+            $draft->getFieldValue('title')->text
+        );
+    }
+
+    /**
+     * Test for ParentContentTypeLimitation and ContentTypeLimitation.
+     *
+     * @return void
+     * @see \eZ\Publish\API\Repository\Values\User\Limitation\ContentTypeLimitation
+     * @see \eZ\Publish\API\Repository\Values\User\Limitation\ParentContentTypeLimitation
+     * @expectedException \eZ\Publish\API\Repository\Exceptions\UnauthorizedException
+     */
+    public function testContentTypeAndParentContentTypeLimitationForbid()
+    {
+        $repository = $this->getRepository();
+
+        /* BEGIN: Use Case */
+        $user = $this->createUserVersion1();
+
+        $roleService = $repository->getRoleService();
+
+        $role = $roleService->loadRoleByIdentifier( 'Editor' );
+
+        $policyCreate = $roleService->newPolicyCreateStruct( 'content', 'create' );
+        $policyCreate->addLimitation(
+            new ParentContentTypeLimitation(
+                array( 'limitationValues' => array( 20 ) )
+            )
+        );
+        $policyCreate->addLimitation(
+            new ContentTypeLimitation(
+                array( 'limitationValues' => array( 33 ) )
+            )
+        );
+
+        $role = $roleService->addPolicy( $role, $policyCreate );
+
+        $roleService->assignRoleToUser( $role, $user );
+
+        $repository->setCurrentUser( $user );
+
+        $this->createWikiPageDraft();
+        /* END: Use Case */
+    }
+
+    /**
+     * Creates a fresh clean content draft.
+     *
+     * @return \eZ\Publish\API\Repository\Values\Content\Content
+     */
+    protected function createWikiPageDraft()
+    {
+        $repository = $this->getRepository();
+
+        $parentLocationId = $this->generateId( 'location', 60 );
+        $sectionId = $this->generateId( 'section', 1 );
+        /* BEGIN: Inline */
         $contentTypeService = $repository->getContentTypeService();
         $locationService = $repository->getLocationService();
         $contentService = $repository->getContentService();
@@ -89,84 +151,13 @@ class LimitationTest extends BaseTest
         $wikiPageCreate->alwaysAvailable = true;
 
         // Create a draft
-        $contentService->createContent(
+        $draft = $contentService->createContent(
             $wikiPageCreate,
             array( $locationCreate )
         );
-        /* END: Use Case */
-    }
+        /* END: Inline */
 
-    /**
-     * Test for ParentContentTypeLimitation and ContentTypeLimitation.
-     *
-     * @return void
-     * @see \eZ\Publish\API\Repository\Values\User\Limitation\ContentTypeLimitation
-     * @see \eZ\Publish\API\Repository\Values\User\Limitation\ParentContentTypeLimitation
-     * @expectedException \eZ\Publish\API\Repository\Exceptions\UnauthorizedException
-     */
-    public function testContentTypeAndParentContentTypeLimitationForbid()
-    {
-        $repository = $this->getRepository();
-
-        $parentLocationId = $this->generateId( 'location', 60 );
-        $sectionId = $this->generateId( 'section', 1 );
-        /* BEGIN: Use Case */
-        $user = $this->createUserVersion1();
-
-        $roleService = $repository->getRoleService();
-
-        $role = $roleService->loadRoleByIdentifier( 'Editor' );
-
-        $policyCreate = $roleService->newPolicyCreateStruct( 'content', 'create' );
-        $policyCreate->addLimitation(
-            new ParentContentTypeLimitation(
-                array( 'limitationValues' => array( 20 ) )
-            )
-        );
-        $policyCreate->addLimitation(
-            new ContentTypeLimitation(
-                array( 'limitationValues' => array( 33 ) )
-            )
-        );
-
-        $role = $roleService->addPolicy( $role, $policyCreate );
-
-        $roleService->assignRoleToUser( $role, $user );
-
-        $repository->setCurrentUser( $user );
-
-        $contentTypeService = $repository->getContentTypeService();
-        $locationService = $repository->getLocationService();
-        $contentService = $repository->getContentService();
-
-        // Configure new location
-        // $parentLocationId is the id of the /Home/Contact-Us node
-        $locationCreate = $locationService->newLocationCreateStruct( $parentLocationId );
-
-        $locationCreate->priority = 23;
-        $locationCreate->hidden = true;
-        $locationCreate->remoteId = '0123456789abcdef0123456789abcdef';
-        $locationCreate->sortField = Location::SORT_FIELD_NODE_ID;
-        $locationCreate->sortOrder = Location::SORT_ORDER_DESC;
-
-        // Load content type
-        $contentType = $contentTypeService->loadContentTypeByIdentifier( 'wiki_page' );
-
-        // Configure new content object
-        $contentCreate = $contentService->newContentCreateStruct( $contentType, 'eng-US' );
-
-        $contentCreate->setField( 'title', 'An awesome wiki page' );
-        $contentCreate->remoteId = 'abcdef0123456789abcdef0123456789';
-        // $sectionId is the ID of section 1
-        $contentCreate->sectionId = $sectionId;
-        $contentCreate->alwaysAvailable = true;
-
-        // This call will fail with an UnauthorizedException
-        $contentService->createContent(
-            $contentCreate,
-            array( $locationCreate )
-        );
-        /* END: Use Case */
+        return $draft;
     }
 
     /**
