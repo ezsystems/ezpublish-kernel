a6bead47b7566623747197c7446a37b804c53054
Kore Nordmann <mail@kore-nordmann.de>
2012-11-27 13:42:35 +0100

    Docs

--- eZ/Publish/Core/Persistence/Legacy/Content/Search/Gateway/EzcDatabase.php
+++ eZ/Publish/Core/Persistence/Legacy/Content/Search/Gateway/EzcDatabase.php
@@ -182,7 +182,6 @@ class EzcDatabase extends Gateway
      */
     protected function getResultCount( Criterion $criterion, array $sort, $translations )
     {
-        // Get full object count
         $query = $this->handler->createSelectQuery();
 
         $query
@@ -222,8 +221,6 @@ class EzcDatabase extends Gateway
      */
     protected function getContentIds( Criterion $criterion, array $sort, $offset, $limit, $translations )
     {
-        // Getting a clean new query, because rebinding query properties does
-        // not work.
         $query = $this->handler->createSelectQuery();
 
         $query->select(
