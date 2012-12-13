3e2866bbb37dbe6a48cb3a376c2c7a5519257e45
Manuel Pichler <mapi@manuel-pichler.de>
2012-12-13 11:11:47 +0100

    Added: Integration test for the LocationLimitation

---
+++ eZ/Publish/API/Repository/Tests/Values/User/Limitation/LocationLimitationTest.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * File containing the LocationLimitationTest class
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\API\Repository\Tests\Values\User\Limitation;
+
+use eZ\Publish\API\Repository\Values\User\Limitation\LocationLimitation;
+use eZ\Publish\API\Repository\Tests\Values\User\Limitation\BaseLimitationTest;
+
+/**
+ * Test case for the {@link \eZ\Publish\API\Repository\Values\User\Limitation\LocationLimitation}
+ * class.
+ *
+ * @see eZ\Publish\API\Repository\Values\User\Limitation
+ * @see eZ\Publish\API\Repository\Values\User\Limitation\LocationLimitation
+ * @group integration
+ * @group limitation
+ */
+class LocationLimitationTest extends BaseLimitationTest
+{
+    /**
+     * Tests a LocationLimitation
+     *
+     * @return void
+     * @see eZ\Publish\API\Repository\Values\User\Limitation\LocationLimitation
+     */
+    public function testLocationLimitationAllow()
+    {
+        $repository = $this->getRepository();
+
+        $parentLocationId = $this->generateId( 'location', 60 );
+        /* BEGIN: Use Case */
+        $user = $this->createUserVersion1();
+
+        $roleService = $repository->getRoleService();
+
+        $role = $roleService->loadRoleByIdentifier( 'Editor' );
+
+        $policyCreate = $roleService->newPolicyCreateStruct( 'content', 'create' );
+        $policyCreate->addLimitation(
+            new LocationLimitation(
+                array( 'limitationValues' => array( $parentLocationId ) )
+            )
+        );
+
+        $role = $roleService->addPolicy( $role, $policyCreate );
+
+        $roleService->assignRoleToUser( $role, $user );
+
+        $repository->setCurrentUser( $user );
+
+        $draft = $this->createWikiPageDraft();
+        /* END: Use Case */
+
+        $this->assertEquals(
+            'An awesome wiki page',
+            $draft->getFieldValue( 'title' )->text
+        );
+    }
+
+    /**
+     * Tests a LocationLimitation
+     *
+     * @return void
+     * @see eZ\Publish\API\Repository\Values\User\Limitation\LocationLimitation
+     * @expectedException \eZ\Publish\API\Repository\Exceptions\UnauthorizedException
+     */
+    public function testLocationLimitationForbid()
+    {
+        $repository = $this->getRepository();
+
+        $parentLocationId = $this->generateId( 'location', 61 );
+        /* BEGIN: Use Case */
+        $user = $this->createUserVersion1();
+
+        $roleService = $repository->getRoleService();
+
+        $role = $roleService->loadRoleByIdentifier( 'Editor' );
+
+        $policyCreate = $roleService->newPolicyCreateStruct( 'content', 'create' );
+        $policyCreate->addLimitation(
+            new LocationLimitation(
+                array( 'limitationValues' => array( $parentLocationId ) )
+            )
+        );
+
+        $role = $roleService->addPolicy( $role, $policyCreate );
+
+        $roleService->assignRoleToUser( $role, $user );
+
+        $repository->setCurrentUser( $user );
+
+        $this->createWikiPageDraft();
+        /* END: Use Case */
+    }
+}
