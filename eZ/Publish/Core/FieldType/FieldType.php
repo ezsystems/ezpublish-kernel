bea4eda4683a25ff8a2e7f25fa965df77d9afa54
Petar Spanja <petar.spanja@gmail.com>
2013-01-25 17:34:21 +0100

    Fixed: unexisting Relation type in code example

--- eZ/Publish/Core/FieldType/FieldType.php
+++ eZ/Publish/Core/FieldType/FieldType.php
@@ -384,7 +384,7 @@ abstract class FieldType implements FieldTypeInterface
      *          "contentIds" => array( 12 ),
      *          "locationIds" => array( 24, 45 )
      *      ),
-     *      \eZ\Publish\API\Repository\Values\Content\Relation::ATTRIBUTE => array( 12 )
+     *      \eZ\Publish\API\Repository\Values\Content\Relation::FIELD => array( 12 )
      *  )
      * </code>
      */
