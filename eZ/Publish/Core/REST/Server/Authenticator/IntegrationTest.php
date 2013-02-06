96121b1c63f07a80902b0fe600c6fb1ef43b9f5f
Patrick Allaert <pa@ez.no>
2013-02-06 15:56:45 +0100

    Changed: welcome 2013

--- eZ/Publish/Core/REST/Server/Authenticator/IntegrationTest.php
+++ eZ/Publish/Core/REST/Server/Authenticator/IntegrationTest.php
@@ -5,7 +5,7 @@
  * ATTENTION: This is a only meant for the test setup for the REST server. DO
  * NOT USE IT IN PRODUCTION!
  *
- * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
  * @version //autogentag//
  */
