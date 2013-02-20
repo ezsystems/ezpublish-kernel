4a70fc728c1390b0fccb6246bf22585342b5d818
Patrick Allaert <pa@ez.no>
2013-02-20 14:53:56 +0100

    Changed: adapted Solr fixtures against results received

--- eZ/Publish/API/Repository/Tests/_fixtures/Solr/FullText.php
+++ eZ/Publish/API/Repository/Tests/_fixtures/Solr/FullText.php
@@ -13,7 +13,7 @@ return eZ\Publish\API\Repository\Values\Content\Search\SearchResult::__set_state
             'id' => 58,
             'title' => 'Contact Us',
         ),
-        'score' => 0.51916456,
+        'score' => 1.5986123,
         'index' => NULL,
         'highlight' => NULL,
     )),
@@ -21,7 +21,7 @@ return eZ\Publish\API\Repository\Values\Content\Search\SearchResult::__set_state
    'spellSuggestion' => NULL,
    'time' => 1,
    'timedOut' => NULL,
-   'maxScore' => 0.51916456,
+   'maxScore' => 1.5986123,
    'totalCount' => 1,
 ));
 
