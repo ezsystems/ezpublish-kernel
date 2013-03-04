363c5682cbb7d6b660b72f761ec170d2e1d554f4
Jérôme Vieilledent <lolautruche@gmail.com>
2013-03-04 17:51:53 +0100

    EZP-20424 - Refactored Page field type converter and value objects to make them more explicit

--- eZ/Publish/Core/FieldType/Page/Parts/Item.php
+++ eZ/Publish/Core/FieldType/Page/Parts/Item.php
@@ -9,7 +9,57 @@
 
 namespace eZ\Publish\Core\FieldType\Page\Parts;
 
+/**
+ * @property-read mixed $contentId Related content Id.
+ * @property-read mixed $locationId Related location Id.
+ * @property-read \DateTime $publicationDate Date when the item has been published.
+ * @property-read \DateTime $visibilityDate Date when the item has been made visible.
+ * @property-read \DateTime|null $hiddenDate Date when the item must be hidden.
+ * @property-read \DateTime|null $rotationUntilDate Date until this item can be made in rotation with other items of the same block.
+ * @property-read mixed $movedTo
+ * @property-read string $action Action to be executed. Can be either "add", "modify" or "remove" (see \eZ\Publish\Core\FieldType\Page\Parts\Base for ACTION_* constants)
+ */
 class Item extends Base
 {
+    /**
+     * @var mixed
+     */
+    protected $contentId;
 
+    /**
+     * @var mixed
+     */
+    protected $locationId;
+
+    /**
+     * @var \DateTime
+     */
+    protected $publicationDate;
+
+    /**
+     * @var \DateTime
+     */
+    protected $visibilityDate;
+
+    /**
+     * @var \DateTime|null
+     */
+    protected $hiddenDate;
+
+    /**
+     * @var \DateTime|null
+     */
+    protected $rotationUntilDate;
+
+    /**
+     * @var mixed
+     */
+    protected $movedTo;
+
+    /**
+     * @see \eZ\Publish\Core\FieldType\Page\Parts\Base for ACTION_* constants
+     *
+     * @var string
+     */
+    protected $action;
 }
