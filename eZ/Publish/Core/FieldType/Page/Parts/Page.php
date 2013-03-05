023f6b812b0df780eda5eb268ce9d2cce0a95bc1
Jérôme Vieilledent <lolautruche@gmail.com>
2013-03-05 18:03:54 +0100

    EZP-20424 - Added missing $layout property in Page\Parts\Page

--- eZ/Publish/Core/FieldType/Page/Parts/Page.php
+++ eZ/Publish/Core/FieldType/Page/Parts/Page.php
@@ -11,12 +11,21 @@ namespace eZ\Publish\Core\FieldType\Page\Parts;
 
 use OutOfBoundsException;
 
+/**
+ * @property-read string $layout The layout identifier (e.g. "2ZonesLayout1").
+ * @property-read \eZ\Publish\Core\FieldType\Page\Parts\Zone[] $zones Zone objects for current page.
+ */
 class Page extends Base
 {
     /**
      * @var \eZ\Publish\Core\FieldType\Page\Parts\Zone[]
      */
     protected $zones = array();
+
+    /**
+     * @var string
+     */
+    protected $layout;
 
     /**
      * Returns zone object by given $index.
