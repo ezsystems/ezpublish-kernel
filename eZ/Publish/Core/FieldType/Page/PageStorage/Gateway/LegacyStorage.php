a08640468d40c2add0026bb3bb976f7e45629980
Jérôme Vieilledent <lolautruche@gmail.com>
2013-03-06 13:50:05 +0100

    EZP-20424 - Implemented methods in Page legacy storage gateway to get valid/waiting/archived block items

--- eZ/Publish/Core/FieldType/Page/PageStorage/Gateway/LegacyStorage.php
+++ eZ/Publish/Core/FieldType/Page/PageStorage/Gateway/LegacyStorage.php
@@ -14,6 +14,8 @@ use eZ\Publish\Core\Persistence\Legacy\EzcDbHandler;
 use eZ\Publish\Core\FieldType\Page\Parts\Block;
 use eZ\Publish\Core\FieldType\Page\Parts\Item;
 use RuntimeException;
+use DateTime;
+use ezcQuerySelect;
 
 class LegacyStorage extends Gateway
 {
@@ -45,29 +47,144 @@ class LegacyStorage extends Gateway
     }
 
     /**
+     * Returns the active connection
+     *
+     * @throws \RuntimeException if no connection has been set, yet.
+     *
+     * @return \eZ\Publish\Core\Persistence\Legacy\EzcDbHandler
+     */
+    protected function getConnection()
+    {
+        if ( $this->dbHandler === null )
+        {
+            throw new RuntimeException( "Missing database connection." );
+        }
+        return $this->dbHandler;
+    }
+
+    /**
+     * Returns valid items (that are to be displayed), for a given block.
+     *
      * @param \eZ\Publish\Core\FieldType\Page\Parts\Block
+     *
      * @return \eZ\Publish\Core\FieldType\Page\Parts\Item[]
      */
     public function getValidBlockItems( Block $block )
     {
-        // TODO: Implement getValidBlockItems() method.
+        $dbHandler = $this->getConnection();
+        /** @var $q \ezcQuerySelect */
+        $q = $dbHandler->createSelectQuery();
+        $q
+            ->select( '*' )
+            ->from( $dbHandler->quoteTable( self::POOL_TABLE ) )
+            ->where(
+                $q->expr->eq( 'block_id', $q->bindValue( $block->id ) ),
+                $q->expr->gt( 'ts_visible', $q->bindValue( 0, null, \PDO::PARAM_INT ) ),
+                $q->expr->eq( 'ts_hidden', $q->bindValue( 0, null, \PDO::PARAM_INT ) )
+            )
+            ->orderBy( 'priority', ezcQuerySelect::DESC );
+
+        $stmt = $q->prepare();
+        $stmt->execute();
+        $rows = $stmt->fetchAll( \PDO::FETCH_ASSOC );
+        $items = array();
+        foreach ( $rows as $row )
+        {
+            $items[] = $this->buildBlockItem( $row );
+        }
+
+        return $items;
     }
 
     /**
+     * Returns queued items (the next to be displayed), for a given block.
+     *
      * @param \eZ\Publish\Core\FieldType\Page\Parts\Block
+     *
      * @return \eZ\Publish\Core\FieldType\Page\Parts\Item[]
      */
     public function getWaitingBlockItems( Block $block )
     {
-        // TODO: Implement getWaitingBlockItems() method.
+        $dbHandler = $this->getConnection();
+        /** @var $q \ezcQuerySelect */
+        $q = $dbHandler->createSelectQuery();
+        $q
+            ->select( '*' )
+            ->from( $dbHandler->quoteTable( self::POOL_TABLE ) )
+            ->where(
+                $q->expr->eq( 'block_id', $q->bindValue( $block->id ) ),
+                $q->expr->eq( 'ts_visible', $q->bindValue( 0, null, \PDO::PARAM_INT ) ),
+                $q->expr->eq( 'ts_hidden', $q->bindValue( 0, null, \PDO::PARAM_INT ) )
+            )
+            ->orderBy( 'ts_publication' )
+            ->orderBy( 'priority' );
+
+        $stmt = $q->prepare();
+        $stmt->execute();
+        $rows = $stmt->fetchAll( \PDO::FETCH_ASSOC );
+        $items = array();
+        foreach ( $rows as $row )
+        {
+            $items[] = $this->buildBlockItem( $row );
+        }
+
+        return $items;
     }
 
     /**
+     * Returns archived items (that were previously displayed), for a given block.
+     *
      * @param \eZ\Publish\Core\FieldType\Page\Parts\Block
+     *
      * @return \eZ\Publish\Core\FieldType\Page\Parts\Item[]
      */
     public function getArchivedBlockItems( Block $block )
     {
-        // TODO: Implement getArchivedBlockItems() method.
+        $dbHandler = $this->getConnection();
+        /** @var $q \ezcQuerySelect */
+        $q = $dbHandler->createSelectQuery();
+        $q
+            ->select( '*' )
+            ->from( $dbHandler->quoteTable( self::POOL_TABLE ) )
+            ->where(
+                $q->expr->eq( 'block_id', $q->bindValue( $block->id ) ),
+                $q->expr->gt( 'ts_hidden', $q->bindValue( 0, null, \PDO::PARAM_INT ) )
+            )
+            ->orderBy( 'ts_hidden' );
+
+        $stmt = $q->prepare();
+        $stmt->execute();
+        $rows = $stmt->fetchAll( \PDO::FETCH_ASSOC );
+        $items = array();
+        foreach ( $rows as $row )
+        {
+            $items[] = $this->buildBlockItem( $row );
+        }
+
+        return $items;
+    }
+
+    /**
+     * Builds a Page\Parts\Item object from a row returned from ezm_pool table.
+     *
+     * @param array $row Hash representing a block item as stored in ezm_pool table.
+     *
+     * @return \eZ\Publish\Core\FieldType\Page\Parts\Item
+     */
+    protected function buildBlockItem( array $row )
+    {
+        return new Item(
+            array(
+                'id'                => $row['block_id'],
+                'contentId'         => $row['object_id'],
+                'locationId'        => $row['node_id'],
+                'priority'          => $row['priority'],
+                'publicationDate'   => new DateTime( "@{$row['ts_publication']}" ),
+                'visibilityDate'    => $row['ts_visible'] ? new DateTime( "@{$row['ts_visible']}" ) : null,
+                'hiddenDate'        => $row['ts_hidden'] ? new DateTime( "@{$row['ts_hidden']}" ) : null,
+                'rotationUntilDate' => $row['rotation_until'] ? new DateTime( "@{$row['rotation_until']}" ) : null,
+                'movedTo'           => $row['moved_to']
+            )
+        );
     }
 }
