50f53b957796c41fe6aa4f4a24b052bfbfb85c6c
Jérôme Vieilledent <lolautruche@gmail.com>
2013-03-15 17:03:22 +0100

    EZP-20424 - Added empty PageController

---
+++ eZ/Publish/Core/MVC/Symfony/Controller/PageController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * File containing the PageController class.
+ *
+ * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\Core\MVC\Symfony\Controller;
+
+use eZ\Publish\Core\FieldType\Page\Parts\Block;
+
+class PageController extends Controller
+{
+    public function viewBlock( Block $block, array $cacheSettings = array() )
+    {
+
+    }
+}
