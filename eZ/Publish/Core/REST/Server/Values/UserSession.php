ab69051cf776af03e3367045dd37bce08a234a95
Petar Spanja <petar.spanja@gmail.com>
2013-03-18 11:29:12 +0100

    Fixed: phpdoc c/p

--- eZ/Publish/Core/REST/Server/Values/UserSession.php
+++ eZ/Publish/Core/REST/Server/Values/UserSession.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * File containing the UserList class
+ * File containing the UserSession class
  *
  * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
