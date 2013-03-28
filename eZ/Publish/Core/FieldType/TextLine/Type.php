9fa64ccb63c85d1efd0fe752f732950f63f90eae
Petar Spanja <petar.spanja@gmail.com>
2013-03-28 08:58:19 +0100

    Fixed: disabled StringLengthValidator value should be false
    (cherry picked from commit bf83690493400b9e260a3127b2912eea7d5ed0e6)

--- eZ/Publish/Core/FieldType/TextLine/Type.php
+++ eZ/Publish/Core/FieldType/TextLine/Type.php
@@ -29,7 +29,7 @@ class Type extends FieldType
             ),
             'maxStringLength' => array(
                 'type' => 'int',
-                'default' => null
+                'default' => false
             )
         )
     );
