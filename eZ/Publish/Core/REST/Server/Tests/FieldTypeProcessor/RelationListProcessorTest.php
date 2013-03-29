84d63322ad328ea49ca29080eb396986d9a8327d
Petar Spanja <petar.spanja@gmail.com>
2013-03-29 19:40:20 +0100

    EZP-20608: implement processors for DateAndTime, Date, Time, Media, Relation, RelationList and XmlText FieldTypes
    (cherry picked from commit 72ee1a267e0ca8c47327634fac2856d487ceb975)

---
+++ eZ/Publish/Core/REST/Server/Tests/FieldTypeProcessor/RelationListProcessorTest.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * File containing the RelationListProcessor class
+ *
+ * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\Core\REST\Server\Tests\FieldTypeProcessor;
+
+use eZ\Publish\Core\REST\Server\Tests\BaseTest;
+use eZ\Publish\Core\REST\Server\FieldTypeProcessor\RelationListProcessor;
+
+class RelationListProcessorTest extends BaseTest
+{
+    protected $constants = array(
+        "SELECTION_BROWSE",
+        "SELECTION_DROPDOWN"
+    );
+
+    public function fieldSettingsHashes()
+    {
+        return array_map(
+            function( $constantName )
+            {
+                return array(
+                    array( "selectionMethod" => $constantName ),
+                    array( "selectionMethod" => constant( "eZ\\Publish\\Core\\FieldType\\RelationList\\Type::{$constantName}" ) )
+                );
+            },
+            $this->constants
+        );
+    }
+
+    /**
+     * @dataProvider fieldSettingsHashes
+     */
+    public function testPreProcessFieldSettingsHash( $inputSettings, $outputSettings )
+    {
+        $processor = $this->getProcessor();
+
+        $this->assertEquals(
+            $outputSettings,
+            $processor->preProcessFieldSettingsHash( $inputSettings )
+        );
+    }
+
+    /**
+     * @dataProvider fieldSettingsHashes
+     */
+    public function testPostProcessFieldSettingsHash( $outputSettings, $inputSettings )
+    {
+        $processor = $this->getProcessor();
+
+        $this->assertEquals(
+            $outputSettings,
+            $processor->postProcessFieldSettingsHash( $inputSettings )
+        );
+    }
+
+    /**
+     * @return \eZ\Publish\Core\REST\Server\FieldTypeProcessor\RelationListProcessor
+     */
+    protected function getProcessor()
+    {
+        return new RelationListProcessor;
+    }
+}
