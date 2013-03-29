db3abf4e975f950954e1e8bb2d6c67067c08d1b0
Joe Kepley <joe@blendinteractive.com>
2013-03-29 06:59:18 -0500

    Added exception handling, preserved service links between calls.

--- eZ/Publish/Core/FieldType/XmlText/Converter/EzLinkToHtml5.php
+++ eZ/Publish/Core/FieldType/XmlText/Converter/EzLinkToHtml5.php
@@ -10,21 +10,41 @@
 namespace eZ\Publish\Core\FieldType\XmlText\Converter;
 
 use eZ\Publish\Core\FieldType\XmlText\Converter;
-use eZ\Publish\Core\MVC\Symfony\View\Manager;
 use eZ\Publish\API\Repository\Repository;
+use Psr\Log\LoggerInterface;
+use eZ\Publish\API\Repository\Exceptions\NotFoundException as APINotFoundException;
+use eZ\Publish\API\Repository\Exceptions\UnauthorizedException as APIUnauthorizedException;
+
 use DOMDocument;
 
 class EzLinkToHtml5 implements Converter
 {
+    /**
+     * @var \eZ\Publish\API\Repository\LocationService
+     */
+    protected $locationService;
 
     /**
-     * @var \eZ\Publish\API\Repository\Repository
+     * @var \eZ\Publish\API\Repository\ContentService
      */
-    protected $repository;
+    protected $contentService;
 
-    public function __construct( Repository $repository )
+    /**
+     * @var \eZ\Publish\API\Repository\URLAliasService
+     */
+    protected $urlAliasService;
+
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    protected $logger;
+
+    public function __construct( Repository $repository, LoggerInterface $logger = null )
     {
-        $this->repository = $repository;
+        $this->locationService = $repository->getLocationService();
+        $this->contentService = $repository->getContentService();
+        $this->urlAliasService = $repository->getURLAliasService();
+        $this->logger = $logger;
     }
 
     /**
@@ -34,37 +54,74 @@ class EzLinkToHtml5 implements Converter
      *
      * @return string|null
      */
-    public function convert(DOMDocument $xmlDoc)
+    public function convert( DOMDocument $xmlDoc )
     {
-        $locationService = $this->repository->getLocationService();
-        $contentService = $this->repository->getContentService();
-        $urlAliasService = $this->repository->getURLAliasService();
-
         foreach ( $xmlDoc->getElementsByTagName( "link" ) as $link )
         {
-            $location = false;
+            $location = null;
 
             if ( $link->hasAttribute( 'object_id' ) )
             {
-                $content = $contentService->loadContent( $link->getAttribute( 'object_id' ) );
-                if ( $content )
+                try
                 {
-                    $location = $locationService->loadLocation( $content->contentInfo->mainLocationId );
+                    $content = $this->contentService->loadContent( $link->getAttribute( 'object_id' ) );
+                    $location = $this->locationService->loadLocation( $content->contentInfo->mainLocationId );
+                }
+                catch ( APINotFoundException $e )
+                {
+                    if ( $this->logger )
+                    {
+                        $this->logger->info(
+                            "While generating links for xmltext, could not locate " .
+                            "Content object with ID " . $link->getAttribute( 'object_id' )
+                        );
+                    }
+                }
+                catch ( APIUnauthorizedException $e )
+                {
+                    if ( $this->logger )
+                    {
+                        $this->logger->debug(
+                            "While generating links for xmltext, unauthorized to load " .
+                            "Content object with ID " . $link->getAttribute( 'object_id' )
+                        );
+                    }
                 }
             }
 
             if ( $link->hasAttribute( 'node_id' ) )
             {
-                $location = $locationService->loadLocation( $link->getAttribute( 'node_id' ) );
+                try
+                {
+                    $location = $this->locationService->loadLocation( $link->getAttribute( 'node_id' ) );
+                }
+                catch ( APINotFoundException $e )
+                {
+                    if ( $this->logger )
+                    {
+                        $this->logger->info(
+                            "While generating links for xmltext, could not locate " .
+                            "Location with ID " . $link->getAttribute( 'node_id' )
+                        );
+                    }
+                }
+                catch ( APIUnauthorizedException $e )
+                {
+                    if ( $this->logger )
+                    {
+                        $this->logger->debug(
+                            "While generating links for xmltext, unauthorized to load " .
+                            "Location with ID " . $link->getAttribute( 'node_id' )
+                        );
+                    }
+                }
             }
 
-            if ( $location )
+            if ( $location !== null )
             {
-                $urlAlias = $urlAliasService->reverseLookup( $location );
+                $urlAlias = $this->urlAliasService->reverseLookup( $location );
                 $link->setAttribute( 'url', $urlAlias->path );
             }
         }
-
-        return null;
     }
 }
