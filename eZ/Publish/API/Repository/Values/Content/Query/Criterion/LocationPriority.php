fc76f61570f05b6ecff752434430004662e7fe31
Michael <michael@thinkcreative.com>
2013-04-08 12:59:59 -0400

    add "location priority" filtering capability

---
+++ eZ/Publish/API/Repository/Values/Content/Query/Criterion/LocationPriority.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace eZ\Publish\API\Repository\Values\Content\Query\Criterion;
+
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion\Operator\Specifications;
+use eZ\Publish\API\Repository\Values\Content\Query\CriterionInterface;
+
+class LocationPriority extends Criterion implements CriterionInterface
+{
+
+    public function __construct($operator, $value) {
+        parent::__construct(null, $operator, $value);
+    }
+
+    public function getSpecifications() {
+        return array(
+            new Specifications(Operator::BETWEEN, Specifications::FORMAT_ARRAY, Specifications::TYPE_INTEGER),
+            new Specifications(Operator::GT, Specifications::FORMAT_SINGLE, Specifications::TYPE_INTEGER),
+            new Specifications(Operator::GTE, Specifications::FORMAT_SINGLE, Specifications::TYPE_INTEGER),
+            new Specifications(Operator::LT, Specifications::FORMAT_SINGLE, Specifications::TYPE_INTEGER),
+            new Specifications(Operator::LTE, Specifications::FORMAT_SINGLE, Specifications::TYPE_INTEGER),
+        );
+    }
+
+    public static function createFromQueryBuilder($target, $operator, $value) {
+        return new self($operator, $value);
+    }
+}
---
+++ eZ/Publish/Core/Persistence/Legacy/Content/Search/Gateway/CriterionHandler/LocationPriority.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace eZ\Publish\Core\Persistence\Legacy\Content\Search\Gateway\CriterionHandler;
+
+use eZ\Publish\Core\Persistence\Legacy\Content\Search\Gateway\CriterionHandler;
+use eZ\Publish\Core\Persistence\Legacy\Content\Search\Gateway\CriteriaConverter;
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
+use ezcQuerySelect;
+
+class LocationPriority extends CriterionHandler
+{
+    
+    public function accept( Criterion $criterion )
+    {
+        return $criterion instanceof Criterion\LocationPriority;
+    }
+
+    public function handle( CriteriaConverter $converter, ezcQuerySelect $query, Criterion $criterion )
+    {
+        $subSelect = $query->subSelect();
+        $column = $this->dbHandler->quoteColumn( 'id', 'ezcontentobject' );
+
+        switch ( $criterion->operator )
+        {
+            case Criterion\Operator::BETWEEN:
+                $subSelect
+                    ->select(
+                            $this->dbHandler->quoteColumn( 'contentobject_id' )
+                    )->from(
+                        $this->dbHandler->quoteTable( 'ezcontentobject_tree' )
+                    )->where(
+                        $query->expr->between(
+                            $this->dbHandler->quoteColumn( 'priority' ),
+                            $criterion->value[0],
+                            $criterion->value[1]
+                        )
+                    )
+                ;
+                break;
+
+            case Criterion\Operator::GT:
+            case Criterion\Operator::GTE:
+            case Criterion\Operator::LT:
+            case Criterion\Operator::LTE:
+                $operatorFunction = $this->comparatorMap[$criterion->operator];
+                $subSelect
+                    ->select(
+                            $this->dbHandler->quoteColumn( 'contentobject_id' )
+                    )->from(
+                        $this->dbHandler->quoteTable( 'ezcontentobject_tree' )
+                    )->where(
+                        $query->expr->$operatorFunction(
+                            $this->dbHandler->quoteColumn( 'priority' ),
+                            reset( $criterion->value )
+                        )
+                    )
+                ;
+        }
+
+        return $query->expr->in(
+            $this->dbHandler->quoteColumn( 'id', 'ezcontentobject' ),
+            $subSelect
+        );
+    }
+}
+
