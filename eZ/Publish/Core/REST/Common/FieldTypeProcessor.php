f126af89ee9330a5c49b491d0df60c11fb044257
Petar Spanja <petar.spanja@gmail.com>
2013-04-08 14:30:53 +0200

    EZP-20608: improved base FieldTypeProcessor methods docs
    (cherry picked from commit 2a5586dc3d7a49a3d7a6165c29e36ca0a562cb9f)

--- eZ/Publish/Core/REST/Common/FieldTypeProcessor.php
+++ eZ/Publish/Core/REST/Common/FieldTypeProcessor.php
@@ -17,12 +17,14 @@ abstract class FieldTypeProcessor
     /**
      * Perform manipulations on a received $incomingValueHash
      *
-     * This method is called by the REST server to allow a field type to pre
-     * process the given $incomingValueHash before it is handled by {@link
-     * eZ\Publish\SPI\FieldType\FieldType::fromHash()}. The $incomingValueHash
-     * can be expected to conform to the rules that need to apply to hashes
-     * accepted by fromHash(). The return value of this method replaces the
-     * $incomingValueHash.
+     * This method is called by the REST input parsers to allow a field
+     * type to pre process the given $incomingValueHash before it is handled by
+     * {@link eZ\Publish\SPI\FieldType\FieldType::fromHash()}. The
+     * $incomingValueHash can be expected to conform to the rules that need to
+     * apply to hashes accepted by fromHash(). The return value of this method
+     * replaces the $incomingValueHash.
+     *
+     * @see \eZ\Publish\Core\REST\Common\Input\FieldTypeParser
      *
      * @param mixed $incomingValueHash
      *
@@ -36,12 +38,14 @@ abstract class FieldTypeProcessor
     /**
      * Perform manipulations on an a generated $outgoingValueHash
      *
-     * This method is called by the REST server to allow a field type to post
-     * process the given $outgoingValueHash, which was previously generated
+     * This method is called by the REST output visitors to allow a field type to
+     * post process the given $outgoingValueHash, which was previously generated
      * using {@link eZ\Publish\SPI\FieldType\FieldType::toHash()}, before it is
      * sent to the client. The return value of this method replaces
      * $outgoingValueHash and must obey to the same rules as the original
      * $outgoingValueHash.
+     *
+     * @see \eZ\Publish\Core\REST\Common\Output\FieldTypeSerializer
      *
      * @param mixed $outgoingValueHash
      *
@@ -55,12 +59,14 @@ abstract class FieldTypeProcessor
     /**
      * Perform manipulations on a received $incomingSettingsHash
      *
-     * This method is called by the REST server to allow a field type to pre
-     * process the given $incomingSettingsHash before it is handled by {@link
-     * eZ\Publish\SPI\FieldType\FieldType::fieldSettingsFromHash()}. The
+     * This method is called by the REST input parsers to allow a field type to
+     * pre process the given $incomingSettingsHash before it is handled by
+     * {@link eZ\Publish\SPI\FieldType\FieldType::fieldSettingsFromHash()}. The
      * $incomingSettingsHash can be expected to conform to the rules that
      * need to apply to hashes accepted by fieldSettingsFromHash(). The return
      * value of this method replaces the $incomingSettingsHash.
+     *
+     * @see \eZ\Publish\Core\REST\Common\Input\FieldTypeParser
      *
      * @param mixed $incomingSettingsHash
      *
@@ -74,12 +80,14 @@ abstract class FieldTypeProcessor
     /**
      * Perform manipulations on a received $outgoingSettingsHash
      *
-     * This method is called by the REST server to allow a field type to post
+     * This method is called by the REST output visitors to allow a field type to post
      * process the given $outgoingSettingsHash, which was previously generated
      * using {@link eZ\Publish\SPI\FieldType\FieldType::fieldSettingsToHash()},
      * before it is sent to the client. The return value of this method replaces
      * $outgoingSettingsHash and must obey to the same rules as the original
      * $outgoingSettingsHash.
+     *
+     * @see \eZ\Publish\Core\REST\Common\Output\FieldTypeSerializer
      *
      * @param mixed $outgoingSettingsHash
      *
@@ -93,12 +101,14 @@ abstract class FieldTypeProcessor
     /**
      * Perform manipulations on a received $incomingValidatorConfigurationHash
      *
-     * This method is called by the REST server to allow a field type to pre
+     * This method is called by the REST input parsers to allow a field type to pre
      * process the given $incomingValidatorConfigurationHash before it is handled
      * by {@link eZ\Publish\SPI\FieldType\FieldType::validatorConfigurationFromHash()}.
      * The $incomingValidatorConfigurationHash can be expected to conform to the
      * rules that need to apply to hashes accepted by validatorConfigurationFromHash().
      * The return value of this method replaces the $incomingValidatorConfigurationHash.
+     *
+     * @see \eZ\Publish\Core\REST\Common\Input\FieldTypeParser
      *
      * @param mixed $incomingValidatorConfigurationHash
      *
@@ -112,12 +122,14 @@ abstract class FieldTypeProcessor
     /**
      * Perform manipulations on a received $outgoingValidatorConfigurationHash
      *
-     * This method is called by the REST server to allow a field type to post
+     * This method is called by the REST output visitors to allow a field type to post
      * process the given $outgoingValidatorConfigurationHash, which was previously generated
      * using {@link eZ\Publish\SPI\FieldType\FieldType::validatorConfigurationToHash()},
      * before it is sent to the client. The return value of this method replaces
      * $outgoingValidatorConfigurationHash and must obey to the same rules as the original
      * $outgoingValidatorConfigurationHash.
+     *
+     * @see \eZ\Publish\Core\REST\Common\Output\FieldTypeSerializer
      *
      * @param mixed $outgoingValidatorConfigurationHash
      *
