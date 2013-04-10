bb498af26f158e4cfb03007258bbfbc06d3b09a1
Michael <michael@thinkcreative.com>
2013-04-10 11:33:02 -0400

    add phpdoc comments

--- eZ/Publish/API/Repository/Values/Content/Query/Criterion/LocationPriority.php
+++ eZ/Publish/API/Repository/Values/Content/Query/Criterion/LocationPriority.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * File containing the eZ\Publish\API\Repository\Values\Content\Query\Criterion\LocationPriority class.
+ *
+ * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
 
 namespace eZ\Publish\API\Repository\Values\Content\Query\Criterion;
 
@@ -6,9 +13,23 @@ use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
 use eZ\Publish\API\Repository\Values\Content\Query\Criterion\Operator\Specifications;
 use eZ\Publish\API\Repository\Values\Content\Query\CriterionInterface;
 
+/**
+ * A criterion that matches content based on its own location priority
+ *
+ * Supported operators:
+ * - BETWEEN: matches content whose location priority is between (included) the TWO given priorities
+ * - GT, GTE: matches content whose location priority is greater than/greater than or equals the given priority
+ * - LT, LTE: matches content whose location priority is lower than/lower than or equals the given priority
+ */
 class LocationPriority extends Criterion implements CriterionInterface
 {
 
+    /**
+     * Creates a new LocationPriority criterion
+     *
+     * @param string $operator One of the Operator constants
+     * @param mixed $value The match value, either as an array of as a single value, depending on the operator
+     */
     public function __construct( $operator, $value )
     {
         parent::__construct( null, $operator, $value );
--- eZ/Publish/Core/Persistence/Legacy/Content/Search/Gateway/CriterionHandler/LocationPriority.php
+++ eZ/Publish/Core/Persistence/Legacy/Content/Search/Gateway/CriterionHandler/LocationPriority.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * File containing the EzcDatabase location priority criterion handler class
+ *
+ * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
 
 namespace eZ\Publish\Core\Persistence\Legacy\Content\Search\Gateway\CriterionHandler;
 
@@ -7,14 +14,35 @@ use eZ\Publish\Core\Persistence\Legacy\Content\Search\Gateway\CriteriaConverter;
 use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
 use ezcQuerySelect;
 
+/**
+ * Location priority criterion handler
+ */
 class LocationPriority extends CriterionHandler
 {
 
+    /**
+     * Check if this criterion handler accepts to handle the given criterion.
+     *
+     * @param \eZ\Publish\API\Repository\Values\Content\Query\Criterion$criterion
+     *
+     * @return boolean
+     */
     public function accept( Criterion $criterion )
     {
         return $criterion instanceof Criterion\LocationPriority;
     }
 
+    /**
+     * Generate query expression for a Criterion this handler accepts
+     *
+     * accept() must be called before calling this method.
+     *
+     * @param \eZ\Publish\Core\Persistence\Legacy\Content\Search\Gateway\CriteriaConverter$converter
+     * @param \ezcQuerySelect $query
+     * @param \eZ\Publish\API\Repository\Values\Content\Query\Criterion$criterion
+     *
+     * @return \ezcQueryExpression
+     */
     public function handle( CriteriaConverter $converter, ezcQuerySelect $query, Criterion $criterion )
     {
         $subSelect = $query->subSelect();
