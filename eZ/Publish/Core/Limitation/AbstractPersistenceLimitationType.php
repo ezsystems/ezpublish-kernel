76ec8fd9679a9b7e60a030708cd42229cee608ee
André R <ar@ez.no>
2013-04-11 23:05:59 +0200

    Implement LimitationType validation
    Reusing the validationError from FiledType
    Todo:
    - Move ValidationError it out of field type
    - Find a better exception for validation error ala ContentFieldValidation
    
    Warning: This commit breaks tests, some tests are using invalid values.

---
+++ eZ/Publish/Core/Limitation/AbstractPersistenceLimitationType.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * File containing the eZ\Publish\Core\Limitation\AbstractPersistenceLimitationType class.
+ *
+ * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\Core\Limitation;
+
+use eZ\Publish\SPI\Persistence\Handler as SPIPersistenceHandler;
+
+/**
+ * LocationLimitation is a Content limitation
+ */
+class AbstractPersistenceLimitationType
+{
+    /**
+     * @var \eZ\Publish\SPI\Persistence\Handler
+     */
+    protected $persistence;
+
+    /**
+     * @param \eZ\Publish\SPI\Persistence\Handler $persistence
+     */
+    public function __construct( SPIPersistenceHandler $persistence )
+    {
+        $this->persistence = $persistence;
+    }
+}
--- eZ/Publish/Core/Limitation/Tests/LocationLimitationTypeTest.php
+++ eZ/Publish/Core/Limitation/Tests/LocationLimitationTypeTest.php
@@ -11,6 +11,7 @@ namespace eZ\Publish\Core\Limitation\Tests;
 
 use eZ\Publish\API\Repository\Values\Content\Query\Criterion\Operator;
 use eZ\Publish\API\Repository\Values\User\Limitation\LocationLimitation;
+use eZ\Publish\API\Repository\Values\User\Limitation\ObjectStateLimitation;
 use eZ\Publish\Core\Limitation\LocationLimitationType;
 
 class LocationLimitationTypeTest extends Base
@@ -58,13 +59,25 @@ class LocationLimitationTypeTest extends Base
     /**
      * @depends testConstruct
      * @covers \eZ\Publish\Core\Limitation\LocationLimitationType::acceptValue
-     * @expectedException \eZ\Publish\API\Repository\Exceptions\NotImplementedException
      *
      * @param \eZ\Publish\Core\Limitation\LocationLimitationType $locationLimitationType
      */
     public function testAcceptValue( LocationLimitationType $locationLimitationType )
     {
         $locationLimitationType->acceptValue( new LocationLimitation() );
+        $locationLimitationType->acceptValue( new LocationLimitation( array() ) );
+    }
+
+    /**
+     * @depends testConstruct
+     * @covers \eZ\Publish\Core\Limitation\LocationLimitationType::acceptValue
+     * @expectedException \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException
+     *
+     * @param \eZ\Publish\Core\Limitation\LocationLimitationType $locationLimitationType
+     */
+    public function testAcceptValueFail( LocationLimitationType $locationLimitationType )
+    {
+        $locationLimitationType->acceptValue( new ObjectStateLimitation() );
     }
 
     /**
