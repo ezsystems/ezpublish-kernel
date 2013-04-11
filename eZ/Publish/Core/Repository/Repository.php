3ca708e2d6d9430c6769c35695512827e0423438
André R <ar@ez.no>
2013-04-11 14:14:52 +0200

    Fix typo in phpdoc example

--- eZ/Publish/Core/Repository/Repository.php
+++ eZ/Publish/Core/Repository/Repository.php
@@ -239,7 +239,7 @@ class Repository implements RepositoryInterface
      * Example use:
      *     $location = $repository->sudo(
      *         function( $repo ) use ( $locationId ){
-                   return $repo->getLocationService()->loadLocations( $locationId )
+                   return $repo->getLocationService()->loadLocation( $locationId )
      *         }
      *     );
      *
