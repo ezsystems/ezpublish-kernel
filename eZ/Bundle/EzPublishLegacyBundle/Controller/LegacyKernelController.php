95bc8203a4090086c6686c3cff791f5afcde5a2c
Yannick ROGER <yannick.roger@ez.no>
2013-04-17 13:29:50 +0200

    CS Fixes on LegacyKernelController.php

--- eZ/Bundle/EzPublishLegacyBundle/Controller/LegacyKernelController.php
+++ eZ/Bundle/EzPublishLegacyBundle/Controller/LegacyKernelController.php
@@ -27,9 +27,9 @@ class LegacyKernelController
 
     /**
      * Template declaration to wrap legacy responses in a Twig pagelayout (optional)
-     * Either a template declaration string or null/false to use legacy pagelayout 
+     * Either a template declaration string or null/false to use legacy pagelayout
      * Default is null.
-     * 
+     *
      * @var mixed
      */
     private $legacyLayout;
@@ -39,7 +39,7 @@ class LegacyKernelController
      *       Injection can be done through "parent service" feature for DIC : http://symfony.com/doc/master/components/dependency_injection/parentservices.html
      * @param \Closure $kernelClosure
      * @param \Symfony\Component\Templating\EngineInterface $templateEngine
-     * @param \eZ\Publish\Core\MVC\ConfigResolverInterface $configResolver     
+     * @param \eZ\Publish\Core\MVC\ConfigResolverInterface $configResolver
      * @param mixed $legacyLayout
      */
     public function __construct( \Closure $kernelClosure, EngineInterface $templateEngine, ConfigResolverInterface $configResolver )
