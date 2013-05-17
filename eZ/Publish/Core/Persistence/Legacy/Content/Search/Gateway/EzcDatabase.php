9cc9a9fdd6178e3e62356a7aa8c40ae616e0579c
Petar Spanja <petar.spanja@gmail.com>
2013-05-17 11:03:32 +0200

    EZP-20848: load only searchable fields and throw InvalidArgumentException when Field criterion target is not applicable
    (cherry-picked from commit 7ebd1b1faefa082dbc626e9428ed4505852ab1ec)

--- eZ/Publish/Core/Persistence/Legacy/Content/Search/Gateway/EzcDatabase.php
+++ eZ/Publish/Core/Persistence/Legacy/Content/Search/Gateway/EzcDatabase.php
@@ -100,6 +100,7 @@ class EzcDatabase extends Gateway
      *
      * @todo Check Query recreation in this method. Something breaks if we reuse
      *       the query, after we have added the applyJoin() stuff here.
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException if Criterion is not applicable to its target
      *
      * @param Criterion $criterion
      * @param int $offset
