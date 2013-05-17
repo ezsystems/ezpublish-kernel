049d4918efa17ff45e6eb4e08ea43d52b60afa36
Damien Pobel <dp@ez.no>
2013-05-17 15:32:36 +0200

    Fix EZP-20-877: Improve unit test code coverage of eZ\Publish\Core\SignalSlot
    Added unit tests for SignalSlot field type service

---
+++ eZ/Publish/Core/SignalSlot/Tests/FieldTypeServiceTest.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * File containing the FieldTypeServiceTest class.
+ *
+ * @copyright Copyright (C) 2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+namespace eZ\Publish\Core\SignalSlot\Tests;
+
+use eZ\Publish\Core\FieldType\TextLine\Type;
+
+use eZ\Publish\Core\SignalSlot\SignalDispatcher;
+use eZ\Publish\Core\SignalSlot\FieldTypeService;
+use eZ\Publish\API\Repository\FieldTypeService as SectionServiceInterface;
+use eZ\Publish\Core\SignalSlot\Tests\ServiceTest;
+
+class FieldTypeServiceTest extends ServiceTest
+{
+    protected function getServiceMock()
+    {
+        return $this->getMock(
+            'eZ\\Publish\\API\\Repository\\FieldTypeService'
+        );
+    }
+
+    protected function getSignalSlotService( $coreService, SignalDispatcher $dispatcher )
+    {
+        return new FieldTypeService( $coreService, $dispatcher );
+    }
+
+    public function serviceProvider()
+    {
+        $fieldType = new Type();
+
+        return array(
+            array(
+                'getFieldTypes',
+                array(),
+                array( $fieldType ),
+                0
+            ),
+            array(
+                'getFieldType',
+                array( 'ezstring' ),
+                $fieldType,
+                0
+            ),
+            array(
+                'hasFieldType',
+                array( 'ezstring' ),
+                true,
+                0
+            )
+        );
+    }
+}
