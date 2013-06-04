861fea67e2bd93cc0ca711872d55890a99e33657
Petar Spanja <petar.spanja@gmail.com>
2013-06-04 16:31:23 +0200

    EZP-20978: prevent argument mutation

--- eZ/Publish/Core/Repository/ContentService.php
+++ eZ/Publish/Core/Repository/ContentService.php
@@ -1162,6 +1162,8 @@ class ContentService implements ContentServiceInterface
      */
     public function updateContent( APIVersionInfo $versionInfo, APIContentUpdateStruct $contentUpdateStruct )
     {
+        $contentUpdateStruct = clone $contentUpdateStruct;
+
         /** @var $content \eZ\Publish\Core\Repository\Values\Content\Content */
         $content = $this->loadContent(
             $versionInfo->getContentInfo()->id,
