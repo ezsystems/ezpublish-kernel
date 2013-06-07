2c3906885c0c89e30f5640c5a9fac881a22f92ef
Petar Spanja <petar.spanja@gmail.com>
2013-06-07 12:56:06 +0200

    EZP-20979: enable Legacy Storage to implicitly create fields for main and initial language

--- eZ/Publish/Core/Persistence/Legacy/Content/Gateway/EzcDatabase.php
+++ eZ/Publish/Core/Persistence/Legacy/Content/Gateway/EzcDatabase.php
@@ -153,7 +153,9 @@ class EzcDatabase extends Gateway
             $this->dbHandler->quoteColumn( 'language_mask' ),
             $q->bindValue(
                 $this->generateLanguageMask(
-                    $struct->fields, $struct->alwaysAvailable
+                    $struct->fields,
+                    $this->languageHandler->load( $struct->initialLanguageId )->languageCode,
+                    $struct->alwaysAvailable
                 ),
                 null,
                 \PDO::PARAM_INT
@@ -175,9 +177,9 @@ class EzcDatabase extends Gateway
      *
      * @return int
      */
-    protected function generateLanguageMask( array $fields, $alwaysAvailable )
+    protected function generateLanguageMask( array $fields, $initialLanguageCode, $alwaysAvailable )
     {
-        $languages = array();
+        $languages = array( $initialLanguageCode => true );
         foreach ( $fields as $field )
         {
             if ( isset( $languages[$field->languageCode] ) )
@@ -227,7 +229,11 @@ class EzcDatabase extends Gateway
             $q->bindValue( $versionInfo->status, null, \PDO::PARAM_INT )
         )->set(
             $this->dbHandler->quoteColumn( 'initial_language_id' ),
-            $q->bindValue( $this->languageHandler->loadByLanguageCode( $versionInfo->initialLanguageCode )->id, null, \PDO::PARAM_INT )
+            $q->bindValue(
+                $this->languageHandler->loadByLanguageCode( $versionInfo->initialLanguageCode )->id,
+                null,
+                \PDO::PARAM_INT
+            )
         )->set(
             $this->dbHandler->quoteColumn( 'contentobject_id' ),
             $q->bindValue( $versionInfo->contentInfo->id, null, \PDO::PARAM_INT )
@@ -239,7 +245,9 @@ class EzcDatabase extends Gateway
             $this->dbHandler->quoteColumn( 'language_mask' ),
             $q->bindValue(
                 $this->generateLanguageMask(
-                    $fields, $versionInfo->contentInfo->alwaysAvailable
+                    $fields,
+                    $versionInfo->initialLanguageCode,
+                    $versionInfo->contentInfo->alwaysAvailable
                 ),
                 null,
                 \PDO::PARAM_INT
@@ -351,7 +359,15 @@ class EzcDatabase extends Gateway
             $this->dbHandler->quoteColumn( 'language_mask' ),
             $q->expr->bitOr(
                 $this->dbHandler->quoteColumn( 'language_mask' ),
-                $q->bindValue( $this->generateLanguageMask( $struct->fields, false ), null, \PDO::PARAM_INT )
+                $q->bindValue(
+                    $this->generateLanguageMask(
+                        $struct->fields,
+                        $this->languageHandler->load( $struct->initialLanguageId )->languageCode,
+                        false
+                    ),
+                    null,
+                    \PDO::PARAM_INT
+                )
             )
         )->where(
             $q->expr->lAnd(
