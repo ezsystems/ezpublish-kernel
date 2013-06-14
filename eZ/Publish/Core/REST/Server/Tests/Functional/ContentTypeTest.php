a838f86dc9901dd845511ed5da8ab4b5442f36dd
Bertrand Dunogier <bd@ez.no>
2013-06-14 22:26:28 +0200

    EZP-21018: REST Tests, renamed tests

---
+++ eZ/Publish/Core/REST/Server/Tests/Functional/ContentTypeTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace eZ\Publish\Core\Server\Tests\Functional;
+
+use eZ\Publish\Core\REST\Server\Tests\Functional\TestCase as RESTFunctionalTestCase;
+
+class ContentTypeTest extends RESTFunctionalTestCase
+{
+    public function testGetImageVariation()
+    {
+        self::markTestIncomplete( "@todo implement" );
+    }
+
+}
