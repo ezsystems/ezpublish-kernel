ad04cfbcedaacfe290c40184e401badc443629af
Bertrand Dunogier <bd@ez.no>
2013-06-24 11:47:01 +0200

    EZP-21107: added CompilerPass tests

---
+++ eZ/Bundle/EzPublishRestBundle/Tests/DependencyInjection/Compiler/InputParserPassTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * File containing the InputParserPassTest class.
+ *
+ * @copyright Copyright (C) 2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Bundle\EzPublishRestBundle\Tests\DependencyInjection\Compiler;
+
+use eZ\Bundle\EzPublishRestBundle\DependencyInjection\Compiler\InputParserPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use \PHPUnit_Framework_TestCase;
+
+class InputParserPassTest extends PHPUnit_Framework_TestCase
+{
+    public function testProcess()
+    {
+        $visitorDefinition = new Definition();
+        $visitorDefinition->addTag( 'ezpublish_rest.input.parser', array( 'mediaType' => 'application/vnd.ez.api.UnitTest' ) );
+
+        $containerBuilder = new ContainerBuilder();
+        $containerBuilder->addDefinitions(
+            array(
+                'ezpublish_rest.input.parsing_dispatcher' => new Definition(),
+                'ezpublish_rest.input.parser.UnitTest' => $visitorDefinition,
+            )
+        );
+
+        $compilerPass = new InputParserPass;
+        $compilerPass->process( $containerBuilder );
+
+        $dispatcherMethodCalls = $containerBuilder
+            ->getDefinition( 'ezpublish_rest.input.parsing_dispatcher' )
+            ->getMethodCalls();
+        self::assertTrue( isset( $dispatcherMethodCalls[0][0] ), "Failed asserting that dispatcher has a method call" );
+        self::assertEquals( 'addParser', $dispatcherMethodCalls[0][0], "Failed asserting that called method is 'addParser'" );
+        self::assertInstanceOf( 'Symfony\\Component\\DependencyInjection\\Reference', $dispatcherMethodCalls[0][1][1], "Failed asserting that method call is to a Reference object" );
+
+        self::assertEquals( 'ezpublish_rest.input.parser.unittest', $dispatcherMethodCalls[0][1][1]->__toString(), "Failed asserting that Referenced service is 'ezpublish_rest.input.parser.UnitTest'" );
+    }
+}
