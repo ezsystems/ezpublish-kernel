9f70f33fedd6ee139c68d7e38e9a93394b9c350c
Petar Spanja <petar.spanja@gmail.com>
2013-06-28 00:41:44 +0200

    EZP-21036: refactored fieldtype: Integer

--- eZ/Publish/Core/FieldType/Integer/Type.php
+++ eZ/Publish/Core/FieldType/Integer/Type.php
@@ -13,6 +13,8 @@ use eZ\Publish\Core\FieldType\FieldType;
 use eZ\Publish\Core\FieldType\ValidationError;
 use eZ\Publish\API\Repository\Values\ContentType\FieldDefinition;
 use eZ\Publish\Core\Base\Exceptions\InvalidArgumentType;
+use eZ\Publish\SPI\FieldType\Value as SPIValue;
+use eZ\Publish\Core\FieldType\Value as BaseValue;
 
 /**
  * Integer field types
@@ -101,7 +103,7 @@ class Type extends FieldType
      *
      * @return \eZ\Publish\SPI\FieldType\ValidationError[]
      */
-    public function validate( FieldDefinition $fieldDefinition, $fieldValue )
+    public function validate( FieldDefinition $fieldDefinition, SPIValue $fieldValue )
     {
         $validationErrors = array();
 
@@ -160,14 +162,12 @@ class Type extends FieldType
      * It will be used to generate content name and url alias if current field is designated
      * to be used in the content name/urlAlias pattern.
      *
-     * @param mixed $value
+     * @param \eZ\Publish\Core\FieldType\Integer\Value $value
      *
-     * @return mixed
+     * @return string
      */
-    public function getName( $value )
+    public function getName( SPIValue $value )
     {
-        $value = $this->acceptValue( $value );
-
         return (string)$value->value;
     }
 
@@ -183,67 +183,85 @@ class Type extends FieldType
     }
 
     /**
-     * Returns if the given $value is considered empty by the field type
+     * Implements the core of {@see isEmptyValue()}.
      *
      * @param mixed $value
      *
      * @return boolean
      */
-    public function isEmptyValue( $value )
+    public function isEmptyValue( SPIValue $value )
     {
-        return $value === null || $value->value === null;
+        return $value->value === null;
     }
 
     /**
-     * Implements the core of {@see acceptValue()}.
+     * Inspects given $inputValue and potentially converts it into a dedicated value object.
      *
-     * @param mixed $inputValue
+     * @param int|\eZ\Publish\Core\FieldType\Integer\Value $inputValue
      *
      * @return \eZ\Publish\Core\FieldType\Integer\Value The potentially converted and structurally plausible value.
      */
-    protected function internalAcceptValue( $inputValue )
+    protected function createValueFromInput( $inputValue )
     {
         if ( is_int( $inputValue ) )
         {
             $inputValue = new Value( $inputValue );
-        }
-        else if ( !$inputValue instanceof Value )
-        {
-            throw new InvalidArgumentType(
-                '$inputValue',
-                'eZ\\Publish\\Core\\FieldType\\Integer\\Value',
-                $inputValue
-            );
-        }
-
-        if ( $this->isEmptyValue( $inputValue ) )
-        {
-            return $this->getEmptyValue();
-        }
-
-        if ( !is_integer( $inputValue->value ) )
-        {
-            throw new InvalidArgumentType(
-                '$inputValue->value',
-                'integer',
-                $inputValue->value
-            );
         }
 
         return $inputValue;
     }
 
     /**
+     * Throws an exception if the given $value is not an instance of the supported value subtype.
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException If the parameter is not an instance of the supported value subtype.
+     *
+     * @param mixed $value A value returned by {@see createValueFromInput()}.
+     *
+     * @return void
+     */
+    protected function checkValueType( $value )
+    {
+        if ( !$value instanceof Value )
+        {
+            throw new InvalidArgumentType(
+                '$value',
+                'eZ\\Publish\\Core\\FieldType\\Integer\\Value',
+                $value
+            );
+        }
+    }
+
+    /**
+     * Throws an exception if value structure is not of expected format.
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException If the value does not match the expected structure.
+     *
+     * @param \eZ\Publish\Core\FieldType\Integer\Value $value
+     *
+     * @return void
+     */
+    public function checkValueStructure( BaseValue $value )
+    {
+        if ( !is_int( $value->value ) )
+        {
+            throw new InvalidArgumentType(
+                '$value->value',
+                'integer',
+                $value->value
+            );
+        }
+    }
+
+    /**
      * Returns information for FieldValue->$sortKey relevant to the field type.
+     *
+     * @param \eZ\Publish\Core\FieldType\Integer\Value $value
      *
      * @return array
      */
-    protected function getSortInfo( $value )
+    protected function getSortInfo( BaseValue $value )
     {
-        if ( $value === null )
-        {
-            return null;
-        }
         return $value->value;
     }
 
@@ -258,7 +276,7 @@ class Type extends FieldType
     {
         if ( $hash === null )
         {
-            return null;
+            return $this->getEmptyValue();
         }
         return new Value( $hash );
     }
@@ -270,7 +288,7 @@ class Type extends FieldType
      *
      * @return mixed
      */
-    public function toHash( $value )
+    public function toHash( SPIValue $value )
     {
         if ( $this->isEmptyValue( $value ) )
         {
