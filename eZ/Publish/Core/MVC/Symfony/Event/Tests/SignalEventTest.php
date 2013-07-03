2e946e7e767224c39cb8155005b336a7b00bcceb
Jérôme Vieilledent <lolautruche@gmail.com>
2013-07-03 21:31:35 +0200

    EZP-20069 - Added unit tests

---
+++ eZ/Publish/Core/MVC/Symfony/Event/Tests/SignalEventTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * File containing the SignalEventTest class.
+ *
+ * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\Core\MVC\Symfony\Event\Tests;
+
+use eZ\Publish\Core\MVC\Symfony\Event\SignalEvent;
+
+class SignalEventTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers eZ\Publish\Core\MVC\Symfony\Event\SignalEvent::__construct
+     * @covers eZ\Publish\Core\MVC\Symfony\Event\SignalEvent::getSignal
+     */
+    public function testGetSignal()
+    {
+        $signal = $this->getMock( 'eZ\\Publish\\Core\\SignalSlot\\Signal' );
+        $event = new SignalEvent( $signal );
+        $this->assertSame( $signal, $event->getSignal() );
+    }
+}
