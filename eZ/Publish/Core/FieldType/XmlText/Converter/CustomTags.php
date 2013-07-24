0ab7eb359fadd1fca5ddb0e14e3e08afacd5cf6b
Jérôme Vieilledent <lolautruche@gmail.com>
2013-07-24 17:35:54 +0200

    Fix 21334: Wrong XSLT rendering for some ezxml elements

---
+++ eZ/Publish/Core/FieldType/XmlText/Converter/CustomTags.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * File containing the CustomTags class.
+ *
+ * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\Core\FieldType\XmlText\Converter;
+
+use DOMDocument;
+use eZ\Publish\Core\FieldType\XmlText\Converter;
+
+/**
+ * Pre-converter for custom tags, used to determine if it's supposed to be inline or not.
+ * "block" custom tags are always contained in a <paragraph> declaring a local "tmp" namespace.
+ */
+class CustomTags implements Converter
+{
+    public function convert( DOMDocument $xmlDoc )
+    {
+        /** @var \DOMElement $customTag */
+        foreach ( $xmlDoc->getElementsByTagName( 'custom' ) as $customTag )
+        {
+            if ( $customTag->parentNode->lookupNamespaceUri( 'tmp' ) !== null )
+            {
+                $customTag->setAttribute( 'inline', 'false' );
+            }
+        }
+    }
+}
