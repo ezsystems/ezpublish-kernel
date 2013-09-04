419d92dffc22739c661e64d67a505318cde9fcf8
Kore Nordmann <mail@kore-nordmann.de>
2013-09-04 12:48:34 +0200

    Introduced matchAll criterion

---
+++ eZ/Publish/API/Repository/Values/Content/Query/Criterion/MatchAll.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * File containing the eZ\Publish\API\Repository\Values\Content\Query\Criterion\ContentId class.
+ *
+ * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\API\Repository\Values\Content\Query\Criterion;
+
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion\Operator\Specifications;
+use eZ\Publish\API\Repository\Values\Content\Query\CriterionInterface;
+
+/**
+ * A criterion that just matches everything
+ */
+class MatchAll extends Criterion implements CriterionInterface
+{
+    /**
+     * Creates a new MatchAll criterion
+     */
+    public function __construct()
+    {
+        parent::__construct( null, null, null );
+    }
+
+    public function getSpecifications()
+    {
+        return array();
+    }
+
+    public static function createFromQueryBuilder( $target, $operator, $value )
+    {
+        return new self();
+    }
+}
