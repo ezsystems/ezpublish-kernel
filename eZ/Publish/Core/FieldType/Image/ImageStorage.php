ddd61a273bf26ea7e35cf4d88ae7969042548aa2
Bertrand Dunogier <bd@ez.no>
2013-09-12 14:59:22 +0200

    Fix EZP-21522: wrong usage of image uri/id
    Also adds leading slash to FileSystem IOHandler URI
    
    Makes it work like the Legacy one so that API integration
    tests actually behaves (and fails) in the same way.
    
    This change makes the integration tests fail without the
    fix in this commit.

--- eZ/Publish/Core/FieldType/Image/ImageStorage.php
+++ eZ/Publish/Core/FieldType/Image/ImageStorage.php
@@ -128,7 +128,7 @@ class ImageStorage extends GatewayBasedStorage
             $field->value->externalData['mimeType'] = $binaryFile->mimeType;
             $field->value->externalData['imageId'] = $versionInfo->contentInfo->id . '-' . $field->id;
             $field->value->externalData['uri'] = $binaryFile->uri;
-            $field->value->externalData['id'] = $binaryFile->uri;
+            $field->value->externalData['id'] = ltrim( $binaryFile->uri, '/' );
 
             $field->value->data = array_merge(
                 $field->value->externalData,
