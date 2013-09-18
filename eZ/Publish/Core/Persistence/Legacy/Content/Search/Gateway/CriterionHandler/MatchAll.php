52d3928121924c000e4ecd70aaa32b606133029d
Kore Nordmann <mail@kore-nordmann.de>
2013-09-18 11:01:42 +0200

    Fixed file doc comments

--- eZ/Publish/Core/Persistence/Legacy/Content/Search/Gateway/CriterionHandler/MatchAll.php
+++ eZ/Publish/Core/Persistence/Legacy/Content/Search/Gateway/CriterionHandler/MatchAll.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * File containing the EzcDatabase content id criterion handler class
+ * File containing the EzcDatabase match all criterion handler class
  *
  * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
