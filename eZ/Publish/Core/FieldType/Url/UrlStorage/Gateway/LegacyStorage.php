684af29cbba6b3ab35cc7f094ff4d8f3e57782b4
Petar Spanja <petar.spanja@gmail.com>
2013-09-19 17:34:51 +0200

    Fixed: class description (cherry picked from commit 3f276a5cde77868fa3ab7efc0c604a13447caa39)

--- eZ/Publish/Core/FieldType/Url/UrlStorage/Gateway/LegacyStorage.php
+++ eZ/Publish/Core/FieldType/Url/UrlStorage/Gateway/LegacyStorage.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * File containing the abstract Gateway class
+ * File containing the Url LegacyStorage Gateway
  *
  * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
@@ -15,7 +15,7 @@ use eZ\Publish\SPI\Persistence\Content\VersionInfo;
 use eZ\Publish\SPI\Persistence\Content\Field;
 
 /**
- *
+ * Url field type external storage gateway implementation using Zeta Database Component.
  */
 class LegacyStorage extends Gateway
 {
