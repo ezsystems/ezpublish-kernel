5dea28b546adf323b0c1114a7ae77b6321f1b71e
Petar Spanja <petar.spanja@gmail.com>
2013-09-24 09:50:52 +0200

    Fixed: removed unnecessary import

--- eZ/Publish/Core/FieldType/User/Type.php
+++ eZ/Publish/Core/FieldType/User/Type.php
@@ -13,7 +13,6 @@ use eZ\Publish\Core\FieldType\FieldType;
 use eZ\Publish\SPI\FieldType\Value as SPIValue;
 use eZ\Publish\SPI\Persistence\Content\FieldValue;
 use eZ\Publish\Core\FieldType\Value as BaseValue;
-use eZ\Publish\Core\Base\Exceptions\InvalidArgumentType;
 
 /**
  * The User field type.
