27ea5f3aedcdde96a8ecc180e4328e5cf40493d2
Petar Spanja <petar.spanja@gmail.com>
2013-09-25 09:54:36 +0200

    EZP-21575: added Legacy cache clearing slot for UserService::updateUser() (cherry picked from commit d11dc3de878c0af9ceabf711b4f13ae401483d2b)

---
+++ eZ/Publish/Core/SignalSlot/Slot/LegacyUpdateUserSlot.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * File containing the LegacyUpdateUserSlot class
+ *
+ * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\Core\SignalSlot\Slot;
+
+use eZ\Publish\Core\SignalSlot\Signal;
+use eZContentCacheManager;
+use eZContentObject;
+use eZContentOperationCollection;
+
+/**
+ * A legacy slot handling UpdateUserSignal.
+ */
+class LegacyUpdateUserSlot extends AbstractLegacySlot
+{
+    /**
+     * Receive the given $signal and react on it
+     *
+     * @param \eZ\Publish\Core\SignalSlot\Signal $signal
+     *
+     * @return void
+     */
+    public function receive( Signal $signal )
+    {
+        if ( !$signal instanceof Signal\UserService\UpdateUserSignal )
+        {
+            return;
+        }
+
+        $kernel = $this->getLegacyKernel();
+        $kernel->runCallback(
+            function () use ( $signal )
+            {
+                eZContentCacheManager::clearContentCacheIfNeeded( $signal->userId );
+                eZContentOperationCollection::registerSearchObject( $signal->userId );
+                eZContentObject::clearCache();// Clear all object memory cache to free memory
+            },
+            false
+        );
+    }
+}
