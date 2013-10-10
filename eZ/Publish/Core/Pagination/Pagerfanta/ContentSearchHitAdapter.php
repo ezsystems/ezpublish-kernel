232ba94854e5272355a5e4ef71e731c2cb1ac372
Jérôme Vieilledent <lolautruche@gmail.com>
2013-10-10 15:50:13 +0200

    Fix EZP-21723: As a developer, I want to use PagerFanta to do my pagination

---
+++ eZ/Publish/Core/Pagination/Pagerfanta/ContentSearchHitAdapter.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * File containing the ContentSearchHitAdapter class.
+ *
+ * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\Core\Pagination\Pagerfanta;
+
+use eZ\Publish\API\Repository\Values\Content\Query;
+use eZ\Publish\API\Repository\SearchService;
+use Pagerfanta\Adapter\AdapterInterface;
+
+/**
+ * Pagerfanta adapter for eZ Publish content search.
+ * Will return results as SearchHit objects.
+ */
+class ContentSearchHitAdapter implements AdapterInterface
+{
+    /**
+     * @var \eZ\Publish\API\Repository\Values\Content\Query
+     */
+    private $query;
+
+    /**
+     * @var \eZ\Publish\API\Repository\SearchService
+     */
+    private $searchService;
+
+    public function __construct( Query $query, SearchService $searchService )
+    {
+        $this->query = $query;
+        $this->searchService = $searchService;
+    }
+
+    /**
+     * Returns the number of results.
+     *
+     * @return integer The number of results.
+     */
+    public function getNbResults()
+    {
+        $countQuery = clone $this->query;
+        $countQuery->limit = 0;
+        return $this->searchService->findContent( $countQuery )->totalCount;
+    }
+
+    /**
+     * Returns as slice of the results, as SearchHit objects.
+     *
+     * @param integer $offset The offset.
+     * @param integer $length The length.
+     *
+     * @return \eZ\Publish\API\Repository\Values\Content\Search\SearchHit The slice.
+     */
+    public function getSlice( $offset, $length )
+    {
+        $query = clone $this->query;
+        $query->offset = $offset;
+        $query->limit = $length;
+
+        return $this->searchService->findContent( $query )->searchHits;
+    }
+}
