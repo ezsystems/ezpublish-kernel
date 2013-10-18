ad68f984d436c0b656e0c26701b948328c74989b
Petar Spanja <petar.spanja@gmail.com>
2013-10-18 15:15:46 +0200

    EZP-21733: unit tests for throwing LimitationValidationException in RoleService (cherry picked from commit 59e3d2adb745b47c8161d0250e9c724ee3ba7ab4)

--- eZ/Publish/Core/Repository/Tests/Service/Mock/RoleTest.php
+++ eZ/Publish/Core/Repository/Tests/Service/Mock/RoleTest.php
@@ -10,12 +10,215 @@
 namespace eZ\Publish\Core\Repository\Tests\Service\Mock;
 
 use eZ\Publish\Core\Repository\Tests\Service\Mock\Base as BaseServiceMockTest;
+use eZ\Publish\Core\Base\Exceptions\NotFoundException;
 
 /**
  * Mock test case for Role service
  */
 class RoleTest extends BaseServiceMockTest
 {
+    /**
+     * Test for the createRole() method.
+     *
+     * @covers \eZ\Publish\Core\Repository\RoleService::createRole
+     * @covers \eZ\Publish\Core\Repository\RoleService::validateRoleCreateStructLimitations
+     * @covers \eZ\Publish\Core\Repository\RoleService::validateLimitations
+     * @covers \eZ\Publish\Core\Repository\RoleService::validateLimitation
+     * @expectedException \eZ\Publish\API\Repository\Exceptions\LimitationValidationException
+     */
+    public function testCreateRoleThrowsLimitationValidationException()
+    {
+        $repository = $this->getRepositoryMock();
+        $roleServiceMock = $this->getPartlyMockedRoleService( array( "loadRoleByIdentifier" ) );
+        /** @var \eZ\Publish\API\Repository\Values\User\RoleCreateStruct $roleCreateStructMock */
+        $roleCreateStructMock = $this->getMock( "eZ\\Publish\\API\\Repository\\Values\\User\\RoleCreateStruct" );
+        $policyCreateStructMock = $this->getMock( "eZ\\Publish\\API\\Repository\\Values\\User\\PolicyCreateStruct" );
+        $limitationMock = $this->getMock( "eZ\\Publish\\API\\Repository\\Values\\User\\Limitation\\RoleLimitation" );
+        $limitationTypeMock = $this->getMock( "eZ\\Publish\\SPI\\Limitation\\Type" );
+
+        /** @var \eZ\Publish\API\Repository\Values\User\PolicyCreateStruct $policyCreateStructMock */
+        $policyCreateStructMock->module = "mockModule";
+        $policyCreateStructMock->function = "mockFunction";
+        $roleCreateStructMock->identifier = "mockIdentifier";
+        $roleServiceMock->expects( $this->once() )
+            ->method( "loadRoleByIdentifier" )
+            ->with( $this->equalTo( "mockIdentifier" ) )
+            ->will( $this->throwException( new NotFoundException( "Role", "mockIdentifier" ) ) );
+
+        $limitationTypeMock->expects( $this->once() )
+            ->method( "acceptValue" )
+            ->with( $this->equalTo( $limitationMock ) );
+        $limitationTypeMock->expects( $this->once() )
+            ->method( "validate" )
+            ->with( $this->equalTo( $limitationMock ) )
+            ->will( $this->returnValue( array( 42 ) ) );
+
+        $limitationMock->expects( $this->any() )
+            ->method( "getIdentifier" )
+            ->will( $this->returnValue( "mockIdentifier" ) );
+
+        $settings = array(
+            "limitationMap" => array( "mockModule" => array( "mockFunction" => array( "mockIdentifier" => true ) ) ),
+            "limitationTypes" => array( "mockIdentifier" => $limitationTypeMock )
+        );
+        $this->setConfiguration( $roleServiceMock, $settings );
+
+        /** @var \PHPUnit_Framework_MockObject_MockObject $roleCreateStructMock */
+        $roleCreateStructMock->expects( $this->once() )
+            ->method( "getPolicies" )
+            ->will( $this->returnValue( array( $policyCreateStructMock ) ) );
+
+        /** @var \PHPUnit_Framework_MockObject_MockObject $policyCreateStructMock */
+        $policyCreateStructMock->expects( $this->once() )
+            ->method( "getLimitations" )
+            ->will( $this->returnValue( array( $limitationMock ) ) );
+
+        $repository->expects( $this->once() )
+            ->method( "hasAccess" )
+            ->with(
+                $this->equalTo( "role" ),
+                $this->equalTo( "create" )
+            )->will( $this->returnValue( true ) );
+
+        /** @var \eZ\Publish\API\Repository\Values\User\RoleCreateStruct $roleCreateStructMock */
+        $roleServiceMock->createRole( $roleCreateStructMock );
+    }
+
+    /**
+     * Test for the addPolicy() method.
+     *
+     * @covers \eZ\Publish\Core\Repository\RoleService::addPolicy
+     * @covers \eZ\Publish\Core\Repository\RoleService::validateLimitations
+     * @covers \eZ\Publish\Core\Repository\RoleService::validateLimitation
+     * @expectedException \eZ\Publish\API\Repository\Exceptions\LimitationValidationException
+     */
+    public function testAddPolicyThrowsLimitationValidationException()
+    {
+        $repository = $this->getRepositoryMock();
+        $roleServiceMock = $this->getPartlyMockedRoleService( array( "loadRole" ) );
+        $roleMock = $this->getMock( "eZ\\Publish\\API\\Repository\\Values\\User\\Role" );
+        $policyCreateStructMock = $this->getMock( "eZ\\Publish\\API\\Repository\\Values\\User\\PolicyCreateStruct" );
+        $limitationMock = $this->getMock( "eZ\\Publish\\API\\Repository\\Values\\User\\Limitation\\RoleLimitation" );
+        $limitationTypeMock = $this->getMock( "eZ\\Publish\\SPI\\Limitation\\Type" );
+
+        $roleMock->expects( $this->any() )
+            ->method( "__get" )
+            ->with( "id" )
+            ->will( $this->returnValue( 42 ) );
+        /** @var \eZ\Publish\API\Repository\Values\User\PolicyCreateStruct $policyCreateStructMock */
+        $policyCreateStructMock->module = "mockModule";
+        $policyCreateStructMock->function = "mockFunction";
+
+        $roleServiceMock->expects( $this->once() )
+            ->method( "loadRole" )
+            ->with( $this->equalTo( 42 ) )
+            ->will( $this->returnValue( $roleMock ) );
+
+        $limitationTypeMock->expects( $this->once() )
+            ->method( "acceptValue" )
+            ->with( $this->equalTo( $limitationMock ) );
+        $limitationTypeMock->expects( $this->once() )
+            ->method( "validate" )
+            ->with( $this->equalTo( $limitationMock ) )
+            ->will( $this->returnValue( array( 42 ) ) );
+
+        $limitationMock->expects( $this->any() )
+            ->method( "getIdentifier" )
+            ->will( $this->returnValue( "mockIdentifier" ) );
+
+        $settings = array(
+            "limitationMap" => array( "mockModule" => array( "mockFunction" => array( "mockIdentifier" => true ) ) ),
+            "limitationTypes" => array( "mockIdentifier" => $limitationTypeMock )
+        );
+        $this->setConfiguration( $roleServiceMock, $settings );
+
+        /** @var \PHPUnit_Framework_MockObject_MockObject $policyCreateStructMock */
+        $policyCreateStructMock->expects( $this->once() )
+            ->method( "getLimitations" )
+            ->will( $this->returnValue( array( $limitationMock ) ) );
+
+        $repository->expects( $this->once() )
+            ->method( "hasAccess" )
+            ->with(
+                $this->equalTo( "role" ),
+                $this->equalTo( "update" )
+            )->will( $this->returnValue( true ) );
+
+        /** @var \eZ\Publish\API\Repository\Values\User\Role $roleMock */
+        /** @var \eZ\Publish\API\Repository\Values\User\PolicyCreateStruct $policyCreateStructMock */
+        $roleServiceMock->addPolicy( $roleMock, $policyCreateStructMock );
+    }
+
+    /**
+     * Test for the updatePolicy() method.
+     *
+     * @covers \eZ\Publish\Core\Repository\RoleService::updatePolicy
+     * @covers \eZ\Publish\Core\Repository\RoleService::validateLimitations
+     * @covers \eZ\Publish\Core\Repository\RoleService::validateLimitation
+     * @expectedException \eZ\Publish\API\Repository\Exceptions\LimitationValidationException
+     */
+    public function testUpdatePolicyThrowsLimitationValidationException()
+    {
+        $repository = $this->getRepositoryMock();
+        $roleServiceMock = $this->getPartlyMockedRoleService( array( "loadRole" ) );
+        $policyMock = $this->getMock( "eZ\\Publish\\API\\Repository\\Values\\User\\Policy" );
+        $policyUpdateStructMock = $this->getMock( "eZ\\Publish\\API\\Repository\\Values\\User\\PolicyUpdateStruct" );
+        $limitationMock = $this->getMock( "eZ\\Publish\\API\\Repository\\Values\\User\\Limitation\\RoleLimitation" );
+        $limitationTypeMock = $this->getMock( "eZ\\Publish\\SPI\\Limitation\\Type" );
+
+        $policyMock->expects( $this->any() )
+            ->method( "__get" )
+            ->will(
+                $this->returnCallback(
+                    function ( $propertyName )
+                    {
+                        switch ( $propertyName )
+                        {
+                            case "module":
+                                return "mockModule";
+                            case "function":
+                                return "mockFunction";
+                        }
+                        return null;
+                    }
+                )
+            );
+
+        $limitationTypeMock->expects( $this->once() )
+            ->method( "acceptValue" )
+            ->with( $this->equalTo( $limitationMock ) );
+        $limitationTypeMock->expects( $this->once() )
+            ->method( "validate" )
+            ->with( $this->equalTo( $limitationMock ) )
+            ->will( $this->returnValue( array( 42 ) ) );
+
+        $limitationMock->expects( $this->any() )
+            ->method( "getIdentifier" )
+            ->will( $this->returnValue( "mockIdentifier" ) );
+
+        $settings = array(
+            "limitationMap" => array( "mockModule" => array( "mockFunction" => array( "mockIdentifier" => true ) ) ),
+            "limitationTypes" => array( "mockIdentifier" => $limitationTypeMock )
+        );
+        $this->setConfiguration( $roleServiceMock, $settings );
+
+        /** @var \PHPUnit_Framework_MockObject_MockObject $policyCreateStructMock */
+        $policyUpdateStructMock->expects( $this->once() )
+            ->method( "getLimitations" )
+            ->will( $this->returnValue( array( $limitationMock ) ) );
+
+        $repository->expects( $this->once() )
+            ->method( "hasAccess" )
+            ->with(
+                $this->equalTo( "role" ),
+                $this->equalTo( "update" )
+            )->will( $this->returnValue( true ) );
+
+        /** @var \eZ\Publish\API\Repository\Values\User\Policy $policyMock */
+        /** @var \eZ\Publish\API\Repository\Values\User\PolicyUpdateStruct $policyUpdateStructMock */
+        $roleServiceMock->updatePolicy( $policyMock, $policyUpdateStructMock );
+    }
+
     /**
      * Test for the assignRoleToUser() method.
      *
@@ -48,9 +251,9 @@ class RoleTest extends BaseServiceMockTest
      *
      * @covers \eZ\Publish\Core\Repository\RoleService::assignRoleToUser
      * @covers \eZ\Publish\Core\Repository\RoleService::validateLimitation
-     * @expectedException \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException
+     * @expectedException \eZ\Publish\API\Repository\Exceptions\LimitationValidationException
      */
-    public function testAssignRoleToUserThrowsInvalidArgumentException()
+    public function testAssignRoleToUserThrowsLimitationValidationException()
     {
         $repository = $this->getRepositoryMock();
         $roleServiceMock = $this->getPartlyMockedRoleService();
@@ -361,9 +564,9 @@ class RoleTest extends BaseServiceMockTest
      *
      * @covers \eZ\Publish\Core\Repository\RoleService::assignRoleToUserGroup
      * @covers \eZ\Publish\Core\Repository\RoleService::validateLimitation
-     * @expectedException \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException
+     * @expectedException \eZ\Publish\API\Repository\Exceptions\LimitationValidationException
      */
-    public function testAssignRoleToUserGroupThrowsInvalidArgumentException()
+    public function testAssignRoleToUserGroupThrowsLimitationValidationException()
     {
         $repository = $this->getRepositoryMock();
         $roleServiceMock = $this->getPartlyMockedRoleService();
