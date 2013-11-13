472c1537316af6f4e574eba162717a25a9582037
Tobias Schlitt <toby@php.net>
2013-11-13 16:20:34 +0100

    SPI: Added basic interfaces for new URLAlias SPI.

---
+++ eZ/Publish/SPI/URLAlias/URLAliasGenerationService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace eZ\Publish\SPI\URLAlias;
+
+/**
+ * In implementation of this service triggers generation of URL aliases.
+ */
+interface URLAliasGenerationService
+{
+    /**
+     * Triggers generation of URL aliases when a new content version is
+     * published.
+     *
+     * @param string $contentId
+     * @param int $versionNo
+     */
+    public function onContentVersionPublished( $contentId, $versionNo );
+
+    /**
+     * Triggers update of URL aliases when a location was moved.
+     *
+     * @param string $locationId
+     * @param string $oldParentId
+     * @param string $newParentId
+     */
+    public function onLocationMoved( $locationId, $oldParentId, $newParentId );
+
+    /**
+     * Triggers creation of URL aliases when a location was copied.
+     *
+     * @param string $copiedLocationId
+     */
+    public function onLocationCopied( $originalLocationId, $copiedLocationId );
+
+    /**
+     * Triggers removal of URL aliases when a location was deleted.
+     *
+     * @param string $deletedLocationId
+     */
+    public function onLocationDeleted( $deletedLocationId );
+}
---
+++ eZ/Publish/SPI/URLAlias/URLLookupService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace eZ\Publish\SPI\URLAlias;
+
+/**
+ * Service for lookup of URL aliases.
+ */
+interface URLLookupService
+{
+    /**
+     * looks up the URLAlias for the given url.
+     *
+     * @param string $url
+     * @param string $languageCode
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException if the path does not exist or is not valid for the given language
+     *
+     * @return \eZ\Publish\API\Repository\Values\Content\URLAlias
+     */
+    public function lookup( $url, $languageCode = null );
+
+    /**
+     * Returns the URL alias for the given location in the given language.
+     *
+     * If $languageCode is null the method returns the url alias in the most prioritized language.
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException if no url alias exist for the given language
+     *
+     * @param \eZ\Publish\API\Repository\Values\Content\Location $location
+     * @param string $languageCode
+     *
+     * @return \eZ\Publish\API\Repository\Values\Content\URLAlias
+     */
+    public function reverseLookup( Location $location, $languageCode = null );
+}
