34dc8ea18c2cdae16e83d3172ae9c05781f8d64f
Jérôme Vieilledent <lolautruche@gmail.com>
2013-11-14 16:55:16 +0100

    EZP-21949: Added coverage for IdentityDefinerPass

---
+++ eZ/Bundle/EzPublishCoreBundle/Tests/DependencyInjection/Compiler/IdentityDefinerPassTest.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * File containing the IdentityDefinerPassTest class.
+ *
+ * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Bundle\EzPublishCoreBundle\Tests\DependencyInjection\Compiler;
+
+use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Compiler\IdentityDefinerPass;
+use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractCompilerPassTest;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+
+class IdentityDefinerPassTest extends AbstractCompilerPassTest
+{
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->setDefinition( 'ezpublish.user.hash_generator', new Definition() );
+    }
+
+    /**
+     * Register the compiler pass under test, just like you would do inside a bundle's load()
+     * method:
+     *
+     *   $container->addCompilerPass(new MyCompilerPass());
+     */
+    protected function registerCompilerPass( ContainerBuilder $container )
+    {
+        $container->addCompilerPass( new IdentityDefinerPass() );
+    }
+
+    /**
+     * @covers eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Compiler\IdentityDefinerPass::process
+     */
+    public function testSetIdentityDefiner()
+    {
+        $def = new Definition();
+        $def->addTag( 'ezpublish.identity_definer' );
+        $serviceId = 'some_service_id';
+        $this->setDefinition( $serviceId, $def );
+
+        $this->compile();
+
+        $this->assertContainerBuilderHasServiceDefinitionWithMethodCall(
+            'ezpublish.user.hash_generator',
+            'setIdentityDefiner',
+            array( new Reference( $serviceId ) )
+        );
+    }
+}
+ 
