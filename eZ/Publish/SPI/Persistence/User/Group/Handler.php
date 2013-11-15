96cc44eecc17092822c3e921a98b42361dda510f
Tobias Schlitt <toby@php.net>
2013-11-15 13:09:49 +0100

    SPI: Assignment methods for User Group Handler.

--- eZ/Publish/SPI/Persistence/User/Group/Handler.php
+++ eZ/Publish/SPI/Persistence/User/Group/Handler.php
@@ -2,6 +2,9 @@
 
 namespace eZ\Publish\SPI\Persistence\User\Group;
 
+/**
+ * @todo: Should this be merged to User\Handler?
+ */
 interface Handler
 {
     /**
@@ -35,4 +38,36 @@ interface Handler
      * @param mixed $newParentId
      */
     public function move( $groupId, $newParentId );
+
+    /**
+     * Assigns $userId to $groupId
+     *
+     * @param mixed $userId
+     * @param mixed $groupId
+     */
+    public function assignUserToGroup( $userId, $groupId );
+
+    /**
+     * Remove $userId from $groupId
+     *
+     * @param mixed $userId
+     * @param mixed $groupId
+     */
+    public function unAssignUserFromGroup( $userId, $groupId );
+
+    /**
+     * Loads groups for $userId
+     *
+     * @param mixed $userId
+     * @return \eZ\Publish\SPI\Persistence\User\Group[]
+     */
+    public function loadGroupsOfUser( $userId );
+
+    /**
+     * Loads all users that are assigned to $groupId
+     *
+     * @param mixed $groupId
+     * @return \eZ\Publish\SPI\Persistence\User[]
+     */
+    public function loadUsersOfGroup( $groupId );
 }
