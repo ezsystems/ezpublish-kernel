3379fe91afd2523d8967674c87f6f7b386af2f6d
Benjamin Eberlei <kontakt@beberlei.de>
2013-11-18 09:10:33 +0100

    Add ConnectionHandler::createFromDSN() method with tests for the travis connection data.

--- eZ/Publish/Core/Persistence/Doctrine/ConnectionHandler.php
+++ eZ/Publish/Core/Persistence/Doctrine/ConnectionHandler.php
@@ -7,6 +7,7 @@ use eZ\Publish\Core\Persistence\Database\SelectQuery;
 use eZ\Publish\Core\Persistence\Database\QueryException;
 
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\DriverManager;
 use Doctrine\DBAL\DBALException;
 
 class ConnectionHandler implements DatabaseHandler
@@ -16,6 +17,239 @@ class ConnectionHandler implements DatabaseHandler
      */
     protected $connection;
 
+    /**
+     * Create a Connection Handler with corresponding Doctrine connection from DSN.
+     *
+     * @param string|array $dsn
+     * @return ConnectionHandler
+     */
+    public static function createFromDSN( $dsn )
+    {
+        if ( is_string( $dsn ) )
+        {
+            $parsed = self::parseDSN( $dsn );
+        }
+        else
+        {
+            $parsed = $dsn;
+        }
+
+        return new self( DriverManager::getConnection( $parsed ) );
+    }
+
+    /**
+     * Returns the Data Source Name as a structure containing the various parts of the DSN.
+     *
+     * Additional keys can be added by appending a URI query string to the
+     * end of the DSN.
+     *
+     * The format of the supplied DSN is in its fullest form:
+     * <code>
+     *  driver://username:password@protocol+host/database?option=8&another=true
+     * </code>
+     *
+     * Most variations are allowed:
+     * <code>
+     *  driver://username:password@protocol+host:110//usr/db_file.db?mode=0644
+     *  driver://username:password@host/dbname
+     *  driver://username:password@host
+     *  driver://username@host
+     *  driver://host/dbname
+     *  driver://host
+     *  driver
+     * </code>
+     *
+     * This function is 'borrowed' from PEAR /DB.php .
+     *
+     * @license Apache2 from Zeta Components Database project
+     * @param string $dsn Data Source Name to be parsed
+     *
+     * @return array an associative array with the following keys:
+     *  + driver:  Database backend used in PHP (mysql, odbc etc.)
+     *  + host: Host specification (hostname[:port])
+     *  + dbname: Database to use on the DBMS server
+     *  + username: User name for login
+     *  + password: Password for login
+     */
+    public static function parseDSN( $dsn )
+    {
+        $parsed = array(
+            'driver'      => false,
+            'username'    => false,
+            'password'    => false,
+            'host'        => false,
+            'port'        => false,
+            'unix_socket' => false,
+            'dbname'      => false,
+            'memory'      => false,
+            'path'        => false,
+        );
+
+        // Find driver and dbsyntax
+        if ( ( $pos = strpos( $dsn, '://' ) ) !== false )
+        {
+            $str = substr( $dsn, 0, $pos );
+            $dsn = substr( $dsn, $pos + 3 );
+        }
+        else
+        {
+            $str = $dsn;
+            $dsn = null;
+        }
+
+        // Get driver and dbsyntax
+        // $str => driver(dbsyntax)
+        if ( preg_match( '|^(.+?)\((.*?)\)$|', $str, $arr ) )
+        {
+            $parsed['driver']  = $arr[1];
+        }
+        else
+        {
+            $parsed['driver']  = $str;
+        }
+
+        if ( !count( $dsn ) )
+        {
+            return $parsed;
+        }
+
+        // Get (if found): username and password
+        // $dsn => username:password@protocol+host/database
+        if ( ( $at = strrpos( (string)$dsn, '@' ) ) !== false )
+        {
+            $str = substr( $dsn, 0, $at );
+            $dsn = substr( $dsn, $at + 1 );
+            if ( ( $pos = strpos( $str, ':' ) ) !== false )
+            {
+                $parsed['username'] = rawurldecode( substr( $str, 0, $pos ) );
+                $parsed['password'] = rawurldecode( substr( $str, $pos + 1 ) );
+            }
+            else
+            {
+                $parsed['username'] = rawurldecode( $str );
+            }
+        }
+
+        // Find protocol and host
+
+        if ( preg_match( '|^([^(]+)\((.*?)\)/?(.*?)$|', $dsn, $match ) )
+        {
+            // $dsn => proto(proto_opts)/database
+            $proto       = $match[1];
+            $proto_opts  = $match[2] ? $match[2] : false;
+            $dsn         = $match[3];
+        }
+        else
+        {
+            // $dsn => protocol+host/database (old format)
+            if ( strpos( $dsn, '+' ) !== false )
+            {
+                list( $proto, $dsn ) = explode( '+', $dsn, 2 );
+            }
+            if ( strpos( $dsn, '/' ) !== false )
+            {
+                list( $proto_opts, $dsn ) = explode( '/', $dsn, 2 );
+            }
+            else
+            {
+                $proto_opts = $dsn;
+                $dsn = null;
+            }
+        }
+
+        // process the different protocol options
+        $protocol = ( !empty( $proto ) ) ? $proto : 'tcp';
+        $proto_opts = rawurldecode( $proto_opts );
+        if ( $protocol == 'tcp' )
+        {
+            if ( strpos( $proto_opts, ':' ) !== false )
+            {
+                list( $parsed['host'], $parsed['port'] ) = explode( ':', $proto_opts );
+            }
+            else
+            {
+                $parsed['host'] = $proto_opts;
+            }
+        }
+        else if ( $protocol == 'unix' )
+        {
+            $parsed['unix_socket'] = $proto_opts;
+        }
+
+        // Get dabase if any
+        // $dsn => database
+        if ( $dsn )
+        {
+            if ( ( $pos = strpos( $dsn, '?' ) ) === false )
+            {
+                // /database
+                $parsed['dbname'] = rawurldecode( $dsn );
+            }
+            else
+            {
+                // /database?param1=value1&param2=value2
+                $parsed['dbname'] = rawurldecode( substr( $dsn, 0, $pos ) );
+                $dsn = substr( $dsn, $pos + 1 );
+                if ( strpos( $dsn, '&' ) !== false )
+                {
+                    $opts = explode( '&', $dsn );
+                }
+                else
+                {
+                    $opts = array( $dsn );
+                }
+
+                foreach ( $opts as $opt )
+                {
+                    list( $key, $value ) = explode( '=', $opt );
+                    if ( !isset( $parsed[$key] ) )
+                    {
+                        // don't allow params overwrite
+                        $parsed[$key] = rawurldecode( $value );
+                    }
+                }
+            }
+        }
+
+        if ( $parsed['driver'] === 'sqlite' )
+        {
+            if ( isset( $parsed['port'] ) && $parsed['port'] === 'memory' )
+            {
+                $parsed['memory'] = true;
+                unset($parsed['port']);
+                unset($parsed['host']);
+            }
+            else if ( isset( $parsed['dbname'] ) )
+            {
+                $parsed['path'] = $parsed['dbname'];
+                unset($parsed['dbname']);
+                unset($parsed['host']);
+            }
+        }
+
+        $driverMap = array(
+            'mysql' => 'pdo_mysql',
+            'pgsql' => 'pdo_pgsql',
+            'sqlite' => 'pdo_sqlite'
+        );
+
+        if ( isset( $driverMap[$parsed['driver']] ) )
+        {
+            $parsed['driver'] = $driverMap[$parsed['driver']];
+        }
+
+        return array_filter(
+            $parsed,
+            function ($element)
+            {
+                return $element !== false;
+            }
+        );
+    }
+
+    /**
+     * @param \Doctrine\DBAL\Connection $connection
+     */
     public function __construct( Connection $connection )
     {
         $this->connection = $connection;
