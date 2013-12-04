8b3f3213a95545e921d08240ed2e73064ac03e24
Tobias Schlitt <toby@php.net>
2013-12-04 16:41:56 +0100

    SPI: Added $typeHint to handler generate method.

--- eZ/Publish/SPI/Asset/Handler.php
+++ eZ/Publish/SPI/Asset/Handler.php
@@ -8,16 +8,18 @@ interface Handler
      * Generates the (default) variants for $inputFile
      *
      * @param string $inputFile
+     * @param string $typeHint
      * @return Variant[] The generated variants
      */
-    public function generateVariants($inputFile);
+    public function generateVariants($inputFile, $typeHint = null);
 
     /**
      * Generates the variant with $variantIdentifier for $inputFile
      *
      * @param string $inputFile
      * @param string $variantIdentifier
+     * @param string $typeHint
      * @return Variant The generated variant data
      */
-    public function generateVariant($inputFile, $variantIdentifier);
+    public function generateVariant($inputFile, $variantIdentifier, $typeHint = null);
 }
