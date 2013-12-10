66ca55e2c8194382e3c6a20b25bb196cb7c3701f
StephaneDiot <sdiot@live.fr>
2013-12-10 16:37:36 +0100

    ezp-22049 implement configurable IndexPage

--- eZ/Bundle/EzPublishCoreBundle/EventListener/RequestEventListener.php
+++ eZ/Bundle/EzPublishCoreBundle/EventListener/RequestEventListener.php
@@ -55,8 +55,38 @@ class RequestEventListener implements EventSubscriberInterface
                 array( 'onKernelRequestRedirect', 0 ),
                 // onKernelRequestUserHash needs to be just after Firewall (prio 8), so that user is already logged in the repository.
                 array( 'onKernelRequestUserHash', 7 ),
+                array( 'onKernelRequestIndex', 40 ),
             )
         );
+    }
+
+    /**
+     * Checks if the IndexPage is configured and which page must be shown
+     *
+     * @param GetResponseEvent $event
+     */
+    public function onKernelRequestIndex( GetResponseEvent $event )
+    {
+        /** @var $this->configresolver \eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Configuration\ChainConfigResolver.php */
+        $this->configresolver = $this->container->get( 'ezpublish.config.resolver' );
+        $request = $event->getRequest();
+        if (
+            $event->getRequestType() === HttpKernelInterface::MASTER_REQUEST
+            && $request->attributes->get( 'semanticPathinfo' ) === '/'
+        )
+        {
+            //Parameter from EzPublishCoreBundle/Resources/config/default_settings.yml
+            $indexPage = $this->configresolver->getParameter( 'index_page', 'ezsettings' );
+            if ( $indexPage !== null )
+            {
+                if ( substr( $indexPage, 0, 1 ) !== '/' )
+                {
+                    $indexPage = '/' . $indexPage;
+                }
+                $request->attributes->set( 'semanticPathinfo', $indexPage );
+                $request->attributes->set( 'needsForward', true );
+            }
+        }
     }
 
     /**
