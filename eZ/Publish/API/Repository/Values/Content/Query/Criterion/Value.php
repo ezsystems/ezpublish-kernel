afdad4bc655521d6557f9ddb0799da28f3030ddc
Petar Španja <petar.spanja@gmail.com>
2014-01-10 13:37:16 +0100

    EZP-22103: added Criterion Value base class

---
+++ eZ/Publish/API/Repository/Values/Content/Query/Criterion/Value.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * File containing the eZ\Publish\API\Repository\Values\Content\Query\Criterion\Value class.
+ *
+ * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ *
+ * @package eZ\Publish\API\Repository\Values\Content\Query
+ */
+
+namespace eZ\Publish\API\Repository\Values\Content\Query\Criterion;
+
+/**
+ * Struct that stores extra value information for a Criterion object
+ * @package eZ\Publish\API\Repository\Values\Content\Query
+ */
+abstract class Value
+{
+}
