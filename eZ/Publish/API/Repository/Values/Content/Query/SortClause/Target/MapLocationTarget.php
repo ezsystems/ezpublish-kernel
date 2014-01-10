868e027e9eab199265336c3be912ee63b81d544e
Petar Španja <petar.spanja@gmail.com>
2014-01-10 13:46:55 +0100

    EZP-22103: implemented MapLocation sort clause target

---
+++ eZ/Publish/API/Repository/Values/Content/Query/SortClause/Target/MapLocationTarget.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * File containing the eZ\Publish\API\Repository\Values\Content\Query\SortClause\Target\MapLocationTarget class.
+ *
+ * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ *
+ *
+ * @package eZ\Publish\API\Repository\Values\Content\Query
+ */
+
+namespace eZ\Publish\API\Repository\Values\Content\Query\SortClause\Target;
+
+use eZ\Publish\API\Repository\Values\Content\Query\SortClause\Target;
+
+/**
+ * Struct that stores extra value information for a MapLocationDistance SortClause object
+ * @package eZ\Publish\API\Repository\Values\Content\Query
+ */
+class MapLocationTarget extends Target
+{
+    /**
+     * Latitude of a geographical location
+     *
+     * @var float
+     */
+    public $latitude;
+
+    /**
+     * Longitude of a geographical location
+     *
+     * @var float
+     */
+    public $longitude;
+
+    /**
+     * Identifier of a targeted Field ContentType
+     *
+     * @var string
+     */
+    public $typeIdentifier;
+
+    /**
+     * Identifier of a targeted Field FieldDefinition
+     *
+     * @var string
+     */
+    public $fieldIdentifier;
+
+    /**
+     * Language code of the targeted Field
+     *
+     * @var null|string
+     */
+    public $languageCode;
+
+    /**
+     * @param float $latitude
+     * @param float $longitude
+     * @param string $typeIdentifier
+     * @param string $fieldIdentifier
+     * @param null|string $languageCode
+     */
+    public function __construct(
+        $latitude,
+        $longitude,
+        $typeIdentifier,
+        $fieldIdentifier,
+        $languageCode
+    )
+    {
+        $this->latitude = $latitude;
+        $this->longitude = $longitude;
+        $this->typeIdentifier = $typeIdentifier;
+        $this->fieldIdentifier = $fieldIdentifier;
+        $this->languageCode = $languageCode;
+    }
+}
