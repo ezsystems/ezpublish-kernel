0adcd098e1cd209d3e9e4c7f07dfa75da7ad1357
gggeek <gg@ez.no>
2014-01-25 02:12:25 +0100

    Implement EZP-22241: add an AncestorNodeId criterion for SearchService

---
+++ eZ/Publish/API/Repository/Values/Content/Query/Criterion/AncestorLocationId.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * File containing the eZ\Publish\API\Repository\Values\Content\Query\Criterion\LocationId class.
+ *
+ * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/Resources/Software/Licenses/eZ-Business-Use-License-Agreement-eZ-BUL-Version-2.1 eZ Business Use License Agreement eZ BUL Version 2.1
+ * @version 5.2.0
+ */
+
+namespace eZ\Publish\API\Repository\Values\Content\Query\Criterion;
+
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion\Operator\Specifications;
+use eZ\Publish\API\Repository\Values\Content\Query\CriterionInterface;
+
+/**
+ * A criterion that matches content based on its ancestor location id
+ *
+ * Parent location id is done using {@see ParentLocationId}
+ *
+ * Supported operators:
+ * - EQ: matches against a unique location id
+ */
+class AncestorLocationId extends Criterion implements CriterionInterface
+{
+    /**
+     * Creates a new LocationId criterion
+     *
+     * @param int|int[] $value One or more locationId that must be matched
+     *
+     * @throws \InvalidArgumentException if a non numeric id is given
+     * @throws \InvalidArgumentException if the value type doesn't match the operator
+     */
+    public function __construct( $value )
+    {
+        parent::__construct( null, null, $value );
+    }
+
+    public function getSpecifications()
+    {
+        return array(
+            new Specifications(
+                Operator::EQ,
+                Specifications::FORMAT_SINGLE,
+                Specifications::TYPE_INTEGER | Specifications::TYPE_STRING
+            ),
+        );
+    }
+
+    public static function createFromQueryBuilder( $target, $operator, $value )
+    {
+        return new self( $value );
+    }
+}
