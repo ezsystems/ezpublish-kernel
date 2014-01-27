e10cb7e0f833fab4c717a667985a439ded3646f4
Petar Španja <petar.spanja@gmail.com>
2014-01-27 14:42:28 +0100

    EZP-22105: fixed missing property

--- eZ/Publish/Core/Persistence/Legacy/Handler.php
+++ eZ/Publish/Core/Persistence/Legacy/Handler.php
@@ -280,6 +280,11 @@ class Handler implements HandlerInterface
     protected $transformationProcessor;
 
     /**
+     * @var \eZ\Publish\Core\Persistence\Legacy\Content\Search\Gateway\CriterionHandler\FieldValue\Converter
+     */
+    protected $criterionFieldValueConverter;
+
+    /**
      * General configuration
      *
      * @var array
