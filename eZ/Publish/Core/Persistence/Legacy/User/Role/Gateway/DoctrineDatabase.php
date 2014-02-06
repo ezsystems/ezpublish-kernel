c6f69add0e8ef428f4a25e1133a9eac32d22d36b
Petar Španja <petar.spanja@gmail.com>
2014-02-06 11:27:10 +0100

    EZP-22146: removed deprecated column and join

--- eZ/Publish/Core/Persistence/Legacy/User/Role/Gateway/DoctrineDatabase.php
+++ eZ/Publish/Core/Persistence/Legacy/User/Role/Gateway/DoctrineDatabase.php
@@ -93,7 +93,6 @@ class DoctrineDatabase extends Gateway
         $query->select(
             $this->handler->aliasedColumn( $query, 'id', 'ezrole' ),
             $this->handler->aliasedColumn( $query, 'name', 'ezrole' ),
-            $this->handler->aliasedColumn( $query, 'contentobject_id', 'ezuser_role' ),
             $this->handler->aliasedColumn( $query, 'id', 'ezpolicy' ),
             $this->handler->aliasedColumn( $query, 'function_name', 'ezpolicy' ),
             $this->handler->aliasedColumn( $query, 'module_name', 'ezpolicy' ),
@@ -101,12 +100,6 @@ class DoctrineDatabase extends Gateway
             $this->handler->aliasedColumn( $query, 'value', 'ezpolicy_limitation_value' )
         )->from(
             $this->handler->quoteTable( 'ezrole' )
-        )->leftJoin(
-            $this->handler->quoteTable( 'ezuser_role' ),
-            $query->expr->eq(
-                $this->handler->quoteColumn( 'role_id', 'ezuser_role' ),
-                $this->handler->quoteColumn( 'id', 'ezrole' )
-            )
         )->leftJoin(
             $this->handler->quoteTable( 'ezpolicy' ),
             $query->expr->eq(
