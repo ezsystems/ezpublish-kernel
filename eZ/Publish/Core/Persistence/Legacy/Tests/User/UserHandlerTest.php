164ec97fcacca335f52af544c4dd3988ccdfe65b
Petar Španja <petar.spanja@gmail.com>
2014-02-06 09:42:08 +0100

    EZP-22146: add test for loadRoleAssignmentsByRoleId()

--- eZ/Publish/Core/Persistence/Legacy/Tests/User/UserHandlerTest.php
+++ eZ/Publish/Core/Persistence/Legacy/Tests/User/UserHandlerTest.php
@@ -866,4 +866,34 @@ class UserHandlerTest extends TestCase
             $handler->loadRoleAssignmentsByGroupId( 13, true )
         );
     }
+
+    public function testLoadRoleAssignmentsByRoleId()
+    {
+        $this->insertDatabaseFixture( __DIR__ . '/../../../../Repository/Tests/Service/Integration/Legacy/_fixtures/clean_ezdemo_47_dump.php' );
+        $handler = $this->getUserHandler();
+
+        $this->assertEquals(
+            array(
+                new Persistence\User\RoleAssignment(
+                    array(
+                        'roleId' => 1,
+                        'contentId' => 11
+                    )
+                ),
+                new Persistence\User\RoleAssignment(
+                    array(
+                        'roleId' => 1,
+                        'contentId' => 42
+                    )
+                ),
+                new Persistence\User\RoleAssignment(
+                    array(
+                        'roleId' => 1,
+                        'contentId' => 59
+                    )
+                ),
+            ),
+            $handler->loadRoleAssignmentsByRoleId( 1 )
+        );
+    }
 }
