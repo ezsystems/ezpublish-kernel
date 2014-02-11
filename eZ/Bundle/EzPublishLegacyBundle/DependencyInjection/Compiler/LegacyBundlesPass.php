c45d9e60b14cde18746d011a1a01d98d59b97472
Bertrand Dunogier <bd@ez.no>
2014-02-11 18:13:59 +0100

    EZP-22210: Inplemented compiler pass + legacy mapper

---
+++ eZ/Bundle/EzPublishLegacyBundle/DependencyInjection/Compiler/LegacyBundlesPass.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * File containing the LegacyPass class.
+ *
+ * @copyright Copyright (C) 1999-2014 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Bundle\EzPublishLegacyBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+class LegacyBundlesPass implements CompilerPassInterface
+{
+    /** @var \Symfony\Component\HttpKernel\KernelInterface */private $kernel;
+
+    public function __construct( KernelInterface $kernel )
+    {
+        $this->kernel = $kernel;
+    }
+
+    public function process( ContainerBuilder $container )
+    {
+        $locator = $container->get( 'ezpublish_legacy.legacy_bundles.extension_locator' );
+
+        $extensionNames = array();
+        foreach ( $this->kernel->getBundles() as $bundle )
+        {
+            $bundleExtensions = $locator->locate( $bundle->getPath() );
+            array_walk(
+                $bundleExtensions,
+                function( &$path, $key ) {
+                    $path = basename( $path );
+                }
+            );
+
+            $extensionNames += $bundleExtensions;
+        }
+
+        array_unique( $extensionNames );
+        $container->setParameter( 'ezpublish_legacy.legacy_bundles_extensions', $extensionNames );
+    }
+}
--- eZ/Bundle/EzPublishLegacyBundle/EzPublishLegacyBundle.php
+++ eZ/Bundle/EzPublishLegacyBundle/EzPublishLegacyBundle.php
@@ -9,14 +9,24 @@
 
 namespace eZ\Bundle\EzPublishLegacyBundle;
 
+use eZ\Bundle\EzPublishLegacyBundle\DependencyInjection\Compiler\LegacyBundlesPass;
 use eZ\Bundle\EzPublishLegacyBundle\DependencyInjection\Security\SSOFactory;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use eZ\Bundle\EzPublishLegacyBundle\DependencyInjection\Compiler\LegacyPass;
 use eZ\Bundle\EzPublishLegacyBundle\DependencyInjection\Compiler\TwigPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 class EzPublishLegacyBundle extends Bundle
 {
+    /** @var KernelInterface */
+    private $kernel;
+
+    public function __construct( KernelInterface $kernel )
+    {
+        $this->kernel = $kernel;
+    }
+
     public function boot()
     {
         if ( !$this->container->getParameter( 'ezpublish_legacy.enabled' ) )
@@ -34,6 +44,7 @@ class EzPublishLegacyBundle extends Bundle
         parent::build( $container );
         $container->addCompilerPass( new LegacyPass() );
         $container->addCompilerPass( new TwigPass() );
+        $container->addCompilerPass( new LegacyBundlesPass( $this->kernel ) );
 
         /** @var \Symfony\Bundle\SecurityBundle\DependencyInjection\SecurityExtension $securityExtension */
         $securityExtension = $container->getExtension( 'security' );
