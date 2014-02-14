af95213930945ee481af97e08e1eaf7f7d4d62b5
Bertrand Dunogier <bd@ez.no>
2014-02-14 17:09:14 +0100

    EZP-22210: Made the install script safer

--- eZ/Bundle/EzPublishLegacyBundle/Command/LegacyBundleInstallCommand.php
+++ eZ/Bundle/EzPublishLegacyBundle/Command/LegacyBundleInstallCommand.php
@@ -10,6 +10,7 @@
 namespace eZ\Bundle\EzPublishLegacyBundle\Command;
 
 use eZ\Bundle\EzPublishLegacyBundle\LegacyBundleExtensionInterface;
+use RuntimeException;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
@@ -26,9 +27,10 @@ class LegacyBundleInstallCommand extends ContainerAwareCommand
     {
         $this
             ->setName( 'ezpublish:legacybundles:install_extensions' )
-            ->addOption( 'symlink', null, InputOption::VALUE_NONE, 'Symlinks the assets instead of copying it' )
+            ->addOption( 'copy', null, InputOption::VALUE_NONE, 'Creates copies of the extensions instead of using a symlink' )
             ->addOption( 'relative', null, InputOption::VALUE_NONE, 'Make relative symlinks' )
-            ->setDescription( 'Installs (symlink/copy) legacy extensions defined in Symfony bundles into ezpublish_legacy.' )
+            ->addOption( 'force', null, InputOption::VALUE_NONE, 'Force overwriting of existing directory (will be removed)' )
+            ->setDescription( 'Installs legacy extensions (default: symlink) defined in Symfony bundles into ezpublish_legacy/extensions' )
             ->setHelp(
                 <<<EOT
 The command <info>%command.name%</info> installs <info>legacy extensions</info> stored in a Symfony 2 bundle
@@ -39,49 +41,105 @@ EOT
 
     protected function execute( InputInterface $input, OutputInterface $output )
     {
-        $legacyExtensionsLocator = $this->getContainer()->get( 'ezpublish_legacy.legacy_bundles.extension_locator' );
+        $options = array(
+            'copy' => (bool)$input->getOption( 'copy' ),
+            'relative' => (bool)$input->getOption( 'relative' ),
+            'force' => (bool)$input->getOption( 'force' )
+        );
 
+        $legacyExtensionsLocator = $this->getContainer()->get( 'ezpublish_legacy.legacy_bundles.extension_locator' );
         $kernel = $this->getContainer()->get( 'kernel' );
         foreach ( $kernel->getBundles() as $bundle )
         {
             foreach ( $legacyExtensionsLocator->getExtensionDirectories( $bundle->getPath() ) as $extensionDir )
             {
-                $output->writeln( $extensionDir );
-                $this->linkLegacyExtension(
-                    $extensionDir,
-                    $input->getOption( 'symlink' ),
-                    $input->getOption( 'relative' )
-                );
+                $output->writeln( '- ' . $this->removeCwd( $extensionDir ) );
+                try
+                {
+                    $target = $this->linkLegacyExtension( $extensionDir, $options );
+                    $output->writeln( '  <info>' . ( $options['copy'] ? 'Copied' : 'linked' ) . "</info> to $target</info>" );
+                }
+                catch ( RuntimeException $e )
+                {
+                    $output->writeln( '  <error>' . $e->getMessage() . '</error>' );
+                }
             }
         }
     }
 
     /**
      * Links the legacy extension at $path into ezpublish_legacy/extensions
+     *
      * @param string $extensionPath Absolute path to a legacy extension folder
-     * @param bool $symlink
-     * @param bool $relative
+     * @param array  $options
+     *
+     * @throws \RuntimeException If a target link/directory exists and $options[force] isn't set to true
+     * @return string The resulting link/directory
      */
-    protected function linkLegacyExtension( $extensionPath, $symlink = true, $relative = false )
+    protected function linkLegacyExtension( $extensionPath, array $options = array() )
     {
-        $legacyRootDir = rtrim( $this->getContainer()->getParameter( 'ezpublish_legacy.root_dir' ), '/' );
+        $options += array( 'force' => false, 'copy' => false, 'relative' => false );
         $filesystem = $this->getContainer()->get( 'filesystem' );
+        $legacyRootDir = rtrim( $this->getContainer()->getParameter( 'ezpublish_legacy.root_dir' ), '/' );
 
+        $relativeExtensionPath = $filesystem->makePathRelative( $extensionPath, realpath( "$legacyRootDir/extension/" ) );
         $targetPath = "$legacyRootDir/extension/" . basename( $extensionPath );
-        $filesystem->remove( $targetPath );
-        if ( $symlink )
-        {
-            if ( $relative )
-            {
-                $extensionPath = $filesystem->makePathRelative( $extensionPath, realpath( "$legacyRootDir/extension/" ) );
-            }
 
-            $filesystem->symlink( $extensionPath, $targetPath );
+        if ( file_exists( $targetPath ) && $options['copy'] )
+        {
+            if ( !$options['force'] )
+            {
+                throw new RuntimeException( "Target directory $targetPath already exists" );
+            }
+            $filesystem->remove( $targetPath );
         }
-        else
+
+        if ( file_exists( $targetPath ) && !$options['copy'] )
+        {
+            if ( is_link( $targetPath ) )
+            {
+                $existingLinkTarget = readlink( $targetPath );
+                if ( $existingLinkTarget == $extensionPath || $existingLinkTarget == $relativeExtensionPath )
+                {
+                    return $targetPath;
+                }
+                else if ( !$options['force'] )
+                {
+                    throw new RuntimeException( "Target $targetPath already exists with a different target" );
+                }
+            }
+            else
+            {
+                if ( !$options['force'] )
+                {
+                    throw new RuntimeException( "Target $targetPath already exists with a different target" );
+                }
+            }
+            $filesystem->remove( $targetPath );
+        }
+
+        if ( $options['copy'] )
         {
             $filesystem->mkdir( $targetPath, 0777 );
             $filesystem->mirror( $extensionPath, $targetPath, Finder::create()->in( $extensionPath ) );
         }
+        else
+        {
+            $filesystem->symlink(
+                $options['relative'] ? $relativeExtensionPath : $extensionPath,
+                $targetPath
+            );
+        }
+
+        return $targetPath;
+    }
+
+    /**
+     * Removes the cwd from $path
+     * @param string $path
+     */
+    private function removeCwd( $path )
+    {
+        return str_replace( getcwd() . '/', '', $path );
     }
 }
