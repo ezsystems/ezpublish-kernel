cf57927120f231ed8d096289f9dc97b323768e60
Jérôme Vieilledent <lolautruche@gmail.com>
2014-02-17 13:24:29 +0100

    EZP-22279: Removed obsolete parameter ezpublish.spi.persistence.default_id

--- eZ/Bundle/EzPublishCoreBundle/DependencyInjection/Compiler/RegisterStorageEnginePass.php
+++ eZ/Bundle/EzPublishCoreBundle/DependencyInjection/Compiler/RegisterStorageEnginePass.php
@@ -24,7 +24,6 @@ class RegisterStorageEnginePass implements CompilerPassInterface
      *
      * Does:
      * - Registers all storage engines to ezpublish.api.storage_engine.factory
-     * - Sets the default storage engine id to %ezpublish.spi.persistence.default_id%
      *   as used by ezpublish.spi.persistence.lazy_factory
      *
      * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
