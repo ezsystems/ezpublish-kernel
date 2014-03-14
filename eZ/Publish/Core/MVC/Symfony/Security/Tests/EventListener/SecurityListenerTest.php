fbaa7fd50f5e1f05a3ea7404b77c0c5b497ef176
Jérôme Vieilledent <lolautruche@gmail.com>
2014-03-14 11:28:37 +0100

    EZP-22479: Added tests for SecurityListener

--- eZ/Publish/Core/MVC/Symfony/Security/Tests/EventListener/SecurityListenerTest.php
+++ eZ/Publish/Core/MVC/Symfony/Security/Tests/EventListener/SecurityListenerTest.php
@@ -9,10 +9,13 @@
 
 namespace eZ\Publish\Core\MVC\Symfony\Security\Tests\EventListener;
 
+use eZ\Publish\Core\MVC\Symfony\Security\Authorization\Attribute;
 use eZ\Publish\Core\MVC\Symfony\Security\EventListener\SecurityListener;
-use eZ\Publish\Core\MVC\Symfony\Event\InteractiveLoginEvent;
+use eZ\Publish\Core\MVC\Symfony\SiteAccess;
 use PHPUnit_Framework_TestCase;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent as BaseInteractiveLoginEvent;
 use Symfony\Component\Security\Http\SecurityEvents;
@@ -151,5 +154,228 @@ class SecurityListenerTest extends PHPUnit_Framework_TestCase
             ->with( $this->isInstanceOf( 'eZ\Publish\Core\MVC\Symfony\Security\InteractiveLoginToken' ) );
 
         $this->listener->onInteractiveLogin( $event );
+    }
+
+    /**
+     * @expectedException \eZ\Publish\Core\MVC\Symfony\Security\Exception\UnauthorizedSiteAccessException
+     */
+    public function testCheckSiteAccessPermissionDenied()
+    {
+        $user = $this->getMock( 'eZ\Publish\Core\MVC\Symfony\Security\UserInterface' );
+        $token = $this->getMock( 'Symfony\Component\Security\Core\Authentication\Token\TokenInterface' );
+        $token
+            ->expects( $this->once() )
+            ->method( 'getUser' )
+            ->will( $this->returnValue( $user ) );
+
+        $request = new Request();
+        $siteAccess = new SiteAccess();
+        $request->attributes->set( 'siteaccess', $siteAccess );
+
+        $this->securityContext
+            ->expects( $this->once() )
+            ->method( 'isGranted' )
+            ->with( $this->equalTo( new Attribute( 'user', 'login', array( 'valueObject' => $siteAccess ) ) ) )
+            ->will( $this->returnValue( false ) );
+
+        $this->listener->checkSiteAccessPermission( new BaseInteractiveLoginEvent( $request, $token ) );
+    }
+
+    public function testCheckSiteAccessPermissionGranted()
+    {
+        $user = $this->getMock( 'eZ\Publish\Core\MVC\Symfony\Security\UserInterface' );
+        $token = $this->getMock( 'Symfony\Component\Security\Core\Authentication\Token\TokenInterface' );
+        $token
+            ->expects( $this->once() )
+            ->method( 'getUser' )
+            ->will( $this->returnValue( $user ) );
+
+        $request = new Request();
+        $siteAccess = new SiteAccess();
+        $request->attributes->set( 'siteaccess', $siteAccess );
+
+        $this->securityContext
+            ->expects( $this->once() )
+            ->method( 'isGranted' )
+            ->with( $this->equalTo( new Attribute( 'user', 'login', array( 'valueObject' => $siteAccess ) ) ) )
+            ->will( $this->returnValue( true ) );
+
+        // Nothing should happen or should be returned.
+        $this->listener->checkSiteAccessPermission( new BaseInteractiveLoginEvent( $request, $token ) );
+    }
+
+    public function testCheckSiteAccessNotEzUser()
+    {
+        $user = $this->getMock( 'Symfony\Component\Security\Core\User\UserInterface' );
+        $token = $this->getMock( 'Symfony\Component\Security\Core\Authentication\Token\TokenInterface' );
+        $token
+            ->expects( $this->once() )
+            ->method( 'getUser' )
+            ->will( $this->returnValue( $user ) );
+
+        $request = new Request();
+        $siteAccess = new SiteAccess();
+        $request->attributes->set( 'siteaccess', $siteAccess );
+
+        $this->securityContext
+            ->expects( $this->never() )
+            ->method( 'isGranted' );
+
+        $this->listener->checkSiteAccessPermission( new BaseInteractiveLoginEvent( $request, $token ) );
+    }
+
+    public function testCheckSiteAccessNoSiteAccess()
+    {
+        $user = $this->getMock( 'eZ\Publish\Core\MVC\Symfony\Security\UserInterface' );
+        $token = $this->getMock( 'Symfony\Component\Security\Core\Authentication\Token\TokenInterface' );
+        $token
+            ->expects( $this->once() )
+            ->method( 'getUser' )
+            ->will( $this->returnValue( $user ) );
+
+        $this->securityContext
+            ->expects( $this->never() )
+            ->method( 'isGranted' );
+
+        $this->listener->checkSiteAccessPermission( new BaseInteractiveLoginEvent( new Request(), $token ) );
+    }
+
+    public function testOnKernelRequestSubRequest()
+    {
+        $event = new GetResponseEvent(
+            $this->getMock( 'Symfony\Component\HttpKernel\HttpKernelInterface' ),
+            new Request(),
+            HttpKernelInterface::SUB_REQUEST
+        );
+
+        $this->securityContext
+            ->expects( $this->never() )
+            ->method( 'getToken' );
+        $this->securityContext
+            ->expects( $this->never() )
+            ->method( 'isGranted' );
+
+        $this->listener->onKernelRequest( $event );
+    }
+
+    public function testOnKernelRequestNoSiteAccess()
+    {
+        $event = new GetResponseEvent(
+            $this->getMock( 'Symfony\Component\HttpKernel\HttpKernelInterface' ),
+            new Request(),
+            HttpKernelInterface::MASTER_REQUEST
+        );
+
+        $this->securityContext
+            ->expects( $this->never() )
+            ->method( 'getToken' );
+        $this->securityContext
+            ->expects( $this->never() )
+            ->method( 'isGranted' );
+
+        $this->listener->onKernelRequest( $event );
+    }
+
+    public function testOnKernelRequestNullToken()
+    {
+        $request = new Request();
+        $request->attributes->set( 'siteaccess', new SiteAccess() );
+        $event = new GetResponseEvent(
+            $this->getMock( 'Symfony\Component\HttpKernel\HttpKernelInterface' ),
+            $request,
+            HttpKernelInterface::MASTER_REQUEST
+        );
+
+        $this->securityContext
+            ->expects( $this->once() )
+            ->method( 'getToken' )
+            ->will( $this->returnValue( null ) );
+        $this->securityContext
+            ->expects( $this->never() )
+            ->method( 'isGranted' );
+
+        $this->listener->onKernelRequest( $event );
+    }
+
+    public function testOnKernelRequestLoginRoute()
+    {
+        $request = new Request();
+        $request->attributes->set( 'siteaccess', new SiteAccess() );
+        $request->attributes->set( '_route', 'login' );
+        $event = new GetResponseEvent(
+            $this->getMock( 'Symfony\Component\HttpKernel\HttpKernelInterface' ),
+            $request,
+            HttpKernelInterface::MASTER_REQUEST
+        );
+
+        $this->securityContext
+            ->expects( $this->once() )
+            ->method( 'getToken' )
+            ->will( $this->returnValue( null ) );
+        $this->securityContext
+            ->expects( $this->never() )
+            ->method( 'isGranted' );
+
+        $this->listener->onKernelRequest( $event );
+    }
+
+    /**
+     * @expectedException \eZ\Publish\Core\MVC\Symfony\Security\Exception\UnauthorizedSiteAccessException
+     */
+    public function testOnKernelRequestAccessDenied()
+    {
+        $request = new Request();
+        $request->attributes->set( 'siteaccess', new SiteAccess() );
+        $event = new GetResponseEvent(
+            $this->getMock( 'Symfony\Component\HttpKernel\HttpKernelInterface' ),
+            $request,
+            HttpKernelInterface::MASTER_REQUEST
+        );
+
+        $token = $this->getMock( 'Symfony\Component\Security\Core\Authentication\Token\TokenInterface' );
+        $token
+            ->expects( $this->any() )
+            ->method( 'getUsername' )
+            ->will( $this->returnValue( 'foo' ) );
+
+        $this->securityContext
+            ->expects( $this->once() )
+            ->method( 'getToken' )
+            ->will( $this->returnValue( $token ) );
+        $this->securityContext
+            ->expects( $this->once() )
+            ->method( 'isGranted' )
+            ->will( $this->returnValue( false ) );
+
+        $this->listener->onKernelRequest( $event );
+    }
+
+    public function testOnKernelRequestAccessGranted()
+    {
+        $request = new Request();
+        $request->attributes->set( 'siteaccess', new SiteAccess() );
+        $event = new GetResponseEvent(
+            $this->getMock( 'Symfony\Component\HttpKernel\HttpKernelInterface' ),
+            $request,
+            HttpKernelInterface::MASTER_REQUEST
+        );
+
+        $token = $this->getMock( 'Symfony\Component\Security\Core\Authentication\Token\TokenInterface' );
+        $token
+            ->expects( $this->any() )
+            ->method( 'getUsername' )
+            ->will( $this->returnValue( 'foo' ) );
+
+        $this->securityContext
+            ->expects( $this->once() )
+            ->method( 'getToken' )
+            ->will( $this->returnValue( $token ) );
+        $this->securityContext
+            ->expects( $this->once() )
+            ->method( 'isGranted' )
+            ->will( $this->returnValue( true ) );
+
+        // Nothing should happen or should be returned.
+        $this->listener->onKernelRequest( $event );
     }
 }
