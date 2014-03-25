4c2ccbd4637435a01ec16866f1c301d8c72d799a
Jérôme Vieilledent <lolautruche@gmail.com>
2014-03-25 09:37:39 +0100

    Update LoaderStringTest.php

--- eZ/Publish/Core/MVC/Symfony/Templating/Tests/Twig/LoaderStringTest.php
+++ eZ/Publish/Core/MVC/Symfony/Templating/Tests/Twig/LoaderStringTest.php
@@ -1,9 +1,10 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: lolautruche
- * Date: 24/03/14
- * Time: 14:13
+ * File containing the LoaderStringTest class.
+ *
+ * @copyright Copyright (C) 1999-2014 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
  */
 
 namespace eZ\Publish\Core\MVC\Symfony\Templating\Tests\Twig;
