ff613a35f5b7b029fee35612a5db0c3962d338b3
iosolinf <github@io-solinf.pt>
2014-04-10 11:58:01 +0100

    Fix EZP-22617: Added tests (unit & integration)

---
+++ eZ/Publish/SPI/Tests/FieldType/ISBNIntegrationTest.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * File contains: eZ\Publish\Core\Persistence\Legacy\Tests\HandlerTest class
+ *
+ * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU General Public License v2.0
+ * @version 
+ */
+
+namespace eZ\Publish\SPI\Tests\FieldType;
+
+use eZ\Publish\Core\Persistence\Legacy;
+use eZ\Publish\Core\FieldType;
+use eZ\Publish\SPI\Persistence\Content;
+
+/**
+ * Integration test for legacy storage field types
+ *
+ * This abstract base test case is supposed to be the base for field type
+ * integration tests. It basically calls all involved methods in the field type
+ * ``Converter`` and ``Storage`` implementations. Fo get it working implement
+ * the abstract methods in a sensible way.
+ *
+ * The following actions are performed by this test using the custom field
+ * type:
+ *
+ * - Create a new content type with the given field type
+ * - Load create content type
+ * - Create content object of new content type
+ * - Load created content
+ * - Copy created content
+ * - Remove copied content
+ *
+ * @group integration
+ * @group ezisbn
+ */
+class ISBNIntegrationTest extends BaseIntegrationTest
+{
+    /**
+     * Get name of tested field type
+     *
+     * @return string
+     */
+    public function getTypeName()
+    {
+        return 'ezisbn';
+    }
+
+    /**
+     * Get handler with required custom field types registered
+     *
+     * @return Handler
+     */
+    public function getCustomHandler()
+    {
+        return $this->getHandler();
+    }
+
+    /**
+     * Returns the FieldTypeConstraints to be used to create a field definition
+     * of the FieldType under test.
+     *
+     * @return \eZ\Publish\SPI\Persistence\Content\FieldTypeConstraints
+     */
+    public function getTypeConstraints()
+    {
+        return new Content\FieldTypeConstraints();
+    }
+
+    /**
+     * Get field definition data values
+     *
+     * This is a PHPUnit data provider
+     *
+     * @return array
+     */
+    public function getFieldDefinitionData()
+    {
+        return array(
+            // The ezisbn field type does not have any special field definition
+            // properties
+            array( 'fieldType', 'ezisbn' ),
+            array(
+                'fieldTypeConstraints',
+                new Content\FieldTypeConstraints(
+                    array(
+                        'validators' => array(
+                            'ISBNValueValidator' => array(
+                                'isISBN13' => true,
+                            ),
+                        ),
+                        'fieldSettings' => new FieldType\FieldSettings(
+                            array(
+                                'isISBN13' => true
+                            )
+                        ),
+                    )
+                )
+            ),
+        );
+    }
+
+    /**
+     * Get initial field value
+     *
+     * @return \eZ\Publish\SPI\Persistence\Content\FieldValue
+     */
+    public function getInitialValue()
+    {
+        return new Content\FieldValue(
+            array(
+                'data'         => "978-972-25-1409-5",
+                'externalData' => null,
+                'sortKey'      => "978-972-25-1409-5",
+            )
+        );
+    }
+
+    /**
+     * Get update field value.
+     *
+     * Use to update the field
+     *
+     * @return \eZ\Publish\SPI\Persistence\Content\FieldValue
+     */
+    public function getUpdatedValue()
+    {
+        return new Content\FieldValue(
+            array(
+                'data'         => "978-972-25-1409-5",
+                'externalData' => null,
+                'sortKey'      => "978-972-25-1409-5",
+            )
+        );
+    }
+}
