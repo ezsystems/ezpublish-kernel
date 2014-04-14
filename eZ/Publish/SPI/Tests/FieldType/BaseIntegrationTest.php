b131d27daa5c0360d72d7e54be8949721932c88e
Petar Španja <petar.spanja@gmail.com>
2014-04-14 14:33:34 +0200

    EZP-22465: updated: SPI field type integration test

--- eZ/Publish/SPI/Tests/FieldType/BaseIntegrationTest.php
+++ eZ/Publish/SPI/Tests/FieldType/BaseIntegrationTest.php
@@ -590,6 +590,7 @@ abstract class BaseIntegrationTest extends TestCase
         $loader->load( 'storage_engines/common.yml' );
         $loader->load( 'storage_engines/legacy.yml' );
         $loader->load( 'storage_engines/cache.yml' );
+        $loader->load( 'storage_engines/cached_legacy.yml' );
         $loader->load( 'settings.yml' );
         $loader->load( 'fieldtype_services.yml' );
 
