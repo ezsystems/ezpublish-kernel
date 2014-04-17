67335c13088b830cb9f3433f8c5091d68b4ae00c
Petar Španja <petar.spanja@gmail.com>
2014-04-17 13:51:27 +0200

    EZP-22465: removed deprecation marker

--- container.php
+++ container.php
@@ -4,7 +4,6 @@
  *
  * Expects global $config to be set by caller
  *
- * @deprecated Since 5.0, this is only used for unit tests.
  * @copyright Copyright (C) 1999-2014 eZ Systems AS. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
  * @version //autogentag//
