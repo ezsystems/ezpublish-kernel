ebd03afc09ba2d762766b9585ed270a543090590
Petar Španja <petar.spanja@gmail.com>
2014-04-28 16:22:48 +0200

    EZP-20800: implemented RoleLimitationConverter compile pass

---
+++ eZ/Bundle/EzPublishCoreBundle/Tests/DependencyInjection/Compiler/Storage/Legacy/RoleLimitationConverterPassTest.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * File containing the RoleLimitationConverterPassTest class.
+ *
+ * @copyright Copyright (C) 1999-2014 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Bundle\EzPublishCoreBundle\Tests\DependencyInjection\Compiler\Storage\Legacy;
+
+use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Compiler\Storage\Legacy\RoleLimitationConverterPass;
+use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractCompilerPassTest;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+
+class RoleLimitationConverterPassTest extends AbstractCompilerPassTest
+{
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->setDefinition(
+            'ezpublish.persistence.legacy.role.limitation.converter',
+            new Definition()
+        );
+    }
+
+    /**
+     * Register the compiler pass under test, just like you would do inside a bundle's load()
+     * method:
+     *
+     *   $container->addCompilerPass(new MyCompilerPass());
+     */
+    protected function registerCompilerPass( ContainerBuilder $container )
+    {
+        $container->addCompilerPass( new RoleLimitationConverterPass() );
+    }
+
+    public function testRegisterRoleLimitationConverter()
+    {
+        $serviceId = 'service_id';
+        $def = new Definition();
+        $def->addTag( 'ezpublish.persistence.legacy.role.limitation.handler' );
+        $this->setDefinition( $serviceId, $def );
+
+        $this->compile();
+
+        $this->assertContainerBuilderHasServiceDefinitionWithMethodCall(
+            'ezpublish.persistence.legacy.role.limitation.converter',
+            'addHandler',
+            array( new Reference( $serviceId ) )
+        );
+    }
+}
