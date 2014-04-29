2c010c4580eac15421f5acd38fc52517834250f4
Yannick ROGER <yannick.roger@ez.no>
2014-04-29 15:25:45 +0200

    Fix EZP-22767: ezscript_require not taken into account on modules

--- eZ/Publish/Core/MVC/Symfony/Templating/Tests/Twig/Extension/ContentExtensionTest.php
+++ eZ/Publish/Core/MVC/Symfony/Templating/Tests/Twig/Extension/ContentExtensionTest.php
@@ -17,13 +17,8 @@ use eZ\Publish\Core\Repository\Values\Content\Content;
 use eZ\Publish\Core\Repository\Values\Content\VersionInfo;
 use eZ\Publish\API\Repository\Values\Content\ContentInfo;
 use eZ\Publish\API\Repository\Values\Content\Field;
-use Twig_Test_IntegrationTestCase;
-use Twig_Environment;
-use Twig_Loader_Filesystem;
-use Twig_Loader_Chain;
-use Twig_Loader_Array;
 
-class ContentExtensionIntegrationTest extends Twig_Test_IntegrationTestCase
+class ContentExtensionIntegrationTest extends FileSystemTwigIntegrationTestCase
 {
     /**
      * @var \eZ\Publish\API\Repository\ContentTypeService|\PHPUnit_Framework_MockObject_MockObject
@@ -54,143 +49,6 @@ class ContentExtensionIntegrationTest extends Twig_Test_IntegrationTestCase
                 $this->fieldHelperMock
             )
         );
-    }
-
-    /**
-     * Overrides the default implementation to use the chain loader so that
-     * templates used internally by ez_render_* are correctly loaded
-     */
-    protected function doIntegrationTest( $file, $message, $condition, $templates, $exception, $outputs )
-    {
-        if ( $condition )
-        {
-            eval( '$ret = ' . $condition . ';' );
-            if ( !$ret )
-            {
-                $this->markTestSkipped( $condition );
-            }
-        }
-
-        // changes from the original is here
-        $loader = new Twig_Loader_Chain(
-            array(
-                new Twig_Loader_Array( $templates ),
-                new Twig_Loader_Filesystem( $this->getFixturesDir() )
-            )
-        );
-        // end changes
-
-        foreach ( $outputs as $match )
-        {
-            $config = array_merge(
-                array(
-                    'cache' => false,
-                    'strict_variables' => true,
-                ),
-                $match[2] ? eval( $match[2] . ';' ) : array()
-            );
-            $twig = new Twig_Environment( $loader, $config );
-            $twig->addGlobal( 'global', 'global' );
-            foreach ( $this->getExtensions() as $extension )
-            {
-                $twig->addExtension( $extension );
-            }
-
-            try
-            {
-                $template = $twig->loadTemplate( 'index.twig' );
-            }
-            catch ( Exception $e )
-            {
-                if ( false !== $exception )
-                {
-                    $this->assertEquals(
-                        trim( $exception ),
-                        trim(
-                            sprintf( '%s: %s', get_class( $e ), $e->getMessage() )
-                        )
-                    );
-
-                    return;
-                }
-
-                if ( $e instanceof Twig_Error_Syntax )
-                {
-                    $e->setTemplateFile( $file );
-
-                    throw $e;
-                }
-
-                throw new Twig_Error(
-                    sprintf( '%s: %s', get_class( $e ), $e->getMessage() ),
-                    -1, $file, $e
-                );
-            }
-
-            try
-            {
-                $output = trim(
-                    $template->render( eval( $match[1] . ';' ) ), "\n "
-                );
-            }
-            catch (Exception $e)
-            {
-                if ( false !== $exception )
-                {
-                    $this->assertEquals(
-                        trim( $exception ),
-                        trim(
-                            sprintf( '%s: %s', get_class( $e ), $e->getMessage() )
-                        )
-                    );
-
-                    return;
-                }
-
-                if ( $e instanceof Twig_Error_Syntax )
-                {
-                    $e->setTemplateFile( $file );
-                }
-                else
-                {
-                    $e = new Twig_Error(
-                        sprintf( '%s: %s', get_class( $e ), $e->getMessage() ),
-                        -1, $file, $e
-                    );
-                }
-
-                $output = trim(
-                    sprintf( '%s: %s', get_class( $e ), $e->getMessage() )
-                );
-            }
-
-            if ( false !== $exception )
-            {
-                list( $class, ) = explode( ':', $exception );
-                $this->assertThat(
-                    null, new PHPUnit_Framework_Constraint_Exception( $class )
-                );
-            }
-
-            $expected = trim( $match[3], "\n " );
-
-            if ( $expected != $output )
-            {
-                echo 'Compiled template that failed:';
-
-                foreach ( array_keys( $templates ) as $name )
-                {
-                    echo "Template: $name\n";
-                    $source = $loader->getSource( $name );
-                    echo $twig->compile(
-                        $twig->parse( $twig->tokenize( $source, $name ) )
-                    );
-                }
-            }
-            $this->assertEquals(
-                $expected, $output, $message . ' (in ' . $file . ')'
-            );
-        }
     }
 
     public function getFixturesDir()
