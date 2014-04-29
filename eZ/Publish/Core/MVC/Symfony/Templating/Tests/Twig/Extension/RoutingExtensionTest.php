0822f57e98a0638ee0221d408f01da4ca816607d
Jérôme Vieilledent <lolautruche@gmail.com>
2014-04-29 15:02:14 +0200

    EZP-22464: Implemented ez_route() Twig function

---
+++ eZ/Publish/Core/MVC/Symfony/Templating/Tests/Twig/Extension/RoutingExtensionTest.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * File containing the RoutingExtensionTest class.
+ *
+ * @copyright Copyright (C) 1999-2014 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\Core\MVC\Symfony\Templating\Tests\Twig\Extension;
+
+use eZ\Publish\Core\MVC\Symfony\Routing\Generator\RouteReferenceGenerator;
+use eZ\Publish\Core\MVC\Symfony\Templating\Twig\Extension\RoutingExtension;
+use Symfony\Component\HttpFoundation\Request;
+use Twig_Test_IntegrationTestCase;
+
+class RoutingExtensionTest extends Twig_Test_IntegrationTestCase
+{
+    protected function getExtensions()
+    {
+        return array(
+            new RoutingExtension( $this->getRouteReferenceGenerator() )
+        );
+    }
+
+    protected function getFixturesDir()
+    {
+        return __DIR__ . '/_fixtures/functions/ez_route';
+    }
+
+    protected function getRouteReferenceGenerator()
+    {
+        $generator = new RouteReferenceGenerator(
+            $this->getMock( 'Symfony\Component\EventDispatcher\EventDispatcherInterface' )
+        );
+        $generator->setRequest( new Request() );
+
+        return $generator;
+    }
+}
