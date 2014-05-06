9d5b479dfd93320ad34f986d0d71aec845a926c2
Jérôme Vieilledent <lolautruche@gmail.com>
2014-05-06 12:04:53 +0200

    EZP-22464: Added more description to LanguageSwitchListener

--- eZ/Publish/Core/MVC/Symfony/EventListener/LanguageSwitchListener.php
+++ eZ/Publish/Core/MVC/Symfony/EventListener/LanguageSwitchListener.php
@@ -16,7 +16,7 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
  * Listener for language switcher.
- * Will be triggered when generating a RouteReference
+ * Will be triggered when generating a RouteReference.
  */
 class LanguageSwitchListener implements EventSubscriberInterface
 {
@@ -37,6 +37,16 @@ class LanguageSwitchListener implements EventSubscriberInterface
         );
     }
 
+    /**
+     * If "language" parameter is present, will try to get corresponding SiteAccess for translation.
+     * If found, it will add "siteaccess" parameter to the RouteReference, to trigger SiteAccess switch when generating
+     * the final link.
+     *
+     * @see \eZ\Publish\Core\MVC\Symfony\Routing\Generator::generate()
+     * @see \eZ\Publish\Core\MVC\Symfony\Routing\Generator\UrlAliasGenerator::doGenerate()
+     *
+     * @param \eZ\Publish\Core\MVC\Symfony\Event\RouteReferenceGenerationEvent $event
+     */
     public function onRouteReferenceGeneration( RouteReferenceGenerationEvent $event )
     {
         $routeReference = $event->getRouteReference();
