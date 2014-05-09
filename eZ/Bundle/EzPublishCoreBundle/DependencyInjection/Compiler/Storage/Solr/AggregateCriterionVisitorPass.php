ef124eca1781b4326c7ecc978602b641915e1b36
Petar Španja <petar.spanja@gmail.com>
2014-05-09 16:48:39 +0200

    EZP-22799: add Solr Storage container compile passes

---
+++ eZ/Bundle/EzPublishCoreBundle/DependencyInjection/Compiler/Storage/Solr/AggregateCriterionVisitorPass.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * File containing the AggregateCriterionVisitorPass class.
+ *
+ * @copyright Copyright (C) 1999-2014 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Compiler\Storage\Solr;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+/**
+ * This compiler pass will register Solr Storage criterion visitors.
+ */
+class AggregateCriterionVisitorPass implements CompilerPassInterface
+{
+    /**
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     */
+    public function process( ContainerBuilder $container )
+    {
+        if ( !$container->hasDefinition( 'ezpublish.persistence.solr.search.content.criterion_visitor.aggregate' ) )
+        {
+            return;
+        }
+
+        $aggregateCriterionVisitorDefinition = $container->getDefinition(
+            'ezpublish.persistence.solr.search.content.criterion_visitor.aggregate'
+        );
+
+        foreach ( $container->findTaggedServiceIds( 'ezpublish.persistence.solr.search.content.criterion_visitor' ) as $id => $attributes )
+        {
+            $aggregateCriterionVisitorDefinition->addMethodCall(
+                'addVisitor',
+                array(
+                    new Reference( $id ),
+                )
+            );
+        }
+    }
+}
