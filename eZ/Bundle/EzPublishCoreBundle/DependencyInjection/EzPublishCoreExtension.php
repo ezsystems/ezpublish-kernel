9e566d3961fe58d69af597da17f485dc992d954c
Petar Španja <petar.spanja@gmail.com>
2014-05-09 17:31:39 +0200

    EZP-22799: update bundle dependency injection extension

--- eZ/Bundle/EzPublishCoreBundle/DependencyInjection/EzPublishCoreExtension.php
+++ eZ/Bundle/EzPublishCoreBundle/DependencyInjection/EzPublishCoreExtension.php
@@ -270,9 +270,11 @@ class EzPublishCoreExtension extends Extension
 
         // Storage engine
         $loader->load( 'storage_engines.yml' );
+        $loader->load( 'indexable_fieldtypes.yml' );
         $loader->load( "storage_engines/common.yml" );
         $loader->load( "storage_engines/cache.yml" );
         $loader->load( "storage_engines/legacy.yml" );
+        $loader->load( "storage_engines/legacy_solr.yml" );
     }
 
     /**
