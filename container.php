49bf21189c2d7d0fa197c476193380304c87ba4c
Petar Španja <petar.spanja@gmail.com>
2014-05-14 20:17:42 +0200

    EZP-22662: annotated container.php as internal

--- container.php
+++ container.php
@@ -4,6 +4,9 @@
  *
  * Expects global $config to be set by caller
  *
+ * @internal You can include this file to get started, but note that this is not
+ *           supported way of using the eZ Publish Kernel
+ *
  * @copyright Copyright (C) 1999-2014 eZ Systems AS. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
  * @version //autogentag//
