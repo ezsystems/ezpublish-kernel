498ac35159f7c80cae9c4838de53c22a7e8c77f5
Benjamin Choquet <benjamin.choquet@heliopsis.net>
2014-05-16 10:25:40 +0200

    remove license tags

--- eZ/Publish/API/Repository/Tests/Regression/EZP22408DeleteRelatedObjectTest.php
+++ eZ/Publish/API/Repository/Tests/Regression/EZP22408DeleteRelatedObjectTest.php
@@ -2,8 +2,6 @@
 /**
  * File containing the EZP22408DeleteRelatedObjectTest class
  *
- * @copyright Copyright (C) 1999-2014 eZ Systems AS. All rights reserved.
- * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
  * @version //autogentag//
  */
 
