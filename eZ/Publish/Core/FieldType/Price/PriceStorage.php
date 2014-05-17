1f3a5213b3bac96b27fb89c084d8a230721be3a7
crevillo <crevillo@gmail.com>
2014-05-17 15:24:25 +0200

    getting the value and adding block in content fields template

---
+++ eZ/Publish/Core/FieldType/Price/PriceStorage.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: carlos
+ * Date: 16/05/14
+ * Time: 16:34
+ */
+
+namespace eZ\Publish\Core\FieldType\Price;
+
+
+class PriceStorage {
+
+} 
---
+++ eZ/Publish/Core/FieldType/Price/Type.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: carlos
+ * Date: 15/05/14
+ * Time: 16:59
+ */
+
+namespace eZ\Publish\Core\FieldType\Price;
+
+
+class Type {
+
+} 
