1513f92dc1f85dbbc5b8209f0abb1c67e1a455b7
MLoureiro <mloureiro.dev@gmail.com>
2014-06-09 16:29:11 +0100

    Add fixes for the 'common' to 'specific' changes

--- eZ/Bundle/EzPublishRestBundle/Features/Context/SubContext/Exception.php
+++ eZ/Bundle/EzPublishRestBundle/Features/Context/SubContext/Exception.php
@@ -14,29 +14,72 @@ use Behat\Behat\Context\Step;
 
 class Exception extends Base implements ExceptionSentences
 {
+    /**
+     * Then I see (?:an |)invalid field (?:exception|error)
+     */
     public function iSeeAnInvalidFieldError()
     {
-        $this->getMainContext()->setLastAction( "invalid" );
-
-        $errorDescriptionRegEx = "/^Argument '([^']*)' is invalid:(.*)/";
-
         return array(
             new Step\Then( 'I see 403 status code' ),
             new Step\Then( 'I see "Forbidden" status message' ),
             new Step\Then( 'I see "content-type" header with an "ErrorMessage"' ),
             new Step\Then( 'I see response body with "eZ\\Publish\\Core\\REST\\Common\\Exceptions\\ForbiddenException" object' ),
             new Step\Then( 'I see response error 403 status code' ),
-            new Step\Then( 'I see response error description with "' . $errorDescriptionRegEx . '"' ),
+            new Step\Then( 'I see response error description with "' . self::REGEX_INVALID_FIELD_MESSAGE . '"' ),
         );
     }
 
+    /**
+     * Then I see (?:a |)forbidden (?:exception|error)
+     */
+    public function iSeeAForbiddenError()
+    {
+        return array(
+            new Step\Then( 'I see 403 status code' ),
+            new Step\Then( 'I see "Forbidden" status message' ),
+            new Step\Then( 'I see "content-type" header with an "ErrorMessage"' ),
+            new Step\Then( 'I see response error 403 status code' ),
+        );
+    }
+
+    /**
+     * Then I see (?:a |)forbidden (?:exception|error) with "<message>" message
+     */
+    public function iSeeAForbiddenErrorWithMessage( $message )
+    {
+        return array(
+            new Step\Then( 'I see 403 status code' ),
+            new Step\Then( 'I see "Forbidden" status message' ),
+            new Step\Then( 'I see "content-type" header with an "ErrorMessage"' ),
+            new Step\Then( 'I see response error 403 status code' ),
+            new Step\Then( 'I see response error description with "/' . $message . '/"' ),
+        );
+    }
+
+    /**
+     * Then I see (?:a |)not authorized (?:exception|error)
+     * Then I see (?:an |)unauthorized (?:exception|error)
+     */
     public function iSeeNotAuthorizedError()
     {
-        $this->getMainContext()->setLastAction( "unauthorized" );
-
         return array(
             new Step\Then( 'I see 401 status code' ),
             new Step\Then( 'I see "Unauthorized" status message' ),
+        );
+    }
+
+    /**
+     * Then I see (?:a |)not found (?:exception|error)
+     */
+    public function iSeeNotFoundError()
+    {
+        return array(
+            new Step\Then( 'I see 404 status code' ),
+            new Step\Then( 'I see "Not Found" status message' ),
+            new Step\Then( 'I see "content-type" header with an "ErrorMessage"' ),
+            new Step\Then( 'I see response body with "eZ\\Publish\\Core\\REST\\Common\\Exceptions\\NotFoundException" object' ),
+            new Step\Then( 'I see response error 404 status code' ),
+            new Step\Then( 'I see response error description with "' . self::REGEX_NOT_FOUND_MESSAGE . '"' ),
         );
     }
 }
