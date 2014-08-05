bd3713c39a7423ee8539e404cc808b9322a05610
Petar Španja <petar.spanja@gmail.com>
2014-08-05 08:24:18 +0200

    EZP-22564: implement dedicated Location criterion visitors

--- eZ/Publish/Core/Persistence/Elasticsearch/Content/Search/Location/CriterionVisitor/Location/PriorityRange.php
+++ eZ/Publish/Core/Persistence/Elasticsearch/Content/Search/Location/CriterionVisitor/Location/PriorityRange.php
@@ -1,25 +1,22 @@
 <?php
 /**
- * File containing the range FieldRange Field criterion visitor class
+ * File containing the range PriorityRange Priority criterion visitor class
  *
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  * @version //autogentag//
  */
 
-namespace eZ\Publish\Core\Persistence\Elasticsearch\Content\Search\CriterionVisitor\Field;
+namespace eZ\Publish\Core\Persistence\Elasticsearch\Content\Search\Location\CriterionVisitor\Location;
 
 use eZ\Publish\Core\Persistence\Elasticsearch\Content\Search\CriterionVisitor;
-use eZ\Publish\Core\Persistence\Elasticsearch\Content\Search\CriterionVisitor\Field;
 use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
 use eZ\Publish\API\Repository\Values\Content\Query\Criterion\Operator;
-use eZ\Publish\Core\Base\Exceptions\InvalidArgumentException;
-use RuntimeException;
 
 /**
- * Visits the Field criterion with range operators (LT, LTE, GT, GTE and BETWEEN)
+ * Visits the Priority criterion with range operators (LT, LTE, GT, GTE and BETWEEN)
  */
-class FieldRange extends Field
+class PriorityRange extends CriterionVisitor
 {
     /**
      * Check if visitor is applicable to current criterion
@@ -31,7 +28,7 @@ class FieldRange extends Field
     public function canVisit( Criterion $criterion )
     {
         return
-            $criterion instanceof Criterion\Field &&
+            $criterion instanceof Criterion\Location\Priority &&
             (
                 $criterion->operator === Operator::LT ||
                 $criterion->operator === Operator::LTE ||
@@ -53,41 +50,12 @@ class FieldRange extends Field
      */
     public function visit( Criterion $criterion, CriterionVisitor $subVisitor = null )
     {
-        /** @var \eZ\Publish\API\Repository\Values\Content\Query\Criterion\Field $criterion */
-        $fieldTypes = $this->getFieldTypes( $criterion );
-        $criterion->value = (array)$criterion->value;
-
-        if ( !isset( $fieldTypes[$criterion->target] ) )
-        {
-            throw new InvalidArgumentException(
-                "\$criterion->target",
-                "No searchable fields found for the given criterion target '{$criterion->target}'."
-            );
-        }
-
         $start = $criterion->value[0];
         $end = isset( $criterion->value[1] ) ? $criterion->value[1] : null;
-        $range = $this->getRange( $criterion->operator, $start, $end );
-
-        $ranges = array();
-        foreach ( $fieldTypes[$criterion->target] as $names )
-        {
-            foreach ( $names as $name )
-            {
-                $ranges[] = array(
-                    "range" => array(
-                        "fields_doc.". $name => $range,
-                    ),
-                );
-            }
-        }
 
         return array(
-            "nested" => array(
-                "path" => "fields_doc",
-                "filter" => array(
-                    "or" => $ranges,
-                ),
+            "range" => array(
+                "priority_i" => $this->getRange( $criterion->operator, $start, $end ),
             ),
         );
     }
