5b4267d1d4677887690834a18c235d9adfd46235
MLoureiro <mloureiro.dev@gmail.com>
2014-08-25 15:49:39 +0100

    Add Admin2Context for further testing

---
+++ eZ/Bundle/EzPublishLegacyBundle/Features/Context/Admin2Context.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * File containing the Admin2Context class for the LegacyBundle.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
+
+namespace eZ\Bundle\EzPublishLegacyBundle\Features\Context;
+
+class Admin2Context extends LegacyContext
+{
+}
