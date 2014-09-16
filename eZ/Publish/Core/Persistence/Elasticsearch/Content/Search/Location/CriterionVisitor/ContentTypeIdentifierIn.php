9c36ad80363e9ac63d8ff35c17eae0077f6d9354
Petar Španja <petar.spanja@gmail.com>
2014-09-16 12:48:03 +0200

    EZP-22564: implemented ContentTypeIdentifier criterion visitors

---
+++ eZ/Publish/Core/Persistence/Elasticsearch/Content/Search/Location/CriterionVisitor/ContentTypeIdentifierIn.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * File containing the ContentTypeIdentifierIn criterion visitor class
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\Core\Persistence\Elasticsearch\Content\Search\Location\CriterionVisitor;
+
+use eZ\Publish\Core\Persistence\Elasticsearch\Content\Search\CriterionVisitor;
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion\Operator;
+use eZ\Publish\SPI\Persistence\Content\Type\Handler as ContentTypeHandler;
+
+/**
+ * Visits the ContentTypeIdentifier criterion
+ */
+class ContentTypeIdentifierIn extends CriterionVisitor
+{
+    /**
+     * ContentType handler
+     *
+     * @var \eZ\Publish\SPI\Persistence\Content\Type\Handler
+     */
+    protected $contentTypeHandler;
+
+    /**
+     * Create from content type handler
+     *
+     * @param \eZ\Publish\SPI\Persistence\Content\Type\Handler $contentTypeHandler
+     */
+    public function __construct( ContentTypeHandler $contentTypeHandler )
+    {
+        $this->contentTypeHandler = $contentTypeHandler;
+    }
+
+    /**
+     * Check if visitor is applicable to current criterion
+     *
+     * @param \eZ\Publish\API\Repository\Values\Content\Query\Criterion $criterion
+     *
+     * @return boolean
+     */
+    public function canVisit( Criterion $criterion )
+    {
+        return
+            $criterion instanceof Criterion\ContentTypeIdentifier &&
+            (
+                ( $criterion->operator ?: Operator::IN ) === Operator::IN ||
+                $criterion->operator === Operator::EQ
+            );
+    }
+
+    /**
+     * Map field value to a proper Elasticsearch representation
+     *
+     * @param \eZ\Publish\API\Repository\Values\Content\Query\Criterion $criterion
+     * @param \eZ\Publish\Core\Persistence\Elasticsearch\Content\Search\CriterionVisitor $subVisitor
+     *
+     * @return string
+     */
+    public function visit( Criterion $criterion, CriterionVisitor $subVisitor = null )
+    {
+        if ( count( $criterion->value ) > 1 )
+        {
+            $filter = array(
+                "terms" => array(
+                    "content_type_id" => $this->contentTypeHandler->loadByIdentifier( $criterion->value )->id,
+                ),
+            );
+        }
+        else
+        {
+            $filter = array(
+                "term" => array(
+                    "content_type_id" => $this->contentTypeHandler->loadByIdentifier( $criterion->value[0] )->id,
+                ),
+            );
+        }
+
+        return $filter;
+    }
+}
