d65e51aae5070c3e8177c66d15196865d7bebbe6
Petar Španja <petar.spanja@gmail.com>
2014-09-16 14:04:54 +0200

    EZP-22564: implemented MatchNone criterion visitor

---
+++ eZ/Publish/Core/Persistence/Elasticsearch/Content/Search/CriterionVisitor/MatchNone.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * File containing the MatchNone criterion visitor class
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\Core\Persistence\Elasticsearch\Content\Search\CriterionVisitor;
+
+use eZ\Publish\Core\Persistence\Elasticsearch\Content\Search\CriterionVisitor;
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
+
+/**
+ * Visits the MatchNone criterion
+ */
+class MatchNone extends CriterionVisitor
+{
+    /**
+     * Check if visitor is applicable to current criterion
+     *
+     * @param \eZ\Publish\API\Repository\Values\Content\Query\Criterion $criterion
+     *
+     * @return boolean
+     */
+    public function canVisit( Criterion $criterion )
+    {
+        return $criterion instanceof Criterion\MatchNone;
+    }
+
+    /**
+     * Map field value to a proper Elasticsearch representation
+     *
+     * @param \eZ\Publish\API\Repository\Values\Content\Query\Criterion $criterion
+     * @param \eZ\Publish\Core\Persistence\Elasticsearch\Content\Search\CriterionVisitor $subVisitor
+     *
+     * @return string
+     */
+    public function visit( Criterion $criterion, CriterionVisitor $subVisitor = null )
+    {
+        return array(
+            "terms" => array(
+                "_id" => array(),
+            ),
+        );
+    }
+}
