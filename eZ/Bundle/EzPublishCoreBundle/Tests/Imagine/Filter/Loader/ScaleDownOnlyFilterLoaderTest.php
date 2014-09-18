3bae19507b07436a7eccde70b9326bdfdfaed861
Jérôme Vieilledent <jv@ez.no>
2014-09-18 12:09:19 +0200

    EZP-22280: Added tests for ScaleDownOnlyFilterLoader

---
+++ eZ/Bundle/EzPublishCoreBundle/Tests/Imagine/Filter/Loader/ScaleDownOnlyFilterLoaderTest.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * File containing the ScaleDownOnlyFilterLoaderTest class.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
+
+namespace eZ\Bundle\EzPublishCoreBundle\Tests\Imagine\Filter\Loader;
+
+use eZ\Bundle\EzPublishCoreBundle\Imagine\Filter\Loader\ScaleDownOnlyFilterLoader;
+use Imagine\Image\ImageInterface;
+use PHPUnit_Framework_TestCase;
+
+class ScaleDownOnlyFilterLoaderTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $thumbnailLoader;
+
+    /**
+     * @var ScaleDownOnlyFilterLoader
+     */
+    private $loader;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->thumbnailLoader = $this->getMock( 'Liip\ImagineBundle\Imagine\Filter\Loader\LoaderInterface' );
+        $this->loader = new ScaleDownOnlyFilterLoader( $this->thumbnailLoader );
+    }
+
+    /**
+     * @expectedException \Imagine\Exception\InvalidArgumentException
+     * @dataProvider loadInvalidProvider
+     */
+    public function testLoadInvalidOptions( array $options )
+    {
+        $this->loader->load( $this->getMock( '\Imagine\Image\ImageInterface' ), $options );
+    }
+
+    public function loadInvalidProvider()
+    {
+        return array(
+            array( array() ),
+            array( array( 123 ) ),
+            array( array( 'foo' => 'bar' ) ),
+        );
+    }
+
+    public function testLoad()
+    {
+        $options = array( 123, 456 );
+        $image = $this->getMock( '\Imagine\Image\ImageInterface' );
+        $this->thumbnailLoader
+            ->expects( $this->once() )
+            ->method( 'load' )
+            ->with( $image, $this->equalTo( array( 'size' => $options, 'mode' => ImageInterface::THUMBNAIL_INSET ) ) )
+            ->will( $this->returnValue( $image ) );
+
+        $this->assertSame( $image, $this->loader->load( $image, $options ) );
+    }
+}
