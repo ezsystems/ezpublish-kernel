b32a9de846b1a54fd219e4f0916c50c5639ec3fe
Jérôme Vieilledent <jv@ez.no>
2014-09-18 15:06:12 +0200

    EZP-22280: Implemented ScaleWidthDownOnly / ScaleHeightDownOnly filter loaders

---
+++ eZ/Bundle/EzPublishCoreBundle/Imagine/Filter/Loader/ScaleWidthDownOnlyFilterLoader.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * File containing the ScaleWidthDownOnlyFilterLoader class.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
+
+namespace eZ\Bundle\EzPublishCoreBundle\Imagine\Filter\Loader;
+
+use Imagine\Image\ImageInterface;
+use Imagine\Exception\InvalidArgumentException;
+
+/**
+ * Filter loader for geometry/scalewidthdownonly filter.
+ * Proxy to ThumbnailFilterLoader
+ */
+class ScaleWidthDownOnlyFilterLoader extends FilterLoaderWrapped
+{
+    public function load( ImageInterface $image, array $options = array() )
+    {
+        if ( empty( $options ) )
+        {
+            throw new InvalidArgumentException( 'Missing width option' );
+        }
+
+        return $this->innerLoader->load(
+            $image,
+            array(
+                'size' => array( $options[0], null ),
+                'mode' => ImageInterface::THUMBNAIL_INSET
+            )
+        );
+    }
+}
