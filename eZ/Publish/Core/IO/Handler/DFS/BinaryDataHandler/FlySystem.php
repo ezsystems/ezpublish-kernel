92237282cd84e24f7ae90f1db3b123f135bbe417
Bertrand Dunogier <bd@ez.no>
2014-09-23 21:50:06 +0200

    DFS: Made UrlDecorator optional

--- eZ/Publish/Core/IO/Handler/DFS/BinaryDataHandler/FlySystem.php
+++ eZ/Publish/Core/IO/Handler/DFS/BinaryDataHandler/FlySystem.php
@@ -24,7 +24,7 @@ class FlySystem implements BinaryDataHandler
      */
     private $urlDecorator;
 
-    public function __construct( AdapterInterface $adapter, UrlDecorator $urlDecorator )
+    public function __construct( AdapterInterface $adapter, UrlDecorator $urlDecorator = null )
     {
         $this->filesystem = new FileSystem( $adapter );
         $this->urlDecorator = $urlDecorator;
@@ -135,6 +135,6 @@ class FlySystem implements BinaryDataHandler
      */
     public function getUri( $path )
     {
-        return $this->urlDecorator->decorate( $path );
+        return isset( $this->urlDecorator ) ? $this->urlDecorator->decorate( $path ) : '/' . $path;
     }
 }
