9d3c824b4fffd599307c0926371eea108b740bd2
Petar Španja <petar.spanja@gmail.com>
2014-09-24 11:14:22 +0200

    EZP-22564: implement private methods in Cache LocationSearchHandler

--- eZ/Publish/Core/Persistence/Cache/LocationSearchHandler.php
+++ eZ/Publish/Core/Persistence/Cache/LocationSearchHandler.php
@@ -55,4 +55,44 @@ class LocationSearchHandler extends AbstractHandler implements LocationSearchHan
         $this->logger->logCall( __METHOD__, array( 'contentId' => $contentId ) );
         $this->persistenceHandler->locationSearchHandler()->deleteContent( $contentId );
     }
+
+    /**
+     * Indexes several Location objects at once
+     *
+     * @todo: This function and setCommit() is needed for Persistence\Solr for test speed but not part
+     *       of interface for the reason described in Solr\Content\Search\Gateway\Native::bulkIndexContent
+     *       Short: Bulk handling should be properly designed before added to the interface.
+     *
+     * @param \eZ\Publish\SPI\Persistence\Content\Location[] $locations
+     *
+     * @return void
+     */
+    public function bulkIndexLocations( array $locations )
+    {
+        $this->persistenceHandler->locationSearchHandler()->bulkIndexLocations( $locations );
+    }
+
+    /**
+     * Purges all contents from the index
+     *
+     * @todo: Make this public API?
+     *
+     * @return void
+     */
+    public function purgeIndex()
+    {
+        $this->persistenceHandler->locationSearchHandler()->purgeIndex();
+    }
+
+    /**
+     * Set if index/delete actions should commit or if several actions is to be expected
+     *
+     * This should be set to false before group of actions and true before the last one
+     * (also, see note on bulkIndexContent())
+     * @param bool $commit
+     */
+    public function setCommit( $commit )
+    {
+        $this->persistenceHandler->locationSearchHandler()->setCommit( $commit );
+    }
 }
