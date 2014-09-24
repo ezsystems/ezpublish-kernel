a8e2ce342b827c2a7ef00fdfc9e4649e2abbfdc6
Bertrand Dunogier <bd@ez.no>
2014-09-24 22:07:16 +0200

    Refactored IO elements to EzPublishIOBundle
    See EZP-23328 (sub-task: EZP-23380)
    
    Contains definition of existing (Core) services, as well as semantic configuration + parsing for the io handler.
    
    ez_io:
      system:
        default:
          filesystem
    
    Adds a new service tag: ezpublish.io_handler, with an alias property for the name of the handler.

---
+++ eZ/Bundle/EzPublishIOBundle/DependencyInjection/Compiler/IOHandlerTagPass.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+
+namespace eZ\Bundle\EzPublishIOBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use LogicException;
+
+/**
+ * Registers IO handlers
+ */
+class IOHandlerTagPass implements CompilerPassInterface
+{
+    /**
+     * @throws \LogicException
+     */
+    public function process( ContainerBuilder $container )
+    {
+        $ioHandlersMap = array();
+        foreach ( $container->findTaggedServiceIds( 'ezpublish.io_handler' ) as $id => $attributes )
+        {
+            foreach ( $attributes as $attribute )
+            {
+                if ( !isset( $attribute['alias'] ) )
+                    throw new LogicException(
+                        'ezpublish.io_handler service tag needs an "alias" attribute to identify the handler.'
+                    );
+
+                $ioHandlersMap[$attribute['alias']] = $id;
+            }
+        }
+
+        $container->setParameter( 'ez_io.handlers_map', $ioHandlersMap );
+    }
+}
