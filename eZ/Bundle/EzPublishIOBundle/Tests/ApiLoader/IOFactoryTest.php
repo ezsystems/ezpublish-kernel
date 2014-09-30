629415f229e9d1a2a8dace6642ff893df03dd643
Bertrand Dunogier <bd@ez.no>
2014-09-30 11:46:37 +0200

    IO: tested IOFactory::buildConfiguredHandler

--- eZ/Bundle/EzPublishIOBundle/Tests/ApiLoader/IOFactoryTest.php
+++ eZ/Bundle/EzPublishIOBundle/Tests/ApiLoader/IOFactoryTest.php
@@ -19,15 +19,29 @@ class IOFactoryTest extends \PHPUnit_Framework_TestCase
     private $configResolver;
 
     /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
+     * @var \PHPUnit_Framework_MockObject_MockObject|\eZ\Publish\SPI\IO\MimeTypeDetector
      */
     private $mimeDetector;
+
+    /**
+     * @var \Symfony\Component\DependencyInjection\ContainerInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $container;
+
+    /**
+     * @var IOFactory
+     */
+    private $factory;
 
     protected function setUp()
     {
         parent::setUp();
-        $this->configResolver = $this->getMock( 'eZ\\Publish\\Core\\MVC\\ConfigResolverInterface' );
-        $this->mimeDetector = $this->getMock( 'eZ\\Publish\\SPI\\IO\\MimeTypeDetector' );
+        $this->configResolver = $this->getMock( 'eZ\Publish\Core\MVC\ConfigResolverInterface' );
+        $this->mimeDetector = $this->getMock( 'eZ\Publish\SPI\IO\MimeTypeDetector' );
+        $this->container = $this->getMock( 'Symfony\Component\DependencyInjection\ContainerInterface' );
+        $this->factory = new IOFactory( $this->configResolver, $this->mimeDetector );
+        $this->factory->setContainer( $this->container );
+        $this->factory->setHandlersMap( array( 'configured_handler' => 'my_io_handler' ) );
     }
 
     public function testGetService()
@@ -38,10 +52,9 @@ class IOFactoryTest extends \PHPUnit_Framework_TestCase
             ->method( 'getParameter' )
             ->with( $prefixSetting )
             ->will( $this->returnValue( 'my_prefix' ) );
-        $factory = new IOFactory( $this->configResolver, $this->mimeDetector );
         $this->assertInstanceOf(
-            'eZ\\Publish\\Core\\IO\\IOServiceInterface',
-            $factory->getService( $this->getMock( 'eZ\\Publish\\Core\\IO\\Handler' ), $prefixSetting )
+            'eZ\Publish\Core\IO\IOServiceInterface',
+            $this->factory->getService( $this->getMock( 'eZ\Publish\Core\IO\Handler' ), $prefixSetting )
         );
     }
 
@@ -53,12 +66,10 @@ class IOFactoryTest extends \PHPUnit_Framework_TestCase
             ->method( 'getParameter' )
             ->with( $directorySetting )
             ->will( $this->returnValue( 'something' ) );
-        $factory = new IOFactory( $this->configResolver, $this->mimeDetector );
-
-        $handlerClass = get_class( $this->getMock( 'eZ\\Publish\\Core\\IO\\Handler' ) );
+        $handlerClass = get_class( $this->getMock( 'eZ\Publish\Core\IO\Handler' ) );
         $this->assertInstanceOf(
-            'eZ\\Publish\\Core\\IO\\Handler',
-            $factory->getHandler( $handlerClass, $directorySetting )
+            'eZ\Publish\Core\IO\Handler',
+            $this->factory->getHandler( $handlerClass, $directorySetting )
         );
     }
 
@@ -77,11 +88,51 @@ class IOFactoryTest extends \PHPUnit_Framework_TestCase
                 )
             );
 
-        $handlerClass = get_class( $this->getMock( 'eZ\\Publish\\Core\\IO\\Handler' ) );
-        $factory = new IOFactory( $this->configResolver, $this->mimeDetector );
+        $handlerClass = get_class( $this->getMock( 'eZ\Publish\Core\IO\Handler' ) );
         $this->assertInstanceOf(
-            'eZ\\Publish\\Core\\IO\\Handler',
-            $factory->getHandler( $handlerClass, $directorySettings )
+            'eZ\Publish\Core\IO\Handler',
+            $this->factory->getHandler( $handlerClass, $directorySettings )
         );
+    }
+
+    public function testBuildConfiguredHandler()
+    {
+        $this->configResolver
+            ->expects( $this->once() )
+            ->method( 'getParameter' )
+            ->with( 'handler', 'ez_io' )
+            ->will( $this->returnValue( 'configured_handler' ) );
+
+        $ioHandler = $this->getMock( 'eZ\Publish\Core\IO\Handler' );
+        $this->container
+            ->expects( $this->once() )
+            ->method( 'get' )
+            ->with( 'my_io_handler' )
+            ->will( $this->returnValue( $ioHandler ) );
+
+        $this->assertSame(
+            $ioHandler,
+            $this->factory->buildConfiguredHandler()
+        );
+    }
+
+    /**
+     * @expectedException \Symfony\Component\Config\Definition\Exception\InvalidConfigurationException
+     * @expectedExceptionMessage No IO handler found for alias badly_configured_handler
+     */
+    public function testBuildConfiguredHandlerNotFound()
+    {
+        $this->configResolver
+            ->expects( $this->once() )
+            ->method( 'getParameter' )
+            ->with( 'handler', 'ez_io' )
+            ->will( $this->returnValue( 'badly_configured_handler' ) );
+
+        $this->container
+            ->expects( $this->never() )
+            ->method( 'get' )
+            ->with( 'badly_configured_handler' );
+
+        $this->factory->buildConfiguredHandler();
     }
 }
