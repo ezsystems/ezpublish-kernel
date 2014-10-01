1e75e616579a34881c4f7470a9072d44ba2428a6
Bertrand Dunogier <bd@ez.no>
2014-10-01 22:31:56 +0200

    IO: DFS api update + exception handling

--- eZ/Publish/Core/IO/IOMetadataHandler/LegacyDFSCluster.php
+++ eZ/Publish/Core/IO/IOMetadataHandler/LegacyDFSCluster.php
@@ -1,17 +1,19 @@
 <?php
 /**
- * File containing the DoctrineDBAL MetadataHandler class.
+ * File containing the DoctrineDBAL IOMetadataHandler class.
  *
  * @copyright Copyright (C) 2013 eZ Systems AS. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
  * @version //autogentag//
  */
-namespace eZ\Publish\Core\IO\Handler\DFS\MetadataHandler;
+namespace eZ\Publish\Core\IO\IOMetadataHandler;
 
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\DBALException;
 use eZ\Publish\Core\Base\Exceptions\NotFoundException;
+use eZ\Publish\Core\IO\Exception\BinaryFileNotFoundException;
 use eZ\Publish\Core\IO\Handler\DFS\MetadataHandler;
+use eZ\Publish\SPI\IO\BinaryFileCreateStruct;
 
 /**
  * @todo Describe
@@ -22,22 +24,25 @@ class LegacyDFSCluster implements MetadataHandler
     /** @var Connection */
     private $db;
 
-    public function __construct(Connection $connection)
+    public function __construct( Connection $connection )
     {
         $this->db = $connection;
     }
 
     /**
-     * Inserts a new reference to file $path
+     * Inserts a new reference to file $spiBinaryFileId
      *
-     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException If a file $path already exists
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException If a file $spiBinaryFileId already exists
      *
-     * @param string  $path
+     * @param string  $spiBinaryFileId
      * @param integer $mtime
+     *
+     * @throws DBALException Any unhandled DBAL exception
+     *
+     * @todo fix exception handling
      */
-    public function insert($path, $size)
+    public function create( BinaryFileCreateStruct $binaryFileCreateStruct )
     {
-        $nameTrunk = $path;
         try {
             /**
              * @todo what might go wrong here ? Can another process be trying to insert the same image ?
@@ -52,39 +57,13 @@ ON DUPLICATE KEY UPDATE
   mtime=VALUES(mtime), expired=VALUES(expired)
 SQL
             );
-            $stmt->bindValue('name', $path);
-            $stmt->bindValue('name_hash', md5($path));
-            $stmt->bindValue('name_trunk', $nameTrunk);
-            $stmt->bindValue('mtime', time());
-            $stmt->bindValue('size', $size);
-            $stmt->bindValue('scope', '');
-            $stmt->bindValue('datatype', '');
-            $stmt->execute();
-        } catch (DBALException $e) {
-            throw $e;
-        }
-
-        if ($stmt->rowCount() == 0) {
-            throw new \Exception("@TODO: unexpected rowCount " . $stmt->rowCount());
-        }
-    }
-
-    /**
-     * Deletes file $path
-     *
-     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException If $path is not found
-     *
-     * @param string $path
-     */
-    public function delete( $path )
-    {
-        try
-        {
-            /**
-             * @todo delete or expire ? legacy_mode option ?
-             */
-            $stmt = $this->db->prepare('DELETE FROM ezdfsfile WHERE name_hash LIKE :name_hash');
-            $stmt->bindValue('name_hash', md5($path));
+            $stmt->bindValue( 'name', $binaryFileCreateStruct->id );
+            $stmt->bindValue( 'name_hash', md5( $binaryFileCreateStruct->id ) );
+            $stmt->bindValue( 'name_trunk', $this->getNameTrunk( $binaryFileCreateStruct ) );
+            $stmt->bindValue( 'mtime', $binaryFileCreateStruct->mtime );
+            $stmt->bindValue( 'size', $binaryFileCreateStruct->size );
+            $stmt->bindValue( 'scope', '@todo' );
+            $stmt->bindValue( 'datatype', $binaryFileCreateStruct->mimeType );
             $stmt->execute();
         }
         catch ( DBALException $e )
@@ -92,76 +71,82 @@ SQL
             throw $e;
         }
 
-        if ($stmt->rowCount() != 1) {
-            throw new \Exception("@todo rows affected by delete() weren't exactly 1");
+        if ( $stmt->rowCount() == 0 )
+        {
+            throw new \Exception("@TODO: unexpected rowCount " . $stmt->rowCount());
         }
     }
 
     /**
-     * Loads and returns metadata for $path
-     * @param string $path
-     * @return array A hash with metadata for $path. Keys: mtime, size.
-     * @throws NotFoundException if no row is found for $path
+     * Deletes file $spiBinaryFileId
+     *
+     * @throws BinaryFileNotFoundException If $spiBinaryFileId is not found
+     *
+     * @param string $spiBinaryFileId
      */
-    public function loadMetadata($path)
+    public function delete( $spiBinaryFileId )
     {
-        try {
-            $stmt = $this->db->prepare('SELECT * FROM ezdfsfile WHERE name_hash LIKE ? AND expired != 1 AND mtime > 0');
-            $stmt->bindValue(1, md5($path));
-            $stmt->execute();
-        } catch (\Exception $e) {
-            throw new \Exception("@todo");
-        }
+        // @todo delete or expire ? legacy_mode option ?
+        $stmt = $this->db->prepare( 'DELETE FROM ezdfsfile WHERE name_hash LIKE :name_hash' );
+        $stmt->bindValue( 'name_hash', md5( $spiBinaryFileId ) );
+        $stmt->execute();
 
-        if ($stmt->rowCount() == 0) {
-            throw new NotFoundException('file', $path);
+        if ( $stmt->rowCount() != 1 )
+        {
+            throw new BinaryFileNotFoundException( $spiBinaryFileId );
         }
-
-        return $stmt->fetch(\PDO::FETCH_ASSOC);
     }
 
     /**
-     * Checks if a file $path exists
+     * Loads and returns metadata for $spiBinaryFileId
      *
-     * @param string $path
+     * @param string $spiBinaryFileId
      *
+     * @return array A hash with metadata for $spiBinaryFileId. Keys: mtime, size.
+     *
+     * @throws BinaryFileNotFoundException if no row is found for $spiBinaryFileId
+     * @throws DBALException Any unhandled DBAL exception
+     */
+    public function load( $spiBinaryFileId )
+    {
+        $stmt = $this->db->prepare( 'SELECT * FROM ezdfsfile WHERE name_hash LIKE ? AND expired != 1 AND mtime > 0' );
+        $stmt->bindValue( 1, md5( $spiBinaryFileId ) );
+        $stmt->execute();
+
+        if ( $stmt->rowCount() == 0 )
+        {
+            throw new BinaryFileNotFoundException( $spiBinaryFileId );
+        }
+
+        return $stmt->fetch( \PDO::FETCH_ASSOC );
+    }
+
+    /**
+     * Checks if a file $spiBinaryFileId exists
+     *
+     * @param string $spiBinaryFileId
+     *
+     * @throws NotFoundException
+     * @throws DBALException Any unhandled DBAL exception
      * @return bool
      */
-    public function exists( $path )
+    public function exists( $spiBinaryFileId )
     {
-        try {
-            $stmt = $this->db->prepare('SELECT name FROM ezdfsfile WHERE name_hash LIKE ? and mtime > 0 and expired != 1');
-            $stmt->bindValue(1, md5($path));
-            $stmt->execute();
-        } catch (\Exception $e) {
-            throw new NotFoundException('file', $path);
-        }
-        return ($stmt->rowCount() == 1);
+        $stmt = $this->db->prepare( 'SELECT name FROM ezdfsfile WHERE name_hash LIKE ? and mtime > 0 and expired != 1' );
+        $stmt->bindValue( 1, md5( $spiBinaryFileId ) );
+        $stmt->execute();
+
+        return ( $stmt->rowCount() == 1 );
     }
 
     /**
-     * Renames file $fromPath to $toPath
+     * @param BinaryFileCreateStruct $binaryFileCreateStruct
      *
-     * @param $fromPath
-     * @param $toPath
-     *
-     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException If $toPath already exists
-     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException If $fromPath does not exist
+     * @return mixed
      */
-    public function rename($fromPath, $toPath)
+    protected function getNameTrunk( BinaryFileCreateStruct $binaryFileCreateStruct )
     {
-        try {
-            $stmt = $this->db->prepare('UPDATE ezdfsfile SET name = ?, name_hash = ? WHERE name_hash LIKE ?');
-            $stmt->bindValue(1, $toPath);
-            $stmt->bindValue(2, md5($toPath));
-            $stmt->bindValue(3, $fromPath);
-            $stmt->execute();
-        } catch (\Exception $e) {
-            throw new \Exception("@todo");
-        }
-
-        if ($stmt->rowCount()!=1) {
-            throw new NotFoundException("@todo", $fromPath);
-        }
+        // @todo fixme
+        return $binaryFileCreateStruct->id;
     }
 }
