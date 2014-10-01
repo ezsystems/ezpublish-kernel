45bc1051dfdf73066f7a2ec1a4a64f9c41c40e5c
Bertrand Dunogier <bd@ez.no>
2014-10-01 22:29:53 +0200

    IO: Added exceptions

---
+++ eZ/Publish/Core/IO/Exception/BinarydataException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This file is part of the eZ Publish Legacy package
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributd with this source code.
+ */
+namespace eZ\Publish\Core\IO\Exception;
+
+class BinarydataException
+{
+    public function __construct( $internalId )
+    {
+
+    }
+}
