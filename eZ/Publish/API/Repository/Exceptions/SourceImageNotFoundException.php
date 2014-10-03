d7a0fac45b7344370184a0693f314776e62af42a
Carlos Revillo <crevillo@gmail.com>
2014-10-03 20:28:02 +0200

    Implemented EZP-23420: Log error if source image doesn't exist

---
+++ eZ/Publish/API/Repository/Exceptions/SourceImageNotFoundException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * File containing the eZ\Publish\API\Repository\Exceptions\SourceImageNotFoundException class.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\API\Repository\Exceptions;
+
+use RuntimeException;
+
+class SourceImageNotFoundException extends RuntimeException
+{
+}
