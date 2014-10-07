04259ea65a40d6c00593bf4b74e01b0e032d377a
Bertrand Dunogier <bd@ez.no>
2014-10-07 22:47:21 +0200

    IO: Added DFS metadata handler

--- eZ/Bundle/EzPublishIOBundle/DependencyInjection/Configuration.php
+++ eZ/Bundle/EzPublishIOBundle/DependencyInjection/Configuration.php
@@ -63,6 +63,14 @@ class Configuration extends SiteAccessConfiguration
                         ->end()
                     ->end()
                 ->end()
+                ->arrayNode( 'legacy_dfs_cluster' )
+                    ->canBeUnset()
+                    ->prototype( 'array' )
+                        ->children()
+                            ->scalarNode( 'connection' )->info( 'doctrine connection' )->example( 'default' )->end()
+                        ->end()
+                    ->end()
+                ->end()
             ->end();
 
         return $node;
