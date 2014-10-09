70974c3d5c96066bcf61eba41951e71a5d7fc28d
Bertrand Dunogier <bd@ez.no>
2014-10-09 19:33:21 +0200

    EZP-23437: DFS ConfigurationFactory

---
+++ eZ/Bundle/EzPublishIOBundle/DependencyInjection/ConfigurationFactory/MetadataHandler/LegacyDFSCluster.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This file is part of the eZ Publish Legacy package
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributd with this source code.
+ */
+namespace eZ\Bundle\EzPublishIOBundle\DependencyInjection\ConfigurationFactory\MetadataHandler;
+
+use eZ\Bundle\EzPublishIOBundle\DependencyInjection\ConfigurationFactory;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\DependencyInjection\Definition as ServiceDefinition;
+use Symfony\Component\DependencyInjection\Reference;
+
+class LegacyDFSCluster implements ConfigurationFactory
+{
+    public function getParentServiceId()
+    {
+        return 'ezpublish.core.io.metadata_handler.legacy_dfs_cluster';
+    }
+
+    public function configureHandler( ServiceDefinition $definition, array $config )
+    {
+        $definition->replaceArgument( 0, new Reference( $config['connection'] ) );
+    }
+
+    public function addConfiguration( ArrayNodeDefinition $node )
+    {
+        $node
+            ->info(
+                'A MySQL based handler, compatible with the legacy DFS one, that stores metadata in the ezdfsfile table'
+            )
+            ->children()
+                ->scalarNode( 'connection' )
+                    ->info( 'Doctrine connection service' )
+                    ->example( 'doctrine.dbal.cluster_connection' )
+                ->end()
+            ->end();
+    }
+}
---
+++ eZ/Bundle/EzPublishIOBundle/Tests/DependencyInjection/ConfigurationFactory/MetadataHandler/LegacyDFSClusterTest.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * This file is part of the eZ Publish Kernel package
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributd with this source code.
+ */
+namespace eZ\Bundle\EzPublishIOBundle\Tests\DependencyInjection\ConfigurationFactory\MetadataHandler;
+
+use eZ\Bundle\EzPublishIOBundle\DependencyInjection\ConfigurationFactory\MetadataHandler\LegacyDFSCluster;
+use eZ\Bundle\EzPublishIOBundle\Tests\DependencyInjection\ConfigurationFactoryTest;
+use Symfony\Component\DependencyInjection\Definition;
+
+class LegacyDFSClusterTest extends ConfigurationFactoryTest
+{
+    /**
+     * Returns an instance of the tested factory
+     * @return \eZ\Bundle\EzPublishIOBundle\DependencyInjection\ConfigurationFactory
+     */
+    public function provideTestedFactory()
+    {
+        return new LegacyDFSCluster();
+    }
+
+    public function provideExpectedParentServiceId()
+    {
+        return 'ezpublish.core.io.metadata_handler.legacy_dfs_cluster';
+    }
+
+    public function provideParentServiceDefinition()
+    {
+        return new Definition( null, array( null ) );
+    }
+
+    public function provideHandlerConfiguration()
+    {
+        return array( 'connection' => 'doctrine.dbal.test_connection' );
+    }
+
+    /**
+     * Lets you test the handler definition after it was configured.
+     *
+     * Use the assertContainer* methods from matthiasnoback/SymfonyDependencyInjectionTest.
+     *
+     * @param string $handlerServiceId id of the service that was registered by the compiler pass
+     */
+    public function validateConfiguredHandler( $handlerServiceId )
+    {
+        self::assertContainerBuilderHasServiceDefinitionWithArgument(
+            $handlerServiceId,
+            0,
+            'doctrine.dbal.test_connection'
+        );
+    }
+}
