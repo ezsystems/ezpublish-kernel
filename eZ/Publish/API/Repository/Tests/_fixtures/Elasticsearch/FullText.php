d4d2c154b714d10f993575e70f6b42850e02ff66
Petar Španja <petar.spanja@gmail.com>
2014-10-23 20:27:56 +0200

    EZP-23396: added FullText test fixtures

---
+++ eZ/Publish/API/Repository/Tests/_fixtures/Elasticsearch/FullText.php
@@ -0,0 +1,38 @@
+<?php
+
+return eZ\Publish\API\Repository\Values\Content\Search\SearchResult::__set_state(array(
+   'facets' => 
+  array (
+  ),
+   'searchHits' => 
+  array (
+    0 => 
+    eZ\Publish\API\Repository\Values\Content\Search\SearchHit::__set_state(array(
+       'valueObject' => 
+      array (
+        'id' => 54,
+        'title' => 'eZ Publish Demo Design (without demo content)',
+      ),
+       'score' => 0.49063533999999998,
+       'index' => NULL,
+       'highlight' => NULL,
+    )),
+    1 => 
+    eZ\Publish\API\Repository\Values\Content\Search\SearchHit::__set_state(array(
+       'valueObject' => 
+      array (
+        'id' => 58,
+        'title' => 'Contact Us',
+      ),
+       'score' => 1.0769257999999999,
+       'index' => NULL,
+       'highlight' => NULL,
+    )),
+  ),
+   'spellSuggestion' => NULL,
+   'time' => 1,
+   'timedOut' => NULL,
+   'maxScore' => 1.0769257999999999,
+   'totalCount' => 2,
+));
+
