4513c18832cec7a1e9ac2cdc8e0a5da9a90fd7dc
André R <ar@ez.no>
2014-10-23 16:57:13 +0200

    typo

--- eZ/Bundle/EzPublishLegacyBundle/Command/LegacyWrapperInstallCommand.php
+++ eZ/Bundle/EzPublishLegacyBundle/Command/LegacyWrapperInstallCommand.php
@@ -112,7 +112,7 @@ EOT
  *
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
- *\/
+ */
 
  \$legacyRoot = {$rootDirCode};
  chdir( \$legacyRoot );
