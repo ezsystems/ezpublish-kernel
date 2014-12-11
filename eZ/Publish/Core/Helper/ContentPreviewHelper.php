d64fdf5279384642f744595f44f4a54e6dd744da
Bertrand Dunogier <bd@ez.no>
2014-12-11 12:30:48 +0100

    EZP-23784: Refactored preview location handling
    Moved location fetching out of ContentPreviewHelper to a new Helper\PreviewLocationProvider.
    It uses both the Location Persistence handler as well as the Location service to return
    a Location. The fake Location created for location less objects takes depth and parent location id
    from the actual node assignment.

--- eZ/Publish/Core/Helper/ContentPreviewHelper.php
+++ eZ/Publish/Core/Helper/ContentPreviewHelper.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * File containing the ContentPreviewHelper class.
+ * This file is part of the eZ Publish Kernel package.
  *
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
@@ -9,28 +9,14 @@
 
 namespace eZ\Publish\Core\Helper;
 
-use eZ\Publish\API\Repository\ContentService;
-use eZ\Publish\API\Repository\LocationService;
-use eZ\Publish\Core\MVC\ConfigResolverInterface;
 use eZ\Publish\Core\MVC\Symfony\Event\ScopeChangeEvent;
 use eZ\Publish\Core\MVC\Symfony\MVCEvents;
 use eZ\Publish\Core\MVC\Symfony\SiteAccess\SiteAccessAware;
 use eZ\Publish\Core\MVC\Symfony\SiteAccess;
-use eZ\Publish\Core\Repository\Values\Content\Location;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class ContentPreviewHelper implements SiteAccessAware
 {
-    /**
-     * @var \eZ\Publish\API\Repository\ContentService
-     */
-    protected $contentService;
-
-    /**
-     * @var \eZ\Publish\API\Repository\LocationService
-     */
-    protected $locationService;
-
     /**
      * @var \Symfony\Component\EventDispatcher\EventDispatcherInterface
      */
@@ -41,23 +27,9 @@ class ContentPreviewHelper implements SiteAccessAware
      */
     protected $originalSiteAccess;
 
-    /**
-     * @var \eZ\Publish\Core\MVC\ConfigResolverInterface
-     */
-    private $configResolver;
-
-    public function __construct(
-        ContentService $contentService,
-        LocationService $locationService,
-        EventDispatcherInterface $eventDispatcher,
-        ConfigResolverInterface $configResolver
-    )
+    public function __construct( EventDispatcherInterface $eventDispatcher )
     {
-        $this->contentService = $contentService;
-        $this->locationService = $locationService;
-
         $this->eventDispatcher = $eventDispatcher;
-        $this->configResolver = $configResolver;
     }
 
     public function setSiteAccess( SiteAccess $siteAccess = null )
@@ -101,41 +73,5 @@ class ContentPreviewHelper implements SiteAccessAware
         $this->eventDispatcher->dispatch( MVCEvents::CONFIG_SCOPE_RESTORE, $event );
 
         return $event->getSiteAccess();
-    }
-
-    /**
-     * Returns a valid Location object for $contentId.
-     * Will either load mainLocationId (if available) or build a virtual Location object.
-     *
-     * @param mixed $contentId
-     *
-     * @return \eZ\Publish\API\Repository\Values\Content\Location|null Null when content does not have location
-     */
-    public function getPreviewLocation( $contentId )
-    {
-        // contentInfo must be reloaded as content is not published yet (e.g. no mainLocationId)
-        $contentInfo = $this->contentService->loadContentInfo( $contentId );
-        // mainLocationId already exists, content has been published at least once.
-        if ( $contentInfo->mainLocationId )
-        {
-            $location = $this->locationService->loadLocation( $contentInfo->mainLocationId );
-        }
-        // New Content, never published, create a virtual location object.
-        else
-        {
-            // @todo In future releases this will be a full draft location when this feature
-            // is implemented. Or it might return null when content does not have location,
-            // but for now we can't detect that so we return a virtual draft location
-            $location = new Location(
-                array(
-                    // Faking using root locationId
-                    'id' => $this->configResolver->getParameter( 'content.tree_root.location_id' ),
-                    'contentInfo' => $contentInfo,
-                    'status' => Location::STATUS_DRAFT
-                )
-            );
-        }
-
-        return $location;
     }
 }
---
+++ eZ/Publish/Core/Helper/Tests/PreviewLocationProviderTest.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * This file is part of the eZ Publish Kernel package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\Core\Helper\Tests;
+
+use eZ\Publish\API\Repository\LocationService;
+use eZ\Publish\Core\Helper\ContentPreviewHelper;
+use eZ\Publish\Core\Helper\PreviewLocationProvider;
+use eZ\Publish\Core\MVC\Symfony\Event\ScopeChangeEvent;
+use eZ\Publish\Core\MVC\Symfony\MVCEvents;
+use eZ\Publish\Core\MVC\Symfony\SiteAccess;
+use eZ\Publish\Core\Repository\Values\Content\Location;
+use PHPUnit_Framework_TestCase;
+
+class PreviewLocationProviderTest extends PHPUnit_Framework_TestCase
+{
+    /** @var \eZ\Publish\API\Repository\LocationService|\PHPUnit_Framework_MockObject_MockObject */
+    private $contentService;
+
+    /** @var \eZ\Publish\API\Repository\LocationService|\PHPUnit_Framework_MockObject_MockObject */
+    private $locationService;
+
+    /** @var \eZ\Publish\SPI\Persistence\Content\Location\Handler|\PHPUnit_Framework_MockObject_MockObject */
+    private $locationHandler;
+
+    /** @var \eZ\Publish\Core\Helper\PreviewLocationProvider */
+    private $provider;
+
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->contentService = $this->getMock( 'eZ\Publish\API\Repository\ContentService' );
+        $this->locationService = $this->getMock( 'eZ\Publish\API\Repository\LocationService' );
+
+        $this->locationHandler = $this->getMock( 'eZ\Publish\SPI\Persistence\Content\Location\Handler' );
+
+        $persistenceHandler = $this->getMock( 'eZ\Publish\SPI\Persistence\Handler' );
+        $persistenceHandler->expects( $this->any() )->method( 'locationHandler' )->will( $this->returnValue( $this->locationHandler ) );
+
+        $this->provider = new PreviewLocationProvider( $this->locationService, $this->contentService, $persistenceHandler );
+    }
+
+    public function testGetPreviewLocationDraft()
+    {
+        $contentId = 123;
+        $parentLocationId = 456;
+
+        $contentInfo = $this
+            ->getMockBuilder( 'eZ\Publish\API\Repository\Values\Content\ContentInfo' )
+            ->setConstructorArgs( array( array( 'id' => $contentId ) ) )
+            ->getMockForAbstractClass();
+
+        $this->contentService
+            ->expects( $this->once() )
+            ->method( 'loadContentInfo' )
+            ->with( $contentId )
+            ->will( $this->returnValue( $contentInfo ) );
+
+        $this->locationService
+            ->expects( $this->never() )
+            ->method( 'loadLocation' );
+
+        $this->locationHandler
+            ->expects( $this->once() )
+            ->method( 'loadParentLocationsForDraftContent' )
+            ->with( $contentId )
+            ->will( $this->returnValue( array( new Location( array( 'id' => $parentLocationId ) ) ) ) );
+
+        $location = $this->provider->loadMainLocation( $contentId );
+        $this->assertInstanceOf( 'eZ\Publish\API\Repository\Values\Content\Location', $location );
+        $this->assertSame( $contentInfo, $location->contentInfo );
+        $this->assertNull( $location->id );
+        $this->assertEquals( $parentLocationId, $location->parentLocationId );
+    }
+
+    public function testGetPreviewLocation()
+    {
+        $contentId = 123;
+        $locationId = 456;
+        $contentInfo = $this
+            ->getMockBuilder( 'eZ\Publish\API\Repository\Values\Content\ContentInfo' )
+            ->setConstructorArgs( array( array( 'id' => $contentId, 'mainLocationId' => $locationId ) ) )
+            ->getMockForAbstractClass();
+        $location = $this
+            ->getMockBuilder( 'eZ\Publish\Core\Repository\Values\Content\Location' )
+            ->setConstructorArgs( array( array( 'id' => $locationId, 'contentInfo' => $contentInfo ) ) )
+            ->getMockForAbstractClass();
+        $this->contentService
+            ->expects( $this->once() )
+            ->method( 'loadContentInfo' )
+            ->with( $contentId )
+            ->will( $this->returnValue( $contentInfo ) );
+        $this->locationService
+            ->expects( $this->once() )
+            ->method( 'loadLocation' )
+            ->with( $locationId )
+            ->will( $this->returnValue( $location ) );
+        $this->locationHandler->expects( $this->never() )->method( 'loadParentLocationsForDraftContent' );
+
+        $returnedLocation = $this->provider->loadMainLocation( $contentId );
+        $this->assertSame( $location, $returnedLocation );
+        $this->assertSame( $contentInfo, $returnedLocation->contentInfo );
+    }
+}
