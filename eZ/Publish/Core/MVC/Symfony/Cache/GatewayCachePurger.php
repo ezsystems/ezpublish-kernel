a9af686966624cec9fbb28cf94b336c17e845198
Jérôme Vieilledent <lolautruche@gmail.com>
2015-01-16 17:23:20 +0100

    EZP-23897: Implement mechanism for smart HTTP cache clearing
    > https://jira.ez.no/browse/EZP-23897
    
    New event driven base mechanism for *smart cache clearing*.

--- eZ/Publish/Core/MVC/Symfony/Cache/GatewayCachePurger.php
+++ eZ/Publish/Core/MVC/Symfony/Cache/GatewayCachePurger.php
@@ -10,7 +10,7 @@
 namespace eZ\Publish\Core\MVC\Symfony\Cache;
 
 /**
- * Interface for gateway cache purgers.
+ * Interface for gateway cache purgers, i.e. Http cache purgers.
  */
 interface GatewayCachePurger
 {
@@ -18,11 +18,21 @@ interface GatewayCachePurger
      * Triggers the cache purge of given $cacheElements.
      * It's up to the implementor to decide whether to purge $cacheElements right away or to delegate to a separate process.
      *
+     * @deprecated as of 6.0. Will be removed in 6.1. Use purgeForContent() instead.
+     *
      * @param mixed $cacheElements
      *
      * @return mixed
      */
     public function purge( $cacheElements );
+
+    /**
+     * Triggers cache purge for given content.
+     * If given content has several locations, cache will be purged for all of them.
+     *
+     * @param mixed $contentId Content ID.
+     */
+    public function purgeForContent( $contentId );
 
     /**
      * Triggers the cache purge for all content in cache.
--- eZ/Publish/Core/MVC/Symfony/Cache/Http/InstantCachePurger.php
+++ eZ/Publish/Core/MVC/Symfony/Cache/Http/InstantCachePurger.php
@@ -9,23 +9,43 @@
 
 namespace eZ\Publish\Core\MVC\Symfony\Cache\Http;
 
+use eZ\Publish\API\Repository\ContentService;
 use eZ\Publish\Core\MVC\Symfony\Cache\GatewayCachePurger;
 use eZ\Publish\Core\MVC\Symfony\Cache\PurgeClientInterface;
+use eZ\Publish\Core\MVC\Symfony\Event\ContentCacheClearEvent;
+use eZ\Publish\Core\MVC\Symfony\MVCEvents;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class InstantCachePurger implements GatewayCachePurger
 {
     /**
-     * @var \eZ\Publish\Core\MVC\Symfony\Cache\PurgeClientInterface
+     * @var PurgeClientInterface
      */
-    private $purgeClient;
+    protected $purgeClient;
 
-    public function __construct( PurgeClientInterface $purgeClient )
+    /**
+     * @var ContentService
+     */
+    private $contentService;
+
+    /**
+     * @var EventDispatcherInterface
+     */
+    private $eventDispatcher;
+
+    public function __construct(
+        PurgeClientInterface $purgeClient,
+        ContentService $contentService,
+        EventDispatcherInterface $eventDispatcher
+    )
     {
         $this->purgeClient = $purgeClient;
+        $this->contentService = $contentService;
+        $this->eventDispatcher = $eventDispatcher;
     }
 
     /**
-     * Instantly triggers the cache purge of given $cacheElements.
+     * @deprecated as of 6.0. Will be removed in 6.1. Use purgeForContent() instead.
      *
      * @param mixed $cacheElements
      *
@@ -33,13 +53,27 @@ class InstantCachePurger implements GatewayCachePurger
      */
     public function purge( $cacheElements )
     {
-        $this->purgeClient->purge( $cacheElements );
-
+        $this->purgeClient->purge( (array)$cacheElements );
         return $cacheElements;
     }
 
     public function purgeAll()
     {
         $this->purgeClient->purgeAll();
+    }
+
+    public function purgeForContent( $contentId )
+    {
+        $contentInfo = $this->contentService->loadContentInfo( $contentId );
+        $event = new ContentCacheClearEvent( $contentInfo );
+        $this->eventDispatcher->dispatch( MVCEvents::CACHE_CLEAR_CONTENT, $event );
+
+        $locationIds = [];
+        foreach ( $event->getLocationsToClear() as $location )
+        {
+            $locationIds[] = $location->id;
+        }
+
+        $this->purgeClient->purge( array_unique( $locationIds ) );
     }
 }
