6c3274793d60b47d2a9a21436f43c99194fbc9d0
Jérôme Vieilledent <lolautruche@gmail.com>
2015-01-19 18:09:23 +0100

    EZP-23897: Added tests

--- eZ/Publish/Core/MVC/Symfony/Cache/Tests/Http/InstantCachePurgerTest.php
+++ eZ/Publish/Core/MVC/Symfony/Cache/Tests/Http/InstantCachePurgerTest.php
@@ -9,8 +9,13 @@
 
 namespace eZ\Publish\Core\MVC\Symfony\Cache\Tests\Http;
 
+use eZ\Publish\API\Repository\Values\Content\ContentInfo;
 use eZ\Publish\Core\MVC\Symfony\Cache\Http\InstantCachePurger;
+use eZ\Publish\Core\MVC\Symfony\Event\ContentCacheClearEvent;
+use eZ\Publish\Core\MVC\Symfony\MVCEvents;
+use eZ\Publish\Core\Repository\Values\Content\Location;
 use PHPUnit_Framework_TestCase;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
 class InstantCachePurgerTest extends PHPUnit_Framework_TestCase
 {
@@ -58,5 +63,44 @@ class InstantCachePurgerTest extends PHPUnit_Framework_TestCase
 
         $purger = new InstantCachePurger( $this->purgeClient, $this->contentService, $this->eventDispatcher );
         $purger->purgeAll();
+    }
+
+    public function testPurgeForContent()
+    {
+        $contentId = 123;
+        $contentInfo = new ContentInfo( ['id' => $contentId] );
+        // Assume listeners have added locations.
+        // Adding duplicates on purpose.
+        $locationIds = [123, 456, 789, 234, 567];
+
+        $this->contentService
+            ->expects( $this->once() )
+            ->method( 'loadContentInfo' )
+            ->with( $contentId )
+            ->will( $this->returnValue( $contentInfo ) );
+
+        $eventDispatcher = new EventDispatcher();
+        $eventDispatcher->addListener(
+            MVCEvents::CACHE_CLEAR_CONTENT,
+            function ( ContentCacheClearEvent $event ) use ( $locationIds )
+            {
+                foreach ( $locationIds as $id )
+                {
+                    $event->addLocationToClear( new Location( ['id' => $id] ) );
+                }
+
+                // Adding a few duplicates on purpose.
+                $event->addLocationToClear( new Location( ['id' => 123] ) );
+                $event->addLocationToClear( new Location( ['id' => 567] ) );
+            }
+        );
+
+        $this->purgeClient
+            ->expects( $this->once() )
+            ->method( 'purge' )
+            ->with( $locationIds );
+
+        $purger = new InstantCachePurger( $this->purgeClient, $this->contentService, $eventDispatcher );
+        $purger->purgeForContent( $contentId );
     }
 }
