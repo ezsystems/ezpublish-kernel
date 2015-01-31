14282a25c81b1cf2bba96be31475baa3d04edff1
Petar Španja <petar.spanja@gmail.com>
2015-01-31 08:14:16 +0100

    EZP-23694: refactor FieldMap for extraction and custom/multiple field support

--- eZ/Publish/Core/Persistence/Solr/Content/Search/FieldMap.php
+++ eZ/Publish/Core/Persistence/Solr/Content/Search/FieldMap.php
@@ -11,6 +11,9 @@ namespace eZ\Publish\Core\Persistence\Solr\Content\Search;
 
 use eZ\Publish\SPI\Persistence\Content\Type\Handler as ContentTypeHandler;
 use eZ\Publish\API\Repository\Values\Content\Query\CustomFieldInterface;
+use eZ\Publish\API\Repository\Values\Content\Query\SortClause;
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
+use RuntimeException;
 
 /**
  * Provides field mapping information
@@ -34,7 +37,7 @@ class FieldMap
     /**
      * Field name generator
      *
-     * @var \eZ\Publish\Core\Persistence\Solr\Content\Search\FieldNameGenerator
+     * @var \eZ\Publish\Core\Persistence\Elasticsearch\Content\Search\FieldNameGenerator
      */
     protected $nameGenerator;
 
@@ -52,11 +55,15 @@ class FieldMap
      * @param \eZ\Publish\SPI\Persistence\Content\Type\Handler $contentTypeHandler
      * @param \eZ\Publish\Core\Persistence\Solr\Content\Search\FieldNameGenerator $nameGenerator
      */
-    public function __construct( FieldRegistry $fieldRegistry, ContentTypeHandler $contentTypeHandler, FieldNameGenerator $nameGenerator )
+    public function __construct(
+        FieldRegistry $fieldRegistry,
+        ContentTypeHandler $contentTypeHandler,
+        FieldNameGenerator $nameGenerator
+    )
     {
-        $this->fieldRegistry      = $fieldRegistry;
+        $this->fieldRegistry = $fieldRegistry;
         $this->contentTypeHandler = $contentTypeHandler;
-        $this->nameGenerator      = $nameGenerator;
+        $this->nameGenerator = $nameGenerator;
     }
 
     /**
@@ -66,19 +73,17 @@ class FieldMap
      *
      * <code>
      *  array(
-     *      "field-identifier" => array(
-     *          "solr_field_name",
+     *      "content-type-identifier" => array(
+     *          "field-definition-identifier" => "field-type-identifier",
      *          …
      *      ),
      *      …
      *  )
      * </code>
      *
-     * @param \eZ\Publish\API\Repository\Values\Content\Query\CustomFieldInterface $criterion
-     *
      * @return array
      */
-    public function getFieldTypes( CustomFieldInterface $criterion )
+    protected function getFieldMap()
     {
         // @TODO: temp fixed by disabling caching, see https://jira.ez.no/browse/EZP-22834
         $this->fieldTypes = array();
@@ -94,25 +99,175 @@ class FieldMap
                         continue;
                     }
 
-                    if ( $customField = $criterion->getCustomField( $contentType->identifier, $fieldDefinition->identifier ) )
-                    {
-                        $this->fieldTypes[$fieldDefinition->identifier]["custom"][] = $customField;
-                        continue;
-                    }
-
-                    $fieldType = $this->fieldRegistry->getType( $fieldDefinition->fieldType );
-                    foreach ( $fieldType->getIndexDefinition() as $name => $type )
-                    {
-                        $this->fieldTypes[$fieldDefinition->identifier][$type->type][] =
-                            $this->nameGenerator->getTypedName(
-                                $this->nameGenerator->getName( $name, $fieldDefinition->identifier, $contentType->identifier ),
-                                $type
-                            );
-                    }
+                    $this->fieldTypes[$contentType->identifier][$fieldDefinition->identifier] =
+                        $fieldDefinition->fieldType;
                 }
             }
         }
 
         return $this->fieldTypes;
+    }
+
+    /**
+     * For the given parameters returns a set of index storage field names to search on.
+     *
+     * The method will check for custom fields if given $criterion implements
+     * CustomFieldInterface. With optional parameters $fieldTypeIdentifier and
+     * $name specific field type and field from its Indexable implementation
+     * can be targeted.
+     *
+     * @see \eZ\Publish\API\Repository\Values\Content\Query\CustomFieldInterface
+     * @see \eZ\Publish\SPI\FieldType\Indexable
+     *
+     * @param \eZ\Publish\API\Repository\Values\Content\Query\Criterion $criterion
+     * @param string $fieldDefinitionIdentifier
+     * @param string $fieldTypeIdentifier
+     * @param string $name
+     *
+     * @return array
+     */
+    public function getFieldNames(
+        Criterion $criterion,
+        $fieldDefinitionIdentifier,
+        $fieldTypeIdentifier = null,
+        $name = null
+    )
+    {
+        $fieldMap = $this->getFieldMap();
+        $fieldNames = array();
+
+        foreach ( $fieldMap as $contentTypeIdentifier => $fieldIdentifierMap )
+        {
+            // First check if field exists in the current ContentType, there is nothing to do if it doesn't
+            if ( !isset( $fieldIdentifierMap[$fieldDefinitionIdentifier] ) )
+            {
+                continue;
+            }
+
+            // If $fieldTypeIdentifier is given it must match current field definition
+            if (
+                $fieldTypeIdentifier !== null &&
+                $fieldTypeIdentifier !== $fieldIdentifierMap[$fieldDefinitionIdentifier]
+            )
+            {
+                continue;
+            }
+
+            $fieldNames[] = $this->getIndexFieldName(
+                $criterion,
+                $contentTypeIdentifier,
+                $fieldDefinitionIdentifier,
+                $fieldIdentifierMap[$fieldDefinitionIdentifier],
+                $name
+            );
+        }
+
+        return $fieldNames;
+    }
+
+    /**
+     * For the given parameters returns index storage field name to sort on or
+     * null if the field could not be found.
+     *
+     * The method will check for custom fields if given $sortClause implements
+     * CustomFieldInterface. With optional parameter $name specific field from
+     * field type's Indexable implementation can be targeted.
+     *
+     * Will return null if no sortable field is found.
+     *
+     * @see \eZ\Publish\API\Repository\Values\Content\Query\CustomFieldInterface
+     * @see \eZ\Publish\SPI\FieldType\Indexable
+     *
+     * @param \eZ\Publish\API\Repository\Values\Content\Query\SortClause $sortClause
+     * @param string $contentTypeIdentifier
+     * @param string $fieldDefinitionIdentifier
+     * @param string $name
+     *
+     * @return null|string
+     */
+    public function getSortFieldName(
+        SortClause $sortClause,
+        $contentTypeIdentifier,
+        $fieldDefinitionIdentifier,
+        $name = null
+    )
+    {
+        $fieldMap = $this->getFieldMap();
+
+        // First check if field exists in type, there is nothing to do if it doesn't
+        if ( !isset( $fieldMap[$contentTypeIdentifier][$fieldDefinitionIdentifier] ) )
+        {
+            return null;
+        }
+
+        return $this->getIndexFieldName(
+            $sortClause,
+            $contentTypeIdentifier,
+            $fieldDefinitionIdentifier,
+            $fieldMap[$contentTypeIdentifier][$fieldDefinitionIdentifier],
+            $name
+        );
+    }
+
+    /**
+     * Returns index field name for the given parameters.
+     *
+     * @param object $criterionOrSortClause
+     * @param string $contentTypeIdentifier
+     * @param string $fieldDefinitionIdentifier
+     * @param string $fieldTypeIdentifier
+     * @param string $name
+     *
+     * @return mixed|string
+     */
+    public function getIndexFieldName(
+        $criterionOrSortClause,
+        $contentTypeIdentifier,
+        $fieldDefinitionIdentifier,
+        $fieldTypeIdentifier,
+        $name
+    )
+    {
+        // If criterion or sort clause implements CustomFieldInterface and custom field is set for
+        // ContentType/FieldDefinition, return it
+        if (
+            $criterionOrSortClause instanceof CustomFieldInterface &&
+            $customFieldName = $criterionOrSortClause->getCustomField(
+                $contentTypeIdentifier,
+                $fieldDefinitionIdentifier
+            )
+        )
+        {
+            return $customFieldName;
+        }
+
+        // Else, generate field name from field type's index definition
+
+        $indexFieldType = $this->fieldRegistry->getType( $fieldTypeIdentifier );
+
+        // If $name is not given use default search field name
+        if ( $name === null )
+        {
+            $name = $indexFieldType->getDefaultField();
+        }
+
+        $indexDefinition = $indexFieldType->getIndexDefinition();
+
+        // Should only happen by mistake, so let's throw if it does
+        if ( !isset( $indexDefinition[$name] ) )
+        {
+            throw new RuntimeException(
+                "Could not find '{$name}' field in '{$fieldTypeIdentifier}' field type's index definition"
+            );
+        }
+
+        return $this->nameGenerator->getTypedName(
+            $this->nameGenerator->getName(
+                $name,
+                $fieldDefinitionIdentifier,
+                $contentTypeIdentifier
+            ),
+            $indexDefinition[$name]
+        );
     }
 }
