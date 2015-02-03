bcc42a8b8551a848c40e82da7d626df44ec99861
Petar Španja <petar.spanja@gmail.com>
2015-02-03 09:23:57 +0100

    EZP-23975: adapt: Elasticsearch search/storage

--- eZ/Publish/Core/Persistence/Elasticsearch/Content/Search/Mapper.php
+++ eZ/Publish/Core/Persistence/Elasticsearch/Content/Search/Mapper.php
@@ -15,7 +15,7 @@ use eZ\Publish\SPI\Persistence\Content\Location;
 use eZ\Publish\SPI\Persistence\Content\Section;
 use eZ\Publish\SPI\Search\FieldType;
 use eZ\Publish\SPI\Persistence\Content\Type;
-use eZ\Publish\Core\Persistence\Solr\Content\Search\FieldRegistry;
+use eZ\Publish\Core\Search\Solr\Content\FieldRegistry;
 use eZ\Publish\SPI\Persistence\Content\Handler as ContentHandler;
 use eZ\Publish\SPI\Persistence\Content\Location\Handler as LocationHandler;
 use eZ\Publish\SPI\Persistence\Content\Type\Handler as ContentTypeHandler;
@@ -78,7 +78,7 @@ class Mapper
     protected $fieldRegistry;
 
     /**
-     * @param \eZ\Publish\Core\Persistence\Solr\Content\Search\FieldRegistry $fieldRegistry
+     * @param \eZ\Publish\Core\Search\Solr\Content\FieldRegistry $fieldRegistry
      * @param \eZ\Publish\Core\Persistence\Elasticsearch\Content\Search\FieldNameGenerator $fieldNameGenerator
      * @param \eZ\Publish\SPI\Persistence\Content\Handler $contentHandler
      * @param \eZ\Publish\SPI\Persistence\Content\Location\Handler $locationHandler
