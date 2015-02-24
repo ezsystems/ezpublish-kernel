3a519bbe5b61b494dd0d565d138005a0bb1bb96b
André R <ar@ez.no>
2015-02-24 23:47:36 +0100

    [Travis] Change to use script for behat testing using correct branch on conposer install

--- bin/.travis/prepare_behat.sh
+++ bin/.travis/prepare_behat.sh
@@ -2,25 +2,15 @@
 
 # File for setting up system for behat testing, just like done in DemoBundle's .travis.yml
 
+# Change local git repo to be a full one as we will reuse it for composer install below
+git fetch --unshallow && git checkout -b tmp_travis_branch
 export BRANCH_BUILD_DIR=$TRAVIS_BUILD_DIR
 export TRAVIS_BUILD_DIR="$HOME/build/ezpublish-community"
 cd "$HOME/build"
 
-# Change the branch and/or remote to use a different ezpublish branch/distro
+# Checkout meta repo, change the branch and/or remote to use a different ezpublish branch/distro
 git clone --depth 1 --single-branch --branch master https://github.com/ezsystems/ezpublish-community.git
 cd ezpublish-community
 
-# Use this if you depend on another branch for a dependency (only works for the ezsystems remote)
-# (note that packagist may take time to update the references, leading to errors. Just retrigger the build)
-#
-# Example:
-# composer require --no-update ezsystems/DemoBundle:dev-MyCustomBranch
-
-# Prepare system (Apache, Mysql, Sahi/Selenium, eZ Publish)
-./bin/.travis/prepare_system.sh
-./bin/.travis/prepare_testsystem.sh
-./bin/.travis/prepare_ezpublish.sh
-
-# Replace kernel with the one from pull-request/current checkout
-rm -rf vendor/ezsystems/ezpublish-kernel
-mv "$BRANCH_BUILD_DIR" vendor/ezsystems/ezpublish-kernel
+# Install everything needed for behat testing, using our local branch of this repo
+./bin/.travis/setup_from_external_repo.sh $BRANCH_BUILD_DIR "ezsystems/ezpublish-kernel:dev-tmp_travis_branch"
