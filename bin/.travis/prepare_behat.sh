c9d6562d2ba6482208796561f158844d424d0819
André R <ar@ez.no>
2015-04-10 16:15:56 +0200

    Reflect eZ Platform for behat tests and Readme
    Further changes to the readme is aimed at being done in followup PR's.
    For instance missing things is moving the description of Legacy Stack
    to Legacy Bridge Readme, and API and SPI parts as expanded readmes in their
    respective directories. In addtion spec future directory structure and
    components split is missing.

--- bin/.travis/prepare_behat.sh
+++ bin/.travis/prepare_behat.sh
@@ -5,12 +5,12 @@
 # Change local git repo to be a full one as we will reuse it for composer install below
 git fetch --unshallow && git checkout -b tmp_travis_branch
 export BRANCH_BUILD_DIR=$TRAVIS_BUILD_DIR
-export TRAVIS_BUILD_DIR="$HOME/build/ezpublish-community"
+export TRAVIS_BUILD_DIR="$HOME/build/ezplatform"
 cd "$HOME/build"
 
 # Checkout meta repo, change the branch and/or remote to use a different ezpublish branch/distro
-git clone --depth 1 --single-branch --branch master https://github.com/ezsystems/ezpublish-community.git
-cd ezpublish-community
+git clone --depth 1 --single-branch --branch master https://github.com/ezsystems/ezplatform.git
+cd ezplatform
 
 # Install everything needed for behat testing, using our local branch of this repo
 ./bin/.travis/setup_from_external_repo.sh $BRANCH_BUILD_DIR "ezsystems/ezpublish-kernel:dev-tmp_travis_branch"
