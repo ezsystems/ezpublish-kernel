e03969865c9a9fe3cc0bf1086bbaca510f549c57
Petar Španja <petar.spanja@gmail.com>
2015-05-15 16:32:32 +0200

    EZP-24365: added missing test fixutres

---
+++ eZ/Publish/API/Repository/Tests/_fixtures/Solr/SortFieldMultipleTypesSlice.php
@@ -0,0 +1,71 @@
+<?php
+
+return eZ\Publish\API\Repository\Values\Content\Search\SearchResult::__set_state(array(
+   'facets' => 
+  array (
+  ),
+   'searchHits' => 
+  array (
+    0 => 
+    eZ\Publish\API\Repository\Values\Content\Search\SearchHit::__set_state(array(
+       'valueObject' => 
+      array (
+        'id' => 41,
+        'title' => 'Media',
+      ),
+       'score' => 1,
+       'index' => NULL,
+       'highlight' => NULL,
+    )),
+    1 => 
+    eZ\Publish\API\Repository\Values\Content\Search\SearchHit::__set_state(array(
+       'valueObject' => 
+      array (
+        'id' => 51,
+        'title' => 'Multimedia',
+      ),
+       'score' => 1,
+       'index' => NULL,
+       'highlight' => NULL,
+    )),
+    2 => 
+    eZ\Publish\API\Repository\Values\Content\Search\SearchHit::__set_state(array(
+       'valueObject' => 
+      array (
+        'id' => 45,
+        'title' => 'Setup',
+      ),
+       'score' => 1,
+       'index' => NULL,
+       'highlight' => NULL,
+    )),
+    3 => 
+    eZ\Publish\API\Repository\Values\Content\Search\SearchHit::__set_state(array(
+       'valueObject' => 
+      array (
+        'id' => 11,
+        'title' => 'Members',
+      ),
+       'score' => 1,
+       'index' => NULL,
+       'highlight' => NULL,
+    )),
+    4 => 
+    eZ\Publish\API\Repository\Values\Content\Search\SearchHit::__set_state(array(
+       'valueObject' => 
+      array (
+        'id' => 12,
+        'title' => 'Administrator users',
+      ),
+       'score' => 1,
+       'index' => NULL,
+       'highlight' => NULL,
+    )),
+  ),
+   'spellSuggestion' => NULL,
+   'time' => 1,
+   'timedOut' => NULL,
+   'maxScore' => 1,
+   'totalCount' => 12,
+));
+
