6fe067f2a4dfb90f2cf25a49d7801a8717cdbe36
André R <ar@ez.no>
2015-06-16 14:50:33 +0200

    [Travis] Update composer before setting auth key
    In case the auth format changed between the two different composer versions.

--- bin/.travis/prepare_unittest.sh
+++ bin/.travis/prepare_unittest.sh
@@ -9,11 +9,11 @@ if [ "$TRAVIS_PHP_VERSION" != "hhvm" ] ; then phpenv config-rm xdebug.ini ; fi
 if [ "$DB" = "mysql" ] ; then mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;" -uroot ; fi
 if [ "$DB" = "postgresql" ] ; then psql -c "CREATE DATABASE $DB_NAME;" -U postgres ; psql -c "CREATE EXTENSION pgcrypto;" -U postgres $DB_NAME ; fi
 
-# Setup github key to avoid api rate limit
-./bin/.travis/install_composer_github_key.sh
-
 # Update composer to newest version
 composer self-update -v --no-interaction
+
+# Setup github key to avoid api rate limit
+./bin/.travis/install_composer_github_key.sh
 
 # Switch to another Symfony version if asked for
 if [ "$SYMFONY_VERSION" != "" ] ; then composer require --no-update symfony/symfony=$SYMFONY_VERSION ; fi;
