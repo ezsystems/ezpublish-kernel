6c2b327bdef225af7803e42745384962a4dc272f
Petar Španja <petar.spanja@gmail.com>
2015-06-30 21:30:04 +0200

    EZP-24471: sync language code field with Content document so that it can be handled by common core filter

--- eZ/Publish/Core/Search/Solr/Content/Location/CriterionVisitor/LanguageCodeIn.php
+++ eZ/Publish/Core/Search/Solr/Content/Location/CriterionVisitor/LanguageCodeIn.php
@@ -46,7 +46,7 @@ class LanguageCodeIn extends CriterionVisitor
         $languageCodeExpressions = array_map(
             function ( $value )
             {
-                return 'content_language_code_ms:"' . $value . '"';
+                return 'language_code_ms:"' . $value . '"';
             },
             $criterion->value
         );
