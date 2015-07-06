8aa12e0c51f818c30b94cc32c9e9155016c04e20
Petar Španja <petar.spanja@gmail.com>
2015-07-06 13:25:59 +0200

    EZP-24557: implement DateModified sort clause for Content Search

---
+++ eZ/Publish/Core/Search/Solr/Content/SortClauseVisitor/DateModified.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * File containing the SortClauseVisitor\DateModified class
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\Core\Search\Solr\Content\SortClauseVisitor;
+
+use eZ\Publish\Core\Search\Solr\Content\SortClauseVisitor;
+use eZ\Publish\API\Repository\Values\Content\Query\SortClause;
+
+/**
+ * Visits the sortClause tree into a Solr query
+ */
+class DateModified extends SortClauseVisitor
+{
+    /**
+     * Check if visitor is applicable to current sortClause
+     *
+     * @param \eZ\Publish\API\Repository\Values\Content\Query\SortClause $sortClause
+     *
+     * @return boolean
+     */
+    public function canVisit( SortClause $sortClause )
+    {
+        return $sortClause instanceof SortClause\DateModified;
+    }
+
+    /**
+     * Map field value to a proper Solr representation
+     *
+     * @param \eZ\Publish\API\Repository\Values\Content\Query\SortClause $sortClause
+     *
+     * @return string
+     */
+    public function visit( SortClause $sortClause )
+    {
+        return 'modified_dt' . $this->getDirection( $sortClause );
+    }
+}
