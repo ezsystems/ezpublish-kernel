b452b900db6360db8a1ba918d3b41b976d615594
André R <ar@ez.no>
2015-07-14 19:15:21 +0200

    [Sf 2.7] Remove Twig tempalte collector now that this is handled by Symfony
    As this is now handled by Symfony, we can safly remove this to reduce memory use a bit in dev mode.
    
    Conflicts on PSR-2 rebase:
    	eZ/Bundle/EzPublishDebugBundle/Collector/TemplateDebugInfo.php
    	eZ/Bundle/EzPublishDebugBundle/Collector/TemplatesDataCollector.php
    	eZ/Bundle/EzPublishDebugBundle/Twig/DebugTemplate.php

--- eZ/Bundle/EzPublishDebugBundle/Twig/DebugTemplate.php
+++ eZ/Bundle/EzPublishDebugBundle/Twig/DebugTemplate.php
@@ -11,24 +11,18 @@
 
 namespace eZ\Bundle\EzPublishDebugBundle\Twig;
 
-use eZ\Bundle\EzPublishDebugBundle\Collector\TemplateDebugInfo;
 use Twig_Template;
 
 /**
  * Meant to be used as a Twig base template class.
  *
  * Wraps the display method to:
- * - Record display() execution time for each template
- * - Log the execution and its time using eZ\Bundle\EzPublishDebugBundle
+ * - Inject debug info into template to be able to see in the markup which one is used
  */
 abstract class DebugTemplate extends Twig_Template
 {
     public function display(array $context, array $blocks = array())
     {
-        $startTime = microtime(true);
-
-        $templateListBefore = TemplateDebugInfo::getTemplatesList();
-
         // Bufferize to be able to insert template name as HTML comments if applicable.
         // Layout template name will only appear at the end, to avoid potential quirks with old browsers
         // when comments appear before doctype declaration.
@@ -68,44 +62,5 @@ abstract class DebugTemplate extends Twig_Template
         } else {
             echo $templateResult;
         }
-
-        $endTime = microtime(true);
-
-        $templateListAfter = TemplateDebugInfo::getTemplatesList();
-
-        TemplateDebugInfo::addTemplate(
-            $this->getTemplateName(),
-            $this->computeExecutionTime(
-                round(($endTime - $startTime) * 1000),
-                $templateListBefore['full'],
-                $templateListAfter['full']
-            )
-        );
-    }
-
-    /**
-     * Given a raw $executionTime, and list of templates before and after display, computes the *real* execution time
-     * by subtracting the time taken to display nested templates.
-     *
-     * @param int $executionTime milliseconds
-     * @param array $templateListBefore templateName => executionTime
-     * @param array $templateListAfter templateName => executionTime
-     *
-     * @return int Computed execution time in milliseconds
-     */
-    protected function computeExecutionTime($executionTime, $templateListBefore, $templateListAfter)
-    {
-        foreach ($templateListAfter as $templateName => $nestedExecutionTime) {
-            if (isset($templateListBefore[$templateName])) {
-                if ($templateListBefore[$templateName] == $templateListAfter[$templateName]) {
-                    continue;
-                }
-                $executionTime -= ($nestedExecutionTime - $templateListBefore[$templateName]);
-            } else {
-                $executionTime -= $nestedExecutionTime;
-            }
-        }
-
-        return $executionTime;
     }
 }
