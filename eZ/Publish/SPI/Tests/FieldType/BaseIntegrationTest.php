a8706648edb3a30db85a980da128b1ac14d464e9
Petar Španja <petar.spanja@gmail.com>
2015-08-21 12:59:11 +0200

    Remove deprecated shortcuts (to removed services)

--- eZ/Publish/SPI/Tests/FieldType/BaseIntegrationTest.php
+++ eZ/Publish/SPI/Tests/FieldType/BaseIntegrationTest.php
@@ -579,7 +579,6 @@ abstract class BaseIntegrationTest extends TestCase
         $loader->load('storage_engines/shortcuts.yml');
         $loader->load('storage_engines/legacy.yml');
         $loader->load('search_engines/legacy.yml');
-        $loader->load('search_engines/shortcuts.yml');
         $loader->load('storage_engines/cache.yml');
         $loader->load('settings.yml');
         $loader->load('fieldtype_services.yml');
