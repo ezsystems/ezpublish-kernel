b139a03692405fb9b3144c2ebd5bc97e42f5f7b0
Bertrand Dunogier <bd@ez.no>
2015-09-01 20:30:05 +0200

    Updated REST Views routes
    All /content/views routes are moved to /views.
    BC is maintained for /content/views as it was already implemented.
    
    Actions are moved to their own Controller.
    
    EZP-24671

---
+++ eZ/Publish/Core/REST/Server/Controller/Views.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace eZ\Publish\Core\REST\Server\Controller;
+
+use eZ\Publish\API\Repository\Exceptions\NotImplementedException;
+use eZ\Publish\API\Repository\SearchService;
+use eZ\Publish\API\Repository\Values\Content\LocationQuery;
+use eZ\Publish\Core\REST\Server\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use eZ\Publish\Core\REST\Common\Message;
+use eZ\Publish\Core\REST\Server\Values;
+
+/**
+ * Controller for Repository Views (Search, mostly).
+ */
+class Views extends Controller
+{
+    /**
+     * @var \eZ\Publish\API\Repository\SearchService
+     */
+    private $searchService;
+
+    public function __construct(SearchService $searchService)
+    {
+        $this->searchService = $searchService;
+    }
+    /**
+     * Creates and executes a content view.
+     *
+     * @return \eZ\Publish\Core\REST\Server\Values\RestExecutedView
+     */
+    public function createView(Request $request)
+    {
+        $viewInput = $this->inputDispatcher->parse(
+            new Message(
+                array('Content-Type' => $request->headers->get('Content-Type')),
+                $request->getContent()
+            )
+        );
+
+        if ($viewInput->query instanceof LocationQuery) {
+            $method = 'findLocations';
+        } else {
+            $method = 'findContent';
+        }
+
+        return new Values\RestExecutedView(
+            array(
+                'identifier' => $viewInput->identifier,
+                'searchResults' => $this->searchService->$method($viewInput->query),
+            )
+        );
+    }
+
+    /**
+     * List content views.
+     *
+     * @return NotImplementedException;
+     */
+    public function listView()
+    {
+        return new NotImplementedException('ezpublish_rest.controller.content:listView');
+    }
+
+    /**
+     * Get a content view.
+     *
+     * @return NotImplementedException;
+     */
+    public function getView()
+    {
+        return new NotImplementedException('ezpublish_rest.controller.content:getView');
+    }
+
+    /**
+     * Get a content view results.
+     *
+     * @return NotImplementedException;
+     */
+    public function loadViewResults()
+    {
+        return new NotImplementedException('ezpublish_rest.controller.content:loadViewResults');
+    }
+}
