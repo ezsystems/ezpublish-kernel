f240ef94f396386bac24dcf74d80220a783a7148
Jérôme Vieilledent <lolautruche@gmail.com>
2015-09-03 16:04:30 +0200

    Added common base for SectionCreateStruct / SectionUpdateStruct
    Both classes share the same properties (mostly the same for other
    structs in the repository).
    This helps with form management / validation config
    
    See EZP-24380 / https://github.com/ezsystems/repository-forms/pull/37

---
+++ eZ/Publish/API/Repository/Values/Content/SectionStruct.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of the eZ Publish Kernel package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
+namespace eZ\Publish\API\Repository\Values\Content;
+
+use eZ\Publish\API\Repository\Values\ValueObject;
+
+abstract class SectionStruct extends ValueObject
+{
+    /**
+     * If set the Unique identifier of the section is changes.
+     *
+     * Needs to be a unique Section->identifier string value.
+     *
+     * @var string
+     */
+    public $identifier;
+
+    /**
+     * If set the name of the section is changed.
+     *
+     * @var string
+     */
+    public $name;
+}
--- eZ/Publish/API/Repository/Values/Content/SectionUpdateStruct.php
+++ eZ/Publish/API/Repository/Values/Content/SectionUpdateStruct.php
@@ -10,26 +10,9 @@
  */
 namespace eZ\Publish\API\Repository\Values\Content;
 
-use eZ\Publish\API\Repository\Values\ValueObject;
-
 /**
  * This class is used to provide data for updating a section. At least one property has to set.
  */
-class SectionUpdateStruct extends ValueObject
+class SectionUpdateStruct extends SectionStruct
 {
-    /**
-     * If set the Unique identifier of the section is changes.
-     *
-     * Needs to be a unique Section->identifier string value.
-     *
-     * @var string
-     */
-    public $identifier;
-
-    /**
-     * If set the name of the section is changed.
-     *
-     * @var string
-     */
-    public $name;
 }
