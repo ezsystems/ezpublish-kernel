97dc97604273713c6819fd34ce979fc81225341c
Bertrand Dunogier <bd@ez.no>
2015-09-08 16:45:57 +0200

    Behat: added EzPlatformBehatBundle
    Will contain the application elements required by behat: Console scripts,
    service definitions, controllers...

---
+++ eZ/Bundle/PlatformBehatBundle/EzPlatformBehatBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace EzSystems\PlatformBehatBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Bundle used by Behat tests, only loaded in the behat env.
+ */
+class EzPlatformBehatBundle extends Bundle
+{
+}
