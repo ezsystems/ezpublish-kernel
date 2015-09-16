21d94b34e9b16e7b943fddcf85eedeb3928630bb
Petar Španja <petar.spanja@gmail.com>
2015-09-16 22:06:09 +0200

    EZP-24816: implement Indexable for Url field type

---
+++ eZ/Publish/API/Repository/Tests/FieldType/SearchableUrl.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This file is part of the eZ Publish Kernel package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ *
+ * @version //autogentag//
+ */
+namespace eZ\Publish\API\Repository\Tests\FieldType;
+
+use eZ\Publish\Core\FieldType\Url\Type;
+use eZ\Publish\Core\Base\Exceptions\InvalidArgumentType;
+
+/**
+ * Url field type is not searchable in Legacy search engine, but will
+ * be searchable with Solr and Elasticsearch search engines.
+ *
+ * This is implementation simply extends the original implementation in order to
+ * define the field type as searchable, so that it can be tested.
+ */
+class SearchableUrl extends Type
+{
+    public function isSearchable()
+    {
+        return true;
+    }
+
+    protected static function checkValueType($value)
+    {
+        $fieldTypeFQN = 'eZ\\Publish\\Core\\FieldType\\Url\\Value';
+        $valueFQN = substr_replace($fieldTypeFQN, 'Value', strrpos($fieldTypeFQN, '\\') + 1);
+
+        if (!$value instanceof $valueFQN) {
+            throw new InvalidArgumentType('$value', $valueFQN, $value);
+        }
+    }
+}
---
+++ eZ/Publish/Core/FieldType/Url/SearchField.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * This file is part of the eZ Publish Kernel package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ *
+ * @version //autogentag//
+ */
+namespace eZ\Publish\Core\FieldType\Url;
+
+use eZ\Publish\SPI\Persistence\Content\Field;
+use eZ\Publish\SPI\Persistence\Content\Type\FieldDefinition;
+use eZ\Publish\SPI\FieldType\Indexable;
+use eZ\Publish\SPI\Search;
+
+/**
+ * Indexable definition for Url field type.
+ */
+class SearchField implements Indexable
+{
+    /**
+     * Get index data for field for search backend.
+     *
+     * @param \eZ\Publish\SPI\Persistence\Content\Field $field
+     * @param \eZ\Publish\SPI\Persistence\Content\Type\FieldDefinition $fieldDefinition
+     *
+     * @return \eZ\Publish\SPI\Search\Field[]
+     */
+    public function getIndexData(Field $field, FieldDefinition $fieldDefinition)
+    {
+        return array(
+            new Search\Field(
+                'value_url',
+                $field->value->externalData,
+                new Search\FieldType\StringField()
+            ),
+            new Search\Field(
+                'value_id',
+                isset($field->value->data['urlId']) ? $field->value->data['urlId'] : '',
+                new Search\FieldType\StringField()
+            ),
+            new Search\Field(
+                'value_text',
+                isset($field->value->data['text']) ? $field->value->data['text'] : '',
+                new Search\FieldType\StringField()
+            ),
+        );
+    }
+
+    /**
+     * Get index field types for search backend.
+     *
+     * @return \eZ\Publish\SPI\Search\FieldType[]
+     */
+    public function getIndexDefinition()
+    {
+        return array(
+            'value_url' => new Search\FieldType\StringField(),
+            'value_id' => new Search\FieldType\StringField(),
+            'value_text' => new Search\FieldType\StringField(),
+        );
+    }
+
+    /**
+     * Get name of the default field to be used for matching.
+     *
+     * As field types can index multiple fields (see MapLocation field type's
+     * implementation of this interface), this method is used to define default
+     * field for matching. Default field is typically used by Field criterion.
+     *
+     * @return string
+     */
+    public function getDefaultMatchField()
+    {
+        return 'value_url';
+    }
+
+    /**
+     * Get name of the default field to be used for sorting.
+     *
+     * As field types can index multiple fields (see MapLocation field type's
+     * implementation of this interface), this method is used to define default
+     * field for sorting. Default field is typically used by Field sort clause.
+     *
+     * @return string
+     */
+    public function getDefaultSortField()
+    {
+        return $this->getDefaultMatchField();
+    }
+}
