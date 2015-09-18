c881f02053b31a56e37414fef0e29d99f526eebb
Jérôme Vieilledent <lolautruche@gmail.com>
2015-09-18 16:02:30 +0200

    EZP-24793: Translate transformed Repository exceptions transformed to HTTP

--- eZ/Bundle/EzPublishCoreBundle/EventListener/ExceptionListener.php
+++ eZ/Bundle/EzPublishCoreBundle/EventListener/ExceptionListener.php
@@ -11,14 +11,27 @@ namespace eZ\Bundle\EzPublishCoreBundle\EventListener;
 use eZ\Publish\API\Repository\Exceptions\ForbiddenException;
 use eZ\Publish\API\Repository\Exceptions\NotFoundException;
 use eZ\Publish\API\Repository\Exceptions\UnauthorizedException;
+use eZ\Publish\Core\Base\Exceptions\TranslatableExceptionInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Translation\TranslatorInterface;
+use Exception;
 
 class ExceptionListener implements EventSubscriberInterface
 {
+    /**
+     * @var TranslatorInterface
+     */
+    private $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
     public static function getSubscribedEvents()
     {
         return [
@@ -29,10 +42,28 @@ class ExceptionListener implements EventSubscriberInterface
     public function onKernelException(GetResponseForExceptionEvent $event)
     {
         $exception = $event->getException();
+
         if ($exception instanceof NotFoundException) {
-            $event->setException(new NotFoundHttpException($exception->getMessage(), $exception));
+            $event->setException(new NotFoundHttpException($this->getTranslatedMessage($exception), $exception));
         } elseif ($exception instanceof UnauthorizedException || $exception instanceof ForbiddenException) {
-            $event->setException(new AccessDeniedHttpException($exception->getMessage(), $exception));
+            $event->setException(new AccessDeniedHttpException($this->getTranslatedMessage($exception), $exception));
         }
+    }
+
+    /**
+     * Translates the exception message if it is translatable.
+     *
+     * @param Exception $exception
+     *
+     * @return string
+     */
+    private function getTranslatedMessage(Exception $exception)
+    {
+        $message = $exception->getMessage();
+        if ($exception instanceof TranslatableExceptionInterface) {
+            $message = $this->translator->trans($exception->getMessageTemplate(), $exception->getParameters());
+        }
+
+        return $message;
     }
 }
