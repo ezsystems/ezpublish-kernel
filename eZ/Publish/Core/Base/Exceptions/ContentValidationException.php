4df55b7a2220854db662cbb1d81eaf30aea49189
Jérôme Vieilledent <lolautruche@gmail.com>
2015-09-18 12:00:59 +0200

    EZP-24793: Made ContentValidationException translatable

--- eZ/Publish/Core/Base/Exceptions/ContentValidationException.php
+++ eZ/Publish/Core/Base/Exceptions/ContentValidationException.php
@@ -15,6 +15,21 @@ use eZ\Publish\API\Repository\Exceptions\ContentValidationException as APIConten
 /**
  * This Exception is thrown on create or update content one or more given fields are not valid.
  */
-class ContentValidationException extends APIContentValidationException
+class ContentValidationException extends APIContentValidationException implements TranslatableExceptionInterface
 {
+    use TranslatableException;
+
+    /**
+     * @param string $messageTemplate The message template, with placeholders for parameters.
+     *                                E.g. "Content with ID %contentId% could not be found".
+     * @param array $parameters Hash map with param placeholder as key and its corresponding value.
+     *                          E.g. array('%contentId%' => 123).
+     */
+    public function __construct($messageTemplate, array $parameters = [])
+    {
+        $this->setMessageTemplate($messageTemplate);
+        $this->setParameters($parameters);
+
+        parent::__construct($this->getBaseTranslation());
+    }
 }
