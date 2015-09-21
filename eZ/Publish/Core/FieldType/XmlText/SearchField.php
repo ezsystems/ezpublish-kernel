720ea86b65181c5f9078b38c3060df6b57c138fa
Petar Španja <petar.spanja@gmail.com>
2015-09-21 19:46:07 +0200

    EZP-23247: implement indexables for XmlText and RichText

---
+++ eZ/Publish/Core/FieldType/XmlText/SearchField.php
@@ -0,0 +1,140 @@
+<?php
+
+/**
+ * This file is part of the eZ Publish Kernel package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ *
+ * @version //autogentag//
+ */
+namespace eZ\Publish\Core\FieldType\XmlText;
+
+use eZ\Publish\SPI\Persistence\Content\Field;
+use eZ\Publish\SPI\Persistence\Content\Type\FieldDefinition;
+use eZ\Publish\SPI\FieldType\Indexable;
+use eZ\Publish\SPI\Search;
+use DOMDocument;
+use DOMNode;
+
+/**
+ * Indexable definition for XmlText field type.
+ */
+class SearchField implements Indexable
+{
+    /**
+     * Get index data for field for search backend.
+     *
+     * @param \eZ\Publish\SPI\Persistence\Content\Field $field
+     * @param \eZ\Publish\SPI\Persistence\Content\Type\FieldDefinition $fieldDefinition
+     *
+     * @return \eZ\Publish\SPI\Search\Field[]
+     */
+    public function getIndexData(Field $field, FieldDefinition $fieldDefinition)
+    {
+        $document = new DOMDocument();
+        $document->loadXML($field->value->data);
+
+        return array(
+            new Search\Field(
+                'value',
+                $this->extractShortText($document),
+                new Search\FieldType\StringField()
+            ),
+            new Search\Field(
+                'fulltext',
+                $this->extractText($document->documentElement),
+                new Search\FieldType\FullTextField()
+            ),
+        );
+    }
+
+    /**
+     * Extracts text content of the given $node.
+     *
+     * @param \DOMNode $node
+     *
+     * @return string
+     */
+    private function extractText(DOMNode $node)
+    {
+        $text = '';
+
+        if ($node->childNodes) {
+            foreach ($node->childNodes as $child) {
+                $text .= $this->extractText($child);
+            }
+        } else {
+            $text .= $node->nodeValue . ' ';
+        }
+
+        return $text;
+    }
+
+    /**
+     * Extracts short text content of the given $document.
+     *
+     * @param \DOMDocument $document
+     *
+     * @return string
+     */
+    private function extractShortText(DOMDocument $document)
+    {
+        $result = null;
+        if ($section = $document->documentElement->firstChild) {
+            $textDom = $section->firstChild;
+
+            if ($textDom && $textDom->hasChildNodes()) {
+                $result = $textDom->firstChild->textContent;
+            } elseif ($textDom) {
+                $result = $textDom->textContent;
+            }
+        }
+
+        if ($result === null) {
+            $result = $document->documentElement->textContent;
+        }
+
+        return trim($result);
+    }
+
+    /**
+     * Get index field types for search backend.
+     *
+     * @return \eZ\Publish\SPI\Search\FieldType[]
+     */
+    public function getIndexDefinition()
+    {
+        return array(
+            'value' => new Search\FieldType\StringField(),
+        );
+    }
+
+    /**
+     * Get name of the default field to be used for matching.
+     *
+     * As field types can index multiple fields (see MapLocation field type's
+     * implementation of this interface), this method is used to define default
+     * field for matching. Default field is typically used by Field criterion.
+     *
+     * @return string
+     */
+    public function getDefaultMatchField()
+    {
+        return 'value';
+    }
+
+    /**
+     * Get name of the default field to be used for sorting.
+     *
+     * As field types can index multiple fields (see MapLocation field type's
+     * implementation of this interface), this method is used to define default
+     * field for sorting. Default field is typically used by Field sort clause.
+     *
+     * @return string
+     */
+    public function getDefaultSortField()
+    {
+        return $this->getDefaultMatchField();
+    }
+}
