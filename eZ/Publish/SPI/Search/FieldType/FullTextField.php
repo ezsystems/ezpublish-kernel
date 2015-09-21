78d1ab541efa7ad815f62ba674f9907876bf49a5
Petar Španja <petar.spanja@gmail.com>
2015-09-21 19:40:13 +0200

    EZP-23247: SPI: added FullText search backend field

---
+++ eZ/Publish/SPI/Search/FieldType/FullTextField.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This file is part of the eZ Publish Kernel package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ *
+ * @version //autogentag//
+ */
+namespace eZ\Publish\SPI\Search\FieldType;
+
+use eZ\Publish\SPI\Search\FieldType;
+
+/**
+ * Full text document field.
+ */
+class FullTextField extends FieldType
+{
+    /**
+     * The type name of the facet. Has to be handled by the solr schema.
+     *
+     * @var string
+     */
+    protected $type = 'ez_fulltext';
+}
