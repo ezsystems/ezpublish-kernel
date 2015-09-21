4333d0cac2dfe856a328d09a331787dfa3c58e74
Jérôme Vieilledent <lolautruche@gmail.com>
2015-09-21 14:29:43 +0200

    EZP-24793: Added test for ExceptionListener

---
+++ eZ/Bundle/EzPublishCoreBundle/Tests/EventListener/ExceptionListenerTest.php
@@ -0,0 +1,212 @@
+<?php
+/**
+ * This file is part of the eZ Publish Kernel package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
+namespace eZ\Bundle\EzPublishCoreBundle\Tests\EventListener;
+
+use Exception;
+use eZ\Bundle\EzPublishCoreBundle\EventListener\ExceptionListener;
+use eZ\Publish\Core\Base\Exceptions\BadStateException;
+use eZ\Publish\Core\Base\Exceptions\ContentFieldValidationException;
+use eZ\Publish\Core\Base\Exceptions\ContentTypeFieldDefinitionValidationException;
+use eZ\Publish\Core\Base\Exceptions\ContentTypeValidationException;
+use eZ\Publish\Core\Base\Exceptions\ContentValidationException;
+use eZ\Publish\Core\Base\Exceptions\ForbiddenException;
+use eZ\Publish\Core\Base\Exceptions\InvalidArgumentException;
+use eZ\Publish\Core\Base\Exceptions\InvalidArgumentType;
+use eZ\Publish\Core\Base\Exceptions\InvalidArgumentValue;
+use eZ\Publish\Core\Base\Exceptions\LimitationValidationException;
+use eZ\Publish\Core\Base\Exceptions\MissingClass;
+use eZ\Publish\Core\Base\Exceptions\NotFound\FieldTypeNotFoundException;
+use eZ\Publish\Core\Base\Exceptions\NotFound\LimitationNotFoundException;
+use eZ\Publish\Core\Base\Exceptions\NotFoundException;
+use eZ\Publish\Core\Base\Exceptions\UnauthorizedException;
+use PHPUnit_Framework_TestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class ExceptionListenerTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|\Symfony\Component\Translation\TranslatorInterface
+     */
+    private $translator;
+
+    /**
+     * @var ExceptionListener
+     */
+    private $listener;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->translator = $this->getMock('\Symfony\Component\Translation\TranslatorInterface');
+        $this->listener = new ExceptionListener($this->translator);
+    }
+
+    public function testGetSubscribedEvents()
+    {
+        self::assertSame(
+            [KernelEvents::EXCEPTION => ['onKernelException', -90]],
+            ExceptionListener::getSubscribedEvents()
+        );
+    }
+
+    /**
+     * @param Exception $exception
+     * @return GetResponseForExceptionEvent
+     */
+    private function generateExceptionEvent(Exception $exception)
+    {
+        return new GetResponseForExceptionEvent(
+            $this->getMock('\Symfony\Component\HttpKernel\HttpKernelInterface'),
+            new Request(),
+            'master',
+            $exception
+        );
+    }
+
+    public function testNotFoundException()
+    {
+        $messageTemplate = 'some message template';
+        $translationParams = ['some' => 'thing'];
+        $exception = new NotFoundException('foo', 'bar');
+        $exception->setMessageTemplate($messageTemplate);
+        $exception->setParameters($translationParams);
+        $event = $this->generateExceptionEvent($exception);
+
+        $translatedMessage = 'translated message';
+        $this->translator
+            ->expects($this->once())
+            ->method('trans')
+            ->with($messageTemplate, $translationParams)
+            ->willReturn($translatedMessage);
+
+        $this->listener->onKernelException($event);
+        $convertedException = $event->getException();
+        self::assertInstanceOf('\Symfony\Component\HttpKernel\Exception\NotFoundHttpException', $convertedException);
+        self::assertSame($exception, $convertedException->getPrevious());
+        self::assertSame($translatedMessage, $convertedException->getMessage());
+    }
+
+    public function testUnauthorizedException()
+    {
+        $messageTemplate = 'some message template';
+        $translationParams = ['some' => 'thing'];
+        $exception = new UnauthorizedException('foo', 'bar');
+        $exception->setMessageTemplate($messageTemplate);
+        $exception->setParameters($translationParams);
+        $event = $this->generateExceptionEvent($exception);
+
+        $translatedMessage = 'translated message';
+        $this->translator
+            ->expects($this->once())
+            ->method('trans')
+            ->with($messageTemplate, $translationParams)
+            ->willReturn($translatedMessage);
+
+        $this->listener->onKernelException($event);
+        $convertedException = $event->getException();
+        self::assertInstanceOf('\Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException', $convertedException);
+        self::assertSame($exception, $convertedException->getPrevious());
+        self::assertSame($translatedMessage, $convertedException->getMessage());
+    }
+
+    /**
+     * @dataProvider badRequestExceptionProvider
+     *
+     * @param Exception|\eZ\Publish\Core\Base\Exceptions\TranslatableException $exception
+     */
+    public function testBadRequestException(Exception $exception)
+    {
+        $messageTemplate = 'some message template';
+        $translationParams = ['some' => 'thing'];
+        $exception->setMessageTemplate($messageTemplate);
+        $exception->setParameters($translationParams);
+        $event = $this->generateExceptionEvent($exception);
+
+        $translatedMessage = 'translated message';
+        $this->translator
+            ->expects($this->once())
+            ->method('trans')
+            ->with($messageTemplate, $translationParams)
+            ->willReturn($translatedMessage);
+
+        $this->listener->onKernelException($event);
+        $convertedException = $event->getException();
+        self::assertInstanceOf('\Symfony\Component\HttpKernel\Exception\BadRequestHttpException', $convertedException);
+        self::assertSame($exception, $convertedException->getPrevious());
+        self::assertSame($translatedMessage, $convertedException->getMessage());
+    }
+
+    public function badRequestExceptionProvider()
+    {
+        return [
+            [new BadStateException('foo', 'bar')],
+            [new InvalidArgumentException('foo', 'bar')],
+            [new InvalidArgumentType('foo', 'bar')],
+            [new InvalidArgumentValue('foo', 'bar')],
+        ];
+    }
+
+    /**
+     * @dataProvider otherExceptionProvider
+     *
+     * @param Exception|\eZ\Publish\Core\Base\Exceptions\TranslatableException $exception
+     */
+    public function testOtherRepositoryException(Exception $exception)
+    {
+        $messageTemplate = 'some message template';
+        $translationParams = ['some' => 'thing'];
+        $exception->setMessageTemplate($messageTemplate);
+        $exception->setParameters($translationParams);
+        $event = $this->generateExceptionEvent($exception);
+
+        $translatedMessage = 'translated message';
+        $this->translator
+            ->expects($this->once())
+            ->method('trans')
+            ->with($messageTemplate, $translationParams)
+            ->willReturn($translatedMessage);
+
+        $this->listener->onKernelException($event);
+        $convertedException = $event->getException();
+        self::assertInstanceOf('\Symfony\Component\HttpKernel\Exception\HttpException', $convertedException);
+        self::assertSame($exception, $convertedException->getPrevious());
+        self::assertSame(get_class($exception) . ': ' . $translatedMessage, $convertedException->getMessage());
+        self::assertSame(Response::HTTP_INTERNAL_SERVER_ERROR, $convertedException->getStatusCode());
+    }
+
+    public function otherExceptionProvider()
+    {
+        return [
+            [new ForbiddenException('foo')],
+            [new LimitationValidationException([])],
+            [new MissingClass('foo')],
+            [new ContentValidationException('foo')],
+            [new ContentTypeValidationException('foo')],
+            [new ContentFieldValidationException([])],
+            [new ContentTypeFieldDefinitionValidationException([])],
+            [new FieldTypeNotFoundException('foo')],
+            [new LimitationNotFoundException('foo')],
+        ];
+    }
+
+    public function testUntouchedException()
+    {
+        $exception = new \RuntimeException('foo');
+        $event = $this->generateExceptionEvent($exception);
+        $this->translator
+            ->expects($this->never())
+            ->method('trans');
+
+        $this->listener->onKernelException($event);
+        self::assertSame($exception, $event->getException());
+    }
+}
