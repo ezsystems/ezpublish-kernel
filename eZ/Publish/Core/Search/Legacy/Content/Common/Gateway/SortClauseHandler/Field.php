28c6d0fef4b40f191fd2860ab411be97bd41e76a
Petar Španja <petar.spanja@gmail.com>
2015-09-30 14:05:04 +0200

    EZP-24538: implemented field language filtering condition

--- eZ/Publish/Core/Search/Legacy/Content/Common/Gateway/SortClauseHandler/Field.php
+++ eZ/Publish/Core/Search/Legacy/Content/Common/Gateway/SortClauseHandler/Field.php
@@ -200,4 +200,92 @@ class Field extends SortClauseHandler
                 )
             );
     }
+
+    /**
+     * Returns a field language join condition for the given $languageSettings.
+     *
+     * @param \eZ\Publish\Core\Persistence\Database\SelectQuery $query
+     * @param array $languageSettings
+     * @param string $fieldTableName
+     *
+     * @return string
+     */
+    protected function getFieldCondition(SelectQuery $query, array $languageSettings, $fieldTableName)
+    {
+        // 1. Use main language(s) by default
+        if (empty($languageSettings['languages'])) {
+            return $query->expr->gt(
+                $query->expr->bitAnd(
+                    $this->dbHandler->quoteColumn('initial_language_id', 'ezcontentobject'),
+                    $this->dbHandler->quoteColumn('language_id', $fieldTableName)
+                ),
+                $query->bindValue(0, null, PDO::PARAM_INT)
+            );
+        }
+
+        // 2. Otherwise use prioritized languages
+        $leftSide = $query->expr->bitAnd(
+            $query->expr->sub(
+                $this->dbHandler->quoteColumn('language_mask', 'ezcontentobject'),
+                $query->expr->bitAnd(
+                    $this->dbHandler->quoteColumn('language_mask', 'ezcontentobject'),
+                    $this->dbHandler->quoteColumn('language_id', $fieldTableName)
+                )
+            ),
+            $query->bindValue(1, null, PDO::PARAM_INT)
+        );
+        $rightSide = $query->expr->bitAnd(
+            $this->dbHandler->quoteColumn('language_id', $fieldTableName),
+            $query->bindValue(1, null, PDO::PARAM_INT)
+        );
+
+        for ($index = count($languageSettings['languages']) - 1, $multiplier = 2; $index >= 0; $index--, $multiplier *= 2) {
+            $languageId = $this->languageHandler
+                ->loadByLanguageCode($languageSettings['languages'][$index])->id;
+
+            $addToLeftSide = $query->expr->bitAnd(
+                $query->expr->sub(
+                    $this->dbHandler->quoteColumn('language_mask', 'ezcontentobject'),
+                    $query->expr->bitAnd(
+                        $this->dbHandler->quoteColumn('language_mask', 'ezcontentobject'),
+                        $this->dbHandler->quoteColumn('language_id', $fieldTableName)
+                    )
+                ),
+                $query->bindValue($languageId, null, PDO::PARAM_INT)
+            );
+            $addToRightSide = $query->expr->bitAnd(
+                $this->dbHandler->quoteColumn('language_id', $fieldTableName),
+                $query->bindValue($languageId, null, PDO::PARAM_INT)
+            );
+
+            if ($multiplier > $languageId) {
+                $factor = $multiplier / $languageId;
+                for ($shift = 0; $factor > 1; $factor = $factor / 2, $shift++);
+                $factorTerm = ' << ' . $shift;
+                $addToLeftSide .= $factorTerm;
+                $addToRightSide .= $factorTerm;
+            }
+            else if ($multiplier < $languageId) {
+                $factor = $languageId / $multiplier;
+                for ($shift = 0; $factor > 1; $factor = $factor / 2, $shift++);
+                $factorTerm = ' >> ' . $shift;
+                $addToLeftSide .= $factorTerm;
+                $addToRightSide .= $factorTerm;
+            }
+
+            $leftSide = $query->expr->add($leftSide, "($addToLeftSide)");
+            $rightSide = $query->expr->add($rightSide, "($addToRightSide)");
+        }
+
+        return $query->expr->lAnd(
+            $query->expr->gt(
+                $query->expr->bitAnd(
+                    $this->dbHandler->quoteColumn('language_mask', 'ezcontentobject'),
+                    $this->dbHandler->quoteColumn('language_id', $fieldTableName)
+                ),
+                $query->bindValue(0, null, PDO::PARAM_INT)
+            ),
+            $query->expr->lt($leftSide, $rightSide)
+        );
+    }
 }
