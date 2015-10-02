edc50ba4131878a770ea63669481f0adcb242862
Bertrand Dunogier <bd@ez.no>
2015-10-02 14:44:48 +0200

    Implemented the ViewConfigurator
    A ViewConfigurator configures a View object it is given (template, controller...).
    
    A ViewProvider implementation is provided, that uses the registered ViewProviders,
    via the view_provider registry.

--- eZ/Publish/Core/MVC/Symfony/View/BaseView.php
+++ eZ/Publish/Core/MVC/Symfony/View/BaseView.php
@@ -17,12 +17,12 @@ abstract class BaseView implements View
     /**
      * @var array
      */
-    protected $parameters;
+    protected $parameters = [];
 
     /**
      * @var array
      */
-    protected $configHash;
+    protected $configHash = [];
 
     /**
      * @var string
---
+++ eZ/Publish/Core/MVC/Symfony/View/Configurator/ViewProvider.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace eZ\Publish\Core\MVC\Symfony\View\Configurator;
+
+use eZ\Publish\Core\MVC\Symfony\View\Configurator;
+use eZ\Publish\Core\MVC\Symfony\View\Provider\Registry;
+use eZ\Publish\Core\MVC\Symfony\View\View;
+
+/**
+ * Configures a view based on the ViewProviders.
+ *
+ * Typically, the Configured ViewProvider will be included, meaning that Views will be customized based on the
+ * view rules defined in the siteaccess aware configuration (content_view, block_view, ...).
+ */
+class ViewProvider implements Configurator
+{
+    /** @var Registry */
+    private $providerRegistry;
+
+    /**
+     * ViewProvider constructor.
+     *
+     * @param \eZ\Publish\Core\MVC\Symfony\View\Provider\Registry $providersRegistry
+     */
+    public function __construct(Registry $providersRegistry)
+    {
+        $this->providerRegistry = $providersRegistry;
+    }
+
+    public function configure(View $view)
+    {
+        foreach ($this->providerRegistry->getViewProviders($view) as $viewProvider) {
+            if ($providerView = $viewProvider->getView($view)) {
+                $view->setConfigHash($providerView->getConfigHash());
+                if (($templateIdentifier = $providerView->getTemplateIdentifier()) !== null) {
+                    $view->setTemplateIdentifier($templateIdentifier);
+                }
+
+                if (($controllerReference = $providerView->getControllerReference()) !== null) {
+                    $view->setControllerReference($controllerReference);
+                }
+
+                return;
+            }
+        }
+    }
+}
