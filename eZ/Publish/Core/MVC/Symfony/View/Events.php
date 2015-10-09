b108484a3ea0db2f4b4410bb075239258ba48f3a
Bertrand Dunogier <bd@ez.no>
2015-10-09 18:11:41 +0200

    Added event for viewbuilder parameter collection

---
+++ eZ/Publish/Core/MVC/Symfony/View/Events.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace eZ\Publish\Core\MVC\Symfony\View;
+
+/**
+ * Events constants of views.
+ */
+class Events
+{
+    const BUILDER_PARAMETER_COLLECTION = 'view.builder_parameter_collection';
+}
