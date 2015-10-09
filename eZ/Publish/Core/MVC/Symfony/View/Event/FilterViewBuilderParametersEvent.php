4b4779c42270dd2b2b740901384afbc0e8d9a99c
Bertrand Dunogier <bd@ez.no>
2015-10-09 18:11:41 +0200

    Added event for viewbuilder parameter collection

---
+++ eZ/Publish/Core/MVC/Symfony/View/Event/FilterViewBuilderParametersEvent.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace eZ\Publish\Core\MVC\Symfony\View\Event;
+
+use Symfony\Component\EventDispatcher\Event;
+use Symfony\Component\HttpFoundation\ParameterBag;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * An event that collects the parameters the ViewBuilder will be provided to build View objects.
+ */
+class FilterViewBuilderParametersEvent extends Event
+{
+    /**
+     * @var \Symfony\Component\HttpFoundation\Request
+     */
+    private $request;
+
+    /**
+     * Parameters the ViewBuilder will use.
+     * @var ParameterBag
+     */
+    private $parameters;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+        $this->parameters = new ParameterBag();
+    }
+
+    /**
+     * @return Request
+     */
+    public function getRequest()
+    {
+        return $this->request;
+    }
+
+    /**
+     * Returns the ParameterBag that holds the ViewBuilder's parameters.
+     * @return \Symfony\Component\HttpFoundation\ParameterBag
+     */
+    public function getParameters()
+    {
+        return $this->parameters;
+    }
+}
