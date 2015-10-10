0a54f4c72a1bb9dd5dc0708db06b685f06ca76c8
Edi Modrić <edi@netgen.hr>
2015-10-10 22:24:25 +0200

    Enable view parameters injector and configurator in block view builder

--- eZ/Publish/Core/MVC/Symfony/View/Builder/BlockViewBuilder.php
+++ eZ/Publish/Core/MVC/Symfony/View/Builder/BlockViewBuilder.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
@@ -7,6 +8,8 @@ namespace eZ\Publish\Core\MVC\Symfony\View\Builder;
 use eZ\Publish\Core\FieldType\Page\PageService;
 use eZ\Publish\Core\FieldType\Page\Parts\Block;
 use eZ\Publish\Core\MVC\Symfony\View\BlockView;
+use eZ\Publish\Core\MVC\Symfony\View\Configurator;
+use eZ\Publish\Core\MVC\Symfony\View\ParametersInjector;
 
 /**
  * Builds BlockView objects.
@@ -16,9 +19,20 @@ class BlockViewBuilder implements ViewBuilder
     /** @var PageService */
     private $pageService;
 
-    public function __construct(PageService $pageService)
-    {
+    /** @var Configurator */
+    private $viewConfigurator;
+
+    /** @var ParametersInjector */
+    private $viewParametersInjector;
+
+    public function __construct(
+        PageService $pageService,
+        Configurator $viewConfigurator,
+        ParametersInjector $viewParametersInjector
+    ) {
         $this->pageService = $pageService;
+        $this->viewConfigurator = $viewConfigurator;
+        $this->viewParametersInjector = $viewParametersInjector;
     }
 
     public function matches($argument)
@@ -31,13 +45,15 @@ class BlockViewBuilder implements ViewBuilder
         $view = new BlockView();
 
         if (isset($parameters['id'])) {
-            $view->addParameters(['id' => $parameters['id']]);
             $view->setBlock(
                 $this->pageService->loadBlock($parameters['id'])
             );
         } elseif ($parameters['block'] instanceof Block) {
             $view->setBlock($parameters['block']);
         }
+
+        $this->viewConfigurator->configure($view);
+        $this->viewParametersInjector->injectViewParameters($view, $parameters);
 
         return $view;
     }
