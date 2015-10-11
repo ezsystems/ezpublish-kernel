4d3c696936fabe38c1ad6034cca243fa002fa0c0
Bertrand Dunogier <bd@ez.no>
2015-10-11 12:18:12 +0200

    Implemented QueryType results view param injector

---
+++ eZ/Publish/Core/MVC/Symfony/View/ParametersInjector/QueryTypeResults.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace eZ\Publish\Core\MVC\Symfony\View\ParametersInjector;
+
+use eZ\Publish\Core\Base\Exceptions\InvalidArgumentException;
+use eZ\Publish\Core\MVC\Symfony\View\Event\FilterViewParametersEvent;
+use eZ\Publish\Core\MVC\Symfony\View\QueryTypeView;
+use eZ\Publish\Core\MVC\Symfony\View\ViewEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class QueryTypeResults implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return [ViewEvents::FILTER_VIEW_PARAMETERS => 'injectQueryTypeResults'];
+    }
+
+    public function injectQueryTypeResults(FilterViewParametersEvent $event)
+    {
+        $view = $event->getView();
+        if (!$view instanceof QueryTypeView) {
+            return;
+        }
+
+        $searchResult = $view->getSearchResult();
+        $parameters = [
+            'total_count' => $searchResult->totalCount,
+            'parameters' => $view->getQueryParameters(),
+            'list_count' => count($searchResult->searchHits),
+        ] + $this->extractSearchResults($view);
+
+        $event->getParameterBag()->add($parameters);
+    }
+
+    /**
+     * Extracts the search results into a $key + value objects array, with $key depending on the search type.
+     *
+     * @param \eZ\Publish\Core\MVC\Symfony\View\QueryTypeView $view
+     *
+     * @return array resultsKey => resultsArray
+     * @throws \eZ\Publish\Core\Base\Exceptions\InvalidArgumentException If the searched type is unknown
+     */
+    private function extractSearchResults(QueryTypeView $view)
+    {
+        $searchedTypeListKeyMap = [
+            QueryTypeView::SEARCH_TYPE_CONTENT_INFO => 'content_info_list',
+            QueryTypeView::SEARCH_TYPE_CONTENT => 'content_list',
+            QueryTypeView::SEARCH_TYPE_LOCATION => 'location_list',
+        ];
+
+        if (!isset($searchedTypeListKeyMap[$view->getSearchedType()])) {
+            throw new InvalidArgumentException('searchedType', 'Unknown search type ' . $view->getSearchedType());
+        }
+        $itemsKey = $searchedTypeListKeyMap[$view->getSearchedType()];
+
+        $items = [];
+        foreach ($view->getSearchResult()->searchHits as $searchHit) {
+            $items[] = $searchHit->valueObject;
+        }
+
+        return [$itemsKey => $items];
+    }
+}
