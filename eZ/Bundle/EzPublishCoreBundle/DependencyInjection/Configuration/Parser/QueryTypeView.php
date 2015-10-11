a1c22cfa8cf2e8dcafbf3f1783c9af11574b3665
Bertrand Dunogier <bd@ez.no>
2015-10-11 15:57:58 +0200

    Implemented query_type_view config parsing

---
+++ eZ/Bundle/EzPublishCoreBundle/DependencyInjection/Configuration/Parser/QueryTypeView.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * File containing the LocationView class.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ *
+ * @version //autogentag//
+ */
+namespace eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Configuration\Parser;
+
+class QueryTypeView extends View
+{
+    const NODE_KEY = 'query_type_view';
+    const INFO = 'Template selection settings when viewing a query type result';
+}
