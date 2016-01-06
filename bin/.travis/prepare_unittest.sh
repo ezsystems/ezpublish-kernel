6ebf7b17159dab5fe472b88884c1168d30946ceb
André R <ar@ez.no>
2016-01-06 15:36:07 +0100

    [Travis] Update approach for Github token to same as Symfony uses
    This approach, *as used by [Symfony](https://github.com/symfony/symfony/commit/6215437bdd6d6fc67ee167aea5e88324117be668) now*, is more straight forward, and avoids setting a bad example for others *(risking they create tokens with a scope without bein protected by Github's commit token scanners, unintentionally exposing rights to their repos)*.
    
    Besides this moves us one micro step towards enabling appveyor use (Continuous integration on Windows).
    
    This is copy of what was done in https://github.com/ezsystems/ezplatform/pull/78.

--- bin/.travis/prepare_unittest.sh
+++ bin/.travis/prepare_unittest.sh
@@ -9,18 +9,18 @@ if [ "$TRAVIS_PHP_VERSION" != "hhvm" ] ; then phpenv config-rm xdebug.ini ; fi
 if [ "$DB" = "mysql" ] ; then mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;" -uroot ; fi
 if [ "$DB" = "postgresql" ] ; then psql -c "CREATE DATABASE $DB_NAME;" -U postgres ; psql -c "CREATE EXTENSION pgcrypto;" -U postgres $DB_NAME ; fi
 
-# Update composer to newest version
-# disabled, issues with packagist/github, something..
-#composer self-update -v --no-interaction
+echo "> Setup github auth key to not reach api limit"
+cp bin/.travis/composer-auth.json ~/.composer/auth.json
 
-# Setup github key to avoid api rate limit
-./bin/.travis/install_composer_github_key.sh
-
-# Install packages using composer
-composer install -v --no-progress --no-interaction
-
-# Switch to another Symfony version if asked for
-if [ "$SYMFONY_VERSION" != "" ] ; then composer require symfony/symfony="$SYMFONY_VERSION" ; fi;
+# Switch to another Symfony version if asked for (with composer update to not use composer.lock if present)
+if [ "$SYMFONY_VERSION" != "" ] ; then
+    echo "> Install dependencies through Composer (with custom Symfony version: ${SYMFONY_VERSION})"
+    composer require --no-update symfony/symfony="${SYMFONY_VERSION}"
+    composer update --no-progress --no-interaction --prefer-dist
+else
+    echo "> Install dependencies through Composer"
+    composer install --no-progress --no-interaction --prefer-dist
+fi
 
 # Setup Solr / Elastic search if asked for
 if [ "$TEST_CONFIG" = "phpunit-integration-legacy-elasticsearch.xml" ] ; then ./bin/.travis/init_elasticsearch.sh ; fi
