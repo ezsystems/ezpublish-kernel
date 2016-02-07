8127a2f8efb0f521047bb11abec1c0d6c5824295
Gabriele Perego <gabriele.perego@me.com>
2016-02-07 14:53:33 +0100

    Fixed CS issue

--- eZ/Publish/Core/Repository/Helper/NameSchemaService.php
+++ eZ/Publish/Core/Repository/Helper/NameSchemaService.php
@@ -193,7 +193,7 @@ class NameSchemaService
 
             // Make sure length is not longer then $limit unless it's 0
             if ($this->settings['limit'] && strlen($name) > $this->settings['limit']) {
-                $name = rtrim(mb_substr($name, 0, $this->settings['limit'] - strlen($this->settings['sequence']),"UTF-8")) . $this->settings['sequence'];
+                $name = rtrim(mb_substr($name, 0, $this->settings['limit'] - strlen($this->settings['sequence']), 'UTF-8')) . $this->settings['sequence'];
             }
 
             $names[$languageCode] = $name;
