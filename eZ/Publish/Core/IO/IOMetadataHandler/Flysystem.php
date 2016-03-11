55ecd46532f77af9a4ca484388fee225b5a868f4
Oliver Stark <os@fortrabbit.com>
2016-03-11 10:59:51 +0100

    psr-2 styles

--- eZ/Publish/Core/IO/IOMetadataHandler/Flysystem.php
+++ eZ/Publish/Core/IO/IOMetadataHandler/Flysystem.php
@@ -6,6 +6,7 @@
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
+
 namespace eZ\Publish\Core\IO\IOMetadataHandler;
 
 use DateTime;
@@ -56,9 +57,9 @@ class Flysystem implements IOMetadataHandler
         $spiBinaryFile = new SPIBinaryFile();
         $spiBinaryFile->id = $spiBinaryFileId;
         $spiBinaryFile->size = $info['size'];
-        
+
         if (isset($info['timestamp'])) {
-            $spiBinaryFile->mtime = new DateTime('@' . $info['timestamp']);
+            $spiBinaryFile->mtime = new DateTime('@'.$info['timestamp']);
         }
 
         return $spiBinaryFile;
