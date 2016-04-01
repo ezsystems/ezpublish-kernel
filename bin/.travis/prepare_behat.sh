b27619a401232abf54489b18819f3eb1ee2ea1fa
André R <ar@ez.no>
2016-04-01 12:56:51 +0200

    [Travis] Adjust apporach used for travis branches for fucntional testing to be 100% maintained in composer.json

--- bin/.travis/prepare_behat.sh
+++ bin/.travis/prepare_behat.sh
@@ -3,7 +3,7 @@
 # File for setting up system for behat testing, just like done in DemoBundle's .travis.yml
 
 # Change local git repo to be a full one as we will reuse it for composer install below
-git fetch --unshallow && git checkout -b tmp_travis_branch
+git fetch --unshallow && git checkout -b tmp_ci_branch
 export BRANCH_BUILD_DIR=$TRAVIS_BUILD_DIR
 export TRAVIS_BUILD_DIR="$HOME/build/ezplatform"
 cd "$HOME/build"
@@ -13,4 +13,4 @@ git clone --depth 1 --single-branch --branch master https://github.com/ezsystems
 cd ezplatform
 
 # Install everything needed for behat testing, using our local branch of this repo
-./bin/.travis/setup_from_external_repo.sh $BRANCH_BUILD_DIR "ezsystems/ezpublish-kernel:dev-tmp_travis_branch as 6.3.x-dev"
+./bin/.travis/setup_from_external_repo.sh $BRANCH_BUILD_DIR "ezsystems/ezpublish-kernel:dev-tmp_ci_branch"
