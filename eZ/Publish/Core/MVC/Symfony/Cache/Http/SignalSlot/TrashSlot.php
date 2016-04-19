5c70e96e327c399d62b2e04c9a93f2b6ef74f798
Bertrand Dunogier <bd@ez.no>
2016-04-19 17:39:09 +0200

    EZP-25003: clear HTTP cache when trashing content

---
+++ eZ/Publish/Core/MVC/Symfony/Cache/Http/SignalSlot/TrashSlot.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of the eZ Publish Kernel package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace eZ\Publish\Core\MVC\Symfony\Cache\Http\SignalSlot;
+
+use eZ\Publish\Core\SignalSlot\Signal;
+
+/**
+ * A slot handling TrashSignal.
+ */
+class TrashSlot extends PurgeAllHttpCacheSlot
+{
+    protected function supports(Signal $signal)
+    {
+        return $signal instanceof Signal\TrashService\TrashSignal;
+    }
+}
