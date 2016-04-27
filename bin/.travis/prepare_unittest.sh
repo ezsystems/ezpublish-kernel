d6f07ee26d241751764bb16d1bbe3821ce4ef1dd
André R <ar@ez.no>
2016-04-27 14:24:27 +0200

    Improve mysql performance by running on tempfs, and use utf8mb4

--- bin/.travis/prepare_unittest.sh
+++ bin/.travis/prepare_unittest.sh
@@ -10,7 +10,21 @@ if [ "$TRAVIS_PHP_VERSION" != "hhvm" ] ; then
 fi
 
 # Setup DB
-if [ "$DB" = "mysql" ] ; then mysql -e "CREATE DATABASE IF NOT EXISTS testdb DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;" -uroot ; fi
+if [ "$DB" = "mysql" ] ; then
+    # https://github.com/travis-ci/travis-ci/issues/3049
+    # make sure we don't run out of entropy apparently (see link above)
+    sudo apt-get -y install haveged
+    sudo service haveged start
+    # make tmpfs and run MySQL on it for reasonable performance
+    sudo mkdir /mnt/ramdisk
+    sudo mount -t tmpfs -o size=1024m tmpfs /mnt/ramdisk
+    sudo stop mysql
+    sudo mv /var/lib/mysql /mnt/ramdisk
+    sudo ln -s /mnt/ramdisk/mysql /var/lib/mysql
+    sudo start mysql
+    # Install test db
+    mysql -e "CREATE DATABASE IF NOT EXISTS testdb DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci;" -uroot
+fi
 if [ "$DB" = "postgresql" ] ; then psql -c "CREATE DATABASE testdb;" -U postgres ; psql -c "CREATE EXTENSION pgcrypto;" -U postgres testdb ; fi
 
 # Setup GitHub key to avoid api rate limit (pure auth read only key, no rights, for use by ezsystems repos only!)
