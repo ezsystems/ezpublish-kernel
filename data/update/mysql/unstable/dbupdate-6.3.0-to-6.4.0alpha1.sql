c8115b10402e32884073eb9005241aa6ff85996a
Gunnstein Lye <gunnstein.lye@ez.no>
2016-06-17 10:16:43 +0200

    fixup! Fix EZP-25817: Invalid subtree path when assigning role with subtree limitation

---
+++ data/update/mysql/unstable/dbupdate-6.3.0-to-6.4.0alpha1.sql
@@ -0,0 +1,9 @@
+SET default_storage_engine=InnoDB;
+-- Set storage engine schema version number
+UPDATE ezsite_data SET value='6.4.0' WHERE name='ezpublish-version';
+
+--
+-- EZP-25817: Invalid subtree path when assigning role with subtree limitation
+--
+
+UPDATE ezuser_role SET limit_value = CONCAT(limit_value, '/') WHERE limit_identifier = 'Subtree' AND limit_value REGEXP '[[:digit:]]$';
