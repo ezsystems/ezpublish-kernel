c19d12b3b985451d61b4f3df8956cca40c7eb0c2
Gunnstein Lye <gunnstein.lye@ez.no>
2016-06-17 13:01:33 +0200

    fixup! Fix EZP-25817: Invalid subtree path when assigning role with subtree limitation

--- data/update/mysql/dbupdate-6.3.0-to-6.4.0.sql
+++ data/update/mysql/dbupdate-6.3.0-to-6.4.0.sql
@@ -3,7 +3,7 @@ SET default_storage_engine=InnoDB;
 UPDATE ezsite_data SET value='6.4.0' WHERE name='ezpublish-version';
 
 --
--- EZP-25880: Make ezuser.login case in-sensitive across databases, using case in-sensitive index
+-- EZP-25817: Invalid subtree path when assigning role with subtree limitation
 --
 
-ALTER TABLE ezuser DROP COLUMN login_normalized, ADD UNIQUE KEY ezuser_login (login);
+UPDATE ezuser_role SET limit_value = CONCAT(limit_value, '/') WHERE limit_identifier = 'Subtree' AND limit_value REGEXP '[[:digit:]]$';
