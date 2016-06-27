11dd90d235f0a2ebe0af40eda8470d588e21125e
André R <ar@ez.no>
2016-06-27 13:37:28 +0200

    EZP-25880: Fix update sql for Postgres

--- data/update/postgres/dbupdate-5.4.0-to-6.4.0.sql
+++ data/update/postgres/dbupdate-5.4.0-to-6.4.0.sql
@@ -5,4 +5,5 @@ UPDATE ezsite_data SET value='6.4.0' WHERE name='ezpublish-version';
 -- EZP-25880: Make ezuser.login case in-sensitive across databases, using case in-sensitive index
 --
 
-ALTER TABLE ezuser DROP CONSTRAINT ezuser_login, ADD CONSTRAINT ezuser_login UNIQUE KEY ((lower(login)));
+ALTER TABLE ezuser DROP CONSTRAINT ezuser_login;
+CREATE UNIQUE INDEX ezuser_login ON ezuser USING btree ((lower(login)));
