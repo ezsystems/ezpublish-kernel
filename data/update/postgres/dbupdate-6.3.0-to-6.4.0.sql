3311893bb4347a8c35d9c1036b73590fe10167ba
André R <andre.romcke@gmail.com>
2016-06-27 16:55:36 +0200

    EZP-25880: Fix update sql for Postgres (#1700)
    * EZP-25880: Fix update sql for Postgres
    
    * Clean data, bump version number

--- data/update/postgres/dbupdate-6.3.0-to-6.4.0.sql
+++ data/update/postgres/dbupdate-6.3.0-to-6.4.0.sql
@@ -5,7 +5,8 @@ UPDATE ezsite_data SET value='6.4.0' WHERE name='ezpublish-version';
 -- EZP-25880: Make ezuser.login case in-sensitive across databases, using case in-sensitive index
 --
 
-ALTER TABLE ezuser DROP CONSTRAINT ezuser_login, ADD CONSTRAINT ezuser_login UNIQUE KEY ((lower(login)));
+ALTER TABLE ezuser DROP COLUMN login_normalized;
+CREATE UNIQUE INDEX ezuser_login ON ezuser USING btree ((lower(login)));
 
 --
 -- EZP-25817: Invalid subtree path when assigning role with subtree limitation
