2bdb7147619a0d0a03111efbf2f0262bcfe15d20
Gunnstein Lye <gunnstein.lye@ez.no>
2016-07-28 14:23:23 +0200

    Fixed wrong class name in doc

--- eZ/Publish/Core/REST/Client/Output/ValueObjectVisitor/ViewInput.php
+++ eZ/Publish/Core/REST/Client/Output/ValueObjectVisitor/ViewInput.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * File containing the BadRequestException ValueObjectVisitor class.
+ * File containing the ViewInput ValueObjectVisitor class.
  *
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
