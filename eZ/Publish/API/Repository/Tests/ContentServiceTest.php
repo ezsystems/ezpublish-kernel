956160c063b89559acd891e5d94e9b855e156801
André R <ar@ez.no>
2016-09-05 17:22:07 +0200

    EZP-25679: Add missing refresh search

--- eZ/Publish/API/Repository/Tests/ContentServiceTest.php
+++ eZ/Publish/API/Repository/Tests/ContentServiceTest.php
@@ -4753,6 +4753,8 @@ class ContentServiceTest extends BaseContentServiceTest
         // Rollback all changes
         $repository->rollback();
 
+        $this->refreshSearch($repository);
+
         // This array will only contain a single admin user object
         $locations = $locationService->loadLocationChildren(
             $locationService->loadLocation($locationId)
