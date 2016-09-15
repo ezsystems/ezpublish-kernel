9e0cbed30b84952c61701876a14dd7979d7f2ddc
Petar Španja <petar@spanja.info>
2016-09-15 13:38:06 +0200

    Add tests for cleanup on URL alias publish

---
+++ eZ/Publish/Core/Persistence/Legacy/Tests/Content/UrlAlias/_fixtures/urlaliases_cleanup_nop.php
@@ -0,0 +1,99 @@
+<?php
+
+return array(
+    'ezurlalias_ml' => array(
+        0 => array(
+            'action' => 'eznode:2',
+            'action_type' => 'eznode',
+            'alias_redirects' => '1',
+            'id' => '1',
+            'is_alias' => '0',
+            'is_original' => '1',
+            'lang_mask' => '3',
+            'link' => '1',
+            'parent' => '0',
+            'text' => '',
+            'text_md5' => 'd41d8cd98f00b204e9800998ecf8427e',
+        ),
+        1 => array(
+            'action' => 'eznode:314',
+            'action_type' => 'eznode',
+            'alias_redirects' => '1',
+            'id' => '2',
+            'is_alias' => '0',
+            'is_original' => '1',
+            'lang_mask' => '2',
+            'link' => '2',
+            'parent' => '0',
+            'text' => 'jedan',
+            'text_md5' => '6896260129051a949051c3847c34466f',
+        ),
+        3 => array(
+            'action' => 'eznode:314',
+            'action_type' => 'eznode',
+            'alias_redirects' => '1',
+            'id' => '2',
+            'is_alias' => '0',
+            'is_original' => '1',
+            'lang_mask' => '4',
+            'link' => '2',
+            'parent' => '0',
+            'text' => 'dva',
+            'text_md5' => 'c67ed9a09ab136fae610b6a087d82e21',
+        ),
+        4 => array(
+            'action' => 'nop:',
+            'action_type' => 'nop',
+            'alias_redirects' => '1',
+            'id' => '3',
+            'is_alias' => '0',
+            'is_original' => '0',
+            'lang_mask' => '1',
+            'link' => '3',
+            'parent' => '0',
+            'text' => 'nop-element',
+            'text_md5' => 'de55c2fff721217cc4cb67b58dc35f85',
+        ),
+        5 => array(
+            'action' => 'module:content/search',
+            'action_type' => 'module',
+            'alias_redirects' => '0',
+            'id' => '4',
+            'is_alias' => '1',
+            'is_original' => '1',
+            'lang_mask' => '4',
+            'link' => '4',
+            'parent' => '3',
+            'text' => 'search',
+            'text_md5' => '06a943c59f33a34bb5924aaf72cd2995',
+        ),
+    ),
+    'ezcontent_language' => array(
+        0 => array(
+            'disabled' => 0,
+            'id' => 2,
+            'locale' => 'cro-HR',
+            'name' => 'Croatian (Hrvatski)'
+        ),
+        1 => array(
+            'disabled' => 0,
+            'id' => 4,
+            'locale' => 'eng-GB',
+            'name' => 'English (United Kingdom)'
+        ),
+    ),
+    'ezurlalias_ml_incr' => array(
+        0 => array(
+            'id' => '1',
+        ),
+        1 => array(
+            'id' => '2',
+        ),
+        2 => array(
+            'id' => '3',
+        ),
+        3 => array(
+            'id' => '4',
+        ),
+    ),
+);
