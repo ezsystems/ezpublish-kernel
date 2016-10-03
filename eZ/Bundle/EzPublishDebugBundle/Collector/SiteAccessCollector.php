18054cb1e46018401b1afee6a157cf04cf341fa5
Douglas Hammond <wizhippo@gmail.com>
2016-10-03 09:14:20 -0400

    Get siteaccess from request.

--- eZ/Bundle/EzPublishDebugBundle/Collector/SiteAccessCollector.php
+++ eZ/Bundle/EzPublishDebugBundle/Collector/SiteAccessCollector.php
@@ -1,13 +1,5 @@
 <?php
 
-/**
- * This file is part of the eZ Publish Kernel package.
- *
- * @copyright Copyright (C) eZ Systems AS. All rights reserved.
- * @license For full copyright and license information view LICENSE file distributed with this source code.
- *
- * @version //autogentag//
- */
 namespace eZ\Bundle\EzPublishDebugBundle\Collector;
 
 use eZ\Publish\Core\MVC\Symfony\SiteAccess;
@@ -20,20 +12,10 @@ use Symfony\Component\HttpKernel\DataCollector\DataCollector;
  */
 class SiteAccessCollector extends DataCollector
 {
-    /**
-     * @var SiteAccess
-     */
-    private $siteAccess;
-
-    public function __construct(SiteAccess $siteAccess)
-    {
-        $this->siteAccess = $siteAccess;
-    }
-
     public function collect(Request $request, Response $response, \Exception $exception = null)
     {
         $this->data = [
-            'siteAccess' => $this->siteAccess,
+            'siteAccess' => $request->attributes->get('siteaccess'),
         ];
     }
 
