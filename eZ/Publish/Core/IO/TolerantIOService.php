1a5f7d76a30443262025bf3735ff316ef8438353
Gunnstein Lye <gunnstein.lye@ez.no>
2016-10-05 15:02:42 +0200

    Fix EZP-26175: Exception instead of 404 when trying to access a file stored with a non default vardir
    (cherry picked from commit 058084ade8b6f5ffc2c093a6dec9f236cbbe48c7)

--- eZ/Publish/Core/IO/TolerantIOService.php
+++ eZ/Publish/Core/IO/TolerantIOService.php
@@ -8,6 +8,7 @@
  */
 namespace eZ\Publish\Core\IO;
 
+use eZ\Publish\Core\Base\Exceptions\InvalidArgumentException;
 use eZ\Publish\Core\IO\Exception\BinaryFileNotFoundException;
 use eZ\Publish\Core\IO\Exception\InvalidBinaryFileIdException;
 use eZ\Publish\Core\IO\Values\BinaryFile;
@@ -94,7 +95,14 @@ class TolerantIOService extends IOService
 
     public function loadBinaryFileByUri($binaryFileUri)
     {
-        $binaryFileId = $this->removeUriPrefix($this->binarydataHandler->getIdFromUri($binaryFileUri));
+        try {
+            $binaryFileId = $this->removeUriPrefix($this->binarydataHandler->getIdFromUri($binaryFileUri));
+        } catch (InvalidArgumentException $e) {
+            $this->logMissingFile($binaryFileUri);
+
+            return new MissingBinaryFile(['uri' => $binaryFileUri]);
+        }
+
         try {
             return $this->loadBinaryFile($binaryFileId);
         } catch (BinaryFileNotFoundException $e) {
