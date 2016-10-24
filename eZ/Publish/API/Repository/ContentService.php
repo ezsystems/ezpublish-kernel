9e4c341ee6390a6713897b3dbf515de21fc41f64
André R <ar@ez.no>
2016-10-24 10:45:01 +0200

    EZP-23281: Add max archive versions limit & enforce on publish

--- eZ/Publish/API/Repository/ContentService.php
+++ eZ/Publish/API/Repository/ContentService.php
@@ -269,6 +269,10 @@ interface ContentService
     /**
      * Publishes a content version.
      *
+     * Publishes a content version and deletes archive versions if they overflow max archive versions.
+     * Max archive versions are currently a configuration for default max limit, by default set to 5.
+     * @todo Introduce null|int ContentType->versionArchiveLimit to be able to let admins override this per type.
+     *
      * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException if the user is not allowed to publish this version
      * @throws \eZ\Publish\API\Repository\Exceptions\BadStateException if the version is not a draft
      *
--- eZ/Publish/Core/Persistence/Legacy/Content/Gateway/ExceptionConversion.php
+++ eZ/Publish/Core/Persistence/Legacy/Content/Gateway/ExceptionConversion.php
@@ -367,13 +367,15 @@ class ExceptionConversion extends Gateway
      * Result is returned with oldest version first (using version id as it has index and is auto increment).
      *
      * @param mixed $contentId
+     * @param mixed|null $status Optional argument to filter versions by status, like {@see VersionInfo::STATUS_ARCHIVED}.
+     * @param int $limit Limit for items returned, -1 means none.
      *
      * @return string[][]
      */
-    public function listVersions($contentId)
+    public function listVersions($contentId, $status = null, $limit = -1)
     {
         try {
-            return $this->innerGateway->listVersions($contentId);
+            return $this->innerGateway->listVersions($contentId, $status, $limit);
         } catch (DBALException $e) {
             throw new RuntimeException('Database error', 0, $e);
         } catch (PDOException $e) {
