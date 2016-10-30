0854459e6b107597b64853d18f36916e3a8bafe3
SKrimo <ksa@techolab.tips>
2016-10-31 00:49:50 +0100

    Fix EZP-23284 : Add StringProcessor which cast value on preprocess

---
+++ eZ/Publish/Core/REST/Common/FieldTypeProcessor/StringProcessor.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * File containing the TextLineProcessor class.
+*
+* @copyright Copyright (C) eZ Systems AS. All rights reserved.
+* @license For full copyright and license information view LICENSE file distributed with this source code.
+*
+* @version //autogentag//
+*/
+namespace eZ\Publish\Core\REST\Common\FieldTypeProcessor;
+
+use eZ\Publish\Core\REST\Common\FieldTypeProcessor;
+
+class StringProcessor extends FieldTypeProcessor
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public function preProcessValueHash($incomingValueHash)
+	{
+		return (string)$incomingValueHash;
+	}
+	
+}
