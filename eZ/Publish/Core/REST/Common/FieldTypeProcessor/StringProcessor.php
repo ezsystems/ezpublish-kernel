e2807ef33665bf35d8476091aa10cdd7c6928a16
SKrimo <ksa@techolab.tips>
2016-10-31 11:36:32 +0100

    Change the phpdoc StringProcessor

--- eZ/Publish/Core/REST/Common/FieldTypeProcessor/StringProcessor.php
+++ eZ/Publish/Core/REST/Common/FieldTypeProcessor/StringProcessor.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * File containing the TextLineProcessor class.
+ * File containing the StringProcessor class.
  *
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
