6433a2d59015887219ca13b84ff2b4b3b6c5d43f
Nicolas Bastien <nicolas.bastien@ez.no>
2017-01-17 12:06:01 +0100

    EZP-26835 : Script to extract translations

--- bin/extract-translations.sh
+++ bin/extract-translations.sh
@@ -6,29 +6,10 @@ echo '# Extract Kernel : EzPublishCoreBundle';
 ./app/console translation:extract en -v \
   --dir=./vendor/ezsystems/ezpublish-kernel/eZ \
   --exclude-dir=Bundle/PlatformBehatBundle \
-  --exclude-dir=Bundle/EzPublishCoreBundle/Tests \
-  --exclude-dir=Bundle/EzPublishCoreBundle/Features \
-  --exclude-dir=Bundle/EzPublishDebugBundle/Tests \
-  --exclude-dir=Bundle/EzPublishIOBundle/Tests \
-  --exclude-dir=Bundle/EzPublishRestBundle/Tests \
-  --exclude-dir=Bundle/EzPublishRestBundle/Features \
-  --exclude-dir=Publish/API/Repository/Tests \
+  --exclude-dir=Tests \
+  --exclude-dir=Features \
   --exclude-dir=Publish/Core/REST/Client \
-  --exclude-dir=Publish/Core/Base/Tests \
-  --exclude-dir=Publish/Core/FieldType/Tests \
-  --exclude-dir=Publish/Core/Helper/Tests \
-  --exclude-dir=Publish/Core/IO/Tests \
-  --exclude-dir=Publish/Core/Limitation/Tests \
-  --exclude-dir=Publish/Core/Pagination/Tests \
-  --exclude-dir=Publish/Core/Persistence/Tests \
-  --exclude-dir=Publish/Core/Persistence/Legacy/Tests \
-  --exclude-dir=Publish/Core/Repository/Tests \
-  --exclude-dir=Publish/Core/REST/Tests \
-  --exclude-dir=Publish/Core/Search/Tests \
-  --exclude-dir=Publish/Core/Search/Legacy/Tests \
-  --exclude-dir=Publish/Core/SignalSlot/Tests \
-  --exclude-dir=Publish/Core/settings/tests \
-  --exclude-dir=Publish/SPI/Tests \
+  --exclude-dir=tests \
   --output-dir=./vendor/ezsystems/ezpublish-kernel/eZ/Bundle/EzPublishCoreBundle/Resources/translations \
   --enable-extractor=ez_fieldtypes \
   --keep
