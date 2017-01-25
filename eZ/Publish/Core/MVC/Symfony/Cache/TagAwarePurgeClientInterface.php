2d9e0a1a558eea543a29e900019e65f43a8f56e7
Bertrand Dunogier <bertrand.dunogier@ez.no>
2017-01-25 18:48:58 +0100

    fixup! Multi-tagging purge client

--- eZ/Publish/Core/MVC/Symfony/Cache/TagAwarePurgeClientInterface.php
+++ eZ/Publish/Core/MVC/Symfony/Cache/TagAwarePurgeClientInterface.php
@@ -14,17 +14,6 @@ namespace eZ\Publish\Core\MVC\Symfony\Cache;
 interface TagAwarePurgeClientInterface
 {
     /**
-     * Triggers the cache purge $locationIds.
-     *
-     * It's up to the implementor to decide whether to purge $locationIds right away or to delegate to a separate process.
-     *
-     * @deprecate Since 6.8, use {@link purgeByTags()}
-     *
-     * @param array $locationIds Cache resource(s) to purge (e.g. array of URI to purge in a reverse proxy)
-     */
-    public function purge($locationIds);
-
-    /**
      * Triggers the cache purge/invalidation of cache by $tags.
      *
      * See doc/specifications/cache/multi_tagging.md for list of system tags and conventions.
@@ -34,13 +23,4 @@ interface TagAwarePurgeClientInterface
      * @param string[] $tags Tags that
      */
     public function purgeByTags(array $tags);
-
-    /**
-     * Purges all content elements currently in cache.
-     *
-     * It's up to the implementor to decide whether to purge $locationIds right away or to delegate to a separate process.
-     *
-     * @deprecated Use cache:clear, with multi tagging theoretically there shouldn't be need to delete all anymore from core.
-     */
-    public function purgeAll();
 }
