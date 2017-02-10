3582f10cd86dd9a1c93f8d213819823cfed29eb1
André R <andre.romcke@gmail.com>
2017-02-10 17:08:45 +0100

    [Doc] EZP-26957: Add performance tips in welcome message (#1886)
    * [Doc] Add performance tips in welcome message
    
    * Proof-Read
    
    Small corrections
    
    * Typo
    
    * Add link for further reading and simplify text

--- eZ/Bundle/EzPublishCoreBundle/Composer/ScriptHandler.php
+++ eZ/Bundle/EzPublishCoreBundle/Composer/ScriptHandler.php
@@ -130,6 +130,12 @@ Note:
 - For development use you can enable full debugging by setting SYMFONY_ENV to "dev".
 - Last command will give you url to frontend of installation, add "/ez" to reach backend.
 
+Performance tips:
+- Use PHP 7.0 (or better yet PHP 7.1), and make sure opcache is enabled.
+- Dump autoload class map, even in development: <fg=green>composer dump-autoload --optimize</>
+- Follow full install instructions below to setup nginx (or Apache).
+- Further reading: https://doc.ez.no/display/DEVELOPER/Performance
+
 For full install instructions, including setting up directory permissions, see install instructions in <fg=green>INSTALL.md</>
 or <fg=green>README.md</>.
 
