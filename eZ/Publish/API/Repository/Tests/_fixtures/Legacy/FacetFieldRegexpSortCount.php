c6c329c12fb3311a93a8099d0cf84ceffad8a82e
André R <ar@ez.no>
2017-04-26 09:18:51 +0200

    [Integration] Now that we expect LegacySE to ignore facetBuilders, record this to get it tested

---
+++ eZ/Publish/API/Repository/Tests/_fixtures/Legacy/FacetFieldRegexpSortCount.php
@@ -0,0 +1,40 @@
+<?php
+
+return eZ\Publish\API\Repository\Values\Content\Search\SearchResult::__set_state(array(
+   'facets' => 
+  array (
+  ),
+   'searchHits' => 
+  array (
+    0 => 
+    eZ\Publish\API\Repository\Values\Content\Search\SearchHit::__set_state(array(
+       'valueObject' => 
+      array (
+        'id' => 57,
+        'title' => 'Home',
+      ),
+       'score' => NULL,
+       'index' => NULL,
+       'matchedTranslation' => 'eng-GB',
+       'highlight' => NULL,
+    )),
+    1 => 
+    eZ\Publish\API\Repository\Values\Content\Search\SearchHit::__set_state(array(
+       'valueObject' => 
+      array (
+        'id' => 58,
+        'title' => 'Contact Us',
+      ),
+       'score' => NULL,
+       'index' => NULL,
+       'matchedTranslation' => 'eng-GB',
+       'highlight' => NULL,
+    )),
+  ),
+   'spellSuggestion' => NULL,
+   'time' => 1,
+   'timedOut' => NULL,
+   'maxScore' => NULL,
+   'totalCount' => 2,
+));
+
