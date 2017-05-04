5cb30b003085eb1397e39e0aee7c3fb786d5e39a
Adam Wójs <adam.wojs@ez.no>
2017-05-04 10:58:01 +0200

    Fix EZP-27254: Command to clean up invalid relations causing "Unknown relation type 0." exception

---
+++ eZ/Bundle/EzPublishMigrationBundle/Command/LegacyStorage/CleanUpRelationTypeEqZeroCommand.php
@@ -0,0 +1,160 @@
+<?php
+
+/**
+ * This file is part of the eZ Publish Kernel package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace eZ\Bundle\EzPublishMigrationBundle\Command\LegacyStorage;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use RuntimeException;
+
+class CleanUpRelationTypeEqZeroCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('ezpublish:update:legacy_storage_clean_up_relation_type_eq_zero')
+            ->setDescription('Clean up invalid relation type in Legacy Storage database')
+            ->addArgument('action', InputArgument::REQUIRED, 'Action: ' . implode(', ', $this->getAvailableActions()))
+            ->setHelp(
+                <<<EOT
+The command <info>%command.name%</info> cleans up corrupted relations in Legacy Storage database. See: https://jira.ez.no/browse/EZP-27254
+
+<warning>During the script execution the database should not be modified.
+
+To avoid surprises you are advised to create a backup or execute a dry run:
+ 
+    %command.name% dry-run
+    
+before proceeding with actual update.</warning>
+
+Since this script can potentially run for a very long time, to avoid memory
+exhaustion run it in production environment using the <info>--env=prod</info> switch.
+EOT
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $action = $input->getArgument('action');
+
+        if (!$this->isValidAction($action)) {
+            throw new RuntimeException(
+                "Action '{$action}' is not supported, use one of: " .
+                implode(', ', $this->getAvailableActions())
+            );
+        }
+
+        $totalCount = $this->getTotalCount();
+        $output->writeln('Found total relations to delete: ' . $totalCount);
+        if ($totalCount == 0) {
+            $output->writeln('Nothing to process, exiting.');
+
+            return;
+        }
+
+        if ($this->confirmExecution($input, $output)) {
+            switch ($action) {
+                case 'fix':
+                    $this->executeFix();
+                    break;
+                case 'list':
+                case 'dry-run':
+                    $this->executeList($output, $totalCount);
+                    break;
+            }
+        }
+
+        $output->writeln('');
+    }
+
+    protected function confirmExecution(InputInterface $input, OutputInterface $output)
+    {
+        $question = new ConfirmationQuestion('<question>Are you sure you want to proceed?</question> ', false);
+
+        return $this
+            ->getHelper('question')
+            ->ask($input, $output, $question);
+    }
+
+    protected function executeFix()
+    {
+        /** @var \eZ\Publish\Core\Persistence\Database\DatabaseHandler $conn */
+        $conn = $this->getContainer()->get('ezpublish.connection');
+        $conn->exec('DELETE FROM ezcontentobject_link WHERE relation_type = 0');
+    }
+
+    protected function executeList(OutputInterface $output, $totalCount)
+    {
+        $output->writeln('The following relations will be removed: ');
+        $output->writeln("ID\tFCO_ID\tFCO_V\tTCO_ID\tRT");
+
+        $passSize = 1000;
+        $passCount = ceil($totalCount / $passSize);
+        for ($pass = 0; $pass <= $passCount; ++$pass) {
+            $relations = $this->getCorruptedRelations($pass * $passSize, $passSize);
+            foreach ($relations as $relation) {
+                $output->writeln(vsprintf("%s\t%s\t%s\t%s\t%s", $relation));
+            }
+            $relations = null;
+        }
+    }
+
+    protected function getCorruptedRelations($offset, $limit)
+    {
+        /** @var \eZ\Publish\Core\Persistence\Database\DatabaseHandler $connection */
+        $connection = $this->getContainer()->get('ezpublish.connection');
+
+        $query = $connection->createSelectQuery();
+        $query
+            ->select(
+                $connection->quoteColumn('id'),
+                $connection->quoteColumn('from_contentobject_id'),
+                $connection->quoteColumn('from_contentobject_version'),
+                $connection->quoteColumn('to_contentobject_id'),
+                $connection->quoteColumn('relation_type')
+            )
+            ->from('ezcontentobject_link')
+            ->where(
+                $query->expr->eq(
+                    $connection->quoteColumn('relation_type'),
+                    $query->bindValue('0')
+                )
+            )
+            ->orderBy('id', 'ASC')
+            ->limit($limit, $offset);
+
+        $stmt = $query->prepare();
+        $stmt->execute();
+
+        return $stmt->fetchAll(\PDO::FETCH_NUM);
+    }
+
+    protected function getTotalCount()
+    {
+        /** @var \eZ\Publish\Core\Persistence\Database\DatabaseHandler $conn */
+        $conn = $this->getContainer()->get('ezpublish.connection');
+
+        $stmt = $conn->prepare('SELECT COUNT(id) FROM ezcontentobject_link WHERE relation_type = 0');
+        $stmt->execute();
+
+        return (int)$stmt->fetchColumn();
+    }
+
+    protected function getAvailableActions()
+    {
+        return ['fix', 'list', 'dry-run'];
+    }
+
+    protected function isValidAction($action)
+    {
+        return in_array($action, $this->getAvailableActions());
+    }
+}
