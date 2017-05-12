144822e785cb705b2c513c74c4f3de60a4fcb3a0
Andrew Longosz <andrzej.longosz@ez.no>
2017-05-12 16:57:20 +0200

    EZP-26885: Deprecated getting GW from StorageHandler context

--- eZ/Publish/Core/FieldType/GatewayBasedStorage.php
+++ eZ/Publish/Core/FieldType/GatewayBasedStorage.php
@@ -11,9 +11,12 @@ namespace eZ\Publish\Core\FieldType;
 use eZ\Publish\SPI\FieldType\FieldStorage;
 use eZ\Publish\SPI\Persistence\Content\Field;
 use eZ\Publish\SPI\Persistence\Content\VersionInfo;
+use eZ\Publish\SPI\FieldType\GatewayBasedStorage as SPIGatewayBasedStorage;
 
 /**
  * Storage gateway base class to be used by FieldType storages.
+ *
+ * @deprecated Since 6.11. Use {@link \eZ\Publish\SPI\FieldType\GatewayBasedStorage}
  *
  * This class gives a common basis to realized gateway based storage
  * dispatching. It is intended to deal as a base class for FieldType storages,
@@ -39,6 +42,16 @@ abstract class GatewayBasedStorage implements FieldStorage
      */
     public function __construct(array $gateways = array())
     {
+        @trigger_error(
+            sprintf(
+                '%s extends deprecated %s. Extend %s instead',
+                static::class,
+                self::class,
+                SPIGatewayBasedStorage::class
+            ),
+            E_USER_DEPRECATED
+        );
+
         foreach ($gateways as $identifier => $gateway) {
             $this->addGateway($identifier, $gateway);
         }
@@ -58,12 +71,23 @@ abstract class GatewayBasedStorage implements FieldStorage
     /**
      * Retrieve the fitting gateway, base on the identifier in $context.
      *
+     * @deprecated Since 6.11. Retrieving gateway based on $context is deprecated
+     *             and will be removed in 7.0. Inject gateway directly into FieldStorage
+     *
      * @param array $context
      *
      * @return \eZ\Publish\Core\FieldType\StorageGateway
      */
     protected function getGateway(array $context)
     {
+        @trigger_error(
+            sprintf(
+                '%s: Retrieving gateway based on $context is deprecated and will be removed in 7.0. Inject gateway directly into FieldStorage',
+                get_class($this)
+            ),
+            E_USER_DEPRECATED
+        );
+
         if (!isset($this->gateways[$context['identifier']])) {
             throw new \OutOfBoundsException("No gateway for ${context['identifier']} available.");
         }
