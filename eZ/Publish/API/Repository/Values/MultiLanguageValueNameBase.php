037d752a6ba08b440be8305e1b760b59706c7bd2
André R <ar@ez.no>
2017-05-26 13:49:57 +0200

    [API] Reduce language base to what is common for most values: names, main language, always available & *new* user prioritized language match
    And split what is common for some, description, into owen base.

---
+++ eZ/Publish/API/Repository/Values/MultiLanguageValueNameBase.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * File containing the eZ\Publish\API\Repository\Values\MultiLanguageValueBase class.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace eZ\Publish\API\Repository\Values;
+
+/**
+ * This is the base class for all classes providing translated name logic.
+ *
+ * Provides a uniform way for API consuming logic to generate translated names / labels for API objects, language logic
+ * is meant to also be used for description, fields, .. lookup as well.
+ */
+abstract class MultiLanguageValueNameBase extends ValueObject
+{
+    /**
+     * This method returns the human readable name in all provided languages.
+     *
+     * The structure of the return value is:
+     * <code>
+     *     array( 'eng' => '<name_eng>', 'de' => '<name_de>' );
+     * </code>
+     *
+     * @return string[]
+     */
+    abstract public function getNames();
+
+    /**
+     * This method returns the name of the domain object in a given language.
+     *
+     * - If $languageCode is defined,
+     *      return if available, otherwise null
+     * - If not pick using the following languages codes when applicable:
+     *      1. Prioritized languages (if provided to api on object retrieval)
+     *      2. Main language if object is $alwaysAvailable
+     *      3. Fallback to return in initial (version objects) or main language
+
+     *
+     * @param string|null $languageCode
+     *
+     * @return string The name for a given language, or null if $languageCode is set and does not exist.
+     */
+    abstract public function getName($languageCode = null);
+}
