c54157392caf956a0a97ea4ba961add669407929
Tobias Schlitt <toby@php.net>
2017-06-20 14:43:14 +0200

    Basic version of ResourceResolverFactory.
    This creates a ResourceResolver with required loading callbacks
    configured.

---
+++ eZ/Publish/Core/REST/Server/ResourceResolverFactory.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace eZ\Publish\Core\REST\Server;
+
+use eZ\Publish\Core\REST\Common\RequestParser;
+
+use eZ\Publish\API\Repository\ContentTypeService;
+use eZ\Publish\API\Repository\SectionService;
+use eZ\Publish\API\Repository\UserService;
+
+class ResourceResolverFactory
+{
+    private $requestParser;
+
+    private $contentTypeService;
+
+    private $sectionService;
+
+    private $userService;
+
+    /**
+     * @param \eZ\Publish\Core\REST\Common\RequestParser $requestParser
+     * @param \eZ\Publish\API\Repository\ContentTypeService $contentTypeService
+     * @param \eZ\Publish\API\Repository\SectionService $sectionService
+     * @param \eZ\Publish\API\Repository\UserService $userService
+     */
+    public function __construct(
+        RequestParser $requestParser,
+        ContentTypeService $contentTypeService,
+        SectionService $sectionService,
+        UserService $userService
+    ) {
+        $this->requestParser = $requestParser;
+
+        $this->contentTypeService = $contentTypeService;
+        $this->sectionService = $sectionService;
+        $this->userService = $userService;
+    }
+
+    /**
+     * @return \eZ\Publish\Core\REST\Server\ResourceResolver
+     */
+    public function createResolver()
+    {
+        $contentTypeService = $this->contentTypeService;
+        $sectionService = $this->sectionService;
+        $userService = $this->userService;
+
+        return new ResourceResolver(
+            $this->requestParser,
+            [
+                'type' => function ($uriParameters) use ($contentTypeService) {
+                    return $contentTypeService->loadContentType($uriParameters['type']);
+                },
+                'typeByIdentifier' => function ($uriParameters) use ($contentTypeService) {
+                    return $contentTypeService->loadContentTypeByIdentifier($uriParameters['type']);
+                },
+                'typeByRemoteId' => function ($uriParameters) use ($contentTypeService) {
+                    return $contentTypeService->loadContentTypeByRemoteId($uriParameters['type']);
+                },
+
+                // TODO: URLAliases still need an alias URI (SIC ;))
+
+                'section' => function ($uriParameters) use ($sectionService) {
+                    return $sectionService->loadSection($uriParameters['section']);
+                },
+                'sectionByIdentifier' => function ($uriParameters) use ($sectionService) {
+                    return $sectionService->loadSectionByIdentifier($uriParameters['section']);
+                },
+
+                'user' => function ($uriParameters) use ($userService) {
+                    return $userService->loadUser($uriParameters['user']);
+                },
+                // TODO: User by Login missing in URL mapping
+            ]
+        );
+    }
+}
