21f820f1bb56dc4f0e680ee85aebc06c2310636d
Andrew Longosz <andrzej.longosz@ez.no>
2017-06-22 10:48:09 +0200

    [PAPI] EZP-27417: Added ContentService::removeTranslation method

--- eZ/Publish/API/Repository/ContentService.php
+++ eZ/Publish/API/Repository/ContentService.php
@@ -407,6 +407,26 @@ interface ContentService
     public function loadTranslationInfos(ContentInfo $contentInfo, array $filter = array());
 
     /**
+     * Remove Content Object translation from all Versions (including archived ones) of a Content Object.
+     *
+     * NOTE: this operation is risky and permanent, so user interface (ideally CLI) should provide
+     *       a warning before performing it.
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\BadStateException if the specified translation
+     *         is the only one a Version has or it is the main translation of a Content Object.
+     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException if the user is not allowed
+     *         to delete the content (in one of the locations of the given Content Object).
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException if languageCode argument
+     *         is invalid for the given content.
+     *
+     * @param \eZ\Publish\API\Repository\Values\Content\ContentInfo $contentInfo
+     * @param string $languageCode
+     *
+     * @since 6.11
+     */
+    public function removeTranslation(ContentInfo $contentInfo, $languageCode);
+
+    /**
      * Instantiates a new content create struct object.
      *
      * alwaysAvailable is set to the ContentType's defaultAlwaysAvailable
