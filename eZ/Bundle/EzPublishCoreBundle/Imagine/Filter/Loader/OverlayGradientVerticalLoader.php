44dc5e72881e212e7da3cb38e80f00a750122f0b
Ternovtsiiendry <at@conciliolabs.com>
2017-07-28 15:59:44 +0300

    Update OverlayGradientVerticalLoader.php

--- eZ/Bundle/EzPublishCoreBundle/Imagine/Filter/Loader/OverlayGradientVerticalLoader.php
+++ eZ/Bundle/EzPublishCoreBundle/Imagine/Filter/Loader/OverlayGradientVerticalLoader.php
@@ -14,20 +14,20 @@ class OverlayGradientVerticalLoader extends FilterLoaderWrapped
             throw new InvalidArgumentException('Missing opacity and/or colors option(s)');
         }
 
-        if (isset($options['opacity'])){
+        if (isset($options['opacity'])) {
             $options[0] = $options['opacity'];
         }
-        if (isset($options['start_color'])){
+        if (isset($options['start_color'])) {
             $options[1] = $options['start_color'];
         }
-        if (isset($options['end_color'])){
+        if (isset($options['end_color'])) {
             $options[2] = $options['end_color'];
         }
 
         return $this->innerLoader->load($image, [
-            'opacity'    => $options[0],
+            'opacity' => $options[0],
             'startColor' => $options[1],
-            'endColor'   => $options[2],
+            'endColor' => $options[2],
             'linerClass' => Vertical::class
         ]);
     }
