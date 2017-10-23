98fb9b1f9cc78e24188d84479f8fd154e56781e6
Kamil Madejski <kamil.madejski@ez.no>
2017-10-23 10:52:16 +0200

    EZP-25487: imageAlias missing width and height parameters

---
+++ eZ/Bundle/EzPublishCoreBundle/Imagine/AliasGeneratorDecorator.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace eZ\Bundle\EzPublishCoreBundle\Imagine;
+
+use eZ\Publish\API\Repository\Values\Content\Field;
+use eZ\Publish\API\Repository\Values\Content\VersionInfo;
+use eZ\Publish\SPI\Variation\VariationHandler;
+use Stash\Interfaces\PoolInterface;
+
+class AliasGeneratorDecorator
+{
+    /**
+     * @var VariationHandler
+     */
+    private $aliasGenerator;
+
+    /**
+     * @var PoolInterface
+     */
+    private $cache;
+
+    public function __construct(VariationHandler $aliasGenerator, PoolInterface $cache)
+    {
+        $this->aliasGenerator = $aliasGenerator;
+        $this->cache = $cache;
+    }
+
+    public function getVariation(Field $field, VersionInfo $versionInfo, $variationName, array $parameters = array())
+    {
+        $item = $this->cache->getItem($this->getCacheKey($field, $versionInfo, $variationName));
+        if ($item->isMiss()) {
+            $image = $this->aliasGenerator->getVariation($field, $versionInfo, $variationName, $parameters);
+            $item->save($image);
+        }
+        return $item->get();
+    }
+
+    private function getCacheKey(Field $field, VersionInfo $versionInfo, $variationName)
+    {
+        return 'variation/' . $field->value . '/' . $field->id . '/' . $field->fieldDefIdentifier . '/' . $versionInfo->id . '/' . $variationName;
+    }
+}
