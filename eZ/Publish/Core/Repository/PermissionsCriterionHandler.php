1ba74474a7ae0f0d942961e9601004d06676b6a0
André R <andre.romcke@gmail.com>
2017-11-08 12:18:22 +0100

    EZP-28143: getPermissionCriterion() should be cached per user (#2126)
    * EZP-28143: getPermissionCriterion() should be cached per user
    
    * Add Tests

--- eZ/Publish/Core/Repository/PermissionsCriterionHandler.php
+++ eZ/Publish/Core/Repository/PermissionsCriterionHandler.php
@@ -10,45 +10,19 @@ namespace eZ\Publish\Core\Repository;
 
 use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
 use eZ\Publish\API\Repository\Values\Content\Query\Criterion\LogicalAnd;
-use eZ\Publish\API\Repository\Values\Content\Query\Criterion\LogicalOr;
-use eZ\Publish\API\Repository\Values\User\Limitation;
-use eZ\Publish\API\Repository\PermissionResolver as PermissionResolverInterface;
-use eZ\Publish\Core\Repository\Helper\LimitationService;
-use RuntimeException;
+use eZ\Publish\Core\Repository\Permission\PermissionCriterionResolver;
 
 /**
  * Handler for permissions Criterion.
+ *
+ * @deprecated 6.7.7
  */
-class PermissionsCriterionHandler
+class PermissionsCriterionHandler extends PermissionCriterionResolver
 {
-    /**
-     * @var \eZ\Publish\API\Repository\PermissionResolver
-     */
-    private $permissionResolver;
-
-    /**
-     * @var \eZ\Publish\Core\Repository\Helper\LimitationService
-     */
-    private $limitationService;
-
-    /**
-     * Constructor.
-     *
-     * @param \eZ\Publish\API\Repository\PermissionResolver $permissionResolver
-     * @param \eZ\Publish\Core\Repository\Helper\LimitationService $limitationService
-     */
-    public function __construct(
-        PermissionResolverInterface $permissionResolver,
-        LimitationService $limitationService
-    ) {
-        $this->permissionResolver = $permissionResolver;
-        $this->limitationService = $limitationService;
-    }
-
     /**
      * Adds content, read Permission criteria if needed and return false if no access at all.
      *
-     * @uses ::getPermissionsCriterion()
+     * @uses \eZ\Publish\Core\Repository\Permission\PermissionCriterionResolver::getPermissionsCriterion()
      *
      * @param \eZ\Publish\API\Repository\Values\Content\Query\Criterion $criterion
      *
@@ -74,98 +48,5 @@ class PermissionsCriterionHandler
         }
 
         return true;
-    }
-
-    /**
-     * Get content-read Permission criteria if needed and return false if no access at all.
-     *
-     * @uses \eZ\Publish\API\Repository\PermissionResolver::hasAccess()
-     *
-     * @throws \RuntimeException If empty array of limitations are provided from hasAccess()
-     *
-     * @param string $module
-     * @param string $function
-     *
-     * @return bool|\eZ\Publish\API\Repository\Values\Content\Query\Criterion
-     */
-    public function getPermissionsCriterion($module = 'content', $function = 'read')
-    {
-        $permissionSets = $this->permissionResolver->hasAccess($module, $function);
-        if ($permissionSets === false || $permissionSets === true) {
-            return $permissionSets;
-        }
-
-        if (empty($permissionSets)) {
-            throw new RuntimeException("Got an empty array of limitations from hasAccess( '{$module}', '{$function}' )");
-        }
-
-        /*
-         * RoleAssignment is a OR condition, so is policy, while limitations is a AND condition
-         *
-         * If RoleAssignment has limitation then policy OR conditions are wrapped in a AND condition with the
-         * role limitation, otherwise it will be merged into RoleAssignment's OR condition.
-         */
-        $currentUserRef = $this->permissionResolver->getCurrentUserReference();
-        $roleAssignmentOrCriteria = array();
-        foreach ($permissionSets as $permissionSet) {
-            // $permissionSet is a RoleAssignment, but in the form of role limitation & role policies hash
-            $policyOrCriteria = array();
-            /**
-             * @var \eZ\Publish\API\Repository\Values\User\Policy
-             */
-            foreach ($permissionSet['policies'] as $policy) {
-                $limitations = $policy->getLimitations();
-                if ($limitations === '*' || empty($limitations)) {
-                    // Given policy gives full access, optimize away all role policies (but not role limitation if any)
-                    // This should be optimized on create/update of Roles, however we keep this here for bc with older data
-                    $policyOrCriteria = [];
-                    break;
-                }
-
-                $limitationsAndCriteria = array();
-                foreach ($limitations as $limitation) {
-                    $type = $this->limitationService->getLimitationType($limitation->getIdentifier());
-                    $limitationsAndCriteria[] = $type->getCriterion($limitation, $currentUserRef);
-                }
-
-                $policyOrCriteria[] = isset($limitationsAndCriteria[1]) ?
-                    new LogicalAnd($limitationsAndCriteria) :
-                    $limitationsAndCriteria[0];
-            }
-
-            /**
-             * Apply role limitations if there is one.
-             *
-             * @var \eZ\Publish\API\Repository\Values\User\Limitation[]
-             */
-            if ($permissionSet['limitation'] instanceof Limitation) {
-                // We need to match both the limitation AND *one* of the policies, aka; roleLimit AND policies(OR)
-                $type = $this->limitationService->getLimitationType($permissionSet['limitation']->getIdentifier());
-                if (!empty($policyOrCriteria)) {
-                    $roleAssignmentOrCriteria[] = new LogicalAnd(
-                        array(
-                            $type->getCriterion($permissionSet['limitation'], $currentUserRef),
-                            isset($policyOrCriteria[1]) ? new LogicalOr($policyOrCriteria) : $policyOrCriteria[0],
-                        )
-                    );
-                } else {
-                    $roleAssignmentOrCriteria[] = $type->getCriterion($permissionSet['limitation'], $currentUserRef);
-                }
-            } elseif (!empty($policyOrCriteria)) {
-                // Otherwise merge $policyOrCriteria into $roleAssignmentOrCriteria
-                // There is no role limitation, so any of the policies can globally match in the returned OR criteria
-                $roleAssignmentOrCriteria = empty($roleAssignmentOrCriteria) ?
-                    $policyOrCriteria :
-                    array_merge($roleAssignmentOrCriteria, $policyOrCriteria);
-            }
-        }
-
-        if (empty($roleAssignmentOrCriteria)) {
-            return false;
-        }
-
-        return isset($roleAssignmentOrCriteria[1]) ?
-            new LogicalOr($roleAssignmentOrCriteria) :
-            $roleAssignmentOrCriteria[0];
     }
 }
