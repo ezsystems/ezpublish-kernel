bee33f41bd90d9763152a9b87770fef0aa67cbc5
Kamil Madejski <kamil.madejski@yahoo.com>
2017-11-20 17:07:31 +0100

    EZP-28210: Logout not working when session cookie domain set (#2155)
    (cherry picked from commit c696bc2a832e387977c4b888ed9a57fab5acbf91)

--- eZ/Publish/Core/REST/Server/Security/RestLogoutHandler.php
+++ eZ/Publish/Core/REST/Server/Security/RestLogoutHandler.php
@@ -8,6 +8,7 @@
  */
 namespace eZ\Publish\Core\REST\Server\Security;
 
+use eZ\Publish\Core\MVC\ConfigResolverInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
@@ -19,12 +20,41 @@ use Symfony\Component\Security\Http\Logout\LogoutHandlerInterface;
  */
 class RestLogoutHandler implements LogoutHandlerInterface
 {
+    /**
+     * @var \eZ\Publish\Core\MVC\ConfigResolverInterface
+     */
+    private $configResolver;
+
+    /**
+     * @param \eZ\Publish\Core\MVC\ConfigResolverInterface $configResolver
+     */
+    public function __construct(ConfigResolverInterface $configResolver)
+    {
+        $this->configResolver = $configResolver;
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param \Symfony\Component\HttpFoundation\Response $response
+     * @param \Symfony\Component\Security\Core\Authentication\Token\TokenInterface $token
+     */
     public function logout(Request $request, Response $response, TokenInterface $token)
     {
         if (!$request->attributes->get('is_rest_request')) {
             return;
         }
 
-        $response->headers->clearCookie($request->getSession()->getName());
+        $path = '/';
+        $domain = null;
+
+        $session = $this->configResolver->getParameter('session');
+        if (array_key_exists('cookie_domain', $session)) {
+            $domain = $session['cookie_domain'];
+        }
+        if (array_key_exists('cookie_path', $session)) {
+            $path = $session['cookie_path'];
+        }
+
+        $response->headers->clearCookie($request->getSession()->getName(), $path, $domain);
     }
 }
