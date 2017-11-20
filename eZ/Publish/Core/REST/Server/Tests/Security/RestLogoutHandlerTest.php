0300c96383d8f8f022b98710e581f7f5bbe8f783
Kamil Madejski <kamil.madejski@ez.no>
2017-11-20 13:09:35 +0100

    EZP-28210: Logout not working when session cookie domain set

--- eZ/Publish/Core/REST/Server/Tests/Security/RestLogoutHandlerTest.php
+++ eZ/Publish/Core/REST/Server/Tests/Security/RestLogoutHandlerTest.php
@@ -8,38 +8,96 @@
  */
 namespace eZ\Publish\Core\REST\Server\Tests\Security;
 
+use eZ\Publish\Core\Base\Tests\PHPUnit5CompatTrait;
+use eZ\Publish\Core\MVC\ConfigResolverInterface;
 use eZ\Publish\Core\REST\Server\Security\RestLogoutHandler;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
 class RestLogoutHandlerTest extends TestCase
 {
-    public function testLogout()
+    use PHPUnit5CompatTrait;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $configResolver;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $session;
+
+    protected function setUp()
     {
-        $session = $this->getMock('Symfony\Component\HttpFoundation\Session\SessionInterface');
+        parent::setUp();
+        $this->configResolver = $this->createMock(ConfigResolverInterface::class);
+        $this->session = $this->createMock(SessionInterface::class);
+    }
+
+    public function testLogoutWithoutSiteaccessSessionSettings()
+    {
         $sessionId = 'eZSESSID';
-        $session
+        $this->session
             ->expects($this->once())
             ->method('getName')
             ->will($this->returnValue($sessionId));
-
         $request = new Request();
-        $request->setSession($session);
+        $request->setSession($this->session);
         $request->attributes->set('is_rest_request', true);
-
+        $this->configResolver
+            ->expects($this->once())
+            ->method('getParameter')
+            ->with('session')
+            ->will($this->returnValue([]));
         $response = new Response();
-        $response->headers = $this->getMock('Symfony\Component\HttpFoundation\ResponseHeaderBag');
+        $response->headers = $this->createMock(ResponseHeaderBag::class);
         $response->headers
             ->expects($this->once())
             ->method('clearCookie')
             ->with($sessionId);
-
-        $logoutHandler = new RestLogoutHandler();
+        $logoutHandler = new RestLogoutHandler($this->configResolver);
         $logoutHandler->logout(
             $request,
             $response,
-            $this->getMock('Symfony\Component\Security\Core\Authentication\Token\TokenInterface')
+            $this->createMock(TokenInterface::class)
+        );
+    }
+
+    public function testLogoutWithSiteaccessSessionSettings()
+    {
+        $sessionId = 'eZSESSID';
+        $this->session
+            ->expects($this->once())
+            ->method('getName')
+            ->will($this->returnValue($sessionId));
+        $request = new Request();
+        $request->setSession($this->session);
+        $request->attributes->set('is_rest_request', true);
+        $sessionSettings = [
+            'cookie_path' => '/',
+            'cookie_domain' => 'ez.no',
+        ];
+        $this->configResolver
+            ->expects($this->once())
+            ->method('getParameter')
+            ->with('session')
+            ->will($this->returnValue($sessionSettings));
+        $response = new Response();
+        $response->headers = $this->createMock(ResponseHeaderBag::class);
+        $response->headers
+            ->expects($this->once())
+            ->method('clearCookie')
+            ->with($sessionId, $sessionSettings['cookie_path'], $sessionSettings['cookie_domain']);
+        $logoutHandler = new RestLogoutHandler($this->configResolver);
+        $logoutHandler->logout(
+            $request,
+            $response,
+            $this->createMock(TokenInterface::class)
         );
     }
 
@@ -59,7 +117,7 @@ class RestLogoutHandlerTest extends TestCase
             ->expects($this->never())
             ->method('clearCookie');
 
-        $logoutHandler = new RestLogoutHandler();
+        $logoutHandler = new RestLogoutHandler($this->configResolver);
         $logoutHandler->logout(
             $request,
             $response,
