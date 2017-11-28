78c3c23a4e80194fabb59c98933e53b1300c5f7d
Adam Wójs <adam.wojs@ez.no>
2017-11-28 13:28:27 +0100

    fixup! Fix EZP-28174: Added validation of ezlocation:// and ezcontent:// links in the richtext field type

--- eZ/Publish/Core/FieldType/Tests/RichText/InternalLinkValidatorTest.php
+++ eZ/Publish/Core/FieldType/Tests/RichText/InternalLinkValidatorTest.php
@@ -6,7 +6,6 @@
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
-
 namespace eZ\Publish\Core\FieldType\Tests\RichText;
 
 use eZ\Publish\API\Repository\ContentService;
