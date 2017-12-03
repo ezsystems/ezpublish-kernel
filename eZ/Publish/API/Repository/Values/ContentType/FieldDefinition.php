e6eb38bd043a6c199b29c7fbc99535d3ca357fdd
Andrew Longosz <andrzej.longosz@ez.no>
2017-12-03 22:52:30 +0100

    EZP-28337: Added API FieldDefinition typeIdentifier property
    * Deprecated API FieldDefinition fieldTypeIdentifier property
    
    * Replaced deprecated fieldTypeIdentifier property with accessors
      to keep BC and trigger deprecation warning.

--- eZ/Publish/API/Repository/Values/ContentType/FieldDefinition.php
+++ eZ/Publish/API/Repository/Values/ContentType/FieldDefinition.php
@@ -21,7 +21,7 @@ use eZ\Publish\SPI\Repository\Values\MultiLanguageDescription;
  * @property-read string $identifier the identifier of the field definition
  * @property-read string $fieldGroup the field group name
  * @property-read int $position the position of the field definition in the content type
- * @property-read string $fieldTypeIdentifier String identifier of the field type
+ * @property-read string $typeIdentifier String identifier of the field type
  * @property-read bool $isTranslatable indicates if fields of this definition are translatable
  * @property-read bool $isRequired indicates if this field is required in the content object
  * @property-read bool $isSearchable indicates if the field is searchable
@@ -63,7 +63,7 @@ abstract class FieldDefinition extends ValueObject implements MultiLanguageName,
      *
      * @var string
      */
-    protected $fieldTypeIdentifier;
+    protected $typeIdentifier;
 
     /**
      * If the field is translatable.
@@ -113,4 +113,51 @@ abstract class FieldDefinition extends ValueObject implements MultiLanguageName,
      * @var bool
      */
     protected $isSearchable;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct(array $properties = [])
+    {
+        if (array_key_exists('fieldTypeIdentifier', $properties)) {
+            $this->triggerDeprecatedPropertyWarning('fieldTypeIdentifier', 'typeIdentifier');
+
+            $properties['typeIdentifier'] = $properties['fieldTypeIdentifier'];
+
+            // avoid setting non-existent property by the parent constructor
+            unset($properties['fieldTypeIdentifier']);
+        }
+
+        parent::__construct($properties);
+    }
+
+    /**
+     * BC accessor for fieldTypeIdentifier used by Twig.
+     *
+     * @see \twig_get_attribute()
+     *
+     * @deprecated Use FieldDefinition::typeIdentifier property instead
+     *
+     * @return string
+     */
+    public function getFieldTypeIdentifier()
+    {
+        $this->triggerDeprecatedPropertyWarning('fieldTypeIdentifier', 'typeIdentifier');
+
+        return $this->typeIdentifier;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __get($property)
+    {
+        if ('fieldTypeIdentifier' === $property) {
+            $this->triggerDeprecatedPropertyWarning('fieldTypeIdentifier', 'typeIdentifier');
+
+            return $this->typeIdentifier;
+        }
+
+        return parent::__get($property);
+    }
 }
