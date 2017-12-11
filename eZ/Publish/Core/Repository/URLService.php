93cf097596b712bfb1ebb74a235b6f449bcbaa4b
Adam Wójs <adam.wojs@ez.no>
2017-12-11 14:31:21 +0100

    EZP-28480: As a Developer I want API to manage external URLs (API impl.)

---
+++ eZ/Publish/Core/Repository/URLService.php
@@ -0,0 +1,232 @@
+<?php
+
+namespace eZ\Publish\Core\Repository;
+
+use DateTime;
+use eZ\Publish\API\Repository\Exceptions\NotFoundException;
+use eZ\Publish\API\Repository\Repository;
+use eZ\Publish\API\Repository\URLService as URLServiceInterface;
+use eZ\Publish\API\Repository\Values\Content\Query;
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion as ContentCriterion;
+use eZ\Publish\API\Repository\Values\URL\SearchResult;
+use eZ\Publish\API\Repository\Values\URL\URL;
+use eZ\Publish\API\Repository\Values\URL\URLQuery;
+use eZ\Publish\API\Repository\Values\URL\URLUpdateStruct;
+use eZ\Publish\Core\Base\Exceptions\InvalidArgumentException;
+use eZ\Publish\Core\Base\Exceptions\InvalidArgumentValue;
+use eZ\Publish\Core\Base\Exceptions\UnauthorizedException;
+use eZ\Publish\SPI\Persistence\URL\Handler as URLHandler;
+use eZ\Publish\SPI\Persistence\URL\URL as SPIUrl;
+use eZ\Publish\SPI\Persistence\URL\URLUpdateStruct as SPIUrlUpdateStruct;
+
+class URLService implements URLServiceInterface
+{
+    /**
+     * @var \eZ\Publish\Core\Repository\Repository
+     */
+    protected $repository;
+
+    /**
+     * @var \eZ\Publish\SPI\Persistence\URL\Handler
+     */
+    protected $urlHandler;
+
+    /**
+     * URLService constructor.
+     *
+     * @param \eZ\Publish\API\Repository\Repository $repository
+     * @param \eZ\Publish\SPI\Persistence\URL\Handler $urlHandler
+     */
+    public function __construct(Repository $repository, URLHandler $urlHandler)
+    {
+        $this->repository = $repository;
+        $this->urlHandler = $urlHandler;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function findUrls(URLQuery $query)
+    {
+        if ($this->repository->hasAccess('url', 'view') !== true) {
+            throw new UnauthorizedException('url', 'view');
+        }
+
+        if ($query->offset !== null && !is_numeric($query->offset)) {
+            throw new InvalidArgumentValue('offset', $query->offset);
+        }
+
+        if ($query->limit !== null && !is_numeric($query->limit)) {
+            throw new InvalidArgumentValue('limit', $query->limit);
+        }
+
+        $results = $this->urlHandler->find($query);
+
+        $items = [];
+        foreach ($results['items'] as $url) {
+            $items[] = $this->buildDomainObject($url);
+        }
+
+        return new SearchResult([
+            'count' => $results['count'],
+            'items' => $items,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function updateUrl(URL $url, URLUpdateStruct $struct)
+    {
+        if ($this->repository->hasAccess('url', 'update') !== true) {
+            throw new UnauthorizedException('url', 'update');
+        }
+
+        if (!$this->isUnique($url->id, $struct->url)) {
+            throw new InvalidArgumentException('struct', 'url already exists');
+        }
+
+        $updateStruct = $this->buildUpdateStruct($this->loadById($url->id), $struct);
+
+        $this->repository->beginTransaction();
+        try {
+            $this->urlHandler->updateUrl($url->id, $updateStruct);
+            $this->repository->commit();
+        } catch (\Exception $e) {
+            $this->repository->rollback();
+            throw $e;
+        }
+
+        return $this->loadById($url->id);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function loadById($id)
+    {
+        if ($this->repository->hasAccess('url', 'view') !== true) {
+            throw new UnauthorizedException('url', 'view');
+        }
+
+        return $this->buildDomainObject(
+            $this->urlHandler->loadById($id)
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function loadByUrl($url)
+    {
+        if ($this->repository->hasAccess('url', 'view') !== true) {
+            throw new UnauthorizedException('url', 'view');
+        }
+
+        return $this->buildDomainObject(
+            $this->urlHandler->loadByUrl($url)
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createUpdateStruct()
+    {
+        return new URLUpdateStruct();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function findUsages(URL $url, $offset = 0, $limit = -1)
+    {
+        $usages = $this->urlHandler->findUsages($url->id);
+
+        $query = new Query();
+        if (!empty($usages)) {
+            $query->filter = new ContentCriterion\LogicalAnd([
+                new ContentCriterion\ContentId($usages),
+                new ContentCriterion\Visibility(ContentCriterion\Visibility::VISIBLE),
+            ]);
+        } else {
+            $query->filter = new ContentCriterion\MatchNone();
+        }
+
+        $query->offset = $offset;
+        if ($limit > -1) {
+            $query->limit = $limit;
+        }
+
+        return $this->repository->getSearchService()->findContentInfo($query);
+    }
+
+    protected function buildDomainObject(SPIUrl $data)
+    {
+        return new URL([
+            'id' => $data->id,
+            'url' => $data->url,
+            'originalUrlMd5' => $data->originalUrlMd5,
+            'isValid' => $data->isValid,
+            'lastChecked' => $this->createDateTime($data->lastChecked),
+            'created' => $this->createDateTime($data->created),
+            'modified' => $this->createDateTime($data->modified),
+        ]);
+    }
+
+    protected function buildUpdateStruct(URL $url, URLUpdateStruct $data)
+    {
+        $updateStruct = new SPIUrlUpdateStruct();
+
+        if ($data->url !== null && $url->url !== $data->url) {
+            $updateStruct->url = $data->url;
+            // Reset URL validity
+            $updateStruct->lastChecked = 0;
+            $updateStruct->isValid = true;
+        } else {
+            $updateStruct->url = $url->url;
+
+            if ($data->lastChecked !== null) {
+                $updateStruct->lastChecked = $data->lastChecked->getTimestamp();
+            } elseif ($data->lastChecked !== null) {
+                $updateStruct->lastChecked = $url->lastChecked->getTimestamp();
+            } else {
+                $updateStruct->lastChecked = 0;
+            }
+
+            if ($data->isValid !== null) {
+                $updateStruct->isValid = $data->isValid;
+            } else {
+                $updateStruct->isValid = $url->isValid;
+            }
+        }
+
+        return $updateStruct;
+    }
+
+    /**
+     * Check if URL is unique.
+     *
+     * @param int $id
+     * @param string $url
+     * @return bool
+     * @throws \eZ\Publish\Core\Base\Exceptions\UnauthorizedException
+     */
+    protected function isUnique($id, $url)
+    {
+        try {
+            return $this->loadByUrl($url)->id === $id;
+        } catch (NotFoundException $e) {
+            return true;
+        }
+    }
+
+    private function createDateTime($timestamp)
+    {
+        if ($timestamp > 0) {
+            return new DateTime("@{$timestamp}");
+        }
+
+        return null;
+    }
+}
