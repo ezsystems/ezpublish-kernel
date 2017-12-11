df88aa26c60b9f1dce03c405564335da8b40d1e0
Andrew Longosz <alongosz@users.noreply.github.com>
2017-12-11 10:57:47 +0100

    EZP-28375: Improved REST Functional Tests (#2181)
    * [REST Tests] Changed remoteId to be generated as md5
    
    We cache by remoteId also and caching components requires keys tags
    to be in specific format (e.g not containing special characters).
    
    * [REST Tests] Added created Role to list of elements for cleanup
    
    At the end of tests execution there is performed a cleanup
    
    * [REST Tests] Updated testing login using existing frontend session

--- eZ/Bundle/EzPublishRestBundle/Tests/Functional/TestCase.php
+++ eZ/Bundle/EzPublishRestBundle/Tests/Functional/TestCase.php
@@ -214,6 +214,7 @@ EOF;
     protected function createFolder($string, $parentLocationId)
     {
         $string = $this->addTestSuffix($string);
+        $remoteId = md5(uniqid($string, true));
         $xml = <<< XML
 <?xml version="1.0" encoding="UTF-8"?>
 <ContentCreate>
@@ -228,7 +229,7 @@ EOF;
   </LocationCreate>
   <Section href="/api/ezp/v2/content/sections/1" />
   <alwaysAvailable>true</alwaysAvailable>
-  <remoteId>{$string}</remoteId>
+  <remoteId>{$remoteId}</remoteId>
   <User href="/api/ezp/v2/user/users/14" />
   <modificationDate>2012-09-30T12:30:00</modificationDate>
   <fields>
