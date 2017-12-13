bdcd05650de41d37027868acb8763b132e77a6b9
André R <ar@ez.no>
2017-12-13 14:15:25 +0100

    [SQL] Add udpate SQL for 6.13LTS (and effectivly also 7.0)

--- data/update/mysql/dbupdate-5.4.0-to-6.13.0.sql
+++ data/update/mysql/dbupdate-5.4.0-to-6.13.0.sql
@@ -1,13 +1,12 @@
 SET default_storage_engine=InnoDB;
 -- Set storage engine schema version number
-UPDATE ezsite_data SET value='6.11.0' WHERE name='ezpublish-version';
+UPDATE ezsite_data SET value='6.13.0' WHERE name='ezpublish-version';
 
 --
 -- EZP-25880: Make ezuser.login case in-sensitive across databases, using case in-sensitive index
 --
 
 ALTER TABLE ezuser DROP KEY ezuser_login, ADD UNIQUE KEY ezuser_login (login);
-
 
 --
 -- EZP-26397: marked User.user_account as not searchable
@@ -46,3 +45,9 @@ INSERT INTO `ezpolicy_limitation_value` (`limitation_id`, `value`)
     AND `p1`.`role_id` = `p0`.`role_id`;
 
 -- END script for EZP-26070
+
+--
+-- EZP-24744: Increase password security
+--
+
+ALTER TABLE ezuser CHANGE password_hash password_hash VARCHAR(255) default NULL;
