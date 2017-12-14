acf4a72d25b10d07dcfbc5f9000dac6b5deef38e
Andrew Longosz <alongosz@users.noreply.github.com>
2017-12-14 14:37:41 +0100

    EZP-28375: Enabled tests running inside Docker container (#2193)

--- bin/.travis/prepare_behat.sh
+++ bin/.travis/prepare_behat.sh
@@ -9,4 +9,4 @@ echo "> Cloning ezsystems/ezplatform:${EZPLATFORM_BRANCH}"
 git clone --depth 1 --single-branch --branch "$EZPLATFORM_BRANCH" https://github.com/ezsystems/ezplatform.git ${EZPLATFORM_BUILD_DIR}
 cd ${EZPLATFORM_BUILD_DIR}
 
-/bin/bash ./bin/.travis/trusty/setup_ezplatform.sh "${PACKAGE_BUILD_DIR}"
+/bin/bash ./bin/.travis/trusty/setup_ezplatform.sh "${COMPOSE_FILE}" '' "${PACKAGE_BUILD_DIR}"
