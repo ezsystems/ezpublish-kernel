cc2f7a73d7fbf1f1b5e7dfddcf565e1973ec7821
Mario Blažek <blazek.mario@gmail.com>
2017-12-14 12:21:22 +0100

    Fixed ezrobot comments

--- eZ/Bundle/EzPublishCoreBundle/Tests/Translation/GlobCollectorTest.php
+++ eZ/Bundle/EzPublishCoreBundle/Tests/Translation/GlobCollectorTest.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * File containing the class GlobCollectorTest
+ * File containing the class GlobCollectorTest.
  *
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
