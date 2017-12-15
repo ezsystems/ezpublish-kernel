c0b315e66cb703bc6cb5194c578d33b47df85ac0
Adam Wójs <adam@wojs.pl>
2017-12-15 11:36:11 +0100

    EZP-28480: As a Developer I want API to manage external URLs (New SPI cache for URL Handler) (#2196)

--- eZ/Publish/Core/Persistence/Cache/URLHandler.php
+++ eZ/Publish/Core/Persistence/Cache/URLHandler.php
@@ -9,47 +9,14 @@ namespace eZ\Publish\Core\Persistence\Cache;
 use eZ\Publish\API\Repository\Values\URL\URLQuery;
 use eZ\Publish\SPI\Persistence\URL\Handler as URLHandlerInterface;
 use eZ\Publish\SPI\Persistence\URL\URLUpdateStruct;
-use eZ\Publish\SPI\Persistence\Handler as PersistenceHandler;
 
 /**
  * SPI cache for URL Handler.
  *
  * @see \eZ\Publish\SPI\Persistence\URL\Handler
  */
-class URLHandler implements URLHandlerInterface
+class URLHandler extends AbstractHandler implements URLHandlerInterface
 {
-    /**
-     * @var \eZ\Publish\Core\Persistence\Cache\CacheServiceDecorator
-     */
-    protected $cache;
-
-    /**
-     * @var \eZ\Publish\SPI\Persistence\Handler
-     */
-    protected $persistenceHandler;
-
-    /**
-     * @var \eZ\Publish\Core\Persistence\Cache\PersistenceLogger
-     */
-    protected $logger;
-
-    /**
-     * Setups current handler with everything needed.
-     *
-     * @param \eZ\Publish\Core\Persistence\Cache\CacheServiceDecorator $cache
-     * @param \eZ\Publish\SPI\Persistence\Handler $persistenceHandler
-     * @param \eZ\Publish\Core\Persistence\Cache\PersistenceLogger $logger
-     */
-    public function __construct(
-        CacheServiceDecorator $cache,
-        PersistenceHandler $persistenceHandler,
-        PersistenceLogger $logger)
-    {
-        $this->cache = $cache;
-        $this->persistenceHandler = $persistenceHandler;
-        $this->logger = $logger;
-    }
-
     /**
      * {@inheritdoc}
      */
@@ -62,8 +29,7 @@ class URLHandler implements URLHandlerInterface
 
         $url = $this->persistenceHandler->urlHandler()->updateUrl($id, $struct);
 
-        $this->cache->clear('url', $id);
-        $this->cache->clear('content');
+        $this->cache->invalidateTags(['url-' . $id]);
 
         return $url;
     }
@@ -85,14 +51,19 @@ class URLHandler implements URLHandlerInterface
      */
     public function loadById($id)
     {
-        $cache = $this->cache->getItem('url', $id);
+        $cacheItem = $this->cache->getItem('ez-url-' . $id);
 
-        $url = $cache->get();
-        if ($cache->isMiss()) {
-            $this->logger->logCall(__METHOD__, ['url' => $id]);
-            $url = $this->persistenceHandler->urlHandler()->loadById($id);
-            $cache->set($url)->save();
+        $url = $cacheItem->get();
+        if ($cacheItem->isHit()) {
+            return $url;
         }
+
+        $this->logger->logCall(__METHOD__, ['url' => $id]);
+        $url = $this->persistenceHandler->urlHandler()->loadById($id);
+
+        $cacheItem->set($url);
+        $cacheItem->tag(['url-' . $id]);
+        $this->cache->save($cacheItem);
 
         return $url;
     }
@@ -102,6 +73,8 @@ class URLHandler implements URLHandlerInterface
      */
     public function loadByUrl($url)
     {
+        $this->logger->logCall(__METHOD__, ['url' => $url]);
+
         return $this->persistenceHandler->urlHandler()->loadByUrl($url);
     }
 
@@ -110,15 +83,8 @@ class URLHandler implements URLHandlerInterface
      */
     public function findUsages($id)
     {
-        $cache = $this->cache->getItem('url', $id, 'usages');
+        $this->logger->logCall(__METHOD__, ['url' => $id]);
 
-        $usages = $cache->get();
-        if ($cache->isMiss()) {
-            $this->logger->logCall(__METHOD__, ['url' => $id]);
-            $usages = $this->persistenceHandler->urlHandler()->findUsages($id);
-            $cache->set($usages)->save();
-        }
-
-        return $usages;
+        return $this->persistenceHandler->urlHandler()->findUsages($id);
     }
 }
