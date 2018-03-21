f76dd81db20b485f1922032f4f569df6bf98f991
kmadejski <kamil.madejski@ez.no>
2018-03-21 11:19:58 +0100

    Remove unecessary context

--- eZ/Publish/Core/Repository/LocationService.php
+++ eZ/Publish/Core/Repository/LocationService.php
@@ -350,7 +350,7 @@ class LocationService implements LocationServiceInterface
         $content = $this->repository->getContentService()->loadContent($contentInfo->id);
         $parentLocation = $this->loadLocation($locationCreateStruct->parentLocationId);
 
-        if (!$this->repository->canUser('content', 'manage_locations', $content->contentInfo, $content)) {
+        if (!$this->repository->canUser('content', 'manage_locations', $content->contentInfo)) {
             throw new UnauthorizedException('content', 'manage_locations');
         }
 
