bdf6d184b688a0643b7a32bff26177612fb6babc
Andrew Longosz <andrzej.longosz@ez.no>
2018-04-13 14:32:25 +0200

    Refactored UrlAlias Persistence Handler::locationSwapped
    Using internal DTOs instead of multiple variables for readablity

---
+++ eZ/Publish/Core/Persistence/Legacy/Content/UrlAlias/DTO/UrlAliasForSwappedLocation.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace eZ\Publish\Core\Persistence\Legacy\Content\UrlAlias\DTO;
+
+/**
+ * @internal To be used internally by UrlAlias Persistence Handler.
+ */
+class UrlAliasForSwappedLocation
+{
+    public function __construct(
+        $id,
+        $parentId,
+        $name,
+        $isAlwaysAvailable,
+        $isPathIdentificationStringModified,
+        $newId
+    ) {
+        $this->id = $id;
+        $this->parentId = $parentId;
+        $this->name = $name;
+        $this->isAlwaysAvailable = $isAlwaysAvailable;
+        $this->isPathIdentificationStringModified = $isPathIdentificationStringModified;
+        $this->newId = $newId;
+    }
+
+    /**
+     * @var int
+     */
+    public $id;
+
+    /**
+     * @var int
+     */
+    public $parentId;
+
+    /**
+     * @var string
+     */
+    public $name;
+
+    /**
+     * @var bool
+     */
+    public $isAlwaysAvailable;
+
+    /**
+     * @var bool
+     */
+    public $isPathIdentificationStringModified;
+
+    /**
+     * @var int
+     */
+    public $newId;
+}
