8ebbc58cdb953db7c851c97a0d199b8bc58432bc
Adam Wójs <adam.wojs@ez.no>
2018-05-14 09:05:06 +0200

    EZP-29146: As a developer, I want a API to manage bookmarks

---
+++ eZ/Publish/API/Repository/Values/Bookmark/BookmarkList.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace eZ\Publish\API\Repository\Values\Bookmark;
+
+use ArrayIterator;
+use IteratorAggregate;
+use eZ\Publish\API\Repository\Values\ValueObject;
+
+class BookmarkList extends ValueObject implements IteratorAggregate
+{
+    /**
+     * The total number of bookmarks.
+     *
+     * @var int
+     */
+    public $totalCount = 0;
+
+    /**
+     * List of bookmarked locations.
+     *
+     * @var \eZ\Publish\API\Repository\Values\Content\Location[]
+     */
+    public $items = [];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getIterator()
+    {
+        return new ArrayIterator($this->items);
+    }
+}
