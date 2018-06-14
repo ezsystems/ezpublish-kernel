ed73674507fed829ca393bdb2cc14407a65510b4
kmadejski <kamil.madejski@ez.no>
2018-06-14 14:12:23 +0000

    EZP-29297: 404 error pages are not tagged correctly

---
+++ eZ/Publish/Core/MVC/Exception/HiddenLocationException.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace eZ\Publish\Core\MVC\Exception;
+
+use eZ\Publish\API\Repository\Values\Content\Location;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class HiddenLocationException extends NotFoundHttpException
+{
+    /**
+     * @var \eZ\Publish\API\Repository\Values\Content\Location
+     */
+    private $location;
+
+    public function __construct(Location $location, $message = null, \Exception $previous = null, $code = 0)
+    {
+        $this->location = $location;
+        parent::__construct($message, $previous, $code);
+    }
+
+    /**
+     * @return \eZ\Publish\API\Repository\Values\Content\Location
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+}
