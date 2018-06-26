41824f1fcec0295c53adad68a5f1b0d243c1201d
Mikołaj Adamczyk <mikadamczyk@gmail.com>
2018-06-26 16:03:00 +0200

    EZP-29322: As an editor I want to use translated Admin panel

--- eZ/Bundle/EzPublishCoreBundle/DependencyInjection/Compiler/TranslationCollectorPass.php
+++ eZ/Bundle/EzPublishCoreBundle/DependencyInjection/Compiler/TranslationCollectorPass.php
@@ -17,6 +17,25 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
  */
 class TranslationCollectorPass implements CompilerPassInterface
 {
+    /** @var array */
+    public const LOCALES_MAP = [
+        'de_DE' => 'de',
+        'el_GR' => 'el',
+        'es_ES' => 'es',
+        'fi_FI' => 'fi',
+        'fr_FR' => 'fr',
+        'hi_IN' => 'hi',
+        'hu_HU' => 'hu',
+        'ja_JP' => 'ja',
+        'nb_NO' => 'no',
+        'pl_PL' => 'pl',
+        'pt_PT' => 'pt',
+        'ru_RU' => 'ru',
+        'en_US' => 'en',
+        'it_IT' => 'it',
+        'hr_HR' => 'hr',
+    ];
+
     public function process(ContainerBuilder $container)
     {
         if (!$container->hasDefinition('translator.default')) {
@@ -26,11 +45,20 @@ class TranslationCollectorPass implements CompilerPassInterface
         $definition = $container->getDefinition('translator.default');
         $collector = new GlobCollector($container->getParameterBag()->get('kernel.root_dir'));
 
+        $availableTranslations = [];
         foreach ($collector->collect() as $file) {
+            /* TODO - to remove when translation files will have proper names. */
+            if (isset(self::LOCALES_MAP[$file['locale']])) {
+                $file['locale'] = self::LOCALES_MAP[$file['locale']];
+            }
+            $availableTranslations[] = $file['locale'];
+
             $definition->addMethodCall(
                 'addResource',
                 array($file['format'], $file['file'], $file['locale'], $file['domain'])
             );
         }
+
+        $container->setParameter('available_translations', array_values(array_unique($availableTranslations)));
     }
 }
