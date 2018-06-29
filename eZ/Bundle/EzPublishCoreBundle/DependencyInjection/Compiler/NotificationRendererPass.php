5726f7c89f56c7227a349a3b364374fbb453520f
Adam Wójs <adam.wojs@ez.no>
2018-06-29 09:51:58 +0200

    EZEE-2081: Replaced tag ezstudio.notification.renderer with ezpublish.notification.renderer

--- eZ/Bundle/EzPublishCoreBundle/DependencyInjection/Compiler/NotificationRendererPass.php
+++ eZ/Bundle/EzPublishCoreBundle/DependencyInjection/Compiler/NotificationRendererPass.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
 
 namespace eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Compiler;
 
+use LogicException;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
@@ -16,20 +17,47 @@ use Symfony\Component\DependencyInjection\Reference;
  */
 class NotificationRendererPass implements CompilerPassInterface
 {
-    /**
-     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
-     */
+    const TAG_NAME = 'ezpublish.notification.renderer';
+    const REGISTRY_DEFINITION_ID = 'notification.renderer.registry';
+
     public function process(ContainerBuilder $container)
     {
-        if (!$container->has('notification.renderer.registry')) {
+        if (!$container->has(self::REGISTRY_DEFINITION_ID)) {
             return;
         }
 
-        $registry = $container->findDefinition('notification.renderer.registry');
+        $registry = $container->findDefinition(self::REGISTRY_DEFINITION_ID);
 
-        foreach ($container->findTaggedServiceIds('ezstudio.notification.renderer') as $id => $tags) {
-            foreach ($tags as $tag) {
-                $registry->addMethodCall('addRenderer', [$tag['alias'], new Reference($id)]);
+        foreach ($container->findTaggedServiceIds(self::TAG_NAME) as $id => $attributes) {
+            foreach ($attributes as $attribute) {
+                if (!isset($attribute['alias'])) {
+                    throw new LogicException(sprintf(
+                        'Tag %s needs a "alias" attribute to identify the notification type. None given.',
+                        self::TAG_NAME
+                    ));
+                }
+
+                $registry->addMethodCall('addRenderer', [$attribute['alias'], new Reference($id)]);
+            }
+        }
+
+        foreach ($container->findTaggedServiceIds('ezstudio.notification.renderer') as $id => $attributes) {
+            @trigger_error(
+                sprintf(
+                    'Tag ezstudio.notification.renderer is deprecated since 2.2. Please use \'%s\' instead.',
+                    self::TAG_NAME
+                ),
+                E_USER_DEPRECATED
+            );
+
+            foreach ($attributes as $attribute) {
+                if (!isset($attribute['alias'])) {
+                    throw new LogicException(
+                        'Tag ezstudio.notification.renderer needs a "alias" attribute to identify the notification type. None given.'
+                    );
+                }
+
+                $registry->addMethodCall('addRenderer', [$attribute['alias'], new Reference($id)]);
             }
         }
     }
