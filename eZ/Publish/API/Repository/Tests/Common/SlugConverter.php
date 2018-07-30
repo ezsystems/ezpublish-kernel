70a86ddad31a15648c20791d909d7162de673158
Andrew Longosz <andrzej.longosz@ez.no>
2018-07-30 11:30:35 +0200

    fixup! [Tests] Created tests for refreshSystemUrlAliasesForLocation

--- eZ/Publish/API/Repository/Tests/Common/SlugConverter.php
+++ eZ/Publish/API/Repository/Tests/Common/SlugConverter.php
@@ -1,9 +1,11 @@
-<?php declare(strict_types=1);
+<?php
 
 /**
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
+declare(strict_types=1);
+
 namespace eZ\Publish\API\Repository\Tests\Common;
 
 use eZ\Publish\Core\Persistence\Legacy\Content\UrlAlias\SlugConverter as LegacySlugConverter;
