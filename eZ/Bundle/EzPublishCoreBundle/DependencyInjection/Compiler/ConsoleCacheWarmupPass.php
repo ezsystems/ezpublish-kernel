1779c6978f12398a646c14f97f4a38a571a92046
Gunnstein Lye <gunnstein.lye@ez.no>
2018-08-10 16:33:10 +0200

    EZP-29034: Cannot install ezplatform on 1.7 branch
    Add ConsoleCacheWarmupPass which disables class cache warmer on CLI.

---
+++ eZ/Bundle/EzPublishCoreBundle/DependencyInjection/Compiler/ConsoleCacheWarmupPass.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * File containing the ConsoleCacheWarmupPass class.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+/**
+ * Cache related compiler pass.
+ *
+ * Ensures class cache warmup is disabled in console mode.
+ */
+class ConsoleCacheWarmupPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        if (PHP_SAPI !== 'cli' ||
+            !$container->hasDefinition('kernel.class_cache.cache_warmer')) {
+            return;
+        }
+
+        $warmers = array();
+        foreach ($container->findTaggedServiceIds('kernel.cache_warmer') as $id => $attributes) {
+            if ($id === 'kernel.class_cache.cache_warmer') {
+                continue;
+            }
+
+            $priority = isset($attributes[0]['priority']) ? $attributes[0]['priority'] : 0;
+            $warmers[$priority][] = new Reference($id);
+        }
+
+        if (empty($warmers)) {
+            return;
+        }
+
+        // sort by priority and flatten
+        krsort($warmers);
+        $warmers = \call_user_func_array('array_merge', $warmers);
+
+        $container->getDefinition('cache_warmer')->replaceArgument(0, $warmers);
+    }
+}
