08e5706d696e134456a2ffced67bca57e483abc1
André R <ar@ez.no>
2018-08-23 19:43:54 +0200

    CS

--- eZ/Publish/Core/Persistence/Cache/Tests/ContentHandlerTest.php
+++ eZ/Publish/Core/Persistence/Cache/Tests/ContentHandlerTest.php
@@ -74,7 +74,7 @@ class ContentHandlerTest extends AbstractCacheHandlerTest
             ['load', [2, 1], 'ez-content-2-1-' . ContentHandler::ALL_TRANSLATIONS_KEY, $content],
             ['load', [2, 1, ['eng-GB', 'eng-US']], 'ez-content-2-1-eng-GB|eng-US', $content],
             ['loadContentList', [[2]], 'ez-content-2-' . ContentHandler::ALL_TRANSLATIONS_KEY, [2 => $content], true],
-            ['loadContentList', [[5],['eng-GB', 'eng-US']], 'ez-content-5-eng-GB|eng-US', [5 => $content], true],
+            ['loadContentList', [[5], ['eng-GB', 'eng-US']], 'ez-content-5-eng-GB|eng-US', [5 => $content], true],
             ['loadContentInfo', [2], 'ez-content-info-2', $info],
             ['loadContentInfoList', [[2]], 'ez-content-info-2', [2 => $info], true],
             ['loadContentInfoByRemoteId', ['3d8jrj'], 'ez-content-info-byRemoteId-3d8jrj', $info],
