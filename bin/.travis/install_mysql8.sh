97ff99c8f40fe45081f488782b0db0de2e6f82f9
Marek Nocon <marek.nocon@ez.no>
2018-10-24 09:44:16 +0200

    Approach 3: Speedup mysql on Docker

--- bin/.travis/install_mysql8.sh
+++ bin/.travis/install_mysql8.sh
@@ -2,16 +2,15 @@
 
 set -ex
 
-echo "Starting MySQL 8.0..."
+sudo mkdir /mnt/ramdisk
+sudo mount -t tmpfs -o size=1024m tmpfs /mnt/ramdisk
+sudo stop mysql
+sudo start mysql
 
 echo -e "[mysqld]\ndefault_authentication_plugin=mysql_native_password" > /tmp/mysql-auth.cnf
 
 docker pull mysql:8.0
-docker run \
-    -d \
-    -e MYSQL_ALLOW_EMPTY_PASSWORD=yes \
-    -e MYSQL_DATABASE=testdb \
-    -v /tmp/mysql-auth.cnf:/etc/mysql/conf.d/auth.cnf:ro \
-    -p 33306:3306 \
-    --name mysql80 \
-    mysql:8.0
+docker run -d -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -e MYSQL_DATABASE=testdb -v /tmp/mysql-auth.cnf:/etc/mysql/conf.d/auth.cnf:ro -v /mnt/ramdisk:/var/lib/mysql -p 33306:3306 --name mysql80 mysql:8.0
+docker exec mysql80 apt-get update
+docker exec mysql80 apt-get -y install haveged
+docker exec mysql80 service haveged start
