29248ac7c2fb667929c1715f58eb12b2379162ea
Marek Nocon <marek.nocon@ez.no>
2018-11-13 12:22:55 +0100

    Create db with correct collaction

--- bin/.travis/install_mysql8.sh
+++ bin/.travis/install_mysql8.sh
@@ -9,7 +9,8 @@ sudo stop mysql
 echo -e "[mysqld]\ndefault_authentication_plugin=mysql_native_password\nskip-log-bin\nssl=0" > /tmp/mysql-auth.cnf
 
 docker pull mysql:8.0
-docker run -d -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -e MYSQL_DATABASE=testdb -v /tmp/mysql-auth.cnf:/etc/mysql/conf.d/auth.cnf:ro -v /mnt/ramdisk:/var/lib/mysql -p 33306:3306 --name mysql80 mysql:8.0
+docker run -d -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -v /tmp/mysql-auth.cnf:/etc/mysql/conf.d/auth.cnf:ro -v /mnt/ramdisk:/var/lib/mysql -p 33306:3306 --name mysql80 mysql:8.0
 docker exec mysql80 apt-get update
 docker exec mysql80 apt-get -y install haveged
 docker exec mysql80 service haveged start
+docker exec mysql21 mysql -u root -e "CREATE DATABASE IF NOT EXISTS testdb DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci;";
