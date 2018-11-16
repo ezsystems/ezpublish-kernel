b36ad651caa25c34138a3d8c71f70c135601f3d2
Marek Nocon <marek.nocon@ez.no>
2018-11-16 12:20:42 +0100

    test

--- bin/.travis/install_mysql8.sh
+++ bin/.travis/install_mysql8.sh
@@ -8,7 +8,7 @@ sudo stop mysql
 
 echo -e "[mysqld]\ndefault_authentication_plugin=mysql_native_password\nskip-log-bin\nssl=0" > /tmp/mysql-auth.cnf
 
-docker pull mysql:8.0
+docker pull mysql:8.0.11
 docker run -d -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -v /tmp/mysql-auth.cnf:/etc/mysql/conf.d/auth.cnf:ro -v /mnt/ramdisk:/var/lib/mysql -p 33306:3306 --name mysql80 mysql:8.0.11
 docker exec mysql80 apt-get update
 docker exec mysql80 apt-get -y install haveged
