82e9c70016744171c8990e35131f9bd326fd154e
André R <ar@ez.no>
2018-11-19 16:59:46 +0100

    EZP-29821: [SPI Cache] Add cache for ContentTypeHandler->getUserPreferenceByUserIdAndName() when empty preference

--- eZ/Publish/SPI/Persistence/UserPreference/Handler.php
+++ eZ/Publish/SPI/Persistence/UserPreference/Handler.php
@@ -25,6 +25,8 @@ interface Handler
      * @param int $userId
      * @param string $name
      *
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException If no value is found for given preference name.
+     *
      * @return \eZ\Publish\SPI\Persistence\UserPreference\UserPreference
      */
     public function getUserPreferenceByUserIdAndName(int $userId, string $name): UserPreference;
