bfb46b412bb3f8e1c9ce2b257c3d4d588b9570e9
Mikołaj Adamczyk <mikadamczyk@gmail.com>
2019-01-03 13:34:22 +0100

    EZP-29961: Add preferred language value to User Preferences

--- eZ/Publish/Core/MVC/Symfony/Locale/Tests/UserLanguagePreferenceProviderTest.php
+++ eZ/Publish/Core/MVC/Symfony/Locale/Tests/UserLanguagePreferenceProviderTest.php
@@ -14,9 +14,16 @@ use Symfony\Component\HttpFoundation\HeaderBag;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Yaml\Yaml;
+use eZ\Publish\API\Repository\UserPreferenceService;
+use eZ\Publish\API\Repository\Values\UserPreference\UserPreference;
+use eZ\Publish\Core\Base\Exceptions\NotFoundException;
 
 class UserLanguagePreferenceProviderTest extends TestCase
 {
+    private const LOCALE_FALLBACK = 'en';
+    private const LANGUAGE_PREFERENCE_NAME = 'language';
+    private const LANGUAGE_PREFERENCE_VALUE = 'no';
+
     /**
      * @var \eZ\Publish\Core\MVC\Symfony\Locale\UserLanguagePreferenceProviderInterface
      */
@@ -27,13 +34,31 @@ class UserLanguagePreferenceProviderTest extends TestCase
      */
     private $requestStackMock;
 
+    /**
+     * @var \eZ\Publish\API\Repository\UserPreferenceService
+     */
+    private $userPreferenceServiceMock;
+
     public function setUp()
     {
         $this->requestStackMock = $this->createMock(RequestStack::class);
 
+        $userLanguagePreference = new UserPreference([
+            'name' => self::LANGUAGE_PREFERENCE_NAME,
+            'value' => self::LANGUAGE_PREFERENCE_VALUE,
+        ]);
+
+        $this->userPreferenceServiceMock = $this->createMock(UserPreferenceService::class);
+        $this->userPreferenceServiceMock
+            ->method('getUserPreference')
+            ->with(self::LANGUAGE_PREFERENCE_NAME)
+            ->willReturn($userLanguagePreference);
+
         $this->userLanguagePreferenceProvider = new UserLanguagePreferenceProvider(
             $this->requestStackMock,
-            $this->getLanguageCodesMap()
+            $this->userPreferenceServiceMock,
+            $this->getLanguageCodesMap(),
+            self::LOCALE_FALLBACK
         );
     }
 
@@ -43,7 +68,7 @@ class UserLanguagePreferenceProviderTest extends TestCase
      * @param array $userLanguages
      * @param array $expectedEzLanguageCodes
      */
-    public function testGetPreferredLanguages(array $userLanguages, array $expectedEzLanguageCodes)
+    public function testGetPreferredLanguagesWithoutUserLanguage(array $userLanguages, array $expectedEzLanguageCodes): void
     {
         $request = new Request();
         $request->headers = new HeaderBag(
@@ -57,9 +82,55 @@ class UserLanguagePreferenceProviderTest extends TestCase
             ->method('getCurrentRequest')
             ->willReturn($request);
 
+        $userPreferenceServiceMock = $this->createMock(UserPreferenceService::class);
+        $userPreferenceServiceMock
+            ->method('getUserPreference')
+            ->with(self::LANGUAGE_PREFERENCE_NAME)
+            ->will($this->throwException(new NotFoundException('User Preference', self::LANGUAGE_PREFERENCE_NAME)));
+
+        $userLanguagePreferenceProvider = new UserLanguagePreferenceProvider(
+            $this->requestStackMock,
+            $userPreferenceServiceMock,
+            $this->getLanguageCodesMap(),
+            self::LOCALE_FALLBACK
+        );
+
         self::assertEquals(
             $expectedEzLanguageCodes,
-            $this->userLanguagePreferenceProvider->getPreferredLanguages()
+            $userLanguagePreferenceProvider->getPreferredLanguages()
+        );
+    }
+
+    /**
+     * @dataProvider providerForTestGetPreferredLanguagesWithUserPreferredLanguage
+     *
+     * @param array $userLanguages
+     * @param array $expectedEzLanguageCodes
+     */
+    public function testGetPreferredLanguagesWithUserPreferredLanguage(array $userLanguages, array $expectedEzLanguageCodes): void
+    {
+        $request = new Request();
+        $request->headers = new HeaderBag(
+            [
+                'Accept-Language' => implode(', ', $userLanguages),
+            ]
+        );
+        $this
+            ->requestStackMock
+            ->expects($this->once())
+            ->method('getCurrentRequest')
+            ->willReturn($request);
+
+        $userLanguagePreferenceProvider = new UserLanguagePreferenceProvider(
+            $this->requestStackMock,
+            $this->userPreferenceServiceMock,
+            $this->getLanguageCodesMap(),
+            self::LOCALE_FALLBACK
+        );
+
+        self::assertEquals(
+            $expectedEzLanguageCodes,
+            $userLanguagePreferenceProvider->getPreferredLanguages()
         );
     }
 
@@ -71,10 +142,27 @@ class UserLanguagePreferenceProviderTest extends TestCase
     public function providerForTestGetPreferredLanguages(): array
     {
         return [
+            [[], ['eng-GB', 'eng-US']],
             [['pl'], ['pol-PL']],
-            [['pol-PL'], ['pol-PL']],
             [['fr'], ['fre-FR']],
             [['en'], ['eng-GB', 'eng-US']],
+            [['en_us'], ['eng-US']],
+        ];
+    }
+
+    /**
+     * @see testGetPreferredLanguages
+     *
+     * @return array
+     */
+    public function providerForTestGetPreferredLanguagesWithUserPreferredLanguage(): array
+    {
+        return [
+            [[], ['nor-NO', 'eng-GB', 'eng-US']],
+            [['pl'], ['nor-NO', 'pol-PL']],
+            [['fr'], ['nor-NO', 'fre-FR']],
+            [['en'], ['nor-NO', 'eng-GB', 'eng-US']],
+            [['en_us'], ['nor-NO', 'eng-US']],
         ];
     }
 
