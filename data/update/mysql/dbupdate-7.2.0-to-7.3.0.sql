245d2142983062a55fdbab515390c52dcce15420
Serhey Dolgushev <dolgushev.serhey@gmail.com>
2019-01-26 13:51:42 +0000

    SQL Update to fix "The maximum column size is 767 bytes" error

--- data/update/mysql/dbupdate-7.2.0-to-7.3.0.sql
+++ data/update/mysql/dbupdate-7.2.0-to-7.3.0.sql
@@ -7,3 +7,10 @@ UPDATE ezsite_data SET value='7.3.0' WHERE name='ezpublish-version';
 --
 
 ALTER TABLE ezcontentobject_trash add  trashed int(11) NOT NULL DEFAULT '0';
+
+--
+-- PR-2495: Fixing The maximum column size is 767 bytes.
+--
+
+ALTER TABLE `ezcontentobject_tree` DROP KEY `ezcontentobject_tree_contentobject_id_path_string`;
+ALTER TABLE `ezcontentobject_tree` ADD KEY `ezcontentobject_tree_contentobject_id_path_string` (`path_string` (191), `contentobject_id`);
