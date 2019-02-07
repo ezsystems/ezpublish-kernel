2365beca40bafa65ac1695e64a0b599c59500bac
Mikolaj Adamczyk <mikadamczyk@gmail.com>
2019-02-07 13:08:57 +0100

    EZP-29961: Add preferred language value to User Preferences (#2516)

--- eZ/Publish/Core/MVC/Symfony/Locale/UserLanguagePreferenceProvider.php
+++ eZ/Publish/Core/MVC/Symfony/Locale/UserLanguagePreferenceProvider.php
@@ -10,6 +10,8 @@ namespace eZ\Publish\Core\MVC\Symfony\Locale;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
+use eZ\Publish\API\Repository\UserPreferenceService;
+use eZ\Publish\API\Repository\Exceptions\NotFoundException;
 
 class UserLanguagePreferenceProvider implements UserLanguagePreferenceProviderInterface
 {
@@ -19,52 +21,71 @@ class UserLanguagePreferenceProvider implements UserLanguagePreferenceProviderIn
     private $requestStack;
 
     /**
+     * @var \eZ\Publish\API\Repository\UserPreferenceService
+     */
+    private $userPreferenceService;
+
+    /**
      * @var array
      */
     private $languageCodesMap;
 
     /**
-     * @param \Symfony\Component\HttpFoundation\RequestStack $requestStack
-     * @param array $languageCodesMap
+     * @var string
      */
-    public function __construct(RequestStack $requestStack, array $languageCodesMap)
-    {
+    private $localeFallback;
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\RequestStack $requestStack
+     * @param \eZ\Publish\API\Repository\UserPreferenceService $userPreferenceService
+     * @param array $languageCodesMap
+     * @param string $localeFallback
+     */
+    public function __construct(
+        RequestStack $requestStack,
+        UserPreferenceService $userPreferenceService,
+        array $languageCodesMap,
+        string $localeFallback
+    ) {
         $this->requestStack = $requestStack;
+        $this->userPreferenceService = $userPreferenceService;
         $this->languageCodesMap = $languageCodesMap;
+        $this->localeFallback = $localeFallback;
     }
 
     public function getPreferredLocales(Request $request = null): array
     {
+        $languages = [$this->localeFallback];
+
         $request = $request ?? $this->requestStack->getCurrentRequest();
-        $preferredLocales = $request->headers->get('Accept-Language') ?? '';
-        $preferredLocales = array_reduce(
-            explode(',', $preferredLocales),
-            function (array $result, string $languageWithQuality) {
-                [$language, $quality] = array_merge(explode(';q=', $languageWithQuality), [1]);
-                $result[$language] = (float) $quality;
+        if (null !== $request) {
+            $browserLanguages = $request->getLanguages();
+            if ([] !== $browserLanguages) {
+                $languages = $browserLanguages;
+            }
+        }
 
-                return $result;
-            },
-            []
-        );
-        arsort($preferredLocales);
+        try {
+            $preferredLanguage = $this->userPreferenceService->getUserPreference('language')->value;
+            array_unshift($languages, $preferredLanguage);
+        } catch (NotFoundException $e) {
+        }
 
-        return array_keys($preferredLocales);
+        return array_unique($languages);
     }
 
     public function getPreferredLanguages(): array
     {
-        $languageCodes = [];
+        $languageCodes = [[]];
         foreach ($this->getPreferredLocales() as $locale) {
             $locale = strtolower($locale);
-            if (isset($this->languageCodesMap[$locale])) {
-                $languageCodes = array_merge($languageCodes, $this->languageCodesMap[$locale]);
-            } elseif (preg_match('/^([a-z]{3})-([a-z]{2})$/', $locale, $matches)) {
-                // if the given locale is already in the eZ format
-                $languageCodes[] = strtolower($matches[1]) . '-' . strtoupper($matches[2]);
+            if (!isset($this->languageCodesMap[$locale])) {
+                continue;
             }
+
+            $languageCodes[] = $this->languageCodesMap[$locale];
         }
 
-        return array_unique($languageCodes);
+        return array_unique(array_merge(...$languageCodes));
     }
 }
