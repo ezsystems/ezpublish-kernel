85a207a3b10d5d706a7e03560e7724c108d6642c
Kamil Madejski <kmadejski@live.com>
2019-02-15 13:44:17 +0100

    EZP-29466: As a Developer, I would like to have configuration for absolute/relative URLs for generated images (#2511)
    * EZP-29466: As a Developer, I would like to have configuration for absolute/relative URLs for generated images
    
    * Added unit tests
    
    * fixup! EZP-29466: As a Developer, I would like to have configuration for absolute/relative URLs for generated images
    
    * Simplified ResolverFactory
    
    * fixup! fixup! EZP-29466: As a Developer, I would like to have configuration for absolute/relative URLs for generated images
    
    * fixup! fixup! fixup! EZP-29466: As a Developer, I would like to have configuration for absolute/relative URLs for generated images

---
+++ eZ/Bundle/EzPublishCoreBundle/Imagine/Cache/ResolverFactory.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace eZ\Bundle\EzPublishCoreBundle\Imagine\Cache;
+
+use eZ\Publish\Core\MVC\ConfigResolverInterface;
+use Liip\ImagineBundle\Imagine\Cache\Resolver\ResolverInterface;
+
+class ResolverFactory
+{
+    /**
+     * @var \eZ\Publish\Core\MVC\ConfigResolverInterface
+     */
+    private $configResolver;
+
+    /**
+     * @var \Liip\ImagineBundle\Imagine\Cache\Resolver\ResolverInterface
+     */
+    private $resolver;
+
+    /**
+     * @var string|null
+     */
+    private $resolverDecoratorClass;
+
+    /**
+     * @var string
+     */
+    private $proxyResolverClass;
+
+    /**
+     * @var string
+     */
+    private $relativeResolverClass;
+
+    /**
+     * @param \eZ\Publish\Core\MVC\ConfigResolverInterface $configResolver
+     * @param \Liip\ImagineBundle\Imagine\Cache\Resolver\ResolverInterface $resolver
+     * @param string $proxyResolverClass
+     * @param string $relativeResolverClass
+     */
+    public function __construct(
+        ConfigResolverInterface $configResolver,
+        ResolverInterface $resolver,
+        $proxyResolverClass,
+        $relativeResolverClass
+    ) {
+        $this->configResolver = $configResolver;
+        $this->resolver = $resolver;
+        $this->proxyResolverClass = $proxyResolverClass;
+        $this->relativeResolverClass = $relativeResolverClass;
+    }
+
+    /**
+     * @return \Liip\ImagineBundle\Imagine\Cache\Resolver\ResolverInterface
+     */
+    public function createCacheResolver()
+    {
+        $imageHost = $this->configResolver->hasParameter('image_host') ?
+            $this->configResolver->getParameter('image_host') :
+            '';
+
+        if ($imageHost === '') {
+            return $this->resolver;
+        }
+
+        if ($imageHost === '/') {
+            $this->resolverDecoratorClass = $this->relativeResolverClass;
+        } else {
+            $this->resolverDecoratorClass = $this->proxyResolverClass;
+        }
+
+        return new $this->resolverDecoratorClass($this->resolver, [$imageHost]);
+    }
+}
