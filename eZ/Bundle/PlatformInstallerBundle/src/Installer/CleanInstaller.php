a484bddc481f84d7d0e2f3e2dbb9d88b59657360
Andrew Longosz <alongosz@users.noreply.github.com>
2019-03-09 20:56:22 +0100

    EZP-29938: Restored upstream version of CleanInstaller, DbBasedInstaller
    Changes to these classes were a potential BC break.
    
    CleanInstaller is now deprecated.

--- eZ/Bundle/PlatformInstallerBundle/src/Installer/CleanInstaller.php
+++ eZ/Bundle/PlatformInstallerBundle/src/Installer/CleanInstaller.php
@@ -8,10 +8,24 @@
  */
 namespace EzSystems\PlatformInstallerBundle\Installer;
 
+/**
+ * @deprecated since 7.5. Use CoreInstaller instead.
+ */
 class CleanInstaller extends DbBasedInstaller implements Installer
 {
     public function createConfiguration()
     {
+    }
+
+    public function importSchema()
+    {
+        @trigger_error(
+            'CleanInstaller is deprecated since 7.5 and will be removed in 8.0. Use CoreInstaller instead.',
+            E_USER_DEPRECATED
+        );
+
+        $this->runQueriesFromFile($this->getKernelSQLFileForDBMS('schema.sql'));
+        $this->runQueriesFromFile($this->getKernelSQLFileForDBMS('dfs_schema.sql'));
     }
 
     public function importData()
--- eZ/Bundle/PlatformInstallerBundle/src/Installer/DbBasedInstaller.php
+++ eZ/Bundle/PlatformInstallerBundle/src/Installer/DbBasedInstaller.php
@@ -9,11 +9,6 @@
 namespace EzSystems\PlatformInstallerBundle\Installer;
 
 use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\DBALException;
-use Doctrine\DBAL\Platforms\AbstractPlatform;
-use Doctrine\DBAL\Schema\Schema;
-use EzSystems\DoctrineSchema\API\Builder\SchemaBuilder;
-use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Filesystem\Filesystem;
 use eZ\Publish\Core\Base\Exceptions\InvalidArgumentException;
 
@@ -34,11 +29,9 @@ class DbBasedInstaller
      */
     protected $baseDataDir;
 
-    public function __construct(Connection $db, SchemaBuilder $schemaBuilder)
+    public function __construct(Connection $db)
     {
         $this->db = $db;
-        $this->schemaBuilder = $schemaBuilder;
-
         // parametrized so other installer implementations can override this
         $this->baseDataDir = __DIR__ . '/../../../../../data';
     }
@@ -49,80 +42,6 @@ class DbBasedInstaller
     public function setOutput($output)
     {
         $this->output = $output;
-    }
-
-    /**
-     * Import Schema using event-driven Schema Builder API from eZ Systems DoctrineSchema Bundle.
-     *
-     * If you wish to extend schema, implement your own EventSubscriber
-     *
-     * @see \EzSystems\DoctrineSchema\API\Builder\SchemaBuilderEvent
-     * @see \EzSystems\PlatformInstallerBundle\Event\Subscriber\BuildSchemaSubscriber
-     *
-     * @throws \Doctrine\DBAL\DBALException
-     */
-    public function importSchema()
-    {
-        // note: schema is built using Schema Builder event-driven API
-        $schema = $this->schemaBuilder->buildSchema();
-        $databasePlatform = $this->db->getDatabasePlatform();
-        $queries = array_merge(
-            $this->getDropSqlStatementsForExistingSchema($schema, $databasePlatform),
-            // generate schema DDL queries
-            $schema->toSql($databasePlatform)
-        );
-
-        $queriesCount = count($queries);
-        $this->output->writeln(
-            sprintf(
-                '<info>Executing %d queries on database <comment>%s</comment> (<comment>%s</comment>)</info>',
-                $queriesCount,
-                $this->db->getDatabase(),
-                $this->db->getDatabasePlatform()->getName()
-            )
-        );
-        $progressBar = new ProgressBar($this->output);
-        $progressBar->start($queriesCount);
-
-        try {
-            $this->db->beginTransaction();
-            foreach ($queries as $query) {
-                $this->db->exec($query);
-                $progressBar->advance(1);
-            }
-            $this->db->commit();
-        } catch (DBALException $e) {
-            $this->db->rollBack();
-            throw $e;
-        }
-
-        $progressBar->finish();
-        // go to the next line after ProgressBar::finish
-        $this->output->writeln('');
-    }
-
-    /**
-     * @param \Doctrine\DBAL\Schema\Schema $newSchema
-     * @param \Doctrine\DBAL\Platforms\AbstractPlatform $databasePlatform
-     *
-     * @return string[]
-     */
-    protected function getDropSqlStatementsForExistingSchema(
-        Schema $newSchema,
-        AbstractPlatform $databasePlatform
-    ): array {
-        $existingSchema = $this->db->getSchemaManager()->createSchema();
-        $statements = [];
-        // reverse table order for clean-up (due to FKs)
-        $tables = array_reverse($newSchema->getTables());
-        // cleanup pre-existing database
-        foreach ($tables as $table) {
-            if ($existingSchema->hasTable($table->getName())) {
-                $statements[] = $databasePlatform->getDropTableSQL($table);
-            }
-        }
-
-        return $statements;
     }
 
     /**
