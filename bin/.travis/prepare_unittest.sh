2ff8110ebb605fa870724e9e34d0c5f81c014fbb
André R <ar@ez.no>
2019-03-25 11:08:46 +0100

    Make sure mysql8 also gets the perf tweaks

--- bin/.travis/prepare_unittest.sh
+++ bin/.travis/prepare_unittest.sh
@@ -35,22 +35,23 @@ if [ "$DB" = "mysql" ] || [ "$DB" = "mariadb" ] ; then
         sudo systemctl restart mysql
         sudo mysql_upgrade
         mysql --version
-        mysql -e "CREATE DATABASE IF NOT EXISTS testdb DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci;" -uroot
-    else
-        # https://github.com/travis-ci/travis-ci/issues/3049
-        # make sure we don't run out of entropy apparently (see link above)
-        sudo apt-get -y install haveged
-        sudo service haveged start
-        # make tmpfs and run MySQL on it for reasonable performance
-        sudo mkdir /mnt/ramdisk
-        sudo mount -t tmpfs -o size=1024m tmpfs /mnt/ramdisk
-        sudo stop mysql
-        sudo mv /var/lib/mysql /mnt/ramdisk
-        sudo ln -s /mnt/ramdisk/mysql /var/lib/mysql
-        sudo start mysql
-        # Install test db
-        mysql -e "CREATE DATABASE IF NOT EXISTS testdb DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci;" -uroot
     fi
+
+    # https://github.com/travis-ci/travis-ci/issues/3049
+    # make sure we don't run out of entropy apparently (see link above)
+    sudo apt-get -y install haveged
+    sudo service haveged start
+
+    # make tmpfs and run MySQL on it for reasonable performance
+    sudo mkdir /mnt/ramdisk
+    sudo mount -t tmpfs -o size=1024m tmpfs /mnt/ramdisk
+    sudo systemctl stop mysql
+    sudo mv /var/lib/mysql /mnt/ramdisk
+    sudo ln -s /mnt/ramdisk/mysql /var/lib/mysql
+    sudo systemctl start mysql
+
+    # Install test db
+    mysql -e "CREATE DATABASE IF NOT EXISTS testdb DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci;" -uroot
 fi
 if [ "$DB" = "postgresql" ] ; then psql -c "CREATE DATABASE testdb;" -U postgres ; psql -c "CREATE EXTENSION pgcrypto;" -U postgres testdb ; fi
 
