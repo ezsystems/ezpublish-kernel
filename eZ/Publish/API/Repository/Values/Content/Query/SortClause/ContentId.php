3e677047b02b332f954cd3e8a5eb792c826136f7
Andrew Longosz <alongosz@users.noreply.github.com>
2019-05-06 15:18:56 +0200

    [Doc] Dropped remaining Elasticsearch references from PhpDocs

--- eZ/Publish/API/Repository/Values/Content/Query/SortClause/ContentId.php
+++ eZ/Publish/API/Repository/Values/Content/Query/SortClause/ContentId.php
@@ -17,8 +17,8 @@ use eZ\Publish\API\Repository\Values\Content\Query\SortClause;
  * Especially useful to get reproducible search results in tests.
  *
  * Note: order will vary per search engine, depending on how Content ID is stored in the search
- * backend. For Legacy search engine IDs are stored as integers, while with Solr and Elasticsearch
- * engines they are stored as strings. Hence the difference will be basically the one between
+ * backend. For Legacy search engine IDs are stored as integers, while with Solr search engine
+ * they are stored as strings. Hence the difference will be basically the one between
  * numerical and alphabetical order of sorting.
  *
  * This reflects API definition of IDs as mixed type (integer or string).
