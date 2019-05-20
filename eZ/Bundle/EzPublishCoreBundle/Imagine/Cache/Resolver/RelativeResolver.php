a3d9b0b8f0079a5806386d0ed725dc58d0039a1f
André R <ar@ez.no>
2019-05-20 12:58:21 +0200

    EZP-25721: 7.x fix for early loaded ez..imagine.cache_resolver_decorator

--- eZ/Bundle/EzPublishCoreBundle/Imagine/Cache/Resolver/RelativeResolver.php
+++ eZ/Bundle/EzPublishCoreBundle/Imagine/Cache/Resolver/RelativeResolver.php
@@ -6,54 +6,20 @@
  */
 namespace eZ\Bundle\EzPublishCoreBundle\Imagine\Cache\Resolver;
 
-use Liip\ImagineBundle\Binary\BinaryInterface;
 use Liip\ImagineBundle\Imagine\Cache\Resolver\ResolverInterface;
+use Liip\ImagineBundle\Imagine\Cache\Resolver\ProxyResolver as ImagineProxyResolver;
 
-class RelativeResolver implements ResolverInterface
+/**
+ * Relative resolver, omits host info.
+ */
+class RelativeResolver extends ImagineProxyResolver
 {
-    /**
-     * @var \Liip\ImagineBundle\Imagine\Cache\Resolver\ResolverInterface
-     */
-    private $resolver;
-
     /**
      * @param \Liip\ImagineBundle\Imagine\Cache\Resolver\ResolverInterface $resolver
      */
     public function __construct(ResolverInterface $resolver)
     {
-        $this->resolver = $resolver;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function isStored($path, $filter)
-    {
-        return $this->resolver->isStored($path, $filter);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function resolve($path, $filter)
-    {
-        return $this->rewriteUrl($this->resolver->resolve($path, $filter));
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function store(BinaryInterface $binary, $path, $filter)
-    {
-        return $this->resolver->store($binary, $path, $filter);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function remove(array $paths, array $filters)
-    {
-        return $this->resolver->remove($paths, $filters);
+        parent::__construct($resolver, []);
     }
 
     /**
