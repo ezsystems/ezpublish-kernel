b6a1868d9298d8de723fc56677dcbf8828c45c37
Nattfarinn <nattfarinn@gmail.com>
2019-05-29 15:05:44 -0700

    EZP-30616: new FieldType service

---
+++ eZ/Publish/Core/Event/FieldTypeService.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace eZ\Publish\Core\Event;
+
+use eZ\Publish\SPI\Repository\Decorator\FieldTypeServiceDecorator;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use eZ\Publish\API\Repository\FieldTypeService as FieldTypeServiceInterface;
+
+class FieldTypeService extends FieldTypeServiceDecorator implements FieldTypeServiceInterface
+{
+    /**
+     * @var \Symfony\Component\EventDispatcher\EventDispatcherInterface
+     */
+    protected $eventDispatcher;
+
+    public function __construct(
+        FieldTypeServiceInterface $innerService,
+        EventDispatcherInterface $eventDispatcher
+    ) {
+        parent::__construct($innerService);
+
+        $this->eventDispatcher = $eventDispatcher;
+    }
+}
