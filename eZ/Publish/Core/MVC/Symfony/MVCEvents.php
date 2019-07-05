cbc6828f94a85d6c0d743f2bbf91c2a3e25ca31a
André R <andre.romcke@gmail.com>
2019-07-05 12:51:07 +0200

    Update eZ/Publish/Core/MVC/Symfony/MVCEvents.php
    Co-Authored-By: Adam Wójs <adam.wojs@ez.no>

--- eZ/Publish/Core/MVC/Symfony/MVCEvents.php
+++ eZ/Publish/Core/MVC/Symfony/MVCEvents.php
@@ -92,7 +92,7 @@ final class MVCEvents
      *
      * @Event("eZ\Publish\Core\MVC\Symfony\Event\ConsoleInitEvent")
      *
-     * @internal For internal use by SiteAccess system on console, might move to symfony in some form in the future.
+     * @internal For internal use by SiteAccess system on console.
      */
     const CONSOLE_INIT = 'ezpublish.console.init';
 }
