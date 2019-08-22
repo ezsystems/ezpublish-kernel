eaf486c0429c621ef2408f1a04216f3af6ae31c4
Adam Wójs <adam.wojs@ez.no>
2019-08-22 11:34:22 +0200

    EZP-30869: Added extension point in login form view (unit test for getter/setter)

--- eZ/Publish/Core/MVC/Symfony/View/Tests/LoginFormViewTest.php
+++ eZ/Publish/Core/MVC/Symfony/View/Tests/LoginFormViewTest.php
@@ -10,12 +10,33 @@ namespace eZ\Publish\Core\MVC\Symfony\View\Tests;
 
 use eZ\Publish\Core\MVC\Symfony\View\LoginFormView;
 use eZ\Publish\Core\MVC\Symfony\View\View;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 
 /**
  * @group mvc
  */
 final class LoginFormViewTest extends AbstractViewTest
 {
+    public function testSetLastUsername(): void
+    {
+        /** @var \eZ\Publish\Core\MVC\Symfony\View\LoginFormView $view */
+        $view = $this->createViewUnderTest();
+        $view->setLastUsername('johndoe');
+
+        $this->assertEquals('johndoe', $view->getLastUsername());
+    }
+
+    public function testSetLastAuthenticationError(): void
+    {
+        $exception = $this->createMock(AuthenticationException::class);
+
+        /** @var \eZ\Publish\Core\MVC\Symfony\View\LoginFormView $view */
+        $view = $this->createViewUnderTest();
+        $view->setLastAuthenticationError($exception);
+
+        $this->assertEquals($exception, $view->getLastAuthenticationException());
+    }
+
     protected function createViewUnderTest($template = null, array $parameters = [], $viewType = 'full'): View
     {
         return new LoginFormView($template, $parameters, $viewType);
