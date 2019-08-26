57e575a8aa034e0a4ce497c8a92087d60dafab7c
Maciej Kobus <maciej.kobus@ez.no>
2019-08-26 14:15:31 +0200

    Made changes after code review

--- eZ/Bundle/EzPublishCoreBundle/Matcher/ViewMatcherRegistry.php
+++ eZ/Bundle/EzPublishCoreBundle/Matcher/ViewMatcherRegistry.php
@@ -1,7 +1,10 @@
 <?php
 /**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
+declare(strict_types=1);
+
 namespace eZ\Bundle\EzPublishCoreBundle\Matcher;
 
 use eZ\Publish\Core\Base\Exceptions\NotFoundException;
