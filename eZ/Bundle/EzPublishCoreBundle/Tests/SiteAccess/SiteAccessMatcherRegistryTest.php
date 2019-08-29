8ff5bd1a2857042826335cb75237f508014f3484
Maciej Kobus <maciej.kobus@ez.no>
2019-08-29 10:14:05 +0200

    Fixed code style in SiteAccessMatcherRegistryTest

--- eZ/Bundle/EzPublishCoreBundle/Tests/SiteAccess/SiteAccessMatcherRegistryTest.php
+++ eZ/Bundle/EzPublishCoreBundle/Tests/SiteAccess/SiteAccessMatcherRegistryTest.php
@@ -1,8 +1,11 @@
 <?php
+
 /**
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
+declare(strict_types=1);
+
 namespace eZ\Bundle\EzPublishCoreBundle\SiteAccess;
 
 use eZ\Publish\API\Repository\Exceptions\NotFoundException;
