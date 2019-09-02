3120016089ad16105427e63a2090f13a3cfefa7a
Andrew Longosz <alongosz@users.noreply.github.com>
2019-09-02 16:21:23 +0200

    Added deprecation warnings for old service names
    * Context: usual "deprecated" DIC service attribute doesn't seem to
      be working for known use cases.

--- eZ/Bundle/PlatformInstallerBundle/src/DependencyInjection/Compiler/SchemaBuilderInstallerPass.php
+++ eZ/Bundle/PlatformInstallerBundle/src/DependencyInjection/Compiler/SchemaBuilderInstallerPass.php
@@ -10,8 +10,11 @@ namespace EzSystems\PlatformInstallerBundle\DependencyInjection\Compiler;
 
 use EzSystems\DoctrineSchema\API\Builder\SchemaBuilder;
 use EzSystems\PlatformInstallerBundle\Installer\CoreInstaller;
+use EzSystems\PlatformInstallerBundle\Installer\DbBasedInstaller;
+use Symfony\Component\DependencyInjection\ChildDefinition;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 
 /**
  * Enable installer which uses SchemaBuilder.
@@ -24,6 +27,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 class SchemaBuilderInstallerPass implements CompilerPassInterface
 {
     const CLEAN_INSTALLER_DEF_ID = 'ezplatform.installer.clean_installer';
+    const DB_BASED_INSTALLER_DEF_ID = 'ezplatform.installer.db_based_installer';
 
     /**
      * Replace Clean installer with CoreInstaller if required SchemaBuilder from DoctrineSchemaBundle is available.
@@ -48,6 +52,52 @@ class SchemaBuilderInstallerPass implements CompilerPassInterface
             // remove the actual definition first for alias to work properly
             $container->removeDefinition(self::CLEAN_INSTALLER_DEF_ID);
             $container->setAlias(self::CLEAN_INSTALLER_DEF_ID, CoreInstaller::class);
+        }
+
+        $this->warnAboutRelyingOnDeprecatedService(
+            $container,
+            [
+                self::DB_BASED_INSTALLER_DEF_ID => DbBasedInstaller::class,
+                self::CLEAN_INSTALLER_DEF_ID => CoreInstaller::class,
+            ]
+        );
+    }
+
+    /**
+     * Find usages of deprecated service definitions in custom services.
+     *
+     * Note: natural choice would be to use `deprecated` attribute in DIC instead, however
+     * in Symfony 3.4 neither deprecating service alias nor deprecating abstract service gives
+     * proper warning in the application logs.
+     *
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     * @param string[] a map of old to new names (associative array)
+     */
+    private function warnAboutRelyingOnDeprecatedService(
+        ContainerBuilder $container,
+        array $oldNewNameMap
+    ): void {
+        // find usages of deprecated definitions as parents
+        /** @var \Symfony\Component\DependencyInjection\ChildDefinition[] $deprecatedParentDefinitions */
+        $deprecatedParentDefinitions = array_filter(
+            $container->getDefinitions(),
+            function (Definition $definition) use ($oldNewNameMap) {
+                return $definition instanceof ChildDefinition
+                    && array_key_exists($definition->getParent(), $oldNewNameMap);
+            }
+        );
+        // trigger deprecation warnings to be logged
+        foreach ($deprecatedParentDefinitions as $id => $definition) {
+            $parent = $definition->getParent();
+            @trigger_error(
+                sprintf(
+                    'The service definition "%s" relies on the deprecated "%s" service. Use "%s" instead',
+                    $id,
+                    $parent,
+                    $oldNewNameMap[$parent]
+                ),
+                E_USER_DEPRECATED
+            );
         }
     }
 }
