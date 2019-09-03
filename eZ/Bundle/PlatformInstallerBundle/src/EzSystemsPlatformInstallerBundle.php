fd343c948d3e90e7c57d25ac63d523858f1849b3
Andrew Longosz <alongosz@users.noreply.github.com>
2019-09-03 14:20:09 +0200

    [DX] Added checking if the required Doctrine Schema Bundle is enabled

--- eZ/Bundle/PlatformInstallerBundle/src/EzSystemsPlatformInstallerBundle.php
+++ eZ/Bundle/PlatformInstallerBundle/src/EzSystemsPlatformInstallerBundle.php
@@ -8,14 +8,28 @@
  */
 namespace EzSystems\PlatformInstallerBundle;
 
+use EzSystems\DoctrineSchemaBundle\DoctrineSchemaBundle;
 use EzSystems\PlatformInstallerBundle\DependencyInjection\Compiler\InstallerTagPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Exception\RuntimeException;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EzSystemsPlatformInstallerBundle extends Bundle
 {
+    /**
+     * @throws \RuntimeException
+     */
     public function build(ContainerBuilder $container)
     {
+        if (!$container->hasExtension('ez_doctrine_schema')) {
+            throw new RuntimeException(
+                sprintf(
+                    'eZ Platform Installer requires Doctrine Schema Bundle (enable %s)',
+                    DoctrineSchemaBundle::class
+                )
+            );
+        }
+
         parent::build($container);
         $container->addCompilerPass(new InstallerTagPass());
     }
