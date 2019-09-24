965345ff821ca19850558af2cf8885d698aa7cdc
André R <andre.romcke@gmail.com>
2019-09-24 11:04:21 +0200

    EZP-30823: [6.x] Isolated cache during transactions to avoid race condition (#2740)
    * EZP-30823: Isolate cache during transactions to avoid race condition issues
    
    * [TEST] Test to trigger issues on trasnaction isolation on process itself
    
    This is unfortunately not covering need to test cross process issues with this, but
    it at least tries to make sure own process recives fresh dfata when it should.
    
    * Fix review comments
    
    (cherry picked from commit f97c2697716c481d3f44731af9ecc8e8b310aa28)

--- eZ/Publish/Core/Persistence/Cache/Tests/TransactionHandlerTest.php
+++ eZ/Publish/Core/Persistence/Cache/Tests/TransactionHandlerTest.php
@@ -25,8 +25,8 @@ class TransactionHandlerTest extends HandlerTest
             ->method('logCall');
 
         $this->cacheMock
-            ->expects($this->never())
-            ->method($this->anything());
+            ->expects($this->once())
+            ->method('beginTransaction');
 
         $innerHandlerMock = $this->createMock(TransactionHandler::class);
         $this->persistenceHandlerMock
@@ -52,8 +52,8 @@ class TransactionHandlerTest extends HandlerTest
             ->method('logCall');
 
         $this->cacheMock
-            ->expects($this->never())
-            ->method($this->anything());
+            ->expects($this->once())
+            ->method('commitTransaction');
 
         $innerHandlerMock = $this->createMock(TransactionHandler::class);
         $this->persistenceHandlerMock
@@ -79,10 +79,14 @@ class TransactionHandlerTest extends HandlerTest
             ->method('logCall');
 
         $this->cacheMock
-            ->expects($this->once())
+            ->expects($this->never())
             ->method('clear');
 
-        $innerHandlerMock = $this->createMock(TransactionHandler::class);
+        $this->cacheMock
+            ->expects($this->once())
+            ->method('rollbackTransaction');
+
+         $innerHandlerMock = $this->createMock(TransactionHandler::class);
         $this->persistenceHandlerMock
             ->expects($this->once())
             ->method('transactionHandler')
