c1deb54afa8ed83f1fc9184ab395a1a765b13ebc
Edi Modrić <edi.modric@gmail.com>
2019-10-02 15:27:21 +0200

    Decorate locale listener instead of extending it

--- eZ/Bundle/EzPublishCoreBundle/EventListener/LocaleListener.php
+++ eZ/Bundle/EzPublishCoreBundle/EventListener/LocaleListener.php
@@ -10,37 +10,39 @@ namespace eZ\Bundle\EzPublishCoreBundle\EventListener;
 
 use eZ\Publish\Core\MVC\ConfigResolverInterface;
 use eZ\Publish\Core\MVC\Symfony\Locale\LocaleConverterInterface;
-use Symfony\Component\HttpKernel\EventListener\LocaleListener as BaseRequestListener;
-use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\Event\FinishRequestEvent;
+use Symfony\Component\HttpKernel\Event\KernelEvent;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\EventListener\LocaleListener as BaseLocaleListener;
 
 /**
  * Enhanced LocaleListener, injecting the converted locale extracted from eZ Publish configuration.
  */
-class LocaleListener extends BaseRequestListener
+class LocaleListener implements EventSubscriberInterface
 {
+    /** @var \Symfony\Component\HttpKernel\EventListener\LocaleListener */
+    private $innerListener;
+
     /** @var \eZ\Publish\Core\MVC\ConfigResolverInterface */
     private $configResolver;
 
     /** @var \eZ\Publish\Core\MVC\Symfony\Locale\LocaleConverterInterface */
     private $localeConverter;
 
-    /**
-     * @param \eZ\Publish\Core\MVC\ConfigResolverInterface $configResolver
-     */
-    public function setConfigResolver(ConfigResolverInterface $configResolver)
+    public function __construct(BaseLocaleListener $innerListener, ConfigResolverInterface $configResolver, LocaleConverterInterface $localeConverter)
     {
+        $this->innerListener = $innerListener;
         $this->configResolver = $configResolver;
-    }
-
-    /**
-     * @param \eZ\Publish\Core\MVC\Symfony\Locale\LocaleConverterInterface $localeConverter
-     */
-    public function setLocaleConverter(LocaleConverterInterface $localeConverter)
-    {
         $this->localeConverter = $localeConverter;
     }
 
-    public function onKernelRequest(GetResponseEvent $event)
+    public static function getSubscribedEvents(): array
+    {
+        return BaseLocaleListener::getSubscribedEvents();
+    }
+
+    public function onKernelRequest(RequestEvent $event): void
     {
         $request = $event->getRequest();
         if (!$request->attributes->has('_locale')) {
@@ -54,6 +56,16 @@ class LocaleListener extends BaseRequestListener
             }
         }
 
-        parent::onKernelRequest($event);
+        $this->innerListener->onKernelRequest($event);
+    }
+
+    public function onKernelFinishRequest(FinishRequestEvent $event): void
+    {
+        $this->innerListener->onKernelFinishRequest($event);
+    }
+
+    public function setDefaultLocale(KernelEvent $event): void
+    {
+        $this->innerListener->setDefaultLocale($event);
     }
 }
