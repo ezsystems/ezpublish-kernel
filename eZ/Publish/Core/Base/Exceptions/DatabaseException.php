12cc07643545c82d349c62e52acacf1c29659bfd
Mikołaj Adamczyk <mikadamczyk@gmail.com>
2019-10-04 11:42:56 +0200

    [CR] Added DatabaseException to replace RuntimeException occurrence in Gateway class

--- eZ/Publish/Core/Base/Exceptions/DatabaseException.php
+++ eZ/Publish/Core/Base/Exceptions/DatabaseException.php
@@ -1,19 +1,28 @@
 <?php
 
 /**
- * Contains Database Exception implementation.
- *
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
+declare(strict_types=1);
+
 namespace eZ\Publish\Core\Base\Exceptions;
 
 use RuntimeException;
+use Throwable;
 
 /**
  * An exception that is thrown when the database encounters an error.
  */
 final class DatabaseException extends RuntimeException
 {
-    public const MESSAGE = 'Database error';
+    public const DEFAULT_MESSAGE = 'Database error';
+
+    public static function wrap(
+        Throwable $previous,
+        string $message = self::DEFAULT_MESSAGE,
+        int $code = 0
+    ): self {
+        return new self($message, $code, $previous);
+    }
 }
