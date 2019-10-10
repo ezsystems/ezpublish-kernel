d5e60821f72733abf61b1c3679a63a24582ff634
Mikołaj Adamczyk <mikadamczyk@gmail.com>
2019-10-10 15:02:02 +0200

    Removed docbloks which are same as in the parent class

--- eZ/Publish/Core/Persistence/Legacy/Content/Location/Gateway/ExceptionConversion.php
+++ eZ/Publish/Core/Persistence/Legacy/Content/Location/Gateway/ExceptionConversion.php
@@ -8,7 +8,6 @@
  */
 namespace eZ\Publish\Core\Persistence\Legacy\Content\Location\Gateway;
 
-use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
 use eZ\Publish\Core\Base\Exceptions\DatabaseException;
 use eZ\Publish\Core\Persistence\Legacy\Content\Location\Gateway;
 use eZ\Publish\SPI\Persistence\Content\Location\UpdateStruct;
@@ -38,9 +37,6 @@ class ExceptionConversion extends Gateway
         $this->innerGateway = $innerGateway;
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function getBasicNodeData($nodeId, array $translations = null, bool $useAlwaysAvailable = true)
     {
         try {
@@ -50,9 +46,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function getNodeDataList(array $locationIds, array $translations = null, bool $useAlwaysAvailable = true): iterable
     {
         try {
@@ -62,9 +55,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function getBasicNodeDataByRemoteId($remoteId, array $translations = null, bool $useAlwaysAvailable = true)
     {
         try {
@@ -74,15 +64,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * Loads data for all Locations for $contentId, optionally only in the
-     * subtree starting at $rootLocationId.
-     *
-     * @param int $contentId
-     * @param int $rootLocationId
-     *
-     * @return array
-     */
     public function loadLocationDataByContent($contentId, $rootLocationId = null)
     {
         try {
@@ -92,9 +73,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * @see \eZ\Publish\Core\Persistence\Legacy\Content\Location\Gateway::loadParentLocationsDataForDraftContent
-     */
     public function loadParentLocationsDataForDraftContent($contentId)
     {
         try {
@@ -104,14 +82,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * Find all content in the given subtree.
-     *
-     * @param mixed $sourceId
-     * @param bool $onlyIds
-     *
-     * @return array
-     */
     public function getSubtreeContent($sourceId, $onlyIds = false)
     {
         try {
@@ -121,13 +91,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * Returns data for the first level children of the location identified by given $locationId.
-     *
-     * @param mixed $locationId
-     *
-     * @return array
-     */
     public function getChildren($locationId)
     {
         try {
@@ -137,17 +100,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * Update path strings to move nodes in the ezcontentobject_tree table.
-     *
-     * This query can likely be optimized to use some more advanced string
-     * operations, which then depend on the respective database.
-     *
-     * @todo optimize
-     *
-     * @param array $fromPathString
-     * @param array $toPathString
-     */
     public function moveSubtreeNodes(array $fromPathString, array $toPathString)
     {
         try {
@@ -157,12 +109,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * Updated subtree modification time for all nodes on path.
-     *
-     * @param string $pathString
-     * @param int|null $timestamp
-     */
     public function updateSubtreeModificationTime($pathString, $timestamp = null)
     {
         try {
@@ -172,14 +118,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * Update node assignment table.
-     *
-     * @param int $contentObjectId
-     * @param int $oldParent
-     * @param int $newParent
-     * @param int $opcode
-     */
     public function updateNodeAssignment($contentObjectId, $oldParent, $newParent, $opcode)
     {
         try {
@@ -189,14 +127,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * Create locations from node assignments.
-     *
-     * Convert existing node assignments into real locations.
-     *
-     * @param mixed $contentId
-     * @param mixed $versionNo
-     */
     public function createLocationsFromNodeAssignments($contentId, $versionNo)
     {
         try {
@@ -206,12 +136,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * Updates all Locations of content identified with $contentId with $versionNo.
-     *
-     * @param mixed $contentId
-     * @param mixed $versionNo
-     */
     public function updateLocationsContentVersionNo($contentId, $versionNo)
     {
         try {
@@ -221,11 +145,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * Sets a location to be hidden, and it self + all children to invisible.
-     *
-     * @param string $pathString
-     */
     public function hideSubtree($pathString)
     {
         try {
@@ -235,12 +154,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * Sets a location to be unhidden, and self + children to visible unless a parent is hiding the tree.
-     * If not make sure only children down to first hidden node is marked visible.
-     *
-     * @param string $pathString
-     */
     public function unHideSubtree($pathString)
     {
         try {
@@ -250,9 +163,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * @param string $pathString
-     **/
     public function setNodeWithChildrenInvisible(string $pathString): void
     {
         try {
@@ -262,9 +172,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * @param string $pathString
-     **/
     public function setNodeHidden(string $pathString): void
     {
         try {
@@ -274,9 +181,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * @param string $pathString
-     **/
     public function setNodeWithChildrenVisible(string $pathString): void
     {
         try {
@@ -286,9 +190,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * @param string $pathString
-     **/
     public function setNodeUnhidden(string $pathString): void
     {
         try {
@@ -298,17 +199,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * Swaps the content object being pointed to by a location object.
-     *
-     * Make the location identified by $locationId1 refer to the Content
-     * referred to by $locationId2 and vice versa.
-     *
-     * @param int $locationId1
-     * @param int $locationId2
-     *
-     * @return bool
-     */
     public function swap(int $locationId1, int $locationId2): bool
     {
         try {
@@ -318,14 +208,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * Creates a new location in given $parentNode.
-     *
-     * @param \eZ\Publish\SPI\Persistence\Content\Location\CreateStruct $createStruct
-     * @param array $parentNode
-     *
-     * @return \eZ\Publish\SPI\Persistence\Content\Location
-     */
     public function create(CreateStruct $createStruct, array $parentNode)
     {
         try {
@@ -335,13 +217,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * Create an entry in the node assignment table.
-     *
-     * @param \eZ\Publish\SPI\Persistence\Content\Location\CreateStruct $createStruct
-     * @param mixed $parentNodeId
-     * @param int $type
-     */
     public function createNodeAssignment(CreateStruct $createStruct, $parentNodeId, $type = self::NODE_ASSIGNMENT_OP_CODE_CREATE_NOP)
     {
         try {
@@ -351,12 +226,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * Deletes node assignment for given $contentId and $versionNo.
-     *
-     * @param int $contentId
-     * @param int $versionNo
-     */
     public function deleteNodeAssignment($contentId, $versionNo = null)
     {
         try {
@@ -366,14 +235,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * Updates an existing location.
-     *
-     * Will not throw anything if location id is invalid or no entries are affected.
-     *
-     * @param \eZ\Publish\SPI\Persistence\Content\Location\UpdateStruct $location
-     * @param int $locationId
-     */
     public function update(UpdateStruct $location, $locationId)
     {
         try {
@@ -383,13 +244,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * Updates path identification string for given $locationId.
-     *
-     * @param mixed $locationId
-     * @param mixed $parentLocationId
-     * @param string $text
-     */
     public function updatePathIdentificationString($locationId, $parentLocationId, $text)
     {
         try {
@@ -399,11 +253,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * Deletes ezcontentobject_tree row for given $locationId (node_id).
-     *
-     * @param mixed $locationId
-     */
     public function removeLocation($locationId)
     {
         try {
@@ -413,18 +262,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * Returns id of the next in line node to be set as a new main node.
-     *
-     * This returns lowest node id for content identified by $contentId, and not of
-     * the node identified by given $locationId (current main node).
-     * Assumes that content has more than one location.
-     *
-     * @param mixed $contentId
-     * @param mixed $locationId
-     *
-     * @return array
-     */
     public function getFallbackMainNodeData($contentId, $locationId)
     {
         try {
@@ -434,15 +271,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * Sends a single location identified by given $locationId to the trash.
-     *
-     * The associated content object is left untouched.
-     *
-     * @param mixed $locationId
-     *
-     * @return bool
-     */
     public function trashLocation($locationId)
     {
         try {
@@ -452,19 +280,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * Returns a trashed location to normal state.
-     *
-     * Recreates the originally trashed location in the new position. If no new
-     * position has been specified, it will be tried to re-create the location
-     * at the old position. If this is not possible ( because the old location
-     * does not exist any more) and exception is thrown.
-     *
-     * @param mixed $locationId
-     * @param mixed $newParentId
-     *
-     * @return \eZ\Publish\SPI\Persistence\Content\Location
-     */
     public function untrashLocation($locationId, $newParentId = null)
     {
         try {
@@ -474,13 +289,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * Loads trash data specified by location ID.
-     *
-     * @param mixed $locationId
-     *
-     * @return array
-     */
     public function loadTrashByLocation($locationId)
     {
         try {
@@ -490,12 +298,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * Removes every entries in the trash.
-     * Will NOT remove associated content objects nor attributes.
-     *
-     * Basically truncates ezcontentobject_trash table.
-     */
     public function cleanupTrash()
     {
         try {
@@ -505,16 +307,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * Lists trashed items.
-     * Returns entries from ezcontentobject_trash.
-     *
-     * @param int $offset
-     * @param int $limit
-     * @param array $sort
-     *
-     * @return array
-     */
     public function listTrashed($offset, $limit, array $sort = null)
     {
         try {
@@ -524,12 +316,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * Removes trashed element identified by $id from trash.
-     * Will NOT remove associated content object nor attributes.
-     *
-     * @param int $id The trashed location Id
-     */
     public function removeElementFromTrash($id)
     {
         try {
@@ -539,14 +325,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * Set section on all content objects in the subtree.
-     *
-     * @param mixed $pathString
-     * @param mixed $sectionId
-     *
-     * @return bool
-     */
     public function setSectionForSubtree($pathString, $sectionId)
     {
         try {
@@ -556,13 +334,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * Returns how many locations given content object identified by $contentId has.
-     *
-     * @param int $contentId
-     *
-     * @return int
-     */
     public function countLocationsByContentId($contentId)
     {
         try {
@@ -572,18 +343,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * Changes main location of content identified by given $contentId to location identified by given $locationId.
-     *
-     * Updates ezcontentobject_tree table for the given $contentId and eznode_assignment table for the given
-     * $contentId, $parentLocationId and $versionNo
-     *
-     * @param mixed $contentId
-     * @param mixed $locationId
-     * @param mixed $versionNo version number, needed to update eznode_assignment table
-     * @param mixed $parentLocationId parent location of location identified by $locationId, needed to update
-     *        eznode_assignment table
-     */
     public function changeMainLocation($contentId, $locationId, $versionNo, $parentLocationId)
     {
         try {
@@ -593,13 +352,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * Get the total number of all Locations, except the Root node.
-     *
-     * @see loadAllLocationsData
-     *
-     * @return int
-     */
     public function countAllLocations()
     {
         try {
@@ -609,14 +361,6 @@ class ExceptionConversion extends Gateway
         }
     }
 
-    /**
-     * Load data of every Location, except the Root node.
-     *
-     * @param int $offset Paginator offset
-     * @param int $limit Paginator limit
-     *
-     * @return array
-     */
     public function loadAllLocationsData($offset, $limit)
     {
         try {
