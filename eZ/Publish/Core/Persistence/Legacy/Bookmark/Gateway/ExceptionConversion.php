0b18a945d121eab90c4855cf2c53703ccc17fd11
Mikołaj Adamczyk <mikadamczyk@gmail.com>
2019-10-11 10:15:26 +0200

    Refactored Bookmark's Exception Conversion to use multi catch

--- eZ/Publish/Core/Persistence/Legacy/Bookmark/Gateway/ExceptionConversion.php
+++ eZ/Publish/Core/Persistence/Legacy/Bookmark/Gateway/ExceptionConversion.php
@@ -9,23 +9,19 @@ declare(strict_types=1);
 namespace eZ\Publish\Core\Persistence\Legacy\Bookmark\Gateway;
 
 use Doctrine\DBAL\DBALException;
+use eZ\Publish\Core\Base\Exceptions\DatabaseException;
 use eZ\Publish\Core\Persistence\Legacy\Bookmark\Gateway;
 use eZ\Publish\SPI\Persistence\Bookmark\Bookmark;
 use PDOException;
-use RuntimeException;
 
 class ExceptionConversion extends Gateway
 {
     /**
-     * The wrapped gateway.
-     *
      * @var \eZ\Publish\Core\Persistence\Legacy\Bookmark\Gateway
      */
     protected $innerGateway;
 
     /**
-     * ExceptionConversion constructor.
-     *
      * @param \eZ\Publish\Core\Persistence\Legacy\Bookmark\Gateway $innerGateway
      */
     public function __construct(Gateway $innerGateway)
@@ -33,75 +29,57 @@ class ExceptionConversion extends Gateway
         $this->innerGateway = $innerGateway;
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function insertBookmark(Bookmark $bookmark): int
     {
         try {
             return $this->innerGateway->insertBookmark($bookmark);
         } catch (DBALException | PDOException $e) {
-            throw new RuntimeException('Database error', 0, $e);
+            throw DatabaseException::wrap($e);
         }
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function deleteBookmark(int $id): void
     {
         try {
             $this->innerGateway->deleteBookmark($id);
         } catch (DBALException | PDOException $e) {
-            throw new RuntimeException('Database error', 0, $e);
+            throw DatabaseException::wrap($e);
         }
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function loadBookmarkDataByUserIdAndLocationId(int $userId, array $locationId): array
     {
         try {
             return $this->innerGateway->loadBookmarkDataByUserIdAndLocationId($userId, $locationId);
         } catch (DBALException | PDOException $e) {
-            throw new RuntimeException('Database error', 0, $e);
+            throw DatabaseException::wrap($e);
         }
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function loadUserBookmarks(int $userId, int $offset = 0, int $limit = -1): array
     {
         try {
             return $this->innerGateway->loadUserBookmarks($userId, $offset, $limit);
         } catch (DBALException | PDOException $e) {
-            throw new RuntimeException('Database error', 0, $e);
+            throw DatabaseException::wrap($e);
         }
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function countUserBookmarks(int $userId): int
     {
         try {
             return $this->innerGateway->countUserBookmarks($userId);
         } catch (DBALException | PDOException $e) {
-            throw new RuntimeException('Database error', 0, $e);
+            throw DatabaseException::wrap($e);
         }
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function locationSwapped(int $location1Id, int $location2Id): void
     {
         try {
             $this->innerGateway->locationSwapped($location1Id, $location2Id);
         } catch (DBALException | PDOException $e) {
-            throw new RuntimeException('Database error', 0, $e);
+            throw DatabaseException::wrap($e);
         }
     }
 }
