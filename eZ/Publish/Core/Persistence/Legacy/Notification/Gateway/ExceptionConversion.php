907ef4c45ec3dc8c4558c3ebe5b151d5bc1159a9
Mikołaj Adamczyk <mikadamczyk@gmail.com>
2019-10-16 09:48:42 +0200

    Refactored Notification's Exception Conversion to use DatabaseException

--- eZ/Publish/Core/Persistence/Legacy/Notification/Gateway/ExceptionConversion.php
+++ eZ/Publish/Core/Persistence/Legacy/Notification/Gateway/ExceptionConversion.php
@@ -9,11 +9,11 @@ declare(strict_types=1);
 namespace eZ\Publish\Core\Persistence\Legacy\Notification\Gateway;
 
 use Doctrine\DBAL\DBALException;
+use eZ\Publish\Core\Base\Exceptions\DatabaseException;
 use eZ\Publish\Core\Persistence\Legacy\Notification\Gateway;
 use eZ\Publish\SPI\Persistence\Notification\CreateStruct;
 use eZ\Publish\SPI\Persistence\Notification\Notification;
 use PDOException;
-use RuntimeException;
 
 class ExceptionConversion extends Gateway
 {
@@ -34,91 +34,66 @@ class ExceptionConversion extends Gateway
         $this->innerGateway = $innerGateway;
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function getNotificationById(int $notificationId): array
     {
         try {
             return $this->innerGateway->getNotificationById($notificationId);
         } catch (DBALException | PDOException $e) {
-            throw new RuntimeException('Database error', 0, $e);
+            throw DatabaseException::wrap($e);
         }
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function updateNotification(Notification $notification): void
     {
         try {
             $this->innerGateway->updateNotification($notification);
         } catch (DBALException | PDOException $e) {
-            throw new RuntimeException('Database error', 0, $e);
+            throw DatabaseException::wrap($e);
         }
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function countUserNotifications(int $userId): int
     {
         try {
             return $this->innerGateway->countUserNotifications($userId);
         } catch (DBALException | PDOException $e) {
-            throw new RuntimeException('Database error', 0, $e);
+            throw DatabaseException::wrap($e);
         }
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function countUserPendingNotifications(int $userId): int
     {
         try {
             return $this->innerGateway->countUserPendingNotifications($userId);
         } catch (DBALException | PDOException $e) {
-            throw new RuntimeException('Database error', 0, $e);
+            throw DatabaseException::wrap($e);
         }
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function loadUserNotifications(int $userId, int $offset = 0, int $limit = -1): array
     {
         try {
             return $this->innerGateway->loadUserNotifications($userId, $offset, $limit);
         } catch (DBALException | PDOException $e) {
-            throw new RuntimeException('Database error', 0, $e);
+            throw DatabaseException::wrap($e);
         }
     }
 
-    /**
-     * Store Notification ValueObject in persistent storage.
-     *
-     * @param \eZ\Publish\SPI\Persistence\Notification\CreateStruct $notification
-     *
-     * @return int
-     */
     public function insert(CreateStruct $notification): int
     {
         try {
             return $this->innerGateway->insert($notification);
         } catch (DBALException | PDOException $e) {
-            throw new RuntimeException('Database error', 0, $e);
+            throw DatabaseException::wrap($e);
         }
     }
 
-    /**
-     * @param int $notificationId
-     */
     public function delete(int $notificationId): void
     {
         try {
             $this->innerGateway->delete($notificationId);
         } catch (DBALException | PDOException $e) {
-            throw new RuntimeException('Database error', 0, $e);
+            throw DatabaseException::wrap($e);
         }
     }
 }
