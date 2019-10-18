7a431adb0880cbc85d0bd143ef06584b5f8aff04
Dawid Parafiński <dawid.parafinski@ez.no>
2019-10-18 16:43:22 +0200

    EZP-30979: moved ComparisonEngine Interface into SPI

---
+++ eZ/Publish/SPI/Comparison/ComparisonEngine.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace eZ\Publish\SPI\Comparison;
+
+use eZ\Publish\API\Repository\Values\Content\VersionDiff\ComparisonResult;
+
+interface ComparisonEngine
+{
+    public function compareFieldsData(ComparisonData $fieldA, ComparisonData $fieldB): ComparisonResult;
+}
