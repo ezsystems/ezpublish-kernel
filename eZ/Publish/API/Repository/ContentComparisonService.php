81a4b6ad56f0eb3b2d0f2c964d6d99024d6cee06
Dawid Parafiński <dawid.parafinski@ez.no>
2019-10-22 17:01:23 +0200

    EZP-30979: Added description to compareVersions method

--- eZ/Publish/API/Repository/ContentComparisonService.php
+++ eZ/Publish/API/Repository/ContentComparisonService.php
@@ -13,6 +13,24 @@ use eZ\Publish\API\Repository\Values\Content\VersionInfo;
 
 interface ContentComparisonService
 {
+    /**
+     * Calculate difference between data in fieldTypes in given Versions.
+     *
+     * Fields should implement \eZ\Publish\SPI\FieldType\Comparable
+     * and be registered with `ezplatform.field_type.comparable` tag to get proper data to ComparisonEngine.
+     *
+     * Engines should implement \eZ\Publish\SPI\Comparison\ComparisonEngine
+     * and be registered with `ezplatform.field_type.comparable.engine`.
+     *
+     * Only Versions in same language can be compared,
+     * if no $languageCode provided, initialLanguageCode ov $versionA is used.
+     *
+     * @param \eZ\Publish\API\Repository\Values\Content\VersionInfo $versionA
+     * @param \eZ\Publish\API\Repository\Values\Content\VersionInfo $versionB
+     * @param string|null $languageCode
+     *
+     * @return \eZ\Publish\API\Repository\Values\Content\VersionDiff\VersionDiff
+     */
     public function compareVersions(
         VersionInfo $versionA,
         VersionInfo $versionB,
