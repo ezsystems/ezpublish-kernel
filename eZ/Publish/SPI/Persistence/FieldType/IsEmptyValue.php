10db3688f20bfd5d0efb34fa4c5c4e34b7ff8dc2
Andrew Longosz <alongosz@users.noreply.github.com>
2019-10-30 11:55:49 +0100

    Renamed IsEmptyFieldType to FieldType\IsEmptyValue

--- eZ/Publish/SPI/Persistence/FieldType/IsEmptyValue.php
+++ eZ/Publish/SPI/Persistence/FieldType/IsEmptyValue.php
@@ -1,12 +1,10 @@
 <?php
 
 /**
- * File containing the FieldType interface.
- *
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
-namespace eZ\Publish\SPI\Persistence;
+namespace eZ\Publish\SPI\Persistence\FieldType;
 
 use eZ\Publish\SPI\Persistence\Content\FieldValue;
 
@@ -14,8 +12,11 @@ use eZ\Publish\SPI\Persistence\Content\FieldValue;
  * The field type interface which field types available to storage engines have to implement.
  *
  * @see \eZ\Publish\SPI\FieldType\FieldType
+ *
+ * @deprecated since 7.5.6. In 8.0 (for eZ Platform 3.0) it will be merged into the
+ *             `\eZ\Publish\SPI\Persistence\FieldType` interface
  */
-interface IsEmptyFieldType
+interface IsEmptyValue
 {
     /**
      * Returns the empty value for the field type that can be processed by the storage engine.
