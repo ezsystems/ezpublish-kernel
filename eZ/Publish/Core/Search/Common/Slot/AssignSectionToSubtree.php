25eaffb0eac8c341ded2d6d9562f883b6397ca50
Kamil Madejski <kamil.madejski@ez.no>
2019-11-07 15:58:06 +0100

    EZP-31113: Implemented indexing subtree after assigning Section to it

---
+++ eZ/Publish/Core/Search/Common/Slot/AssignSectionToSubtree.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace eZ\Publish\Core\Search\Common\Slot;
+
+use eZ\Publish\Core\Search\Common\Slot;
+use eZ\Publish\Core\SignalSlot\Signal;
+use eZ\Publish\Core\SignalSlot\Signal\SectionService\AssignSectionToSubtreeSignal;
+
+class AssignSectionToSubtree extends AbstractSubtree
+{
+    public function receive(Signal $signal)
+    {
+        if (!$signal instanceof AssignSectionToSubtreeSignal) {
+            return;
+        }
+
+        $this->indexSubtree($signal->locationId);
+    }
+}
