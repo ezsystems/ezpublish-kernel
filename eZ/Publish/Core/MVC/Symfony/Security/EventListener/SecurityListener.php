c1422eb4d38b916e2274beb157b72a0f2b370f7a
Thorsten Reiter <34274688+reithor@users.noreply.github.com>
2019-11-14 11:00:22 +0100

    Removed 2nd parameter on SecurityListener::hasAccess calls

--- eZ/Publish/Core/MVC/Symfony/Security/EventListener/SecurityListener.php
+++ eZ/Publish/Core/MVC/Symfony/Security/EventListener/SecurityListener.php
@@ -170,7 +170,7 @@ class SecurityListener implements EventSubscriberInterface
             return;
         }
 
-        if (!$this->hasAccess($siteAccess, $originalUser->getUsername())) {
+        if (!$this->hasAccess($siteAccess)) {
             throw new UnauthorizedSiteAccessException($siteAccess, $originalUser->getUsername());
         }
     }
@@ -203,7 +203,7 @@ class SecurityListener implements EventSubscriberInterface
         if (
             // Leave access to login route, so that user can attempt re-authentication.
             $request->attributes->get('_route') !== 'login'
-            && !$this->hasAccess($siteAccess, $token->getUsername())
+            && !$this->hasAccess($siteAccess)
         ) {
             throw new UnauthorizedSiteAccessException($siteAccess, $token->getUsername());
         }
