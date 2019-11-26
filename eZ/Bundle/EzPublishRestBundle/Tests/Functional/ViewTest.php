5115e6b77798a69a12cce3eb99fac2ba6c546ff8
Andrew Longosz <alongosz@users.noreply.github.com>
2019-11-26 11:42:53 +0100

    [Tests] Unified REST Functional ViewTest to accept multiple payloads

--- eZ/Bundle/EzPublishRestBundle/Tests/Functional/ViewTest.php
+++ eZ/Bundle/EzPublishRestBundle/Tests/Functional/ViewTest.php
@@ -8,54 +8,104 @@
  */
 namespace eZ\Bundle\EzPublishRestBundle\Tests\Functional;
 
+use stdClass;
+
 class ViewTest extends TestCase
 {
+    /** @var array */
+    private static $createdContentRemoteIds = [];
+
     /**
      * Covers POST /views.
+     *
+     * @dataProvider providerForTestViewRequest
+     *
+     * @param string $body
+     * @param string $format
+     * @param int $expectedResultsCount
+     * @param \stdClass[] $contentDataList list of items containing name and remoteId properties
      */
-    public function testViewRequestWithOrStatement()
+    public function testViewRequest($body, $format, $expectedResultsCount, array $contentDataList)
     {
-        $fooRemoteId = md5('View test content foo');
-        $barRemoteId = md5('View test content bar');
-        $this->createFolder('View test content foo', '/api/ezp/v2/content/locations/1/2', $fooRemoteId);
-        $this->createFolder('View test content bar', '/api/ezp/v2/content/locations/1/2', $barRemoteId);
+        $this->createTestContentItems($contentDataList);
 
-        $request = $this->createHttpRequest('POST', '/api/ezp/v2/views', 'ViewInput+xml', 'View+json');
-        $body = <<< XML
+        // search for Content
+        $request = $this->createHttpRequest(
+            'POST',
+            '/api/ezp/v2/views',
+            "ViewInput+{$format}",
+            'View+json'
+        );
+        $request->setContent($body);
+        $response = $this->sendHttpRequest($request);
+        $responseData = json_decode($response->getContent(), true);
+
+        self::assertEquals($expectedResultsCount, $responseData['View']['Result']['count']);
+    }
+
+    /**
+     * Data provider for testViewRequestWithOrStatement.
+     *
+     * @return array
+     */
+    public function providerForTestViewRequest()
+    {
+        $foo = new stdClass();
+        $foo->name = uniqid('View test content foo');
+        $foo->remoteId = md5($foo->name);
+
+        $bar = new stdClass();
+        $bar->name = uniqid('View test content bar');
+        $bar->remoteId = md5($bar->name);
+
+        return [
+            [
+                <<< XML
 <?xml version="1.0" encoding="UTF-8"?>
 <ViewInput>
   <identifier>TitleView</identifier>
   <Query>
     <Filter>
         <OR>
-            <ContentRemoteIdCriterion>{$fooRemoteId}</ContentRemoteIdCriterion>
-            <ContentRemoteIdCriterion>{$barRemoteId}</ContentRemoteIdCriterion>
+            <ContentRemoteIdCriterion>{$foo->remoteId}</ContentRemoteIdCriterion>
+            <ContentRemoteIdCriterion>{$bar->remoteId}</ContentRemoteIdCriterion>
         </OR>
     </Filter>
     <limit>10</limit>
     <offset>0</offset>
   </Query>
 </ViewInput>
-XML;
-        $request->setContent($body);
-        $response = $this->sendHttpRequest($request);
-        $responseData = json_decode($response->getContent(), true);
-
-        self::assertEquals(2, $responseData['View']['Result']['count']);
+XML,
+                'xml',
+                2,
+                [$foo, $bar],
+            ],
+            [
+                <<< JSON
+{
+  "ViewInput": {
+    "identifier": "TitleView",
+    "Query": {
+      "Filter": {
+        "OR": {
+          "ContentRemoteIdCriterion": [
+            "{$foo->remoteId}",
+            "{$bar->remoteId}"
+          ]
+        }
+      },
+      "limit": "10",
+      "offset": "0"
     }
-
-    /**
-     * Covers POST /views.
-     *
-     * @depends testViewRequestWithOrStatement
-     */
-    public function testViewRequestWithAndStatement()
-    {
-        $fooRemoteId = md5('View test content foo');
-        $barRemoteId = md5('View test content bar');
-
-        $request = $this->createHttpRequest('POST', '/api/ezp/v2/views', 'ViewInput+xml', 'View+json');
-        $body = <<< XML
+  }
+}
+JSON,
+                'json',
+                2,
+                [$foo, $bar],
+            ],
+            [
+                <<< XML
 <?xml version="1.0" encoding="UTF-8"?>
 <ViewInput>
   <identifier>TitleView</identifier>
@@ -63,21 +113,41 @@ XML;
     <Filter>
         <AND>
             <OR>
-                <ContentRemoteIdCriterion>{$fooRemoteId}</ContentRemoteIdCriterion>
-                <ContentRemoteIdCriterion>{$barRemoteId}</ContentRemoteIdCriterion>
+                <ContentRemoteIdCriterion>{$foo->remoteId}</ContentRemoteIdCriterion>
+                <ContentRemoteIdCriterion>{$bar->remoteId}</ContentRemoteIdCriterion>
             </OR>
-            <ContentRemoteIdCriterion>{$fooRemoteId}</ContentRemoteIdCriterion>
+            <ContentRemoteIdCriterion>{$foo->remoteId}</ContentRemoteIdCriterion>
         </AND>
     </Filter>
     <limit>10</limit>
     <offset>0</offset>
   </Query>
 </ViewInput>
-XML;
-        $request->setContent($body);
-        $response = $this->sendHttpRequest($request);
-        $responseData = json_decode($response->getContent(), true);
+XML,
+                'xml',
+                1,
+                [$foo, $bar],
+            ],
+        ];
+    }
 
-        self::assertEquals(1, $responseData['View']['Result']['count']);
+    /**
+     * @param \stdClass[] $contentDataList
+     */
+    private function createTestContentItems(array $contentDataList)
+    {
+        foreach ($contentDataList as $contentData) {
+            // skip creating already created items
+            if (in_array($contentData->remoteId, self::$createdContentRemoteIds)) {
+                continue;
+            }
+
+            $this->createFolder(
+                $contentData->name,
+                '/api/ezp/v2/content/locations/1/2',
+                $contentData->remoteId
+            );
+            self::$createdContentRemoteIds[] = $contentData->remoteId;
+        }
     }
 }
