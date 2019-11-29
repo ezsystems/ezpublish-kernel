501754710d42bc04824916b0ce82b400952fa34c
Andrew Longosz <alongosz@users.noreply.github.com>
2019-11-29 16:14:46 +0100

    [TMP] Merged alongosz/ezp-30806-or-crit-with-obj-list
    Squashed commit of the following:
    
    commit 4b8360e0b63d9f8a594063dadcf3a0b4e88cefe5
    Author: Andrew Longosz <alongosz@users.noreply.github.com>
    Date:   Thu Nov 28 15:09:12 2019 +0100
    
        [Tests] Refactored logical op. criterion parser tests to use common base
    
    commit eb146cc41edcb133478dda07431bd4145d4d2182
    Author: Andrew Longosz <alongosz@users.noreply.github.com>
    Date:   Thu Nov 28 12:26:23 2019 +0100
    
        [Tests] Provided more unit test cases for logical op. criterion parsers
    
        Added coverage for combinations of nested logical operators with criteria
    
    commit 4292722a589e09e44bff65f10326915356908065
    Author: Andrew Longosz <alongosz@users.noreply.github.com>
    Date:   Thu Nov 28 12:21:48 2019 +0100
    
        Added yet another payload to ViewTest to expose different regression
    
    commit 1e9dabfd36a66ce0b67dce57857c484863a97ad8
    Author: Andrew Longosz <alongosz@users.noreply.github.com>
    Date:   Thu Nov 28 12:20:09 2019 +0100
    
        fixup! Fixed processing REST LogicalOperator input with list of objects
    
    commit 82225ce4661a35d3fbf9c4edc21d15cd8746c193
    Author: Andrew Longosz <alongosz@users.noreply.github.com>
    Date:   Thu Nov 28 11:00:55 2019 +0100
    
        Refactored LogicalOr input criterion parser test to accept data sets
    
    commit fdf2aeac05705ae6b260e4f4415945dabe00aeea
    Author: Andrew Longosz <alongosz@users.noreply.github.com>
    Date:   Wed Nov 27 14:19:16 2019 +0100
    
        fixup! Deprecated passing to REST logical operator a list of objects
    
    commit c6ea2dee518159f8271f6a46817a6785b47268a9
    Author: Andrew Longosz <alongosz@users.noreply.github.com>
    Date:   Wed Nov 27 14:16:52 2019 +0100
    
        fixup! [Tests] Unified REST Functional ViewTest to accept multiple payloads
    
    commit 91d2c2282caeffc8c3fb958485a35735e611c13e
    Author: Andrew Longosz <alongosz@users.noreply.github.com>
    Date:   Wed Nov 27 13:27:39 2019 +0100
    
        Refactored LogicalAnd input criterion parser test to accept data sets
    
    commit b9d8b69cd944d8817feeafbcb829409eded3fe56
    Author: Andrew Longosz <alongosz@users.noreply.github.com>
    Date:   Tue Nov 26 12:23:54 2019 +0100
    
        Deprecated passing to REST logical operator a list of objects
    
        The reason for this deprecation is that this is inconsistent with the corresponding XML format. Transformation from XML to JSON will produce a list of values for a common key (XML tag).
    
    commit 672ef4a7ec7600f5504462c14776c07ebc65a9b1
    Author: Andrew Longosz <alongosz@users.noreply.github.com>
    Date:   Tue Nov 26 12:17:54 2019 +0100
    
        Fixed processing REST LogicalOperator input with list of objects
    
    commit 3d424e824c8b67e745352f7f6afaa6d5a3f436f5
    Author: Andrew Longosz <alongosz@users.noreply.github.com>
    Date:   Tue Nov 26 12:12:12 2019 +0100
    
        [Tests] Added to the ViewTest the payload which yielded an error
    
        Added a payload which used OR criterion (or any logical operator) with a list of objects
    
    commit 5115e6b77798a69a12cce3eb99fac2ba6c546ff8
    Author: Andrew Longosz <alongosz@users.noreply.github.com>
    Date:   Tue Nov 26 11:42:53 2019 +0100
    
        [Tests] Unified REST Functional ViewTest to accept multiple payloads

--- eZ/Publish/Core/REST/Server/Tests/Input/Parser/Criterion/LogicalAndTest.php
+++ eZ/Publish/Core/REST/Server/Tests/Input/Parser/Criterion/LogicalAndTest.php
@@ -1,29 +1,30 @@
 <?php
 
 /**
- * File containing a test class.
- *
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
 namespace eZ\Publish\Core\REST\Server\Tests\Input\Parser\Criterion;
 
 use eZ\Publish\API\Repository\Values\Content;
+use eZ\Publish\Core\REST\Common\Exceptions\Parser as ParserException;
 use eZ\Publish\Core\REST\Common\Input\ParsingDispatcher;
 use eZ\Publish\Core\REST\Server\Input\Parser;
-use eZ\Publish\Core\REST\Server\Tests\Input\Parser\BaseTest;
 
-class LogicalAndTest extends BaseTest
+class LogicalAndTest extends LogicalOperatorTestCase
 {
     /**
-     * Logical parsing of AND statement.
+     * Data provider for LogicalOr::parse test.
+     *
+     * @see testParse
      *
      * Notice regarding multiple criteria of same type:
      *
-     * The XML decoder of EZ is not creating numeric arrays, instead using the tag as the array key. See
-     * variable $logicalAndParsedFromXml. This causes the ContentTypeIdentifierCriterion-Tag to appear as one-element
-     * (type numeric array) and two criteria configuration inside. The logical or parser will take care
-     * of this and return a flatt LogicalAnd criterion with 3 criteria inside.
+     * The XML decoder of eZ is not creating numeric arrays, instead using the tag as the array key.
+     * This causes the ContentTypeIdentifierCriterion Tag to appear as one-element
+     * (type numeric array) and two criteria configuration inside. The logical AND parser will take
+     * care of this and return a flat LogicalAnd criterion with 3 criteria inside for the following
+     * payload:
      *
      * ```
      * <AND>
@@ -37,49 +38,92 @@ class LogicalAndTest extends BaseTest
      * </AND>
      * ```
      */
-    public function testParseLogicalAnd()
+    public function getPayloads()
     {
-        $logicalAndParsedFromXml = [
-            'AND' => [
-                'ContentTypeIdentifierCriterion' => [
-                    0 => 'author',
-                    1 => 'book',
+        return [
+            'Simple AND Criterion' => [
+                [
+                    'AND' => [
+                        'ContentTypeIdentifierCriterion' => [
+                            0 => 'author',
+                            1 => 'book',
+                        ],
+                        'Field' => [
+                            'name' => 'title',
+                            'operator' => 'EQ',
+                            'value' => 'Contributing to projects',
+                        ],
+                    ],
                 ],
-                'Field' => [
-                    'name' => 'title',
-                    'operator' => 'EQ',
-                    'value' => 'Contributing to projects',
+                3,
+            ],
+            'Combined AND with nested OR Criterion' => [
+                [
+                    'AND' => [
+                        [
+                            'OR' => [
+                                'ContentTypeIdentifierCriterion' => [
+                                    'article',
+                                    'folder',
+                                ],
+                            ],
+                        ],
+                        [
+                            'OR' => [
+                                'ContentTypeIdentifierCriterion' => [
+                                    'forum',
+                                    'board',
+                                ],
+                            ],
+                        ],
+                    ],
                 ],
+                2,
+            ],
+            'Combined AND with nested OR Criterion using old format' => [
+                [
+                    'AND' => [
+                        [
+                            'OR' => [
+                                [
+                                    'ContentTypeIdentifierCriterion' => [
+                                        'article',
+                                        'folder',
+                                    ],
+                                ],
+                            ],
+                        ],
+                        [
+                            'OR' => [
+                                'ContentTypeIdentifierCriterion' => [
+                                    'forum',
+                                    'board',
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+                2,
             ],
         ];
-
-        $criterionMock = $this->createMock(Content\Query\Criterion::class, [], [], '', false);
-
-        $parserMock = $this->createMock(\eZ\Publish\Core\REST\Common\Input\Parser::class);
-        $parserMock->method('parse')->willReturn($criterionMock);
-
-        $result = $this->internalGetParser()->parse($logicalAndParsedFromXml, new ParsingDispatcher([
-            'application/vnd.ez.api.internal.criterion.ContentTypeIdentifier' => $parserMock,
-            'application/vnd.ez.api.internal.criterion.Field' => $parserMock,
-        ]));
-
-        self::assertInstanceOf(Content\Query\Criterion\LogicalAnd::class, $result);
-        self::assertCount(3, (array)$result->criteria);
     }
 
-    /**
-     * @expectedException \eZ\Publish\Core\REST\Common\Exceptions\Parser
-     */
     public function testThrowsExceptionOnInvalidAndStatement()
     {
+        $this->expectException(ParserException::class);
         $this->internalGetParser()->parse(['AND' => 'Should be an array'], new ParsingDispatcher());
     }
 
     /**
-     * @return Parser\Criterion\LogicalAnd
+     * @return \eZ\Publish\Core\REST\Server\Input\Parser\Criterion\LogicalAnd
      */
     protected function internalGetParser()
     {
         return new Parser\Criterion\LogicalAnd();
+    }
+
+    protected function getCriterionClass()
+    {
+        return Content\Query\Criterion\LogicalAnd::class;
     }
 }
