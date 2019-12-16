b7963b97c83aae19f1b9f0e2359323ac675ff938
Nattfarinn <nattfarinn@gmail.com>
2019-12-16 10:13:27 +0100

    fix: Missing license and declare strict types

--- eZ/Publish/Core/FieldType/ImageAsset/ImageAssetThumbnailStrategy.php
+++ eZ/Publish/Core/FieldType/ImageAsset/ImageAssetThumbnailStrategy.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
 namespace eZ\Publish\Core\FieldType\ImageAsset;
 
 use eZ\Publish\API\Repository\ContentService;
