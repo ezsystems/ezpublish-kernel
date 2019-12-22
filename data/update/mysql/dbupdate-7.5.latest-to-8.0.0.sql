9b9627d4dfd1dade8195f75abf6d484083d62de5
Andrew Longosz <alongosz@users.noreply.github.com>
2019-12-23 00:25:29 +0100

    Merged database upgrade scripts for 8.0.0

--- data/update/mysql/dbupdate-7.5.latest-to-8.0.0.sql
+++ data/update/mysql/dbupdate-7.5.latest-to-8.0.0.sql
@@ -1,3 +1,5 @@
 UPDATE ezsite_data SET value='8.0.0' WHERE name='ezpublish-version';
 
+ALTER TABLE ezcontentclass_attribute MODIFY data_text1 VARCHAR(255);
+
 ALTER TABLE ezcontentclass_attribute ADD COLUMN is_thumbnail TINYINT(1) NOT NULL DEFAULT '0';
