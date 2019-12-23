8e11091de8eb6815f8cac890804320c5d2a39aa2
Andrew Longosz <alongosz@users.noreply.github.com>
2019-12-23 01:14:04 +0100

    Merged database upgrade scripts for 8.0.0 (#2906)

--- data/update/mysql/dbupdate-7.5.3-to-8.0.0.sql
+++
@@ -1,3 +0,0 @@
-UPDATE ezsite_data SET value='8.0.0' WHERE name='ezpublish-version';
-
-ALTER TABLE ezcontentclass_attribute MODIFY data_text1 VARCHAR(255);
--- data/update/postgres/dbupdate-7.5.latest-to-8.0.0.sql
+++ data/update/postgres/dbupdate-7.5.latest-to-8.0.0.sql
@@ -1,3 +1,5 @@
 UPDATE ezsite_data SET value='8.0.0' WHERE name='ezpublish-version';
 
+ALTER TABLE ezcontentclass_attribute ALTER COLUMN data_text1 TYPE varchar(255);
+
 ALTER TABLE ezcontentclass_attribute ADD is_thumbnail boolean DEFAULT false NOT NULL;
