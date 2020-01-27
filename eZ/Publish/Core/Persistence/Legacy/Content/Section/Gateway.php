85bb1a42d7a1175e2096339196fa2f6e96e3d564
Andrew Longosz <alongosz@users.noreply.github.com>
2020-01-27 17:19:46 +0100

    [CS] Removed redundant PHPDoc blocks from Content Section gateways
    Aligned the code according to the following rules:
    * PHPDoc exists only for a base method.
    * Straightforward (from strict types) param and return attributes are not needed.

--- eZ/Publish/Core/Persistence/Legacy/Content/Section/Gateway.php
+++ eZ/Publish/Core/Persistence/Legacy/Content/Section/Gateway.php
@@ -18,86 +18,52 @@ abstract class Gateway
     /**
      * Inserts a new section with $name and $identifier.
      *
-     * @param string $name
-     * @param string $identifier
-     *
      * @return int The ID of the new section
      */
     abstract public function insertSection(string $name, string $identifier): int;
 
     /**
      * Updates section with $id to have $name and $identifier.
-     *
-     * @param int $id
-     * @param string $name
-     * @param string $identifier
      */
     abstract public function updateSection(int $id, string $name, string $identifier): void;
 
     /**
      * Loads data for section with $id.
-     *
-     * @param int $id
-     *
-     * @return string[][]
      */
     abstract public function loadSectionData(int $id): array;
 
     /**
      * Loads data for all sections.
-     *
-     * @return string[][]
      */
     abstract public function loadAllSectionData(): array;
 
     /**
      * Loads data for section with $identifier.
-     *
-     * @param string $identifier
-     *
-     * @return string[][]
      */
     abstract public function loadSectionDataByIdentifier(string $identifier): array;
 
     /**
      * Counts the number of content objects assigned to section with $id.
-     *
-     * @param int $id
-     *
-     * @return int
      */
     abstract public function countContentObjectsInSection(int $id): int;
 
     /**
      * Counts the number of role policies using section with $id in their limitations.
-     *
-     * @param int $id
-     *
-     * @return int
      */
     abstract public function countPoliciesUsingSection(int $id): int;
 
     /**
      * Counts the number of role assignments using section with $id in their limitations.
-     *
-     * @param int $id
-     *
-     * @return int
      */
     abstract public function countRoleAssignmentsUsingSection(int $id): int;
 
     /**
      * Deletes the Section with $id.
-     *
-     * @param int $id
      */
     abstract public function deleteSection(int $id): void;
 
     /**
      * Inserts the assignment of $contentId to $sectionId.
-     *
-     * @param int $sectionId
-     * @param int $contentId
      */
     abstract public function assignSectionToContent(int $sectionId, int $contentId): void;
 }
