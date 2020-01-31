b9fbdcbc5041e8aa1e3619204fec3406673799c6
Marek Nocoń <marek.nocon@ez.no>
2020-01-31 11:52:45 +0100

    [Behat] Added BehatSiteAccessInitializer

--- eZ/Bundle/PlatformBehatBundle/ServiceContainer/EzBehatExtension.php
+++ eZ/Bundle/PlatformBehatBundle/ServiceContainer/EzBehatExtension.php
@@ -1,44 +1,43 @@
 <?php
 
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
 namespace EzSystems\PlatformBehatBundle\ServiceContainer;
 
+use Behat\Behat\EventDispatcher\ServiceContainer\EventDispatcherExtension;
+use Behat\Symfony2Extension\ServiceContainer\Symfony2Extension;
 use Behat\Testwork\ServiceContainer\Extension;
 use Behat\Testwork\ServiceContainer\ExtensionManager;
+use eZ\Bundle\PlatformBehatBundle\Initializer\BehatSiteAccessInitializer;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Reference;
 
 /**
  * EzBehatExtension loads extension specific services.
  */
 class EzBehatExtension implements Extension
 {
-    /**
-     * {@inheritdoc}
-     */
     public function getConfigKey()
     {
         return 'ezbehatextension';
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function process(ContainerBuilder $container)
     {
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function initialize(ExtensionManager $extensionManager)
     {
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function configure(ArrayNodeDefinition $builder)
     {
     }
@@ -46,12 +45,24 @@ class EzBehatExtension implements Extension
     /**
      * Loads extension services into temporary container.
      *
-     * @param ContainerBuilder $container
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
      * @param array $config
      */
     public function load(ContainerBuilder $container, array $config)
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
+
+        $this->loadSiteAccessInitializer($container);
+    }
+
+    private function loadSiteAccessInitializer(ContainerBuilder $container): void
+    {
+        $definition = new Definition(BehatSiteAccessInitializer::class);
+        $definition->setArguments([
+            new Reference(Symfony2Extension::KERNEL_ID),
+        ]);
+        $definition->addTag(EventDispatcherExtension::SUBSCRIBER_TAG, ['priority' => 0]);
+        $container->setDefinition(BehatSiteAccessInitializer::class, $definition);
     }
 }
