33b405787bc7d59a380ecc68269e8bb27fb4fd59
Andrew Longosz <alongosz@users.noreply.github.com>
2020-02-21 21:41:17 +0100

    Enforced strict types in ObjectState gateway classes

--- eZ/Publish/Core/Persistence/Legacy/Content/ObjectState/Gateway/ExceptionConversion.php
+++ eZ/Publish/Core/Persistence/Legacy/Content/ObjectState/Gateway/ExceptionConversion.php
@@ -6,6 +6,8 @@
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
+declare(strict_types=1);
+
 namespace eZ\Publish\Core\Persistence\Legacy\Content\ObjectState\Gateway;
 
 use eZ\Publish\Core\Base\Exceptions\DatabaseException;
