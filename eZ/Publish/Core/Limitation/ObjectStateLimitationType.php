5aa50fa31e2a26334d180f558e8edf71c047363e
barw4 <bartek.rofl@gmail.com>
2020-03-04 15:45:13 +0100

    EZP-31002: Added ContentCreateStruct mock to unit test

--- eZ/Publish/Core/Limitation/ObjectStateLimitationType.php
+++ eZ/Publish/Core/Limitation/ObjectStateLimitationType.php
@@ -123,11 +123,11 @@ class ObjectStateLimitationType extends AbstractPersistenceLimitationType implem
         } elseif ($object instanceof VersionInfo) {
             $object = $object->getContentInfo();
         } elseif (!$object instanceof ContentInfo && !$object instanceof ContentCreateStruct) {
-            throw new InvalidArgumentException('$object', 'Must be of type: Content, VersionInfo, ContentInfo or ContentCreateStruct');
+            throw new InvalidArgumentException('$object', 'Must be of type: Content, VersionInfo, ContentInfo, or ContentCreateStruct');
         }
 
         // Skip evaluating for RootLocation
-        if (isset($object->mainLocationId) && 1 === $object->mainLocationId) {
+        if ($object instanceof ContentInfo && 1 === $object->mainLocationId) {
             return true;
         }
 
