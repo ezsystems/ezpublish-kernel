d5192f54ba03da4d96b3197d014bb0fc86fe7865
Andrew Longosz <alongosz@users.noreply.github.com>
2020-03-05 16:07:38 +0100

    Enforced strict types on Content Type gateway classes

--- eZ/Publish/Core/Persistence/Legacy/Content/Type/Gateway/ExceptionConversion.php
+++ eZ/Publish/Core/Persistence/Legacy/Content/Type/Gateway/ExceptionConversion.php
@@ -4,6 +4,8 @@
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
+declare(strict_types=1);
+
 namespace eZ\Publish\Core\Persistence\Legacy\Content\Type\Gateway;
 
 use eZ\Publish\Core\Base\Exceptions\DatabaseException;
