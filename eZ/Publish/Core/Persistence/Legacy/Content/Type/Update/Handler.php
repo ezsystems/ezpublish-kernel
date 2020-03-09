3fecadd3aaba6af101c05d3ccb3e5043ea1abb35
Andrew Longosz <alongosz@users.noreply.github.com>
2020-03-09 16:36:50 +0100

    EZP-31451: Cleaned up Content Type update handlers (#2976)
    * Dropped obsolete DeferredLegacy Content Type update handler
    
    * Fixed erroneous call to Content\Type\Gateway::delete method
    
    * [CS] Aligned Content Type update handler with eZ Platform Code Style
    
    * Documented the changes.

--- eZ/Publish/Core/Persistence/Legacy/Content/Type/Update/Handler.php
+++ eZ/Publish/Core/Persistence/Legacy/Content/Type/Update/Handler.php
@@ -1,40 +1,34 @@
 <?php
 
 /**
- * File containing the Type Update Handler base class.
- *
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
+declare(strict_types=1);
+
 namespace eZ\Publish\Core\Persistence\Legacy\Content\Type\Update;
 
 use eZ\Publish\SPI\Persistence\Content\Type;
 
 /**
  * Base class for update handlers.
+ *
+ * @internal For internal use by Repository.
  */
 abstract class Handler
 {
     /**
-     * Updates existing content objects from $fromType to $toType.
-     *
-     * @param \eZ\Publish\SPI\Persistence\Content\Type $fromType
-     * @param \eZ\Publish\SPI\Persistence\Content\Type $toType
+     * Update existing Content items from one version of a Content Type to another one.
      */
-    abstract public function updateContentObjects(Type $fromType, Type $toType);
+    abstract public function updateContentObjects(Type $fromType, Type $toType): void;
 
     /**
-     * Deletes $fromType and all of its field definitions.
-     *
-     * @param \eZ\Publish\SPI\Persistence\Content\Type $fromType
+     * Delete old version of a Content Type and all of its Field Definitions.
      */
-    abstract public function deleteOldType(Type $fromType);
+    abstract public function deleteOldType(Type $fromType): void;
 
     /**
-     * Publishes $toType to $newStatus.
-     *
-     * @param \eZ\Publish\SPI\Persistence\Content\Type $toType
-     * @param int $newStatus
+     * Change Content Type status.
      */
-    abstract public function publishNewType(Type $toType, $newStatus);
+    abstract public function publishNewType(Type $toType, int $newStatus): void;
 }
--- eZ/Publish/Core/Persistence/Legacy/Content/Type/Update/Handler/DoctrineDatabase.php
+++ eZ/Publish/Core/Persistence/Legacy/Content/Type/Update/Handler/DoctrineDatabase.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * File containing the DoctrineDatabase Type Update Handler class.
- *
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
+declare(strict_types=1);
+
 namespace eZ\Publish\Core\Persistence\Legacy\Content\Type\Update\Handler;
 
 use eZ\Publish\Core\Persistence\Legacy\Content\Type\Update\Handler;
@@ -15,38 +15,24 @@ use eZ\Publish\SPI\Persistence\Content\Type;
 
 /**
  * Doctrine database based type update handler.
+ *
+ * @internal For internal use by Repository
  */
-class DoctrineDatabase extends Handler
+final class DoctrineDatabase extends Handler
 {
     /** @var \eZ\Publish\Core\Persistence\Legacy\Content\Type\Gateway */
     protected $contentTypeGateway;
 
-    /**
-     * Content updater.
-     *
-     * @var \eZ\Publish\Core\Persistence\Legacy\Content\Type\ContentUpdater
-     */
+    /** @var \eZ\Publish\Core\Persistence\Legacy\Content\Type\ContentUpdater */
     protected $contentUpdater;
 
-    /**
-     * Creates a new content type update handler.
-     *
-     * @param \eZ\Publish\Core\Persistence\Legacy\Content\Type\Gateway $contentTypeGateway
-     * @param \eZ\Publish\Core\Persistence\Legacy\Content\Type\ContentUpdater $contentUpdater
-     */
     public function __construct(Gateway $contentTypeGateway, ContentUpdater $contentUpdater)
     {
         $this->contentTypeGateway = $contentTypeGateway;
         $this->contentUpdater = $contentUpdater;
     }
 
-    /**
-     * Updates existing content objects from $fromType to $toType.
-     *
-     * @param \eZ\Publish\SPI\Persistence\Content\Type $fromType
-     * @param \eZ\Publish\SPI\Persistence\Content\Type $toType
-     */
-    public function updateContentObjects(Type $fromType, Type $toType)
+    public function updateContentObjects(Type $fromType, Type $toType): void
     {
         $this->contentUpdater->applyUpdates(
             $fromType->id,
@@ -54,23 +40,12 @@ class DoctrineDatabase extends Handler
         );
     }
 
-    /**
-     * Deletes $fromType and all of its field definitions.
-     *
-     * @param \eZ\Publish\SPI\Persistence\Content\Type $fromType
-     */
-    public function deleteOldType(Type $fromType)
+    public function deleteOldType(Type $fromType): void
     {
-        $this->contentTypeGateway->delete($fromType->id, $fromType->status, $fromType->fieldDefinitions);
+        $this->contentTypeGateway->delete($fromType->id, $fromType->status);
     }
 
-    /**
-     * Publishes $toType to $newStatus.
-     *
-     * @param \eZ\Publish\SPI\Persistence\Content\Type $toType
-     * @param int $newStatus
-     */
-    public function publishNewType(Type $toType, $newStatus)
+    public function publishNewType(Type $toType, int $newStatus): void
     {
         $this->contentTypeGateway->publishTypeAndFields(
             $toType->id,
