6bcd19e01e88c7036b7d6f835ae7b9e758732eb7
Adam Wójs <adam.wojs@ez.no>
2020-03-09 08:17:52 +0100

    EZP-31481: Introduced strict types for eZ\Publish\API\Repository\Repository

--- eZ/Publish/Core/Repository/SiteAccessAware/Repository.php
+++ eZ/Publish/Core/Repository/SiteAccessAware/Repository.php
@@ -1,14 +1,32 @@
 <?php
 
 /**
- * Repository class.
- *
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
+declare(strict_types=1);
+
 namespace eZ\Publish\Core\Repository\SiteAccessAware;
 
 use eZ\Publish\API\Repository\Repository as RepositoryInterface;
+use eZ\Publish\API\Repository\NotificationService as NotificationServiceInterface;
+use eZ\Publish\API\Repository\BookmarkService as BookmarkServiceInterface;
+use eZ\Publish\API\Repository\ContentService as ContentServiceInterface;
+use eZ\Publish\API\Repository\ContentTypeService as ContentTypeServiceInterface;
+use eZ\Publish\API\Repository\FieldTypeService as FieldTypeServiceInterface;
+use eZ\Publish\API\Repository\LanguageService as LanguageServiceInterface;
+use eZ\Publish\API\Repository\LocationService as LocationServiceInterface;
+use eZ\Publish\API\Repository\ObjectStateService as ObjectStateServiceInterface;
+use eZ\Publish\API\Repository\PermissionResolver as PermissionResolverInterface;
+use eZ\Publish\API\Repository\RoleService as RoleServiceInterface;
+use eZ\Publish\API\Repository\SearchService as SearchServiceInterface;
+use eZ\Publish\API\Repository\SectionService as SectionServiceInterface;
+use eZ\Publish\API\Repository\TrashService as TrashServiceInterface;
+use eZ\Publish\API\Repository\URLAliasService as URLAliasServiceInterface;
+use eZ\Publish\API\Repository\URLService as URLServiceInterface;
+use eZ\Publish\API\Repository\URLWildcardService as URLWildcardServiceInterface;
+use eZ\Publish\API\Repository\UserPreferenceService as UserPreferenceServiceInterface;
+use eZ\Publish\API\Repository\UserService as UserServiceInterface;
 
 /**
  * Repository class.
@@ -82,113 +100,113 @@ class Repository implements RepositoryInterface
         $this->notificationService = $notificationService;
     }
 
-    public function sudo(callable $callback, RepositoryInterface $outerRepository = null)
+    public function sudo(callable $callback, ?RepositoryInterface $outerRepository = null)
     {
         return $this->repository->sudo($callback, $outerRepository ?? $this);
     }
 
-    public function getContentService()
+    public function getContentService(): ContentServiceInterface
     {
         return $this->contentService;
     }
 
-    public function getContentLanguageService()
+    public function getContentLanguageService(): LanguageServiceInterface
     {
         return $this->languageService;
     }
 
-    public function getContentTypeService()
+    public function getContentTypeService(): ContentTypeServiceInterface
     {
         return $this->contentTypeService;
     }
 
-    public function getLocationService()
+    public function getLocationService(): LocationServiceInterface
     {
         return $this->locationService;
     }
 
-    public function getTrashService()
+    public function getTrashService(): TrashServiceInterface
     {
         return $this->trashService;
     }
 
-    public function getSectionService()
+    public function getSectionService(): SectionServiceInterface
     {
         return $this->sectionService;
     }
 
-    public function getUserService()
+    public function getUserService(): UserServiceInterface
     {
         return $this->userService;
     }
 
-    public function getURLAliasService()
+    public function getURLAliasService(): URLAliasServiceInterface
     {
         return $this->urlAliasService;
     }
 
-    public function getURLWildcardService()
+    public function getURLWildcardService(): URLWildcardServiceInterface
     {
         return $this->repository->getURLWildcardService();
     }
 
-    public function getObjectStateService()
+    public function getObjectStateService(): ObjectStateServiceInterface
     {
         return $this->objectStateService;
     }
 
-    public function getRoleService()
+    public function getRoleService(): RoleServiceInterface
     {
         return $this->repository->getRoleService();
     }
 
-    public function getSearchService()
+    public function getSearchService(): SearchServiceInterface
     {
         return $this->searchService;
     }
 
-    public function getFieldTypeService()
+    public function getFieldTypeService(): FieldTypeServiceInterface
     {
         return $this->repository->getFieldTypeService();
     }
 
-    public function getPermissionResolver()
+    public function getPermissionResolver(): PermissionResolverInterface
     {
         return $this->repository->getPermissionResolver();
     }
 
-    public function getURLService()
+    public function getURLService(): URLServiceInterface
     {
         return $this->repository->getURLService();
     }
 
-    public function getBookmarkService()
+    public function getBookmarkService(): BookmarkServiceInterface
     {
         return $this->repository->getBookmarkService();
     }
 
-    public function getNotificationService()
+    public function getNotificationService(): NotificationServiceInterface
     {
         return $this->repository->getNotificationService();
     }
 
-    public function getUserPreferenceService()
+    public function getUserPreferenceService(): UserPreferenceServiceInterface
     {
         return $this->repository->getUserPreferenceService();
     }
 
-    public function beginTransaction()
+    public function beginTransaction(): void
     {
-        return $this->repository->beginTransaction();
+        $this->repository->beginTransaction();
     }
 
-    public function commit()
+    public function commit(): void
     {
-        return $this->repository->commit();
+        $this->repository->commit();
     }
 
-    public function rollback()
+    public function rollback(): void
     {
-        return $this->repository->rollback();
+        $this->repository->rollback();
     }
 }
