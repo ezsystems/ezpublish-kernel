0cb091fad5942b4738d8a44a9427835cf5f65255
Adam Wójs <adam@wojs.pl>
2020-03-15 11:28:45 +0100

    EZP-31482: Introduce strict types for \eZ\Publish\API\Values\User\UserRoleAssignment

--- eZ/Publish/Core/Repository/Values/User/UserRoleAssignment.php
+++ eZ/Publish/Core/Repository/Values/User/UserRoleAssignment.php
@@ -1,13 +1,14 @@
 <?php
 
 /**
- * File containing the eZ\Publish\Core\Repository\Values\User\UserRoleAssignment class.
- *
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
+declare(strict_types=1);
+
 namespace eZ\Publish\Core\Repository\Values\User;
 
+use eZ\Publish\API\Repository\Values\User\User as APIUser;
 use eZ\Publish\API\Repository\Values\User\UserRoleAssignment as APIUserRoleAssignment;
 
 /**
@@ -63,7 +64,7 @@ class UserRoleAssignment extends APIUserRoleAssignment
      *
      * @return \eZ\Publish\API\Repository\Values\User\User
      */
-    public function getUser()
+    public function getUser(): APIUser
     {
         return $this->user;
     }
