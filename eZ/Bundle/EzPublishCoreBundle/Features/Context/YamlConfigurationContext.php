586353204d60c87b895745207c9e3b15bb27edf3
Maciej Kobus <maciej.kobus@ez.no>
2020-03-16 17:18:10 +0100

    Fixed behat contexts

--- eZ/Bundle/EzPublishCoreBundle/Features/Context/YamlConfigurationContext.php
+++ eZ/Bundle/EzPublishCoreBundle/Features/Context/YamlConfigurationContext.php
@@ -9,6 +9,7 @@ use Behat\Behat\Context\Context;
 use Behat\Symfony2Extension\Context\KernelDictionary;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Yaml\Yaml;
 
 /**
@@ -19,14 +20,19 @@ use Symfony\Component\Yaml\Yaml;
  */
 class YamlConfigurationContext implements Context
 {
-    use KernelDictionary;
+    /** @var \Symfony\Component\HttpKernel\KernelInterface */
+    private $kernel;
 
     private static $platformConfigurationFilePath = 'config/packages/%env%/ezplatform.yaml';
 
+    public function __construct(KernelInterface $kernel)
+    {
+        $this->kernel = $kernel;
+    }
+
     public function addConfiguration(array $configuration)
     {
-        $kernel = $this->getKernel();
-        $env = $kernel->getEnvironment();
+        $env = $this->kernel->getEnvironment();
 
         $yamlString = Yaml::dump($configuration, 5, 4);
         $destinationFileName = 'ezplatform_behat_' . sha1($yamlString) . '.yaml';
@@ -38,7 +44,7 @@ class YamlConfigurationContext implements Context
 
         $this->addImportToPlatformYaml($destinationFileName, $env);
 
-        $application = new Application($kernel);
+        $application = new Application($this->kernel);
         $application->setAutoExit(false);
 
         $input = new ArrayInput([
