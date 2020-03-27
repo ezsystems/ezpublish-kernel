cb7400dbc2b2ae990258a20c873d8bcafec2f9ce
Mateusz Debinski <mateusz.debinski@ez.no>
2020-03-27 15:04:34 +0100

    EZP-31240: Added ContentTranslationHandler for delete translation document in solr

---
+++ eZ/Publish/SPI/Search/ContentTranslationHandler.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This file is part of the eZ Publish Kernel package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace eZ\Publish\SPI\Search;
+
+
+/**
+ * The Search Content translation handler
+ */
+interface ContentTranslationHandler
+{
+
+    /**
+     * Deletes a translation content object from the index.
+     *
+     * @param int $contentId
+     * @param string $languageCode
+     */
+    public function deleteTranslation($contentId, $languageCode);
+    
+}
