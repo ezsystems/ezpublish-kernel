450905bb55c3194221425c744652bb5576afaec8
mikolaj <mikolaj.adamczyk@ez.no>
2020-05-20 16:09:22 +0200

    Fixed CS issues

--- eZ/Publish/Core/MVC/Symfony/SiteAccess.php
+++ eZ/Publish/Core/MVC/Symfony/SiteAccess.php
@@ -1,11 +1,10 @@
 <?php
 
 /**
- * File containing the SiteAccess class.
- *
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
+
 namespace eZ\Publish\Core\MVC\Symfony;
 
 use eZ\Publish\API\Repository\Values\ValueObject;
@@ -57,7 +56,7 @@ class SiteAccess extends ValueObject implements JsonSerializable
         return [
             'name' => $this->name,
             'matchingType' => $this->matchingType,
-            'matcher' => $matcher
+            'matcher' => $matcher,
         ];
     }
 }
