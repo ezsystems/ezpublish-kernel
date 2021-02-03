4634cd6b46ff20f2e8ec74ae5aafe9b08f5b9709
Dawid Parafiński <dawid.parafinski@ez.no>
2021-02-03 17:25:59 +0100

    EZP-32308: Added tests

---
+++ eZ/Publish/API/Repository/Tests/Limitation/PermissionResolver/LocationLimitationIntegrationTest.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace eZ\Publish\API\Repository\Tests\Limitation\PermissionResolver;
+
+use eZ\Publish\API\Repository\Exceptions\InvalidArgumentException;
+use eZ\Publish\API\Repository\Values\User\Limitation\LocationLimitation;
+use eZ\Publish\SPI\Limitation\Target\Version;
+
+class LocationLimitationIntegrationTest extends BaseLimitationIntegrationTest
+{
+    private const LOCATION_ID = 2;
+
+    public function providerForCanUserEditOrPublishContent(): array
+    {
+        $limitationRoot = new LocationLimitation();
+        $limitationRoot->limitationValues = [self::LOCATION_ID];
+
+        return [
+            [[$limitationRoot], true],
+        ];
+    }
+
+    /**
+     * @dataProvider providerForCanUserEditOrPublishContent
+     *
+     * @param array $limitations
+     * @param bool $expectedResult
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\ForbiddenException
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException
+     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException
+     */
+    public function testCanUserEditContent(array $limitations, bool $expectedResult): void
+    {
+        $repository = $this->getRepository();
+        $locationService = $repository->getLocationService();
+
+        $location = $locationService->loadLocation(2);
+
+        $this->loginAsEditorUserWithLimitations('content', 'edit', $limitations);
+
+        $this->assertCanUser(
+            $expectedResult,
+            'content',
+            'edit',
+            $limitations,
+            $location->contentInfo,
+            [$location]
+        );
+
+        $this->assertCanUser(
+            $expectedResult,
+            'content',
+            'edit',
+            $limitations,
+            $location->contentInfo,
+            [$location, new Version(['allLanguageCodesList' => 'eng-GB'])]
+        );
+    }
+}
--- eZ/Publish/API/Repository/Tests/PermissionResolverTest.php
+++ eZ/Publish/API/Repository/Tests/PermissionResolverTest.php
@@ -6,6 +6,7 @@
  */
 namespace eZ\Publish\API\Repository\Tests;
 
+use eZ\Publish\SPI\Limitation\Target\Builder\VersionBuilder;
 use function array_filter;
 use eZ\Publish\API\Repository\Repository;
 use eZ\Publish\API\Repository\Values\Content\ContentCreateStruct;
@@ -1235,6 +1236,74 @@ class PermissionResolverTest extends BaseTest
         self::assertEquals(
             $expected,
             $permissionResolver->lookupLimitations($module, $function, $this->getContentCreateStruct($repository), [])
+        );
+    }
+
+    public function testLookupLimitationsWithMixedTargets(): void
+    {
+        $repository = $this->getRepository();
+        $userService = $repository->getUserService();
+        $permissionResolver = $repository->getPermissionResolver();
+        $roleService = $repository->getRoleService();
+        $locationService = $repository->getLocationService();
+
+        $location = $locationService->loadLocation(2);
+        $module = 'content';
+        $function = 'edit';
+
+        $role = $this->createRoleWithPolicies(
+            'role_' . __FUNCTION__,
+            [
+                [
+                    'module' => $module,
+                    'function' => $function,
+                    'limitations' => [
+                        new Limitation\LocationLimitation(['limitationValues' => [$location->id]])
+                    ]
+                ],
+                [
+                    'module' => $module,
+                    'function' => $function,
+                    'limitations' => [
+                        new Limitation\LanguageLimitation(['limitationValues' => ['eng-GB']])
+                    ]
+                ],
+
+            ]
+        );
+        $user = $this->createUser('user', 'John', 'Doe', $userService->loadUserGroup(4));
+        $roleService->assignRoleToUser($role, $user);
+        $permissionResolver->setCurrentUserReference($user);
+
+        $expected = new LookupLimitationResult(
+            true,
+            [],
+            [
+                new LookupPolicyLimitations(
+                    $role->getPolicies()[0],
+                    []
+                ),
+                new LookupPolicyLimitations(
+                    $role->getPolicies()[1],
+                    [
+                        new Limitation\LanguageLimitation(['limitationValues' => ['eng-GB']]),
+                    ]
+                ),
+            ]
+        );
+
+        self::assertEquals(
+            $expected,
+            $permissionResolver->lookupLimitations(
+                $module,
+                $function,
+                $location->contentInfo,
+                [
+                    (new VersionBuilder())->translateToAnyLanguageOf(['eng-GB'])->build(),
+                    $location
+                ],
+                [Limitation::LANGUAGE]
+            )
         );
     }
 
